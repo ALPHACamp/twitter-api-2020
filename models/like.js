b3a0eb19159c98c589983ec6a13889c2cb0c79ab
JessieCheng <hamy820326@gmail.com>
2020-07-20 22:21:38 +0800

    feat: init like model

--- models/like.js
+++ models/like.js
@@ -1,8 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
   }, {});
-  Like.associate = function(models) {
+  Like.associate = function (models) {
+    Like.belongsTo(models.User)
+    Like.belongsTo(models.Tweet)
   };
   return Like;
 };
