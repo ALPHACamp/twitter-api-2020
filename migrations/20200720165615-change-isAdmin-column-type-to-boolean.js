6588d5b00a68001aab74d3fd5fbee75b90810d77
JessieCheng <hamy820326@gmail.com>
2020-07-21 01:11:38 +0800

    fixed: change account, backgroundImage, isAdmin column type

---
+++ migrations/20200720165615-change-isAdmin-column-type-to-boolean.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+
+    return queryInterface.changeColumn('Users', 'isAdmin', {
+      type: Sequelize.BOOLEAN
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+
+    return queryInterface.changeColumn('Users', 'isAdmin', {
+      type: Sequelize.STRING
+    });
+  }
+};
