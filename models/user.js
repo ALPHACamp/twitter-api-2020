c4d007e22fb8b2d60589357af9881f824da95780
JessieCheng <hamy820326@gmail.com>
2020-07-20 22:10:28 +0800

    feat: init user table and model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    account: DataTypes.STRING,
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    avatar: DataTypes.STRING,
+    backgroundImage: DataTypes.STRING,
+    isAdmin: DataTypes.STRING
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Reply)
+    User.hasMany(models.Like)
+    User.hasMany(models.Followship)
   };
   return User;
 };
