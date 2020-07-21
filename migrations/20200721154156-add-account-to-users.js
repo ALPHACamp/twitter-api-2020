50673f22ab1e143a1be8f244b04857ea57630d35
Harry <harry811016@gmail.com>
2020-07-21 23:48:02 +0800

    add ccolum(coverImg and account)to user model

---
+++ migrations/20200721154156-add-account-to-users.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Users', 'account', {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Users', 'account');
+  }
+};
