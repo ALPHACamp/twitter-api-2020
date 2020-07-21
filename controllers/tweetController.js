a5cf4c74a37f97c8c8a9e480bfd3c1d7800a0cd5
Harry <harry811016@gmail.com>
2020-07-22 00:10:19 +0800

    add getTweet function and update reply model

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -5,7 +5,7 @@ const Reply = db.Reply
 const moment = require('moment')
 
 let restController = {
-  getTweets: (req, res, callback) => {
+  getTweets: (req, res) => {
     Tweet.findAll({
       order: [['createdAt', 'DESC']],
       include: [User, Reply, { model: User, as: 'LikedUsers' }]
@@ -13,15 +13,33 @@ let restController = {
       const data = tweets.map(r => ({
         ...r.dataValues,
         description: r.dataValues.description.substring(0, 50),
+        tweetCreatedAt: moment(r.dataValues.createdAt).fromNow(),
         userName: r.User.name,
         userAvatar: r.User.avatar,
-        tweetCreatedAt: moment(r.dataValues.createdAt).fromNow(),
         userAccount: r.User.account,
         replyConut: r.Replies.length,
         likeConut: r.LikedUsers.length,
         // isLiked: req.user.LikedTweets.map(d => d.id).includes(r.id)
       }))
       return res.json({ Tweets: data })
+    })
+  },
+  getTweet: (req, res) => {
+    return Tweet.findByPk(req.params.id, {
+      include: [
+        User,
+        { model: Reply, include: [User] },
+        { model: User, as: 'LikedUsers' }
+      ]
+    }).then(tweet => {
+      // const isLiked = restaurant.LikedUsers.map(d => d.id).includes(req.user.id)
+      return res.json({
+        tweet: tweet,
+        replyConut: tweet.Replies.length,
+        likeConut: tweet.LikedUsers.length,
+        tweetCreatedAt: moment(tweet.dataValues.createdAt).fromNow(),
+        // isLiked: isLiked
+      })
     })
   },
 }
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,5 @@ const tweetController = require('../controllers/tweetController.js')
 
 module.exports = (app) => {
   app.get('/tweets', tweetController.getTweets)
+  app.get('/tweets/:id', tweetController.getTweet)
 }
