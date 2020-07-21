089f15a538499e45acdebf709cc0a27c27525479
Harry <harry811016@gmail.com>
2020-07-21 17:41:33 +0800

    update tweet model and create tweetController.js/routes.js file

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
+    description: DataTypes.STRING,
+    UserId: DataTypes.INTEGER,
   }, {});
-  Tweet.associate = function(models) {
+  Tweet.associate = function (models) {
+    Tweet.belongsTo(models.User)
+    Tweet.hasMany(models.Reply)
+    Tweet.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'TweetId',
+      as: 'LikedUsers'
+    })
   };
   return Tweet;
 };
