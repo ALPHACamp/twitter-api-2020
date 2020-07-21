a4e0204ab8f3e7ae07c631e2132462e05141a0d6
JessieCheng <hamy820326@gmail.com>
2020-07-21 15:19:32 +0800

    feat: Add passport-jwt strategy and authentication
    Purpose
    - authenticated: check user
    - authenticatedAdmin: check admin
    
    Other
    - include session in app.js (passport need to use session)

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,25 @@
-// express
+// modules and files
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport.js')
+const helpers = require('../_helpers.js')
+
+// passport authentication
+const authenticated = passport.authenticate('jwt', { session: false })
+const authenticatedAdmin = (req, res, next) => {
+  if (helpers.getUser(req)) {
+    if (helpers.getUser(req).isAdmin) { return next() }
+    return res.json({ status: 'error', message: '沒有權限' })
+  } else {
+    return res.json({ status: 'error', message: '沒有權限' })
+  }
+}
 
 // call controller
 const testController = require('../controllers/testController.js')
 const userController = require('../controllers/userController.js')
 
-router.get('/test', testController.getTestData)
+router.get('/test', authenticated, authenticatedAdmin, testController.getTestData)
 router.post('/users', userController.signUp)
 router.post('/login', userController.signIn)
 
