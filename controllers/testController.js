3e8a0044638154d55a1b541f7c61baf36a83d0e8
HUANG Zong-Rong <maibells0@yahoo.com.tw>
2020-07-22 17:07:42 +0800

    refactor: remove test git commit changes

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,10 +1,6 @@
 const testController = {
   getTestData: (req, res) => {
     return res.json({ status: 'success', message: '我是測試資料' })
-  },
-
-  getNewTest: (req, res) => {
-    res.send('test')
   }
 }
 
