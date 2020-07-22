1c4c8354556aff84bc7549fe7fa2a867577f59a0
HUANG Zong-Rong <maibells0@yahoo.com.tw>
2020-07-22 16:24:23 +0800

    add something

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,6 +1,10 @@
 const testController = {
   getTestData: (req, res) => {
     return res.json({ status: 'success', message: '我是測試資料' })
+  },
+
+  getNewTest: (req, res) => {
+    res.send('test')
   }
 }
 
