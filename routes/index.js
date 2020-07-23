b09ad748a5391410c829cd4628939464434eba79
Harry <harry811016@gmail.com>
2020-07-23 16:41:47 +0800

    add authenticated middleware in each route

--- routes/index.js
+++ routes/index.js
@@ -23,10 +23,10 @@ module.exports = (app) => {
   app.get('/', authenticated, (req, res) => res.send('Hello World!')) //postman test
   app.get('/admin', authenticated, authenticatedAdmin, (req, res) => res.send('Hello Admin!')) //postman test
   app.get('/api/tweets', authenticated, tweetController.getTweets)
-  app.get('/api/tweets/:id', tweetController.getTweet)
-  app.post('/api/tweets', tweetController.postTweet)
-  app.post('/api/reply', replyController.postReply)
-  app.get('/api/users/:id', userController.getUser)
+  app.get('/api/tweets/:id', authenticated, tweetController.getTweet)
+  app.post('/api/tweets', authenticated, tweetController.postTweet)
+  app.post('/api/reply', authenticated, replyController.postReply)
+  app.get('/api/users/:id', authenticated, userController.getUser)
 
   app.post('/api/register', userController.register)
   app.post('/api/login', userController.login)
