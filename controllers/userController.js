5d1ef0e5b18954d0b18a1543b4973e81527f89d5
HUANG Zong-Rong <maibells0@yahoo.com.tw>
2020-07-23 17:58:19 +0800

    feat: create PUT /users/:id with file upload func

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,7 @@
+const fs = require('fs')
 const bcrypt = require('bcryptjs')
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const db = require('../models')
 const User = db.User
 
@@ -89,6 +92,94 @@ const userController = {
         })
       })
       .catch(err => res.json({ status: 'error', message: `${err}` }))
+  },
+
+  putUser: (req, res) => {
+    const userId = Number(req.params.id)
+    const account = req.body.account.trim()
+    const name = req.body.name.trim()
+    const email = req.body.email.trim()
+    const password = req.body.password.trim()
+    const introduction = req.body.introduction.trim()
+
+    if (!account || !name || !email || !password) {
+      return res.json({ status: 'error', message: '填寫資訊不完整' })
+    }
+
+    // 確認 email 和 account 和其他人的不一樣
+    // 如果 user.id 和 userId 一樣 => 輸入的資料和自己原有的資料一樣 => 沒有和其他人的重複 => 可以寫入
+    // 如果 user.id 和 userId 不一樣 => 輸入的資料和別人的資料一樣 => 和其他人的重複 => 不可以寫入
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user.id !== userId) return res.json({ status: 'error', message: '電子郵件已被註冊' })
+        return User.findOne({ where: { account } })
+      })
+      .then(user => {
+        if (user.id !== userId) return res.json({ status: 'error', message: '使用者帳號不可重複' })
+        return user
+      })
+      .then(user => {
+        /////////////////////// 下面是有問題的 code  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+        // 判斷有沒有 avatar 和 cover 圖片上傳項目
+        let { avatar, cover } = req.files
+        avatar = avatar ? avatar[0] : null
+        cover = cover ? cover[0] : null
+
+        console.log('avatar 資料', avatar)
+        console.log('cover 資料', cover)
+
+        const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+
+        const tasks = [avatar, cover].map(file => {
+          if (file) {  // tasks 陣列項目存在 => 轉成 Promise，負責上傳到 imgur，再更新到資料庫
+            console.log('上傳檔案資料', file)
+
+            return new Promise((resolve, reject) => {
+              imgur.setClientID(IMGUR_CLIENT_ID)
+              imgur.upload(file.path, (err, img) => {
+                user.update({
+                  account,
+                  name,
+                  email,
+                  password: hashedPassword,
+                  introduction,
+                  [file.fieldname]: file ? img.data.link : user[file.fieldname]
+                })
+              })
+            })
+          } else {  // 項目不存在 => 轉成 Promise，丟出文字訊息
+            return new Promise((resolve, reject) => {
+              resolve('no upload task')
+            })
+          }
+        })
+
+        console.log('tasks 內容', tasks)
+
+        if (!avatar && !cover) {  // 沒有上傳任何圖片，直接更新使用者的其他資料
+          return user.update({
+            account,
+            name,
+            email,
+            password: hashedPassword,
+            introduction,
+          })
+            .then(user => res.json({ status: 'success', message: 'simple user 成功更新使用者資料' }))
+        } else {  // 如果上傳一到二張圖片，等待每個 promise 確定狀態 (這邊會等待到 timeout)
+          return Promise.all(tasks)
+            .then(results => res.json({ status: 'success', message: 'promise 成功更新使用者資料' }))
+        }
+
+        /////////////////////// 上面是有問題的 code  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+      })
+      .then(results => {
+        console.log(results)
+      })
+      .catch(err => {
+        console.log(err)
+        res.json({ status: 'error', message: `${err}` })
+      })
   },
 }
 
