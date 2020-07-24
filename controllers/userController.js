b6dbeb38ffcd041191415d54bf37bdf079e83586
JessieCheng <hamy820326@gmail.com>
2020-07-25 00:45:34 +0800

    feat: Add /api/users/:id/tweets
    [Purpose]
    To get user's all tweets

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,7 @@ const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const db = require('../models')
 const User = db.User
+const Tweet = db.Tweet
 
 // JWT
 const jwt = require('jsonwebtoken')
@@ -181,6 +182,38 @@ const userController = {
         res.json({ status: 'error', message: `${err}` })
       })
   },
+
+  getUserTweets: (req, res) => {
+    const tweetsData = []
+
+    return Tweet.findAll({
+      raw: true,
+      nest: true,
+      where: { UserId: req.params.id },
+      order: [['createdAt', 'DESC']],
+      include: [User]
+    })
+      .then(tweets => {
+        for (const tweet of tweets) {
+          // 回傳值過濾 (role >> isAdmin, remove password)
+          tweet.User.isAdmin = Boolean(Number(tweet.User.role))
+          delete tweet.User.role
+          delete tweet.User.password
+
+          tweetsData.push({
+            status: 'success',
+            message: '成功找到使用者的推文資料',
+            ...tweet
+          })
+        }
+
+        return res.json(tweetsData)
+      })
+      .catch(err => {
+        console.log(err)
+        res.json({ status: 'error', message: `${err}` })
+      })
+  }
 }
 
 module.exports = userController
