4bf32d396029c9d037360a657c29f81453c44e0b
Harry <harry811016@gmail.com>
2020-07-24 19:04:56 +0800

    update getTweets/getTweet function /isLike/

--- models/tweet.js
+++ models/tweet.js
@@ -8,11 +8,11 @@ module.exports = (sequelize, DataTypes) => {
     Tweet.belongsTo(models.User)
     Tweet.hasMany(models.Reply)
     Tweet.hasMany(models.Like)
-    // Tweet.belongsToMany(models.User, {
-    //   through: models.Like,
-    //   foreignKey: 'TweetId',
-    //   as: 'LikedUsers'
-    // })
+    Tweet.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'TweetId',
+      as: 'LikedUsers'
+    })
   };
   return Tweet;
 };
