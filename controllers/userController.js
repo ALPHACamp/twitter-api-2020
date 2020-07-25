d3f22bfbd5f8cdcc75bb974251b713b5b1ec6a4d
Harry <harry811016@gmail.com>
2020-07-25 23:14:06 +0800

    update register function for error message

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,24 +14,20 @@ const { User, Tweet, Reply, Like, Followship } = db
 
 const userController = {
   register: (req, res) => {
-    const { account, name, email, password, passwordConfirm } = req.body
-    // const errors = []
-    // if (!account && !name && !email && !password && !passwordConfirm) {
-    //   errors.push({ status: 'error', message: 'all columns are empty' })
-    // } else if (!account) {
-    //   errors.push({ status: 'error', message: 'account is empty' })
-    // } else if (!name) {
-    //   errors.push({ status: 'error', message: 'name is empty' })
-    // } else if (!email) {
-    //   errors.push({ status: 'error', message: 'email is empty' })
-    // } else if (!password) {
-    //   errors.push({ status: 'error', message: 'password is empty' })
-    // } else if (!passwordConfirm) {
-    //   errors.push({ status: 'error', message: 'passwordConfirm is empty' })
-    // } else if (password !== passwordConfirm) {
-    //   errors.push({ status: 'error', message: 'password or passwordConfirm is incorrect' })
-    // }
-    // if (errors.length) return res.json(...errors);
+    const { account, name, email, password } = req.body
+    const errors = []
+    if (!account && !name && !email && !password) {
+      errors.push({ status: 'error', message: 'all columns are empty' })
+    } else if (!account) {
+      errors.push({ status: 'error', message: 'account is empty' })
+    } else if (!name) {
+      errors.push({ status: 'error', message: 'name is empty' })
+    } else if (!email) {
+      errors.push({ status: 'error', message: 'email is empty' })
+    } else if (!password) {
+      errors.push({ status: 'error', message: 'password is empty' })
+    }
+    if (errors.length) return res.json(...errors);
 
     User.findOne({ where: { account } })
       .then(userOwnedAccount => {
