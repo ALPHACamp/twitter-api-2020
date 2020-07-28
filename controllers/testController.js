296dae8fb85a35e442398ce55dfb9473525d3aec
JessieCheng <hamy820326@gmail.com>
2020-07-28 08:48:22 +0800

    fixed: admin can't pass auth problem

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,6 +1,8 @@
+const helpers = require('../_helpers.js')
+
 const testController = {
   getTestData: (req, res) => {
-    return res.json({ status: 'success', message: '我是測試資料' })
+    return res.json({ status: 'success', message: '我是測試資料', user: helpers.getUser(req).name })
   }
 }
 
