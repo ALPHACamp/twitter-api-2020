38df22f375b7fb7e5d1bb1c8e144620e75f3cb5b
JessieCheng <hamy820326@gmail.com>
2020-08-01 19:37:21 +0800

    feat: add on.send, emit.send, on.disconnect socket event
    [Other] online-users hasn't finished

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,31 +1,51 @@
+const chatController = require('../controllers/chatController.js');
 
-const chatSocket = (io, socket) => {
+const chatSocket = (io, socket, users) => {
 
-  // socket.on('connect', () => {
-  //   io.emit('connect', '你上線了')
+  // 第一次連線：傳回線上使用者清單列表，需要濾掉重複的 userId（一個使用者可能有多個 socket）
+  // const onlineUsersId = []
+  // users.forEach(user => {
+  //   if (!onlineUsersId.includes(user.userId)) {
+  //     onlineUsersId.push(user.userId)
+  //     /*
+  //     onlineUsersId.push({
+  //       id: user.userId,
+  //       avatar: '',
+  //       name: '',
+  //       account: ''
+  //     })
+  //     */
+  //   }
   // })
 
+  // console.log(onlineUsersId)
+  // console.log(chatController.getUsers(onlineUsersId))
+  // const response = chatController.getUsers(onlineUsersId).map(user => {
+  //   return {
+  //     id: user.id,
+  //     avatar: user.avatar,
+  //     name: user.name,
+  //     account: user.account
+  //   }
+  // })
+
+  io.emit('online-users', 'hi')
+
   socket.on('disconnect', () => {
-    console.log('一位使用者離線')
-    io.emit('disconnect', '收到字串了嗎')
+    console.log(`一位使用者離線 with socket id ${socket.id}`)
+    users.splice(users.findIndex(user => user.socketId === socket.id), 1)
+    console.log(users)
   })
 
   socket.on('send', (obj) => {
-    console.log('obj', obj)
-    console.log('使用者已送訊息')
-    io.emit('send', '歡迎你來')
+    console.log('使用者已送訊息', obj)
+    // broadcast
+    io.emit('message', {
+      id: users.find(user => user.socketId === socket.id).userId,
+      time: new Date(),
+      content: obj.content
+    })
   })
-
-  socket.on('online-users', (obj) => {
-    console.log('msg', `One more user`)
-    io.emit('online-users', '歡迎你來成為 online-user')
-  })
-
-  socket.on('message', msg => {
-    console.log('message: ' + msg)
-    io.emit('message', [msg, JSON.stringify({ status: 'success', message: '新增一個使用者' })])
-  })
-
 }
 
 module.exports = chatSocket
