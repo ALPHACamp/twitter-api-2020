36eaea8d0f781339680ed345da9f0cabd171ec8b
Ezra Tsai <ishow8166@icloud.com>
2020-10-09 11:08:05 +0800

    feat: install socket.io

--- app.js
+++ app.js
@@ -7,12 +7,35 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const port = process.env.PORT
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  socket.broadcast.emit('hi');
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+http.listen(2000, () => {
+  console.log('listening on *:2000');
+});
 
 require('./routes')(app)
 
