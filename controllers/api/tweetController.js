b9eb7b9d898bcd79c507915778babc983a81bfff
yunhsuanchin <alicechin20181@gmail.com>
2020-12-14 17:34:45 +0800

    Feat: add controllers and routes files

---
+++ controllers/api/tweetController.js
@@ -0,0 +1,25 @@
+const tweetController = {
+  getTweets: (req, res) => {
+
+  },
+  getTweet: (req, res) => {
+
+  },
+  addTweet: (req, res) => {
+
+  },
+  updateTweet: (req, res) => {
+
+  },
+  removeTweet: (req, res) => {
+
+  },
+  likeTweet: (req, res) => {
+
+  },
+  unlikeTweet: (req, res) => {
+
+  }
+}
+
+module.exports = tweetController
