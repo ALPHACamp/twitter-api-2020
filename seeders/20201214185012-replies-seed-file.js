94544ec5e4d403fff4d659fdaa6dc58be74e9fb2
iamy8000 <amy8000@gmail.com>
2020-12-15 03:09:03 +0800

    Add reply seeders

---
+++ seeders/20201214185012-replies-seed-file.js
@@ -0,0 +1,21 @@
+'use strict';
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Replies',
+      Array.from({ length: 150 }).map((item, index) =>
+        ({
+          id: index * 10 + 1,
+          UserId: Math.floor(Math.random() * 5) + 2,
+          TweetId: (Math.floor(Math.random() * 11) * 10 + 1),
+          comment: faker.lorem.text(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        })
+      ), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', null, {})
+  }
+};
