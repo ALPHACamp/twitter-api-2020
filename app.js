2b1bd9dde38e83e6fef6e34ed8bec6926d4a263c
Kaleb Kuo <klkuo.cx@gmail.com>
2020-12-14 14:21:49 +0800

    feat: heroku init

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 const helpers = require('./_helpers')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 routes(app)
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`))
