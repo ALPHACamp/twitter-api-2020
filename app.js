3a78eaff2cacaa0e5cf4a35c6c7370633b9661a7
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-14 15:07:33 +0800

    feat: model init

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const db = require('./models') // 引入資料庫
+const helpers = require('./_helpers')
 
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
+function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
-};
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  db.sequelize.sync()
+  console.log(`Example app listening on port ${port}!`)
+})
 
 module.exports = app
