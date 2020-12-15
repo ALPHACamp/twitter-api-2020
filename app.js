029fb76f9c4c7efab911f30eab95aed4db6474ce
Kaleb Kuo <klkuo.cx@gmail.com>
2020-12-15 19:23:49 +0800

    feat: add POST /api/signin (return jwt)

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const helpers = require('./_helpers')
 const bodyParser = require('body-parser')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 app.use(bodyParser.urlencoded({ extended: true }))
 routes(app)
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,35 @@
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const db = require('../models')
 const User = db.User
 
 const authController = {
   login: (req, res) => {
+    const { account, password } = req.body
 
+    if (!account || !password) {
+      return res.status(400).json({
+        status: 'failure',
+        message: 'all fields are required'
+      })
+    }
+
+    User.findOne({ where: { account } }).then(user => {
+      if (!user || !bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ status: 'failure', message: 'account or password is wrong' })
+      }
+
+      const payload = { id: user.id }
+      const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' })
+      return res.json({
+        status: 'success',
+        message: `${user.name}, welcome!`,
+        token,
+        user
+      })
+    })
   },
+
   register: (req, res) => {
     const { account, name, email, password, confirmPassword } = req.body
 
