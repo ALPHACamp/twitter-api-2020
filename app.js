0b0dc80ac51ba743e96369ae550fd43ba4575972
Kaleb Kuo <klkuo.cx@gmail.com>
2020-12-15 14:41:56 +0800

    feat: add POST /api/users to regiter

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const db = require('./models')
 const routes = require('./routes')
 const helpers = require('./_helpers')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(bodyParser.urlencoded({ extended: true }))
 routes(app)
 
 app.listen(port, () => {
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,54 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
 const authController = {
   login: (req, res) => {
 
   },
   register: (req, res) => {
+    const { account, name, email, password, confirmPassword } = req.body
 
+    if (password !== confirmPassword) {
+      return res.status(409).json({
+        status: 'failure',
+        message: 'Password & ConfirmPassword are different!'
+      })
+    }
+
+    User.findOrCreate({
+      where: {
+        $or: [
+          { account: { $eq: account } },
+          { email: { $eq: email } }
+        ]
+      },
+      defaults: {
+        account,
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+      }
+    }).then(([user, created]) => {
+      if (!created) {
+        if (user.account === account) {
+          return res.status(409).json({
+            status: 'failure',
+            message: `account: '${account}' has already existed!`
+          })
+        }
+        if (user.email === email) {
+          return res.status(409).json({
+            status: 'failure',
+            message: `email: '${email}' has already existed!`
+          })
+        }
+      }
+      return res.json({
+        status: 'success',
+        message: `account: '${user.account}' is registered successfully!`
+      })
+    })
   }
 }
 
