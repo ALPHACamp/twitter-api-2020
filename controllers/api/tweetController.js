9dfb7c4bb9a38aaf427a7eecf403d24157050b39
ccyang02 <chiachingyang02@gmail.com>
2020-12-17 01:55:02 +0800

    feat: add GET /tweets feature

--- controllers/api/tweetController.js
+++ controllers/api/tweetController.js
@@ -1,3 +1,12 @@
+const db = require('../../models')
+const Tweet = db.Tweet
+const User = db.User
+const Like = db.Like
+
+const sequelize = require('sequelize')
+const helpers = require('../../_helpers.js')
+
+
 const tweetController = {
 
   replyTweet: async (req, res, next) => { },
@@ -12,7 +21,39 @@ const tweetController = {
 
   postTweet: async (req, res, next) => { },
 
-  getTweets: async (req, res, next) => { },
+  getTweets: async (req, res, next) => {
+    try {
+      const rawTweets = await Tweet.findAll({
+        raw: true,
+        nest: true,
+        include: [{
+          model: User,
+          attributes: ['id', 'name', 'account', 'avatar']
+        }],
+        attributes: {
+          include: [
+            [sequelize.literal('(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id)'), 'repliesCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id)'), 'likesCount'],
+          ],
+          exclude: ['updatedAt']
+        }
+      })
+      const likedTweets = await Like.findAll({
+        raw: true,
+        nest: true,
+        where: { UserId: helpers.getUser(req).id },
+        attributes: ['TweetId']
+      })
+      const tweets = rawTweets.map(t => ({
+        ...t,
+        isLiked: likedTweets.map(element => element.TweetId).includes(t.id)
+      }))
+
+      return res.json(tweets)
+    } catch (error) {
+      next(error)
+    }
+  },
 
 }
 
