a3013e54b87f6b40b1c5aaccd64a0e787b56cdd0
Ahno Chen <ahnochen1029@gmail.com>
2020-12-16 16:41:31 +0800

    add cover column into Users model

--- models/user.js
+++ models/user.js
@@ -6,6 +6,7 @@ module.exports = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     name: DataTypes.STRING,
     avatar: DataTypes.STRING,
+    cover: DataTypes.STRING,
     introduction: DataTypes.TEXT,
     role: DataTypes.STRING,
   }, {});
