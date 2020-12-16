12550b1f346d3dd70d1474666b2c443822fa329c
Kaikai8888 <wuky812@gmail.com>
2020-12-16 17:46:56 +0800

    fix: correct login required field from email to account

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -26,17 +26,17 @@ const userController = {
 
   signIn: async (req, res, next) => {
     try {
-      const { email, password } = req.body
-      const user = await User.findOne({ where: { email }, raw: true })
+      const { account, password } = req.body
+      const user = await User.findOne({ where: { account }, raw: true })
 
       if (!bcrypt.compareSync(password, user.password)) return res.json({
         status: 'error',
-        message: 'Wrong email or password.'
+        message: '帳號或密碼錯誤'
       })
 
       return res.json({
         status: 'success',
-        message: 'Successfully login',
+        message: '成功登入',
         token: jwt.sign({ id: user.id }, process.env.JWT_SECRET),
         user: { id: user.id, name: user.name, email: user.email, role: user.role }
       })
