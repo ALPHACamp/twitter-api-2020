98ae526a88c075c6718dfc6678ded136e2dab3b6
Kaleb Kuo <klkuo.cx@gmail.com>
2020-12-16 10:12:36 +0800

    feat: add middleware/auth via passport

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const db = require('./models')
 const routes = require('./routes')
 const helpers = require('./_helpers')
 const bodyParser = require('body-parser')
+const usePassport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 app.use(bodyParser.urlencoded({ extended: true }))
+usePassport(app)
 routes(app)
 
 app.listen(port, () => {
