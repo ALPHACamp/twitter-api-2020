89efbbf59e0851a19f48f911a57b2b383710599c
elliottwuTW <elliott10009@gmail.com>
2020-12-17 10:49:42 +0800

    Feat: follow&unfollow (follow testing NOT passed)

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,11 @@ const { authToken, authUserRole, authAdminRole } = require('../middleware/auth')
 
 //routes
 
+
+//followship
+router.post('/api/followships', authToken, authUserRole, followshipController.createFollowship)
+router.delete('/api/followships/:id', authToken, authUserRole, followshipController.deleteFollowship)
+
 //users
 router.post('/api/login', userController.login)
 router.post('/api/users', userController.createUser)
