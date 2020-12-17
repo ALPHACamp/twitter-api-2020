c2ee6e75093722e52ba5a36cddff75479bfe059d
elliottwuTW <elliott10009@gmail.com>
2020-12-17 13:27:34 +0800

    Feat: reply CRUD

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,13 @@ const { authToken, authUserRole, authAdminRole } = require('../middleware/auth')
 
 //routes
 
+// reply
+router.post('/api/tweets/:id/replies', authToken, authUserRole, replyController.createReply)
+router.get('/api/tweets/:id/replies', authToken, authUserRole, replyController.getReplies)
+router.get('/api/replies/:id', authToken, authUserRole, replyController.getReply)
+router.put('/api/replies/:id', authToken, authUserRole, replyController.updateReply)
+router.delete('/api/replies/:id', authToken, authUserRole, replyController.deleteReply)
+
 //users
 router.post('/api/login', userController.login)
 router.post('/api/users', userController.createUser)
