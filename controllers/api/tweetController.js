cfd64eb666befcdd7b836dfa50eef861eb33d643
ccyang02 <chiachingyang02@gmail.com>
2020-12-18 00:10:43 +0800

    feat: add GET /tweets/:id & POST /tweets & POST /tweets/:id/replies & GET /tweets/:id/replies

--- controllers/api/tweetController.js
+++ controllers/api/tweetController.js
@@ -2,24 +2,98 @@ const db = require('../../models')
 const Tweet = db.Tweet
 const User = db.User
 const Like = db.Like
+const Reply = db.Reply
 
 const sequelize = require('sequelize')
 const helpers = require('../../_helpers.js')
+const { hacker } = require('faker')
 
 
 const tweetController = {
 
-  replyTweet: async (req, res, next) => { },
+  replyTweet: async (req, res, next) => {
+    try {
+      const tweetId = req.params.id
+      const tweet = await Tweet.findByPk(tweetId)
+      if (!tweet) return res.status(400).json({ status: 'error', message: '無此篇貼文' })
 
-  getReplies: async (req, res, next) => { },
+      const comment = req.body.comment
+      await Reply.create({
+        UserId: helpers.getUser(req).id,
+        TweetId: tweetId,
+        comment: comment
+      })
+      return res.json({ status: "success", message: "" })
+    } catch (error) {
+      next(error)
+    }
+  },
+
+  getReplies: async (req, res, next) => {
+    try {
+      const tweetId = req.params.id
+      const tweet = await Tweet.findByPk(tweetId)
+      if (!tweet) return res.status(400).json({ status: 'error', message: '無此篇貼文' })
+
+      const replies = await Reply.findAll({
+        raw: true,
+        nest: true,
+        where: { TweetId: tweetId },
+        include: [{
+          model: User,
+          attributes: ['id', 'name', 'account', 'avatar']
+        }]
+      })
+      replies.map(reply => {
+        reply.createdAt = reply.createdAt.getTime()
+        return reply
+      })
+
+      return res.json(replies)
+    } catch (error) {
+      next(error)
+    }
+  },
 
   likeTweet: async (req, res, next) => { },
 
   unlikeTweet: async (req, res, next) => { },
 
-  getTweet: async (req, res, next) => { },
+  getTweet: async (req, res, next) => {
+    try {
+      const tweet = await Tweet.findByPk(req.params.id, {
+        include: [{
+          model: User,
+          attributes: ['id', 'name', 'account', 'avatar']
+        }],
+        attributes: {
+          include: [
+            [sequelize.literal('(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id)'), 'repliesCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id)'), 'likesCount'],
+          ],
+          exclude: ['updatedAt']
+        }
+      })
+      tweet.createdAt = tweet.createdAt.getTime()
+      return res.json(tweet)
+    } catch (error) {
+      next(error)
+    }
+  },
 
-  postTweet: async (req, res, next) => { },
+  postTweet: async (req, res, next) => {
+    const { description, createdTimestamp } = req.body
+    try {
+      await Tweet.create({
+        UserId: helpers.getUser(req).id,
+        description: description,
+        // createdAt: Date(createdTimestamp) 1608215153858 frontend
+      })
+      return res.json({ status: "success", message: "" })
+    } catch (error) {
+      next(error)
+    }
+  },
 
   getTweets: async (req, res, next) => {
     try {
