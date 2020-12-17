ccc41e3e7782d862098ae9d27535328dcd67cdb6
Kaleb Kuo <klkuo.cx@gmail.com>
2020-12-17 21:06:37 +0800

    feat: add GET /api/users/:id to read user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,27 @@
 const helpers = require('../_helpers')
+const db = require('../models')
+const User = db.User
+const Tweet = db.Tweet
 
 const userController = {
   readUser: (req, res) => {
-    console.log(helpers.getUser(req).toJSON())
-    res.send(`Hello User ${helpers.getUser(req).name}!`)
+    const id = Number(req.params.id)
+    User.findOne({
+      where: { id },
+      include: [
+        Tweet,
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' },
+      ]
+    }).then(user => {
+      const userObj = Object.fromEntries(Object.entries(user.toJSON()).slice(0, 11))
+      userObj.isSelf = (user.id === req.user.id)
+      userObj.isFollowed = user.Followers.map(follower => follower.id).includes(helpers.getUser(req).id)
+      userObj.tweetsCount = user.Tweets.length
+      userObj.followingsCount = user.Followings.length
+      userObj.followersCount = user.Followers.length
+      return res.json(userObj)
+    }).catch(err => console.error(err))
   },
   readTweets: (req, res) => {
 
