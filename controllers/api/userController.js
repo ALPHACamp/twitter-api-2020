7b306a9efddcd6f7136a81310b1ee6a6f12260c3
yunhsuanchin <alicechin20181@gmail.com>
2020-12-17 17:50:23 +0800

    Feat: done for signup route

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,11 +1,41 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt-nodejs')
 const db = require('../../models')
+const { Op } = require('sequelize')
 const User = db.User
 
 const userController = {
-  signUp: (req, res) => {
-
+  signUp: async (req, res) => {
+    try {
+      const { account, name, email, password, passwordCheck } = req.body
+      if (!account || !name || !email || !password) {
+        return res.json({ status: 'error', message: "Required fields didn't exist." })
+      }
+      if (password !== passwordCheck) {
+        return res.json({ status: 'error', message: "Passwords didn't match." })
+      }
+      const users = await User.findAll({ raw: true, where: { [Op.or]: [{ email }, { account }] } })
+      if (users.length) {
+        return users.forEach((user) => {
+          if (user.account === account && user.email === email) {
+            return res.json({ status: 'error', message: 'This email and account are already in use.' })
+          } else if (user.account === account) {
+            return res.json({ status: 'error', message: 'This account is already in use.' })
+          } else if (user.email === email) {
+            return res.json({ status: 'error', message: 'This email is already in use.' })
+          }
+        })
+      }
+      await User.create({
+        account,
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+      })
+      res.json({ status: 'success', message: 'ok' })
+    } catch (error) {
+      console.log(error)
+    }
   },
   signIn: async (req, res) => {
     try {
