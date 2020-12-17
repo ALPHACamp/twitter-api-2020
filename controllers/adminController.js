25ada128551ecd1888a0e8a23f2c7b2645b7e5b1
Kaleb Kuo <klkuo.cx@gmail.com>
2020-12-17 14:05:47 +0800

    feat: add GET /api/admin/users to read users

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,8 +1,30 @@
 const helpers = require('../_helpers')
+const db = require('../models')
+const User = db.User
+const Tweet = db.Tweet
+const Like = db.Like
 
 const adminController = {
   readUsers: (req, res) => {
-    res.send(`Hello Admin ${helpers.getUser(req).name}!`)
+    User.findAll({
+      where: { role: 'user' },
+      include: [
+        Tweet,
+        Like,
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' },
+      ]
+    }).then(users => {
+      users = users.map(user => ({
+        ...(Object.fromEntries(Object.entries(user.dataValues).slice(0, 11))),
+        tweetsCount: user.dataValues.Tweets.length,
+        likesCount: user.dataValues.Likes.length,
+        followersCount: user.dataValues.Followers.length,
+        followingsCount: user.dataValues.Followings.length
+      }))
+      users = users.sort((a, b) => b.tweetsCount - a.tweetsCount)
+      return res.json(users)
+    }).catch(err => console.error(err))
   },
   deleteTweet: (req, res) => {
 
