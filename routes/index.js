7696954370104269441061a2ffcb63acb698197f
elliottwuTW <elliott10009@gmail.com>
2020-12-17 11:33:27 +0800

    Feat: like&unlike tweet

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,11 @@ const { authToken, authUserRole, authAdminRole } = require('../middleware/auth')
 
 //routes
 
+
+// like
+router.post('/api/tweets/:id/like', authToken, authUserRole, likeController.createLike)
+router.post('/api/tweets/:id/unlike', authToken, authUserRole, likeController.deleteLike)
+
 //users
 router.post('/api/login', userController.login)
 router.post('/api/users', userController.createUser)
