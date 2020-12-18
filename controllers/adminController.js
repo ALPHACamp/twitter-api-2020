f0d252b755a4b636c889cd56c4eca1dd81c930f6
r05323045 <r05323045@ntu.edu.tw>
2020-12-19 03:31:44 +0800

    add admin apis

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,20 @@
+const adminService = require('../services/adminServices.js')
+
 const adminController = {
-
+  getTweets: (req, res) => {
+    adminService.getTweets(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  getUsers: (req, res) => {
+    adminService.getUsers(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  deleteTweet: (req, res) => {
+    adminService.deleteTweet(req, res, (data) => {
+      return res.json(data)
+    })
+  }
 }
-
 module.exports = adminController
