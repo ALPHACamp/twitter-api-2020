dc4cbd8e3ccceb347cbe54f9e1cc81df5f73e90d
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-18 11:19:26 +0800

    feat: add POST /api/tweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,9 +1,26 @@
+const helpers = require('../_helpers')
+
+const db = require('../models')
+const Tweet = db.Tweet
+
 const tweetController = {
-  readTweets: (req, res) => {
-
-  },
+  readTweets: (req, res) => {},
   postTweet: (req, res) => {
-
+    const { description } = req.body
+    if (!description) {
+      return res.status(400).json({ status: 'failure', message: "description didn't exist" })
+    } else if (description.length > 140) {
+      return res.status(409).json({ status: 'failure', message: 'number of the words must between 1 ~ 140' })
+    } else {
+      Tweet.create({
+        description: description,
+        UserId: helpers.getUser(req).id
+      })
+        .then(tweet => {
+          return res.json({ status: 'success', message: 'tweet was successfully created', tweet })
+        })
+        .catch(err => next(err))
+    }
   }
 }
 
