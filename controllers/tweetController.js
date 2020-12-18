74992d8181a22317d4dde61d98413924908f22fd
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-18 11:21:22 +0800

    feat: add GET /api/tweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -2,9 +2,36 @@ const helpers = require('../_helpers')
 
 const db = require('../models')
 const Tweet = db.Tweet
+const User = db.User
+const Reply = db.Reply
+const Like = db.Like
 
 const tweetController = {
-  readTweets: (req, res) => {},
+  readTweets: (req, res) => {
+    Tweet.findAll({
+      order: [['createdAt', 'DESC']],
+      include: [
+        { model: User, attributes: ['id', 'account', 'name', 'avatar'] },
+        { model: Like, attributes: ['TweetId', 'UserId'] },
+        { model: Reply, attributes: ['TweetId'] }
+      ]
+    })
+      .then(tweets => {
+        tweets = tweets.map(tweet => ({
+          ...Object.keys(tweet.dataValues)
+            .slice(0, 6)
+            .reduce((result, key) => {
+              result[key] = tweet[key]
+              return result
+            }, {}),
+          isLiked: tweet.Likes.map(l => l.UserId).includes(helpers.getUser(req).id),
+          repliedCount: tweet.Replies.length,
+          LikeCount: tweet.Likes.length
+        }))
+        return res.json(tweets)
+      })
+      .catch(err => next(err))
+  },
   postTweet: (req, res) => {
     const { description } = req.body
     if (!description) {
