f14eeffa003ac3dec1edcfbc5dec20d0a5437cd6
Kaikai8888 <wuky812@gmail.com>
2020-12-18 16:06:19 +0800

    fix: modify followship routes to pass testing add id setting in followship model remove req.user.Followings check in delete route

--- controllers/api/followshipController.js
+++ controllers/api/followshipController.js
@@ -5,9 +5,8 @@ const followshipController = {
 
   deleteFollowing: async (req, res, next) => {
     try {
-      const followingId = Number(req.params.id)
+      const followingId = Number(req.params.followingId)
       if (!followingId) return res.status(400).json({ status: 'error', message: '無效的使用者id' })
-      if (!helpers.getUser(req).Followings.includes(followingId)) return res.json({ status: 'success', message: '未追蹤' })
       const followerId = helpers.getUser(req).id
       const followship = await Followship.findOne({ where: { followingId, followerId } })
       if (!followship) return res.json({ status: 'success', message: '未追蹤' })
@@ -21,19 +20,11 @@ const followshipController = {
   addFollowing: async (req, res, next) => {
     try {
       const followingId = Number(req.body.id)
-      console.log('@@1', followingId)
       if (!followingId) return res.status(400).json({ status: 'error', message: '無效的使用者id' })
-      console.log('@@2')
       if (helpers.getUser(req).Followings.includes(followingId)) return res.json({ status: 'success', message: '已追蹤' })
       const followerId = helpers.getUser(req).id
-      console.log('@@3', followerId)
       if (followingId === followerId) return res.status(400).json({ status: 'error', message: '不能追蹤自己' })
-      console.log('@@4', followerId)
-      const followship = await Followship.findOrCreate({ followingId, followerId })
-
-      console.log('@@5', followship)
-      const data = await Followship.findByPk(1)
-      console.log('@@6', data)
+      const followship = await Followship.findOrCreate({ where: { followingId, followerId } })
       return res.json({ status: 'success', message: '已追蹤' })
     } catch (error) {
       next(error)
--- models/followship.js
+++ models/followship.js
@@ -4,6 +4,11 @@ const user = require("./user");
 
 module.exports = (sequelize, DataTypes) => {
   const Followship = sequelize.define('Followship', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER
+    },
     followingId: DataTypes.INTEGER,
     followerId: DataTypes.INTEGER
   }, {
