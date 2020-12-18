15716e10bcc832282c1b7833a516967a99eef99c
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-18 21:37:11 +0800

    feat: add prevent not exist tweet feature

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -3,14 +3,23 @@ const helpers = require('../_helpers')
 const db = require('../models')
 const Reply = db.Reply
 const User = db.User
+const Tweet = db.Tweet
 
 const replyController = {
   readReplies: (req, res, next) => {
-    let whereQuery = {}
-    whereQuery.TweetId = req.params.id
-    Reply.findAll({ order: [['createdAt', 'DESC']], where: whereQuery, include: User })
-      .then(replies => {
-        return res.json(replies)
+    Tweet.findByPk(req.params.id)
+      .then(tweet => {
+        if (!tweet) {
+          return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
+        } else {
+          let whereQuery = {}
+          whereQuery.TweetId = req.params.id
+          return Reply.findAll({ order: [['createdAt', 'DESC']], where: whereQuery, include: User })
+            .then(replies => {
+              return res.json(replies)
+            })
+            .catch(next)
+        }
       })
       .catch(next)
   },
@@ -19,13 +28,21 @@ const replyController = {
     if (!req.body.comment) {
       return res.status(400).json({ status: 'failure', message: "number of the words can't be less than 1" })
     } else {
-      Reply.create({
-        comment,
-        TweetId: req.params.id,
-        UserId: helpers.getUser(req).id
-      })
-        .then(reply => {
-          return res.json({ status: 'success', message: 'OK', reply })
+      return Tweet.findByPk(req.params.id)
+        .then(tweet => {
+          if (!tweet) {
+            return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
+          } else {
+            return Reply.create({
+              comment,
+              TweetId: req.params.id,
+              UserId: helpers.getUser(req).id
+            })
+              .then(reply => {
+                return res.json({ status: 'success', message: 'OK', reply })
+              })
+              .catch(next)
+          }
         })
         .catch(next)
     }
