97e1becf9cbc6aeaedd8eb7583e0726bfd71127c
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-18 20:31:41 +0800

    feat: add POST /api/tweets/:id/replies

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -1,9 +1,25 @@
+const helpers = require('../_helpers')
+
+const db = require('../models')
+const Reply = db.Reply
+
 const replyController = {
-  readReplies: (req, res) => {
-
-  },
-  postReply: (req, res) => {
-
+  readReplies: (req, res) => {},
+  postReply: (req, res, next) => {
+    const { comment } = req.body
+    if (!req.body.comment) {
+      return res.status(400).json({ status: 'failure', message: "number of the words can't be less than 1" })
+    } else {
+      Reply.create({
+        comment,
+        TweetId: req.params.id,
+        UserId: helpers.getUser(req).id
+      })
+        .then(reply => {
+          return res.json({ status: 'success', message: 'OK', reply })
+        })
+        .catch(next)
+    }
   }
 }
 
