3470f255acb9960ae5f50a0284a4298643c0c1f5
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-20 01:05:22 +0800

    refactor: modify readReply and postReply more readable

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -7,45 +7,39 @@ const Tweet = db.Tweet
 
 const replyController = {
   readReplies: (req, res, next) => {
-    Tweet.findByPk(req.params.id)
+    Tweet.findByPk(req.params.id, {
+      include: { model: Reply, include: [{ model: User, attributes: ['id', 'account', 'name', 'avatar'] }] },
+      order: [['createdAt', 'DESC']]
+    })
       .then(tweet => {
         if (!tweet) {
           return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
-        } else {
-          let whereQuery = {}
-          whereQuery.TweetId = req.params.id
-          return Reply.findAll({ order: [['createdAt', 'DESC']], where: whereQuery, include: User })
-            .then(replies => {
-              return res.json(replies)
-            })
-            .catch(next)
         }
+        return res.json(tweet.Replies)
       })
       .catch(next)
   },
   postReply: (req, res, next) => {
     const { comment } = req.body
-    if (!req.body.comment) {
+    if (!comment) {
       return res.status(400).json({ status: 'failure', message: "number of the words can't be less than 1" })
-    } else {
-      return Tweet.findByPk(req.params.id)
-        .then(tweet => {
-          if (!tweet) {
-            return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
-          } else {
-            return Reply.create({
-              comment,
-              TweetId: req.params.id,
-              UserId: helpers.getUser(req).id
-            })
-              .then(reply => {
-                return res.json({ status: 'success', message: 'OK', reply })
-              })
-              .catch(next)
-          }
-        })
-        .catch(next)
     }
+    return Tweet.findByPk(req.params.id)
+      .then(tweet => {
+        if (!tweet) {
+          return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
+        }
+        return Reply.create({
+          comment,
+          TweetId: req.params.id,
+          UserId: helpers.getUser(req).id
+        })
+          .then(reply => {
+            return res.json({ status: 'success', message: 'OK', reply })
+          })
+          .catch(next)
+      })
+      .catch(next)
   }
 }
 
