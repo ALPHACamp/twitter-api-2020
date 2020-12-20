4c52ab94fc2ff5c4823b511c0a3d0f964147b44a
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-21 00:49:02 +0800

    feat: add POST /api/tweets/:id/like

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -1,10 +1,35 @@
+const helpers = require('../_helpers')
+
+const db = require('../models')
+const Like = db.Like
+const Tweet = db.Tweet
+
 const likeController = {
-  like: (req, res) => {
-
+  like: (req, res, next) => {
+    Tweet.findByPk(req.params.id, {
+      include: Like
+    }).then(tweet => {
+      if (!tweet) {
+        return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
+      }
+      if (tweet.UserId === helpers.getUser(req).id) {
+        return res.status(400).json({ status: 'failure', message: "Don't be narcissism" })
+      }
+      return Like.findOrCreate({
+        where: { TweetId: Number(req.params.id), UserId: Number(helpers.getUser(req).id) },
+        default: { UserId: Number(helpers.getUser(req).id) }
+      })
+        .spread((like, created) => {
+          if (!created) {
+            return res.status(409).json({ status: 'failure', message: 'Already Liked' })
+          } else {
+            return res.json({ status: 'success', message: 'OK', like })
+          }
+        })
+        .catch(next)
+    })
   },
-  unlike: (req, res) => {
-
-  }
+  unlike: (req, res) => {}
 }
 
 module.exports = likeController
