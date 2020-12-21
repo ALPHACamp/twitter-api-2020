65a8d0203241360bd9216ad17e93dee79d496b04
REN <windsailg@gmail.com>
2020-12-21 16:50:59 +0800

    fix cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const methodOverride = require('method-override')
 const helpers = require('./_helpers')
+const cors = require('cors')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -15,6 +16,7 @@ const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
