e5ecc38628ba4de5cef526c6d109084e29b01667
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-22 16:10:04 +0800

    feat: add POST /api/followships

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -1,10 +1,37 @@
+const helpers = require('../_helpers')
+
+const db = require('../models')
+const Followship = db.Followship
+const User = db.User
+
 const followshipController = {
-  follow: (req, res) => {
-
+  follow: (req, res, next) => {
+    const followerId = Number(helpers.getUser(req).id)
+    const followingId = Number(req.body.id)
+    User.findByPk(followingId)
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({ status: 'failure', message: 'this user not exist' })
+        }
+        if (followerId === followingId) {
+          return res.json({ status: 'failure', message: "Don't be narcissism" })
+        }
+        return Followship.findOrCreate({
+          where: { followingId, followerId },
+          default: { followingId, followerId }
+        })
+          .spread((follow, created) => {
+            if (!created) {
+              return res.json({ status: 'failure', message: 'Already Followed' })
+            } else {
+              return res.json({ status: 'success', message: 'OK', follow })
+            }
+          })
+          .catch(next)
+      })
+      .catch(next)
   },
-  unfollow: (req, res) => {
-
-  }
+  unfollow: (req, res) => {}
 }
 
 module.exports = followshipController
