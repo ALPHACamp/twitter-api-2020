5316cf56a161f00383ea323ced9d4795c13446c2
Ahno Chen <ahnochen1029@gmail.com>
2020-12-22 16:11:55 +0800

    followship seed

--- seeders/20201214185012-replies-seed-file.js
+++ seeders/20201214185012-replies-seed-file.js
@@ -4,14 +4,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Replies',
       Array.from({ length: 150 }).map((item, index) =>
-        ({
-          id: index * 10 + 1,
-          UserId: Math.floor(Math.random() * 5) + 2,
-          TweetId: (Math.floor(Math.random() * 11) * 10 + 1),
-          comment: faker.lorem.text(),
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        })
+      ({
+        id: index * 10 + 1,
+        UserId: Math.floor(Math.random() * 5) + 2,
+        TweetId: (Math.floor(Math.random() * 11) * 10 + 1),
+        comment: faker.lorem.text(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })
       ), {})
   },
 
