be91caa947a103e17e3b0cb6773503dcf5046398
Ahno Chen <ahnochen1029@gmail.com>
2020-12-22 16:42:46 +0800

    add likes-seed and add 5 users in user-seed

--- seeders/20201214081020-users-seed-file.js
+++ seeders/20201214081020-users-seed-file.js
@@ -81,6 +81,71 @@ module.exports = {
         role: 'user',
         createdAt: new Date(),
         updatedAt: new Date()
+      },
+      {
+        id: 7,
+        account: '@user6',
+        email: 'user6@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        name: 'user5',
+        avatar: `https://loremflickr.com/320/240/restaurant,food/?lock=${Math.random() * 100}`,
+        cover: 'https://m.dw.com/image/48396304_101.jpg',
+        introduction: 'I am Groot.',
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 8,
+        account: '@user7',
+        email: 'user7@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        name: 'user5',
+        avatar: `https://loremflickr.com/320/240/restaurant,food/?lock=${Math.random() * 100}`,
+        cover: 'https://m.dw.com/image/48396304_101.jpg',
+        introduction: 'I am Groot.',
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 9,
+        account: '@user8',
+        email: 'user8@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        name: 'user5',
+        avatar: `https://loremflickr.com/320/240/restaurant,food/?lock=${Math.random() * 100}`,
+        cover: 'https://m.dw.com/image/48396304_101.jpg',
+        introduction: 'I am Groot.',
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 10,
+        account: '@user9',
+        email: 'user9@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        name: 'user5',
+        avatar: `https://loremflickr.com/320/240/restaurant,food/?lock=${Math.random() * 100}`,
+        cover: 'https://m.dw.com/image/48396304_101.jpg',
+        introduction: 'I am Groot.',
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 11,
+        account: '@user10',
+        email: 'user10@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        name: 'user5',
+        avatar: `https://loremflickr.com/320/240/restaurant,food/?lock=${Math.random() * 100}`,
+        cover: 'https://m.dw.com/image/48396304_101.jpg',
+        introduction: 'I am Groot.',
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
       }
     ], {})
   },
--- seeders/20201222072915-followship-seed.js
+++ seeders/20201222072915-followship-seed.js
@@ -5,7 +5,7 @@ const { random } = require("faker");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const followships = []
-    for (let followerId = 1; followerId <= 6; followerId++) {
+    for (let followerId = 1; followerId <= 11; followerId++) {
       const followingId = Math.floor(Math.random() * 5) + 1
       if (followerId !== followingId) {
         followships.push({
