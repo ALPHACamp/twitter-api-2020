94a4050ea8c9b777597978b4e07d2ff032f0df44
Leonardo Roosevelt <nolanjeannnn@hotmail.com>
2020-12-22 15:47:12 +0800

    refactor: modify variable make more readable

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -6,52 +6,57 @@ const Tweet = db.Tweet
 
 const likeController = {
   like: (req, res, next) => {
-    Tweet.findByPk(req.params.id, {
-      include: Like
-    }).then(tweet => {
-      if (!tweet) {
-        return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
-      }
-      if (tweet.UserId === helpers.getUser(req).id) {
-        return res.status(400).json({ status: 'failure', message: "Don't be narcissism" })
-      }
-      return Like.findOrCreate({
-        where: { TweetId: Number(req.params.id), UserId: Number(helpers.getUser(req).id) },
-        default: { UserId: Number(helpers.getUser(req).id) }
-      })
-        .spread((like, created) => {
-          if (!created) {
-            return res.status(409).json({ status: 'failure', message: 'Already Liked' })
-          } else {
-            return res.json({ status: 'success', message: 'OK', like })
-          }
+    const tweetId = req.params.id
+    const userId = helpers.getUser(req).id
+
+    Tweet.findByPk(tweetId)
+      .then(tweet => {
+        if (!tweet) {
+          return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
+        }
+        if (tweet.UserId === userId) {
+          return res.status(400).json({ status: 'failure', message: "Don't be narcissism" })
+        }
+        return Like.findOrCreate({
+          where: { TweetId: Number(tweetId), UserId: Number(userId) },
+          default: { UserId: Number(userId) }
         })
-        .catch(next)
-    })
+          .spread((like, created) => {
+            if (!created) {
+              return res.status(409).json({ status: 'failure', message: 'Already Liked' })
+            } else {
+              return res.json({ status: 'success', message: 'OK', like })
+            }
+          })
+          .catch(next)
+      })
+      .catch(next)
   },
   unlike: (req, res, next) => {
-    Tweet.findByPk(req.params.id, {
-      include: Like
-    }).then(tweet => {
-      if (!tweet) {
-        return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
-      }
-      return Like.findOne({
-        where: {
-          UserId: helpers.getUser(req).id,
-          TweetId: req.params.id
+    const tweetId = req.params.id
+    const userId = helpers.getUser(req).id
+    Tweet.findByPk(tweetId)
+      .then(tweet => {
+        if (!tweet) {
+          return res.status(404).json({ status: 'failure', message: 'this tweet not exist' })
         }
-      })
-        .then(like => {
-          if (!like) {
-            return res.status(409).json({ status: 'failure', message: 'unlike not exist' })
+        return Like.findOne({
+          where: {
+            UserId: userId,
+            TweetId: tweetId
           }
-          return like.destroy().then(unliked => {
-            res.json({ status: 'success', message: 'OK', unliked })
-          })
         })
-        .catch(next)
-    })
+          .then(like => {
+            if (!like) {
+              return res.status(409).json({ status: 'failure', message: 'unlike not exist' })
+            }
+            return like.destroy().then(unliked => {
+              res.json({ status: 'success', message: 'OK', unliked })
+            })
+          })
+          .catch(next)
+      })
+      .catch(next)
   }
 }
 
