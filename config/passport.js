34481170880e0bb9f7023b1c5e0bcd0f1f02a927
郭耿綸 Kaleb <68842518+klkuocx@users.noreply.github.com>
2020-12-23 14:48:12 +0800

    refactor: delete codes that did not use

--- config/passport.js
+++ config/passport.js
@@ -26,26 +26,3 @@ passport.use(new JwtStrategy(jwtOptions, (jwt_payload, done) => {
 }))
 
 module.exports = passport
-
-// module.exports = app => {
-//   // init Passport 
-//   app.use(passport.initialize())
-
-//   // set JwtStrategy
-//   let jwtOptions = {}
-//   jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
-//   jwtOptions.secretOrKey = process.env.JWT_SECRET
-
-//   passport.use(new JwtStrategy(jwtOptions, (jwt_payload, done) => {
-//     User.findByPk(jwt_payload.id, {
-//       include: [
-//         Like,
-//         { model: User, as: 'Followers' },
-//         { model: User, as: 'Followings' }
-//       ]
-//     }).then(user => {
-//       if (!user) return done(null, false)
-//       return done(null, user)
-//     })
-//   }))
-// }
