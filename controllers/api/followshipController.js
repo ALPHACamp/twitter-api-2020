5c7c65de8bfb40003da9309777f3597f486fc8ea
Kaikai8888 <wuky812@gmail.com>
2020-12-23 11:10:13 +0800

    fix: Followings undefined issue in addFollowing

--- controllers/api/followshipController.js
+++ controllers/api/followshipController.js
@@ -21,7 +21,8 @@ const followshipController = {
     try {
       const followingId = Number(req.body.id)
       if (!followingId) return res.status(400).json({ status: 'error', message: '無效的使用者id' })
-      if (helpers.getUser(req).Followings.includes(followingId)) return res.json({ status: 'success', message: '已追蹤' })
+      const followings = helpers.getUser(req).Followings
+      if (followings && followings.includes(followingId)) return res.json({ status: 'success', message: '已追蹤' })
       const followerId = helpers.getUser(req).id
       if (followingId === followerId) return res.status(400).json({ status: 'error', message: '不能追蹤自己' })
       const followship = await Followship.findOrCreate({ where: { followingId, followerId } })
