c88b296060299dd8d4ab4cd0d9a7e055bff95454
ccyang02 <chiachingyang02@gmail.com>
2020-12-27 02:55:53 +0800

    feat: stream public room view page

--- routes/chat.js
+++ routes/chat.js
@@ -2,15 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('chat', { page: 'public-chat' })
+  res.render('public', { page: 'public-chat' })
 })
 
 router.get('/private', (req, res) => {
-  res.render('private-chat', { page: 'private-chat', home: true })
+  res.render('private', { page: 'private-chat', home: true })
 })
 
 router.get('/:userId', (req, res) => {
-  res.render('private-chat', { page: 'private-chat' })
+  res.render('private', { page: 'private-chat' })
 })
 
 
