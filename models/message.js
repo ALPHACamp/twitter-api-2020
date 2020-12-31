4c882110301315d8ec8fe9994a2498a4c88a9b40
iamy8000 <amy8000@gmail.com>
2020-12-31 14:49:38 +0800

    Modify message model

--- models/message.js
+++ models/message.js
@@ -4,6 +4,8 @@ module.exports = (sequelize, DataTypes) => {
     UserId: DataTypes.INTEGER,
     message: DataTypes.STRING,
     targetChannel: DataTypes.STRING,
+    type: DataTypes.STRING,
+    sendTo: DataTypes.INTEGER,
     isRead: DataTypes.BOOLEAN
   }, {})
   Message.associate = function (models) {
