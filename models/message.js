d82d39e28f36629fc1a81e923dc83c0cb8cdef51
iamy8000 <amy8000@gmail.com>
2020-12-31 14:35:36 +0800

    Add type and sendTo column to Message model

--- models/message.js
+++ models/message.js
@@ -1,12 +1,12 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Message = sequelize.define('Message', {
     UserId: DataTypes.INTEGER,
     message: DataTypes.STRING,
     targetChannel: DataTypes.STRING
-  }, {});
+  }, {})
   Message.associate = function (models) {
     Message.belongsTo(models.User)
-  };
-  return Message;
-};
+  }
+  return Message
+}
