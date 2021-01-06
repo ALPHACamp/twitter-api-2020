0fa2292b4bf43462efa56eabf1579fdfbdd4bd85
iamy8000 <amy8000@gmail.com>
2021-01-06 16:04:14 +0800

    create notification model

---
+++ migrations/20210106080247-create-notification.js
@@ -0,0 +1,36 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Notifications', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      senderId: {
+        type: Sequelize.INTEGER
+      },
+      recipientId: {
+        type: Sequelize.INTEGER
+      },
+      read: {
+        type: Sequelize.BOOLEAN
+      },
+      messageData: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Notifications')
+  }
+}
