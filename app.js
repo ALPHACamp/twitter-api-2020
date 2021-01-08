4de612f0a30919f0f84b5889edd31f7ba0e174f1
iamy8000 <amy8000@gmail.com>
2021-01-08 22:32:01 +0800

    add res.header setting at app.js

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ const io = require('socket.io')(http, {
   }
 })
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 http.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
 
 require('./routes')(app)
