1a54d60a97031373e5a7aff6740abbcb28591318
r05323045 <r05323045@ntu.edu.tw>
2021-01-08 22:47:17 +0800

    revert to PR init socket setting for notification feature

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ app.use((req, res, next) => {
 })
 
 app.use(cors())
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 const http = require('http').createServer(app)
 const io = require('socket.io')(http, {
   cors: {
