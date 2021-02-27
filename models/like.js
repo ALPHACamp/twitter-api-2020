976cdfc04db215b372bf8d6f76541971afcaf87c
Jackson Hsieh <jackson97042@gmail.com>
2021-02-27 15:41:47 +0800

    feat: add Like model association

--- models/like.js
+++ models/like.js
@@ -3,6 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
   }, {});
   Like.associate = function(models) {
+    Like.belongsTo(models.User)
+    Like.belongsTo(models.Tweet)
   };
   return Like;
 };
