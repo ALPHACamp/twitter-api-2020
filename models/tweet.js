238708ad82db770acb0acccfcce7bd1c11900482
Jackson Hsieh <jackson97042@gmail.com>
2021-02-27 15:52:17 +0800

    feat: add Tweet model association

--- models/tweet.js
+++ models/tweet.js
@@ -3,6 +3,9 @@ module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
   }, {});
   Tweet.associate = function(models) {
+    Tweet.belongsTo(models.User)
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
   };
   return Tweet;
 };
