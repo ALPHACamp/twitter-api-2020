6ce5040c500b501134439036620bd899b154aa9d
goater <goater1095@gmail.com>
2021-03-01 22:16:44 +0800

    add tweets seed

---
+++ seeders/20210301135954-tweets-seed-file.js
@@ -0,0 +1,21 @@
+'use strict';
+const faker = require('faker');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Tweets',
+      Array.from({ length: 50 }).map((d, i) => ({
+        UserId: Math.floor(i / 10), //UserId 為 0~4
+        description: faker.lorem.text(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })),
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Tweets', null, {});
+  },
+};
