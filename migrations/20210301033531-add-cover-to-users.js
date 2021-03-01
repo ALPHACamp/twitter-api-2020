85563e715b28162cab8b251af676b528f47000ce
Song Shao Ting <qwe820610@gmail.com>
2021-03-01 12:50:26 +0800

    feat: Add user seed data

--- migrations/20210301033531-add-cover-to-users.js
+++ migrations/20210301033531-add-cover-to-users.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Users', 'cover', {
-      type: Sequelize.BOOLEAN,
+      type: Sequelize.STRING,
       defaultValue: false,
     })
   },
