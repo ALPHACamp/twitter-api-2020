f0a8f6380f449ed0cd132125587a7ba42f8543ae
goater <goater1095@gmail.com>
2021-03-01 23:50:26 +0800

    modify replies seed to random UserId

--- seeders/20210301141736-replies-seed-file.js
+++ seeders/20210301141736-replies-seed-file.js
@@ -6,7 +6,7 @@ module.exports = {
     await queryInterface.bulkInsert(
       'Replies',
       Array.from({ length: 150 }).map((d, i) => ({
-        UserId: Math.floor(i / 30) + 2, //暫定2~6    //UserId 為 1 ,11 ,21 ,31 ,41
+        UserId: Math.floor(Math.random() * 5) + 2, //隨機2~6    //UserId 為 1 ,11 ,21 ,31 ,41
         TweetId: Math.floor(i / 3) + 1, //
         Comment: faker.lorem.text(),
         createdAt: new Date(),
