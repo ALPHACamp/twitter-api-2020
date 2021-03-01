640e26ac4c9ded2a8477dc7141ee1e129b35061f
goater <goater1095@gmail.com>
2021-03-01 23:29:40 +0800

    add replies seed

--- seeders/20210301141736-replies-seed-file.js
+++ seeders/20210301141736-replies-seed-file.js
@@ -4,10 +4,11 @@ const faker = require('faker');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      'Tweets',
-      Array.from({ length: 50 }).map((d, i) => ({
-        UserId: Math.floor(i / 10) + 2, //暫定2~6 //UserId 為 1 ,11 ,21 ,31 ,41
-        description: faker.lorem.text(),
+      'Replies',
+      Array.from({ length: 150 }).map((d, i) => ({
+        UserId: Math.floor(i / 30) + 2, //暫定2~6    //UserId 為 1 ,11 ,21 ,31 ,41
+        TweetId: Math.floor(i / 3) + 1, //
+        Comment: faker.lorem.text(),
         createdAt: new Date(),
         updatedAt: new Date(),
       })),
@@ -16,6 +17,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Tweets', null, {});
+    await queryInterface.bulkDelete('Replies', null, {});
   },
 };
