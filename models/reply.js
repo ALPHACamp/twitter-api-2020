5d6269f5359808534bcdf57241f27093e09a0fe7
subaruuuu <meteorbs13@gmail.com>
2021-03-01 10:11:30 +0800

    set datatypes for models

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER,
+    comment: DataTypes.TEXT
   }, {});
-  Reply.associate = function(models) {
+  Reply.associate = function (models) {
   };
   return Reply;
 };
