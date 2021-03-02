64d2fdfebd8d28f93545d7f660789d4855c49534
Allen-TaiLin <skygary2000@gmail.com>
2021-03-02 21:29:41 +0800

    Modify Models and migrations

--- models/like.js
+++ models/like.js
@@ -1,8 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    UserId: {
+      type: DataTypes.INTEGER
+    },
+    TweetId: {
+      type: DataTypes.INTEGER
+    }
   }, {});
-  Like.associate = function(models) {
+  Like.associate = function (models) {
+    Like.belongsTo(models.User)
+    Like.belongsTo(models.Tweet)
   };
   return Like;
 };
