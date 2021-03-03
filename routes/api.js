0524d927b82ab37e6b298890d2aa47b9fac56c5b
goater <goater1095@gmail.com>
2021-03-03 20:24:40 +0800

    add get & delete & post in followshipController

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,29 @@
-const express = require('express')
-const router = express.Router()
-const passport = require('../config/passport')
-const helpers = require('../_helpers')
-const userController = require('../controllers/api/userControllers')
-
+const express = require('express');
+const router = express.Router();
+const passport = require('../config/passport');
+const helpers = require('../_helpers');
+const userController = require('../controllers/api/userControllers');
+const followshipController = require('../controllers/api/followshipControllers');
 //尚未完成
-const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = passport.authenticate('jwt', { session: false });
 
 const authenticatedAdmin = (req, res, next) => {
   if (helpers.getUser(req)) {
-    if (helpers.getUser(req).isAdmin) { return next() }
-    return res.json({ status: 'error', message: 'permission denied' })
+    if (helpers.getUser(req).isAdmin) {
+      return next();
+    }
+    return res.json({ status: 'error', message: 'permission denied' });
   } else {
-    return res.json({ status: 'error', message: 'permission denied' })
+    return res.json({ status: 'error', message: 'permission denied' });
   }
-}
+};
 
-router.post('/signin', userController.signIn)
+router.post('/signin', userController.signIn);
 
-module.exports = router
+//followship
+router.get('/following', followshipController.getFollowing);
+router.get('/follower', followshipController.getFollower);
+router.post('/following/:id', followshipController.postFollowship);
+router.delete('/following/:id', followshipController.deleteFollowship);
+
+module.exports = router;
