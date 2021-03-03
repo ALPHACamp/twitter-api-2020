bce66331b9b33bcb22ae988ea282c5f4e1946565
stan_wang <a7569209@gmail.com>
2021-03-03 21:35:16 +0800

    change app&port position

--- app.js
+++ app.js
@@ -4,18 +4,15 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
-const app = express()
-const port = process.env.PORT || 3000
 
-// // use helpers.getUser(req) to replace req.user
-// function authenticated(req, res, next) {
-//   // passport.authenticate('jwt', { ses...
-// };
+
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const passport = require('./config/passport')
+const app = express()
+const port = process.env.PORT || 3000
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
