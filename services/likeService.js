7b45bf4b335eb1e2b13ede8bfdc97b71e84674c0
subaruuuu <meteorbs13@gmail.com>
2021-03-04 22:52:27 +0800

    refactor LikeService for async/await

--- services/likeService.js
+++ services/likeService.js
@@ -3,30 +3,38 @@ const Like = db.Like
 const helper = require('../_helpers')
 
 const likeService = {
-  addLike: (req, res, callback) => {
-    const user = helper.getUser(req)
-    Like.findOrCreate({
-      where: { TweetId: req.params.id },
-      defaults: {
-        UserId: user.id,
-        TweetId: req.params.id
-      }
-    }).then(() => {
+  addLike: async (req, res, callback) => {
+    try {
+      const user = helper.getUser(req)
+      await Like.findOrCreate({
+        where: { TweetId: req.params.id },
+        defaults: {
+          UserId: user.id,
+          TweetId: req.params.id
+        }
+      })
       callback({ status: 'success', message: '' })
-    })
+    } catch (err) {
+      console.log(err)
+      callback({ status: 'error', message: 'codeStatus 500' })
+    }
   },
-  removeLike: (req, res, callback) => {
-    const user = helper.getUser(req)
-    Like.findOne({
-      where: {
-        UserId: user.id,
-        TweetId: req.params.id
-      }
-    }).then(like => {
+  removeLike: async (req, res, callback) => {
+    try {
+      const user = helper.getUser(req)
+      const like = await Like.findOne({
+        where: {
+          UserId: user.id,
+          TweetId: req.params.id
+        }
+      })
       if (like === null) callback({ status: 'success', message: '' })
-      like.destroy()
+      await like.destroy()
       callback({ status: 'success', message: '' })
-    })
+    } catch (err) {
+      console.log(err)
+      callback({ status: 'error', message: 'codeStatus 500' })
+    }
   }
 }
 
