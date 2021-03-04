cac339574261164fc1e9f25afd31fe35ff157b96
subaruuuu <meteorbs13@gmail.com>
2021-03-04 14:36:16 +0800

    refactor tweetService async/await

--- services/tweetService.js
+++ services/tweetService.js
@@ -2,38 +2,45 @@ const db = require('../models')
 const { Tweet, Like, Reply } = db
 
 const tweetService = {
-  postTweet: (req, res, callback) => {
-    if (!req.body.description) {
-      callback({ status: 'error', message: "content didn't exist" })
+  postTweet: async (req, res, callback) => {
+    try {
+      if (!req.body.description) {
+        return callback({ status: 'error', message: "content didn't exist" })
+      }
+      await Tweet.create({ ...req.body })
+      callback({ status: 'success', message: 'tweet was successfully created' })
+    } catch (err) {
+      console.log(err)
+      callback({ status: 'error', message: 'codeStatus 500' })
     }
-    Tweet.create({ ...req.body })
-      .then(tweet => {
-        callback({ status: 'success', message: 'tweet was successfully created' })
-      })
   },
-  getTweets: (req, res, callback) => {
-    Tweet.findAll({
-      raw: true, nest: true,
-      order: [['createdAt', 'DESC']],
-      include: [
-        { model: Like },
-        { model: Reply }
-      ]
-    })
-      .then(tweets => {
-        callback(tweets)
-      })
-  },
-  getTweet: (req, res, callback) => {
-    Tweet.findByPk(req.params.id,
-      {
+  getTweets: async (req, res, callback) => {
+    try {
+      const tweets = await Tweet.findAll({
+        raw: true, nest: true,
+        order: [['createdAt', 'DESC']],
         include: [
           { model: Like },
           { model: Reply }
         ]
-      }).then(tweet => {
-        callback(tweet)
       })
+      callback(tweets)
+    } catch (err) {
+      console.log(err)
+      callback({ status: 'error', message: 'codeStatus 500' })
+    }
+  },
+  getTweet: async (req, res, callback) => {
+    try {
+      const tweet = await Tweet.findByPk(
+        req.params.id,
+        { include: [{ model: Like }, { model: Reply }] }
+      )
+      callback(tweet)
+    } catch (err) {
+      console.log(err)
+      callback({ status: 'error', message: 'codeStatus 500' })
+    }
   }
 }
 
