4194e022d103ec089705ae3bd572e4024d5f5681
subaruuuu <meteorbs13@gmail.com>
2021-03-04 22:14:35 +0800

    feat: build followService for followController

--- controllers/api/followController.js
+++ controllers/api/followController.js
@@ -3,8 +3,16 @@ const Followship = db.Followship
 const followService = require('../../services/followService')
 
 const followController = {
-  addFollowing: (req, res) => { },
-  removeFollowing: (req, res) => { }
+  addFollowing: (req, res) => {
+    followService.addFollowing(req, res, (data) => {
+      return res.json(data)
+    })
+  },
+  removeFollowing: (req, res) => {
+    followService.removeFollowing(req, res, (data) => {
+      return res.json(data)
+    })
+  }
 }
 
 module.exports = followController
--- services/followService.js
+++ services/followService.js
@@ -1,9 +1,33 @@
 const db = require('../models')
 const Followship = db.Followship
+const helper = require('../_helpers')
 
 const followService = {
-  addFollowing: (req, res, callback) => { },
-  removeFollowing: (req, res, callback) => { }
+  addFollowing: (req, res, callback) => {
+    const user = helper.getUser(req)
+    Followship.findOrCreate({
+      where: { followingId: req.body.id },
+      defaults: {
+        followerId: user.id,
+        followingId: req.body.id
+      }
+    }).then(() => {
+      callback({ status: 'success', message: '' })
+    })
+  },
+  removeFollowing: (req, res, callback) => {
+    const user = helper.getUser(req)
+
+    Followship.findOne({
+      where: {
+        followerId: user.id,
+        followingId: req.params.followingId
+      }
+    }).then(followship => {
+      followship.destroy()
+      callback({ status: 'success', message: '' })
+    })
+  }
 }
 
 module.exports = followService
