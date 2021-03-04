9020263a6bb44c109fbe87dedc5f0506deee9835
subaruuuu <meteorbs13@gmail.com>
2021-03-04 22:25:23 +0800

    refactor followService to async/await

--- services/followService.js
+++ services/followService.js
@@ -3,30 +3,38 @@ const Followship = db.Followship
 const helper = require('../_helpers')
 
 const followService = {
-  addFollowing: (req, res, callback) => {
-    const user = helper.getUser(req)
-    Followship.findOrCreate({
-      where: { followingId: req.body.id },
-      defaults: {
-        followerId: user.id,
-        followingId: req.body.id
-      }
-    }).then(() => {
+  addFollowing: async (req, res, callback) => {
+    try {
+      const user = helper.getUser(req)
+      await Followship.findOrCreate({
+        where: { followingId: req.body.id },
+        defaults: {
+          followerId: user.id,
+          followingId: req.body.id
+        }
+      })
       callback({ status: 'success', message: '' })
-    })
+    } catch (err) {
+      console.log(err)
+      callback({ status: 'error', message: 'codeStatus 500' })
+    }
   },
-  removeFollowing: (req, res, callback) => {
-    const user = helper.getUser(req)
-
-    Followship.findOne({
-      where: {
-        followerId: user.id,
-        followingId: req.params.followingId
-      }
-    }).then(followship => {
-      followship.destroy()
+  removeFollowing: async (req, res, callback) => {
+    try {
+      const user = helper.getUser(req)
+      const followship = await Followship.findOne({
+        where: {
+          followerId: user.id,
+          followingId: req.params.followingId
+        }
+      })
+      if (followship === null) callback({ status: 'success', message: '' })
+      await followship.destroy()
       callback({ status: 'success', message: '' })
-    })
+    } catch (err) {
+      console.log(err)
+      callback({ status: 'error', message: 'codeStatus 500' })
+    }
   }
 }
 
