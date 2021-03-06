ed0d61613f4fd39ab48722dc9eb7b29208016d57
YinWei <cba5530@gmail.com>
2021-03-06 21:37:23 +0800

    add status code on followService

--- controllers/api/followController.js
+++ controllers/api/followController.js
@@ -5,11 +5,13 @@ const followService = require('../../services/followService')
 const followController = {
   addFollowing: (req, res) => {
     followService.addFollowing(req, res, (data) => {
+      if (data.status === 'error') return res.status(data.statusCode).json(data)
       return res.json(data)
     })
   },
   removeFollowing: (req, res) => {
     followService.removeFollowing(req, res, (data) => {
+      if (data.status === 'error') return res.status(data.statusCode).json(data)
       return res.json(data)
     })
   }
