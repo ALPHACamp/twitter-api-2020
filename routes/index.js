1176379fd236126c7c7238c9a3db63861e6062dd
Song Shao Ting <qwe820610@gmail.com>
2021-03-07 20:20:16 +0800

    feat: modify middleware

--- routes/index.js
+++ routes/index.js
@@ -23,10 +23,17 @@ const authenticated = function (req, res, next) {
     return next()
   })(req, res, next)
 }
-
 // 驗證管理員
 function authenticatedAdmin(req, res, next) {
   if (helpers.getUser(req).role === 'admin' || helpers.getUser(req).isAdmin === true) {
+    return next()
+  } else {
+    return res.status(401).json({ status: 'error', message: '權限驗證失敗' })
+  }
+}
+// 驗證使用者
+function authenticatedUser(req, res, next) {
+  if (!helpers.getUser(req).isAdmin) {
     return next()
   } else {
     return res.status(401).json({ status: 'error', message: '權限驗證失敗' })
@@ -40,19 +47,19 @@ router.post('/api/users', userController.signUp)
 // 當前用戶
 router.get('/api/users/currentUser', authenticated, userController.getCurrentUser)
 // 個人資料
-router.get('/api/users/:id', authenticated, userController.getUser)
+router.get('/api/users/:id', authenticated, authenticatedUser, userController.getUser)
 // 修改個人資料
-router.put('/api/users/:id', authenticated, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), userController.putUser)
+router.put('/api/users/:id', authenticated, authenticatedUser, upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), userController.putUser)
 // 使用者正在追蹤誰
-router.get('/api/users/:id/followings', authenticated, userController.getFollowings)
+router.get('/api/users/:id/followings', authenticated, authenticatedUser, userController.getFollowings)
 // 誰在追蹤這個使用者
-router.get('/api/users/:id/followers', authenticated, userController.getFollowers)
+router.get('/api/users/:id/followers', authenticated, authenticatedUser, userController.getFollowers)
 // 看見某使用者發過回覆的推文
-router.get('/api/users/:id/replied_tweets', authenticated, userController.getRepliedTweets)
+router.get('/api/users/:id/replied_tweets', authenticated, authenticatedUser, userController.getRepliedTweets)
 // 看見某使用者點過的 Like 
-router.get('/api/users/:id/likes', authenticated, userController.getLikeTweets)
+router.get('/api/users/:id/likes', authenticated, authenticatedUser, userController.getLikeTweets)
 // 看見某使用者發過的推文
-router.get('/api/users/:id/tweets', authenticated, userController.getUserTweets)
+router.get('/api/users/:id/tweets', authenticated, authenticatedUser, userController.getUserTweets)
 
 
 //admin
@@ -62,31 +69,31 @@ router.delete('/api/admin/tweets/:id', authenticated, authenticatedAdmin, adminC
 
 
 // 瀏覽推文
-router.get('/api/tweets', authenticated, tweetController.getTweets)
+router.get('/api/tweets', authenticated, authenticatedUser, tweetController.getTweets)
 // 修改推文
-router.post('/api/tweets', authenticated, tweetController.postTweet)
+router.post('/api/tweets', authenticated, authenticatedUser, tweetController.postTweet)
 // 單一推文
-router.get('/api/tweets/:id', authenticated, tweetController.getTweet)
+router.get('/api/tweets/:id', authenticated, authenticatedUser, tweetController.getTweet)
 // 編輯推文
-router.put('/api/tweets/:id', authenticated, tweetController.putTweet)
+router.put('/api/tweets/:id', authenticated, authenticatedUser, tweetController.putTweet)
 // 刪除推文
-router.delete('/api/tweets/:id', authenticated, tweetController.deleteTweet)
+router.delete('/api/tweets/:id', authenticated, authenticatedUser, tweetController.deleteTweet)
 // 新增Like
-router.post('/api/tweets/:id/like', authenticated, tweetController.postLike)
+router.post('/api/tweets/:id/like', authenticated, authenticatedUser, tweetController.postLike)
 // 刪除 Like
-router.post('/api/tweets/:id/unlike', authenticated, tweetController.deleteLike)
+router.post('/api/tweets/:id/unlike', authenticated, authenticatedUser, tweetController.deleteLike)
 // 新增回覆
-router.post('/api/tweets/:tweet_id/replies', authenticated, tweetController.postReply)
+router.post('/api/tweets/:tweet_id/replies', authenticated, authenticatedUser, tweetController.postReply)
 // 瀏覽回覆
-router.get('/api/tweets/:tweet_id/replies', authenticated, tweetController.getReplies)
+router.get('/api/tweets/:tweet_id/replies', authenticated, authenticatedUser, tweetController.getReplies)
 // 刪除回覆
-router.delete('/api/replies/:id', authenticated, replyController.deleteReply)
+router.delete('/api/replies/:id', authenticated, authenticatedUser, replyController.deleteReply)
 // 
-router.put('/api/replies/:id', authenticated, replyController.putReply)
+router.put('/api/replies/:id', authenticated, authenticatedUser, replyController.putReply)
 
 
-router.post('/api/followships', authenticated, followshipController.postFollowship)
-router.delete('/api/followships/:followingId', authenticated, followshipController.deleteFollowship)
+router.post('/api/followships', authenticated, authenticatedUser, followshipController.postFollowship)
+router.delete('/api/followships/:followingId', authenticated, authenticatedUser, followshipController.deleteFollowship)
 
 
 
