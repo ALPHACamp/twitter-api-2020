8dcb4e2944c936511e6177915a48a1be289c0463
YinWei <cba5530@gmail.com>
2021-03-07 20:15:46 +0800

    add isFollowed to userService.getFollowers & userService.getFollowings

--- services/userService.js
+++ services/userService.js
@@ -78,7 +78,7 @@ const userService = {
       users = await users.map(user => ({
         ...user.dataValues,
         FollowerCount: user.Followers.length,
-        isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+        isFollowed: req.user.Followings.map(following => following.id).includes(user.id)
       }))
       users = await users.sort((a, b) => b.FollowerCount - a.FollowerCount)
       callback({ users: users })
@@ -229,11 +229,11 @@ const userService = {
             { model: User, as: 'Followers' }
           ],
         })
-
       user = await user.Followings.map(user => ({
         ...user.dataValues,
         followerId: user.Followship.followerId,
-        followingId: user.Followship.followingId
+        followingId: user.Followship.followingId,
+        isFollowed: helpers.getUser(req).Followings.map(following => following.id).includes(user.id)
       }))
       callback(user)
     } catch (err) {
@@ -255,7 +255,8 @@ const userService = {
       user = await user.Followers.map(user => ({
         ...user.dataValues,
         followerId: user.Followship.followerId,
-        followingId: user.Followship.followingId
+        followingId: user.Followship.followingId,
+        isFollowed: helpers.getUser(req).Followings.map(following => following.id).includes(user.id)
       }))
       callback(user)
     } catch (err) {
