fbe69a57e03d7e55ddef8ee729758c3b3f9246a3
subaruuuu <meteorbs13@gmail.com>
2021-03-10 02:39:12 +0800

    fix putUser issue

--- services/userService.js
+++ services/userService.js
@@ -127,9 +127,6 @@ const userService = {
         return callback({ status: 'error', message: "Please insert a name for user!", statusCode: 400 })
       }
 
-      console.log(req)
-      console.log(req.body)
-
       const { files } = req
 
       if (files.length !== 0) {    //編輯個人資料
