e082167c2b32f0a9492af7a9694b60a3ff18cad7
Song Shao Ting <qwe820610@gmail.com>
2021-03-12 23:46:17 +0800

    feat: socket initialization and add publicRoom

--- migrations/20210309073850-create-chatPublic.js
+++ migrations/20210309073850-create-chatPublic.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Chatpublic', {
+    await queryInterface.createTable('Chatpublics', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Chatpublic');
+    await queryInterface.dropTable('Chatpublics');
   }
 };
--- sockets/index.js
+++ sockets/index.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken')
-const passport = require('../config/passport')
-const { User, Message, sequelize } = require('../models')
+const { User, Chatpublic, sequelize } = require('../models')
+
 
 // 驗證身分
 async function authenticated(socket, next) {
   // 取出 token
   const token = socket.handshake.auth.token
+  // console.log('token', token)
   // 驗證使用者
   if (!token) return
   // 驗證 token 並取出 id
@@ -23,122 +24,71 @@ async function authenticated(socket, next) {
   }
 }
 
-// 歷史訊息分類 私聊 & 公聊
-function historicalRecord() {
-
-  // 發送歷史紀錄
-  Message.findAll({
-    // attributes: ['msg', 'time'],
-    order: [
-      // 資料庫端進行排列
-      [sequelize.literal('createdAt'), 'ASC']
-    ],
-    raw: true,
-    nest: true,
-  }).then(userMessage => {
-    socket.emit("chatRecord", userMessage)
-  })
-}
-
 
 module.exports = (io) => {
+  console.log('已連線')
   // 驗證身分
   io.use(authenticated)
-  // 開始連線
-  io.on('connection', async (socket) => {
 
-    socket.join(privateRoom)
-
+  io.on('connection', socket => {
     // 計算上線人數
     let onlineCount = 0
+    onlineCount++;
+    io.emit("online", onlineCount)
+
     // 取出登入使用者
     const user = socket.user
+    
+    user.channel = 'publicRoom'
+
+    // 回傳使用者資訊 渲染前端
+    io.emit("onlineUser", user)
+
     // 儲存目前上線使用者
     let userList = []
     userList.push(user)
 
-    console.log('user connection')
-    // 轉換型態 建立房間名 ? user.id.toString()
-    // console.log('=========誰進入===============', typeof(user.id.toString()))
+    // 分配聊天室 先驗證
+    let roomName = socket.handshake.query.channel
+    if (roomName !== 'publicRoom') {
+      // 將兩位使用者帶入 特定Room
+      socket.join(user.roomName)
+    }
+    // 將使用者帶入公開聊天Room
+    socket.join(user.roomName)
+
+     //針對特定房間用戶連接時廣播
+     function formatMessage(username, text) {
+      return {
+        username,
+        text,
+        // time: moment().format('h:mm a')
+      };
+    }
+    const botName = 'ChatCord Bot'
+    // console.log('===username', user)
+     socket.broadcast.to(user.roomName).emit(
+      'message',
+      formatMessage(botName, `${user.name} 加入了聊天`)
+    )
 
 
-    // 回傳使用者資訊 渲染前端
-    io.emit("userData", user)
-
-    // 發送連線人數給網頁
-    onlineCount++;
-    io.emit("online", onlineCount)
-
-    // 監聽登入資料
-    socket.on('login', userData => {
-      // console.log('======userData', userData)
-    })
-
-    // 監聽使用者進入私聊 // 聊天對象的 id & name
-    socket.on('get-private-chat', ({ userId, userName }) => {
-      // 建立房間名
-      const roomName = []
-      // 建立私聊房間編號
-      roomName.push(userId.toString(), user.id.toString())
-      roomName.sort()
-      const privateRoom = roomName[0] + roomName[1]
-
-      // 將登入使用者加入 privateRoom
-      user.privateRoom = privateRoom
-
-      // 建立私聊 我 & 他 資訊陣列
-      const users = [];
-      const userJoinData = userJoin(Number(userId), userName, Number(privateRoom))
-
-      // 私聊對象資料
-      function userJoin(userId, userName, privateRoom) {
-        const userPrivate = { userId, userName, privateRoom }
-        return userPrivate;
-      }
-
-      users.push(user);
-      users.push(userJoinData);
-      console.log('====加入用戶聊天====', users)
-
-      // 將使用者分配到此房間 // 只有選擇該房間使用者聊天訊息可被看到
-      socket.join(privateRoom)
-
-      // Welcome current user
-      const botName = 'ChatCord Bot'
-      socket.emit('msg', formatMessage(botName, 'Welcome to ChatCord!'))
-
-      //針對特定房間用戶連接時廣播
-      socket.broadcast.to(privateRoom).emit("msg", )
-        
-
-      // 發送用戶和房間信息
-      io.to(user.room).emit('roomUsers', {
-        room: user.room,
-        users: getRoomUsers(user.room)
-      });
-
-    })
-
-    // 監聽使用者送出訊息
+    // 監聽使用者送出訊息 送出 'message' 
     socket.on("send", async (msg) => {
-      console.log('監聽前端使用者訊息', msg)
       // 如果 msg 內容鍵值小於 2 等於是訊息傳送不完全
       // 因此我們直接 return ，終止函式執行。
       if (Object.keys(msg).length < 2) return;
-
-      await Message.create({
+      await Chatpublic.create({
         UserId: msg.id,
-        msg: msg.msg,
+        message: msg.msg,
         time: msg.time
       }).then(user => {
         const data = {
-          name: user.dataValues.name,
-          msg: user.dataValues.msg
+          UserId: user.dataValues.UserId,
+          msg: user.dataValues.message
         }
-        console.log('========="send"======', user);
-        io.emit("msg", data)
+        io.emit("message", data)
       })
-
     })
 
     // 離線
@@ -146,6 +96,5 @@ module.exports = (io) => {
       onlineCount = (onlineCount < 0) ? 0 : onlineCount -= 1
       io.emit("online", onlineCount)
     })
-
   })
 }
