1f1be71afbd93345fff9dad0255540e1c8774dc6
yuwen chang <provence5058@gmail.com>
2021-03-12 19:18:49 +0800

    feat: emitting events

--- app.js
+++ app.js
@@ -53,6 +53,9 @@ app.get('/', (req, res) => {
 })
 io.on('connection', (socket) => {
   console.log('a user connected')
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  })
   socket.on('disconnect', () => {
     console.log('user disconnected')
   })
