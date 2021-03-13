ca11e2ccf2597d3f706f367db3122c9aaf26f8e6
Song Shao Ting <qwe820610@gmail.com>
2021-03-14 01:20:04 +0800

    refactor: User related functions

--- sockets/index.js
+++ sockets/index.js
@@ -1,29 +1,6 @@
 const jwt = require('jsonwebtoken')
 const { User, Chat, sequelize } = require('../models')
-
-// 驗證身分
-async function authenticated(socket, next) {
-  // 取出 token
-  const token = socket.handshake.auth.token
-  // console.log('token', token)
-  // 驗證使用者
-  if (!token) return
-  // 驗證 token 並取出 id
-  const { id } = jwt.verify(token, process.env.JWT_SECRET)
-  // 找出該使用者資訊
-  const user = await User.findByPk(id, {
-    attributes: ['id', 'name', 'account', 'email', 'avatar', "isAdmin"],
-    raw: true,
-    nest: true
-  })
-  // 存進 socket
-  if (user) {
-    socket.user = user
-    socket.user.socketId = socket.id
-    console.log(user)
-    next()
-  }
-}
+const { authenticated, formatMessage, userLeave } = require('./utils/user')
 
 module.exports = (io) => {
   console.log('已連線')
@@ -74,14 +51,7 @@ module.exports = (io) => {
       historicalRecord(user.channel)
     })
 
-    //針對特定房間用戶連接時廣播
-    function formatMessage(username, text) {
-      return {
-        username,
-        text,
-        // time: moment().format('h:mm a')
-      };
-    }
+
     const botName = 'ChatCord Bot'
     socket.broadcast.to(user.roomName).emit(
       'message',
@@ -116,13 +86,11 @@ module.exports = (io) => {
               messageId: usermsg.id
             }
             io.emit("message", { ...data, ...user })
-
           })
         }
       } catch (e) {
         console.log(e)
       }
-
     })
 
     function historicalRecord(channelData) {
@@ -159,13 +127,7 @@ module.exports = (io) => {
     // 離線
     socket.on('disconnect', () => {
       onlineCount = (onlineCount < 0) ? 0 : onlineCount -= 1
-      // 找出誰離開
-      function userLeave(id) {
-        const index = userList.findIndex(user => user.socketid === id)
-        if (index !== -1) {
-          return userList.splice(index, 1)[0];
-        }
-      }
+
       // 帶入 userLeave() 判斷誰離開
       const userLeft = userLeave(user.socketid, userList);
       // 向該頻道通知誰離開
