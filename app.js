55160c6e9edc338c129ac8cfa58edf41b9ea6a31
yuwen chang <provence5058@gmail.com>
2021-03-13 13:52:06 +0800

    modify eventName

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.get('/', (req, res) => {
 // })
 sio.on('connection', (socket) => { // 建立連線
   console.log('a user connected')
-  socket.on('message', (msg) => {// server 收到 client 的訊息 (Emitting events:client往通道內丟的訊息)
+  socket.on('chat message', (msg) => {// server 收到 client 的訊息 (Emitting events:client往通道內丟的訊息)
     console.log(msg)
     socket.broadcast.emit('other', msg) // broadcast：再透過通道把msg轉發給其他聊天室的使用者 
     socket.emit('self', msg) //emit：再透過通道把msg傳給自己 
