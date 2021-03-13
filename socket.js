6a0cc5c7dd4fd551b18bb62dec4f832a52b58176
yuwen chang <provence5058@gmail.com>
2021-03-13 21:25:46 +0800

    modify msg

--- socket.js
+++ socket.js
@@ -4,7 +4,7 @@ const { PublicMessage, User } = db
 module.exports = socket = (httpServer) => {
   const sio = require('socket.io')(httpServer, {
     cors: {
-      // origin: "https://twitter-simple-one.herokuapp.com/api",
+      // origin: "https://twitter-simple-one.herokuapp.com",
       origin: "*",
       methods: ["GET", "POST"]
     }
@@ -17,24 +17,26 @@ module.exports = socket = (httpServer) => {
       .then(msg => {
         if (!msg) return
         msg.map(m => {
-          socket.emit('message', m.message) //連上線之後會出現歷史訊息
+          socket.emit('self', m.message) //連上線之後，自己會出現歷史訊息
         })
       })
+
     socket.on('message', (msg, err) => {// server 收到 client 的訊息 (Emitting events:client往通道內丟的訊息)
       console.log(msg)
-      //存入資料庫
-      PublicMessage.create({
-        message: msg,
-        UserId: 11
-      })
-      User.findAll({ where: { id: 11 } })
-        .then(user => {
-          const { id, name, avatar, createdAt } = user[0].dataValues
-          socket.emit('self', { msg, id, name, avatar, createdAt }) //emit：再透過通道把msg傳給自己 
-        })
+
       //轉發
       socket.broadcast.emit('other', msg) // broadcast：再透過通道把msg轉發給其他聊天室的使用者 
+      User.findAll({ where: { id: 11 } })
+        .then(user => {
+          const { id, name, avatar, account, createdAt } = user[0].dataValues
+          socket.emit('self', { msg: msg.msg, id, account, name, avatar, createdAt }) //emit：再透過通道把msg傳給自己 
+        })
 
+      //存入資料庫
+      PublicMessage.create({
+        message: msg.msg,
+        UserId: 11
+      })
     })
   })
 }
