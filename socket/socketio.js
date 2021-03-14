1c173dd127c3eef598cfde576975ee89a481699d
stan_wang <a7569209@gmail.com>
2021-03-14 15:27:30 +0800

    edit publicMessage

--- socket/socketio.js
+++ socket/socketio.js
@@ -2,12 +2,6 @@ const { User, Message } = require('../models')
 
 
 module.exports = (io) => {
-  const fakeHistoryMsg = [
-    {
-      name: "stan",
-      message: "打給賀"
-    }
-  ]
 
   io.on('connection', (socket) => {
 
@@ -17,7 +11,7 @@ module.exports = (io) => {
     // socket.emit("allMessage", fakeHistoryMsg)
 
     socket.on('startChat', (user) => {
-      console.log(user)
+      console.log('user', user)
       Message.findAll({
         include: [{ model: User, attributes: ['id', 'name', 'account', 'avatar', 'role'] }],
         order: [['createdAt', 'ASC']]
@@ -26,10 +20,11 @@ module.exports = (io) => {
           historyMsg = historyMsg.map(m => ({
             ...m.toJSON()
           }))
+          console.log('historyMsg', historyMsg)
           //發送歷史訊息
           socket.emit('history', historyMsg)
           //通知所有人，有人上線
-          // socket.broadcast.emit('userOnline', `${username}上線`)
+          // socket.broadcast.emit('userOnline', `${user.name}上線`)
 
         })
     })
@@ -43,8 +38,8 @@ module.exports = (io) => {
         UserId: msg.id, //暫時設假數字，等前端傳來再改
         content: msg.msg
       })
-      // console.log('testmessage: ' + msg);
-      io.emit('publicMessage', msg);
+
+      io.emit('publicMessage', msg.msg);
     });
 
 
