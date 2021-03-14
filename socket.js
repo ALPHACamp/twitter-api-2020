397359c444ffdebfaab4f0b5df410b6314616929
yuwen chang <provence5058@gmail.com>
2021-03-14 12:31:31 +0800

    modify chat history

--- socket.js
+++ socket.js
@@ -10,15 +10,17 @@ module.exports = socket = (httpServer) => {
     }
   })
 
-  let users = [];
+  let users = []  // 目前上線的使用者資料，包含socket.id
 
+
+  // 公開聊天室
   sio.on('connection', (socket) => { // 建立連線
     console.log('a user connected')
 
     //歷史訊息
     socket.on('messages', (msg, err) => {
+      let allMessages = []
       PublicMessage.findAll({
-        where: { UserId: 11 },
         include: User,
         raw: true,
         nest: true
@@ -26,10 +28,11 @@ module.exports = socket = (httpServer) => {
         .then((msgs) => {
           if (!msgs) return
           msgs.map(m => {
-            const { id, name, account, avatar, createdAt } = m.User
-            socket.emit('getAllMessages', { msg: m.message, id, name, account, avatar, createdAt }) //連上線之後，自己會出現歷史訊息
+            const { id, name, avatar, createdAt } = m.User
+            allMessages.push({ text: m.message, userId: id, userName: name, userAvatar: avatar, createdAt })
           })
         })
+      socket.emit('getAllMessages', allMessages) //連上線之後，自己會出現歷史訊息
     })
 
     // 取得線上使用者
@@ -93,6 +96,8 @@ module.exports = socket = (httpServer) => {
     })
 
   })
+
+  // 私人聊天室
 }
 
 
