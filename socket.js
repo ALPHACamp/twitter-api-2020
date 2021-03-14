690c5d7803565c8a29cf79ccc649f38e7fb4b398
yuwen chang <provence5058@gmail.com>
2021-03-14 11:00:12 +0800

    modfiy data name

--- socket.js
+++ socket.js
@@ -14,15 +14,6 @@ module.exports = socket = (httpServer) => {
 
   sio.on('connection', (socket) => { // 建立連線
     console.log('a user connected')
-    //歷史訊息
-    // PublicMessage.findAll({ where: { UserId: 11 }, include: User, raw: true, nest: true })
-    //   .then(msg => {
-    //     if (!msg) return
-    //     msg.map(m => {
-    //       const { id, name, account, avatar, createdAt } = m.User
-    //       socket.emit('self', { msg: m.message, id, name, account, avatar, createdAt }) //連上線之後，自己會出現歷史訊息
-    //     })
-    //   })
 
     //歷史訊息
     socket.on('messages', (msg, err) => {
@@ -81,8 +72,8 @@ module.exports = socket = (httpServer) => {
       const socketId = socket.id
       const userData = {
         id: data.userId,
-        name: data.name,
-        avatar: data.avatar,
+        name: data.userName,
+        avatar: data.userAvatar,
         socketId: socketId
       }
 
@@ -101,50 +92,6 @@ module.exports = socket = (httpServer) => {
       io.sockets.emit('sendOffline', users)
     })
 
-
-    // 保留原方法
-    socket.on('message', (msg, err) => {// server 收到 client 的訊息 (Emitting events:client往通道內丟的訊息)
-      console.log(msg)
-
-      //轉發
-      // socket.broadcast.emit('other', { msg: msg.msg }) // broadcast：再透過通道把msg轉發給其他聊天室的使用者 
-
-      //撈自己的info
-      User.findAll({ where: { id: 11 } })
-        .then(user => {
-          const { id, name, avatar, account, createdAt } = user[0].dataValues
-
-          socket.broadcast.emit('other', { msg: msg.msg, id, account, name, avatar, createdAt })
-
-          socket.emit('self', { msg: msg.msg, id, account, name, avatar, createdAt }) //emit：再透過通道把msg傳給自己 
-        })
-
-      //存入資料庫
-      PublicMessage.create({
-        message: msg.msg,
-        UserId: 11
-      })
-    })
-
-    socket.on('other', (msg, err) => {// server 收到 client 的訊息 (Emitting events:client往通道內丟的訊息)
-      console.log(msg)
-
-      //轉發
-      socket.broadcast.emit('other', { msg: msg.msg }) // broadcast：再透過通道把msg轉發給其他聊天室的使用者 
-
-      //撈自己的info
-      User.findAll({ where: { id: 11 } })
-        .then(user => {
-          const { id, name, avatar, account, createdAt } = user[0].dataValues
-          socket.emit('self', { msg: msg.msg, id, account, name, avatar, createdAt }) //emit：再透過通道把msg傳給自己 
-        })
-
-      //存入資料庫
-      PublicMessage.create({
-        message: msg.msg,
-        UserId: 11
-      })
-    })
   })
 }
 
