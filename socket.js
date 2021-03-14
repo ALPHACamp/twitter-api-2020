4871a8543c4ee3783694df33270392a4a316e028
yuwen chang <provence5058@gmail.com>
2021-03-14 14:51:50 +0800

    modify receivePublic

--- socket.js
+++ socket.js
@@ -52,23 +52,21 @@ module.exports = socket = (httpServer) => {
     socket.on('sendPublic', (data, err) => {
       const { text, userId } = data
       const createdAt = new Date()
+      //存入資料庫
+      PublicMessage.create({
+        message: text,
+        UserId: userId,
+        createdAt,
+        updatedAt: createdAt,
+      })
       //撈自己的info
-      User.findAll({ where: { id: userId } })
+      // User.findAll({ where: { id: userId } })
+      User.findByPk(userId)
         .then((user) => {
-          const { name, avatar } = user[0].dataValues
-          //存入資料庫
-          PublicMessage.create({
-            message: text,
-            UserId: userId,
-            createdAt,
-            updatedAt: createdAt,
-          }).then(() => {
-            socket.broadcast.emit('receivePublic', { text, userId, userName: name, userAvatar: avatar, createdAt })
-            socket.emit('receivePublic', { text, userId, userName: name, userAvatar: avatar, createdAt })
-          })
+          const { name, avatar } = user
+          socket.broadcast.emit('receivePublic', { text, userId, userName: name, userAvatar: avatar, createdAt })
+          socket.emit('receivePublic', { text, userId, userName: name, userAvatar: avatar, createdAt })
           // io.sockets.emit('receivePublic', { text, userId, userName: name, userAvatar: avatar, createdAt })
-          // socket.broadcast.emit('other', { msg: msg.msg, id, account, name, avatar, createdAt })
-          // socket.emit('self', { msg: msg.msg, id, account, name, avatar, createdAt }) //emit：再透過通道把msg傳給自己 
         })
     })
 
