e7236a9015d2d98e8ceaed9e220bab1d39e4899c
stan_wang <a7569209@gmail.com>
2021-03-16 21:06:36 +0800

    add test html & fix message model equal migrations

--- models/message.js
+++ models/message.js
@@ -1,8 +1,26 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Message = sequelize.define('Message', {
-    UserId: DataTypes.INTEGER,
-    content: DataTypes.STRING
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    UserId: {
+      type: DataTypes.INTEGER
+    },
+    content: {
+      type: DataTypes.STRING
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
   }, {
     sequelize,
     modelName: 'Message'
