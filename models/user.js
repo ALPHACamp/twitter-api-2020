658e1c6b7c4bc07d644cac38d079ba96ba5a0784
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-16 16:35:53 -0700

    feat: create models

--- models/user.js
+++ models/user.js
@@ -1,8 +1,44 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
-  };
-  return User;
-};
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.BOOLEAN,
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.STRING,
+      account: DataTypes.STRING,
+      cover: DataTypes.STRING
+    },
+    {}
+  )
+  User.associate = function (models) {
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Like)
+    User.hasMany(models.Reply)
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    })
+    User.belongsToMany(models.Tweet, {
+      through: models.Reply,
+      foreignKey: 'UserId',
+      as: 'RepliedTweets'
+    })
+
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
+  }
+  return User
+}
--- test/models/Like.spec.js
+++ test/models/Like.spec.js
@@ -1,6 +1,6 @@
-var chai = require('chai');
-var sinon = require('sinon');
-chai.use(require('sinon-chai'));
+var chai = require('chai')
+var sinon = require('sinon')
+chai.use(require('sinon-chai'))
 
 const { expect } = require('chai')
 const {
@@ -15,7 +15,6 @@ const db = require('../../models')
 const LikeModel = require('../../models/like')
 
 describe('# Like Model', () => {
-  
   before(done => {
     done()
   })
@@ -25,8 +24,7 @@ describe('# Like Model', () => {
   checkModelName(Like)('Like')
 
   context('properties', () => {
-    ;[
-    ].forEach(checkPropertyExists(like))
+    ;[].forEach(checkPropertyExists(like))
   })
 
   context('associations', () => {
@@ -37,48 +35,46 @@ describe('# Like Model', () => {
       Like.associate({ Tweet })
     })
 
-    it('should belong to user', (done) => {
+    it('should belong to user', done => {
       expect(Like.belongsTo).to.have.been.calledWith(User)
       done()
     })
-    it('should belong to tweet', (done) => {
+    it('should belong to tweet', done => {
       expect(Like.belongsTo).to.have.been.calledWith(Tweet)
       done()
     })
   })
 
   context('action', () => {
-
     let data = null
 
-    it('create', (done) => {
-      db.Like.create({}).then((like) => {   
+    it('create', done => {
+      db.Like.create({}).then(like => {
         data = like
         done()
       })
     })
-    it('read', (done) => {
-        db.Like.findByPk(data.id).then((like) => {  
-          expect(data.id).to.be.equal(like.id)
-          done()
-        })
+    it('read', done => {
+      db.Like.findByPk(data.id).then(like => {
+        expect(data.id).to.be.equal(like.id)
+        done()
+      })
     })
-    it('update', (done) => {
-      db.Like.update({}, { where: { id: data.id }}).then(() => {
-        db.Like.findByPk(data.id).then((like) => { 
-          expect(data.updatedAt).to.be.not.equal(like.updatedAt) 
+    it('update', done => {
+      db.Like.update({}, { where: { id: data.id } }).then(() => {
+        db.Like.findByPk(data.id).then(like => {
+          expect(data.updatedAt).to.be.not.equal(like.updatedAt)
           done()
         })
       })
     })
-    it('delete', (done) => {
-      db.Like.destroy({ where: { id: data.id }}).then(() => {
-        db.Like.findByPk(data.id).then((like) => { 
-          expect(like).to.be.equal(null) 
+    it('delete', done => {
+      db.Like.destroy({ where: { id: data.id } }).then(() => {
+        db.Like.findByPk(data.id).then(like => {
+          expect(like).to.be.equal(null)
           done()
         })
       })
     })
   })
-
 })
