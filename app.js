b148eb3bd5e66a7227f8aeeeab8c61f99fb9e013
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-16 20:28:34 -0700

    refactor: change router's structure

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 }
 
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+require('./routes')(app)
 
 module.exports = app
