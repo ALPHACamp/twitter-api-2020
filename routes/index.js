7a0d5d38ffbb837193104002eba7049f3d546b86
Sherry <sherry.c.liao@gmail.com>
2021-04-18 01:03:11 +0800

    feat: add swagger-jsdoc && swagger-ui-express

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,35 @@ const followships = require('./modules/followships')
 
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
+const swaggerJsDoc = require('swagger-jsdoc')
+const swaggerUi = require('swagger-ui-express')
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: "Simple Twitter API",
+      description: "Simple Twitter API documentation",
+      contact: {
+        name: 'user',
+        email: 'user@example.com'
+      },
+      servers: [
+        'http://localhost:3000'
+      ]
+    }
+  },
+  apis: ['../routes/*.js']
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
+
+
+
+
+
+
 module.exports = app => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
   app.use('/api/users', users)
   app.use('/api/admin', authenticated, authenticatedAdmin, admin)
   app.use('/api/tweets', authenticated, tweets)
