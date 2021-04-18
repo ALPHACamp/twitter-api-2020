1d27f279710835c741bc5ed1cbae9b5b2dd9cd2b
Sherry <sherry.c.liao@gmail.com>
2021-04-18 17:17:35 +0800

    feat: add swagger for POST /api/users

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -9,12 +9,43 @@ const userController = require('../../controllers/userController')
 * @swagger
 * /api/users:
 *   get:
-*     summary: Retrieve a list of users of Simple Twitter
+*     summary: Retrieve a list of users of Simple Twitter.
 *     description: Retrieve a list of users from Simple Twitter.
+*     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The user ID.
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: The user's name.
+ *                         example: Leanne Graham
 */
+
+
+
 router
   .route('/')
   .get(authenticated, (req, res) => res.send('Hello World!'))
+  /**
+* @swagger
+* /api/users:
+*   post:
+*     summary: Register an account Simple Twitter.
+*     description: Register an account Simple Twitter.
+*/
   .post(userController.register)
 
 
