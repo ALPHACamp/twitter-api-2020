0f661b2c11723ab76d676b5432377c3e4097ccab
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-18 07:21:36 -0700

    refactor: modify tweets seeder

--- seeders/20210417214618-tweets-seed-file.js
+++ seeders/20210417214618-tweets-seed-file.js
@@ -4,15 +4,16 @@ const db = require('../models')
 const User = db.User
 
 const faker = require('faker')
+const tweet = require('../models/tweet')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = await User.findAll()
+    const users = await User.findAll({ where: { role: false } })
 
     await queryInterface.bulkInsert(
       'Tweets',
       Array.from({ length: 50 }).map((d, i) => ({
-        UserId: users[parseInt(i / 10) + 1].id,
+        UserId: users[parseInt(i / 10)].id,
         description: faker.lorem.text(),
         createdAt: new Date(),
         updatedAt: new Date()
