dd9735ace10d36747757513fb54e90c431a3e3a3
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-18 06:45:26 -0700

    feat: modify users seeder

--- seeders/20210417191351-users-seed-file.js
+++ seeders/20210417191351-users-seed-file.js
@@ -43,6 +43,42 @@ module.exports = {
           cover: 'https://i.imgur.com/1jDf2Me.png',
           createdAt: new Date(),
           updatedAt: new Date()
+        },
+        {
+          name: 'user3',
+          email: 'user3@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: false,
+          avatar: 'https://i.imgur.com/q6bwDGO.png',
+          introduction: faker.lorem.text().substring(0, 50),
+          account: 'user3',
+          cover: 'https://i.imgur.com/1jDf2Me.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'user4',
+          email: 'user4@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: false,
+          avatar: 'https://i.imgur.com/q6bwDGO.png',
+          introduction: faker.lorem.text().substring(0, 50),
+          account: 'user4',
+          cover: 'https://i.imgur.com/1jDf2Me.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'user5',
+          email: 'user5@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: false,
+          avatar: 'https://i.imgur.com/q6bwDGO.png',
+          introduction: faker.lorem.text().substring(0, 50),
+          account: 'user5',
+          cover: 'https://i.imgur.com/1jDf2Me.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
         }
       ],
       {}
