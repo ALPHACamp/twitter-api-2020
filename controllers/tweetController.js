d02e38d01ee688e579a7db75149fea73da1afeea
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-19 08:28:38 -0700

    feat: include number of reply and like for getTweets function of userController

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -12,11 +12,17 @@ const tweetController = {
       const UserId = helpers.getUser(req).id
 
       if (!description) {
-        return res.json({ status: 'error', message: 'input should not be blank' })
+        return res.json({
+          status: 'error',
+          message: 'input should not be blank'
+        })
       }
 
       if (description.length > 140) {
-        return res.json({ status: 'error', message: 'input should be less than 140 characters' })
+        return res.json({
+          status: 'error',
+          message: 'input should be less than 140 characters'
+        })
       }
 
       await Tweet.create({
@@ -33,23 +39,20 @@ const tweetController = {
         order: [['createdAt', 'DESC']]
       })
 
-      return res.json({ status: 'success', message: 'successfully posted a tweet', tweet })
-    }
-    catch (error) {
+      return res.status(200).json({
+        status: 'success',
+        message: 'successfully posted a tweet',
+        tweet
+      })
+    } catch (error) {
       console.log(error)
     }
-  }
-  ,
-
+  },
   getTweets: async (req, res) => {
     try {
       let tweets = await Tweet.findAll({
-        raw: true,
-        nest: true,
-        include: [User, Reply, Like],
+        include: [User, Reply, Like]
       })
-
-      // let replies = await tweet.Reply.findAndCountAll({ raw: true, nest: true })
 
       tweets = tweets.map(tweet => ({
         id: tweet.id,
@@ -57,17 +60,20 @@ const tweetController = {
         description: tweet.description,
         createdAt: tweet.createdAt,
         updatedAt: tweet.updatedAt,
-        // replyCount: Reply.findAndCountAll({ where: { TweetId: tweet.id } }).count,  // trying to figure out how to get the reply count
-        // likeCount: tweet.Like.length,   // trying to figure out how to get the like count
+        replyCount: tweet.Replies.length,
+        likeCount: tweet.Likes.length,
         user: {
           avatar: tweet.User.avatar,
           name: tweet.User.name,
-          account: tweet.User.account,
+          account: tweet.User.account
         }
       }))
-      return res.json({ status: 'success', message: 'successfully retrieved tweets data', tweets })
-    }
-    catch (error) {
+      return res.json({
+        status: 'success',
+        message: 'successfully retrieved tweets data',
+        tweets
+      })
+    } catch (error) {
       console.log(error)
     }
   },
@@ -76,13 +82,15 @@ const tweetController = {
     try {
       const TweetId = req.params.id
       const tweet = await Tweet.findByPk(TweetId)
-      return res.json({ status: 'success', message: 'successfully retrieved tweet data', tweet })
-    }
-    catch (error) {
+      return res.json({
+        status: 'success',
+        message: 'successfully retrieved tweet data',
+        tweet
+      })
+    } catch (error) {
       console.log(error)
     }
   }
-
 }
 
 module.exports = tweetController
