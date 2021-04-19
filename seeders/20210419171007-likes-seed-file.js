7bccf84cc771893818d65b852a575f0f56f48f7d
Yuchi Su <astraiostor@gmail.com>
2021-04-20 01:18:12 +0800

    Add like seed data

---
+++ seeders/20210419171007-likes-seed-file.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const faker = require('faker')
+
+module.exports = {
+
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Likes',
+      [{
+        id: 1,
+        UserId: 1,
+        TweetId: 11,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        id: 2,
+        UserId: 2,
+        TweetId: 21,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      ], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Likes', null, {})
+  }
+}
