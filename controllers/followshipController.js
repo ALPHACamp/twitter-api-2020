d8140bd814c1ced5fa5a38a5a096bfb19eaa5a47
Sherry <sherry.c.liao@gmail.com>
2021-04-20 19:30:21 +0800

    feat: add POST /api/followships

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -1,7 +1,37 @@
 const db = require('../models')
-const Tweet = db.Tweet
+const Followship = db.Followship
 const User = db.User
+const helpers = require('../_helpers')
 
-const followshipController = {}
+const followshipController = {
+  followUser: async (req, res) => {
+    try {
+      const followingId = req.body.id
+      const followingUser = await User.findByPk(followingId)
+      const followerId = helpers.getUser(req).id
+
+      if (Number(followingId) !== followerId) {
+        await Followship.create({
+          followerId,
+          followingId,   // 前端要埋在 form 裡傳過來
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+        return res.json({ status: 'success', message: `followed @${followingUser.account}`, followingUser })
+      }
+
+      return res.json({ status: 'error', message: 'You cannot follow yourself.' })
+    }
+
+    catch (error) {
+      console.log(error)
+    }
+
+  },
+
+  unfollowUser: (req, res) => {
+
+  }
+}
 
 module.exports = followshipController
