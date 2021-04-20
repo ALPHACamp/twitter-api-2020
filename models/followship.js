03de8e2c0e2c53c8e47e524a7d26af685dcb7b8b
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-20 07:44:55 -0700

    fix: modify followship model

--- models/followship.js
+++ models/followship.js
@@ -3,6 +3,11 @@ module.exports = (sequelize, DataTypes) => {
   const Followship = sequelize.define(
     'Followship',
     {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
       followerId: DataTypes.INTEGER,
       followingId: DataTypes.INTEGER
     },
