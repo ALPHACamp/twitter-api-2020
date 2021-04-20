7a1b114b7a5701eed99ecd9223134be1982e4eee
Flora2020 <rossignols1989@gmail.com>
2021-04-20 18:17:32 +0800

    Log in (administrator only)

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const { authenticated, authenticatedAdmin } = require('../middleware/authenticate')
 const userController = require('../controllers/userController')
+const adminController = require('../controllers/adminController')
 const users = require('./modules/users')
+const admin = require('./modules/admin')
 module.exports = (app) => {
   app.post('/api/users/login', userController.login)
   app.use('/api/users', authenticated, users)
+
+  app.post('/api/admin/login', adminController.login)
+  app.use('/api/admin', authenticated, authenticatedAdmin, admin)
 }
---
+++ routes/modules/admin.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const adminController = require('../../controllers/adminController')
+const router = express.Router()
+
+module.exports = router
