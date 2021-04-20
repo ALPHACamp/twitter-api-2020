976808617f30384529cb12fdc6d87caa086e81c9
Yuchi Su <astraiostor@gmail.com>
2021-04-21 00:41:01 +0800

    Adjust coding style in likes seed file

--- seeders/20210419171007-likes-seed-file.js
+++ seeders/20210419171007-likes-seed-file.js
@@ -4,20 +4,21 @@ module.exports = {
 
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Likes',
-      [{
-        id: 1,
-        UserId: 1,
-        TweetId: 11,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        id: 2,
-        UserId: 2,
-        TweetId: 21,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
+      [
+        {
+          id: 1,
+          UserId: 1,
+          TweetId: 11,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 2,
+          UserId: 2,
+          TweetId: 21,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
       ], {})
   },
 
