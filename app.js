e5ce68ef81493b6b0fa59f06fdc2a8ddccbdf308
Sherry <sherry.c.liao@gmail.com>
2021-04-23 21:31:08 +0800

    feat: pull new progress to local

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const helpers = require('./_helpers')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -39,6 +40,7 @@ const swaggerOptions = {
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
