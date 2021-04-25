e98ac33e662a48ad213bd827721d1bb7e1e05112
Yuchi Su <astraiostor@gmail.com>
2021-04-25 16:05:59 +0800

    Add postReply feature

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -1,5 +1,7 @@
+const { catchError } = require('../utils/errorHandling')
 const db = require('../models')
 const Reply = db.Reply
+const Tweet = db.Tweet
 
 module.exports = {
   getReplies: (req, res) => {
@@ -16,9 +18,35 @@ module.exports = {
         return res.status(200).json(replies)
       })
       .catch(error => {
-        const data = { status: 'error', message: error.toString() }
-        console.log(error)
-        return res.status(500).json(data)
+        catchError(res, error)
+      })
+  },
+
+  postReply: (req, res) => {
+    const { comment } = req.body
+    const { tweetId } = req.params
+
+    Tweet.findOne({ where: { id: tweetId } })
+      .then(tweet => {
+        if (!tweet) {
+          return res.status(200).json({ status: 'error', message: 'Tweet doesn\'t esist' })
+        }
+      })
+
+    if (!comment) {
+      return res.status(400).json({ status: 'error', message: 'write the comment before reply' })
+    }
+
+    Reply.create({
+      TweetId: tweetId,
+      UserId: req.user.id,
+      comment: comment
+    })
+      .then(() => {
+        return res.status(200).json({ status: 'success', message: 'reply tweet successfully' })
+      })
+      .catch(error => {
+        catchError(res, error)
       })
   }
 }
--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -7,5 +7,6 @@ router.get('/', tweetController.getTweets)
 router.post('/', tweetController.postTweet)
 router.get('/:tweetId', tweetController.getTweet)
 router.get('/:tweetId/replies', replyController.getReplies)
+router.post('/:tweetId/replies', replyController.postReply)
 
 module.exports = router
