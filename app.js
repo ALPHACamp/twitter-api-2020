7cfd9dcc4f4cb50f538f60d013f5c370fb3cfc45
Karol <super000999888@gmail.com>
2021-04-26 16:31:57 +0800

    add socketio & do some settings

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const http = require('http')
+const socketIo = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 // .env
 if (process.env.NODE_ENV !== 'production') {
@@ -9,6 +12,8 @@ if (process.env.NODE_ENV !== 'production') {
 
 const app = express()
 const port = process.env.PORT || 3000
+const server = http.createServer(app)
+const io = socketIo(server)
 
 // 載入 cors
 app.use(cors())
@@ -28,6 +33,21 @@ app.use((err, req, res, next) => {
   }
 })
 
+// socket
+const botName = 'Chat Bot'
+io.on('connection', socket => {
+  socket.emit('message', formatMessage(botName, 'Welcome to Chat!'))
+  socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'))
+  socket.on('disconnect', () => {
+    io.emit('message', formatMessage(botName, 'A user has left the chat'))
+  })
+  socket.on('chatMessage', (msg) => {
+    io.emit('message', formatMessage('USER', msg))
+  })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+server.listen(4000, () => console.log('Server is running on 4000'))
 
 module.exports = app
