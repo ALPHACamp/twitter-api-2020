c2b9106d8acd12de60340b1e1db014a5e3c6ad7f
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-28 12:04:32 -0700

    fix: modify app.js

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', true)
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  )
+  next()
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
