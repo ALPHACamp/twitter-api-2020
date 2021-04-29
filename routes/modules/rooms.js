e8e704900f47210346ceb17882104286f42c89b9
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-04-29 12:46:04 -0700

    feat: add POST /api/rooms/public/messages route

--- routes/modules/rooms.js
+++ routes/modules/rooms.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const roomController = require('../../controllers/roomController')
 
 router.route('/').post(roomController.createRoom)
+router.route('/public/messages').post(roomController.sendPublicMessage)
 router.route('/:roomId/messages').post(roomController.sendMessage)
 
 module.exports = router
