77c822adef9fec63e505ea3a0a0d8f0defbc4cf9
Flora2020 <rossignols1989@gmail.com>
2021-04-30 17:27:46 +0800

    Initialize socket

--- config/socket.js
+++ config/socket.js
@@ -1,16 +1,28 @@
 const socket = (httpServer) => {
   const options = {
+    allowEIO3: true,
     cors: {
-      origin: 'http://localhost:8080/',
+      origin: 'http://localhost:8080',
       methods: ['GET', 'POST'],
-      allowedHeaders: ['my-custom-header'],
       credentials: true
     }
   }
   const io = require('socket.io')(httpServer, options)
   io.on('connection', socket => {
+    console.log('客户端有连接')
+
+    socket.on('connect', () => {
+      console.log('客户端开始连接')
+    })
+
+    socket.on('disconnect', () => {
+      console.log('客户端断开')
+    })
+
+    socket.emit('welcome', '欢迎连接socket')
+
     socket.on('hello', (arg) => {
-      console.log(arg)
+      console.log('接收客户端发送数据:', arg)
     })
   })
 }
