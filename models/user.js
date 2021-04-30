3aa884f15f02f94c708ca03d942157414442a005
yuchengH1988 <yucheng.h.1988@gmail.com>
2021-04-30 21:05:17 +0800

    built subscript model

--- models/user.js
+++ models/user.js
@@ -3,15 +3,15 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User', {
-      account: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      name: DataTypes.STRING,
-      avatar: DataTypes.STRING,
-      cover: DataTypes.STRING,
-      introduction: DataTypes.STRING,
-      role: DataTypes.STRING
-    }, {}
+    account: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    cover: DataTypes.STRING,
+    introduction: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {}
   )
   User.associate = function (models) {
     User.hasMany(models.Reply)
@@ -36,6 +36,16 @@ module.exports = (sequelize, DataTypes) => {
       through: models.Followship,
       foreignKey: 'followerId',
       as: 'Followings'
+    })
+    User.belongsToMany(User, {
+      through: models.Subscript,
+      foreignKey: 'authorId',
+      as: 'Subscribers'
+    })
+    User.belongsToMany(User, {
+      through: models.Subscript,
+      foreignKey: 'subscriberId',
+      as: 'Authors'
     })
   }
   return User
