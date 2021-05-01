e630cd755d0577149c6a01c1faec1cbcd876c27e
Sherry <sherry.c.liao@gmail.com>
2021-05-01 18:04:02 +0800

    feat: add GET /rooms/availableUsers

--- routes/modules/rooms.js
+++ routes/modules/rooms.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const roomController = require('../../controllers/roomController')
 
 router.route('/').post(roomController.createRoom)
+router.route('/availableUsers').get(roomController.getAvailableUsers)
 router.route('/:roomId').get(roomController.getRoom)
 router.route('/:roomId/messages').post(roomController.sendMessage)
 
