27386efcc23b9783ae12fc3c92ba71796a0de362
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-05-13 18:59:31 -0700

    feat: add authenticate socket middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+
 const helpers = require('../_helpers')
 const passport = require('../config/passport')
 
@@ -40,6 +42,22 @@ module.exports = {
       return res
         .status(401)
         .json({ status: 'error', message: 'permission denied' })
+    }
+  },
+  authenticatedSocket: (socket, next) => {
+    console.log('==== authenticatedSocket ====')
+    console.log('socket.handshake.auth', socket.handshake.auth)
+    if (socket.handshake.auth && socket.handshake.auth.token) {
+      jwt.verify(
+        socket.handshake.auth.token,
+        process.env.JWT_SECRET,
+        (err, decoded) => {
+          if (err) return next(new Error('Authentication error'))
+          socket.userId = decoded.id
+          console.log('socket.userId', socket.userId)
+          next()
+        }
+      )
     }
   }
 }
