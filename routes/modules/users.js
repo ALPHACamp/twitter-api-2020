8220cb158133205df97668f455c61b75aa950f97
Ivy Hung <ivyhungtaiwan@gmail.com>
2021-05-15 08:18:50 -0700

    refactor: change subscriptions routes to POST/DELETE /api/users/:id/subscriptions

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,6 +5,7 @@ const { authenticated, authenticatedUser } = require('../../middleware/auth')
 
 const userController = require('../../controllers/userController')
 const followshipController = require('../../controllers/followshipController')
+const subscriptionController = require('../../controllers/subscriptionController')
 
 // Upload image
 const multer = require('multer')
@@ -49,8 +50,16 @@ router.route('/:id/likes').get(authenticated, userController.getLikes)
 router.route('/:id/followers').get(authenticated, userController.getFollowers)
 router.route('/:id/followings').get(authenticated, userController.getFollowings)
 
-router.route('/:id/followships')
-  .post(authenticated, authenticatedUser, followshipController.followUser)
-  .delete(authenticated, authenticatedUser, followshipController.unfollowUser)
+router
+  .route('/:id/followships')
+  .all(authenticated, authenticatedUser)
+  .post(followshipController.followUser)
+  .delete(followshipController.unfollowUser)
+
+router
+  .route('/:id/subscriptions')
+  .all(authenticated, authenticatedUser)
+  .post(subscriptionController.subscribeUser)
+  .delete(subscriptionController.unsubscribeUser)
 
 module.exports = router
