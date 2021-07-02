719d8808dcb27dc48b35a66858fe9f1c2c46c06e
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-02 14:49:21 +0800

    feat: add middleware for mocha test's requirement

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const helpers = require('./_helpers');
 const app = express()
 const port = 3000
 
+// for mocha test's requirement
+app.use((req, res, next) => {
+  req.user = helpers.getUser(req)
+  next()
+})
+
 // use helpers.getUser(req) to replace req.user
 function authenticated(req, res, next){
   // passport.authenticate('jwt', { ses...
-};
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
