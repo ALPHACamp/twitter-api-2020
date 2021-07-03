4ed53f57188b9421f3550cea7e2c40c61481d38a
Charlie <wl00606352@gmail.com>
2021-07-03 18:45:34 +0800

    feat: add routes of post new tweet

--- routes/apis/tweetRoute.js
+++ routes/apis/tweetRoute.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const tweetController = require('../../controllers/tweetController')
 
 router.get('/', tweetController.getTweets)
+router.post('/', tweetController.postTweet)
 router.get('/:tweet_id/replies', tweetController.getTweetAndReplies)
 router.post('/:tweet_id/replies', tweetController.postReply)
 router.post('/:tweet_id/like', tweetController.likeTweet)
