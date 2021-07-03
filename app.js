d58731f4d159ca838354386097b20e2ab3d4e68e
stylelinz <cwchuang1997@gmail.com>
2021-07-03 15:14:50 +0800

    feat: modify app.js

--- app.js
+++ app.js
@@ -3,24 +3,18 @@ const session = require('express-session')
 const methodOverride = require('method-override')
 const helpers = require('./_helpers')
 
+const routes = require('./routes')
+
 const app = express()
 const port = 3000
 
-// use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next) {
-  // passport.authenticate('jwt', { ses...
-};
-
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(express({ secret: "devSecretIsVeryMystery", resave: false, saveUninitialized: false }))
+app.use(session({ secret: 'devSecretIsVeryMystery', resave: false, saveUninitialized: false }))
 app.use(methodOverride('_method'))
 
-app.use((req, res, next) => {
-  res.locals.user = helpers.getUser(req)
-})
+app.use(routes)
 
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
