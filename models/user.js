68a3c27bf8636b375504eacb3f93032a47f8bc18
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-04 20:56:34 +0800

    feat: add user model's datatypes

--- models/user.js
+++ models/user.js
@@ -1,8 +1,49 @@
-'use strict'
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    account: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://www.seekpng.com/png/full/114-1149972_avatar-free-png-image-avatar-png.png'
+    },
+    cover: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://www.seekpng.com/png/full/153-1536586_free-facebook-cover-image-transparent-png-facebook-cover.png'
+    },
+    intro: DataTypes.TEXT,
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'user'
+    }
   }, {})
   User.associate = function (models) {
+    User.hasMany(models.Reply)
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Like)
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweets'
+    })
   }
   return User
 }
