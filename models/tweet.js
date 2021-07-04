1d78442bc9e92a1ff95363075ee2bbd2fc138eda
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-04 21:09:14 +0800

    feat: add tweet model's datatype and connect to other models

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,18 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
+    UserId: DataTypes.INTEGER,
+    description: DataTypes.TEXT
   }, {})
   Tweet.associate = function (models) {
+    Tweet.belongsTo(models.User)
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
+    Tweet.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'TweetId',
+      as: 'LikedUsers'
+    })
   }
   return Tweet
 }
