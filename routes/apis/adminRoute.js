2c1ff4c7673f8cb707380568b0838f03ddfe904e
Charlie <wl00606352@gmail.com>
2021-07-04 21:41:56 +0800

    gitflow-feature-stash: admin_routing

--- routes/apis/adminRoute.js
+++ routes/apis/adminRoute.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const adminController = require('../../controllers/adminController')
+
 router.get('/', (req, res, next) => {
   res.send('this is /api/admin router.')
 })
+
+router.post('/signin', adminController.signIn)
+router.delete('/tweets/:tweet_id', adminController.deleteTweet)
+router.get('/users', adminController.getUsers)
 
 module.exports = router
---
+++ services/adminService.js
@@ -0,0 +1,48 @@
+const { User, Tweet, Like, sequelize } = require('../models')
+
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const adminService = {
+  signIn: async (email, pw) => {
+    if (!(email && pw)) {
+      throw new Error('email and password are required')
+    }
+
+    const user = await User.findOne({ where: { email: email } })
+    if (!user) return { status: 404, message: 'user no found' }
+    if (!bcrypt.compareSync(pw, user.password)) return { status: 401, message: 'password not match' }
+    const payload = { id: user.id }
+    const token = jwt.sign(payload, 'JWTSecretIsWeird')
+    return { message: 'OK', token: token, user: user }
+  },
+
+  deleteTweet: async (tweetId) => {
+    await Tweet.destroy({ where: { id: tweetId } })
+    return ({ message: `the tweet id ${tweetId} deleted successfully` })
+  },
+
+  getUsers: async () => {
+    return await User.findAll({
+      raw: true,
+      nest: true,
+      attributes: [
+        'id',
+        'name',
+        [sequelize.literal('count(distinct Tweets.id)'), 'TweetsCount'],
+        [sequelize.literal('count(distinct Likes.id)'), 'LikesCount'],
+        [sequelize.literal('count(distinct Followers.id)'), 'FollowersCount'],
+        [sequelize.literal('count(distinct Followings.id)'), 'FollowingsCount']
+      ],
+      group: 'id',
+      include: [
+        { model: Tweet, attributes: [] },
+        { model: Like, attributes: [] },
+        { model: User, as: 'Followers', attributes: [], through: { attributes: [] } },
+        { model: User, as: 'Followings', attributes: [], through: { attributes: [] } }
+      ]
+    })
+  }
+}
+
+module.exports = adminService
