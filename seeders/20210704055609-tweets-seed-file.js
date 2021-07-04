f7f52408c3d77dd28f88d95bd291d198c3338684
lewislu327 <agooday32@gmail.com>
2021-07-04 19:49:29 +0800

    refactor: adjust seeds loading method , add 140 characters limit

--- seeders/20210704055609-tweets-seed-file.js
+++ seeders/20210704055609-tweets-seed-file.js
@@ -1,72 +1,22 @@
 'use strict'
 const faker = require('faker')
-const tweetCount = 10
-const db = require('../models')
-
+const { User } = require('../models')
+const tweetCount = 10 // 每個 user 10 個 tweets
+const userCount = 5 // 有 5 個 users
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const users = await User.findAll({ where: { role: 'user' } }) // 避免資料庫 id 跳號
     await queryInterface.bulkInsert(
       'Tweets',
-      Array.from({ length: tweetCount }).map((d, i) => ({
-        description: faker.lorem.sentences(),
-        UserId: 1,
+      Array.from({ length: tweetCount * userCount }).map((_, i) => ({
+        description: faker.lorem.sentences().substring(0, 140),
+        UserId: users[Math.floor(i / 10)].id,
         createdAt: new Date(),
         updatedAt: new Date(),
       })),
       {}
-    ),
-      await queryInterface.bulkInsert(
-        'Tweets',
-        Array.from({ length: tweetCount }).map((d, i) => ({
-          description: faker.lorem.sentences(),
-          UserId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        })),
-        {}
-      ),
-      await queryInterface.bulkInsert(
-        'Tweets',
-        Array.from({ length: tweetCount }).map((d, i) => ({
-          description: faker.lorem.sentences(),
-          UserId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        })),
-        {}
-      ),
-      await queryInterface.bulkInsert(
-        'Tweets',
-        Array.from({ length: tweetCount }).map((d, i) => ({
-          description: faker.lorem.sentences(),
-          UserId: 4,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        })),
-        {}
-      ),
-      await queryInterface.bulkInsert(
-        'Tweets',
-        Array.from({ length: tweetCount }).map((d, i) => ({
-          description: faker.lorem.sentences(),
-          UserId: 5,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        })),
-        {}
-      ),
-      await queryInterface.bulkInsert(
-        'Tweets',
-        Array.from({ length: tweetCount }).map((d, i) => ({
-          description: faker.lorem.sentences(),
-          UserId: 6,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        })),
-        {}
-      )
+    )
   },
-
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Tweets', null, {})
   },
