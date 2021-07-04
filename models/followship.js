244e5ac7c38409a4e60a4aed43da1eb0434a72f6
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-04 21:30:41 +0800

    feat: add followship model's datatypes

--- models/followship.js
+++ models/followship.js
@@ -1,6 +1,14 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Followship = sequelize.define('Followship', {
+    followingId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    followerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {})
   Followship.associate = function (models) {
   }
