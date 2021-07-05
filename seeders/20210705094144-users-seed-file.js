f35e086b1131f646d7780cfde787de5dccc763b8
Hsinyehh <rita.yeh@0617gmail.com>
2021-07-05 21:46:32 +0800

    add reply-seed-data

--- seeders/20210705094144-users-seed-file.js
+++ seeders/20210705094144-users-seed-file.js
@@ -6,25 +6,18 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Users', [{
       id: 1,
-      email: 'root@example.com',
-      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
-      name: 'root',
-      account: 'root',
-      intro: faker.lorem.text(),
-      role: 'admin',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }, {
-      id: 2,
       email: 'ryan@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       name: 'Ryan',
       account: 'RyanHuang',
       intro: faker.lorem.text(),
+      avatar: 1,
+      cover: 2,
+      role: 'user',
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      id: 3,
+      id: 2,
       email: 'lyvia@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       name: 'Lyvia',
@@ -33,7 +26,7 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      id: 4,
+      id: 3,
       email: 'aaron@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       name: 'Aaron',
@@ -42,12 +35,34 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
     }, {
-      id: 5,
+      id: 4,
       email: 'betrice@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       name: 'Beatrice',
       account: 'BeatricePai',
       intro: faker.lorem.text(),
+
+
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 5,
+      email: 'tim@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: 'Tim',
+      account: 'TimChien',
+      intro: faker.lorem.text(),
+
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      id: 6,
+      email: 'root@example.com',
+      password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+      name: 'root',
+      account: 'root',
+      intro: faker.lorem.text(),
+      role: 'admin',
       createdAt: new Date(),
       updatedAt: new Date()
     },
---
+++ seeders/20210705130559-reply-seed-file.js
@@ -0,0 +1,21 @@
+'use strict';
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Replies',
+      Array.from({ length: 150 }).map((item, index) => ({
+        id: index + 1,
+        UserId: Math.ceil(Math.random() * 5),
+        TweetId: Math.ceil((index + 1) / 3),
+        comment: faker.lorem.text(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      ), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', null, {})
+  }
+};
