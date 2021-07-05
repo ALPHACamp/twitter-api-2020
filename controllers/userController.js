f6556be5d0c9afa505bfaca9ee15a285cac482b2
Meg <ai285063@gmail.com>
2021-07-05 14:53:03 +0800

    feat: add folders routes & controllers

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const User = db.User
+
+const userController = {
+
+}
+
+module.exports = userController
---
+++ routes/modules/users.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+// const passport = require('passport')
+// const bcrypt = require('bcryptjs')
+
+// const db = require('../../models')
+// const User = db.User
+
+module.exports = router
