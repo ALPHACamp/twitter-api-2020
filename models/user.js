53cfa338c9fcd6793cc7b5350c6dbb7cd8f70de1
zhih <fufong79570@gmail.com>
2021-07-05 23:44:26 +0800

    feature: add attributes of User

--- models/user.js
+++ models/user.js
@@ -1,7 +1,62 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        type: DataTypes.STRING,
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
+      name: {
+        type: DataTypes.STRING,
+      },
+      account: {
+        type: DataTypes.STRING,
+      },
+      avatar: {
+        type: DataTypes.STRING,
+      },
+      cover: {
+        type: DataTypes.STRING,
+      },
+      bio: {
+        type: DataTypes.TEXT,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      likeNum: {
+        type: DataTypes.INTEGER,
+      },
+      replyNum: {
+        type: DataTypes.INTEGER,
+      },
+      followingNum: {
+        type: DataTypes.INTEGER,
+      },
+      followerNum: {
+        type: DataTypes.INTEGER,
+      },
+      lastLoginAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {}
+  );
   User.associate = function(models) {
   };
   return User;
