d1a0a3a599cb713eea5972ac2963e94b79ea1377
Hsinyehh <rita.yeh@0617gmail.com>
2021-07-05 19:45:33 +0800

    add tweets-seed-data

---
+++ seeders/20210705103445-tweets-seed-file.js
@@ -0,0 +1,19 @@
+'use strict';
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Tweets',
+      Array.from({ length: 50 }).map((item, index) => ({
+        UserId: Math.ceil((index + 1) / 10),
+        description: faker.lorem.text(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })
+      ), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Tweets', null, {})
+  }
+};
