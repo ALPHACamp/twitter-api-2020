9597121b6f42290632357fb8379ca3016315f9e8
lewislu327 <agooday32@gmail.com>
2021-07-05 16:10:38 +0800

    feat: add adminController for signin

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,9 @@
-const passport = require("passport")
-const jwt = require("jsonwebtoken")
-const passportJWT = require("passport-jwt")
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
 const ExtractJwt = passportJWT.ExtractJwt
 const JwtStrategy = passportJWT.Strategy
+const { User, Tweet } = require('../models')
 
 let jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
@@ -11,15 +12,15 @@ jwtOptions.secretOrKey = process.env.JWT_SECRET
 let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
   User.findByPk(jwt_payload.id, {
     include: [
-      { model: db.Tweet, as: "LikedTweets" },
-      { model: User, as: "Followers" },
-      { model: User, as: "Followings" },
+      { model: db.Tweet, as: 'LikedTweets' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' },
     ],
   }).then((user) => {
     if (!user) return next(null, false)
     return next(null, user)
   })
 })
-passport.use(strategy)
 
 module.exports = passport
+passport.use(strategy)
