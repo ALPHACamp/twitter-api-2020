3fdcbc97f602475ac0c86b8ce1dba1f2c6ba4ae3
Chris Chen <ok3201123@gmail.com>
2021-07-05 19:59:47 +0800

    feat: add passport and jwt-strategy

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,28 @@
 const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+const { User, Tweet } = require('../models')
 
+// JWT strategy
 
+const jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+jwtOptions.secretOrKey = 'SimpleTwitterSecret'
+
+const strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+  User.findByPk(jwt_payload.id, {
+    include: [
+      { model: Tweet, as: 'LikedTweets' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  }).then(user => {
+    if (!user) return next(null, false)
+    return next(null, user)
+  })
+})
+passport.use(strategy)
 
 module.exports = passport
