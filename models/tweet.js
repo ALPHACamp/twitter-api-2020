c34273bd72a3e5edc7386fcab02e1a1377deb452
Meg <ai285063@gmail.com>
2021-07-05 11:10:39 +0800

    feat: compose  models/tweet

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
+    UserId: DataTypes.INTEGER,
+    description: DataTypes.TEXT
   }, {});
   Tweet.associate = function(models) {
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
+    Tweet.belongsTo(models.User)
   };
   return Tweet;
 };
