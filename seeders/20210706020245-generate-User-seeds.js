e5024fff45eb86391f23ab7282b062c9745b4b60
zhih <fufong79570@gmail.com>
2021-07-06 10:42:46 +0800

    feature: add seeds of model User
    5 users
    
    pasword is 12345678
    
    user's email  is from user1@example.com to user5@example.com

---
+++ seeders/20210706020245-generate-User-seeds.js
@@ -0,0 +1,28 @@
+'use strict';
+const bcrypt = require("bcrypt-nodejs");
+const faker = require("faker");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const data = Array.from({ length: 5 }).map((user, i) => ({
+      id: i + 1,
+      account: `user${i + 1}`,
+      email: `user${i + 1}@example.com`,
+      password: bcrypt.hashSync("12345678", bcrypt.genSaltSync(10), null),
+      name: faker.name.findName(),
+      avatar: "https://loremflickr.com/g/320/240/girl/all",
+      cover: "https://loremflickr.com/800/600/dog",
+      bio: faker.lorem.text(100),
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      likeNum: 0,
+      replyNum: 0,
+      followingNum: 0,
+      followerNum: 0,
+      lastLoginAt: new Date()
+    }))
+    await queryInterface.bulkInsert("Users", data, {});
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Users", null, {});
+  }
+};
