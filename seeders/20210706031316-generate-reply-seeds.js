744841253288effc3b82bc935fa83fe9e20d4bb8
zhih <fufong79570@gmail.com>
2021-07-06 11:49:58 +0800

    feature: add seeds of model Reply
    every tweets has 3 replies

---
+++ seeders/20210706031316-generate-reply-seeds.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const faker = require("faker");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const data = [];
+    const index = 0;
+    Array.from({ length: 5 }).map((user, i) => {
+      for (let j = 0; j < 10; ++j) {
+        data.push(...Array.from({ length: 3 }).map((reply) => ({
+          id: ++index,
+          TweetId: j * 10 + i + 1,
+          UserId: Math.floor(Math.random() * 5 + 1),
+          content: faker.lorem.text(140),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })))
+      }
+    })
+    await queryInterface.bulkInsert("Replies", data, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Replies", null, {});
+  }
+};
