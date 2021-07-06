5f7ddb14ea9fcbc9e791579aa4e189f5d7c68306
lewislu327 <agooday32@gmail.com>
2021-07-06 18:23:23 +0800

    refactor: removeConstraint and adjust output data to pass test

---
+++ migrations/20210706094610-removeConstraint-to-Likes.js
@@ -0,0 +1,17 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.removeConstraint('Likes', 'likes_ibfk_1')
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  },
+}
