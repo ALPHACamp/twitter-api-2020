9503ecdbd3dbb52a9bf33e42b9c730ef067281d2
Jennesy <jennesy.dai@gmail.com>
2021-07-06 16:58:52 +0800

    feat: GET /api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,7 @@
-const express = require("express");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const userController = require('../../controllers/api/userController')
 
-router.get("/", (req, res) => {
-  res.send("user here")
-})
+router.get('/', userController.getUsers)
 
-module.exports = router;
+module.exports = router
