70f8ec1119d097f695926f70cc236e8b562f9742
Chris Chen <ok3201123@gmail.com>
2021-07-06 18:02:26 +0800

    feat: add signin route and userController.signIn

--- routes/apis.js
+++ routes/apis.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 
+// user routes
 router.post('/users', userController.signUp)
+router.post('/signin', userController.signIn)
 
 module.exports = router
