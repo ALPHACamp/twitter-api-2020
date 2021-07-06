3d08154b6d39ce75a967dd5c81d80060d92fc7ca
Jennesy <jennesy.dai@gmail.com>
2021-07-06 18:48:37 +0800

    feat: POST /api/users

--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -1,5 +1,6 @@
 const db = require('../../models')
 const User = db.User
+const bcrypt = require('bcrypt-nodejs')
 const defaultLimit = 10
 
 let userController = {
@@ -9,7 +10,8 @@ let userController = {
     const userId = 1 //before building JWT
     const order = [['followerNum', 'DESC']]
     const attributes = ['id', 'account', 'name', 'avatar', 'cover', 'tweetNum', 'likeNum', 'followingNum', 'followerNum', 'lastLoginAt']
-    User.findAll({ offset, limit, order, attributes,
+    User.findAll({
+      offset, limit, order, attributes,
       include: [{
         model: User,
         as: "Followers",
@@ -24,6 +26,46 @@ let userController = {
       return res.json({ Users: users })
     }).catch(err => {
       return res.json({ status: 'error', message: err })
+    })
+  },
+  postUser: (req, res) => {
+    const { name, account, email, password, checkPassword } = req.body
+    if (!name || !account || !email || !password || !checkPassword) {
+      return res.json({
+        status: 'error', message: "Missing data."
+      })
+    }
+    if (password !== checkPassword) {
+      return res.json({
+        status: 'error', message: "Password and confirm password doesn't match."
+      })
+    }
+    User.findOne({ where: { account } }).then(user => {
+      if (user) {
+        return res.json({
+          status: 'error', message: "Account was already used."
+        })
+      } else {
+        User.findOne({ where: { email } }).then(user => {
+          if (user) {
+            return res.json({
+              status: 'error', message: "Email was already used."
+            })
+          } else {
+            User.create({
+              name,
+              account,
+              email,
+              password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+            })
+              .then(user => {
+                return res.json({
+                  status: 'success', message: "Account successfully created."
+                })
+              })
+          }
+        })
+      }
     })
   }
 }
