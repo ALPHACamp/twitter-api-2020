02e46deb7d5803604a4a4b49257b30478cbc50f5
zhih <fufong79570@gmail.com>
2021-07-06 11:11:39 +0800

    feature: add seeds of model tweet

---
+++ seeders/20210706024408-generate-Tweet-seeds.js
@@ -0,0 +1,25 @@
+'use strict';
+const faker = require("faker");
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const data = [];
+    Array.from({ length: 5 }).map((user, i) => {
+      for (let j = 0; j < 10; ++j) {
+        data.push({
+          id: j * 10 + i + 1,
+          content: faker.lorem.text(140),
+          likeNum: 0,
+          replyNum: 0,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          UserId: i + 1,
+        })
+      }
+    })
+    await queryInterface.bulkInsert("Tweets", data, {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Tweets", null, {});
+  }
+};
