f23975f3942c6416523be0dfcf8cd606e7741596
Meg <ai285063@gmail.com>
2021-07-06 10:22:23 +0800

    feat: POST /api/login (return jwt)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,57 @@
 const bcrypt = require('bcryptjs')
-const db = require('../../models')
+const db = require('../models')
 const User = db.User
 
-const userController = {
+const jwt = require('jsonwebtoken')
+const passportJWT = require('passport-jwt')
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
 
+const userController = {
+  signUp: (req, res) => {
+    if (req.body.checkPassword !== req.body.password) {
+      return res.json({ status: 'error', message: '兩次密碼輸入不同！' })
+    } else {
+      User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          return res.json({ status: 'error', message: '信箱重複！' })
+        } else {
+          User.create({
+            account: req.body.account,
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+          }).then(user => {
+            return res.json({ status: 'success', message: '成功註冊帳號！' })
+          })
+        }
+      })
+    }
+  },
+  logIn: (req, res) => {
+    if (!req.body.account || !req.body.password) {
+      return res.json({ status: 'error', message: "required fields didn't exist" })
+    }
+    const account = req.body.account
+    const password = req.body.password
+
+    User.findOne({ where: { account: account } }).then(user => {
+      if (!user) return res.status(401).json({ status: 'error', message: 'no such user found' })
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(401).json({ status: 'error', message: 'passwords did not match' })
+      }
+      const payload = { id: user.id, role: user.role }
+      const token = jwt.sign(payload, process.env.JWT_SECRET)
+      return res.json({
+        status: 'success',
+        message: 'ok',
+        token: token,
+        user: {
+          id: user.id, name: user.name, email: user.email, role: user.role
+        }
+      })
+    })
+  }
 }
 
 module.exports = userController
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
-
+// const helpers = require('../../_helpers')
 // const passport = require('passport')
-// const bcrypt = require('bcryptjs')
 
-// const db = require('../../models')
-// const User = db.User
+const userController = require('../../controllers/userController')
+
+router.post('/login', userController.logIn)
+router.post('/', userController.signUp)
 
 module.exports = router
