8283f4063ed7d55e5300e9dcd411df97c38bb6b3
zhih <fufong79570@gmail.com>
2021-07-07 18:28:30 +0800

    feature: GET/api/tweets/tweetId/like

--- routes/api/tweet.js
+++ routes/api/tweet.js
@@ -2,11 +2,13 @@ const express = require('express')
 const router = express.Router()
 const tweetController = require('../../controllers/api/tweetController')
 const replyController = require('../../controllers/api/replyController')
+const likeController = require('../../controllers/api/likeController')
 
 router.get('/', tweetController.getTweets)
 router.post('/', tweetController.postTweet)
 router.get('/:tweetId', tweetController.getTweet)
 router.get('/:tweetId/replies', replyController.getReplies)
 router.post('/:tweetId/replies', replyController.postReply)
+router.get('/:tweetId/like', likeController.getLikes)
 
 module.exports = router
