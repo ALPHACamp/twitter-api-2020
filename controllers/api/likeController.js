d6b4d03bcd560ce6055ba8e800e90e3fce2c30cf
zhih <fufong79570@gmail.com>
2021-07-07 16:47:35 +0800

    feature: GET/api/tweets/tweetId/like
    get all users who like the assign tweet

--- controllers/api/likeController.js
+++ controllers/api/likeController.js
@@ -1,5 +1,33 @@
+const db = require('../../models')
+const Like = db.Like
+const User = db.User
+
+let defaultLimit = 10
+
 let likeController = {
+  getLikes: (req, res) => {
+    const options = {
+      limit: +req.query.limit || defaultLimit,
+      offset: +req.query.offset || 0,
+      where: {
+        TweetId: req.params.tweetId,
+      },
+      attributes: [],
+      include: [
+        {
+          model: User,
+          attributes: ['id', 'account', 'name', 'avatar'],
+          order: [['followerNum', 'DESC']],
+        },
+      ],
+    }
+    Like.findAll(options)
+      .then((likes) => {
+        likes = likes.map((like) => like.User)
+        res.status(200).json({ Users: likes })
+      })
+      .catch(() => res.status(404).json({ status: 'error', messgae: '' }))
+  },
+}
 
-};
-
-module.exports = likeController;
+module.exports = likeController
