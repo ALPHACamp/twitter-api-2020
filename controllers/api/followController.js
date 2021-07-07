67d3b7796382f9a3477acaf50bd45fb57f8dd2c9
Jennesy <jennesy.dai@gmail.com>
2021-07-08 03:21:52 +0800

    feat: GET api/users/:id/followings

--- controllers/api/followController.js
+++ controllers/api/followController.js
@@ -1,5 +1,23 @@
-let followController = {
+const db = require('../../models')
+const Followship = db.Followship
+const User = db.User
 
+let followController = {
+  getUserFollowers: (req, res) => {
+    const options = {
+      where: { followingId: req.params.id },
+      include: {
+        model: User,
+        as: "follower",
+        attributes: ['id', 'account', 'name', 'avatar']
+      },
+      attributes: ['id', 'followingId', 'followerId']
+    }
+    Followship.findAll(options)
+      .then(followers => {
+        return res.status(200).json(followers)
+      }).catch(error => res.status(500).json({ status: 'error', message: error }))
+  }
 };
 
 module.exports = followController;
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,10 +4,13 @@ const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 const imageFields = [{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]
 const userController = require('../../controllers/api/userController')
+const followController = require('../../controllers/api/followController')
 
 router.get('/', userController.getUsers)
 router.post('/', userController.postUser)
 router.get('/:id', userController.getUser)
 router.put('/:id', upload.fields(imageFields), userController.putUser)
+
+router.get('/:id/followers', followController.getUserFollowers)
 
 module.exports = router
