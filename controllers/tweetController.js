612292552884205c5713fff5ecd3f1c4f7008837
lewislu327 <agooday32@gmail.com>
2021-07-07 16:04:51 +0800

    add likeTweet and unlikeTweet into tweetController

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -40,6 +40,23 @@ let tweetController = {
       })
       .catch((err) => next(err))
   },
+
+  likeTweet: (req, res, next) => {
+    return Like.create({
+      UserId: helpers.getUser(req).id,
+      TweetId: req.params.tweetId,
+    })
+      .then((tweet) => {
+        res.json({ status: 'success', message: 'Like was successfully created' })
+      })
+      .catch((err) => next(err))
+  },
+
+  unlikeTweet: (req, res, next) => {
+    return Like.findOne({ where: { UserId: helpers.getUser(req).id, TweetId: req.params.tweetId } }).then((tweet) =>
+      tweet.destroy().then(res.json({ status: 'success', message: 'Like was successfully deleted' }))
+    )
+  },
 }
 
 module.exports = tweetController
