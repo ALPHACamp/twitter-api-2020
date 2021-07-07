f3c2f84054abd5ae529cbbe2c36c1bb832520ec9
bradychen <bradychen2@gmail.com>
2021-07-07 11:33:40 +0800

    fix: reverse authenticated func if statement

--- routes/apis.js
+++ routes/apis.js
@@ -11,7 +11,7 @@ const authenticated = passport.authenticate('jwt', { session: false })
 // 驗證登入者是否為管理者=>用於後台路由
 const authenticatedAdmin = (req, res, next) => {
   if (helpers.getUser(req)) {
-    if (helpers.getUser(req).role === 'normal') { return next() }
+    if (helpers.getUser(req).role === 'admin') { return next() }
     return res.json({ status: 'error', message: '非管理者沒有權限登入後台！' })
   } else {
     return res.json({ status: 'error', message: '未通過身份驗證！' })
@@ -20,7 +20,7 @@ const authenticatedAdmin = (req, res, next) => {
 // 驗證登入者是否為非管理者=>用於前台路由
 const authenticatedNotAdmin = (req, res, next) => {
   if (helpers.getUser(req)) {
-    if (helpers.getUser(req).role === 'admin') { return next() }
+    if (helpers.getUser(req).role === 'normal') { return next() }
     return res.json({ status: 'error', message: '管理者沒有權限登入前台！' })
   } else {
     return res.json({ status: 'error', message: '未通過身份驗證！' })
@@ -31,7 +31,7 @@ const authenticatedNotAdmin = (req, res, next) => {
 // user routes
 router.post('/users', userController.signUp)
 router.post('/signin', userController.signIn)
-
+router.get('/users/:id', authenticatedNotAdmin, userController.getUser)
 // admin routes
 router.post('/admin/signin', adminController.signIn)
 
