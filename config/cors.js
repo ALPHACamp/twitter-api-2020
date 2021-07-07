19296d78115f5a43305416367ab53677c4020849
stylelinz <cwchuang1997@gmail.com>
2021-07-07 20:40:56 +0800

    refactor: move CORS whitelist from hard code to .env

--- config/cors.js
+++ config/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors')
 
 module.exports = () => {
-  const whiteList = ['http://localhost:8080']
+  const whiteList = process.env.CORS_WHITE_LIST.split(',')
   const corsOptionsDelegate = (req, callback) => {
     if (whiteList.indexOf(req.header('Origin')) !== -1) {
       return callback(null, { origin: true })
