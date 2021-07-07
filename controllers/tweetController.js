504d26a95e521727fd16e6dd990b4e4a7e4df7ed
Hsinyehh <rita.yeh@0617gmail.com>
2021-07-07 11:54:38 +0800

    add LikeCount in tweetController

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -2,6 +2,7 @@ const db = require("../models")
 const Tweet = db.Tweet
 const User = db.User
 const Reply = db.Reply
+const Like = db.Like
 
 
 const TweetController = {
@@ -18,9 +19,15 @@ const TweetController = {
     return Tweet.findByPk(req.params.tweet_id, {
       include: [
         User,
+        Like,
         { model: Reply, include: [User] }]
     })
-      .then(tweet => { return res.json({ tweet }) })
+      .then(tweet => {
+        return res.json({
+          tweet,
+          LikeCount: tweet.Likes.length
+        })
+      })
   },
 
 }
