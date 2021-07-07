a967579064bd974024f46d0af5fe8b084512686b
zhih <fufong79570@gmail.com>
2021-07-07 14:14:43 +0800

    feature: POST/api/tweets
    create instance of tweet

--- controllers/api/tweetController.js
+++ controllers/api/tweetController.js
@@ -113,6 +113,36 @@ let tweetController = {
       })
       .catch(() => res.status(404).json({ status: 'error', message: '' }))
   },
+  postTweet: (req, res) => {
+    if (!req.body.description) {
+      return res
+        .status(400)
+        .json({ status: 'error', messgae: 'Can not post empty description' })
+    }
+    if (req.body.description.length > 140) {
+      return res
+        .status(400)
+        .json({ status: 'error', messgae: 'Can not post over 140 characters' })
+    }
+    const data = {
+      UserId: currentUserId,
+      description: req.body.description,
+      likeNum: 0,
+      replyNum: 0
+    }
+    Tweet.create(data)
+      .then((tweet) =>
+        res
+          .status(200)
+          .json({ status: '200', message: 'Successfully posted new tweet.' })
+      )
+      .catch((error) =>
+        res.status(400).json({
+          status: 'error',
+          message: ''
+        })
+      )
+  },
 }
 
 module.exports = tweetController
--- routes/api/tweet.js
+++ routes/api/tweet.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const tweetController = require('../../controllers/api/tweetController')
 
 router.get('/', tweetController.getTweets)
+router.post('/', tweetController.postTweet)
 router.get('/:tweetId', tweetController.getTweet)
 
 module.exports = router
