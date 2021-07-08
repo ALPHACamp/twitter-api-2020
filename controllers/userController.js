aa51176ed40b7ec97fa1ff7ad2d04c762670e350
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-08 16:04:35 +0800

    feat: add editAccount feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const db = require('../models')
-const { User,Tweet,Like } = db
+const { User, Tweet, Like } = db
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 
@@ -15,7 +15,7 @@ const userController = {
     // check all inputs are required
       const { account, password } = req.body
       if (!account || !password) {
-        return res.json({ status: 'error', message: 'All fields are required！' })
+        return res.json({ status: 'error', message: 'All fields are required!' })
       }
       const user = await User.findOne({ where: { account } })
       if (!user) return res.status(401).json({ status: 'error', message: 'That account is not registered!' })
@@ -116,10 +116,10 @@ const userController = {
           { model: User, as: 'Followers' }
         ],
         attributes: ['id', 'name', 'email', 'avatar'],
-        limit:10,
+        limit: 10
       })
-      if (!users){
-        res.status(404).json({ status: 'error', message: 'Cannot find any user in db.' })
+      if (!users) {
+        return res.status(404).json({ status: 'error', message: 'Cannot find any user in db.' })
       }
       users = users.map(user => ({
         ...user.dataValues,
@@ -127,13 +127,83 @@ const userController = {
         isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
       }))
       users = users.sort((a, b) => b.followerCount - a.followerCount)
-    return res.status(200).json({users,status: 'success',
-          message: 'Get top ten users successfully'})
+      return res.status(200).json({
+        users,
+        status: 'success',
+        message: 'Get top ten users successfully'
+      })
     } catch (err) {
       console.log(err)
       res.status(500).json({ status: 'error', message: 'error' })
     }
   },
+  editAccount: async (req, res) => {
+    try {
+      const { account, name, email, password, checkPassword } = req.body
+      const { email: currentEmail, account: currentAccount } = req.user
+
+      const id = req.params.id
+  
+      //only user himself allow to edit account
+      if (req.user.id !== Number(id)) {
+        return res.status(401).json({ status: 'error', message: 'Permission denied.' })
+      }
+
+      //check this user is or not in db
+      const user = await User.findByPk(id)
+      if (!user) {
+        return res.status(404).json({ status: 'error', message: 'Cannot find this user in db.' })
+      }
+
+      let message = []
+      
+      // check all inputs are required
+      if (!account || !name || !email || !password || !checkPassword) {
+        message.push('All fields are required！')
+      }
+      // check account length and type
+      if (name && !validator.isByteLength(name, { min: 0, max: 50 })) {
+        message.push('Name can not be longer than 50 characters.')
+      }
+      // check name length and type
+      if (account && !validator.isByteLength(account, { min: 0, max: 20 })) {
+        message.push('Account can not be longer than 20 characters.')
+      }
+      // check email validation
+      if (email && !validator.isEmail(email)) {
+        message.push(`${email} is not a valid email address.`)
+      }
+      // check password length and type
+      if (password && !validator.isByteLength(password, { min: 5, max: 15 })) {
+        message.push('Password does not meet the required length.')
+      }
+      // check password and checkPassword
+      if (password && (password !== checkPassword)) {
+        message.push('The password and confirmation do not match.Please retype them.')
+      }
+      if (email !== currentEmail ) {
+        const userEmail = await User.findOne({ where: { email } })
+        if (userEmail) {
+          message.push('This email address is already being used.')
+        }  
+      }
+      if ( account !== currentAccount) {
+        const  userAccount = await  User.findOne({ where: { account } })
+        if (userAccount) {
+          message.push('This account is already being used.')
+        }
+      }
+      if (message.length) {
+        return res.status(400).json({ status: 'error', message })
+      }
+
+      await user.update({ name, password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)), email, account })
+      return res.status(200).json({ status: 'success', message: `@${account} Update account information successfully.` })
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ status: 'error', message: 'error' })
+    }
+  }
 }
 
 module.exports = userController
