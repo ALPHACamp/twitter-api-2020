a6643981c5778f5ca05815e78d38338d7b540299
bradychen <bradychen2@gmail.com>
2021-07-08 21:55:02 +0800

    fix: Followship model force setting id

--- models/followship.js
+++ models/followship.js
@@ -1,6 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Followship = sequelize.define('Followship', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     followerId: {
       type: DataTypes.INTEGER
     },
