2333040fcb7abef42ad933fe10e5f9a55c582114
Jennesy <jennesy.dai@gmail.com>
2021-07-08 11:31:36 +0800

    fix: fix merge conflicts

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,23 +4,22 @@ const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 const imageFields = [{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]
 const userController = require('../../controllers/api/userController')
-
-
-
-
+const likeController = require('../../controllers/api/likeController')
 const followController = require('../../controllers/api/followController')
+const tweetController = require('../../controllers/api/tweetController')
+const replyController = require('../../controllers/api/replyController')
 
 router.get('/', userController.getUsers)
 router.post('/', userController.postUser)
 router.get('/:id', userController.getUser)
 router.put('/:id', upload.fields(imageFields), userController.putUser)
 router.get('/:id/followings', followController.getUserFollowings)
+router.get('/:id/followers', followController.getUserFollowers)
 router.get('/:id/tweets', tweetController.getUserTweets)
 router.get('/:id/replied_tweets', replyController.getRepliedTweets)
-
-
 router.get('/:id/likes', likeController.getUserLikes)
 
-router.get('/:id/followers', followController.getUserFollowers)
+
+
 
 module.exports = router
