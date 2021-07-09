e5c1ba8281b93d0adffd517b822eecc272795957
tim80411 <tim80411@gmail.com>
2021-07-09 22:46:17 +0800

    feat: add post_unlike API
    ## fix post_like's likeCount increment

--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -5,6 +5,7 @@ const tweetController = require('../../controllers/tweetController')
 
 router.post('/:id/replies', tweetController.postReply)
 router.post('/:id/like', tweetController.postLike)
+router.post('/:id/unlike', tweetController.postUnlike)
 router.get('/:id', tweetController.getSingleTweet)
 router.post('/', tweetController.postTweet)
 router.get('/', tweetController.getTweets)
