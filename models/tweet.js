bb2c336cf07a297e5f88171c10f8fc602a30c060
Charlie <wl00606352@gmail.com>
2021-07-10 21:12:28 +0800

    feat: tweet model ouput 1/0 as true/false

--- models/tweet.js
+++ models/tweet.js
@@ -3,6 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
     UserId: DataTypes.INTEGER,
     description: DataTypes.STRING,
+    isLike: {
+      type: DataTypes.VIRTUAL,
+      get: function () {
+        return !!this.getDataValue('isLike')
+      }
+    }
   }, {})
   Tweet.associate = function (models) {
     Tweet.hasMany(models.Like)
