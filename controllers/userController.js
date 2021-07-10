010c26f1870363d583b7ea5a774b504b5136a652
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-10 14:50:02 +0800

    refactor: editAccount

--- controllers/userController.js
+++ controllers/userController.js
@@ -131,46 +131,18 @@ const userController = {
     try {
       const { account, name, email, password, checkPassword } = req.body
       const { email: currentEmail, account: currentAccount } = req.user
-
       const id = req.params.id
-
       // only user himself allow to edit account
       if (req.user.id !== Number(id)) {
         return res.status(401).json({ status: 'error', message: 'Permission denied.' })
       }
-
       // check this user is or not in db
       const user = await User.findByPk(id)
       if (!user) {
         return res.status(404).json({ status: 'error', message: 'Cannot find this user in db.' })
       }
-
       const message = []
-
-      // check all inputs are required
-      if (!account || !name || !email || !password || !checkPassword) {
-        message.push('All fields are required！')
-      }
-      // check account length and type
-      if (name && !validator.isByteLength(name, { min: 0, max: 50 })) {
-        message.push('Name can not be longer than 50 characters.')
-      }
-      // check name length and type
-      if (account && !validator.isByteLength(account, { min: 0, max: 20 })) {
-        message.push('Account can not be longer than 20 characters.')
-      }
-      // check email validation
-      if (email && !validator.isEmail(email)) {
-        message.push(`${email} is not a valid email address.`)
-      }
-      // check password length and type
-      if (password && !validator.isByteLength(password, { min: 5, max: 15 })) {
-        message.push('Password does not meet the required length.')
-      }
-      // check password and checkPassword
-      if (password && (password !== checkPassword)) {
-        message.push('The password and confirmation do not match.Please retype them.')
-      }
+      formValidation(account, name, email, password, checkPassword)
       if (email !== currentEmail) {
         const userEmail = await User.findOne({ where: { email } })
         if (userEmail) {
@@ -186,7 +158,6 @@ const userController = {
       if (message.length) {
         return res.status(400).json({ status: 'error', message })
       }
-
       await user.update({ name, password: bcrypt.hashSync(password, bcrypt.genSaltSync(10)), email, account })
       return res.status(200).json({ status: 'success', message: `@${account} Update account information successfully.` })
     } catch (err) {
