fd74f7668b4827d3c47d75567e1d264fb4f68080
Chris Chen <ok3201123@gmail.com>
2021-07-10 10:19:27 +0800

    fix: remove unnecessary codes and unify time and message format

--- routes/apis.js
+++ routes/apis.js
@@ -19,7 +19,7 @@ const authenticated = (req, res, next) => {
     if (err) { return next(err); }
     if (!user) {
       if (info.name === "TokenExpiredError") {
-        return res.status(401).json({ message: "Your token has expired." });
+        return res.status(401).json({ message: 'Token已過期！' });
       } else {
         return res.status(401).json({ message: info.message });
       }
