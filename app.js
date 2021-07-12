45765fe3e446e8975dcae4cfdbd0741c2ef52d30
StyleLinz_Man <cwchuang1997@gmail.com>
2021-07-12 22:38:30 +0800

    feat: install socket.io

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express')
-
+const http = require('http')
+const { Server } = require('socket.io')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CORS_WHITE_LIST.split(','),
+    methods: ['GET', 'POST']
+  }
+})
 
 const passport = require('./config/passport')
 const cors = require('./config/cors')
 
 const routes = require('./routes')
 
-const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
@@ -19,6 +28,20 @@ app.use(passport.initialize())
 
 app.use(routes)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+io.on('connection', (socket) => {
+  console.log('A user connected')
+  console.log(io.engine.clientsCount)
+  io.emit('connection', io.engine.clientsCount)
+  socket.broadcast.emit('announce', `User ${socket.id} joined the public room.`, io.engine.clientsCount)
+  socket.on('chat message', (message) => {
+    message = `${socket.id}: ${message}`
+    io.emit('chat message', message)
+  })
+  socket.on('disconnect', (reason) => {
+    console.log(`${socket.id} is leaving.`)
+  })
+})
 
 module.exports = app
