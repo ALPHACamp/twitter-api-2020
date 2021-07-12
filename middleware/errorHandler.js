727bf4bfebd931f8d206ac9b06026536515abbfa
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-12 23:01:08 +0800

    feat: add error handling

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,26 +1,10 @@
-function logError (err) {
-  console.error(err)
-}
-
-function logErrorMiddleware (err, req, res, next) {
-  logError(err)
-  next(err)
-}
-
 function returnError (err, req, res, next) {
-  res.status(err.statusCode || 500).send(err.message)
-}
-
-function isOperationalError (error) {
-  if (error instanceof BaseError) {
-    return error.isOperational
-  }
-  return false
+  res.status(err.statusCode || 500).json({
+    status: 'error',
+    message: err
+  })
 }
 
 module.exports = {
-  logError,
-  logErrorMiddleware,
-  returnError,
-  isOperationalError
+  returnError
 }
