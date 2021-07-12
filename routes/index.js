b423ef22336abc298bde982bcd12ef2445b323d5
Yiting Liao <tim80411@gmail.com>
2021-07-12 11:31:34 +0800

    feat: add roleCheck function

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-const { authenticated } = require('../middlewares/auth')
+const { authenticated, checkRole } = require('../middlewares/auth')
 
 const admin = require('./modules/admin')
 const followships = require('./modules/followships')
 const tweets = require('./modules/tweets')
 const users = require('./modules/users')
 
-router.use('/api/admin', admin)
+router.use('/api/admin', checkRole('Admin'), admin)
 router.use('/api/followships', authenticated, followships)
 router.use('/api/tweets', authenticated, tweets)
 router.use('/api/users', users)
