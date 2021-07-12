f6aec54425dda8ae0a5d398f4a0448886104946e
Ashley <ahappyday4all@gmail.com>
2021-07-12 18:33:54 +0800

    feat: add bodyParser.json() to get the data from front end

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express()
 const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use((req, res, next) => {
   res.locals.user = helpers.getUser(req)
   next()
