da3aa795c80d9c3b52b6ef3e1da1b257af5a3d59
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-13 16:40:24 +0800

    feat: add socket.js

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
 const bodyParser = require('body-parser')
+require('./models')
+const cors = require('cors')
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json') // 剛剛輸出的 JSON
+
 const app = express()
+const server = http.createServer(app)
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const port = process.env.PORT || 3000
-const cors = require('cors')
-require('./models')
-const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger_output.json') // 剛剛輸出的 JSON
-
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use( express.static(__dirname ,'public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/upload', express.static(__dirname + '/upload'))
 app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 require('./routes')(app)
+require('./config/socket').socket(server)
+
+server.listen(port, () => console.log(`Example server listening on port http://localhost:${port}`))
 
 app.use((err, req, res, next) => {
   console.error(err)
@@ -28,7 +34,5 @@ app.use((err, req, res, next) => {
     message: err
   })
 })
-
-app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
 
 module.exports = app
