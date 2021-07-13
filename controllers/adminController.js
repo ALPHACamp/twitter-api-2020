63e6e248b01cf3320533510a49273cc8469f5519
Yiting Liao <tim80411@gmail.com>
2021-07-13 10:59:24 +0800

    feat: add API of deleting Tweet

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,53 @@
-// const db = require('../models')
+const db = require('../models')
+const { User, Tweet, Sequelize } = db
+const { Op } = require('sequelize')
 
 const adminController = {
-  // for testing: test if authenticated & authenticatedAdmin works
+
   getUsers: (req, res) => {
-    return res.json({ status: 'success', message: 'getUsers' })
+    return User.findAll({
+      where: {
+        [Op.or]: [
+          { role: { [Op.ne]: 'admin' } },
+          { role: { [Op.is]: null } }
+        ]
+      },
+      attributes: [
+        'id', 'account', 'name', 'avatar', 'cover', 'followerCount', 'followingCount',
+        [Sequelize.fn('COUNT', Sequelize.col('*')), 'tweetCount'],
+        [Sequelize.fn('SUM', Sequelize.col('Tweets.likeCount')), 'likeCount']
+      ],
+      include: [
+        {
+          model: Tweet, attributes: []
+        }
+      ],
+      group: 'id',
+    }).then(users => {
+      res.status(200).json(users)
+    })
+  },
+
+  deleteTweet: (req, res) => {
+    const tweetId = req.params.id
+
+    return Tweet.findByPk(tweetId)
+      .then(tweet => {
+        if (!tweet) {
+          return res.status(400).json({
+            status: 'error',
+            message: 'Tweet does not exist'
+          })
+        }
+
+        return tweet.destroy()
+          .then(tweetDeleted => {
+            return res.status(200).json({
+              status: 'success',
+              message: `Tweet.id ${tweetDeleted.id} has been destroyed successfully`
+            })
+          })
+      })
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,19 +1,9 @@
 const express = require('express')
-const passport = require('passport')
 const router = express.Router()
-const authenticated = passport.authenticate('jwt', { session: false })
+
 const adminController = require('../../controllers/adminController')
 
-const authenticatedAdmin = (req, res, next) => {
-  if (req.user) {
-    if (req.user.role === 'admin') { return next() }
-    return res.json({ status: 'error', message: 'permission denied' })
-  } else {
-    return res.json({ status: 'error', message: 'permission denied' })
-  }
-}
-
-// for testing: test if authenticated & authenticatedAdmin works
-router.get('/users', authenticated, authenticatedAdmin, adminController.getUsers)
+router.get('/users', adminController.getUsers)
+router.delete('/tweets/:id', adminController.deleteTweet)
 
 module.exports = router
