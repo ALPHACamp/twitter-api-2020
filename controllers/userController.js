4c657b6e1b46b50c8a715919e752c63a15f9bcbc
Yiting Liao <tim80411@gmail.com>
2021-07-13 22:56:22 +0800

    feat: modify giving data structure after login

--- controllers/userController.js
+++ controllers/userController.js
@@ -86,22 +86,23 @@ const userController = {
     const account = req.body.account
     const password = req.body.password
 
-    User.findOne({ where: { account: account } }).then(user => {
-      if (!user) return res.status(401).json({ status: 'error', message: 'no such user found' })
-      if (!bcrypt.compareSync(password, user.password)) {
-        return res.status(401).json({ status: 'error', message: 'passwords did not match' })
-      }
-      const payload = { id: user.id, role: user.role }
-      const token = jwt.sign(payload, process.env.JWT_SECRET)
-      return res.json({
-        status: 'success',
-        message: 'ok',
-        token: token,
-        user: {
-          id: user.id, name: user.name, email: user.email, role: user.role
+    User.findOne({ where: { account: account } })
+      .then(user => {
+        if (!user) return res.status(401).json({ status: 'error', message: 'no such user found' })
+        if (!bcrypt.compareSync(password, user.password)) {
+          return res.status(401).json({ status: 'error', message: 'passwords did not match' })
         }
+        const payload = { id: user.id, role: user.role }
+        const token = jwt.sign(payload, process.env.JWT_SECRET)
+        return res.json({
+          status: 'success',
+          message: 'ok',
+          token: token,
+          user: {
+            id: user.id, name: user.name, email: user.email, isAdmin: Boolean(user.role === 'admin')
+          }
+        })
       })
-    })
   },
   getUser: (req, res) => {
     return User.findByPk(req.params.id)
