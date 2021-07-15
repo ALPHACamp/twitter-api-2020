e292291c1a0a2a3d3a0f375c86e95528a09d5bdd
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-15 16:08:17 +0800

    feat: bulid io connection

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const bodyParser = require('body-parser')
 require('./models')
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
-const swaggerFile = require('./swagger_output.json') // 剛剛輸出的 JSON
+const swaggerFile = require('./swagger_output.json')
 
 const app = express()
 const server = http.createServer(app)
+const socket = require('socket.io')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -23,7 +24,21 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/upload', express.static(__dirname + '/upload'))
 app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 require('./routes')(app)
-require('./config/socket').socket(server)
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true
+  }
+})
+io.on('connection', async socket => {
+  console.log('connection')
+  socket.on('disconnect', () => {
+    console.log('disconnect')
+  })
+})
 
 server.listen(port, () => console.log(`Example server listening on port http://localhost:${port}`))
 
---
+++ public/javascripts/socket.js
@@ -0,0 +1,4 @@
+const socket = (server) => {
+
+}
+module.exports = socket
