9931f443f1e30bdbae7e3d22272fd0ef4b8e2319
Charlie <wl00606352@gmail.com>
2021-07-16 16:50:48 +0800

    feat: router

--- routes/apis/chatRoute.js
+++ routes/apis/chatRoute.js
@@ -3,4 +3,8 @@ const router = express.Router()
 
 const chatController = require('../../controllers/chatController')
 
+router.get('/')
+router.get('/:room')
+router.post('/')
+
 module.exports = router
