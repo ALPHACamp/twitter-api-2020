ebcd81dbbd89e39f02240a3601f8df5930574a76
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-16 15:08:25 +0800

    fix:modify seeder data

--- seeders/20210705130559-reply-seed-file.js
+++ seeders/20210705130559-reply-seed-file.js
@@ -6,9 +6,9 @@ const functions = require('../config/functions')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Replies',
-      Array.from({ length: 330 }).map((item, index) => ({
+      Array.from({ length: 300 }).map((item, index) => ({
         id: index + 1,
-        UserId: Math.ceil(Math.random() * 11),
+        UserId: Math.ceil(Math.random() * 9) + 1,
         TweetId: Math.ceil((index + 1) / 3),
         comment: faker.lorem.text().substring(0, 50),
         createdAt: functions.randomDate(new Date(2021, 0, 1), new Date()),
