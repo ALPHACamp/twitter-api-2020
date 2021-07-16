91827d163e7dcc9592bb484f7056c003bffe6607
Hsinyehh <rita.yeh@0617gmail.com>
2021-07-16 18:37:44 +0800

    feat: add messages seed data

--- seeders/20210716102237-messages-ssed-file.js
+++ seeders/20210716102237-messages-ssed-file.js
@@ -1,26 +1,21 @@
 'use strict';
+const faker = require('faker')
+const functions = require('../config/functions')
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Messages',
+      Array.from({ length: 5 }).map((item, index) => ({
+        id: index + 1,
+        content: faker.lorem.text().substring(0, 30),
+        UserId: Math.ceil(Math.random() * 5),
+        createdAt: functions.randomDate(new Date(2021, 0, 1), new Date()),
+        updatedAt: functions.randomDate(new Date(2021, 0, 1), new Date())
+      })
+      ), {})
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Messages', null, {})
   }
 };
