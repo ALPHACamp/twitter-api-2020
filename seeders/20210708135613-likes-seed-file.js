b451144dc56ec497907999c2ecd7bbc8d9cf70c8
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-16 14:40:56 +0800

    feat: modify like seeder

--- seeders/20210708135613-likes-seed-file.js
+++ seeders/20210708135613-likes-seed-file.js
@@ -6,8 +6,8 @@ module.exports = {
     await queryInterface.bulkInsert('Likes',
       Array.from({ length: 25 }).map((item, index) => ({
         id: index + 1,
-        UserId: Math.ceil(Math.random() * 5),
-        TweetId: Math.ceil(Math.random() * 50),
+        UserId: Math.ceil(Math.random() * 11),
+        TweetId: Math.ceil(Math.random() * 110),
         createdAt: functions.randomDate(new Date(2021, 0, 1), new Date()),
         updatedAt: functions.randomDate(new Date(2021, 0, 1), new Date())
       })
