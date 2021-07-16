e081e2a0428439fdf4024748b5048024be6402e8
Yiting Liao <tim80411@gmail.com>
2021-07-16 20:47:44 +0800

    feat: add socket.io config

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 module.exports = (server) => {
-  const io = require('socket.io')(server)
+  const io = require('socket.io')(server, {
+    cors: {
+      origin: ['http://localhost:8080', 'https://marcolin1.github.io/'],
+      methods: ['GET', 'POST']
+    }
+  })
 
   io.on('connection', socket => {
     console.log('A user connecting')
+    console.log(socket.handshake.headers.host)
+    console.log(socket.handshake.url)
 
     // 可以在伺服器端顯示通道過來的所有事件，以及相關的參數
     socket.onAny((event, ...args) => {
