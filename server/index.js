4e1ba77966586cb9669765c5024607d0a9283d84
Yiting Liao <tim80411@gmail.com>
2021-07-16 20:48:32 +0800

    feat: error handle

--- server/index.js
+++ server/index.js
@@ -28,6 +28,13 @@ module.exports = (server) => {
 
   })
 
+  io.engine.on('connection_error', (err) => {
+    console.log(err.req)     // the request object
+    console.log(err.code)    // the error code, for example 1
+    console.log(err.message)  // the error message, for example "Session ID unknown"
+    console.log(err.context)  // some additional error context
+  })
+
 
 
 }
