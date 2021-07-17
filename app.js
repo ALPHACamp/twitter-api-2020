d02f5d709ccb286181618b6895151b9015e3a776
Jennesy <jennesy.dai@gmail.com>
2021-07-17 20:25:46 +0800

    feat: socket settings

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const session = require('express-session')
@@ -6,7 +9,6 @@ const helpers = require('./_helpers')
 if (process.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const app = express()
 const port = process.env.PORT
 
 //bodyparse set
@@ -20,8 +22,9 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(cors())
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
 
 require('./routes/index')(app)
+require('./socket/socket')(server)
