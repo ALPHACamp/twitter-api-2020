4a3edfd6b60cd6440cad44d97ebaefef3cca6c1f
Jennesy <jennesy.dai@gmail.com>
2021-07-17 23:02:03 +0800

    cancel socketController

--- socket/socket.js
+++ socket/socket.js
@@ -1,34 +1,66 @@
 const sockets = []
 const userSockets = {}
 const socketController = require('../controllers/socket/socketController')
+const socketio = require('socket.io')
+const db = require('../models')
+const Message = db.Message
+const User = db.User
+
 
 module.exports = (server) => {
-  const io = require('socket.io')(server)
-
-
+  const io = socketio(server)
   io.on('connection', (socket) => {
     /* connect */
+    sockets.push(socket)
+    userSockets[socket.id] = 1
     console.log(`User is online: ${socket.id}`)
     socket.emit('message', `Your socket id is  ${socket.id}`)
     socket.on('sendMessage', (data) => console.log(data))
     /* disconnect */
     socket.on('disconnect', () => {
+      delete userSockets[socket.id]
+      sockets.splice(sockets.indexOf(socket), 1)
       console.log(`User is offline: ${socket.id}`)
     })
 
     /* join public room */
-    socket.on('join-public-room',
-      io.emit('new-join', socketController.joinPublicRoom)
-    )
+    socket.on('join_public_room', async ({ userId }) => {
+      const user = await User.findByPk(userId)
+      io.emit('new_join', {
+        name: user.name
+      })
+    })
 
     /* get public history */
-    socket.on('get-public-history', socketController.getPublicHistory)
+    socket.on('get_public_history', async (offset, limit, cb) => {
+      const message = await Message.findAll({
+        offset,
+        limit,
+        order: [['createdAt', 'desc']],
+        include: [
+          {
+            model: User,
+            attributes: ['avatar'],
+            as: 'User'
+          }
+        ]
+      })
+      cb(message)
+    })
 
     /* public message */
-    socket.on('post-public-msg',
-      socket.broadcast.emit('get-public-msg',
-        socketController.postPublicMsg)
-    )
+    socket.on('post_public_msg', async ({ msg, userId }) => {
+      const message = await Message.create({
+        RoomId: 1,
+        UserId: userId,
+        content: msg
+      })
+      const user = await User.findByPk(userId)
+      socket.broadcast.emit('get_public_msg', {
+        msg: message.content,
+        createdAt: message.createdAt,
+        avatar: user.avatar
+      })
+    })
   })
-
 }
