7c627ca57c02b6fac82923c9a5d8469eee9c5b7b
Jennesy <jennesy.dai@gmail.com>
2021-07-18 12:46:46 +0800

    feat: add online users at public room

--- socket/socket.js
+++ socket/socket.js
@@ -1,5 +1,6 @@
-const sockets = []
-const userSockets = {}
+const sockets = [] // array of sockets
+const socketUsers = {} // key(userId) to value(socketId, name, account, avatar)
+const publicRoomUsers = [] // array of userIds
 const socketio = require('socket.io')
 const db = require('../models')
 const Message = db.Message
@@ -11,49 +12,58 @@ module.exports = (server) => {
   const io = socketio(server)
   const wrap = (middleware) => (socket, next) =>
     middleware(socket.request, {}, next)
+
   async function onlineUsers() {
-    return await User.findAll({
-      where: {
-        id: {
-          [Op.or]: Object.keys(userSockets).map(x => +x)
-        }
-      },
-      attributes: [id, name, account, avatar]
+    return publicRoomUsers.map(id => {
+      return {
+        id,
+        name: socketUsers[id].name,
+        account: socketUsers[id].account,
+        avatar: socketUsers[id].avatar
+      }
     })
   }
 
   io.use(wrap(authenticatedSocket)).on('connection', (socket) => {
-    // console.log(socket.request.user)
     const currentUser = socket.request.user
     /* connect */
     sockets.push(socket)
-    userSockets[currentUser.id] = socket.id
+    socketUsers[currentUser.id] = {
+      socketId: currentUser.socketId,
+      name: currentUser.name,
+      account: currentUser.account,
+      avatar: currentUser.avatar
+    }
     console.log(`User is online: ${socket.id}`)
     socket.emit('message', `Your socket id is  ${socket.id}`)
-    io.emit('online_users', { users: onlineUsers() })
     socket.on('sendMessage', (data) => console.log(data))
 
     /* disconnect */
     socket.on('disconnect', () => {
-      delete userSockets[currentUser.id]
+      delete socketUsers[currentUser.id]
       sockets.splice(sockets.indexOf(socket), 1)
       console.log(`User is offline: ${socket.id}`)
-      io.emit('online_users', { users: onlineUsers() })
     })
 
     /* join public room */
     socket.on('join_public_room', async ({ userId }) => {
-      const user = await User.findByPk(userId)
+      publicRoomUsers.push(userId)
+      const user = socketUsers[userId]
       io.emit('new_join', {
         name: user.name
+      })
+      io.emit('online_users', {
+        users: onlineUsers()
       })
     })
     /* leave public room */
     socket.on('leave_public_room', async ({ userId }) => {
-      const user = await User.findByPk(userId)
+      publicRoomUser.splice(publicRoomUser.indexOf(userId), 1)
+      const user = socketUsers[userId]
       io.emit('user_leave', {
         name: user.name
       })
+      io.emit('online_users', { users: onlineUsers() })
     })
 
     /* get public history */
@@ -80,7 +90,7 @@ module.exports = (server) => {
         UserId: userId,
         content: msg
       })
-      const user = await User.findByPk(userId)
+      const user = socketUsers[userId]
       socket.broadcast.emit('get_public_msg', {
         msg: message.content,
         createdAt: message.createdAt,
@@ -109,12 +119,12 @@ module.exports = (server) => {
       //return roomId to client
       console.log('roomId', roomId)
       // check isOnline or not
-      if (userSockets[User2Id]) {
+      if (socketUsers[User2Id]) {
         //join User1 into room
         socket.join(roomId)
         //join User2 into room
         user2Socket = sockets.find(
-          (socket) => socket.id === userSockets[User2Id]
+          (socket) => socket.id === socketUsers[User2Id]
         )
         user2Socket.join(roomId)
       }
@@ -122,16 +132,8 @@ module.exports = (server) => {
     })
     //listen privacy msg and send
     socket.on('post_private_msg', async ({ UserId, RoomId, content }) => {
-      // console.log('=========')
-      // console.log({ UserId, RoomId, content })
-      // console.log('=========')
-
-      const user = await User.findByPk(+UserId)
+      const user = socketUsers[userId]
       const message = await Message.create({ UserId, RoomId, content })
-      // console.log(user.toJSON())
-      // console.log(message.toJSON())
-      // const user = array[0].toJSON()
-      //  = array[1].toJSON()
       let createdAt = message.createdAt
       const avatar = user.avatar
       socket
