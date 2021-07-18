71b1bef46a87bb363317b125da4f47d2956a5f85
Ashley <bornonefreekiss@alum.ccu.edu.tw>
2021-07-18 18:00:08 +0800

    refactor: add socketServer

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
-// add Model
-const db = require('./models')
-const { User, Message } = db
+
+require('./models')
 
 const app = express()
 const server = http.createServer(app)
-const jwt = require('jsonwebtoken')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -37,98 +35,8 @@ const io = require('socket.io')(server, {
   },
   allowEIO3: true
 })
-const activeUsers = []
-let activeUsersCount = 0
-io.use(async (socket, next) => {
-  const token = socket.handshake.query.token
-  if (!token) return
-  if (socket.handshake.query && token) {
-    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
-      try {
-        if (err) return next(new Error('Authentication error'))
-        socket.decoded = decoded
-        socket.userId = decoded.id
-        next()
-      } catch (err) {
-        console.log(err)
-      }
-    })
-    next()
-  } else {
-    next(new Error('Authentication error'))
-  }
-}).on('connection', async socket => {
-  try {
-    console.log('connection', socket.userId)
-    const user = await User.findByPk(socket.userId, {
-      attributes: ['id', 'name', 'account', 'avatar', 'role']
-    })
 
-    if (user) {
-      socket.userId = user.dataValues.id
-      socket.user = user.dataValues
-      socket.user.socketId = socket.id
-      console.log('socket.userId', user.dataValues.id)
-      console.log('socket.user', user.dataValues)
-      console.log('socket.user.socketId', socket.id)
-    }
-    console.log('online user', socket.user)
-    // 線上使用者列表加入新使用者的資料
-    const onlineUser = socket.user
-    if (activeUsers.map(u => u.id).includes(user.id)) {
-      console.log('This user already existed.')
-      activeUsersCount = activeUsers.length
-    } else {
-      activeUsers.push(onlineUser)
-      activeUsersCount++
-      console.log('activeUsersCount', activeUsersCount)
-    }
-    console.log('activeUsers', activeUsers)
-    // 發送線上使用者列表//發送上線人數
-    io.emit('activeUsers', activeUsersCount, activeUsers)
-    // 向聊天室廣播新的使用者上線
-    const data = { online: onlineUser }
-    io.emit('notification', data)
-
-    socket.on('disconnect', async () => {
-      // emit使用者離線通知
-      if (!socket.user) { return }
-      console.log('disconnect', socket.user)
-      const offlineUser = socket.user
-      // 線上使用者列表移除離線使用者資料
-      const activeUsersIndex = activeUsers.map(u => u.id).indexOf(offlineUser.id)
-      activeUsers.splice(activeUsersIndex, 1)
-      console.log(activeUsers)
-      activeUsersCount = activeUsers.length
-      console.log(activeUsersCount)
-      // 聊天室通知該名使用者離開聊天
-
-      io.emit('notification', offlineUser)
-      // 發送線上使用者列表
-      io.emit('activeUsers', activeUsers, activeUsersCount)
-    })
-    // api發送歷史訊息(avatar id account name messages)
-    // on監聽使用者發送的訊息//儲存訊息到db//emit發送使用者的訊息到聊天室
-    socket.on('sendMessage', async (data) => {
-      console.log('sendMessage socket.user', socket.user)
-      try {
-        if (data) {
-          const createdMessage = await Message.create({
-            content: data,
-            UserId: socket.userId,
-            createdAt: Date.now()
-          })
-          // 傳送使用者和訊息
-          console.log('message: ', createdMessage.toJSON())
-          console.log('message content: ', data)
-          io.emit('newMessage', { message: createdMessage.toJSON(), user: socket.user })
-        }
-      } catch (err) { console.log(err) }
-    })
-  } catch (err) {
-    console.log(err)
-  }
-})
+require('./config/socketServer')(io)
 
 server.listen(port, () => console.log(`Example server listening on port http://localhost:${port}`))
 
---
+++ config/socketServer.js
@@ -0,0 +1,98 @@
+const db = require('../models')
+const jwt = require('jsonwebtoken')
+const { Message, User } = db
+const activeUsers = []
+let activeUsersCount = 0
+
+module.exports = (io) => {
+  io.use(async (socket, next) => {
+    const token = socket.handshake.query.token
+    if (!token) return
+    if (socket.handshake.query && token) {
+      jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        try {
+          if (err) return next(new Error('Authentication error'))
+          socket.decoded = decoded
+          socket.userId = decoded.id
+          next()
+        } catch (err) {
+          console.log(err)
+        }
+      })
+      next()
+    } else {
+      next(new Error('Authentication error'))
+    }
+  }).on('connection', async socket => {
+    try {
+      console.log('connection', socket.userId)
+      const user = await User.findByPk(socket.userId, {
+        attributes: ['id', 'name', 'account', 'avatar', 'role']
+      })
+
+      if (user) {
+        socket.userId = user.dataValues.id
+        socket.user = user.dataValues
+        socket.user.socketId = socket.id
+        console.log('socket.userId', user.dataValues.id)
+        console.log('socket.user', user.dataValues)
+        console.log('socket.user.socketId', socket.id)
+      }
+      console.log('online user', socket.user)
+      // 線上使用者列表加入新使用者的資料
+      const onlineUser = socket.user
+      if (activeUsers.map(u => u.id).includes(user.id)) {
+        console.log('This user already existed.')
+        activeUsersCount = activeUsers.length
+      } else {
+        activeUsers.push(onlineUser)
+        activeUsersCount++
+        console.log('activeUsersCount', activeUsersCount)
+      }
+      console.log('activeUsers', activeUsers)
+      // 發送線上使用者列表//發送上線人數
+      io.emit('activeUsers', activeUsersCount, activeUsers)
+      // 向聊天室廣播新的使用者上線
+      const data = { online: onlineUser }
+      io.emit('notification', data)
+
+      socket.on('disconnect', async () => {
+      // emit使用者離線通知
+        if (!socket.user) { return }
+        console.log('disconnect', socket.user)
+        const offlineUser = socket.user
+        // 線上使用者列表移除離線使用者資料
+        const activeUsersIndex = activeUsers.map(u => u.id).indexOf(offlineUser.id)
+        activeUsers.splice(activeUsersIndex, 1)
+        console.log(activeUsers)
+        activeUsersCount = activeUsers.length
+        console.log(activeUsersCount)
+        // 聊天室通知該名使用者離開聊天
+
+        io.emit('notification', offlineUser)
+        // 發送線上使用者列表
+        io.emit('activeUsers', activeUsers, activeUsersCount)
+      })
+      // api發送歷史訊息(avatar id account name messages)
+      // on監聽使用者發送的訊息//儲存訊息到db//emit發送使用者的訊息到聊天室
+      socket.on('sendMessage', async (data) => {
+        console.log('sendMessage socket.user', socket.user)
+        try {
+          if (data) {
+            const createdMessage = await Message.create({
+              content: data,
+              UserId: socket.userId,
+              createdAt: Date.now()
+            })
+            // 傳送使用者和訊息
+            console.log('message: ', createdMessage.toJSON())
+            console.log('message content: ', data)
+            io.emit('newMessage', { message: createdMessage.toJSON(), user: socket.user })
+          }
+        } catch (err) { console.log(err) }
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  })
+}
