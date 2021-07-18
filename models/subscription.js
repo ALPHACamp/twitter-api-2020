0d5826019e3fa53d115f994eac00bab32f7f6856
Meg <ai285063@gmail.com>
2021-07-18 21:38:16 +0800

    feat: add isRead to subscriptions

--- models/subscription.js
+++ models/subscription.js
@@ -2,7 +2,8 @@
 module.exports = (sequelize, DataTypes) => {
   const Subscription = sequelize.define('Subscription', {
     UserId: DataTypes.INTEGER,
-    TweetId: DataTypes.INTEGER
+    TweetId: DataTypes.INTEGER,
+    isRead: DataTypes.BOOLEAN
   }, {})
   Subscription.associate = function (models) {
     Subscription.belongsTo(models.User)
