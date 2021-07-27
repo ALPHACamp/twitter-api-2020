00e2c51af67bb128947a025428c0ac0cf78be5d4
Meg <ai285063@gmail.com>
2021-07-27 12:26:10 +0800

    feat: recompose messageController

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -4,18 +4,18 @@ const messageController = {
   getMessages: async (req, res) => {
     try {
       const data = await messageService.getMessages(req.query)
-    
+
       return res.status(200).json(data)
     } catch (error) {
       return res.status(400).json({
         status: error.name,
         message: error.message
       })
-    }  
+    }
   },
 
   getChattedUsers: async (req, res) => {
-    const id = req.params.id
+    const id = Number(req.params.id)
 
     try {
       const data = await messageService.getChattedUsers(id)
@@ -26,7 +26,7 @@ const messageController = {
         status: error.name,
         message: error.message
       })
-    }  
+    }
   }
 }
 
