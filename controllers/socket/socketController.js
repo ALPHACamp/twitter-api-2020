cfffac3f43704cbd27e52fcca4a40f66c9bf37ab
Jennesy <jennesy.dai@gmail.com>
2021-08-02 20:42:04 +0800

    refactor: socketService private rooms

--- controllers/socket/socketController.js
+++ controllers/socket/socketController.js
@@ -34,8 +34,8 @@ let socketController = {
     socket.emit('message', `Your socket id is  ${socket.id}`)
   },
   deleteSocket: (socket, io) => {
-    if (privateRoomUsers[socket.id]) {
-      delete privateRoomUsers[socket.id]
+    if (socketService.getPrivateRoomData(socket.id)) {
+      socketService.deletePrivateUser(socket.id)
       console.log(notice(`leave Private Page: ${socketUsers[socket.id].id}`))
     }
     if (publicRoomUsers.includes(socket.id)) {
@@ -51,18 +51,6 @@ let socketController = {
     }
     delete socketUsers[socket.id]
     console.log(highlight(`User is offline: ${socket.id}`))
-  },
-  getMsgNotice: async (userId, socket) => {
-    if (socket) {
-      userId = socketUsers[socket.id].id
-    }
-    const { count } = await MessageRecord.findAndCountAll({
-      where: {
-        ReceiverId: userId,
-        isSeen: false
-      }
-    })
-    return count
   },
   joinPublicRoom: async (userId, socket, io) => {
     console.log(notice(`join_public_room: ${userId}`))
@@ -89,94 +77,38 @@ let socketController = {
   joinPrivatePage: async function (userId, socket) {
     console.log(notice(`join_private_page: ${userId}`))
     console.log(notice(`join_private_page-socketId ${socket.id}`))
-    const userInfo = {
-      id: userId,
-      currentRoom: null
-    }
-    //加入privateRoomUsers
-    privateRoomUsers[socket.id] = userInfo
-    //更新isSeen為true
-    const MsgRecordOption = {
-      where: {
-        ReceiverId: userId,
-        isSeen: false
-      },
-      attributes: ['id']
-    }
-    await MessageRecord.findAll(MsgRecordOption).then((records) => {
-      records = records.map(record => record.id)
-      MessageRecord.update({ isSeen: true }, { where: { id: records } })
-    })
+    socketService.addPrivateUser(userId, socket)
+    await socketService.toggleSeenMsgRecord(userId)
     //emit get_private_rooms
-    const rooms = await this.getPrivateRooms
+    const rooms = await socketService.getPrivateRooms(userId)
     socket.emit('get_private_rooms', rooms)
-    const getMsgNoticeDetails = await helper.getMsgNoticeDetails(userId)
+    const getMsgNoticeDetails = await socketService.getMsgNoticeDetails(userId)
     socket.emit('get_msg_notice_details', getMsgNoticeDetails)
   },
   joinPrivateRoom: async (User1Id, User2Id, socket, io) => {
     console.log(notice(`join_private_room:`), { User1Id, User2Id })
     /* if miss join_private_page */
-    if (!privateRoomUsers[socket.id]) {
+    if (!socketService.getPrivateRoomData(socket.id)) {
       console.log(notice(`[補] join_private_page: ${userId}`))
       console.log(notice(`join_private_page-socketId ${socket.id}`))
-      const userInfo = {
-        id: userId,
-        currentRoom: null
-      }
-      //加入privateRoomUsers
-      privateRoomUsers[socket.id] = userInfo
+      socketService.addPrivateUser(userId, socket.id)
     }
-    const roomOptions = {
-      where: {
-        $or: [
-          { User1Id, User2Id },
-          { User1Id: User2Id, User2Id: User1Id }
-        ]
-      }
-    }
-    const recordOptions = {
-      where: {
-        ReceiverId: User1Id,
-        SenderId: User2Id
-      }
-    }
-    const record = await MessageRecord.findOne(recordOptions)
-    if (record) {
-      await record.update({ unreadNum: 0 })
-    }
-    let roomId
-    const room = await Room.findOne(roomOptions)
-
-    if (room) {
-      console.log(detail("room已存在，roomId為:"), room.id)
-      roomId = room.id
-    } else {
-      roomId = await Room.create({ User1Id, User2Id })
-      console.log(detail("建立新的room，roomId為:"), room.toJSON())
-      roomId = roomId.toJSON().id
-    }
-    console.log(detail(`set new roomId to ${socket.id}`))
-    privateRoomUsers[socket.id].currentRoom = roomId
+    socketService.toggleSeenPrivateMsg(User1Id, User2Id)
+    const roomId = await socketService.getRoomId(User1Id, User2Id)
+    socketService.setPrivateRoomId(socket.id, roomId)
+    console.log(detail(`set ${socket.id} new roomId to ${roomId}`))
+    
     // 找到User2 的socketId
     // check isOnline or not
-    const isUserOnline = helper.isUserOnline(User2Id)
-    if (isUserOnline) {
+    const user2Sockets = socketService.checkUserOnline(User2Id)
+    if (user2Sockets) {
       //join User1 into room
       socket.join(roomId)
       //join User2 into room
       console.log(detail(`user2的socket:`), isUserOnline)
-      isUserOnline.forEach((socketId) => {
-        const targetSocket = sockets.find((element) => element.id === socketId)
-        console.log(targetSocket.id)
-        targetSocket.join(roomId)
-      })
+      socketService.joinRoom(user2Sockets, roomId)
     }
-    console.log(detail('all sockets [伺服器紀錄]'), '\n', sockets.map(item => item.id))
-    const ids = await io.allSockets()
-    console.log(detail('all sockets [系統偵測]'), '\n', Array.from(ids))
-    console.log(detail('all socketUsers [詳細資料]'), '\n', socketUsers)
-    console.log(detail('all publicRoomUsers '), '\n', publicRoomUsers)
-    console.log(detail('all privateRoomUsers '), '\n', privateRoomUsers)
+    socketService.showSocketDetails()
     console.log(detail(`最後roomId結果: ${roomId} `))
     return roomId
   },
@@ -195,9 +127,9 @@ let socketController = {
   },
   leavePrivatePage: (socket) => {
     //去除privateRoomUsers內要離開的使用者
-    if (privateRoomUsers[socket.id]) {
-      console.log(notice(`leave_private_page: `), privateRoomUsers[socket.id])
-      delete privateRoomUsers[socket.id]
+    if (socketService.getPrivateRoomData(socket.id)) {
+      console.log(notice(`leave_private_page: `), socketService.getPrivateRoomData(socket.id))
+      socketService.deletePrivateUser(socket.id)
     }
   },
   getPublicHistory: async (offset, limit) => {
@@ -227,88 +159,8 @@ let socketController = {
   },
   getPrivateHistory: async (offset, limit, RoomId) => {
     console.log(notice(`get_private_history:`), { offset, limit, RoomId })
-    const options = {
-      offset,
-      limit,
-      order: [['createdAt', 'desc']],
-      include: [
-        {
-          model: User,
-          attributes: ['avatar'],
-          as: 'User'
-        }
-      ],
-      where: {
-        RoomId
-      }
-    }
-    const messages = await Message.findAll(options)
-    messages.forEach((message) => {
-      message.dataValues.avatar = message.dataValues.User.avatar
-      delete message.dataValues.User
-    })
+    const messages = await socketService.getRoomHistory(offset, limit, RoomId)
     return messages
-  },
-  getPrivateRooms: async (userId) => {
-    const roomOption = {
-      where: {
-        [Op.or]: [{ User1Id: userId }, { User2Id: userId }],
-        [Op.and]: [
-          sequelize.literal(
-            'EXISTS (select createdAt from Messages where Messages.RoomId = Room.id LIMIT 1)'
-          )
-        ]
-      },
-      include: [
-        {
-          model: Message,
-          as: 'Messages',
-          limit: 1,
-          include: [
-            {
-              model: User,
-              as: 'User',
-              attributes: ['id'],
-            },
-          ],
-          order: [['createdAt', 'desc']]
-        },
-        {
-          model: User,
-          as: 'User1',
-          attributes: ['id', 'name', 'account', 'avatar']
-        },
-        {
-          model: User,
-          as: 'User2',
-          attributes: ['id', 'name', 'account', 'avatar']
-        }
-      ],
-      attributes: {
-        exclude: ['updatedAt', 'User1Id', 'User2Id', 'createdAt']
-      },
-      order: [[sequelize.literal(
-        '(select createdAt from Messages where Messages.RoomId = Room.id order by Messages.createdAt DESC LIMIT 1)'
-      ), 'DESC']],
-      limit: 5
-    }
-    const rooms = await Room.findAll(roomOption)
-      .then((rooms) => {
-        rooms.forEach((room) => {
-          const user = room.dataValues.User1.dataValues.id !== userId ? room.dataValues.User1.dataValues : room.dataValues.User2.dataValues
-          room.dataValues.lastMsg = {}
-          room.dataValues.lastMsg.fromRoomMember = room.dataValues.Messages[0].dataValues.User.id !== userId
-          room.dataValues.lastMsg.content = room.dataValues.Messages[0].dataValues.content
-          room.dataValues.lastMsg.createdAt = room.dataValues.Messages[0].dataValues.createdAt
-          room.dataValues.roomMember = user
-          delete room.dataValues.Messages
-          delete room.dataValues.User1
-          delete room.dataValues.User2
-          return room.dataValues
-        })
-        return rooms
-      })
-    return rooms
   },
   postPublicMsg: async (content, userId, socket) => {
     console.log(notice(`post_public_msg:`, { content, userId }))
@@ -333,9 +185,9 @@ let socketController = {
       return
     }
     const user = socketUsers[socket.id]
-    const message = await Message.create({ UserId: SenderId, RoomId, content })
-    const isUserOnline = helper.isUserOnline(ReceiverId)
-    const isReceiverOnPrivatePage = helper.isReceiverOnPrivatePage(ReceiverId)
+    const message = await socketService.addMessage(SenderId, RoomId, content)
+    const isUserOnline = socketService.checkUserOnline(ReceiverId)
+    const isReceiverOnPrivatePage = socketService.checkReceiverOnPrivatePage
     /* Receiver is in room */ //Receiver在聊天室裡
     if (isReceiverOnPrivatePage && isReceiverOnPrivatePage.includes(message.RoomId)) {
       let createdAt = message.createdAt
@@ -350,28 +202,17 @@ let socketController = {
     }
     /* Receiver is not in room */ //Receiver不在聊天室裡
     else {
-      let record = await MessageRecord.findOne({
-        where: {
-          RoomId: RoomId,
-          SenderId: SenderId
-        }
-      })
+      let record = await socketService.getMsgRecord(RoomId, SenderId)
 
       if (!record) {
-        record = await MessageRecord.create({
-          SenderId: SenderId,
-          ReceiverId: ReceiverId,
-          RoomId: RoomId,
-          isSeen: true,
-          unreadNum: 0
-        })
+        record = await socketService.getMsgRecord(RoomId, SenderId, ReceiverId)
       }
       /* Receiver is not on private page */
       if (!isReceiverOnPrivatePage) {
         record.isSeen = false
         /* Send Notice if Receiver is online */
         if (isUserOnline) {
-          const getMsgNotice = await this.getMsgNotice(ReceiverId, null)
+          const getMsgNotice = await socketService.getMsgNotice(ReceiverId, null)
           isUserOnline.forEach((socketid) => {
             socket.to(socketid).emit('get_msg_notice', getMsgNotice)
           })
@@ -379,7 +220,7 @@ let socketController = {
       }
       record.increment({ unreadNum: 1 })
       record.save()
-      const getMsgNoticeDetails = helper.getMsgNoticeDetails(ReceiverId)
+      const getMsgNoticeDetails = socketService.getMsgNoticeDetails(ReceiverId)
       if (isUserOnline) {
         isUserOnline.forEach((socketid) => {
           socket
--- service/socketService.js
+++ service/socketService.js
@@ -51,6 +51,209 @@ let socketService = {
     }
     return false
   },
+  addPrivateUser: (userId, socket) => {
+    const userInfo = {
+      id: userId,
+      currentRoom: null
+    }
+    //加入privateRoomUsers
+    privateRoomUsers[socket.id] = userInfo
+  },
+  deletePrivateUser: (socketId) => {
+    delete privateRoomUsers[socketId]
+  },
+  getPrivateRooms: async (userId) => {
+    const roomOption = {
+      where: {
+        [Op.or]: [{ User1Id: userId }, { User2Id: userId }],
+        [Op.and]: [
+          sequelize.literal(
+            'EXISTS (select createdAt from Messages where Messages.RoomId = Room.id LIMIT 1)'
+          )
+        ]
+      },
+      include: [
+        {
+          model: Message,
+          as: 'Messages',
+          limit: 1,
+          include: [
+            {
+              model: User,
+              as: 'User',
+              attributes: ['id'],
+            },
+          ],
+          order: [['createdAt', 'desc']]
+        },
+        {
+          model: User,
+          as: 'User1',
+          attributes: ['id', 'name', 'account', 'avatar']
+        },
+        {
+          model: User,
+          as: 'User2',
+          attributes: ['id', 'name', 'account', 'avatar']
+        }
+      ],
+      attributes: {
+        exclude: ['updatedAt', 'User1Id', 'User2Id', 'createdAt']
+      },
+      order: [[sequelize.literal(
+        '(select createdAt from Messages where Messages.RoomId = Room.id order by Messages.createdAt DESC LIMIT 1)'
+      ), 'DESC']],
+      limit: 5
+    }
+    const rooms = await Room.findAll(roomOption)
+      .then((rooms) => {
+        rooms.forEach((room) => {
+          const user = room.dataValues.User1.dataValues.id !== userId ? room.dataValues.User1.dataValues : room.dataValues.User2.dataValues
+          room.dataValues.lastMsg = {}
+          room.dataValues.lastMsg.fromRoomMember = room.dataValues.Messages[0].dataValues.User.id !== userId
+          room.dataValues.lastMsg.content = room.dataValues.Messages[0].dataValues.content
+          room.dataValues.lastMsg.createdAt = room.dataValues.Messages[0].dataValues.createdAt
+          room.dataValues.roomMember = user
+          delete room.dataValues.Messages
+          delete room.dataValues.User1
+          delete room.dataValues.User2
+          return room.dataValues
+        })
+        return rooms
+      })
+    return rooms
+  },
+  toggleSeenMsgRecord: async (userId) => {
+    //更新isSeen為true
+    const MsgRecordOption = {
+      where: {
+        ReceiverId: userId,
+        isSeen: false
+      },
+      attributes: ['id']
+    }
+    await MessageRecord.findAll(MsgRecordOption).then((records) => {
+      records = records.map(record => record.id)
+      MessageRecord.update({ isSeen: true }, { where: { id: records } })
+    })
+  },
+  getPrivateRoomData: async (socketId) => {
+    return privateRoomUsers[socketId]
+  },
+  toggleSeenPrivateMsg: async (User1Id, User2Id) => {
+    const recordOptions = {
+      where: {
+        ReceiverId: User1Id,
+        SenderId: User2Id
+      }
+    }
+    const record = await MessageRecord.findOne(recordOptions)
+    if (record) {
+      await record.update({ unreadNum: 0 })
+    }
+    return
+  },
+  getRoomId: async (User1Id, User2Id) => {
+    const roomOptions = {
+      where: {
+        $or: [
+          { User1Id, User2Id },
+          { User1Id: User2Id, User2Id: User1Id }
+        ]
+      }
+    }
+    const room = await Room.findOne(roomOptions)
+    let roomId
+    if (room) {
+      console.log(detail("room已存在，roomId為:"), room.id)
+      roomId = room.id
+    } else {
+      roomId = await Room.create({ User1Id, User2Id })
+      console.log(detail("建立新的room，roomId為:"), room.toJSON())
+      roomId = roomId.toJSON().id
+    }
+    return roomId
+  },
+  joinRoom: (userSocketIds, roomId) => {
+    userSocketIds.forEach((socketId) => {
+      const targetSocket = sockets.find((element) => element.id === socketId)
+      console.log(targetSocket.id)
+      targetSocket.join(roomId)
+    })
+  },
+  setPrivateRoomId: (socketId, roomId) => {
+    privateRoomUsers[socketId].currentRoom = roomId
+    return
+  },
+  showSocketDetails: () => {
+    console.log(detail('all sockets [伺服器紀錄]'), '\n', sockets.map(item => item.id))
+    const ids = await io.allSockets()
+    console.log(detail('all sockets [系統偵測]'), '\n', Array.from(ids))
+    console.log(detail('all socketUsers [詳細資料]'), '\n', socketUsers)
+    console.log(detail('all publicRoomUsers '), '\n', publicRoomUsers)
+    console.log(detail('all privateRoomUsers '), '\n', privateRoomUsers)
+  },
+  getRoomHistory: async (offset, limit, RoomId) => {
+    const options = {
+      offset,
+      limit,
+      order: [['createdAt', 'desc']],
+      include: [
+        {
+          model: User,
+          attributes: ['avatar'],
+          as: 'User'
+        }
+      ],
+      where: {
+        RoomId
+      }
+    }
+    const messages = await Message.findAll(options)
+    messages.forEach((message) => {
+      message.dataValues.avatar = message.dataValues.User.avatar
+      delete message.dataValues.User
+    })
+    return messages
+  },
+  addMessage: async (UserId, RoomId, content) => {
+    const message = await Message.create({ UserId, RoomId, content })
+    return message
+  },
+  getMsgRecord: async (RoomId, SenderId) => {
+    const record = await MessageRecord.findOne({
+      where: {
+        RoomId: RoomId,
+        SenderId: SenderId
+      }
+    })
+    return record
+  },
+  createMsgRecord: async (RoomId, SenderId, ReceiverId) => {
+    const record = await MessageRecord.create({
+      SenderId: SenderId,
+      ReceiverId: ReceiverId,
+      RoomId: RoomId,
+      isSeen: true,
+      unreadNum: 0
+    })
+    return record
+  },
+  updateMsgRecord: async () => {
+
+  },
+  getMsgNotice: async (userId, socket) => {
+    if (socket) {
+      userId = socketUsers[socket.id].id
+    }
+    const { count } = await MessageRecord.findAndCountAll({
+      where: {
+        ReceiverId: userId,
+        isSeen: false
+      }
+    })
+    return count
+  },
   getMsgNoticeDetails: async (userId) => {
     const notices = await MessageRecord.findAll({
       attributes: ['SenderId', 'RoomId', 'unreadNum', 'isSeen'],
@@ -74,7 +277,6 @@ let socketService = {
     })
     return { unseenRooms, unreadRooms }
   }
-
 }
 
 module.exports = socketService
