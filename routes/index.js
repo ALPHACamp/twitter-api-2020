635f4bd590fb9c45fb2928a61b4a1ebb77915447
lewislu327 <agooday32@gmail.com>
2021-08-04 17:51:45 +0800

    refactor: change route's name for getHistoryMessage API

--- routes/index.js
+++ routes/index.js
@@ -42,5 +42,5 @@ module.exports = (app) => {
   app.post('/api/tweets/:tweetId/unlike', authenticated, tweetController.unlikeTweet)
 
   // chats
-  app.get('/api/chats/public-room/', authenticated, chatController.getHistoryMessage)
+  app.get('/api/chats/public-messages', authenticated, chatController.getHistoryMessage)
 }
