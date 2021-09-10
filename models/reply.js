40ed990daab6d62225408c2a8b30a89473bd9832
JHIH-LEI <vicky88888@gmail.com>
2021-09-11 00:19:28 +0800

    feat: add attributes to reply model

--- models/reply.js
+++ models/reply.js
@@ -1,6 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER,
+    comment: DataTypes.TEXT
   }, {});
   Reply.associate = function (models) {
     Reply.belongsTo(models.User)
