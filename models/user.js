fc5e33d20e2666474dd9d5620276d89877696b5c
JHIH-LEI <vicky88888@gmail.com>
2021-09-10 22:43:57 +0800

    feat: update user model attributes & default value

--- models/user.js
+++ models/user.js
@@ -1,16 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-  }, {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     account: DataTypes.STRING,
     cover: DataTypes.STRING,
-    avatar: DataTypes.STRING,
+    avatar: { type: DataTypes.STRING, defaultValue: 'https://image.flaticon.com/icons/png/512/149/149071.png' },
     introduction: DataTypes.STRING,
-    role: DataTypes.STRING,
-  });
+    role: { type: DataTypes.STRING, defaultValue: 'user' },
+  }, {});
   User.associate = function (models) {
   };
   return User;
