24385b4eba96a05a75969eb35c499fcdf92680be
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-11 10:55:22 +0800

    feat: Add User model's schema define

--- models/user.js
+++ models/user.js
@@ -1,6 +1,26 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {}, {})
+  const User = sequelize.define(
+    'User',
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      avatar: {
+        type: DataTypes.STRING,
+        defaultValue: 'https://i.imgur.com/mLoDkSR.png'
+      },
+      introduction: DataTypes.STRING,
+      role: { type: DataTypes.STRING, defaultValue: 'user' },
+      account: DataTypes.STRING,
+      cover: {
+        type: DataTypes.STRING,
+        defaultValue:
+          'http://apy-ingenierie.fr/wp-content/plugins/uix-page-builder/uixpb_templates/images/UixPageBuilderTmpl/default-cover-6.jpg'
+      }
+    },
+    {}
+  )
   User.associate = function (models) {
     User.hasMany(models.Tweet)
     User.hasMany(models.Reply)
