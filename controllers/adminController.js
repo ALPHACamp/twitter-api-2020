91d095736a2d85ca9749bbb43bcb35c886ea7b53
WilliamTsou818 <cccc1589@gmail.com>
2021-09-11 23:46:56 +0800

    feat: add adminController.getUsers for geting all users data

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,20 @@
 const adminService = require('../services/adminService')
 
 const adminController = {
+  getUsers: async (req, res) => {
+    const users = await adminService.getUsers()
+
+    // Check whether users exists
+    if (!users) {
+      return res
+        .status(401)
+        .json({ status: 'error', message: 'No users found' })
+    }
+
+    return res
+      .status(200)
+      .json(users)
+  }
 }
 
 module.exports = adminController
