f4ccd113d561cdd1972091f9dc1f86afc129beee
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-11 11:10:22 +0800

    feat: Add Like model's schema define

--- models/like.js
+++ models/like.js
@@ -1,6 +1,13 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Like = sequelize.define('Like', {}, {})
+  const Like = sequelize.define(
+    'Like',
+    {
+      UserId: DataTypes.INTEGER,
+      TweetId: DataTypes.INTEGER
+    },
+    {}
+  )
   Like.associate = function (models) {
     Like.belongsTo(models.User)
     Like.belongsTo(models.Tweet)
