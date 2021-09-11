a0391a1ecc636dd1e36ef4d246174b1b7f0f8c05
WilliamTsou818 <cccc1589@gmail.com>
2021-09-11 21:44:37 +0800

    feat: add userService.getCurrentUser for current user

--- services/userService.js
+++ services/userService.js
@@ -3,7 +3,21 @@ const { User, Tweet, Reply, Like } = require('../models')
 const userService = {
   signIn: async (account) => {
     return await User.findOne({ where: { account } })
-  }
+  },
+  getCurrentUser: async (id) => {
+    return await User.findOne({
+      where: { id },
+      attributes: [
+        'id',
+        'name',
+        'account',
+        'avatar',
+        'introduction',
+        'role',
+        'cover',
+      ],
+    })
+  },
 }
 
 module.exports = userService
