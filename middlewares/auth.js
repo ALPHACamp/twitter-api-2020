dd20d842eb421dceba1f7f6615b92f7997182958
WilliamTsou818 <cccc1589@gmail.com>
2021-09-12 00:50:41 +0800

    fix: modify passport authenticate for passing unit tests

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,14 +4,29 @@ const passport = require('../config/passport')
 const helpers = require('../_helpers')
 
 // use helpers.getUser(req) to replace req.user
-const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = (req, res, next) =>
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err) {
+      return new Error(err)
+    }
+    // Check if the user exists
+    if (!user) {
+      return res
+        .status(401)
+        .json({ status: 'error', message: 'Permission denied' })
+    }
+
+    return next()
+  })
 
 // authenticatedRole('user') to verify req is user
 // authenticatedRole('admin') to verify req is admin
 const authenticatedRole = (role) => {
   return (req, res, next) => {
     if (helpers.getUser(req).role !== role) {
-      return res.json({ status: 'error', message: 'Permission denied' })
+      return res
+      .status(401)
+      .json({ status: 'error', message: 'Permission denied' })
     }
     return next()
   }
