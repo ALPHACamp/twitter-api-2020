64a278a735440c3585666fd3dfe9c02b2f0722aa
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-11 08:48:25 +0800

    feat: Setting method-override into app.js

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
 
 const app = express()
+const methodOverride = require('method-override')
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
+function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
-};
+}
+
+// Setting middleware: method-override
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
