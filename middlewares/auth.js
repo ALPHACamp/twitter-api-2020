163a7466fc9273f1a3a7580062fd36578a2d0915
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-11 15:13:32 +0800

    feat: Add middleware authenticated to verify that request has permission

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken')
-const { User, sequelize } = require('../models')
+const { User } = require('../models')
 const passport = require('../config/passport')
 const helpers = require('../_helpers')
 
 // use helpers.getUser(req) to replace req.user
-const authenticated = (req, res, next) => {}
+const authenticated = passport.authenticate('jwt', { session: false })
 
 module.exports = authenticated
