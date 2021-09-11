4b14ad442e0f8dda4dc46a389938114df1761e87
WilliamTsou818 <cccc1589@gmail.com>
2021-09-12 00:23:56 +0800

    fix: add middleware in admin route for checking role

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/adminController')
-const { authenticated } = require('../../middlewares/auth')
+const { authenticated, authenticatedRole } = require('../../middlewares/auth')
 
-router.get('/users', authenticated, adminController.getUsers)
+router.get('/users', authenticated, authenticatedRole('admin'), adminController.getUsers)
 
 module.exports = router
