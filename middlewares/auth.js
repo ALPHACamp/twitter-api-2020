31952043f58c2590a7e40f54f6764a0d8b27b29d
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-11 15:16:27 +0800

    feat: Add middleware authenticatedRole to verify user role

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,4 +6,15 @@ const helpers = require('../_helpers')
 // use helpers.getUser(req) to replace req.user
 const authenticated = passport.authenticate('jwt', { session: false })
 
+// authenticatedRole('user') to verify req is user
+// authenticatedRole('admin') to verify req is admin
+const authenticatedRole = (role) => {
+  return (req, res, next) => {
+    if (helpers.getUser(req).role !== role) {
+      return res.json({ status: 'error', message: 'Permission denied' })
+    }
+    return next()
+  }
+}
+
 module.exports = authenticated
