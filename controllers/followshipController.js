a70172d18674ab5499c7ad61057f0b1fadfd20ce
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-12 00:22:30 +0800

    feat: Add followshipService.addFollowing to add following
    1. Use followerId and followingId to check self-following

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -1,5 +1,15 @@
 const followshipService = require('../services/followshipService')
 
-const followshipController = {}
+const followshipController = {
+  addFollowing: (req, res) => {
+    followshipService.addFollowing(req, res, (data) => {
+      if (data['status'] === 'error') {
+        return res.status(401).json(data)
+      }
+
+      return res.status(200).json(data)
+    })
+  }
+}
 
 module.exports = followshipController
--- services/followshipService.js
+++ services/followshipService.js
@@ -1,5 +1,26 @@
 const { Followship } = require('../models')
 
-const followshipService = {}
+const helpers = require('../_helpers')
+
+const followshipService = {
+  addFollowing: (req, res, callback) => {
+    const followerId = Number(helpers.getUser(req).id)
+    const followingId = Number(req.body.id)
+
+    if (followerId === followingId) {
+      return callback({
+        status: 'error',
+        message: 'You cannot follow yourself.'
+      })
+    }
+
+    return Followship.create({
+      followerId,
+      followingId
+    }).then(() => {
+      callback({ status: 'success', message: 'Followed successfully' })
+    })
+  }
+}
 
 module.exports = followshipService
