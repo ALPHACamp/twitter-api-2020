a0c38a66bf2e2aea34ed448b06106c96c0e22fa0
WilliamTsou818 <cccc1589@gmail.com>
2021-09-11 15:20:38 +0800

    fix: modify typo in like seed file

--- seeders/20210911063725-likes-seed-file.js
+++ seeders/20210911063725-likes-seed-file.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
       'Likes',
-      Array.from({ length: 150 }).map((reply, index) => ({
+      Array.from({ length: 150 }).map((like, index) => ({
         id: index * 10 + 5,
         userId: ((index % 5) + 1) * 10 + 5,
         tweetId: Math.floor(index / 3) * 10 + 5,
