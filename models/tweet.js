0066834553fd9babada37fa914132869371394ac
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-11 10:37:05 +0800

    feat: Modify Tweet's associate with Reply、Like
    1. Set up onDelete hook with tweet

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,12 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-  }, {});
-  Tweet.associate = function(models) {
-  };
-  return Tweet;
-};
+  const Tweet = sequelize.define('Tweet', {}, {})
+  Tweet.associate = function (models) {
+    // When deleting a tweet, the related like and reply will be deleted at the same time
+    Tweet.hasMany(models.Like, { onDelete: 'cascade', hooks: true })
+    Tweet.hasMany(models.Reply, { onDelete: 'cascade', hooks: true })
+
+    Tweet.belongsTo(models.User)
+  }
+  return Tweet
+}
