9a68b9d2d539f3741c5285c5fe3662c1173144dd
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-11 10:38:52 +0800

    feat: Modify Reply's associate with User、Tweet

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,9 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {
-  }, {});
-  Reply.associate = function(models) {
-  };
-  return Reply;
-};
+  const Reply = sequelize.define('Reply', {}, {})
+  Reply.associate = function (models) {
+    Reply.belongsTo(models.User)
+    Reply.belongsTo(models.Tweet)
+  }
+  return Reply
+}
