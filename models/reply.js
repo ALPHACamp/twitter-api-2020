09b69fdc3ff91e0b47906652cd2b88341ee7adf0
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-11 11:11:42 +0800

    feat: Add Reply model's schema define

--- models/reply.js
+++ models/reply.js
@@ -1,6 +1,14 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {}, {})
+  const Reply = sequelize.define(
+    'Reply',
+    {
+      UserId: DataTypes.INTEGER,
+      TweetId: DataTypes.INTEGER,
+      comment: DataTypes.STRING
+    },
+    {}
+  )
   Reply.associate = function (models) {
     Reply.belongsTo(models.User)
     Reply.belongsTo(models.Tweet)
