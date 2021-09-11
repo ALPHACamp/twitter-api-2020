f4050aed11287468d68f58fb8477bbfa24065a0e
WilliamTsou818 <cccc1589@gmail.com>
2021-09-11 22:14:40 +0800

    feat: add adminService.getUsers for getting all users

--- services/adminService.js
+++ services/adminService.js
@@ -1,5 +1,32 @@
-const { User, Tweet, Like } = require('../models')
+const { User, Tweet, Like, sequelize } = require('../models')
+const Sequelize = require('sequelize')
 
-const adminService = {}
+const adminService = {
+  getUsers: async (req, res) => {
+    return await User.findAll({
+      raw: true,
+      subQuery: false,
+      where: { role: 'user' },
+      attributes: [
+        'id',
+        'name',
+        [Sequelize.fn('concat', '@', Sequelize.col('users.account')), 'account'],
+        'avatar',
+        'cover',
+        [Sequelize.fn('count', Sequelize.col('followships.followingId')), 'followerCount'],
+        [Sequelize.fn('count', Sequelize.col('followships.followerId')), 'followingCount'],
+        [Sequelize.fn('count', Sequelize.col('likes.userId')), 'likeCount'],
+        [Sequelize.fn('count', Sequelize.col('replies.userId')), 'replyCount']
+      ],
+      include: [
+        { model: User, as: 'Followings' },
+        { model: User, as: 'followers' },
+        { model: Like, attributes: [] },
+        { model: Reply, attributes: [] }
+      ],
+      group: ['id']
+    })
+  }
+}
 
 module.exports = adminService
