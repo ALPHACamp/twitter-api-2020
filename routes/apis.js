f92aa6a04d87f8698bf94cce9340a4a11cb10403
BoboLin <bobolincowo@gmail.com>
2021-09-11 23:51:10 +0800

    feat: add POST /adminLogin api

--- routes/apis.js
+++ routes/apis.js
@@ -4,8 +4,12 @@ const router = express.Router()
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
 const userController = require('../controllers/userController.js')
+const adminController = require('../controllers/adminController.js')
 
 // 前台 Login
 router.post('/login', userController.logIn)
+
+// 後台 Login
+router.post('/adminLogin', adminController.adminLogIn)
 
 module.exports = router
