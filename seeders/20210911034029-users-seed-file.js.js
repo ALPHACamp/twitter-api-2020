ac6a623264cc0033f58f6c4051fe3199e240d9cd
WilliamTsou818 <cccc1589@gmail.com>
2021-09-11 12:45:46 +0800

    feat: add user seed file

---
+++ seeders/20210911034029-users-seed-file.js.js
@@ -0,0 +1,96 @@
+'use strict'
+
+const faker = require('faker')
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          id: 5,
+          email: 'root@example.com',
+          account: 'root@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
+          name: 'root',
+          role: 'admin',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 15,
+          email: 'user1@example.com',
+          account: 'user1',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
+          name: 'User1',
+          avatar: `https://loremflickr.com/320/240/boy/?lock=${Math.random() * 100}`,
+          introduction: faker.lorem.sentence(),
+          cover: `https://loremflickr.com/320/240/landscape/?lock=${Math.random() * 100}`,
+          role: 'user',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 25,
+          email: 'user2@example.com',
+          account: 'user2',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
+          name: 'User2',
+          avatar: `https://loremflickr.com/320/240/boy/?lock=${Math.random() * 100}`,
+          introduction: faker.lorem.sentence(),
+          cover: `https://loremflickr.com/320/240/landscape/?lock=${Math.random() * 100}`,
+          role: 'user',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 35,
+          email: 'user3@example.com',
+          account: 'user3',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
+          name: 'User3',
+          avatar: `https://loremflickr.com/320/240/girl/?lock=${Math.random() * 100}`,
+          introduction: faker.lorem.sentence(),
+          cover: `https://loremflickr.com/320/240/landscape/?lock=${Math.random() * 100}`,
+          role: 'user',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 45,
+          email: 'user4@example.com',
+          account: 'user4',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
+          name: 'User4',
+          avatar: `https://loremflickr.com/320/240/boy/?lock=${Math.random() * 100}`,
+          introduction: faker.lorem.sentence(),
+          cover: `https://loremflickr.com/320/240/landscape/?lock=${
+            Math.random() * 100
+          }`,
+          role: 'user',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          id: 55,
+          email: 'user5@example.com',
+          account: 'user5',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
+          name: 'User5',
+          avatar: `https://loremflickr.com/320/240/girl/?lock=${Math.random() * 100}`,
+          introduction: faker.lorem.sentence(),
+          cover: `https://loremflickr.com/320/240/landscape/?lock=${Math.random() * 100}`,
+          role: 'user',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('People', null, {})
+  }
+}
