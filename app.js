3ea1d195a5cb361699c09802bb72c23fad4b831d
JHIH-LEI <vicky88888@gmail.com>
2021-09-11 14:25:28 +0800

    feat: add bodyParser

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const helpers = require('./_helpers');
-
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
+function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 };
-
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
