5f26c7e627c9343e8da5ec9614007049729d958a
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-12 16:09:12 +0800

    feat: set up passport into app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const methodOverride = require('method-override')
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+const passport = require('./config/passport')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -12,9 +13,11 @@ function authenticated(req, res, next){
   // passport.authenticate('jwt', { ses...
 };
 
+app.use(passport.initialize())
+
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
 module.exports = app
