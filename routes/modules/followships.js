ad980d5613b9897b3491d4982ae4d38f1479ed02
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-12 14:49:47 +0800

    feat: Set up /api/followships/:followingId route path

--- routes/modules/followships.js
+++ routes/modules/followships.js
@@ -4,5 +4,6 @@ const followshipController = require('../../controllers/followshipController')
 const { authenticated, authenticatedRole } = require('../../middlewares/auth')
 
 router.post('/', followshipController.addFollowing)
+router.delete('/:followingId', followshipController.deleteFollowing)
 
 module.exports = router
