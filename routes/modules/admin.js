25dcf980b4643fe3b36231c9a1308a3235b5d7ca
WilliamTsou818 <cccc1589@gmail.com>
2021-09-12 12:14:03 +0800

    feat: add GET /api/admin/tweets to admin route

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,10 @@ const router = express.Router()
 const adminController = require('../../controllers/adminController')
 const { authenticated, authenticatedRole } = require('../../middlewares/auth')
 
+// Get all users except admin
 router.get('/users', authenticated, authenticatedRole('admin'), adminController.getUsers)
+
+// Get all tweets
+router.get('/tweets', authenticated, authenticatedRole('admin'), adminController.getTweets)
 
 module.exports = router
