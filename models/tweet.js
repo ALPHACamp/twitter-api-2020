2450cf0a13e0b637063a5b43fdef8681ab54029e
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-12 16:59:32 +0800

    feat: add Tweet's associate with Reply & Like

--- models/tweet.js
+++ models/tweet.js
@@ -3,6 +3,10 @@ module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
   }, {});
   Tweet.associate = function(models) {
+    Tweet.hasMany(models.Like, { onDelete: "cascade", hooks: true });
+    Tweet.hasMany(models.Reply, { onDelete: "cascade", hooks: true });
+
+    Tweet.belongsTo(models.User);
   };
   return Tweet;
 };
