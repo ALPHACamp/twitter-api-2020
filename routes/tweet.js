c90eb71c6ced67f4e6b4568ff15a24041542ef8a
JHIH-LEI <vicky88888@gmail.com>
2021-09-12 22:00:47 +0800

    POST /api/tweets

---
+++ routes/tweet.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const tweetController = require('../controllers/tweetController')
+
+router.post('/', tweetController.postTweet)
+
+module.exports = router
