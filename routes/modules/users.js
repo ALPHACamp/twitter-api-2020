bdf727575d201a246d8d101fba00e03e65e4cf14
WilliamTsou818 <cccc1589@gmail.com>
2021-09-12 18:26:54 +0800

    feat: add POST /api/users to user route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,6 +4,7 @@ const userController = require('../../controllers/userController')
 const { authenticated, authenticatedRole } = require('../../middlewares/auth')
 
 router.post('/signin', userController.signIn)
+router.post('/', userController.postUser)
 router.get('/current_user', authenticated, authenticatedRole(), userController.getCurrentUser)
 
 module.exports = router
