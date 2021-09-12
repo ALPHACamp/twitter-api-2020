69f4d65095d50e1408efac68051b17868c03780f
Ruby Tsai <hjtsai0307@gmail.com>
2021-09-12 23:09:12 +0800

    add account & cover to user, modify user model, and create users seed file

--- models/user.js
+++ models/user.js
@@ -1,8 +1,38 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
+    email: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    account: {
+      type: DataTypes.STRING,
+      unique: true
+    },
+    cover: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+      sequelize,
+      modelName: 'User',
+  });
+  User.associate = function (models) {
+    User.hasMany(models.Reply)
+    User.hasMany(models.Tweet)
+    User.hasMany(models.Like)
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
   };
   return User;
 };
