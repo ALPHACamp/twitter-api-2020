55b6a18d690acf450a5ef55f3bcf6caae2f5a6d6
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-12 17:22:17 +0800

    feat: add Like model's schema define

--- models/like.js
+++ models/like.js
@@ -1,6 +1,8 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
   }, {});
   Like.associate = function(models) {
     Like.belongsTo(models.User);
--- models/user.js
+++ models/user.js
@@ -35,6 +35,14 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         defaultValue: "https://i.imgur.com/1jDf2Me.png",
       },
+      followerCount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      followingCount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
     },
     {}
   );
