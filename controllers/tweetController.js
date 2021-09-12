b99bcff32e1e9a55f3e3712e6782885a2786ca4c
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-12 21:49:19 +0800

    feat: Add tweetService.getTweet to respone one tweet's info

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -12,6 +12,18 @@ const tweetController = {
     }
 
     return res.status(200).json(tweets)
+  },
+  getTweet: async (req, res) => {
+    const tweet = await tweetService.getTweet(req.user.id, req.params.tweetId)
+
+    // Check whether tweets exists
+    if (!tweet) {
+      return res
+        .status(401)
+        .json({ status: 'error', message: 'No tweet found' })
+    }
+
+    return res.status(200).json(tweet)
   }
 }
 
