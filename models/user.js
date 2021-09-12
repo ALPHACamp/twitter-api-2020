c7cd7a3b042e09ef2b29c7c9d538948d39a1bd41
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-12 16:57:20 +0800

    feat: add User's associate with Tweet & Reply & Like & Followship models

--- models/user.js
+++ models/user.js
@@ -3,6 +3,21 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
   }, {});
   User.associate = function(models) {
+    User.hasMany(models.Tweet);
+    User.hasMany(models.Reply);
+    User.hasMany(models.Like);
+
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: "followingId",
+      as: "Followers",
+    });
+
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: "followerId",
+      as: "Followings",
+    });
   };
   return User;
 };
