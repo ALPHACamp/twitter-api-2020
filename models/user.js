0fa0c1381929399a2617d83afca9cace813c3bc0
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-12 17:21:07 +0800

    feat: add User model's schema define

--- models/user.js
+++ models/user.js
@@ -1,7 +1,43 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
+  const User = sequelize.define(
+    "User",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        defaultValue: "user",
+      },
+      avatar: {
+        type: DataTypes.STRING,
+        defaultValue: "https://i.imgur.com/q6bwDGO.png",
+      },
+      introduction: DataTypes.STRING,
+      account: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      cover: {
+        type: DataTypes.STRING,
+        defaultValue: "https://i.imgur.com/1jDf2Me.png",
+      },
+    },
+    {}
+  );
   User.associate = function(models) {
     User.hasMany(models.Tweet);
     User.hasMany(models.Reply);
