b15c47f796c9a54905e5e12c5d29e5418c513844
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-12 20:46:28 +0800

    feat: Add tweetService.getTweets to get all tweets from database

--- services/tweetService.js
+++ services/tweetService.js
@@ -1,5 +1,43 @@
-const { Tweet, Reply, Like, User } = require('../models')
+const { Tweet, Reply, Like, User, Sequelize } = require('../models')
 
-const tweetService = {}
+const tweetService = {
+  getTweets: async (id) => {
+    return await Tweet.findAll({
+      raw: true,
+      nest:true,
+      include: [
+        { model: Like, attributes: [] },
+        { model: Reply, attributes: [] },
+        { model: User, attributes: ['id', 'name', 'account', 'avatar'] }
+      ],
+      attributes: [
+        ['id', 'TweetId'],
+        [
+          Sequelize.literal(
+            `exists(select 1 from Likes where UserId = ${id} and TweetId = Tweet.id)`
+          ),
+          'isLike'
+        ],
+        [
+          Sequelize.literal(
+            '(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id)'
+          ),
+          'likesCount'
+        ],
+        [
+          Sequelize.literal(
+            '(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id)'
+          ),
+          'repliesCount'
+        ],
+        'description',
+        'createdAt',
+        'updatedAt'
+      ],
+      group: ['TweetId'],
+      order: [['createdAt', 'DESC']]
+    })
+  }
+}
 
 module.exports = tweetService
