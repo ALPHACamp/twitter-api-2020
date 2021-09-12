f292a35d35364404b79d57cd3eefca1fb09ab008
WilliamTsou818 <cccc1589@gmail.com>
2021-09-12 15:53:31 +0800

    fix: add authenticatedRole to GET /current_user route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/userController')
-const { authenticated } = require('../../middlewares/auth')
+const { authenticated, authenticatedRole } = require('../../middlewares/auth')
 
 router.post('/signin', userController.signIn)
-router.get('/current_user', authenticated, userController.getCurrentUser)
+router.get('/current_user', authenticated, authenticatedRole(), userController.getCurrentUser)
 
 module.exports = router
