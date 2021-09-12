11d3aa6c59625861608ddbba260ed76a9e66be69
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-12 16:05:38 +0800

    feat: Setup method-override & modify app.js PORT

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const helpers = require('./_helpers');
-
+const methodOverride = require('method-override')
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 
 // use helpers.getUser(req) to replace req.user
 function authenticated(req, res, next){
   // passport.authenticate('jwt', { ses...
 };
+
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
