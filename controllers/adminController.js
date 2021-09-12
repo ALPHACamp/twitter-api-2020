f65fc6dd1768f345973aee43cf4aac6d7acd6fa3
WilliamTsou818 <cccc1589@gmail.com>
2021-09-12 15:09:35 +0800

    refactor: Re-format adminController for coding style

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -11,9 +11,7 @@ const adminController = {
         .json({ status: 'error', message: 'No users found' })
     }
 
-    return res
-      .status(200)
-      .json(users)
+    return res.status(200).json(users)
   },
 
   getTweets: async (req, res) => {
@@ -26,24 +24,18 @@ const adminController = {
         .json({ status: 'error', message: 'No tweets found' })
     }
 
-    return res
-      .status(200)
-      .json(tweets)
+    return res.status(200).json(tweets)
   },
 
   deleteTweet: async (req, res) => {
     const data = await adminService.deleteTweet(req.params.id)
 
     // Check data status
-    if (data.status === 'error') {
-      return res
-        .status(401)
-        .json(data)
+    if (data['status'] === 'error') {
+      return res.status(401).json(data)
     }
-    
-    return res
-      .status(200)
-      .json(data)
+
+    return res.status(200).json(data)
   }
 }
 
