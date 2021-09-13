c83a583fadc7201c27d06873227b614706eb9d06
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-14 00:10:25 +0800

    refactor: modify req.user using helpers

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require('../config/passport')
 const router = express.Router();
 const admin = require("./modules/admin");
+const helpers = require('../_helpers')
 const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 const followships = require('./modules/followships')
@@ -9,8 +10,8 @@ const followships = require('./modules/followships')
 const authenticated = passport.authenticate("jwt", { session: false });
 
 const authenticatedAdmin = (req, res, next) => {
-  if (req.user) {
-    if (req.user.role === "admin") {
+  if (helpers.getUser(req)) {
+    if (helpers.getUser(req).role === "admin") {
       return next();
     }
     return res.json({ status: "error", message: "permission denied" });
