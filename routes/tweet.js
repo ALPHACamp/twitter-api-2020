984c1314702a5a187e9d25ebcccccdb5f91c768e
JHIH-LEI <vicky88888@gmail.com>
2021-09-13 18:28:09 +0800

    GET /api/tweets/:tweet_id

--- routes/tweet.js
+++ routes/tweet.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const tweetController = require('../controllers/tweetController')
 
 router.post('/:tweet_id/replies', tweetController.postReply)
+router.get('/:tweet_id', tweetController.getTweet)
 router.post('/', tweetController.postTweet)
 
 module.exports = router
