955d2f009b230bd6fc16aaac8aa85006a862f7a1
wilson614 <s110613053@stu.ntue.edu.tw>
2021-09-13 23:25:54 +0800

    Modify tweetController with async function

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -5,44 +5,56 @@ const User = db.User
 const Reply = db.Reply
 
 const tweetController = {
-  getTweets: (req, res) => {
-    Tweet.findAll({
-      include: User,
-      order: [
-        ['createdAt', 'DESC']
-      ]
-    }).then(tweet => {
+  getTweets: async (req, res) => {
+    try {
+      const tweet = await Tweet.findAll({
+        include: User,
+        order: [
+          ['createdAt', 'DESC']
+        ]
+      })
       const tweetdata = tweet.map(tweet => ({
         ...tweet.dataValues,
         name: tweet.User.name,
         avatar: tweet.User.avatar,
-        account: tweet.User.account,
+        account: tweet.User.account
       }))
-      res.json(tweetdata)
-    })
-  },
-  getTweet: (req, res) => {
-    return Tweet.findByPk(req.params.tweet_id, {
-      include: [
-        User,
-        { model: Reply, include: [User] }
-      ]
-    }).then(tweet => {
-      res.json(tweet)
-    })
-  },
-  postTweet: (req, res) => {
-    if (!req.body.description) {
-      const data = { status: 'error', message: "content didn't exist" }
+      return res.json(tweetdata)
+    } catch (err) {
+      const data = { status: 'error', message: err }
       return res.json(data)
     }
-    Tweet.create({
-      UserId: helpers.getUser(req).id,
-      description: req.body.description
-    }).then(() => {
+  },
+  getTweet: async (req, res) => {
+    try {
+      const tweet = await Tweet.findByPk(req.params.tweet_id, {
+        include: [
+          User,
+          { model: Reply, include: [User] }
+        ]
+      })
+      return res.json(tweet)
+    } catch (err) {
+      const data = { status: 'error', message: err }
+      return res.json(data)
+    }
+  },
+  postTweet: async (req, res) => {
+    try {
+      if (!req.body.description) {
+        const data = { status: 'error', message: "content didn't exist" }
+        return res.json(data)
+      }
+      await Tweet.create({
+        UserId: helpers.getUser(req).id,
+        description: req.body.description
+      })
       const data = { status: 'success', message: 'a new tweet was successfully posted' }
-      res.json(data)
-    })
+      return res.json(data)
+    } catch (err) {
+      const data = { status: 'error', message: err }
+      return res.json(data)
+    }
   }
 }
 
