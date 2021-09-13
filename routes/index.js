68f1bcbe44e7f21125a9fbab7281a5f0659a1858
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-13 23:03:04 +0800

    feat: add authenticate middleware

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const passport = require('../config/passport')
 const router = express.Router();
 const admin = require("./modules/admin");
 const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 const followships = require('./modules/followships')
+
+const authenticated = passport.authenticate("jwt", { session: false });
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.role === "admin") {
+      return next();
+    }
+    return res.json({ status: "error", message: "permission denied" });
+  } else {
+    return res.json({ status: "error", message: "permission denied" });
+  }
+};
 
 router.use("/admin", admin);
 
