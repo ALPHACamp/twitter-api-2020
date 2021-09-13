1558e249c3995d00a3b3821bd0c2957e3857f4ae
Lucas-Peng-CS <cs20210310@gmail.com>
2021-09-13 18:46:07 +0800

    modify reply seeder code

--- seeders/20210912162548-replies-seed-file.js
+++ seeders/20210912162548-replies-seed-file.js
@@ -1,20 +1,24 @@
 'use strict'
 const faker = require('faker')
 // 每篇 post 有隨機 3 個留言者
-const totalUserId = []
+const totalUserId = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91]
+const randomUserId = []
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Replies',
       Array.from({ length: 300 }).map((d, index) => {
-        if (!totalUserId.length) {
-          [1, 11, 21, 31, 41, 51, 61, 71, 81, 91].forEach((id) => {
-            totalUserId.push(id)
-          })
+        if (!randomUserId.length) {
+          while (randomUserId.length < 3) {
+            const i = Math.floor(Math.random() * totalUserId.length)
+            if (!randomUserId.includes(totalUserId[i])) {
+              randomUserId.push(totalUserId[i])
+            }
+          }
         }
-
-        const i = Math.floor(Math.random() * totalUserId.length)
-        const id = totalUserId[i]
-        totalUserId.splice(i, 1)
+        const i = Math.floor(Math.random() * randomUserId.length)
+        const id = randomUserId[i]
+        randomUserId.splice(i, 1)
         return ({
           id: index * 10 + 1,
           UserId: id,
