e25c43b903be9f74142c2cb05df76cc11c6f4fb3
JHIH-LEI <vicky88888@gmail.com>
2021-09-14 02:14:36 +0800

    DELETE /api/followships/:followingId

--- routes/followship.js
+++ routes/followship.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const followshipController = require('../controllers/followshipController')
 
 router.post('/', followshipController.followUser)
+router.delete('/:followingId', followshipController.unFollowUser)
 
 module.exports = router
