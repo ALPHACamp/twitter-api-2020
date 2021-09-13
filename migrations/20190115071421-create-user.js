5c9c8cd6f8d049baf27f75cd10b98f8f91eeb8bb
Whaleep <billy_1817@yahoo.com.tw>
2021-09-14 01:41:53 +0800

    add seeder

--- migrations/20190115071421-create-user.js
+++ migrations/20190115071421-create-user.js
@@ -17,7 +17,7 @@ module.exports = {
         unique: true,
       },
       password: {
-        type: Sequelize.STRING(50),
+        type: Sequelize.STRING,
       },
       name: {
         type: Sequelize.STRING(50),
