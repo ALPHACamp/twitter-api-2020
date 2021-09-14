7c1c9c9f398db73d9ddce052976b4367018c6884
wilson614 <s110613053@stu.ntue.edu.tw>
2021-09-14 14:44:59 +0800

    Modify replyController with try & catch

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -6,25 +6,35 @@ const Tweet = db.Tweet
 
 const replyController = {
   postReply: async (req, res) => {
-    await Reply.create({
-      UserId: helpers.getUser(req).id,
-      TweetId: req.params.tweet_id,
-      comment: req.body.comment
-    })
-    const tweet = await Tweet.findByPk(req.params.tweet_id)
-    tweet.increment(['replyCount'], { by: 1 })
-    const data = { status: 'success', message: 'a new comment was successfully replied' }
-    return res.json(data)
+    try {
+      await Reply.create({
+        UserId: helpers.getUser(req).id,
+        TweetId: req.params.tweet_id,
+        comment: req.body.comment
+      })
+      const tweet = await Tweet.findByPk(req.params.tweet_id)
+      tweet.increment(['replyCount'], { by: 1 })
+      const data = { status: 'success', message: 'a new comment was successfully replied' }
+      return res.json(data)
+    } catch (err) {
+      const data = { status: 'error', message: err }
+      return res.json(data)
+    }
   },
   getReply: async (req, res) => {
-    const replies = await Reply.findAll({
-      where: { TweetId: req.params.tweet_id },
-      include: [
-        User,
-        Tweet
-      ]
-    })
-    return res.json(replies)
+    try {
+      const replies = await Reply.findAll({
+        where: { TweetId: req.params.tweet_id },
+        include: [
+          User,
+          Tweet
+        ]
+      })
+      return res.json(replies)
+    } catch (err) {
+      const data = { status: 'error', message: err }
+      return res.json(data)
+    }
   }
 }
 
