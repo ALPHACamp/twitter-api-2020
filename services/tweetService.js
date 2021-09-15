a9573cd57dbcbb76592455f635716c36b6b9bb28
JHIH-LEI <vicky88888@gmail.com>
2021-09-15 16:34:23 +0800

    feat: add getTweets feature

--- services/tweetService.js
+++ services/tweetService.js
@@ -1,7 +1,5 @@
 const db = require('../models')
-const Tweet = db.Tweet
-const Reply = db.Reply
-const Like = db.Like
+const { Tweet, Reply, Like, Followship, User } = db
 
 const tweetService = {
   postTweet: async (req, res, cb) => {
@@ -57,6 +55,52 @@ const tweetService = {
       delete tweet.Replies
       delete tweet.Likes
       return cb({ status: '200', ...tweet, totalReply, totalLike })
+    } catch (err) {
+      console.warn(err)
+      return cb({ status: '500', message: err })
+    }
+  },
+
+  getTweets: async (req, res, cb) => {
+    try {
+      const liked = await User.findOne({
+        attributes: [],
+        where: { id: req.user.id },
+        include: { model: Like, attributes: ['TweetId'] }
+      })
+      // 取得追蹤中清單
+      // let followings = await Followship.findAll({
+      //   raw: true, nest: true,
+      //   attributes: ['followingId'],
+      //   where: {
+      //     followerId: req.user.id
+      //   }
+      // })
+      // 追蹤中的id清單
+      // followings = followings.map(d => (d.followingId))
+      let tweets = await Tweet.findAll({
+        attributes: { exclude: ['UserId'] },
+        order: [['createdAt', 'DESC']],
+        nest: true,
+        // where: {
+        //   userId: followings //僅限有追蹤的人，但是測試檔會不過，因為他沒有先追蹤用戶
+        // },
+        include: [
+          { model: User, attributes: ['id', 'name', 'account', 'avatar'] },
+          { model: Reply, attributes: ['id'] },
+          { model: Like, attributes: ['id'] }
+        ]
+      })
+      tweets.map(d => {
+        // totalLikes totalReplies isLiked
+        d.dataValues.totalLike = d.dataValues.Likes.length
+        d.dataValues.totalReply = d.dataValues.Replies.length
+        d.dataValues.isLiked = liked.Likes.map(d => (d.TweetId)).includes(d.dataValues.Likes.id)
+        // 刪掉前端不要的資料
+        delete d.dataValues.Likes
+        delete d.dataValues.Replies
+      })
+      return cb(tweets)
     } catch (err) {
       console.warn(err)
       return cb({ status: '500', message: err })
