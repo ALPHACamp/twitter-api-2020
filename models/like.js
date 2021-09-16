07872deff1c314eea377d376a1dad84757a04645
Ruby Tsai <hjtsai0307@gmail.com>
2021-09-16 15:37:14 +0800

    define like model and create likes seed file

--- models/like.js
+++ models/like.js
@@ -1,8 +1,18 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
-  }, {});
-  Like.associate = function(models) {
-  };
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
+  }, {})
+  Like.associate = function (models) {
+    Like.belongsTo(models.User, {
+      foreignKey: 'UserId',
+      as: 'user'
+    })
+    Like.belongsTo(models.Tweet, {
+      foreignKey: 'TweetId',
+      as: 'tweet'
+    })
+  }
   return Like;
-};
+}
---
+++ seeders/20210916072400-likes-seed-file.js
@@ -0,0 +1,28 @@
+'use strict'
+const faker = require('faker')
+
+let likes = []
+for (let i = 0; i < 150; i++) {
+  // set Start and End date to generate random date
+  let createdStart = new Date(2012, 0, 1)
+  let createdEnd = new Date(2015, 0, 1)
+  let updatedStart = new Date(2016, 0, 1)
+  let updatedEnd = new Date(2020, 0, 1)
+
+  let like = {
+    userId: Math.floor(Math.random() * 10),
+    tweetId: Math.floor(Math.random() * 50),
+    createdAt: new Date(createdStart.getTime() + Math.random() * (createdEnd.getTime() - createdStart.getTime())),
+    updatedAt: new Date(updatedStart.getTime() + Math.random() * (updatedEnd.getTime() - updatedStart.getTime()))
+  }
+  likes.push(like)
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Likes', likes, {})
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Likes', null, {})
+  }
+}
