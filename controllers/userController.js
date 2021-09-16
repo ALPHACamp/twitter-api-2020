a52946866315d55a4d44e74a98eb7d8deb785c83
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-16 23:52:57 +0800

    feat: Add error handling into GET /api/users/:userID/followings

--- controllers/userController.js
+++ controllers/userController.js
@@ -200,27 +200,32 @@ const userController = {
     }
   },
 
-  getUserFollowings: async (req, res) => {
-    const [targetUserId, currentUserId] = [
-      req.params.id,
-      helpers.getUser(req).id
-    ]
+  getUserFollowings: async (req, res, next) => {
+    try {
+      const [targetUserId, currentUserId] = [
+        req.params.id,
+        helpers.getUser(req).id
+      ]
 
-    let users = await userService.getUserFollowings(targetUserId, currentUserId)
+      let users = await userService.getUserFollowings(
+        targetUserId,
+        currentUserId
+      )
 
-    // Check whether the users exist
-    if (!users) {
-      return res
-        .status(200)
-        .json({ status: 'success', message: 'No users found' })
+      // Check whether the users exist
+      if (!users.length) {
+        throw new ApiError('GetUserFollowingsError', 401, 'No users found')
+      }
+
+      // translate to boolean in isFollowed attribute
+      users.forEach((user) => {
+        user.isFollowed = !!user.isFollowed
+      })
+
+      return res.status(200).json(users)
+    } catch (error) {
+      next(error)
     }
-
-    // translate to boolean in isFollowed attribute
-    users.forEach((user) => {
-      user.isFollowed = !!user.isFollowed
-    })
-
-    return res.status(200).json(users)
   },
 
   getUserFollowers: async (req, res) => {
