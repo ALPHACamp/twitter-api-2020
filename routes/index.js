faa4ddf66941c76d241c5ca239df5de6bc6d3810
JHIH-LEI <vicky88888@gmail.com>
2021-09-16 12:41:48 +0800

    feat: modify admin routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const user = require('./user')
 const tweet = require('./tweet')
 const followship = require('./followship')
+const admin = require('./admin')
 const { authenticated } = require('../middlewares/auth')
 const userController = require('../controllers/userController')
 
@@ -9,4 +10,5 @@ module.exports = (app) => {
   app.post('/api/login', userController.login)
   app.use('/api/tweets', authenticated, tweet)
   app.use('/api/followships', authenticated, followship)
+  app.use('/api/admin', authenticated, admin)
 }
