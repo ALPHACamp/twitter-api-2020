0ddb8a110c590f45a33cb53ec4c4110b575ed0a5
JHIH-LEI <vicky88888@gmail.com>
2021-09-16 12:41:16 +0800

    feat: modify adminController by remove res.render

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,17 +2,13 @@ const adminService = require('../services/adminService')
 
 const adminController = {
   getTweets: (req, res) => {
-    adminService.getTweets(req, res, (data) => res.render('admin/tweets', data))
+    adminService.getTweets(req, res, (data) => res.status(data.status).json(data))
   },
   removeTweet: (req, res) => {
-    adminService.removeTweet(req, res, (data) => {
-      if (data['status'] === 'success') {
-        res.redirect('back')
-      }
-    })
+    adminService.removeTweet(req, res, (data) => res.json(data))
   },
-  getUsers: (req, res)   => {
-    adminService.getUsers(req, res, (data) => res.render('admin/users', data))
+  getUsers: (req, res) => {
+    adminService.getUsers(req, res, (data) => res.status(data.status).json(data.users))
   }
 }
 
