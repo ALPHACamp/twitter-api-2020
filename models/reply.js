b4587467b8aaa2b77417a729a25a8bd66a64acb5
Ruby Tsai <hjtsai0307@gmail.com>
2021-09-16 15:21:11 +0800

    define reply model and create replies seed file

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,19 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
-  }, {});
-  Reply.associate = function(models) {
-  };
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER,
+    comment: DataTypes.TEXT
+  }, {})
+  Reply.associate = function (models) {
+    Reply.belongsTo(models.User, {
+      foreignKey: 'UserId',
+      as: 'user'
+    })
+    Reply.belongsTo(models.Tweet, {
+      foreignKey: 'TweetId',
+      as: 'tweet'
+    })
+  }
   return Reply;
-};
+}
