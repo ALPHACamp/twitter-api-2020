79087e733b63b7a2b33eb7dfac748cac600983ef
Chris Chen <ok3201123@gmail.com>
2021-09-17 00:07:35 +0800

    refactor: move authentication middleware from route to auth.js

--- routes/apis.js
+++ routes/apis.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('passport')
-const helpers = require('../_helpers.js')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
-const { User } = require('../models')
+const { authenticated, authenticatedAdmin, authenticatedNotAdmin } = require('../middleware/auth.js')
 
 const userController = require('../controllers/userController.js')
 const adminController = require('../controllers/adminController.js')
@@ -12,46 +10,6 @@ const followshipController = require('../controllers/followshipController.js')
 const likeController = require('../controllers/likeController.js')
 const tweetController = require('../controllers/tweetController.js')
 const replyController = require('../controllers/replyController.js')
-
-// jwt驗證
-const authenticated = (req, res, next) => {
-  passport.authenticate('jwt', { session: false }, async (err, user, info) => {
-    if (err) { return next(err) }
-    if (!user) {
-      if (info.name === 'TokenExpiredError') {
-        return res.status(401).json({ message: 'Token已過期！' })
-      } else {
-        return res.status(401).json({ message: info.message })
-      }
-    }
-    user = await User.findByPk(user.dataValues.id, {
-      include: [
-        { model: User, as: 'Followers' },
-        { model: User, as: 'Followings' }
-      ]
-    })
-    req.user = user.dataValues
-    return next()
-  })(req, res, next)
-}
-// 驗證登入者是否為管理者=>用於後台路由
-const authenticatedAdmin = (req, res, next) => {
-  if (helpers.getUser(req)) {
-    if (helpers.getUser(req).role === 'admin') { return next() }
-    return res.json({ status: 'error', message: '非管理者沒有權限登入後台！' })
-  } else {
-    return res.json({ status: 'error', message: '未通過身份驗證！' })
-  }
-}
-// 驗證登入者是否為非管理者=>用於前台路由
-const authenticatedNotAdmin = (req, res, next) => {
-  if (helpers.getUser(req)) {
-    if (helpers.getUser(req).role !== 'admin') { return next() }
-    return res.json({ status: 'error', message: '管理者沒有權限登入前台！' })
-  } else {
-    return res.json({ status: 'error', message: '未通過身份驗證！' })
-  }
-}
 
 // current user
 router.get('/current_user', authenticated, userController.getCurrentUser)
