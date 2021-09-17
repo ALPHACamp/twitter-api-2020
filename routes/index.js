8dff2bc83d8fa7a64a6c311743c09f57e88c4349
Whaleep <billy_1817@yahoo.com.tw>
2021-09-18 01:54:56 +0800

    add authenticated User & Admin

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,13 @@ const user = require('./user')
 const tweet = require('./tweet')
 const followship = require('./followship')
 const admin = require('./admin')
-const { authenticated } = require('../middlewares/auth')
+const { authenticated, authenticatedUser, authenticatedAdmin } = require('../middlewares/auth')
 const userController = require('../controllers/userController')
 
 module.exports = (app) => {
   app.use('/api/users', user)
   app.post('/api/login', userController.login)
-  app.use('/api/tweets', authenticated, tweet)
-  app.use('/api/followships', authenticated, followship)
-  app.use('/api/admin', authenticated, admin)
+  app.use('/api/tweets', authenticated, authenticatedUser, tweet)
+  app.use('/api/followships', authenticated, authenticatedUser, followship)
+  app.use('/api/admin', authenticated, authenticatedAdmin, admin)
 }
