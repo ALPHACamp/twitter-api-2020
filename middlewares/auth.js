5702358eb7b69bfab866e463496c9ebdcd9fc4af
JHIH-LEI <vicky88888@gmail.com>
2021-09-19 17:25:12 +0800

    feat: modify authenticatedUser & admin status code & message

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -15,18 +15,18 @@ const authenticated = (req, res, next) => {
 const authenticatedUser = (req, res, next) => {
   if (req.user) {
     if (req.user.role === 'user') { return next() }
-    return res.json({ status: 'error', message: 'permission denied' })
+    return res.status(403).json({ message: '管理員不可登入前台' })
   } else {
-    return res.json({ status: 'error', message: 'permission denied' })
+    return res.status(401).json({ message: '請先登入在使用' })
   }
 }
 
 const authenticatedAdmin = (req, res, next) => {
   if (helpers.getUser(req)) {
     if (helpers.getUser(req).role === 'admin') { return next() }
-    return res.json({ status: 'error', message: 'permission denied' })
+    return res.status(403).json({ message: '普通用戶不可登入後台' })
   } else {
-    return res.json({ status: 'error', message: 'permission denied' })
+    return res.status(401).json({ message: '請先登入在使用' })
   }
 }
 
