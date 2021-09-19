316e03adc6ba28d4ace825c10ac421e3bcf77c59
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-19 12:57:09 +0800

    refactor: Remove middlewares/auth.js useless require

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,7 @@
-const jwt = require('jsonwebtoken')
-const { User } = require('../models')
 const passport = require('../config/passport')
 const helpers = require('../_helpers')
 const ApiError = require('../utils/customError')
 
-// use helpers.getUser(req) to replace req.user
 const authenticated = (req, res, next) =>
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) {
@@ -15,7 +12,7 @@ const authenticated = (req, res, next) =>
     return next()
   })(req, res, next)
 
-// authenticatedRole('user') to verify req is user
+// authenticatedRole() to verify req is user
 // authenticatedRole('admin') to verify req is admin
 const authenticatedRole = (role = 'user') => {
   return (req, res, next) => {
