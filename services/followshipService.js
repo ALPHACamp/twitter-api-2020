4185dc17f8b24a6a9c0a7e73a526345513fce90f
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-19 13:03:01 +0800

    sytle: Add FIXME into followshipService

--- services/followshipService.js
+++ services/followshipService.js
@@ -1,8 +1,8 @@
 const { Followship } = require('../models')
 const ApiError = require('../utils/customError')
-
 const helpers = require('../_helpers')
 
+// FIXME: Move the verification of addFollowing to the controller?
 const followshipService = {
   addFollowing: async (req, res) => {
     const followerId = Number(helpers.getUser(req).id)
@@ -15,11 +15,14 @@ const followshipService = {
     const [_, isCreated] = await Followship.findOrCreate({
       where: { followerId, followingId }
     })
+
     if (!isCreated) {
       throw new ApiError('addFollowingError', 400, 'You have followed')
     }
+
     return { status: 'success', message: 'Followed successfully' }
   },
+
   deleteFollowing: async (req, res) => {
     await Followship.destroy({
       where: {
