6f547c72e782a510670f68651e8084f5e25348ad
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-20 20:44:44 +0800

    feat: delete console.log

--- services/userService.js
+++ services/userService.js
@@ -113,7 +113,6 @@ const userService = {
 
     if (body.deleteCover) {
       await user.update({ cover: 'https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png' })
-      console.log(user)
     }
 
     if (files) {
