82e8383cdbbe3e967436bee0f78b755226f94cd0
haru5386 <snsn550@gmail.com>
2021-09-21 21:54:54 +0800

    cors

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,11 @@ const users = require('./apis/users')
 const admin = require('./apis/admin')
 const tweets = require('./apis/tweets')
 const followships = require('./apis/followships')
-const cors = require('cors')
 
 
 module.exports = app => {
-  app.use('/api/users', cors(), users)
-  app.use('/api/admin', cors(), admin)
-  app.use('/api/tweets', cors(), tweets)
-  app.use('/api/followships', cors(), followships)
+  app.use('/api/users',  users)
+  app.use('/api/admin',  admin)
+  app.use('/api/tweets',tweets)
+  app.use('/api/followships',  followships)
 }
