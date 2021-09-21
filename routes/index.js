e972d4ffa21550b3f1b8deedbc7bfb3eb83d518c
haru5386 <snsn550@gmail.com>
2021-09-21 21:48:42 +0800

    revise: cors

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,12 @@ const users = require('./apis/users')
 const admin = require('./apis/admin')
 const tweets = require('./apis/tweets')
 const followships = require('./apis/followships')
+const cors = require('cors')
 
-// const authenticated...
-// TODO:登入權限還未加入
 
 module.exports = app => {
-  app.use('/api/users', users)
-  app.use('/api/admin', admin)
-  app.use('/api/tweets', tweets)
-  app.use('/api/followships', followships)
+  app.use('/api/users', cors(), users)
+  app.use('/api/admin', cors(), admin)
+  app.use('/api/tweets', cors(), tweets)
+  app.use('/api/followships', cors(), followships)
 }
