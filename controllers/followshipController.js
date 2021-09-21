9b10043cfdb94df1e9ff9388bf53d574a3c1e654
Lucas-Peng-CS <cs20210310@gmail.com>
2021-09-22 00:21:59 +0800

    refactor add following api

--- controllers/followshipController.js
+++ controllers/followshipController.js
@@ -1,16 +1,32 @@
 const followshipService = require('../services/followshipService.js')
+const helpers = require('../_helpers')
 
 const followshipController = {
-  addFollowing: (req, res) => {
-    followshipService.addFollowing(req, res, (status, data) => {
-      return res.status(status).json(data)
-    })
+  addFollowing: async (req, res, next) => {
+    const followingId = Number(req.body.id)
+    const followerId = Number(helpers.getUser(req).id)
+
+    if (followingId === followerId) {
+      return res.status(403).json({
+        status: 'error',
+        message: 'Cannot follow yourself'
+      })
+    }
+    try {
+      const { status, message } = await followshipService.addFollowing(followingId, followerId)
+      return res.status(200).json({
+        status,
+        message
+      })
+    } catch (error) {
+      next(error)
+    }
   },
   removeFollowing: (req, res) => {
     followshipService.removeFollowing(req, res, (status, data) => {
       return res.status(status).json(data)
     })
-  },
+  }
 }
 
 module.exports = followshipController
--- services/followshipService.js
+++ services/followshipService.js
@@ -1,39 +1,28 @@
 const { Followship, User, Sequelize } = require('../models')
-const helpers = require('../_helpers')
+const apiError = require('../libs/apiError')
 
 const followshipService = {
-  addFollowing: async (req, res, callback) => {
-    const followingId = Number(req.body.id)
-    const followerId = Number(helpers.getUser(req).id)
-    if (followingId === followerId) {
-      return callback(400, { status: 'error', message: 'cannot follow yourself' })
+  addFollowing: async (followingId, followerId) => {
+    const user = await User.findByPk(followingId, { raw: true })
+    if (!user) {
+      throw apiError.badRequest(404, "User doesn't exist")
     }
 
-    try {
-      const user = await User.findByPk(followingId, { raw: true })
-      if (!user) {
-        return callback(404, { status: 'error', message: "user doesn't exist" })
-      }
+    if (user.role === 'admin') {
+      throw apiError.badRequest(403, 'Cannot follow administrator')
+    }
 
-      if (user.role === 'admin') {
-        return callback(400, { status: 'error', message: 'cannot follow administrator' })
-      }
+    const [_, isFollowed] = await Followship.findOrCreate({
+      where: { followerId, followingId }
+    })
 
-      const followingsList = await User.findByPk(followerId, { include: { model: User, as: 'Followings' }, attributes: [] })
-      const isFollowed = followingsList.Followings.find(following => following.dataValues.id === followingId) !== undefined
+    if (!isFollowed) {
+      throw apiError.badRequest(403, 'You have already followed this user')
+    }
 
-      if (isFollowed) {
-        return callback(400, { status: 'error', message: 'you have already followed this user' })
-      }
-
-      await Followship.create({
-        followerId,
-        followingId
-      })
-      callback(200, { status: 'success', message: 'add following successfully' })
-    } catch (err) {
-      console.log('addFollowing error', err)
-      res.sendStatus(500)
+    return {
+      status: 'success',
+      message: 'Add following successfully'
     }
   },
   removeFollowing: async (req, res, callback) => {
