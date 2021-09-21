a2be982f89ae809c5e2ae623e2923afbace05c2f
JHIH-LEI <vicky88888@gmail.com>
2021-09-22 04:36:16 +0800

    feat: init socket.io & hbs(for test chat)

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
+const exphbs = require('express-handlebars')
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-// use helpers.getUser(req) to replace req.user
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
+// 測試聊天室用的暫時路由
+app.get('/', (req, res) => {
+  res.render('index')
+})
 require('./routes')(app)
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+require('./server')(io)
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
