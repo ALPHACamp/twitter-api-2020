cc66cef9168a3f7ccf991a0a8db5b0c2017826a0
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-23 15:53:47 +0800

    feat: Set up /api/messages route path

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const users = require('./modules/users')
 const tweets = require('./modules/tweets')
+const messages = require('./modules/messages')
 const followships = require('./modules/followships')
 const { authenticated, authenticatedRole } = require('../middlewares/auth')
 
@@ -10,5 +11,6 @@ router.use('/api/tweets', authenticated, authenticatedRole(), tweets)
 router.use('/api/followships', authenticated, authenticatedRole(), followships)
 router.use('/api/users', users)
 router.use('/api/admin', admin)
+router.use('/api/messages', messages)
 
 module.exports = router
---
+++ routes/modules/messages.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const router = express.Router()
+const messageController = require('../../controllers/messageController')
+
+module.exports = router
