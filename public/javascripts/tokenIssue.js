eab15a6845698361c2ef3574879cf0f2ee5ce303
rexlin <rexlin6245@gmail.com>
2021-09-23 14:55:39 -0700

    adjust secrets for AC online testing

--- public/javascripts/tokenIssue.js
+++ public/javascripts/tokenIssue.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 module.exports = (user) => {
-  const TOKEN_KEY = user.role === 'user' ? process.env.USER_KEY : process.env.ADMIN_KEY
+  const TOKEN_KEY = user.role === 'user' ? 'process.env.USER_KEY' : 'process.env.ADMIN_KEY'
   const id = user.id
   const payload = {
     sub: id,
