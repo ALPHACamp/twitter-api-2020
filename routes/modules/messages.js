39fa96bcf2a00ffd892d256039177ab362744fb2
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-23 21:20:48 +0800

    feat: Add /api/messages/:RoomId route path

--- routes/modules/messages.js
+++ routes/modules/messages.js
@@ -2,4 +2,7 @@ const express = require('express')
 const router = express.Router()
 const messageController = require('../../controllers/messageController')
 
+router.get('/:RoomId', messageController.getMessages)
+
+
 module.exports = router
