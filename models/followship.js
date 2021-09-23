aaed539a6b3e843a5c9c28845c821b50d5adfa7c
JHIH-LEI <vicky88888@gmail.com>
2021-09-24 06:11:27 +0800

    feat: modify reply & followship model

--- models/followship.js
+++ models/followship.js
@@ -1,8 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Followship = sequelize.define('Followship', {
-    followerId: DataTypes.INTEGER,
-    followingId: DataTypes.INTEGER
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    followerId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    followingId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
   }, {});
   Followship.associate = function (models) {
   };
