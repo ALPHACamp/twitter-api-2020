004968e4373cc03b8427380888e62c14d8211e37
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-23 21:18:19 +0800

    feat: Add messageController.getPrivateRooms to handle private rooms

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -12,6 +12,22 @@ const messageController = {
     } catch (error) {
       next(error)
     }
+  },
+
+  getPrivateRooms: async (req, res, next) => {
+    try {
+      const [targetUserId, currentUserId] = [
+        req.params.id,
+        helpers.getUser(req).id
+      ]
+      const rooms = await messageService.getPrivateRooms(
+        targetUserId,
+        currentUserId
+      )
+      return res.status(200).json(rooms)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
