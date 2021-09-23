3c885f72b20917eab97c11306098cf1b405efeee
WilliamTsou818 <cccc1589@gmail.com>
2021-09-23 17:01:33 +0800

    feat: add welcome message to joinPublicRoom for current user

--- socket/events/publicRooms.js
+++ socket/events/publicRooms.js
@@ -2,15 +2,25 @@ module.exports = (io, socket, publicUsers) => {
   const { name, id } = socket.user
   socket.on('joinPublicRoom', async () => {
     try {
+      // TODO : add chatService for getting public room history message 
+
+      // Add current user to public user list
       const isUserExists = publicUsers
         .map((user) => user.id)
         .includes(socket.user.id)
       isUserExists ? false : publicUsers.push(socket.user)
       console.log(publicUsers)
-
+      
+      // Send announce to other public room users
       socket.broadcast.emit('announce', {
         publicUsers,
         message: `${name} joined`
+      })
+      
+      // Send welcome message to current user
+      socket.emit('announce', {
+        publicUsers,
+        message: `Welcome, ${name}!`
       })
     } catch (error) {
       return socket.emit('error', {
@@ -37,8 +47,10 @@ module.exports = (io, socket, publicUsers) => {
 
   socket.on('leavePublicRoom', async () => {
     try {
+      // Remove current user from public user list
       publicUsers.splice(publicUsers.indexOf(socket.user), 1)
       io.emit('totalUser', publicUsers)
+
       return socket.broadcast.emit('announce', {
         publicUsers,
         message: `${name} leaved`
