343f9f741370f36b71fe759efb94769ea6464abc
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-23 21:14:34 +0800

    feat: Add messageController.getMessages to handle public and private message

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,6 +1,18 @@
 const messageService = require('../services/messageService')
 const ApiError = require('../utils/customError')
 
-const messageController = {}
+const messageController = {
+  // publicRoom RoomID can be null or defaultValue
+  // privateRoom need have RoomID
+  getMessages: async (req, res, next) => {
+    try {
+      const { RoomId } = req.params.RoomId
+      const messages = await messageService.getMessages(RoomId)
+      return res.status(200).json(messages)
+    } catch (error) {
+      next(error)
+    }
+  }
+}
 
 module.exports = messageController
