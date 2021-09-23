483e1e972153bffd38e7a01707efe9c812f0a610
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-23 21:12:33 +0800

    feat: Add messageService.postMessage to save message into database

--- services/messageService.js
+++ services/messageService.js
@@ -1,3 +1,19 @@
-const messageService = {}
+const { Message, Room, Member, User } = require('../models')
+
+const messageService = {
+  postMessage: async (message) => {
+    const { UserId, RoomId, content } = message
+    await Message.create({
+      UserId,
+      RoomId,
+      content
+    })
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'A message has created'
+    })
+  }
+}
 
 module.exports = messageService
