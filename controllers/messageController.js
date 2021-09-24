4e0096c5aa11dd82cfcc8b6ad7df5ab83f67f4ba
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-24 12:11:09 +0800

    fix: Fix controllers/messageController not require helper

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,12 +1,13 @@
 const messageService = require('../services/messageService')
 const ApiError = require('../utils/customError')
+const helpers = require('../_helpers')
 
 const messageController = {
   // publicRoom RoomID can be null or defaultValue
   // privateRoom need have RoomID
   getMessages: async (req, res, next) => {
     try {
-      const { RoomId } = req.params.RoomId
+      const RoomId = req.params.RoomId
       const messages = await messageService.getMessages(RoomId)
       return res.status(200).json(messages)
     } catch (error) {
@@ -20,6 +21,7 @@ const messageController = {
         req.params.id,
         helpers.getUser(req).id
       ]
+
       const rooms = await messageService.getPrivateRooms(
         targetUserId,
         currentUserId
