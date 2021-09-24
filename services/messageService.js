0a84397fddc3eb1d5e5efeed0998f40af5a09971
Avery Yang(楊長珈) <avery_yang@asus.com>
2021-09-24 22:29:31 +0800

    feat: Modify messageService.postMessage to return message

--- services/messageService.js
+++ services/messageService.js
@@ -3,15 +3,10 @@ const { Message, Room, Member, User } = require('../models')
 const messageService = {
   postMessage: async (message) => {
     const { UserId, RoomId, content } = message
-    await Message.create({
+    return await Message.create({
       UserId,
       RoomId,
       content
-    })
-
-    return res.status(200).json({
-      status: 'success',
-      message: 'A message has created'
     })
   },
 
