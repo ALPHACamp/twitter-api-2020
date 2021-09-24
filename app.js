909b87bc75d3f7432587efda1f699584f44a989c
BoboLin <bobolincowo@gmail.com>
2021-09-24 22:43:57 +0800

    install socket.io and try connection

--- app.js
+++ app.js
@@ -14,8 +14,37 @@ app.use(bodyParser.json())
 app.use(passport.initialize())
 app.use(cors())
 
+// socket
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+app.get('/chat', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  // 一連線後的廣播
+  // socket.emit('hi everyone')
+  io.emit('welcome message', `welcome to this room`)
+  console.log('a user connected')
+  
+  // 接收 client 端
+  socket.on('giveServer', function (message) {
+    console.log(message)
+  })
+  // 監聽
+  socket.on('chat message', (msg) => {
+    // 廣播收到的訊息給大家
+    io.emit('chat message', msg)
+    console.log('message: ' + msg)
+  })
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+server.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
 require('./routes')(app)
 
