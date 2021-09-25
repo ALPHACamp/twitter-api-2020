cb4033a152fc2dc0f727dffe29a5db76783afd78
BoboLin <bobolincowo@gmail.com>
2021-09-25 16:08:58 +0800

    socket: add jwt token auth

--- utils/socketServer.js
+++ utils/socketServer.js
@@ -1,21 +1,40 @@
+const jwt = require('jsonwebtoken')
 const db = require('../models')
 const Chat = db.Chat
+const User = db.User
+const onlineUser = []
 
 module.exports = async (io) => {
-  // 從資料庫抓取歷史訊息
-  const record = await Chat.findAll({
-    raw: true,
-    nest: true,
-    order: [
-      ['createdAt', 'ASC']
-    ]
-  })
-  io.on('connection', (socket) => {
-    // 一連線後的廣播
-    // socket.emit('hi everyone')
-    io.emit('welcome message', `welcome to this room`)
-    console.log('a user connected')
-
+  // user token 驗證
+  io.use(async (socket, next) => {
+    const token = socket.handshake.query.token
+    if (!token) { return }
+    else {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      let user = await User.findByPk(decoded.id, {
+        raw: true, nest: true,
+        attributes: ['id', 'name', 'account', 'avatar']
+      })
+      onlineUser.push(user)
+      next()
+    }
+  }).on('connection', (socket) => {
+    socket.on('joinRoom', async () => {
+      console.log('user connected 有人進來了')
+      // 從資料庫抓取歷史訊息
+      const record = await Chat.findAll({
+        raw: true,
+        nest: true,
+        order: [['createdAt', 'ASC']],
+      })
+      // 載入歷史訊息
+      socket.emit('allMsg', record)
+      // 一連線後的廣播
+      io.emit('welcome message', '歡迎來到公開聊天室！')
+      // 丟 onlineUser 給前端
+      io.emit('onlineUser', onlineUser)
+    })
+    
     // 接收 client 端
     socket.on('giveServer', function (message) {
       console.log(message)
@@ -23,14 +42,12 @@ module.exports = async (io) => {
     socket.on('userPro', function (message) {
       console.log(message)
     })
-    // 載入歷史訊息
-    socket.emit('allMsg', record)
-
+    
     // 監聽
     socket.on('chat message', (msg) => {
       Chat.create({
         UserId: msg.UserId,
-        message: msg.message
+        message: msg.message,
       })
       // 廣播收到的訊息給大家
       io.emit('chat message', msg)
