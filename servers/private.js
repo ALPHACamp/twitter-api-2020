3c34c0633c09af9748e3817e2f3fbaa5de1eff5a
JHIH-LEI <vicky88888@gmail.com>
2021-09-25 18:26:15 +0800

    feat: modify chatList data structure

--- servers/private.js
+++ servers/private.js
@@ -25,9 +25,9 @@ module.exports = (io, socket, loginUser) => {
 
       io.to(room.id).emit('join room success', room.id)
       // TODO:更新聊天紀錄人員列表，回傳的使用者資料排除登入使用者的，只回傳對方資料
-      const chatList = await Room.findAll({
+      let chatList = await Room.findAll({
         raw: true, nest: true,
-        attributes: ['id'],
+        attributes: [],
         where: {
           [sequelize.Op.or]: [
             { joinerId: loginUser.id },
@@ -45,7 +45,20 @@ module.exports = (io, socket, loginUser) => {
           attributes: ['id', 'account', 'avatar', 'name']
         }
         ]
+      }) //TODO:根據聊天紀錄排序，越新的越上面，或是說先排沒已讀的在最上，在根據時間排序
+      // 只回傳聊天對象的個人資料。 [{user: {個人資料}}, {user: {個人資料}}]
+      chatList.forEach(data => {
+        if (data.Creator.id === loginUser.id) {
+          delete data.Creator
+          // 沒被刪掉的就是聊天對象，將key改為user
+          delete Object.assign(data, { ['user']: data['Joiner'] })['Joiner']
+        } else {
+          // 如果A不是登入者，就是聊天對象，此時刪掉B，並將key(A)改為user
+          delete data.Joiner
+          delete Object.assign(data, { ['user']: data['Creator'] })['Creator']
+        }
       })
+      console.log(chatList)
       io.to(room.id).emit('chat member list', chatList)
 
       // 將該房間傳給我的訊息改為已讀
