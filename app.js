d2f531516d937cf3855c4181f76543ad9ab245b4
haru5386 <snsn550@gmail.com>
2021-09-25 14:00:25 +0800

    feat: chat init

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const helpers = require('./_helpers')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const exphbs = require('express-handlebars')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -13,6 +14,12 @@ const passport = require('./config/passport')
 
 const port = process.env.PORT || 3000
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -21,6 +28,36 @@ app.use(passport.initialize())
 app.use('/upload', express.static(__dirname + '/upload'))
 require('./routes')(app)
 
+let onlineCount = 0;
+
+// 修改 connection 事件
+io.on('connection', (socket) => {
+  onlineCount++;
+  io.emit("online", onlineCount);
+
+  socket.on("greet", () => {
+    socket.emit("greet", onlineCount)
+  })
+
+  socket.on('disconnect', () => {
+    onlineCount = (onlineCount < 0) ? 0 : onlineCount -= 1;
+    io.emit("online", onlineCount)
+  })
+
+  socket.on("send", (msg) => {
+    io.emit("msg", msg)
+  })
+
+  socket.on("userin", (msg) => {
+    io.emit("userin", msg)
+  })
+
+  socket.on("userout", (msg) => {
+    io.emit("userout", msg)
+  })
+})
+
+
 app.use((err, req, res, next) => {
   res.status(422).json({
     status: 'error',
@@ -28,5 +65,5 @@ app.use((err, req, res, next) => {
   })
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
 module.exports = app
