19f27a04b659e44a849083308e2d6f4d28be2d5e
JHIH-LEI <vicky88888@gmail.com>
2021-09-26 21:53:13 +0800

    feat: add subscribeship associate between User in order to get subscriber data

--- models/subscribeship.js
+++ models/subscribeship.js
@@ -10,7 +10,11 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Subscribeship.belongsTo(models.User, {
+        through: 'Subscribeship',
+        foreignKey: 'subscriberId',
+        as: 'Subscriber',
+      })
     }
   };
   Subscribeship.init({
