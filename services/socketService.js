fbd4a38f3772ac26f0d99b40ef1560fd472310a3
NokoDOjo <81893207+NokoDOjo@users.noreply.github.com>
2021-09-26 15:46:56 +0800

    fix: fix heroku crashed

--- services/socketService.js
+++ services/socketService.js
@@ -44,11 +44,11 @@ const socketService = {
     let result = data.filter(i => !set.has(i.roomId)?set.add(i.roomId):false)
     result.forEach( i => {
       if (i.UserId === UserId) {
-        i.user = await User.findByPk(i.receiverId, {
+        i.user = User.findByPk(i.receiverId, {
           attributes: ['id', 'name', 'account', 'avatar']
         })
       }
-      i.user = await User.findByPk(i.UserId, {
+      i.user = User.findByPk(i.UserId, {
         attributes: ['id', 'name', 'account', 'avatar'],
       })
     })
