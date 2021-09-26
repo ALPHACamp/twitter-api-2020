4a98fe22e8c8e84e2071eb5106df86cbb65b97c8
WilliamTsou818 <cccc1589@gmail.com>
2021-09-26 11:52:04 +0800

    feat: store lastLogin value to socket.user

--- migrations/20210926033357-add-lastLogin-to-user.js
+++ migrations/20210926033357-add-lastLogin-to-user.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -12,4 +12,4 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Users', 'lastLogin')
   }
-};
+}
