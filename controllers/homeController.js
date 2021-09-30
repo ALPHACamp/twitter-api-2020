55e797e4c16f8c46e7168f18d4aa2d5372cd3a85
rexlin <rexlin6245@gmail.com>
2021-09-29 21:07:00 -0700

    prepare function test

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -37,7 +37,7 @@ const homeController = {
   
   postSignUp: async (req, res) => {
     const userData = req.body
-    console.log("🚀 ~ file: homeController.js ~ line 40 ~ postSignUp: ~ userData", userData.FormData)
+    console.log("🚀 ~ file: homeController.js ~ line 40 ~ postSignUp: ~ userData", userData)
     if (req.body.checkPassword !== req.body.password) {
       return res.redirect('/signup')
     }
