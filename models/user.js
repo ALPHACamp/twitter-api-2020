584ffe129eef676efda3ca2d298caee061131b20
haru5386 <snsn550@gmail.com>
2021-10-02 16:26:28 +0800

    modify: user model id setting

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
