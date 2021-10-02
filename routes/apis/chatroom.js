0d16628212e8afa9842f26946c8d538c8e03738a
asterchang96@gmail.com <asterchang96@gmail.com>
2021-10-02 14:27:04 +0800

    feat: check push.

--- routes/apis/chatroom.js
+++ routes/apis/chatroom.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const chatroomController = require('../../controllers/chatroomController.js')
 
-
 router.get('/getHistoryMsg', chatroomController.getHistoryMsg)
 
 module.exports = router
