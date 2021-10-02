8c8381a029431dfc0e6849975e4c9aa5ef738eb5
rexlin <rexlin6245@gmail.com>
2021-10-01 21:29:38 -0700

    debug db setting

--- models/index.js
+++ models/index.js
@@ -10,10 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config, {define:{
-    charset: 'utf8',
-    collate: 'utf8_unicode_ci'
-  }});
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
