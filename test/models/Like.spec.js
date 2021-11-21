c00f69c045f19c733e54b7782c9659f541992e09
Amber Yan <freehourse2sh@gmail.com>
2021-11-21 13:54:33 +0800

    add comments on test/models

--- test/models/Like.spec.js
+++ test/models/Like.spec.js
@@ -7,7 +7,6 @@ const {
   sequelize,
   dataTypes,
   checkModelName,
-  checkUniqueIndex,
   checkPropertyExists
 } = require('sequelize-test-helpers')
 
@@ -15,54 +14,61 @@ const db = require('../../models')
 const LikeModel = require('../../models/like')
 
 describe('# Like Model', () => {
-  
-  before(done => {
-    done()
-  })
-
+  // 使用寫好的 Like Model
   const Like = LikeModel(sequelize, dataTypes)
+  // 創建 like instance 
   const like = new Like()
+  // 檢查 Model name
   checkModelName(Like)('Like')
 
+  // 檢查 like 是否有 ___ 屬性(由於希望學員可以彈性命名 model 欄位，因此這邊留空)
   context('properties', () => {
     ;[
     ].forEach(checkPropertyExists(like))
   })
 
+  // 檢查 like 的關聯是否正確
   context('associations', () => {
     const User = 'User'
     const Tweet = 'Tweet'
     before(() => {
+      // 將 Like model 對 User, Tweet 做關聯(呼叫 associate)
       Like.associate({ User })
       Like.associate({ Tweet })
     })
 
     it('should belong to user', (done) => {
+      // 檢查是否有呼叫 belongsTo(User)
       expect(Like.belongsTo).to.have.been.calledWith(User)
       done()
     })
     it('should belong to tweet', (done) => {
+      // 檢查是否有呼叫 belongsTo(Tweet)
       expect(Like.belongsTo).to.have.been.calledWith(Tweet)
       done()
     })
   })
 
+  // 檢查 model 的新增、修改、刪除、更新
   context('action', () => {
 
     let data = null
 
+    // 檢查 db.Like 是否真的可以新增一筆資料
     it('create', (done) => {
       db.Like.create({}).then((like) => {   
         data = like
         done()
       })
     })
+    // 檢查 db.Like 是否真的可以讀取一筆資料
     it('read', (done) => {
         db.Like.findByPk(data.id).then((like) => {  
           expect(data.id).to.be.equal(like.id)
           done()
         })
     })
+    // 檢查 db.Like 是否真的可以更新一筆資料
     it('update', (done) => {
       db.Like.update({}, { where: { id: data.id }}).then(() => {
         db.Like.findByPk(data.id).then((like) => { 
@@ -71,6 +77,7 @@ describe('# Like Model', () => {
         })
       })
     })
+    // 檢查 db.Like 是否真的可以刪除一筆資料 
     it('delete', (done) => {
       db.Like.destroy({ where: { id: data.id }}).then(() => {
         db.Like.findByPk(data.id).then((like) => { 
--- test/models/Reply.spec.js
+++ test/models/Reply.spec.js
@@ -7,7 +7,6 @@ const {
   sequelize,
   dataTypes,
   checkModelName,
-  checkUniqueIndex,
   checkPropertyExists
 } = require('sequelize-test-helpers')
 
@@ -15,54 +14,60 @@ const db = require('../../models')
 const ReplyModel = require('../../models/reply')
 
 describe('# Reply Model', () => {
-  
-  before(done => {
-    done()
-  })
-
+  // 使用寫好的 Reply Model
   const Reply = ReplyModel(sequelize, dataTypes)
+  // 創建 reply instance 
   const like = new Reply()
+  // 檢查 Model name
   checkModelName(Reply)('Reply')
 
+   // 檢查 reply 是否有 __ 屬性(由於希望學員可以彈性命名 model 欄位，因此這邊留空)
   context('properties', () => {
     ;[
     ].forEach(checkPropertyExists(like))
   })
 
+  // 檢查 reply 的關聯是否正確   
   context('associations', () => {
     const User = 'User'
     const Tweet = 'Tweet'
     before(() => {
+      // 將 Reply model 對 User, Tweet 做關聯(呼叫 associate)
       Reply.associate({ User })
       Reply.associate({ Tweet })
     })
 
     it('should belong to user', (done) => {
+      // 檢查是否有呼叫 belongsTo(User)
       expect(Reply.belongsTo).to.have.been.calledWith(User)
       done()
     })
     it('should belong to tweet', (done) => {
+      // 檢查是否有呼叫 belongsTo(Tweet)
       expect(Reply.belongsTo).to.have.been.calledWith(Tweet)
       done()
     })
   })
 
+  // 檢查 model 的新增、修改、刪除、更新
   context('action', () => {
 
     let data = null
-
+    // 檢查 db.Reply 是否真的可以新增一筆資料
     it('create', (done) => {
       db.Reply.create({}).then((reply) => {   
         data = reply
         done()
       })
     })
+    // 檢查 db.Reply 是否真的可以讀取一筆資料
     it('read', (done) => {
       db.Reply.findByPk(data.id).then((reply) => {  
         expect(data.id).to.be.equal(reply.id)
           done()
         })
     })
+    // 檢查 db.Reply 是否真的可以更新一筆資料
     it('update', (done) => {
       db.Reply.update({}, { where: { id: data.id }}).then(() => {
         db.Reply.findByPk(data.id).then((reply) => { 
@@ -71,6 +76,7 @@ describe('# Reply Model', () => {
         })
       })
     })
+    // 檢查 db.Reply 是否真的可以刪除一筆資料
     it('delete', (done) => {
       db.Reply.destroy({ where: { id: data.id }}).then(() => {
         db.Reply.findByPk(data.id).then((reply) => { 
