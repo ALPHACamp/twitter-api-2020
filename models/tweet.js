634ef9a82597edcbea479798f5f57b2b4284feae
heidichen90 <toddypet@gmail.com>
2021-11-28 23:16:30 +0000

    add model set up

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,17 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-  }, {});
-  Tweet.associate = function(models) {
-  };
-  return Tweet;
-};
+  const Tweet = sequelize.define(
+    'Tweet',
+    {
+      description: DataTypes.STRING,
+      UserId: DataTypes.INTEGER
+    },
+    {}
+  )
+  Tweet.associate = function (models) {
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
+    Tweet.belongsTo(models.User)
+  }
+  return Tweet
+}
