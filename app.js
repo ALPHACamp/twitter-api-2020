ecdd8e6736ec02a3e35c077f90434c7178d51ef3
CHLin13 <lch132502@gmail.com>
2021-11-29 20:53:54 +0800

    20211129 api signup and signin feature by CH

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
+const session = require('express-session')
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+
 const helpers = require('./_helpers');
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
 
-// use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
-  // passport.authenticate('jwt', { ses...
-};
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(__dirname + '/upload'))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.localUser = helpers.getUser(req)
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.error = req.flash('error')
+  next()
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+require('./routes')(app)
 
 module.exports = app
--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,64 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+const Tweet = db.Tweet
 
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (!user) {
+        return done(null, false, { message: '帳號或密碼輸入錯誤' })
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        return done(null, false, { message: '帳號或密碼輸入錯誤' })
+      }
+      return done(null, user)
+    })
+}))
 
+passport.serializeUser((user, done) => {
+  return done(null, user.id)
+})
+passport.deserializeUser((id, done) => {
+  User.findByPk(id, {
+    include: [
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+    .then(user => {
+      user = user.toJSON()
+      return done(null, user)
+    })
+    .catch(err => done(err, null))
+})
+
+//jwt
+const passportJwt = require('passport-jwt')
+const ExtractJwt = passportJwt.ExtractJwt
+const JwtStrategy = passportJwt.Strategy
+
+const opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.JWT_SECRET
+passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+  User.findByPk(jwt_payload.id, {
+    include: [
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  }).then(user => {
+    if (!user) {
+      return done(null, false)
+    }
+    return done(null, user)
+  })
+}));
 
 module.exports = passport
