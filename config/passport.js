ee075392b8e114d1a53ac846e2418ffae1e6f14e
CHLin13 <lch132502@gmail.com>
2021-11-30 01:28:30 +0800

    20211130 remove uselesslines by CH

--- config/passport.js
+++ config/passport.js
@@ -3,45 +3,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcryptjs')
-const db = require('../models')
-const User = db.User
-
-passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-  User.findOne({ where: { email } })
-    .then(user => {
-      if (!user) {
-        return done(null, false, { message: '帳號或密碼輸入錯誤' })
-      }
-      if (!bcrypt.compareSync(password, user.password)) {
-        return done(null, false, { message: '帳號或密碼輸入錯誤' })
-      }
-      return done(null, user)
-    })
-}))
-
-passport.serializeUser((user, done) => {
-  return done(null, user.id)
-})
-passport.deserializeUser((id, done) => {
-  User.findByPk(id, {
-    include: [
-      { model: User, as: 'Followers' },
-      { model: User, as: 'Followings' }
-    ]
-  })
-    .then(user => {
-      user = user.toJSON()
-      return done(null, user)
-    })
-    .catch(err => done(err, null))
-})
-
-//jwt
 const passportJwt = require('passport-jwt')
 const ExtractJwt = passportJwt.ExtractJwt
 const JwtStrategy = passportJwt.Strategy
+
+const db = require('../models')
+const User = db.User
 
 const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
