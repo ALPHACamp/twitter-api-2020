41aa5ba1c48bfddd1ba7d13690201ec1a398fa42
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-01 02:10:45 +0800

    generate tweet model

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
+    description: DataTypes.TEXT,
+    UserId: DataTypes.INTEGER,
   }, {});
-  Tweet.associate = function(models) {
+  Tweet.associate = function (models) {
+    Tweet.belongsTo(models.User)
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
+    // 待確認是否需要LikedUsers 或增加Tweet欄位likesCount
+    // Tweet.belongsToMany(models.User, { 
+    //   through: models.Like,
+    //   foreignKey: 'TweetId',
+    //   as: 'LikedUsers'
+    // })
   };
   return Tweet;
 };
