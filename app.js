20a9048bc52fc5a4bf7f8112e5a13705ee85c428
rayray1010 <da866810@gmail.com>
2021-11-30 17:46:43 +0800

    refactor: 調整 model

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
 
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
+function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
-};
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,18 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-  }, {});
-  Tweet.associate = function(models) {
-  };
-  return Tweet;
-};
+  const Tweet = sequelize.define(
+    'Tweet',
+    {
+      description: DataTypes.TEXT,
+      UserId: DataTypes.INTEGER
+    },
+    {}
+  )
+  Tweet.associate = function (models) {
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
+    Tweet.belongsTo(models.User)
+  }
+
+  return Tweet
+}
