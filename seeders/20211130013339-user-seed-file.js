8a7e359839224c6f83c6c32ce2a328e005dcd62f
heidichen90 <toddypet@gmail.com>
2021-11-30 20:55:37 +0000

    fix style

--- seeders/20211130013339-user-seed-file.js
+++ seeders/20211130013339-user-seed-file.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const mockAdmin = {
       id: 1,
-      account: "root",
-      email: "root@example.com",
-      password: "12345678",
-      name: "root",
-      role: "admin",
+      account: 'root',
+      email: 'root@example.com',
+      password: '12345678',
+      name: 'root',
+      role: 'admin',
       createdAt: new Date(),
-      updatedAt: new Date(),
-    };
+      updatedAt: new Date()
+    }
     const mockUsers = Array.from({ length: 5 }).map((d, i) => ({
       id: Number(i + 2),
       account: `user${i + 2}`,
       email: `user${i + 2}@example.com`,
       password: `${i + 2}`,
       name: `user${i + 2}`,
-      role: "user",
+      role: 'user',
       createdAt: new Date(),
-      updatedAt: new Date(),
-    }));
-    await queryInterface.bulkInsert("Users", [mockAdmin, ...mockUsers]);
+      updatedAt: new Date()
+    }))
+    await queryInterface.bulkInsert('Users', [mockAdmin, ...mockUsers])
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("Users", null, {});
-  },
-};
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
--- seeders/20211130015237-tweet-seed-file.js
+++ seeders/20211130015237-tweet-seed-file.js
@@ -1,9 +1,9 @@
-"use strict";
-const faker = require("faker");
+'use strict'
+const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const mockTweets = [];
+    const mockTweets = []
 
     for (let i = 0; i < 5; i++) {
       for (let j = 1; j < 11; j++) {
@@ -12,15 +12,15 @@ module.exports = {
           description: faker.lorem.words(),
           createdAt: new Date(),
           updatedAt: new Date(),
-          UserId: i + 2,
-        };
-        mockTweets.push(tweet);
+          UserId: i + 2
+        }
+        mockTweets.push(tweet)
       }
     }
-    await queryInterface.bulkInsert("Tweets", mockTweets);
+    await queryInterface.bulkInsert('Tweets', mockTweets)
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("Tweets", null, {});
-  },
-};
+    await queryInterface.bulkDelete('Tweets', null, {})
+  }
+}
