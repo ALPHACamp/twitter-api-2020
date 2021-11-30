1e2320f7227c1c42faf52222894326ad584b0839
Kerwin <tkoleo84119@gmail.com>
2021-11-30 18:23:34 +0800

    feat: add reference in Users and Tweets & add account and cover in Users & add likeCounts and commentCounts in Tweets

---
+++ migrations/20190115071417-create-tweet.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tweets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Users', key: 'id' },
+        onDelete: 'cascade'
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      likeCounts: {
+        type: Sequelize.INTEGER
+      },
+      commentCounts: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tweets');
+  }
+};
