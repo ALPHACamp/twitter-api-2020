7ee576bffe317d76b21a3d35bdf17b2a877bedc4
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-01 04:29:37 +0800

    install bcryptjs and add user seed data

---
+++ seeders/20211130191757-users-seed-file.js
@@ -0,0 +1,49 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [
+      ...Array.from({ length: 5 }).map((d, i) => ({
+        email: `user${i}@example.com`,
+        password: bcrypt.hashSync('23456789', bcrypt.genSaltSync(10), null),
+        name: faker.name.findName(),
+        avatar: 'https://loremflickr.com/320/240/face',
+        account: faker.name.firstName(),
+        cover: 'https://loremflickr.com/1200/400/landscape',
+        introduction: faker.lorem.text(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {
+        email: 'root@example.com',
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        role: 'admin',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {})
+  }
+};
