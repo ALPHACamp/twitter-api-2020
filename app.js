0c54c8dae1486dfce28d9b1992e0a090e3489bba
william1993314 <william1993314@gmail.com>
2021-11-30 13:53:21 +0800

    Install pg and update production config

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const helpers = require('./_helpers');
 const passport = require('./config/passport')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
