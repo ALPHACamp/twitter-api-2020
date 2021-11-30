fee53814a3d5c9a98fbc507a15609282c07e253c
Emily81926 <emily81926@gmail.com>
2021-11-30 18:45:27 +0800

    feat: add definitions and associations to models

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
+    UserId: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
+    likeCounts: DataTypes.INTEGER,
+    commentCounts: DataTypes.INTEGER
   }, {});
   Tweet.associate = function(models) {
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
+    Tweet.belongsTo(models.User)
   };
   return Tweet;
 };
