3d69afe69217641da5e142feb685de72ef305b81
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-01 02:09:34 +0800

    generate followship, like, and reply models

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
+    comment: DataTypes.STRING,
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
   }, {});
-  Reply.associate = function(models) {
+  Reply.associate = function (models) {
+    Reply.belongsTo(models.User)
+    Reply.belongsTo(models.Tweet)
   };
   return Reply;
 };
