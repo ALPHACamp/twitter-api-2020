fa31921ce4ea83a2e61a17028904ace5ca7ee70f
heidichen90 <toddypet@gmail.com>
2021-11-30 20:51:37 +0000

    set up seeder file for user, tweet, reply

---
+++ seeders/20211130203519-reply-seed-file.js
@@ -0,0 +1,22 @@
+"use strict";
+const faker = require("faker");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "Replies",
+      Array.from({ length: 50 }).map((d, i) => ({
+        comment: faker.lorem.sentences(),
+        UserId: Math.floor(1 + Math.random() * 5) + 1,
+        TweetId: Math.floor(Math.random() * 49) + 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      })),
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("Replies", null, {});
+  },
+};
