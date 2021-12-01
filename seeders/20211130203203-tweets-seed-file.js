e0b03ea845bb95a877c2c8bf76bc46261ef8c694
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-01 17:38:34 +0800

    fix repeated descriptions and add recent days

--- seeders/20211130203203-tweets-seed-file.js
+++ seeders/20211130203203-tweets-seed-file.js
@@ -2,22 +2,19 @@
 const faker = require('faker')
 let userIdArray = []  // [{UserId: 1}, {UserId: 1}, ..., {UserId: 2}...]
 for (let i = 1; i <= 5; i ++) {
-  let oneUserIdArray = Array(10).fill({UserId: i})
+  let oneUserIdArray = Array(10).fill({ UserId: i })  //[{UserId: 1} * 10]
   userIdArray.push(...oneUserIdArray)
 }
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    // 方法一：每位user的tweet數量不一，但是faker產生內容正常
-    // return queryInterface.bulkInsert('Tweets', Array.from({ length: 50 }).map((d, i) => ({
-    // 方法二：每位user皆有10筆tweet，但是faker產生description, date，每位使用者都一樣
     return queryInterface.bulkInsert('Tweets', 
-    userIdArray.map((d, i) => (Object.assign(d, {
+    userIdArray.map((d, i) => ({
+      UserId: d.UserId,
       description: faker.lorem.sentence().slice(0, 140),
-      // UserId: Math.ceil(Math.random() * 5), //1,2,3,4,5隨機選取，不符合每位10筆的需求
-      createdAt: faker.date.recent(),
+      createdAt: faker.date.recent(30),
       updatedAt: new Date()
-      })))
+      }))
     )
   },
 
