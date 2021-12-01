66bea681d20cbf555accc3b9e8d7905cf01418e9
paulchnag0801 <x1232178987@gmail.com>
2021-12-02 02:24:35 +0800

    refactor: remove authenticatedAdmin

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,7 @@ const userController = require('../controllers/userController.js')
 const passport = require('../config/passport')
 const authenticated = passport.authenticate('jwt', { session: false })
 
-const authenticatedAdmin = (req, res, next) => {
-  if (req.user) {
-    if (req.user.isAdmin) {
-      return next()
-    }
-    return res.json({ status: 'error', message: 'permission denied' })
-  } else {
-    return res.json({ status: 'error', message: 'permission denied' })
-  }
-}
+
 
 
 module.exports = (app) => {
