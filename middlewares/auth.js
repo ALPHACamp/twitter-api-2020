40ddb88fd653926a4c30e7b8ee691ae60b449225
Kerwin <tkoleo84119@gmail.com>
2021-12-01 18:03:39 +0800

    feat: add middleware auth.js(use passport-jwt to authenticate user login status)

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+// 載入所需套件
+const passport = require('../config/passport')
+const helpers = require('../_helpers')
+
+module.exports = {
+  authenticated: (req, res, next) => {
+    passport.authenticate('jwt', { session: false }, (err, user, info) => {
+      if (err) { return next(err) }
+      if (!user) {
+        return res.status(401).json({ status: 'error', message: '帳號不存在！' })
+      }
+      req.user = user // 把user資料塞到req.user裡面
+      return next()
+    })(req, res, next)
+  },
+
+  checkNotAdmin: (req, res, next) => {
+    if (helpers.getUser(req).role === 'admin') {
+      return res.status(401).json({ status: 'error', message: '帳號不存在' })
+    }
+    return next()
+  },
+
+  checkNotUser: (req, res, next) => {
+    if (helpers.getUser(req).role === 'user') {
+      return res.status(401).json({ status: 'error', message: '帳號不存在' })
+    }
+    return next()
+  },
+}
