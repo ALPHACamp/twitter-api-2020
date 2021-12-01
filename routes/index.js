46def5ba7edd58b7841d9c6f5ccf8beb09101169
paulchnag0801 <x1232178987@gmail.com>
2021-12-02 00:03:38 +0800

    feat : create index.js/routes & signUp routes

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const userController = require('../controllers/userController.js')
+const passport = require('../config/passport')
+const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.isAdmin) {
+      return next()
+    }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
+  }
+}
+
+
+module.exports = (app) => {
+  // JWT signin & signup
+  app.post('/api/users', userController.signUp)
+}
