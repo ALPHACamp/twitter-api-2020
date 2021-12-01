bdc3916582255e5350cdbb92e5dc933bb97d22ce
cschang07 <manutdsfan@gmail.com>
2021-12-01 14:14:47 +0800

    Add admin tweet index page

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 const tweetController = require('../controllers/tweetController.js')
+const adminController = require('../controllers/adminController.js')
+
 module.exports = app => {
 
   //如果使用者訪問首頁，就導向 /tweets 的頁面
@@ -6,4 +8,12 @@ module.exports = app => {
 
   //在 /tweets 底下則交給 tweetController.getRestaurants 來處理
   app.get('/tweets', tweetController.getTweets)
+
+  // 連到 /admin 頁面就轉到 /admin/tweets
+  app.get('/admin', (req, res) => res.redirect('/admin/tweets'))
+
+  // 在 /admin/tweets 底下則交給 adminController.getTweets 處理
+  app.get('/admin/tweets', adminController.getTweets)
+
+  
 }
