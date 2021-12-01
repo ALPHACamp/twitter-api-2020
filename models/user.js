e920e31f8b14d7751329bb71c683c16bcf8e6d38
steven4program <stevenchang421@gmail.com>
2021-12-01 15:47:22 +0800

    add account attribute to User model

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define(
     'User',
     {
+      account: DataTypes.STRING,
       email: DataTypes.STRING,
       password: DataTypes.STRING,
       name: DataTypes.STRING,
