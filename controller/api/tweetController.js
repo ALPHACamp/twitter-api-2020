e35f70f76ac5518fcc776e1f53e1a36ae9143d9e
Tim <s87462830@gmail.com>
2021-12-02 00:46:25 +0800

    feat: modify tweetController.getTweet

--- controller/api/tweetController.js
+++ controller/api/tweetController.js
@@ -1,11 +1,16 @@
-const db = require('../../models')
-const Tweet = db.Tweet
 const helper = require('../../_helpers')
+const db = require('../../models')
+const sequelize = require('sequelize')
+const Tweet = db.Tweet
+const User = db.User
+const Like = db.Like
+const Reply = db.Reply
+
 const tweetController = {
   getTweets: async (req, res) => {
     try {
       const tweets = await Tweet.findAll({ raw: true, nest: true })
-      return res.json([...tweets, { status: 200, message: '' }])
+      return res.json({tweets, status: 200, message: '' })
     } catch (err) {
       return console.log(err)
     }
@@ -13,8 +18,20 @@ const tweetController = {
   getTweet: async (req, res) => {
     try {
       const id = req.params.id
-      const tweet = await Tweet.findByPk(id, { raw: true })
-      return res.json({ status: 200, message: '', ...tweet })
+      const tweet = await Tweet.findByPk(id, {
+        raw: true,
+        nest: true,
+        include: [
+          { model: User, attributes: ["id", "name", "account", "avatar"] },
+          { model: Like, attributes: [] },
+          { model: Reply, attributes: [] }
+        ],
+        attributes: ["description", "UserId", "createdAt",
+          [sequelize.fn('COUNT', sequelize.fn('DISTINCT', sequelize.col('likes.id'))), 'likes_count'], //新增欄位計算like
+          [sequelize.fn('COUNT', sequelize.fn('DISTINCT', sequelize.col('replies.id'))), 'replies_count'] //新增欄位計算reply
+        ]
+      })
+      return res.json({ tweet, status: 200, message: '' })
     } catch (err) {
       console.log(err)
     }
