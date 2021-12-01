9b18e878cea94962403b32ceafa1bd8ebe3351fb
Martin-no1tw <kljhger7728@gmail.com>
2021-12-01 20:01:09 +0800

    feat:add passport-local and bcrypt

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,38 @@
 const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
 
+passport.use(new LocalStrategy(
+  // customize user field
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  // authenticate user
+  (req, username, password, cb) => {
+    User.findOne({ where: { email: username } }).then(user => {
+      if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤'))
+      if (!bcrypt.compareSync(password, user.password)) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+      return cb(null, user)
+    })
+  }
+))
 
+// serialize and deserialize user
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id)
+    .then(user => {
+      user = user.toJSON()
+      return cb(null, user)
+    })
+})
+
+passport.use(strategy)
 
 module.exports = passport
