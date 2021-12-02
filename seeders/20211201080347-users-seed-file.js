1b12ed1be6e23ef3ac8cdf1ea1473b18a7e8c8ff
steven4program <stevenchang421@gmail.com>
2021-12-02 14:09:08 +0800

    modify userId in seed

--- seeders/20211201080347-users-seed-file.js
+++ seeders/20211201080347-users-seed-file.js
@@ -8,6 +8,7 @@ module.exports = {
       'Users',
       [
         {
+          id: 1,
           account: 'admin',
           email: 'root@example.com',
           password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
@@ -22,88 +23,27 @@ module.exports = {
           role: 'admin',
           createdAt: new Date(),
           updatedAt: new Date()
-        },
-        {
-          account: 'user1',
-          email: 'user1@example.com',
-          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
-          name: 'user1',
-          avatar: `https://i.pravatar.cc/150?u=${
-            Math.ceil(Math.random()) * 10 + 10
-          }`,
-          cover: `https://loremflickr.com/320/240/landscape/?random=${
-            Math.random() * 100
-          }`,
-          introduction: faker.lorem.sentence().substring(0, 160),
-          role: 'user',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          account: 'user2',
-          email: 'user2@example.com',
-          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
-          name: 'user2',
-          avatar: `https://i.pravatar.cc/150?u=${
-            Math.ceil(Math.random()) * 10 + 20
-          }`,
-          cover: `https://loremflickr.com/320/240/landscape/?random=${
-            Math.random() * 100
-          }`,
-          introduction: faker.lorem.sentence().substring(0, 160),
-          role: 'user',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          account: 'user3',
-          email: 'user3@example.com',
-          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
-          name: 'user3',
-          avatar: `https://i.pravatar.cc/150?u=${
-            Math.ceil(Math.random()) * 10 + 30
-          }`,
-          cover: `https://loremflickr.com/320/240/landscape/?random=${
-            Math.random() * 100
-          }`,
-          introduction: faker.lorem.sentence().substring(0, 160),
-          role: 'user',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          account: 'user4',
-          email: 'user4@example.com',
-          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
-          name: 'user4',
-          avatar: `https://i.pravatar.cc/150?u=${
-            Math.ceil(Math.random()) * 10 + 40
-          }`,
-          cover: `https://loremflickr.com/320/240/landscape/?random=${
-            Math.random() * 100
-          }`,
-          introduction: faker.lorem.sentence().substring(0, 160),
-          role: 'user',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          account: 'user5',
-          email: 'user5@example.com',
-          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
-          name: 'user5',
-          avatar: `https://i.pravatar.cc/150?u=${
-            Math.ceil(Math.random()) * 10 + 50
-          }`,
-          cover: `https://loremflickr.com/320/240/landscape/?random=${
-            Math.random() * 100
-          }`,
-          introduction: faker.lorem.sentence().substring(0, 160),
-          role: 'user',
-          createdAt: new Date(),
-          updatedAt: new Date()
         }
       ],
+      {}
+    )
+    await queryInterface.bulkInsert(
+      'users',
+      ['user1', 'user2', 'user3', 'user4', 'user5'].map((user, index) => ({
+        id: (index + 1) * 10 + 1,
+        account: user,
+        email: `${user}@example.com`,
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+        name: user,
+        avatar: `https://i.pravatar.cc/150?u=${Math.ceil(Math.random()) * 10}`,
+        cover: `https://loremflickr.com/600/240/landscape/?random=${
+          Math.random() * 100
+        }`,
+        introduction: faker.lorem.sentence().substring(0, 160),
+        role: 'user',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
       {}
     )
   },
