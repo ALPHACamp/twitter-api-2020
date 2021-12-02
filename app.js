49f3df169a30de979ae7bebe8e12df160dd58dcb
heidichen90 <toddypet@gmail.com>
2021-12-02 22:16:46 +0000

    add feature to return all tweets

--- app.js
+++ app.js
@@ -5,12 +5,11 @@ const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next) {
+function authenticated (req, res, next) {
   // passport.authenticate('jwt', { ses...
-} //登入驗證
+} // 登入驗證
 
 app.use(express.urlencoded({ extended: true }))
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
