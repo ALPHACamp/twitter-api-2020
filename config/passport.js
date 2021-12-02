90fa2559b7f8734f0881ffe5ad3e3150967d6399
heidichen90 <toddypet@gmail.com>
2021-12-02 23:04:44 +0000

    add the logic to get user's like twitter

--- config/passport.js
+++ config/passport.js
@@ -15,7 +15,7 @@ jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
 jwtOptions.secretOrKey = process.env.JWT_SECRET
 
 let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
-  User.findByPk(jwt_payload.id).then((user) => {
+  User.findByPk(jwt_payload.id, {include: [{ model: Like }]}).then((user) => {
     user = user.toJSON()
     if (!user) return next(null, false)
     return next(null, user)
--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -2,6 +2,7 @@ const { Tweet, sequelize } = require('../models')
 const { User } = require('../models')
 const { Like } = require('../models')
 const { Reply } = require('../models')
+const {getUer} = require('../_helpers')
 
 const tweetController = {
   getTweets: async (req, res) => {
@@ -9,6 +10,7 @@ const tweetController = {
       const tweets = await Tweet.findAll({
         include: [{ model: User }, { model: Like }, { model: Reply }]
       })
+
 
       const results = tweets.map((tweet) => ({
         id: tweet.dataValues.id,
@@ -21,7 +23,7 @@ const tweetController = {
 
       return res.status(200).json(results)
     } catch (error) {
-      console.log('>>>', error)
+      console.log(error)
       return res.status(500).json({ status: 'error', message: 'Server error' })
     }
   }
