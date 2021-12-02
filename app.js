6ade3834983895ce2c82a9857d0044815d028da0
cschang07 <manutdsfan@gmail.com>
2021-12-02 20:45:10 +0800

    before merge

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 })
 //-----------
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 require('./routes')(app)
