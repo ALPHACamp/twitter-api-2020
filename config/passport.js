085ae35f6c8e14ba7aa121592f31f21add7627f7
paulchnag0801 <x1232178987@gmail.com>
2021-12-02 16:46:23 +0800

    fix jwt-strategy user

--- config/passport.js
+++ config/passport.js
@@ -8,8 +8,6 @@ const passportJWT = require('passport-jwt')
 const ExtractJwt = passportJWT.ExtractJwt
 const JwtStrategy = passportJWT.Strategy
 
-
-
 // setup JWT
 
 let jwtOptions = {}
@@ -18,11 +16,11 @@ jwtOptions.secretOrKey = process.env.JWT_SECRET
 
 let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
   User.findByPk(jwt_payload.id).then((user) => {
+    user = user.toJSON()
     if (!user) return next(null, false)
     return next(null, user)
   })
 })
 passport.use(strategy)
-
 
 module.exports = passport
