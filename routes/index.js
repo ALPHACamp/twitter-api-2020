460124c1b061bd6a41993c5c6b839d4013e4002b
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-02 15:30:47 +0800

    add authenticated and adminAuthenticated middleware

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const userController = require('../controllers/userController')
 
 
-const authenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next()
+// const authenticated = (req, res, next) => {
+//   if (req.isAuthenticated()) {
+//     return next()
+//   }
+//   res.redirect('/api/signin')
+// }
+const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticatedAdmin = (req, res, next) => {
+  if (req.user) {
+    if (req.user.role === "admin") { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
   }
-  res.redirect('/api/signin')
 }
 
 
