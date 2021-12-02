221f6fd2e5d368d4f54889e947c2f3a1512a37fc
paulchnag0801 <x1232178987@gmail.com>
2021-12-02 16:43:26 +0800

    feat : creat test route & add authenticatedUser/admin function

--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,38 @@
 const express = require('express')
 const userController = require('../controllers/userController.js')
 const passport = require('../config/passport')
-const authenticated = passport.authenticate('jwt', { session: false })//登入token驗證
+const authenticated = passport.authenticate('jwt', { session: false }) //登入token驗證
 const helpers = require('../_helpers')
 
 //驗前台是user身分
-const authenticatedIsNotAdmin = (req, res, next) => {
-  if (authenticated) {
-    if (helpers.getUser(req).role !== 'admin') {
-      return next()
-    }
-    return res
-      .status(401)
-      .json({ status: 'error', message: 'permission denied' })
-  } else {
+const authenticatedUser = (req, res, next) => {
+  if (helpers.getUser(req).role === 'admin') {
     return res
       .status(401)
       .json({ status: 'error', message: 'permission denied' })
   }
+  return next()
 }
 
 // use helpers.getUser(req) to replace req.user
 //驗後台身分
-const authenticatedIsNotUser = (req, res, next) => {
-  if (authenticated) {
-    if (helpers.getUser(req).role !== 'user') {
-      return next()
-    }
-    return res
-      .status(401)
-      .json({ status: 'error', message: 'permission denied' })
-  } else {
+const authenticatedAdmin = (req, res, next) => {
+  if (helpers.getUser(req).role === 'user') {
     return res
       .status(401)
       .json({ status: 'error', message: 'permission denied' })
   }
+  return next()
 }
 
 module.exports = (app) => {
   // JWT signin & signup
   app.post('/api/signin', userController.signIn)
+
+  app.get(
+    '/api/users/:id',
+    authenticated,
+    authenticatedUser,
+    userController.getUser
+  )
 }
