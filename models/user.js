b073a7d536d67f6b7e1245baa8a8309c8d5ca3db
cschang07 <manutdsfan@gmail.com>
2021-12-02 15:45:30 +0800

    fix sequelize syntax error caused by version difference

--- models/user.js
+++ models/user.js
@@ -3,18 +3,9 @@ const { datatype } = require('faker')
 const {
   Model
 } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
+  const User = sequelize.define('User', {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     name: DataTypes.STRING(50),
@@ -23,9 +14,9 @@ module.exports = (sequelize, DataTypes) => {
     role: DataTypes.STRING,
     account: DataTypes.STRING,
     cover: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User'
-  })
-  return User
-}
+  }, {});
+  User.associate = function (models) {
+  }
+
+  return User;
+};
