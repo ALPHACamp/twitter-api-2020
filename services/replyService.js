f0365e86e566a4ce3e1dc3f04454811c67474ab4
Emily81926 <emily81926@gmail.com>
2021-12-04 00:25:23 +0800

    feat:modify tweet and user relation in getReplies feature

--- services/replyService.js
+++ services/replyService.js
@@ -1,5 +1,5 @@
 // 載入所需套件
-const { Reply, User } = require('../models')
+const { Reply, User, Tweet } = require('../models')
 const helpers = require('../_helpers')
 
 const replyService = {
@@ -26,12 +26,29 @@ const replyService = {
   getReplies: async (req, res, callback) => {
     try {
       //撈出特定:tweet_id的reply所有資料，並取得關聯User的資料
-      const replies = await Reply.findAll({
-        where: { TweetId: req.params.tweet_id },
-        raw: true,
-        nest: true,
-        include: [{ model: User, attributes: ['id', 'account', 'name', 'avatar'] }],
-      })
+      const replies = await Promise.all([
+
+        //先在reply model中取得comment,並關聯出user id, account, name及avatar
+        Reply.findAll({
+          where: { TweetId: req.params.tweet_id },
+          raw: true,
+          nest: true,
+          attributes: {
+            exclude: ['updatedAt', 'TweetId', 'UserId']
+          },
+          include: [
+            { model: User, attributes: ['id', 'account', 'name', 'avatar'] },
+          ],
+        }),
+
+        //在tweet model中取得userId,並關聯出user account
+        Tweet.findByPk(req.params.tweet_id, {
+          raw: true,
+          nest: true,
+          attributes: ['UserId'],
+          include: [{ model: User, attributes: ['account'] }]
+        })
+      ])
       return callback(replies)
     } catch (err) {
       console.log(err)
