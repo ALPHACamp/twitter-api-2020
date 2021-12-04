127e0840b85f91d11bfa3138426c699cd8ee6f99
Emily81926 <emily81926@gmail.com>
2021-12-04 18:31:10 +0800

    feat: modify followship model

--- models/followship.js
+++ models/followship.js
@@ -1,6 +1,11 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Followship = sequelize.define('Followship', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     followerId: DataTypes.INTEGER,
     followingId: DataTypes.INTEGER
   }, {});
--- test/requests/followship.spec.js
+++ test/requests/followship.spec.js
@@ -16,44 +16,35 @@ describe('# followship requests', () => {
   context('# POST ', () => {
 
     describe(' /api/followships', () => {
-      before(async() => {
+      before(async () => {
         // 清除 User, Followship table 的測試資料庫資料
-        await db.User.destroy({where: {},truncate: true})
-        await db.Followship.destroy({where: {},truncate: true})
+        await db.User.destroy({ where: {}, truncate: true })
+        await db.Followship.destroy({ where: {}, truncate: true })
         // 模擬登入資料
-        const rootUser = await db.User.create({name: 'root'});this.authenticate =  sinon.stub(passport,"authenticate").callsFake((strategy, options, callback) => {            
-          callback(null, {...rootUser}, null);
-          return (req,res,next)=>{};
+        const rootUser = await db.User.create({ name: 'root' }); this.authenticate = sinon.stub(passport, "authenticate").callsFake((strategy, options, callback) => {
+          callback(null, { ...rootUser }, null);
+          return (req, res, next) => { };
         });
         this.getUser = sinon.stub(
-            helpers, 'getUser'
-        ).returns({id: 1, Followings: []});
+          helpers, 'getUser'
+        ).returns({ id: 1, Followings: [] });
         // 在測試資料庫中，新增 mock 資料
-        await db.User.create({account: 'User1', name: 'User1', email: 'User1', password: 'User1'})
-        await db.User.create({account: 'User2', name: 'User2', email: 'User2', password: 'User2'})
+        await db.User.create({ account: 'User1', name: 'User1', email: 'User1', password: 'User1' })
+        await db.User.create({ account: 'User2', name: 'User2', email: 'User2', password: 'User2' })
       })
 
       // 新增 POST /followships
-      it.only(' - successfully', (done) => {
+      it(' - successfully', (done) => {
         request(app)
           .post('/api/followships')
           .send('id=2')
           .set('Accept', 'application/json')
           .expect(200)
-          .end(function(err, res) {
+          .end(function (err, res) {
             if (err) return done(err);
             // 檢查 Followship 資料裡，是否有 followerId=1, followingId = 2 的資料
-            
-            // 這段是自己加的，測試檔可以在followship model中找到新增的資料
-            db.Followship.findOne({ where: {id: 1 }}).then(followship => {
-              console.log(followship)
-              followship.followerId.should.equal(1);
-              followship.followingId.should.equal(2);
-              return done();
-            })
-            //這段是原本的，測試檔無法在followship model中找到新增的資料
+
             db.Followship.findByPk(1).then(followship => {
-              console.log(followship)
               followship.followerId.should.equal(1);
               followship.followingId.should.equal(2);
               return done();
@@ -65,8 +56,8 @@ describe('# followship requests', () => {
         // 清除登入及測試資料庫資料
         this.authenticate.restore();
         this.getUser.restore();
-        await db.User.destroy({where: {},truncate: true})
-        await db.Followship.destroy({where: {},truncate: true})
+        await db.User.destroy({ where: {}, truncate: true })
+        await db.Followship.destroy({ where: {}, truncate: true })
       })
 
     });
@@ -76,22 +67,22 @@ describe('# followship requests', () => {
   context('# DELETE ', () => {
 
     describe(' /api/followships/:followingId', () => {
-      before(async() => {
+      before(async () => {
         // 清除 User table 的測試資料庫資料
-        await db.User.destroy({where: {},truncate: true})
-        await db.Followship.destroy({where: {},truncate: true})
+        await db.User.destroy({ where: {}, truncate: true })
+        await db.Followship.destroy({ where: {}, truncate: true })
         // 模擬登入資料
-        const rootUser = await db.User.create({name: 'root'});this.authenticate =  sinon.stub(passport,"authenticate").callsFake((strategy, options, callback) => {            
-          callback(null, {...rootUser}, null);
-          return (req,res,next)=>{};
+        const rootUser = await db.User.create({ name: 'root' }); this.authenticate = sinon.stub(passport, "authenticate").callsFake((strategy, options, callback) => {
+          callback(null, { ...rootUser }, null);
+          return (req, res, next) => { };
         });
         this.getUser = sinon.stub(
-            helpers, 'getUser'
-        ).returns({id: 1, Followings: []});
+          helpers, 'getUser'
+        ).returns({ id: 1, Followings: [] });
         // 在測試資料庫中，新增 mock 資料
-        await db.User.create({account: 'User1', name: 'User1', email: 'User1', password: 'User1'})        
-        await db.User.create({account: 'User2', name: 'User2', email: 'User2', password: 'User2'})        
-        await db.Followship.create({followerId: 1, followingId: 2})
+        await db.User.create({ account: 'User1', name: 'User1', email: 'User1', password: 'User1' })
+        await db.User.create({ account: 'User2', name: 'User2', email: 'User2', password: 'User2' })
+        await db.Followship.create({ followerId: 1, followingId: 2 })
       })
 
       // 刪除 DETELE /followships/:followingId
@@ -100,7 +91,7 @@ describe('# followship requests', () => {
           .delete('/api/followships/2')
           .set('Accept', 'application/json')
           .expect(200)
-          .end(function(err, res) {
+          .end(function (err, res) {
             if (err) return done(err);
             // 檢查 Followship 的資料是否已空，表示已被刪除
             db.Followship.findByPk(1).then(followship => {
@@ -114,8 +105,8 @@ describe('# followship requests', () => {
         // 清除登入及測試資料庫資料
         this.authenticate.restore();
         this.getUser.restore();
-        await db.User.destroy({where: {},truncate: true})
-        await db.Followship.destroy({where: {},truncate: true})
+        await db.User.destroy({ where: {}, truncate: true })
+        await db.Followship.destroy({ where: {}, truncate: true })
       })
 
     });
