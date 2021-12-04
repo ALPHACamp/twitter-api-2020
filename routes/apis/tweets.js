bcea9184ce15167c395bdd5483d6228856cb79bc
Emily81926 <emily81926@gmail.com>
2021-12-04 13:40:31 +0800

    feat:add POST/api/tweets/:tweet_id/like route

--- routes/apis/tweets.js
+++ routes/apis/tweets.js
@@ -3,13 +3,17 @@ const express = require('express')
 const router = express.Router()
 const tweetController = require('../../controllers/tweetController')
 const replyController = require('../../controllers/replyController')
+const likeController = require('../../controllers/likeController')
 const { authenticated, checkNotAdmin } = require('../../middlewares/auth')
 
 router.post('/', authenticated, checkNotAdmin, tweetController.postTweet)
 router.get('/', authenticated, checkNotAdmin, tweetController.getTweets)
 router.get('/:tweet_id', authenticated, checkNotAdmin, tweetController.getTweet)
+
 router.post('/:tweet_id/replies', authenticated, checkNotAdmin, replyController.postReply)
 router.get('/:tweet_id/replies', authenticated, checkNotAdmin, replyController.getReplies)
+
+router.post('/:tweet_id/like', authenticated, checkNotAdmin, likeController.postLike)
 
 
 // router exports
---
+++ services/likeService.js
@@ -0,0 +1,29 @@
+// 載入所需套件
+const { Like } = require('../models')
+const { Op } = require('sequelize')
+const helpers = require('../_helpers')
+
+const likeService = {
+  postLike: async (req, res, callback) => {
+    try {
+      const like = await Like.findOne({ where: { [Op.and]: [{ UserId: helpers.getUser(req).id }, { TweetId: req.params.tweet_id }] } })
+
+      // 確認是否重複按讚
+      if (like) {
+        return callback({ status: 'error', message: '已按過讚！' })
+      } else {
+        await Like.create({
+          UserId: helpers.getUser(req).id,
+          TweetId: req.params.tweet_id
+        })
+        return callback({ status: 'success', message: '成功按讚' })
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+
+
+// likeController exports
+module.exports = likeService
