e746c008be24434686cb5b923c32b21969bf2351
heidichen90 <toddypet@gmail.com>
2021-12-04 18:43:52 +0000

    revert back id

--- test/requests/admin.spec.js
+++ test/requests/admin.spec.js
@@ -77,7 +77,7 @@ describe('# admin requests', () => {
         // 在測試資料庫中，新增 mock 資料
         await db.User.create({ account: 'User1', name: 'User1', email: 'User1', password: 'User1', role: 'admin' })
         await db.User.create({ account: 'User2', name: 'User2', email: 'User2', password: 'User2' })
-        await db.Tweet.create({ id: 1, UserId: 1, description: 'User1 的 description' })
+        await db.Tweet.create({ UserId: 1, description: 'User1 的 description' })
       })
 
       // DELETE /admin/tweets/:id - 刪除使用者的推文
