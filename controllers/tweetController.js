e8c45d480494c6241889609fb4a07c3da8644720
heidichen90 <toddypet@gmail.com>
2021-12-04 07:23:29 +0000

    remove getTweets

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -5,40 +5,12 @@ const { Reply } = require('../models')
 const helpers = require('../_helpers')
 
 const tweetController = {
-  getTweets: async (req, res) => {
-    try {
-      const tweets = await Tweet.findAll({
-        include: [{ model: User }, { model: Like }, { model: Reply }]
-      })
-
-      let results = tweets.map((tweet) => ({
-        id: tweet.dataValues.id,
-        description: tweet.dataValues.description,
-        createdAt: tweet.dataValues.createdAt,
-        User: tweet.dataValues.User.toJSON(),
-        likeCounts: tweet.dataValues.Likes.length,
-        replyCounts: tweet.dataValues.Replies.length,
-        isLike: helpers.getUser(req).Likes ? helpers.getUser(req).Likes.some(like => like.TweetId === tweet.dataValues.id) : false
-      }))
-
-      results = results.sort((a, z) => z.createdAt - a.createdAt)
-
-      return res.status(200).json(results)
-    } catch (error) {
-      console.log(error)
-      return res.status(500).json({ status: 'error', message: 'Server error' })
-    }
-  },
   getTweet: async (req, res) => {
     try {
-      const id = req.params.tweet_id
-
-      const tweet = await Tweet.findByPk(id, {
+      const tweet = await Tweet.findByPk(req.params.tweet_id, {
         include: [{ model: User }, { model: Like }, { model: Reply }]
       })
-
       const data = tweet.toJSON()
-
       const result = {
         id: data.id,
         description: data.description,
@@ -48,10 +20,10 @@ const tweetController = {
         replyCounts: data.Replies.length,
         isLike: helpers.getUser(req).Likes ? helpers.getUser(req).Likes.some(like => like.TweetId === data.id) : false
       }
-
       return res.status(200).json(result)
     } catch (error) {
       console.log(error)
+      return res.status(500).json({ status: 'error', message: 'Server error' })
     }
   }
 }
