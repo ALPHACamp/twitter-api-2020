58d1308b0541acbcf782387e10fac6b900560115
heidichen90 <toddypet@gmail.com>
2021-12-05 10:27:24 +0000

    fix the likecounts logi

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,24 +3,33 @@ const { Tweet, User, Like } = require('../models')
 const adminController = {
   getUsers: async (req, res) => {
     try {
-      const users = await User.findAll({ include: [{ model: User, as: 'Followers' }, { model: User, as: 'Followings' }, { model: Like }, { model: Tweet }] })
+      const users = await User.findAll({ include: [{ model: User, as: 'Followers' }, { model: User, as: 'Followings' }, { model: Tweet, include: Like }] })
 
-      let result = users.map(user => ({
-        id: user.dataValues.id,
-        avatar: user.dataValues.avatar,
-        account: user.dataValues.account,
-        name: user.dataValues.name,
-        tweetCounts: user.dataValues.Tweets.length,
-        likeCounts: user.dataValues.Likes.length,
-        followships: {
-          followerCounts: user.dataValues.Followers.length,
-          followingCounts: user.dataValues.Followings.length
+      let result = users.map(user => {
+        let likeCounts = 0
+        user.dataValues.Tweets.forEach((data) => {
+          if (data.Likes.length > 0) {
+            likeCounts = likeCounts + data.Likes.length
+          }
+        })
+        const result = {
+          id: user.dataValues.id,
+          avatar: user.dataValues.avatar,
+          account: user.dataValues.account,
+          name: user.dataValues.name,
+          tweetCounts: user.dataValues.Tweets.length,
+          likeCounts: likeCounts,
+          followships: {
+            followerCounts: user.dataValues.Followers.length,
+            followingCounts: user.dataValues.Followings.length
+          }
         }
-      }))
+        return result
+      })
 
       result = result.sort((a, z) => z.tweetCounts - a.tweetCounts)
       return res.status(200).json(result)
-      } catch (error) {
+    } catch (error) {
       console.log(error)
       return res.status(500).json({ status: 'error', message: 'Server error' })
     }
@@ -28,7 +37,7 @@ const adminController = {
 
   deleteTweet: async (req, res) => {
     try {
-      await Tweet.destroy({where: {id: Number(req.params.id)}});
+      await Tweet.destroy({ where: { id: Number(req.params.id) } })
       return res.status(200).json({ status: 'success', message: '成功刪除貼文' })
     } catch (error) {
       console.log(error)
