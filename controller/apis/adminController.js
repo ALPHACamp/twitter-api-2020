12d448be538740b254c08f41e24321546239fec9
Tim <s87462830@gmail.com>
2021-12-05 18:23:36 +0800

    feat: modify adminController.getUsers

--- controller/apis/adminController.js
+++ controller/apis/adminController.js
@@ -1,13 +1,48 @@
+const helper = require('../../_helpers')
 const db = require('../../models')
 const Tweet = db.Tweet
 const User = db.User
-const Reply = db.Reply
-const helper = require('../../_helpers')
+const Like = db.Like
+const { sequelize } = require('../../models')
+
 const adminController = {
-  getUsers: async (req, res) => {
+  getUsers: async (req, res, next) => {
     try {
-      const users = await User.findAll({ raw: true, nest: true })
-      return res.status(200).json(users)
+      const users = await Tweet.findAll({
+        raw: true,
+        nest: true,
+        include: [
+          { model: User, attributes: [] },
+          { model: Like, attributes: [] }
+
+        ],
+        attributes: [
+          [sequelize.col('User.id'), 'userId'],
+          [sequelize.col('User.name'), 'userName'],
+          [sequelize.col('User.cover'), 'userCover'],
+          [sequelize.col('User.avatar'), 'userAvatar'],
+          [sequelize.fn('COUNT', sequelize.fn('DISTINCT', sequelize.col('likes.id'))), 'likesCount'],
+          [sequelize.fn('COUNT', sequelize.fn('DISTINCT', sequelize.col('Tweet.id'))), 'tweetsCount'],
+          [
+            sequelize.literal(
+              `(SELECT COUNT(*) FROM Followships WHERE Followships.followerId = User.id)`
+            ),
+            `followingsCount`
+          ],
+          [
+            sequelize.literal(
+              `(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id)`
+            ),
+            `followersCount`
+          ],
+        ],
+        group: ['User.id'],
+        order: [
+          [sequelize.literal('followersCount DESC')],
+          [sequelize.literal('userName ASC')] //追蹤人數一樣時，依userName排序
+        ]
+      })
+      return res.json(users)
     } catch (err) {
       console.log(err)
     }
