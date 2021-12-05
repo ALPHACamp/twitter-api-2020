2e04a82b09a75053c3ac509840a54e5e944db9dd
Steavn Chen <tom9876555@yahoo.com.tw>
2021-12-06 00:33:13 +0800

    Add a specific tweet and all reply routes

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -4,30 +4,26 @@ const User = db.User
 const Reply = db.Reply
 const Like = db.Like
 
+const tweetService = require("../services/tweetService");
 
 const tweetController = {
   getTweets: (req, res) => {
     const currentUser = req.user ? req.user : helpers.getUser(req);
-    Tweet.findAll({ include: [
-        User, 
-        Reply,
-        Like
-      ] 
-    }).then((tweets) => { 
+    Tweet.findAll({ include: [User, Reply, Like] }).then((tweets) => {
       // console.log("]]]]]]]]]", currentUser.id);
       // console.log(tweets)
-     let likeData = tweets.Replies
-    //  console.log(likeData)
+      let likeData = tweets.Replies;
+      //  console.log(likeData)
       return res.render("tweets", {
         tweets: tweets,
         Reply: Reply,
-        userId: currentUser.id 
+        userId: currentUser.id,
       });
     });
   },
 
   postTweet: (req, res) => {
-    const currentUser = req.user ? req.user : helpers.getUser(req)
+    const currentUser = req.user ? req.user : helpers.getUser(req);
     Tweet.create({
       description: req.body.description,
       UserId: currentUser.id,
@@ -37,43 +33,9 @@ const tweetController = {
   },
 
   getTweet: (req, res) => {
-    const currentUser = req.user ? req.user : helpers.getUser(req);
-    return Tweet.findByPk(req.params.id, {
-      include: [ 
-        User,
-        { model: Like },
-        { model: Reply, include: [User] }],
-    }).then((tweet) => {
-      const tweetReplyCount = tweet.Replies.length
-      // const tweetLikeCount = tweet.Likes.map(d => d.isLike === true).length
-      console.log('%%%%%%%%%%%',tweet.Likes.length)
-      const tweetLikeCount = tweet.Likes.filter((d) => {
-        console.log('##################',d.isLike);
-        return d.isLike === true 
-      }).length;
-      // const tweetLikeCount = tweet.Likes.length
-      console.log("tweetLikeCount", tweetLikeCount);
-      const isLike = tweet.Likes.map((d) => d.UserId).includes(currentUser.id)
-      // const isLike = tweet.Likes.map(d => {
-      //   console.log('```````',d)
-      //   return {
-      //     id: d.id,
-      //     isLike: d.isLike
-      //   }
-      // })
-      // console.log(
-      //   "tweet",
-      //   tweetReplyCount,
-      //   "tweetLikeCount",
-      //   tweetLikeCount,
-      //   isLike
-      // );
-      return res.render("tweet", {
-        tweet: tweet.toJSON(),
-        tweetReplyCount: tweetReplyCount,
-        tweetLikeCount: tweetLikeCount
-      });
-    });
+    tweetService.getTweet(req, res, (data) => {
+      return res.render('tweet', data)
+    })
   },
 };
 
--- services/tweetService.js
+++ services/tweetService.js
@@ -8,43 +8,40 @@ const helpers = require('../_helpers')
 const tweetService = {
   postTweet: (req, res, callback) => {
     if (!req.body.description) {
-      callback({ status: 'error', message: 'text didn\'t exist' })
+      callback({ status: "error", message: "text didn't exist" });
     } else {
       return Tweet.create({
         description: req.body.description,
-        UserId: helpers.getUser(req).id
-      })
-        .then((category) => {
-          callback({
-            status: 'success',
-            message: 'tweet was successfully created',
-          })
-        })
+        UserId: helpers.getUser(req).id,
+      }).then((category) => {
+        callback({
+          status: "success",
+          message: "tweet was successfully created",
+        });
+      });
     }
   },
   getTweets: (req, res, callback) => {
-    return Tweet.findAll({ include: User }).then(result => {
-      callback(
-        { tweets: result }
-      )
-    })
+    return Tweet.findAll({ include: User }).then((result) => {
+      callback({ tweets: result });
+    });
   },
   getTweet: (req, res, callback) => {
+    const currentUser = req.user ? req.user : helpers.getUser(req);
     return Tweet.findByPk(req.params.id, {
-      include: [
-        User,
-        { model: Reply, include: [User] }
-      ]
-    }).then(tweet => {
-      console.log(tweet)
-      // console.log(tweet.User)
-      const isLiked = tweet.User.map(d => d.id).includes(helpers.getUser(req).id)
-      callback({
-        tweet: tweet,
-        isLiked: isLiked
-      })
-    })
+      include: [User, { model: Like }, { model: Reply, include: [User] }]
+    }).then((tweet) => {
+      const tweetReplyCount = tweet.Replies.length;
+      const tweetLikeCount = tweet.Likes.filter((d) => d.isLike === true
+      ).length;
+      let isLike = tweet.Likes.find((d) => d.UserId === currentUser.id);
+      isLike = isLike.isLike;
+      callback({ tweet: tweet.toJSON(),
+        tweetReplyCount: tweetReplyCount,
+        tweetLikeCount: tweetLikeCount,
+        isLike: isLike,})
+    });
   },
-}
+};
 
 module.exports = tweetService
