3088e11607aeeeb1e23631b493323f41a9a2b53e
Martin-no1tw <kljhger7728@gmail.com>
2021-12-06 11:34:35 +0800

    feat:fix cors

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 3000
 
+const allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  next();
+}
+
+app.use(allowCrossDomain)
+
 app.use(cors())
 
 if (process.env.NODE_ENV !== "production") {
