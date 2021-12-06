29af2f3b9dbf8b64b7dff5890981bbd9ea691adf
Martin-no1tw <kljhger7728@gmail.com>
2021-12-06 11:54:36 +0800

    feat:fix cors issue

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ if (process.env.NODE_ENV !== "production") {
 const passport = require('./config/passport')
 
 app.use(cors())
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 
 // use helpers.getUser(req) to replace req.user
 function authenticated(req, res, next) {
