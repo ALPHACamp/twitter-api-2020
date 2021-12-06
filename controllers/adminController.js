703a2fb986cbf3b0011a8a63119457824db652be
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-07 02:47:32 +0800

    GET /api/admin/tweets add LikedTweetCount

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,19 +1,20 @@
 const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
-const Like = db.Like
-const Sequelize = require('Sequelize')
+const sequelize = require('sequelize')
 const { Op } = require("sequelize");
 
 const adminController = {
   getUsers: (req, res) => {
     User.findAll({
       where: { role: { [Op.is]: null } },  // 排除管理者
-      attributes: ['id', 'account', 'name', 'cover', 'avatar'],
+      attributes: ['id', 'account', 'name', 'cover', 'avatar',
+        [sequelize.literal(`(SELECT COUNT(Tweets.UserId) FROM Tweets INNER JOIN Likes ON Tweets.id = Likes.TweetId WHERE Tweets.UserId = User.id)`), 'LikedTweetCount'],
+      ],
       include: [
         { model: User, as: 'Followers', attributes: ['id', 'account', 'name'] },  // User belongs to many User, through Followship
         { model: User, as: 'Followings', attributes: ['id', 'account', 'name'] },
-        Tweet,  //User has many Tweets
+        Tweet,  // OK ; User has many Tweets
       ],
     }).then(users => {
       users = users.map((user) => ({
@@ -24,6 +25,7 @@ const adminController = {
         // LikesCount: user.Likes.length  // 須改為推文"被"like的數量
         // TweetLikedCount: ,
       }))
+      users.forEach(user => { delete user.Tweets })
       return res.json(users)
     })
   }
