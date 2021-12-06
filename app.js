6e7b4f49fcb67f83688bb5086e7550fdb1714eac
wentingliu <cindy810317@gmail.com>
2021-12-06 22:06:45 +0800

    modify config & app.js

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   res.locals.user = helpers.getUser(req)
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE')
+    return res.status(200).json({})
+  }
   next()
 })
 
