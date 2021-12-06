828f471a7cff6a092f593f8c9874b528583659c2
Emily81926 <emily81926@gmail.com>
2021-12-06 17:52:47 +0800

    feat: modify replyService error handle

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -3,9 +3,13 @@ const replyService = require('../services/replyService')
 
 const replyController = {
   postReply: (req, res) => {
-    replyService.postReply(req, res, data => {
-      return res.json(data)
-    })
+    try {
+      replyService.postReply(req, res, data => {
+        return res.json(data)
+      })
+    } catch (err) {
+      return res.status(400).json({ status: err.name, message: err.message })
+    }
   },
 
   getReplies: (req, res) => {
--- services/replyService.js
+++ services/replyService.js
@@ -1,50 +1,43 @@
 // 載入所需套件
 const { Reply, User, Tweet } = require('../models')
 const helpers = require('../_helpers')
+const ReqError = require('../helpers/ReqError')
 
 const replyService = {
   postReply: async (req, res, callback) => {
-    try {
-      const { comment } = req.body
+    const { comment } = req.body
 
-      //確認回覆欄是否有填寫
-      if (comment.trim() === '') {
-        return callback({ status: 'error', message: '內容不可空白' })
-      } else {
-        await Reply.create({
-          comment,
-          TweetId: req.params.tweet_id,
-          UserId: helpers.getUser(req).id,
-        })
-      }
-      return callback({ status: 'success', message: '成功回覆貼文' })
-    } catch (err) {
-      console.log(err)
+    //確認回覆欄是否有填寫
+    if (comment.trim() === '') {
+      throw new ReqError('內容不可空白')
+    } else {
+      await Reply.create({
+        comment,
+        TweetId: req.params.tweet_id,
+        UserId: helpers.getUser(req).id,
+      })
     }
+    return callback({ status: 'success', message: '成功回覆貼文' })
   },
 
   getReplies: async (req, res, callback) => {
-    try {
-      //撈出特定:tweet_id的reply所有資料，並取得tweet及user關聯資料
-      const replies = await Reply.findAll({
-        where: { TweetId: req.params.tweet_id },
-        raw: true,
-        nest: true,
-        attributes: {
-          exclude: ['updatedAt', 'TweetId', 'UserId']
-        },
-        include: [
-          { model: User, attributes: ['id', 'account', 'name', 'avatar'] },
-          {
-            model: Tweet, attributes: ['UserId'],
-            include: [{ model: User, attributes: ['account'] }]
-          }
-        ],
-      })
-      return callback(replies)
-    } catch (err) {
-      console.log(err)
-    }
+    //撈出特定:tweet_id的reply所有資料，並取得tweet及user關聯資料
+    const replies = await Reply.findAll({
+      where: { TweetId: req.params.tweet_id },
+      raw: true,
+      nest: true,
+      attributes: {
+        exclude: ['updatedAt', 'TweetId', 'UserId']
+      },
+      include: [
+        { model: User, attributes: ['id', 'account', 'name', 'avatar'] },
+        {
+          model: Tweet, attributes: ['UserId'],
+          include: [{ model: User, attributes: ['account'] }]
+        }
+      ],
+    })
+    return callback(replies)
   }
 }
 
