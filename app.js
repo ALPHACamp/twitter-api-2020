2e495d198b5296e88e2772e61ced993e496c343d
Martin-no1tw <kljhger7728@gmail.com>
2021-12-06 11:23:03 +0800

    feat:fix cors

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const helpers = require('./_helpers');
 const routes = require('./routes')
-var cors = require('cors');
+const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT || 3000
