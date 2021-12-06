d70dd1b71a4e8d1f95a7f0556a643c946b4f5045
jadokao <jackykao528@outlook.com>
2021-12-06 21:10:04 +0800

    change Access-Control-Allow-Headers

--- app.js
+++ app.js
@@ -35,8 +35,11 @@ app.use((req, res, next) => {
   res.locals.user = helpers.getUser(req)
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-  res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  )
   next()
 })
 
