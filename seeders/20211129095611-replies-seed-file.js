323bd8e55b75d30390d7bb4e3bcaeea2a3ede7f2
william1993314 <william1993314@gmail.com>
2021-12-07 21:26:14 +0800

    20211207 Fix reply seed data by Will

--- seeders/20211129095611-replies-seed-file.js
+++ seeders/20211129095611-replies-seed-file.js
@@ -12,9 +12,9 @@ module.exports = {
         Array.from({ length: 30 }).map((d, i) =>
             ({
                 id: i * 10 + 1,
+                TweetId: tweets[parseInt(i / 3)].id,
                 comment: faker.lorem.text().substring(0, 140),
                 UserId: i = 1 ? Math.floor(Math.random() * 5) * 10 + 11: i * 10 + 11,
-                TweetId: tweets[parseInt(i / 3)].id,
                 createdAt: new Date(),
                 updatedAt: new Date(),
             })
