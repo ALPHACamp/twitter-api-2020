966b18982bd941e212461d39363dd92d57355f76
smilingfroggy <smilingfroggy@hotmail.com>
2021-12-08 01:06:21 +0800

    modify user seed password

--- seeders/20211130191757-users-seed-file.js
+++ seeders/20211130191757-users-seed-file.js
@@ -17,7 +17,7 @@ module.exports = {
     return queryInterface.bulkInsert('Users', [
       ...Array.from({ length: 5 }).map((d, i) => ({
         email: `user${i}@example.com`,
-        password: bcrypt.hashSync('23456789', bcrypt.genSaltSync(10), null),
+        password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
         name: faker.name.findName(),
         avatar: 'https://loremflickr.com/320/240/face',
         account: `user${i+1}`,
