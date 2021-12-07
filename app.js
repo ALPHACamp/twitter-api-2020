aaca8dddbc4d0c09be71fe7bbb65d07d9b47a1db
rayray1010 <da866810@gmail.com>
2021-12-07 20:46:27 +0800

    refactor: port option

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(passport.initialize())
