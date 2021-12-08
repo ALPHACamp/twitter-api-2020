5077486a46f07968f4a0298bd2bf73f4fbbf34e7
Steavn Chen <tom9876555@yahoo.com.tw>
2021-12-08 13:49:19 +0800

    Modify the ISKLIKE to get all the user's tweet routes

--- services/tweetService.js
+++ services/tweetService.js
@@ -25,33 +25,16 @@ const tweetService = {
 
   getTweets: (req, res, callback) => {
     const currentUser = req.user ? req.user : helpers.getUser(req);
-    // Tweet.findAll({ include: [User, Reply, Like] }).then((tweets) => {
     Tweet.findAll({ include: [User, { model: Reply }, { model: Like }] }).then(
       (tweets) => {
-      let tweetReplyCount;
-      console.log("tweet", tweets.length);
-
       let newTweets = tweets.map((d) => {
         let tweetLikeCount = d.dataValues.Likes.filter(
           (d) => d.isLike === true
         ).length;
         let tweetReplyCount = d.dataValues.Replies.map((d) => d.id).length;
         let userIsLike = d.dataValues.Likes.find((d) => d.UserId);
-        // userIsLike = {
-        //   id: userIsLike.id ? userIsLike.id : false,
-        //   TweetId: userIsLike.TweetId ? userIsLike.TweetId : false,
-        //   UserId: userIsLike.UserId ? userIsLike.UserId : false,
-        //   isLike: userIsLike.isLike ? userIsLike.isLike : false
-        // }
-        // console.log(userIsLike)
         if (!userIsLike) {
           userIsLike = false;
-          userIsLike = {
-            id: userIsLike.id ? userIsLike.id : false,
-            TweetId: userIsLike.TweetId ? userIsLike.TweetId : false,
-            UserId: userIsLike.UserId ? userIsLike.UserId : false,
-            isLike: userIsLike.isLike ? userIsLike.isLike : false,
-          };
         } else {
           userIsLike = userIsLike.isLike;
         }
@@ -65,15 +48,8 @@ const tweetService = {
         return d;
       });
       return callback({ tweets: newTweets });
-      // return res.render("tweets", {
-      //   tweets: newTweets,
     });
   },
-  // getTweets: (req, res, callback) => {
-  //   return Tweet.findAll({ include: User }).then((result) => {
-  //     callback({ tweets: result });
-  //   });
-  // },
 
   getTweet: (req, res, callback) => {
     return Tweet.findByPk(req.params.id, {
@@ -83,8 +59,33 @@ const tweetService = {
       const tweetLikeCount = tweet.Likes.filter(
         (d) => d.isLike === true
       ).length;
+
       let isLike =
         tweet.Likes.length !== 0 ? tweet.Likes.map((d) => d.isLike) : false;
+      
+        // 測試 ISLIKE
+        // let userIsLike = d.dataValues.Likes.find((d) => d.UserId);
+        // // userIsLike = {
+        // //   id: userIsLike.id ? userIsLike.id : false,
+        // //   TweetId: userIsLike.TweetId ? userIsLike.TweetId : false,
+        // //   UserId: userIsLike.UserId ? userIsLike.UserId : false,
+        // //   isLike: userIsLike.isLike ? userIsLike.isLike : false
+        // // }
+        // // console.log(userIsLike)
+        // if (!userIsLike) {
+        //   userIsLike = false;
+        //   userIsLike = {
+        //     isLike: false
+        //     // id: userIsLike.id ? userIsLike.id : false,
+        //     // TweetId: userIsLike.TweetId ? userIsLike.TweetId : false,
+        //     // UserId: userIsLike.UserId ? userIsLike.UserId : false,
+        //     // isLike: userIsLike.isLike ? userIsLike.isLike : false,
+        //   };
+        // } else {
+        //   userIsLike = userIsLike.isLike;
+        // }
+        // console.log(userIsLike);
+
       callback({
         tweet: tweet.toJSON(),
         tweetReplyCount: tweetReplyCount,
@@ -94,7 +95,6 @@ const tweetService = {
     });
   },
 };
-
 module.exports = tweetService
 
  // getTweets: (req, res) => {
