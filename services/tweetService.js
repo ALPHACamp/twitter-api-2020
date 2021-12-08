6ce6fed2cc05f4b5b935f2d5cd2fe15b00d4c4fe
Steavn Chen <tom9876555@yahoo.com.tw>
2021-12-08 16:46:23 +0800

    Query the route of all tweets of user to increase islike a second time

--- services/tweetService.js
+++ services/tweetService.js
@@ -27,60 +27,29 @@ const tweetService = {
     const currentUser = req.user ? req.user : helpers.getUser(req);
     Tweet.findAll({ include: [User, { model: Reply }, { model: Like }] }).then(
       (tweets) => {
-      console.log('tweets.',tweets.length)
-      console.log("tweets", tweets[0].Likes[0]);
+        console.log('tweets',tweets.length)
+        console.log('1111111',tweets[0].Likes[0])
       let newTweets = tweets.map((d) => {
         let tweetLikeCount = d.dataValues.Likes.filter(
           (d) => d.isLike === true
         ).length;
         let tweetReplyCount = d.dataValues.Replies.map((d) => d.id).length;
-        // let userIsLike = d.dataValues.Likes.find((d) => d.UserId);
-        let userIsLike = d.dataValues.Likes.map((d) => d.UserId).includes(currentUser.id)
-        let paramsLike
-        console.log(userIsLike)
+        let userIsLike = d.dataValues.Likes.find((d) => d.UserId);
+        // let userIsLike = d.dataValues.Likes.map(d => d.UserId).includes(currentUser.id)
         if (!userIsLike) {
-          // paramsLike = false
           userIsLike = false;
-        } else  {
-          // userIsLike = {
-          //   id: userIsLike.Likes.id ? userIsLike.Likes.id : false,
-          //   UserId: userIsLike.Likes.UserId ? userIsLike.UserId.id : false,
-          //   tweetId: userIsLike.Likes.TweetId ? userIsLike.TweetId.id : false,
-          // };
-          userIsLike = userIsLike
+        } else {
+          userIsLike = userIsLike.isLike
         }
-        //    if (!userIsLike) {
-        //   userIsLike = false;
-        //   paramsLike = {
-        //     // isLike: false,
-        //     id: userIsLike.id ? userIsLike.id : false,
-        //     TweetId: userIsLike.TweetId ? userIsLike.TweetId : false,
-        //     UserId: userIsLike.UserId ? userIsLike.UserId : false,
-        //     isLike: userIsLike.isLike ? userIsLike.isLike : false,
-        //   };
-        // } else {
-        //   userIsLike = userIsLike.isLike;
-        //     paramsLike = {
-        //       // isLike: false,
-        //       id: userIsLike.id ? userIsLike.id : false,
-        //       TweetId: userIsLike.TweetId ? userIsLike.TweetId : false,
-        //       UserId: userIsLike.UserId ? userIsLike.UserId : false,
-        //       isLike: userIsLike.isLike ? userIsLike.isLike : false,
-        //     };
-        // }
-        
-        console.log("req.params.userId", req.params.id,'當下使用者',currentUser.id,'userIsLike',userIsLike)
-        // console.log('params',paramsLike);
+        console.log("~~~~~~",userIsLike);
         d = {
           ...d.dataValues,
           tweetReplyCount,
           tweetLikeCount,
           isLike: userIsLike,
-        }
+        };
         return d;
       });
-      let count = newTweets.filter(d => d.isLike === true)
-      console.log(count.length)
       return callback({ tweets: newTweets });
     });
   },
@@ -107,10 +76,10 @@ const tweetService = {
         // // }
         // // console.log(userIsLike)
         // if (!userIsLike) {
-          // userIsLike = false;
-          // userIsLike = {
-            // isLike: false
-            // id: userIsLike.id ? userIsLike.id : false,
+        //   userIsLike = false;
+        //   userIsLike = {
+        //     isLike: false
+        //     // id: userIsLike.id ? userIsLike.id : false,
         //     // TweetId: userIsLike.TweetId ? userIsLike.TweetId : false,
         //     // UserId: userIsLike.UserId ? userIsLike.UserId : false,
         //     // isLike: userIsLike.isLike ? userIsLike.isLike : false,
