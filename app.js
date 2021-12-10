b6cd477499a1004afe0197dde61da9f8eee6fd73
jadokao <jackykao528@outlook.com>
2021-12-10 21:52:59 +0800

    seperate public and private chatroom

--- app.js
+++ app.js
@@ -53,11 +53,28 @@ const server = require('http').Server(app).listen(port, () => {
   console.log(`The app is listening on port ${port}`)
 })
 const io = require('socket.io')(server)
-io.on('connection', socket => {
+
+// 公開聊天室
+const publicNamespace = io.of('/public')
+// publicNamespace.use((socket, next) => {
+//   passport.authenticate('jwt', { session: false }, (err, user, info) => {
+//     if (err) next(err)
+//     if (!user) {
+//       return res.json({
+//         status: 'error',
+//         message: '帳號不存在！'
+//       })
+//     }
+//     req.user = user
+//     return next()
+//   })
+// })
+publicNamespace.on('connection', socket => {
   console.log('連接成功，上線ID: ', socket.id)
+
   //監聽並提示有人上線了
   socket.on('onlineHint', userName => {
-    socket.broadcast.emit('onlineHint', userName)
+    publicNamespace.broadcast.emit('onlineHint', userName)
   })
 
   // 監聽訊息
@@ -71,14 +88,65 @@ io.on('connection', socket => {
     //     include: [User]
     //   }).then(message => {
     //     //回傳 message 給所有客戶端(包含自己)
-    //     io.sockets.emit('getMessage', message)
+    //     publicNamespace.emit('getMessage', message)
     //   })
     // })
   })
 
   //監聽並提示有人下線了
   socket.on('offlineHint', userName => {
-    socket.broadcast.emit('offlineHint', userName)
+    publicNamespace.emit('offlineHint', userName)
+  })
+
+  // 連接斷開
+  socket.on('disconnect', () => {
+    console.log('有人離開了！， 下線ID: ', socket.id)
+  })
+})
+
+// 私人聊天室
+const privateNamespaces = io.of(/(?:user)\d+/)
+// privateNamespace.use((socket, next) => {
+//   passport.authenticate('jwt', { session: false }, (err, user, info) => {
+//     if (err) next(err)
+//     if (!user) {
+//       return res.json({
+//         status: 'error',
+//         message: '帳號不存在！'
+//       })
+//     }
+//     req.user = user
+//     return next()
+//   })
+// })
+privateNamespaces.on('connection', socket => {
+  console.log('連接成功，上線ID: ', socket.id)
+  const privateNamespace = socket.nsp
+
+  //監聽並提示有人上線了
+  socket.on('onlineHint', userName => {
+    privateNamespace.emit('onlineHint', userName)
+  })
+
+  // 監聽訊息
+  socket.on('getMessage', data => {
+    console.log('服務端 接收 訊息: ', data)
+    // Message.create({
+    //   content: data.text,
+    //   UserId: data.UserId
+    // }).then(message => {
+    //   Message.findByPk(message.id, {
+    //     include: [User]
+    //   }).then(message => {
+    //     //回傳 message 給所有客戶端(包含自己)
+    //     privateNamespace.emit('getMessage', message)
+    //   })
+    // })
+  })
+
+  //監聽並提示有人下線了
+  socket.on('offlineHint', userName => {
+    privateNamespace.emit('offlineHint', userName)
   })
 
   // 連接斷開
