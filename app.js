1afa3aa3c09675a8e9ca7887ac20ad77b4f037dd
jadokao <jackykao528@outlook.com>
2021-12-10 14:57:01 +0800

    basic socket.io

--- app.js
+++ app.js
@@ -44,8 +44,47 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 require('./routes')(app)
+
+// 加入 socket.io 監聽
+const server = require('http').Server(app).listen(port, () => {
+  console.log(`The app is listening on port ${port}`)
+})
+const io = require('socket.io')(server)
+io.on('connection', socket => {
+  console.log('連接成功，上線ID: ', socket.id)
+  //監聽並提示有人上線了
+  socket.on('onlineHint', userName => {
+    socket.broadcast.emit('onlineHint', userName)
+  })
+
+  // 監聽訊息
+  socket.on('getMessage', data => {
+    console.log('服務端 接收 訊息: ', data)
+    Message.create({
+      content: data.text,
+      UserId: data.UserId
+    }).then(message => {
+      Message.findByPk(message.id, {
+        include: [User]
+      }).then(message => {
+        //回傳 message 給所有客戶端(包含自己)
+        io.sockets.emit('getMessage', message)
+      })
+    })
+  })
+
+  //監聽並提示有人下線了
+  socket.on('offlineHint', userName => {
+    socket.broadcast.emit('offlineHint', userName)
+  })
+
+  // 連接斷開
+  socket.on('disconnect', () => {
+    console.log('有人離開了！， 下線ID: ', socket.id)
+  })
+})
 
 module.exports = app
