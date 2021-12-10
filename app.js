c1a7b5123bd1c9860ca40eb1b8db2d88f055e145
Kerwin <tkoleo84119@gmail.com>
2021-12-10 21:29:58 +0800

    feat: add socket.io setting

--- app.js
+++ app.js
@@ -3,18 +3,31 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
-const helpers = require('./_helpers')
+const { createServer } = require('http')
+const { Server } = require('socket.io')
 const routes = require('./routes')
 const cors = require('cors')
 
 const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:8084",
+    methods: ["GET", "POST"]
+  }
+})
 const PORT = process.env.PORT
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+io.on('connection', (socket) => {
+  console.log(socket.id)
+})
+
 app.use(routes)
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
+httpServer.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
 
 module.exports = app
