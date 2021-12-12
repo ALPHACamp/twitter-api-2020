5931f7f0fb4d7ed21621c3b73b288dae43be99c0
Steavn Chen <tom9876555@yahoo.com.tw>
2021-12-12 11:48:43 +0800

    Report an error before merging to pull

--- services/tweetService.js
+++ services/tweetService.js
@@ -1,9 +1,9 @@
-const db = require('../models')
-const Tweet = db.Tweet
-const Reply = db.Reply
-const User = db.User
-const Like = db.Like
-const helpers = require('../_helpers')
+const db = require("../models");
+const Tweet = db.Tweet;
+const Reply = db.Reply;
+const User = db.User;
+const Like = db.Like;
+const helpers = require("../_helpers");
 
 const tweetService = {
   postTweet: (req, res, callback) => {
@@ -22,36 +22,35 @@ const tweetService = {
     }
   },
   getTweets: (req, res, callback) => {
-    Tweet.findAll({ 
+    Tweet.findAll({
       include: [User, { model: Reply }, { model: Like }],
-      order: [['createdAt', 'DESC']]
-     }).then(
-      (tweets) => {
-        // console.log(tweets,'#################')
+      order: [["createdAt", "DESC"]],
+    }).then((tweets) => {
+      // console.log(tweets,'#################')
       // let newTweets = tweets.map((d) => {
-         tweets = tweets.map((d) => {
-           let tweetLikeCount = d.dataValues.Likes.filter(
-             (d) => d.isLike === true
-           ).length
-           let tweetReplyCount = d.dataValues.Replies.map((d) => d.id).length;
-           let userIsLike = d.dataValues.Likes.find(
-             (d) => d.UserId === helpers.getUser(req).id
-           )
-           if (!userIsLike) {
-             userIsLike = false;
-           } else {
-             userIsLike = userIsLike.isLike;
-           }
-           d = {
-             ...d.dataValues,
-             tweetReplyCount,
-             tweetLikeCount,
-             isLike: userIsLike,
-           };
-           return d
-         })
-        //  console.log(tweets)
-        //  tweets = [...tweets.dataValues]
+      tweets = tweets.map((d) => {
+        let tweetLikeCount = d.dataValues.Likes.filter(
+          (d) => d.isLike === true
+        ).length;
+        let tweetReplyCount = d.dataValues.Replies.map((d) => d.id).length;
+        let userIsLike = d.dataValues.Likes.find(
+          (d) => d.UserId === helpers.getUser(req).id
+        );
+        if (!userIsLike) {
+          userIsLike = false;
+        } else {
+          userIsLike = userIsLike.isLike;
+        }
+        d = {
+          ...d.dataValues,
+          tweetReplyCount,
+          tweetLikeCount,
+          isLike: userIsLike,
+        };
+        return d;
+      });
+      //  console.log(tweets)
+      //  tweets = [...tweets.dataValues]
       // console.log("newTweets", newTweets[0].UserId);
       return callback(tweets);
     });
@@ -61,22 +60,27 @@ const tweetService = {
       where: { UserId: Number(req.params.id) },
       include: [User, { model: Like }, { model: Reply, include: [User] }],
     }).then((tweet) => {
-      let tweetReplyCount = tweet.Replies.length 
-      let tweetLikeCount = tweet.Likes.filter(
-        (d) => d.isLike === true
-      ).length;
-      let tweetLike = tweet.Likes.filter((d, index) => d.isLike === true)
-      tweetLike = tweetLike.map((d) => d.UserId).includes(helpers.getUser(req).id)
-      tweet = { ...tweet.dataValues, tweetReplyCount, tweetLikeCount,  isLike: tweetLike}
-      console.log(tweet)
-       callback(tweet);
+      let tweetReplyCount = tweet.Replies.length;
+      let tweetLikeCount = tweet.Likes.filter((d) => d.isLike === true).length;
+      let tweetLike = tweet.Likes.filter((d, index) => d.isLike === true);
+      tweetLike = tweetLike
+        .map((d) => d.UserId)
+        .includes(helpers.getUser(req).id);
+      tweet = {
+        ...tweet.dataValues,
+        tweetReplyCount,
+        tweetLikeCount,
+        isLike: tweetLike,
+      };
+      console.log(tweet);
+      callback(tweet);
       // callback({
       //   tweet: tweet.toJSON(),
       //   tweetReplyCount: tweetReplyCount,
       //   tweetLikeCount: tweetLikeCount,
       //   isLike: tweetLike,
       // });
-      })
+    });
   },
 };
-module.exports = tweetService
+module.exports = tweetService;
