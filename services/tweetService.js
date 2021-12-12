08eb580950047c174cc86d48d8e39c3710de193d
Steavn Chen <tom9876555@yahoo.com.tw>
2021-12-12 11:56:02 +0800

    After pulling, update branch B02 and push again

--- services/tweetService.js
+++ services/tweetService.js
@@ -72,7 +72,6 @@ const tweetService = {
         tweetLikeCount,
         isLike: tweetLike,
       };
-      console.log(tweet);
       callback(tweet);
       // callback({
       //   tweet: tweet.toJSON(),
