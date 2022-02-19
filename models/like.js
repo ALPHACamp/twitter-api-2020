2d726ec6aee900a1cd78b7b4c0b9b0ffbd55de25
martinchiu <martin87713@gmail.com>
2022-02-19 17:40:02 +0800

    feat: modify like model

--- models/like.js
+++ models/like.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
-  }, {});
+    userId: DataTypes.INTEGER,
+    tweetId: DataTypes.INTEGER
+  }, {
+    tableName: 'Likes'
+  });
   Like.associate = function(models) {
+    Like.belongsTo(models.User, { foreignKey: 'userId' })
+    Like.belongsTo(models.Tweet, { foreignKey: 'tweetId' })
   };
   return Like;
 };
