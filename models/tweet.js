9bbf6faf7f0cfc786b5dab406e12d22c61ee586a
martinchiu <martin87713@gmail.com>
2022-02-19 17:35:50 +0800

    feat: modify tweet model

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
-  }, {});
-  Tweet.associate = function(models) {
+    description: DataTypes.TEXT,
+    userId: DataTypes.INTEGER
+  }, {
+    tableName: 'Tweets'
+  });
+  Tweet.associate = function (models) {
+    Tweet.hasMany(models.Reply)
+    Tweet.hasMany(models.Like)
+    Tweet.belongsTo(models.User, { foreignKey: 'userId' })
   };
   return Tweet;
 };
