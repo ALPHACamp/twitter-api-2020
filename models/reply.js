9c0c53e15df3187981081538a029f81e5ec33cab
martinchiu <martin87713@gmail.com>
2022-02-19 17:38:22 +0800

    feat:modify reply model

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,15 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
-  }, {});
+    comment: DataTypes.TEXT,
+    userId: DataTypes.INTEGER,
+    tweetId: DataTypes.INTEGER
+  }, {
+    tableName: 'Replies'
+  });
   Reply.associate = function(models) {
+    Reply.belongsTo(models.User, { foreignKey: 'userId' })
+    Reply.belongsTo(models.Tweet, { foreignKey: 'tweetId' })
   };
   return Reply;
 };
