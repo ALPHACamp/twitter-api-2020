7eb10c7135af937e9f6558311876ebc80133a906
Richie-Yang <richieyang8813@gmail.com>
2022-02-22 00:34:03 +0800

    fix: modify reply model/migration files to pass the test

--- models/reply.js
+++ models/reply.js
@@ -1,12 +1,19 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
     UserId: DataTypes.INTEGER,
     TweetId: DataTypes.INTEGER,
     comment: DataTypes.TEXT,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
   }, {
-    tableName: 'Replies',
-    underscored: true
+    tableName: 'Replies'
   });
   Reply.associate = function(models) {
     Reply.belongsTo(models.User, { foreignKey: 'UserId' })
