01567c96d6e4c858c0d305e1714de276d86a2ac6
Richard Widjaya <ricwidjaya@gmail.com>
2022-02-21 14:16:18 +0800

    refactor: auth

--- config/passport.js
+++ config/passport.js
@@ -9,21 +9,21 @@ const jwtOptions = {
   secretOrKey: process.env.JWT_SECRET
 }
 
-let strategy = new JWTStrategy(jwtOptions, async (jwtPayLoad, done) => {
-  try {
-    const user = await User.findByPk(jwtPayLoad.id, {
-      include: [
-        { model: User, as: 'Followers' },
-        { model: User, as: 'Followings' }
-      ]
-    })
+passport.use(
+  new JWTStrategy(jwtOptions, async (jwtPayLoad, done) => {
+    try {
+      const user = await User.findByPk(jwtPayLoad.id, {
+        include: [
+          { model: User, as: 'Followers' },
+          { model: User, as: 'Followings' }
+        ]
+      })
 
-    return done(null, user)
-  } catch (error) {
-    done(error)
-  }
-})
-
-passport.use(strategy)
+      return done(null, user)
+    } catch (error) {
+      done(error)
+    }
+  })
+)
 
 module.exports = passport
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { authenticated, authenticatedAdmin } = require('../../helpers/auth')
+const { authenticated } = require('../../helpers/auth')
 
 const userController = require('../../controllers/user-controllers')
 
