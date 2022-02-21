9e2d2308368e7376592cf92a515c808170e4598d
Richie-Yang <richieyang8813@gmail.com>
2022-02-22 00:30:06 +0800

    fix: modify like model/migration files to pass the test

--- migrations/20190115071419-create-like.js
+++ migrations/20190115071419-create-like.js
@@ -8,19 +8,17 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      user_id: {
-        allowNull: false,
+      UserId: {
         type: Sequelize.INTEGER
       },
-      tweet_id: {
-        allowNull: false,
+      TweetId: {
         type: Sequelize.INTEGER
       },
-      created_at: {
+      createdAt: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updated_at: {
+      updatedAt: {
         allowNull: false,
         type: Sequelize.DATE
       }
