92118371ec8f848570c1535e7b234bb8ab03e248
martinchiu <martin87713@gmail.com>
2022-02-21 11:17:43 +0800

    feat: add passport module

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const helpers = require('./_helpers')
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
 
+app.use(passport.initialize())
+
 // use helpers.getUser(req) to replace req.user
 function authenticated (req, res, next) {
-  // passport.authenticate('jwt', { ses...
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+
+    req.user = user
+    next()
+  })(req, res, next)
 };
 
 app.get('/', (req, res) => res.send('Hello World!'))
