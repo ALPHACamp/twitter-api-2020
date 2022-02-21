03f69d92c1d9340a17c08efee9dd8f49634adf55
Richie-Yang <richieyang8813@gmail.com>
2022-02-21 23:06:26 +0800

    feat: add like model

--- models/like.js
+++ models/like.js
@@ -1,7 +1,12 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
-  }, {});
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
+  }, {
+    tableName: 'Likes',
+    underscored: true
+  });
   Like.associate = function(models) {
   };
   return Like;
