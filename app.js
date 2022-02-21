073437c5391d40bc15c04ca48d2a7c1d372305ff
martinchiu <martin87713@gmail.com>
2022-02-21 22:39:14 +0800

    feat: add authenticate middleware

--- app.js
+++ app.js
@@ -12,14 +12,6 @@ const port = 3000
 app.use(passport.initialize())
 
 // use helpers.getUser(req) to replace req.user
-function authenticated (req, res, next) {
-  passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
-
-    req.user = user
-    next()
-  })(req, res, next)
-};
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
