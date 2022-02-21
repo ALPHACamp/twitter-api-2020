9122e0601fdae18397dc103c9ba88e9e814a0784
Richard Widjaya <ricwidjaya@gmail.com>
2022-02-21 14:21:48 +0800

    add tweet controller

---
+++ controllers/tweet-controllers.js
@@ -0,0 +1,5 @@
+const { User, Tweet } = require('../models')
+
+const tweetController = {}
+
+module.exports = tweetController
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const passport = require('../config/passport')
 const { generalErrorHandler } = require('../helpers/error-handler')
 const { authenticated, authenticatedAdmin } = require('../helpers/auth')
 
 const users = require('./modules/users')
+const tweets = require('./modules/tweets')
 
 router.use('/users', users)
+router.use('/tweets', authenticated, tweets)
 
 // fallback route for 404 not found (temporary)
 router.get('/', (req, res) => res.send('Hello World!'))
