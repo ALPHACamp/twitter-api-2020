2b2e4a9f8625be92dafa27b47c837283ac518c6a
Richie-Yang <richieyang8813@gmail.com>
2022-02-21 23:23:35 +0800

    feat: add reply model

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
-  }, {});
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER,
+    comment: DataTypes.TEXT,
+  }, {
+    tableName: 'Replies',
+    underscored: true
+  });
   Reply.associate = function(models) {
+    Reply.belongsTo(models.User, { foreignKey: 'user_id' })
+    Reply.belongsTo(models.Tweet, { foreignKey: 'tweet_id' })
   };
   return Reply;
 };
