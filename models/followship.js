e98e6c439eb9bbd946e1acd6951c433a1df8581d
deansyue <c1318131801@gmail.com>
2022-02-22 23:58:39 +0800

    init models

--- models/followship.js
+++ models/followship.js
@@ -1,7 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Followship = sequelize.define('Followship', {
-  }, {});
+    followerId: DataTypes.INTEGER,
+    followingId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Followship',
+    tableName: 'Followships'
+  });
   Followship.associate = function(models) {
   };
   return Followship;
--- models/like.js
+++ models/like.js
@@ -1,8 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
-  }, {});
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Like',
+    tableName: 'Likes'
+  });
   Like.associate = function(models) {
+    Like.belongsTo(models.User, { foreignKey: 'UserId' })
+    Like.belongsTo(models.Tweet, { foreignKey: 'TweetId' })
   };
   return Like;
 };
