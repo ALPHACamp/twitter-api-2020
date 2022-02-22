a447a792f30654c4fac14cdc1676e815d27ef0f4
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-02-21 16:39:55 -0800

    feature: create model and db-migration file

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,18 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Tweet = sequelize.define('Tweet', {
-  }, {});
+    userId: DataTypes.INTEGER,
+    description: DataTypes.TEXT,
+  }, {
+    sequelize,
+    modelName: 'Tweet',
+    tableName: 'Tweets',
+    underscored: true  
+  });
   Tweet.associate = function(models) {
+    Tweet.hasMany(model.Reply, { foreignKey: 'tweetId' })
+    Tweet.hasMany(model.Like, { foreignKey: 'tweetId' })
+    Tweet.belongsTo(model.User, { foreignKey: 'userId' })
   };
   return Tweet;
 };
