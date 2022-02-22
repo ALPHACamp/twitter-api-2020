c58a221cdba37c7d71dfeeffa8c8fa0fa096a0b1
Richie-Yang <richieyang8813@gmail.com>
2022-02-22 15:21:09 +0800

    feat: add both axios and Random User API in order to generate avatar pictures for user seed

--- seeders/20220222020941-users-seed-file.js
+++ seeders/20220222020941-users-seed-file.js
@@ -1,5 +1,7 @@
 'use strict';
+const faker = require('faker')
 const bcrypt = require('bcryptjs')
+const axios = require('axios')
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -14,30 +16,40 @@ module.exports = {
       }], {});
     */
 
+    const RANDOM_USER_API_URL = 'https://randomuser.me/api'
     const DEFAULT_PASSWORD = '12345678'
+    const DEFAULT_USERS_NUMBER = 5
 
-    return queryInterface.bulkInsert('Users', [
-      // root account setting
-      {
-        email: 'root@example.com',
-        name: 'root',
-        account: 'root',
-        password: bcrypt.hashSync(DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
-        role: 'admin',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      // user account settings as array
-      ...Array.from({ length: 5 }, (_, i) => ({
-        email: `user${i + 1}@example.com`,
-        name: `user${i + 1}`,
-        account: `user${i + 1}`,
-        password: bcrypt.hashSync(DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
-        role: 'user',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-    ], {})
+    // using axios to get some avatar pictures from Random User API
+    return axios.get(`${RANDOM_USER_API_URL}?results=${DEFAULT_USERS_NUMBER}`)
+      .then(({ data }) => {
+
+        // then start inserting both root and other user accounts into database
+        return queryInterface.bulkInsert('Users', [
+          // root account setting
+          {
+            email: 'root@example.com',
+            name: 'root',
+            account: 'root',
+            password: bcrypt.hashSync(DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
+            role: 'admin',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          },
+          // user account settings as array
+          ...Array.from({ length: DEFAULT_USERS_NUMBER }, (_, i) => ({
+            email: `user${i + 1}@example.com`,
+            name: `user${i + 1}`,
+            avatar: data.results[i].picture.large,
+            introduction: faker.lorem.text(10),
+            account: `user${i + 1}`,
+            password: bcrypt.hashSync(DEFAULT_PASSWORD, bcrypt.genSaltSync(10)),
+            role: 'user',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }))
+        ], {})
+      })
   },
 
   down: (queryInterface, Sequelize) => {
