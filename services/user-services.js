8d628ffeadd90e1082334b7e30bb9a614f12aa73
ronnychiang <ronnychiang1164@gmail.com>
2022-02-22 12:09:48 +0800

    fix: fix signup function

--- services/user-services.js
+++ services/user-services.js
@@ -2,7 +2,6 @@ const { User } = require('../models')
 const bcrypt = require('bcryptjs')
 const userServices = {
   signUp: (req, cb) => {
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
     return Promise.all([
       User.findOne({ where: { email: req.body.email } }),
       User.findOne({ where: { account: req.body.account } })
@@ -12,7 +11,14 @@ const userServices = {
         if (account) throw new Error('Account already exists!')
         return bcrypt.hash(req.body.password, 10)
       })
+      .then(hash => User.create({
+        account: req.body.account,
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      }))
       .then((createdUser) => {
+        console.log(createdUser)
         createdUser = createdUser.toJSON()
         delete createdUser.password
         return cb(null, { user: createdUser })
