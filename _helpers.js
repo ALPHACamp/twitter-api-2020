5ec6fd951b7379d1efc87fe56a5c796fb692d9d8
eklipsorz <eklipsorz@protonmail.ch>
2022-02-22 22:30:22 +0800

    feat: add api-auth.js and api-error-handler.js

--- _helpers.js
+++ _helpers.js
@@ -1,6 +1,26 @@
 
+/**
+ * 
+ * @param {object} req 
+ * @returns {object} user
+ */
+
 function getUser(req) {
-  return req.user;
+  return req.user
+}
+
+function we(){
+  
+}
+
+/**
+ * 
+ * @param {object} req 
+ * @returns {number} id
+ */
+
+function getUserId(req) {
+  return getUser(req).id
 }
 
 module.exports = {
