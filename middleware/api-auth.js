75783d071a91456add5ee718c5547a650e1e78ae
ReoNaBear <89585478+ReoNaBear@users.noreply.github.com>
2022-02-22 21:29:50 +0800

    adminLogin function

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -17,7 +17,7 @@ const authenticated = (req, res, next) => {
 const authenticatedUser = (req, res, next) => {
   if (helpers.getUser(req)) {
     if (helpers.getUser(req).role === 'user') { return next() }
-    return res.status(403).json({ message: '不可以用管理者帳戶登入前台' })
+    return res.status(403).json({ message: '不可以用管理者帳戶進入前台' })
   } else {
     return res.status(401).json({ message: '請先登入' })
   }
@@ -29,7 +29,7 @@ const authenticatedAdmin = (req, res, next) => {
     if (helpers.getUser(req).role === 'admin') { return next() }
     return res.status(403).json({ message: '普通用戶不可以登入後台' })
   } else {
-    return res.status(401).json({ message: '請先登入' })
+    return res.status(401).json({ message: '請先登入管理者帳戶' })
   }
 }
 
