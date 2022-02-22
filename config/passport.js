469956b9d312d7ca03da1b4f5fe4593eaa692888
Richie-Yang <richieyang8813@gmail.com>
2022-02-22 15:57:45 +0800

    feat: add passport LocalStrategy and related config

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,28 @@
 const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const { User } = require('../models')
 
+
+passport.use(new LocalStrategy({
+  usernameField: 'account',
+  passwordField: 'password'
+}, async (account, password, cb) => {
+  try {
+    const user = await User.findOne({ where: { account } })
+    // if user is not found, then return false
+    if (!user) return cb(null, false)
+
+    const isMatched = await bcrypt.compare(password, user.password)
+    // if password is not matched, then return false
+    if (!isMatched) return cb(null, false)
+
+    // if both account and password checks all passed
+    // then return user
+    return cb(null, user)
+
+  } catch (err) { return cb(err, false) }
+}))
 
 
 module.exports = passport
