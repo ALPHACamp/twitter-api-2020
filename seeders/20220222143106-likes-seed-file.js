687cfd88ae8a7e35e22db127694db689c505e4a5
songerard <songerard@gmail.com>
2022-02-23 22:25:25 +0800

    Fix like seeder to to non-admin user only

--- seeders/20220222143106-likes-seed-file.js
+++ seeders/20220222143106-likes-seed-file.js
@@ -2,6 +2,10 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const adminId = await queryInterface.sequelize.query(
+      'SELECT id FROM Users WHERE role="admin";',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     const users = await queryInterface.sequelize.query(
       'SELECT id FROM Users;',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
@@ -11,13 +15,13 @@ module.exports = {
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
 
-    // randomly set (tweets.length * 2) likes for 6 users to their 60 tweets, no duplicate like
+    // randomly set (tweets.length * 2) likes to tweets, no duplicate
     const likeArray = []
     do {
       let UserId = users[Math.floor(Math.random() * users.length)].id
       let TweetId = tweets[Math.floor(Math.random() * tweets.length)].id
 
-      if (!likeArray.some(like => like.UserId === UserId && like.TweetId === TweetId)) {
+      if (!likeArray.some(like => like.UserId === UserId && like.TweetId === TweetId) && !adminId.some(a => a.id === UserId)) {
         likeArray.push({
           UserId,
           TweetId,
