e8ed284f0ee92104aa9f1d13b8df51546f30f67f
songerard <songerard@gmail.com>
2022-02-23 22:25:36 +0800

    Fix followship seeder to to non-admin user only

--- seeders/20220222143135-followships-seed-file.js
+++ seeders/20220222143135-followships-seed-file.js
@@ -2,12 +2,16 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const adminId = await queryInterface.sequelize.query(
+      'SELECT id FROM Users WHERE role="admin";',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     const users = await queryInterface.sequelize.query(
       'SELECT id FROM Users;',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
 
-    // randomly set (users.length * 2) followships for 6 users, no duplicate like
+    // randomly set (users.length * 2) followships for 5 non-admin users, no duplicate
     const followshipArray = []
     do {
       let randFollowerId = users[Math.floor(Math.random() * users.length)].id
@@ -15,7 +19,7 @@ module.exports = {
 
       if (!followshipArray.some(follow => follow.followerId === randFollowerId && follow.followingId === randFollowingId)) {
 
-        if (randFollowerId !== randFollowingId) {
+        if (randFollowerId !== randFollowingId && !adminId.some(a => a.id === randFollowerId && a.id === randFollowingId)) {
           followshipArray.push({
             followerId: randFollowerId,
             followingId: randFollowingId,
