137711a7237dde9b3617ab3101036dbecede3e4e
Richie-Yang <richieyang8813@gmail.com>
2022-02-23 09:47:46 +0800

    feat: add error handler

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const user = require('./modules/user')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 
 router.use('/api/admin', admin)
 router.use('/api/users', user)
 router.use('/api/signin')
+
+router.use('/', generalErrorHandler)
 
 
 module.exports = router
