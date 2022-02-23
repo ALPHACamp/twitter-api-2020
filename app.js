4a238d7bd160f61d2a97821b92e9942c725b8516
DAI <senseye935@gmail.com>
2022-02-24 01:12:32 +0800

    setting login router

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express');
 const { apis } = require('./routes');
 const helpers = require('./_helpers');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 const passport = require('./config/passport')
 const app = express()
 const port = 3000
@@ -14,6 +15,9 @@ function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 };
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(passport.initialize())
 app.use('/api', apis)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- controllers/apis/user-controller.js
+++ controllers/apis/user-controller.js
@@ -1,21 +1,7 @@
 const jwt = require('jsonwebtoken')
+
 const userController = {
-  signIn: (req, res, next) => {
-    try {
-      const userData = req.user.toJSON()
-      delete userData.password
-      const token = jwt.signIn(userData, process.env.JWT_SECRET, { expiresIn: '30' })
-      res.json({
-        status: 'success',
-        data: {
-          token,
-          user: userData
-        }
-      })
-    } catch (err) {
-      next(err)
-    }
-  }
+  signIn: (req, res, next) 
 }
 
 module.exports = userController
