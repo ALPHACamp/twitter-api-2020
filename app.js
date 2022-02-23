043c85727e8aa27efb7d7c8fd6f1084700802840
Prysline <mayhu1993@gmail.com>
2022-02-23 17:17:38 +0800

    feat: add error handlers install packages: bcryptjs dotenv jsonwebtoken passport-jwt

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const helpers = require('./_helpers')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-const db = require('./models')
+app.use(express.json())
 
-// use helpers.getUser(req) to replace req.user
-function authenticated (req, res, next) {
-  // passport.authenticate('jwt', { ses...
-};
-
+app.use((req, res, next) => {
+  res.locals.user = helpers.getUser(req)
+  next()
+})
 app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/api', apis)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
