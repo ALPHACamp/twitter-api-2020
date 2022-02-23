1a631a7b25f0cd17cb466d8fd69fd44bf2c36502
Richard Widjaya <ricwidjaya@gmail.com>
2022-02-23 14:59:54 +0800

    refactor: login & register route extract

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,19 @@ const router = express.Router()
 const { generalErrorHandler } = require('../helpers/error-handler')
 const { authenticated, blockRole } = require('../helpers/auth')
 
+const userController = require('../controllers/user-controllers')
+
 const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 const admin = require('./modules/admin')
 const followships = require('./modules/followships')
 
+// Login and Registration
+router.post('/users/login', userController.login)
+router.post('/users/', userController.register)
 
 router.use('/admin', authenticated, blockRole('user'), admin)
-router.use('/users', users)
+router.use('/users', authenticated, blockRole('admin'), users)
 router.use('/tweets', authenticated, blockRole('admin'), tweets)
 router.use('/followships', authenticated, blockRole('admin'), followships)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { authenticated, blockRole } = require('../../helpers/auth')
 
 const userController = require('../../controllers/user-controllers')
 
-router.post('/login', userController.login)
-router.get('/:id', authenticated, blockRole('admin'), userController.getUser)
-router.post('/', userController.register)
+router.get('/:id', userController.getUser)
 
 module.exports = router
