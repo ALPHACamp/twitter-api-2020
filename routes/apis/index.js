8217ce790d71dbf1f2f59459d1bc6efd71b8a1af
DAI <senseye935@gmail.com>
2022-02-23 21:55:59 +0800

    setting login router

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('../../config/passport')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.use('/')
 
 module.exports = router
