b8e418fb372114e10ecfc11237834ce5fcb6883e
hmrvc <hmrvc@yahoo.com.tw>
2022-02-23 18:26:41 +0800

    Fix reply seed

--- seeders/20220223045542-reply-seed-file.js
+++ seeders/20220223045542-reply-seed-file.js
@@ -8,15 +8,26 @@ module.exports = {
         { type: queryInterface.sequelize.QueryTypes.SELECT }
       )
     users.shift()
+    
     const tweets = await queryInterface.sequelize.query(
-        'SELECT id FROM Tweets;',
+        'SELECT id, UserId FROM Tweets;',
         { type: queryInterface.sequelize.QueryTypes.SELECT }
       )
-
-
+    
     for (const tweet of tweets) {
       await queryInterface.bulkInsert('Replies', 
-        Array.from({ length: 3 }, () => ({
+        Array.from({ length: 1 }, () => ({
+        comment: faker.lorem.text(140),
+        UserId: tweet.UserId,
+        TweetId: tweet.id,
+        createdAt: new Date(),
+        updatedAt: new Date()
+        }))
+      )
+    }
+    for (const tweet of tweets) {
+      await queryInterface.bulkInsert('Replies', 
+        Array.from({ length: 2 }, () => ({
         comment: faker.lorem.text(140),
         UserId: users[Math.floor(Math.random() * users.length)].id,
         TweetId: tweet.id,
