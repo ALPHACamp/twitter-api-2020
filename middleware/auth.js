3cefb84c3868856693fa40934cecb49077a88360
hsinyi8997 <hsinyi8997@gmail.com>
2022-02-23 12:00:19 +0800

    modify auth.js to ban admin login as normal user

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,14 +3,16 @@ const { getUser } = require('../_helpers')
 
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
+    const userData = user.toJSON()
     if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
-    req.user = user
+    if (userData.role === 'admin') return res.status(401).json({ status: 'error', message: 'permission denied' })
+    req.user = userData
     next()
   })(req, res, next)
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if ( getUser(req) && getUser(req).role === 'admin') return next()
+  if (getUser(req) && getUser(req).role === 'admin') return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
 module.exports = {
