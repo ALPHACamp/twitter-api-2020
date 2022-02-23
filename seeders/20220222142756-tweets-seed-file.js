ddba8405141e93fe96f97a6fa8e66765af2c7667
songerard <songerard@gmail.com>
2022-02-23 22:24:37 +0800

    Fix tweet seeder to to non-admin user and each user has 10 tweets

--- seeders/20220222142756-tweets-seed-file.js
+++ seeders/20220222142756-tweets-seed-file.js
@@ -2,21 +2,29 @@
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const adminId = await queryInterface.sequelize.query(
+      'SELECT id FROM Users WHERE role="admin";',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     const users = await queryInterface.sequelize.query(
       'SELECT id FROM Users;',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
 
-    // randomly assign 60 tweets to 6 users
-    await queryInterface.bulkInsert('Tweets',
-      Array.from({ length: 60 }, () => ({
-        UserId: users[Math.floor(Math.random() * users.length)].id,
-        description: faker.lorem.text().substring(0, 100),
-        image: 'https://loremflickr.com/320/240/nature?random=100',
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-    )
+    // assign 10 tweets for each non-admin users
+    for (let u of users) {
+      if (!adminId.some(a => a.id === u.id)) {
+        await queryInterface.bulkInsert('Tweets',
+          Array.from({ length: 10 }, () => ({
+            UserId: u.id,
+            description: faker.lorem.text().substring(0, 100),
+            image: 'https://loremflickr.com/320/240/nature?random=100',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }))
+        )
+      }
+    }
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Tweets', null, {})
