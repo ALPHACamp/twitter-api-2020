2ee600efdf5acca8b621ea63ccd4fcc75a52a217
Richie-Yang <richieyang8813@gmail.com>
2022-02-23 16:39:53 +0800

    feat: add signup route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const helpers = require('../_helpers')
 const { User } = require('../models')
 
@@ -24,6 +25,55 @@ module.exports = {
           )
           return res.json({ token, user: userData })
       }
+
+    } catch (err) { next(err) }
+  },
+
+  signup: async (req, res, next) => {
+    try {
+      // if no any following property within req.body,
+      // then just return null instead
+      const account = req.body?.account?.trim() || null
+      const name = req.body.name?.trim() || null
+      const email = req.body.email?.trim() || null
+      const password = req.body.password?.trim() || null
+      const checkPassword = req.body?.checkPassword?.trim() || null
+
+      if (!account || !name || !email || !password || !checkPassword) {
+        throw new Error('每個欄位都屬必填')
+      }
+
+      if (name.length > 50) {
+        throw new Error('名稱欄位不得超過50字')
+      }
+
+      if (password !== checkPassword) {
+        throw new Error('密碼欄位必須相符')
+      }
+
+      // in order to handle two exceptions,
+      // it's necessary to do two queries to database
+      const [userForAccount, userForEmail] = await Promise.all([
+        User.findOne({ where: { account }, raw: true }),
+        User.findOne({ where: { email }, raw: true })
+      ])
+      if (userForAccount) throw new Error('account 已重複註冊！')
+      if (userForEmail) throw new Error('email 已重複註冊！')
+
+      // hash password with bcrypt.js
+      const salt = await bcrypt.genSalt(10)
+      const hash = await bcrypt.hash(password, salt)
+
+      // create user in database
+      let user = await User.create({
+        account, name, email, password: hash
+      })
+
+      // retrieve complete user data from database
+      user = await User.findByPk(user.toJSON().id, { raw: true })
+      delete user.password
+
+      return res.status(200).json({ ...user })
 
     } catch (err) { next(err) }
   }
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../../controllers/user-controller')
 
 
-router.post('/')
+router.post('/', userController.signup)
 
 
 module.exports = router
