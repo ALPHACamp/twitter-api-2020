ab1ecce75b21f3c17572bad78668cfd13b90bcea
chenchiachi <chenchiachi.ccc@gmail.com>
2022-02-23 01:03:19 +0100

    create getUser api

--- app.js
+++ app.js
@@ -4,25 +4,23 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const passport = require('./config/passport')
-const flash = require('connect-flash')
 const session = require('express-session')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
 
 const { getUser } = require('./helpers/auth-helpers')
-const { apis } =require('./routes')
+const { apis } = require('./routes')
 
 const app = express()
 const port = 3000
+
 const SESSION_SECRET = 'secret'
 
 app.use(express.json())
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(flash())
+
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.error_messages = req.flash('error_messages')
   res.locals.user = getUser(req)
   next()
 })
--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,16 +1,22 @@
-const helpers = require('../helpers/auth-helpers')
 const passport = require('../config/passport') // 引入 passport
 
-const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+    req.user = user
+    next()
+  })(req, res, next)
+}
 
 const authenticatedAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) return next()
+  if
+    (req.user && req.user.isAdmin) return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
 
 const authenticatedNoAdmin = (req, res, next) => {
-    if (!req.user || !req.user.isAdmin) return next()
-    return res.status(403).json({ status: 'error', message: 'this account is admin' })
+  if (!req.user || !req.user.isAdmin) return next()
+  return res.status(403).json({ status: 'error', message: 'this account is admin' })
 }
 
 module.exports = {
