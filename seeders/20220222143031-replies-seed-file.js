37ee26e0b2b81d9be2a349dff23463d2af483126
songerard <songerard@gmail.com>
2022-02-23 22:25:06 +0800

    Fix reply seeder to to non-admin user and each tweet has 3 replies by different user

--- seeders/20220222143031-replies-seed-file.js
+++ seeders/20220222143031-replies-seed-file.js
@@ -3,6 +3,10 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const adminId = await queryInterface.sequelize.query(
+      'SELECT id FROM Users WHERE role="admin";',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     const users = await queryInterface.sequelize.query(
       'SELECT id FROM Users;',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
@@ -12,16 +16,32 @@ module.exports = {
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
 
-    // randomly assign 120 replies to 60 tweets distributed by 6 users
-    await queryInterface.bulkInsert('Replies',
-      Array.from({ length: 120 }, () => ({
-        UserId: users[Math.floor(Math.random() * users.length)].id,
-        TweetId: tweets[Math.floor(Math.random() * tweets.length)].id,
-        comment: faker.lorem.text().substring(0, 50),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-    )
+    // 每篇 tweet 有隨機 3 個留言者，每個人有 1 則留言
+    const replies = []
+    for (let t of tweets) {
+
+      // generate array of 3 random distinct non-admin user id
+      const userArray = []
+      do {
+        let randId = users[Math.floor(Math.random() * users.length)].id
+        if (!userArray.some(u => u === randId) && !adminId.some(a => a.id === randId)) {
+          userArray.push(randId)
+        }
+      } while (userArray.length < 3)
+
+      // generate 3 replies to current tweet by userArray
+      for (let u of userArray) {
+        replies.push({
+          UserId: u,
+          TweetId: t.id,
+          comment: faker.lorem.text().substring(0, 50),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      }
+    }
+
+    await queryInterface.bulkInsert('Replies', replies)
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Replies', null, {})
