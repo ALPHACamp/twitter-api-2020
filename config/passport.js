e4a477123fd353017d9690be95744e496bcb8df1
hsinyi8997 <hsinyi8997@gmail.com>
2022-02-23 20:49:56 +0800

    final version

--- config/passport.js
+++ config/passport.js
@@ -1,34 +1,9 @@
 const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const bcrypt = require('bcryptjs')
 const passportJWT = require('passport-jwt')
 const { User } = require('../models')
 
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
-
-// setup passport strategy
-passport.use(new LocalStrategy(
-  // customize user field
-  {
-    usernameField: 'account',
-    passwordField: 'password',
-    passReqToCallback: true
-  },
-  // authenticate user
-  (req, account, password, cb) => {
-    User.findOne({ where: { account } })
-      .then(user => {
-        if (!user) return cb(new Error('Account or password does not match!'), false)
-
-        bcrypt.compare(password, user.password).then(res => {
-          if (!res) return cb(new Error('Account or password does not match!'), false)
-
-          return cb(null, user)
-        })
-      })
-  }
-))
 
 const jwtOptions = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
@@ -44,20 +19,5 @@ passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
     .then(user => cb(null, user))
     .catch(err => cb(err))
 }))
-
-// serialize and deserialize user
-passport.serializeUser((user, cb) => {
-  cb(null, user.id)
-})
-passport.deserializeUser((id, cb) => {
-  User.findByPk(id, {
-    include: [
-      { model: User, as: 'Followers' },
-      { model: User, as: 'Followings' }
-    ]
-  })
-    .then(user => cb(null, user.toJSON()))
-    .catch(err => cb(err))
-})
 
 module.exports = passport
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,23 +1,8 @@
-const jwt = require('jsonwebtoken')
-const { getUser } = require('../_helpers')
 const userService = require('../services/user-services')
 
 const userController = {
   signIn: (req, res, next) => {
-    try {
-      const userData = getUser(req).toJSON()
-      delete userData.password
-      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
-      res.json({
-        status: 'success',
-        data: {
-          token,
-          user: userData
-        }
-      })
-    } catch (err) {
-      next(err)
-    }
+    userService.signIn(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
   signUp: (req, res, next) => {
     userService.signUp(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
