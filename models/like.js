de36cfd9b9ba5fde3c7b1ac5d77dcde05d14d3d5
Richard Widjaya <ricwidjaya@gmail.com>
2022-02-24 15:19:58 +0800

    fix: addfollowing controller to meet test

--- models/like.js
+++ models/like.js
@@ -1,13 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Like = sequelize.define('Like', {
-    UserId: DataTypes.INTEGER,
-    TweetId: DataTypes.INTEGER,
-    deletedAt: DataTypes.DATE
-  }, {
-    tableName: 'Likes',
-    paranoid: true
-  });
+  const Like = sequelize.define(
+    'Like',
+    {
+      UserId: DataTypes.INTEGER,
+      TweetId: DataTypes.INTEGER,
+      deletedAt: DataTypes.DATE
+    },
+    {
+      tableName: 'Likes',
+      paranoid: true
+    }
+  )
   Like.associate = function (models) {
     Like.belongsTo(models.Tweet, { foreignKey: 'TweetId' })
     Like.belongsTo(models.User, { foreignKey: 'UserId' })
