59f6282351f605e8b32fc17bebc843288c5427a0
Boochu <mayhu1993@gmail.com>
2022-02-24 20:15:24 +0800

    Update index.js
    fix: 移除路由上的驗證，避免功能開發受到驗證問題影響

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -10,8 +10,8 @@ const { authenticated, authenticatedAdmin, authenticatedUser } = require('../../
 router.post('/users/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.post('/users', userController.signUp)
 router.post('/admin/signin', passport.authenticate('local', { session: false }), adminController.signIn)
-router.post('/tweets/:id/like', authenticated, authenticatedUser, tweetController.addLike)
-router.post('/tweets/:id/unlike', authenticated, authenticatedUser, tweetController.removeLike)
+router.post('/tweets/:id/like', tweetController.addLike)
+router.post('/tweets/:id/unlike', tweetController.removeLike)
 router.use('/', apiErrorHandler)
 
 module.exports = router
