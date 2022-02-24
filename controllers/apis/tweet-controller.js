640c2a46108128644582e767d287dfd5af3c4b9c
xnubber <aabbccdd10902@gmail.com>
2022-02-24 19:32:47 +0800

    fix: change data sended to the front through add/remove-like function

--- controllers/apis/tweet-controller.js
+++ controllers/apis/tweet-controller.js
@@ -13,9 +13,13 @@ const tweetController = {
       if (like) throw new Error('You have liked this tweet!')
 
       const liked = await Like.create({ UserId, TweetId })
+      tweet.isLiked = true
       return res.json({
         status: 'success',
-        data: liked
+        data: {
+          ...tweet.toJSON(),
+          isLiked: true
+        }
       })
     } catch (err) {
       next(err)
@@ -25,12 +29,19 @@ const tweetController = {
     try {
       const UserId = helpers.getUser(req).id
       const TweetId = req.params.id
+      const tweet = await Tweet.findByPk(TweetId)
       const liked = await Like.findOne({ where: { UserId, TweetId } })
+
+      if (!tweet) throw new Error("Tweet didn't exist!")
       if (!liked) throw new Error("You haven't Liked this tweet")
+
       await liked.destroy()
       return res.json({
         status: 'success',
-        data: liked
+        data: {
+          ...tweet.toJSON(),
+          isLiked: false
+        }
       })
     } catch (err) {
       next(err)
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -5,12 +5,13 @@ const { apiErrorHandler } = require('../../middleware/error-handlers')
 const userController = require('../../controllers/apis/user-controller')
 const adminController = require('../../controllers/apis/admin-controller')
 const tweetController = require('../../controllers/apis/tweet-controller')
+const { authenticated, authenticatedAdmin, authenticatedUser } = require('../../middleware/apiAuth')
 
 router.post('/users/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.post('/users', userController.signUp)
 router.post('/admin/signin', passport.authenticate('local', { session: false }), adminController.signIn)
-router.post('/tweets/:id/like', tweetController.addLike)
-router.post('/tweets/:id/unlike', tweetController.removeLike)
+router.post('/tweets/:id/like', authenticated, authenticatedUser, tweetController.addLike)
+router.post('/tweets/:id/unlike', authenticated, authenticatedUser, tweetController.removeLike)
 router.use('/', apiErrorHandler)
 
 module.exports = router
