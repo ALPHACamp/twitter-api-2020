0eb06555ae446a3008fa6216feb4e23416c82911
deansyue <c1318131801@gmail.com>
2022-02-25 00:43:44 +0800

    feat: 新增imgur功能 儲存使用者上傳的圖片檔

---
+++ helpers/file-helper.js
@@ -0,0 +1,29 @@
+// 引入模組
+const imgur = require('imgur')
+
+// 取出clintId變數
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+// imgur設定clientId
+imgur.setClientId(IMGUR_CLIENT_ID)
+
+
+// 圖片上傳至第三方_imgur
+const imgurFileHandler = file => {
+  return new Promise((resolve, reject) => {
+    // 沒有檔案，回傳null值
+    if (!file) return resolve(null)
+
+    // 上傳至imgur
+    return imgur.uploadFile(file.path)
+      .then(img => {
+        // 成功上傳，回傳img.link，無則回傳null
+        resolve(img?.link || null)
+      })
+      .catch(err => reject(err))
+  })
+}
+
+// 匯出模組
+module.exports = {
+  imgurFileHandler
+}
