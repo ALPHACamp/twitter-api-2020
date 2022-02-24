4e590c1e7e08f8bafc6a39e80505b7e1aca31e09
eklipsorz <eklipsorz@protonmail.ch>
2022-02-25 02:44:04 +0800

    can cancel like

--- controllers/like-controller.js
+++ controllers/like-controller.js
@@ -2,7 +2,7 @@ const { User, Tweet, Like } = require('../models')
 const authHelpers = require('../_helpers')
 
 const likeController = {
-  // 以目前使用者來對某個推文表示喜歡
+  // 以目前使用者來對某個推文表示喜歡 API
   postTweetLike: async (req, res, next) => {
     // params => tweetid
     // id => getUser
@@ -12,7 +12,7 @@ const likeController = {
       const userId = authHelpers.getUser(req).id
 
       const targetTweet = await Tweet.findByPk(tweetId)
-      const targetUser = await User.findByPk(userId)
+      const loginUser = await User.findByPk(userId)
       // 找不到推文可以按喜歡
       if (!targetTweet) {
         error.code = 404
@@ -36,7 +36,7 @@ const likeController = {
 
       // 可以按喜歡
       await targetTweet.increment('likeCount', { by: 1 })
-      await targetUser.increment('likeCount', { by: 1 })
+      await loginUser.increment('likeCount', { by: 1 })
       const result = await Like.create({
         UserId: userId,
         TweetId: tweetId
@@ -56,7 +56,67 @@ const likeController = {
       return next(error)
     }
 
+  },
+  // 以目前使用者來對某個推文撤銷原本的喜歡 API
+  postTweetUnlike: async (req, res, next) => {
+
+
+    try {
+      const error = new Error()
+      const tweetId = req.params.id
+      const userId = authHelpers.getUser(req).id
+
+      const targetTweet = await Tweet.findByPk(tweetId)
+      const loginUser = await User.findByPk(userId)
+
+      // 找不到推文可以取消喜歡
+      if (!targetTweet) {
+        error.code = 404
+        error.message = '對應推文不存在'
+        return next(error)
+      }
+      // 不可取消從未喜歡過的推文
+      const isExistLike = await Like.findOne({
+        where: {
+          UserId: userId,
+          TweetId: tweetId
+        }
+      })
+
+      if (!isExistLike) {
+        error.code = 403
+        error.message = '該推文從未被喜歡，不可取消喜歡'
+        return next(error)
+      }
+
+      // 可以取消喜歡
+      await targetTweet.decrement('likeCount', { by: 1 })
+      await loginUser.decrement('likeCount', { by: 1 })
+
+      const result = await Like.findOne({
+        where: {
+          UserId: userId,
+          TweetId: tweetId
+        }
+      })
+        .then(like => like.destroy())
+
+      return res
+        .status(200)
+        .json({
+          status: 'success',
+          message: '成功取消喜歡推文',
+          data: result
+        })
+
+
+    } catch (error) {
+      // 系統出錯
+      error.code = 500
+      return next(error)
+    }
   }
 }
+
 
 exports = module.exports = likeController
--- routes/apis/modules/tweet.js
+++ routes/apis/modules/tweet.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const likeController = require('../../../controllers/like-controller')
 
 router.post('/:id/like', likeController.postTweetLike)
+router.post('/:id/unlike', likeController.postTweetUnlike)
 
 module.exports = router
