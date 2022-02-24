9cecdf7b4d008cd69cd5954acfc2c579af96d537
martinchiu <martin87713@gmail.com>
2022-02-24 16:31:16 +0800

    feat: add req.body check in put user api

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -29,9 +29,13 @@ const userController = {
   },
   putUserProfile: (req, res, next) => {
     if (Number(req.params.id) !== getUser(req).dataValues.id) throw new Error('只能編輯自己的資料')
-    console.log(req.body.account)
-    if (req.body.name.trim().length > 50) throw new RangeError('暱稱字數超過上限')
-    if (req.body.introduction.trim().length > 160) throw new RangeError('自我介紹字數超過上限')
+
+    if (req.body.name && req.body.name.trim().length > 50) throw new RangeError('暱稱字數超過上限')
+    if (req.body.introduction && req.body.introduction.trim().length > 160) throw new RangeError('自我介紹字數超過上限')
+
+    if (req.body.name === undefined) req.body.name = ''
+    if (req.body.introduction === undefined) req.body.introduction = ''
+
     userServices.putUserProfile(req, (err, data) => err ? next(err) : res.status(200).json(data))
   },
   putUserAccount: (req, res, next) => {
@@ -43,6 +47,7 @@ const userController = {
     if (req.body.password === undefined || req.body.password.trim() === '') throw new Error('密碼為必填項目')
     if (req.body.password.length < 5 || req.body.password.length > 20) throw new RangeError('密碼字數不符合規定')
     if (req.body.name && req.body.name.trim().length > 50) throw new RangeError('暱稱字數超過上限')
+    if (req.body.name === undefined) req.body.name = ''
 
     userServices.putUserAccount(req, (err, data) => err ? next(err) : res.status(200).json(data))
   }
