f94a090c5f53cb465276d431a10e53b2ce5160a3
Richard Widjaya <ricwidjaya@gmail.com>
2022-02-24 17:21:05 +0800

    fix: user helper for followship

--- helpers/user.js
+++ helpers/user.js
@@ -1,10 +1,12 @@
 const helpers = require('../_helpers')
 
 module.exports = {
-  // Return an array of IDs for check followships
+  // Return an array of id numbers for check followships
   // followship should be string and either be "Followings" or "Followers"
   getFollowshipId: (req, followship) => {
     const user = helpers.getUser(req)
-    return user[followship]
+    const followshipIds = []
+    user[followship].forEach(user => followshipIds.push(user.dataValues.id))
+    return followshipIds
   }
 }
