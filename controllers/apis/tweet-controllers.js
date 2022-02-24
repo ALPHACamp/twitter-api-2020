dfa1a5d6ede75b1d2ec9d8e6735453d4dfd9f800
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-02-24 07:47:14 -0800

    fix merge conflict and eslint

--- controllers/apis/tweet-controllers.js
+++ controllers/apis/tweet-controllers.js
@@ -1,32 +1,32 @@
 const tweetServices = require('../../services/tweet-services')
 
 const tweetController = {
-getTweets: (req, res, next) => {
+  getTweets: (req, res, next) => {
   // tweetServices.getTweets(req, (err, data) => err ? next(err) : res.json(data))
-  tweetServices.getTweets(req, (err, data) => {
-    if (err) {
-      return next(err)
-    }
-    return res.status(200).json(data)
-  })
-},
-getTweet: (req, res, next) => {
-  tweetServices.getTweet(req, (err, data) => {
-    if (err) {
-      return next(err)
-    }
-    return res.status(200).json(data)
-  })
-},
-    postTweet: (req, res, next) => {
-        console.log(req.user)
-        tweetServices.postTweet(req, (err, data) => {
-            if (err) {
-                return next(err)
-            } 
-            return res.status(200).json(data)
-        })
-    }
+    tweetServices.getTweets(req, (err, data) => {
+      if (err) {
+        return next(err)
+      }
+      return res.status(200).json(data)
+    })
+  },
+  getTweet: (req, res, next) => {
+    tweetServices.getTweet(req, (err, data) => {
+      if (err) {
+        return next(err)
+      }
+      return res.status(200).json(data)
+    })
+  },
+  postTweet: (req, res, next) => {
+    console.log(req.user)
+    tweetServices.postTweet(req, (err, data) => {
+      if (err) {
+        return next(err)
+      }
+      return res.status(200).json(data)
+    })
+  }
 
 }
 
--- services/tweet-services.js
+++ services/tweet-services.js
@@ -1,56 +1,55 @@
 const { User, Tweet, Reply } = require('../models')
 
 const tweetController = {
-    getTweets: async (req, cb) => {
-        try {
-          const tweets = await Tweet.findAll({
-            include: User,
-            raw: true,
-            nest: true
-          })
-          return cb(null, tweets)
-        } catch (err) {
-          return cb(err)
-        }
-      },    
-    getTweet: async (req, cb) => {
-        try{
-            const tweet = await Tweet.findByPk(req.params.tweet_id, {
-                include: [
-                    Reply,
-                    User
-                ],
-                raw: true,
-                nest: true
-            })
-            return cb(null, tweet)
-        }catch(err){
-            return cb(err)
-        }
-
-    },
-    postTweet: async (req, cb) => {
-        try{
-            const { description } = req.body
-            const userId = req.user
-            console.log(req)
-            if (!description) {
-                return cb('Description is required.')
-            }
-            const newTweet = await Tweet.create({
-                description,
-                userId
-            })
-            const tweetData = {
-                status: "suceess",
-                data: {
-                    tweet: newTweet 
-                }
-            }
-            return cb(null, tweetData)
-        }catch(err){
-            return cb(err)
-        }
+  getTweets: async (req, cb) => {
+    try {
+      const tweets = await Tweet.findAll({
+        include: User,
+        raw: true,
+        nest: true
+      })
+      return cb(null, tweets)
+    } catch (err) {
+      return cb(err)
     }
+  },
+  getTweet: async (req, cb) => {
+    try {
+      const tweet = await Tweet.findByPk(req.params.tweet_id, {
+        include: [
+          Reply,
+          User
+        ],
+        raw: true,
+        nest: true
+      })
+      return cb(null, tweet)
+    } catch (err) {
+      return cb(err)
+    }
+  },
+  postTweet: async (req, cb) => {
+    try {
+      const { description } = req.body
+      const userId = req.user
+      console.log(req)
+      if (!description) {
+        return cb('Description is required.')
+      }
+      const newTweet = await Tweet.create({
+        description,
+        userId
+      })
+      const tweetData = {
+        status: 'suceess',
+        data: {
+          tweet: newTweet
+        }
+      }
+      return cb(null, tweetData)
+    } catch (err) {
+      return cb(err)
+    }
+  }
 }
 module.exports = tweetController
