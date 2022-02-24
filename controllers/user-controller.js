9c87b1c9ba21334a4b98569a42e77b571446db6a
hsinyi8997 <hsinyi8997@gmail.com>
2022-02-24 20:44:14 +0800

    modify: remove sevices file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,26 +1,192 @@
-const userService = require('../services/user-services')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { User, Tweet, Reply } = require('../models')
+const { getUser } = require('../_helpers')
+const { imgurFileHandler } = require('../_helpers')
+const sequelize = require('sequelize')
 
 const userController = {
   signIn: (req, res, next) => {
-    userService.signIn(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    const { account, password } = req.body
+    if (!account || !password) throw new Error('account and password are required!')
+
+    return User.findOne({
+      where: { account }
+    })
+      .then(user => {
+        if (!user) throw new Error('User not exist!')
+        if (!bcrypt.compareSync(password, user.password)) throw new Error('password incorrect!')
+        const userData = user.toJSON()
+        delete userData.password
+        const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+        return res.status(200).json({
+          token,
+          user: userData
+        })
+      })
+      .catch(err => next(err))
   },
   signUp: (req, res, next) => {
-    userService.signUp(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    const { account, name, email, password, checkPassword } = req.body
+    if (password !== checkPassword) throw new Error('Passwords do not match!')
+
+    return User.findAll({
+      $or: [
+        { where: { account } },
+        { where: { email } },
+        { where: { name } }
+      ]
+    })
+      .then(users => {
+        if (users.some(u => u.email === email)) throw new Error('Email already exists!')
+        if (users.some(u => u.account === account)) throw new Error('Account already exists!')
+        if (users.some(u => u.name === name)) throw new Error('Name already exists!')
+
+        return bcrypt.hash(password, 10)
+      })
+      .then(hash => {
+        return User.create({
+          account,
+          password: hash,
+          name,
+          email,
+          role: ''
+        })
+      })
+      .then(newUser => {
+        const user = newUser.toJSON()
+        delete user.password
+        return res.status(200).json(user)
+      })
+      .catch(err => next(err))
   },
   getUser: (req, res, next) => {
-    userService.getUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    const userId = Number(req.params.id)
+    User.findByPk(userId, {
+      attributes: { exclude: ['password'] },
+      include: [
+        { model: User, as: 'Followers', attributes: { exclude: ['password'] } },
+        { model: User, as: 'Followings', attributes: { exclude: ['password'] } }
+      ]
+    })
+      .then(user => {
+        if (!user) throw new Error('User not exits!')
+        if (user.role === 'admin') throw new Error('User not exits!')
+        return res.status(200).json(user)
+      })
+      .catch(err => next(err))
   },
   putUserSetting: (req, res, next) => {
-    userService.putUserSetting(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    const { account, name, email, password, checkPassword } = req.body
+    if (password !== checkPassword) throw new Error('Passwords do not match!')
+    if (!account || !name || !email) throw new Error('Account, name and email are required!')
+    if (getUser(req).id !== Number(req.params.id)) throw new Error('permission denied')
+    return Promise.all([
+      User.findAll({
+        where: {
+          $or: [
+            { account },
+            { email }
+          ]
+        },
+        raw: true,
+        nest: true
+      }),
+      User.findByPk(Number(req.params.id)),
+      bcrypt.hash(password, 10)
+    ])
+      .then(([checkUsers, user, hash]) => {
+        if (checkUsers.some(u => u.email === email)) throw new Error('email is registered')
+        if (checkUsers.some(u => u.account === account)) throw new Error('account is registered')
+        return user.update({
+          account,
+          name,
+          email,
+          password: hash
+        })
+      })
+      .then(updatedUser => res.status(200).json({ user: updatedUser }))
+      .catch(err => next(err))
   },
   putUser: (req, res, next) => {
-    userService.putUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    const getUserId = Number(req.params.id)
+    const { name, introduction } = req.body
+    if (!name) throw new Error('name is required!')
+    if (getUser(req).id !== getUserId) throw new Error('permission denied')
+    const { files } = req
+    return Promise.all([
+      User.findByPk(getUserId),
+      imgurFileHandler(files.avatar),
+      imgurFileHandler(files.cover)
+    ])
+      .then(([user, avatarPath, coverPath]) => {
+        if (!user) throw new Error('user not exist!')
+        return user.update({
+          name,
+          introduction,
+          avatar: avatarPath || user.avatar,
+          cover: coverPath || user.cover
+        })
+      })
+      .then(updatedUser => res.status(200).json({ user: updatedUser }))
+      .catch(err => next(err))
   },
   getUserTweets: (req, res, next) => {
-    userService.getUserTweets(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    const getUserId = Number(req.params.id)
+    return Tweet.findAll({
+      where: { UserId: getUserId },
+      attributes: [
+        ['id', 'tweetId'],
+        'createdAt',
+        'description',
+        'image',
+        [
+          sequelize.literal(
+            '(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id)'
+          ),
+          'LikesCount'
+        ],
+        [
+          sequelize.literal(
+            '(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id)'
+          ),
+          'RepliesCount'
+        ]
+      ],
+      include: [
+        { model: User, attributes: ['id', 'name', 'account', 'avatar'] }
+      ],
+      order: [['createdAt', 'DESC']],
+      group: 'tweetId',
+      nest: true,
+      raw: true
+    })
+      .then(tweets => {
+        if (!tweets) throw new Error('User not exits!')
+        return res.status(200).json(tweets)
+      })
+      .catch(err => next(err))
   },
   getReplies: (req, res, next) => {
-    userService.getReplies(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    const getUserId = Number(req.params.id)
+    return Reply.findAll({
+      where: { UserId: getUserId },
+      attributes: [
+        ['id', 'replyId'],
+        'comment',
+        'createdAt'
+      ],
+      include: [
+        { model: Tweet, attributes: [['id', 'tweetId'], 'description', 'image'] },
+        { model: User, attributes: ['id', 'name', 'account', 'avatar'] }
+      ],
+      order: [['createdAt', 'DESC']]
+    })
+      .then(replies => {
+        if (!replies) throw new Error('No replies')
+        return res.status(200).json(replies)
+      })
+      .catch(err => next(err))
   }
 }
 
