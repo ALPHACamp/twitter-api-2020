d3c0eefb74f928669c9e4e68ebebb0ba02255163
Prysline <mayhu1993@gmail.com>
2022-02-25 18:54:00 +0800

    feat: 建立 PUT /api/users/:id 功能 上傳圖片需要的 file-helpers

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -5,7 +5,12 @@ const { apiErrorHandler } = require('../../middleware/error-handlers')
 const userController = require('../../controllers/apis/user-controller')
 const adminController = require('../../controllers/apis/admin-controller')
 const tweetController = require('../../controllers/apis/tweet-controller')
+const upload = require('../../middleware/multer')
 const { authenticated, authenticatedAdmin, authenticatedUser } = require('../../middleware/apiAuth')
+const uploadFields = [
+  { name: 'avatar', maxCount: 1 },
+  { name: 'cover', maxCount: 1 }
+]
 
 router.post('/tweets/:id/like', tweetController.addLike)
 router.post('/tweets/:id/unlike', tweetController.removeLike)
@@ -15,6 +20,8 @@ router.get('/tweets/:id', tweetController.getTweet)
 router.post('/tweets', tweetController.postTweet)
 router.get('/tweets', tweetController.getTweets)
 router.post('/users/signin', passport.authenticate('local', { session: false }), userController.signIn)
+router.put('/users/:id/account', userController.putUserAccount)
+router.put('/users/:id', upload.fields(uploadFields), userController.putUser)
 router.post('/users', userController.signUp)
 router.post('/admin/signin', passport.authenticate('local', { session: false }), adminController.signIn)
 
