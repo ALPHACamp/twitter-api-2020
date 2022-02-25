ffc7f45507343acf5af6ab376065eac07a3cf475
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-02-25 23:58:41 +0800

    init passport and set jwt authentication

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+if (process.env.NODE_NEV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
 
-// use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
-  // passport.authenticate('jwt', { ses...
-};
+// // use helpers.getUser(req) to replace req.user
+// function authenticated (req, res, next) {
+//   // passport.authenticate('jwt', { ses...
+// };
+
+app.use(passport.initialize())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
