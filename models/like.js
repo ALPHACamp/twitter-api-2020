bb785ae3adf841beca711639c55a511bb70486d4
chenchiachi <chenchiachi.ccc@gmail.com>
2022-02-25 10:28:29 +0100

    fix like model

--- models/like.js
+++ models/like.js
@@ -1,12 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER
+    },
     UserId: DataTypes.INTEGER,
-    TweetId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Like',
-    tableName: 'Likes',
+    tableName: 'Likes'
   });
   Like.associate = function(models) {
     Like.belongsTo(models.User, { foreignKey: 'UserId' })
