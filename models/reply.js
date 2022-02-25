79f23e28f15158f23da07a8ccf3470cf7187cf74
Young <phanton0117@gmail.com>
2022-02-25 21:42:08 +0800

    feat: add Reply model

--- models/reply.js
+++ models/reply.js
@@ -1,7 +1,13 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
-  }, {})
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER,
+    comment: DataTypes.TEXT
+  }, {
+    modelName: 'Reply',
+    tableName: 'Replies'
+  })
   Reply.associate = function (models) {
   }
   return Reply
