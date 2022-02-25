acd54ffffb3954e4b1e4453041c41797ea5c1f26
Young <phanton0117@gmail.com>
2022-02-25 17:22:04 +0800

    feat: add User model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,59 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-  }, {})
+  }, {
+    account: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    introduction: DataTypes.STRING,
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'user'
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://i.imgur.com/q6bwDGO.png'
+    },
+    cover: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://i.imgur.com/1jDf2Me.png'
+    },
+    tweetCount: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    followerCount: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    followingCount: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    },
+    likeCount: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+    }
+  }, {
+    modelName: 'User',
+    tableName: 'Users'
+  })
   User.associate = function (models) {
   }
   return User
