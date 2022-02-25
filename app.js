f58d1b2b249ae2d6922f47671aa72f2d64571ac1
hsinyi8997 <hsinyi8997@gmail.com>
2022-02-25 20:42:54 +0800

    modify: app.js port setting

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
