7f05e7e1564e86451fd18b89697c807f48408385
Young <phanton0117@gmail.com>
2022-02-25 21:45:04 +0800

    feat: add Like model

--- models/like.js
+++ models/like.js
@@ -1,7 +1,12 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
-  }, {})
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
+  }, {
+    modelName: 'Like',
+    tableName: 'Likes'
+  })
   Like.associate = function (models) {
   }
   return Like
