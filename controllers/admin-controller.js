228dca57482ffcc9d04e274db3568a9e00de941f
songerard <songerard@gmail.com>
2022-02-26 22:08:12 +0800

    Add tweet and user info in admin get tweets

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -4,25 +4,25 @@ const jwt = require('jsonwebtoken')
 
 const adminController = {
   signIn: (req, res, next) => {
-  const { account, password } = req.body
-  if (!account || !password) throw new Error('account and password are required!')
+    const { account, password } = req.body
+    if (!account || !password) throw new Error('account and password are required!')
 
-  return User.findOne({
-    where: { account }
-  })
-    .then(user => {
-      if (!user) throw new Error('User not exist!')
-      if (user.role !== 'admin') throw new Error('User not exist!')
-      if (!bcrypt.compareSync(password, user.password)) throw new Error('password incorrect!')
-      const userData = user.toJSON()
-      delete userData.password
-      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
-      return res.status(200).json({
-        token,
-        user: userData
-      })
+    return User.findOne({
+      where: { account }
     })
-    .catch(err => next(err))
+      .then(user => {
+        if (!user) throw new Error('帳號不存在！')
+        if (user.role !== 'admin') throw new Error('帳號不存在！')
+        if (!bcrypt.compareSync(password, user.password)) throw new Error('password incorrect!')
+        const userData = user.toJSON()
+        delete userData.password
+        const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+        return res.status(200).json({
+          token,
+          user: userData
+        })
+      })
+      .catch(err => next(err))
   },
   getAdminUsers: (req, res, next) => {
     return User.findAll({
@@ -34,9 +34,26 @@ const adminController = {
       .catch(err => next(err))
   },
   getAdminTweets: (req, res, next) => {
-    return Tweet.findAll()
+    return Tweet.findAll({
+      attributes: ['id', 'description', 'createdAt'],
+      order: [['createdAt', 'DESC']],
+      include: { model: User, attributes: ['id', 'name', 'account'] }
+    })
       .then(tweets => {
-        res.status(200).json(tweets)
+        const result = tweets
+          .map(t => ({
+            ...t.toJSON()
+          }))
+
+        result.forEach(r => {
+          r.tweetId = r.id
+          r.description = r.description.toString(0, 50)
+          r.User.userId = r.User.id
+          delete r.id
+          delete r.User.id
+        })
+
+        res.status(200).json(result)
       })
       .catch(err => next(err))
   },
@@ -47,7 +64,7 @@ const adminController = {
     })
       .then(deletedTweet => {
         if (!deletedTweet) throw new Error('Tweet not exist!')
-        return res.status(200).json({ message: 'Tweet deleted!'})
+        return res.status(200).json({ message: 'Tweet deleted!' })
       })
       .catch(err => next(err))
   }
