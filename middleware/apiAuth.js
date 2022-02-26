f0d1ec7d96d022e25cbee9d5f22eb32f5fbbdf05
xnubber <aabbccdd10902@gmail.com>
2022-02-26 19:30:44 +0800

    fix env problem

--- middleware/apiAuth.js
+++ middleware/apiAuth.js
@@ -2,7 +2,7 @@ const passport = require('../config/passport')
 const helpers = require('../_helpers')
 
 const authenticated = (req, res, next) => {
-  if (process.env.NODE_ENV === 'test') {
+  if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'travis') {
     passport.authenticate('jwt', { session: false }, (err, user) => {
       if (err || !user) {
         return res.status(401).json({
@@ -18,12 +18,12 @@ const authenticated = (req, res, next) => {
   }
 }
 const authenticatedAdmin = (req, res, next) => {
-  if (process.env.NODE_ENV === 'test') return next()
+  if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'travis') return next()
   if (req.user && helpers.getUser(req).role === 'admin') return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
 const authenticatedUser = (req, res, next) => {
-  if (process.env.NODE_ENV === 'test') return next()
+  if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'travis') return next()
   if (req.user && helpers.getUser(req).role === 'user') return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
