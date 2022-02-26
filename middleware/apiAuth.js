0cace068b5c6fbe76806cd5af816e2719b0d14d8
xnubber <aabbccdd10902@gmail.com>
2022-02-26 12:13:25 +0800

    fix auth to pass test

--- middleware/apiAuth.js
+++ middleware/apiAuth.js
@@ -10,19 +10,19 @@ const authenticated = (req, res, next) => {
           message: 'permission denied!'
         })
       }
+      req.user = user
       return next()
     })(req, res, next)
   }
-  return passport.authenticate('jwt', { session: false })(req, res, next)
 }
 const authenticatedAdmin = (req, res, next) => {
   if (process.env.NODE_ENV === 'test') return next()
-  if (helpers.getUser(req) && helpers.getUser(req).role === 'admin') return next()
+  if (req.user && helpers.getUser(req).role === 'admin') return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
 const authenticatedUser = (req, res, next) => {
   if (process.env.NODE_ENV === 'test') return next()
-  if (helpers.getUser(req) && helpers.getUser(req).role === 'user') return next()
+  if (req.user && helpers.getUser(req).role === 'user') return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
 
