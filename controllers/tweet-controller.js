d39ab2bf19b812c766a58ce396f11e916ea78e04
songerard <songerard@gmail.com>
2022-02-26 16:28:46 +0800

    Add counts, isLiked and user info in get tweets

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,13 +1,37 @@
-const { Tweet, Reply } = require('../models')
+const { Tweet, Reply, Like, User } = require('../models')
 const helpers = require('../_helpers')
 
 const tweetController = {
   getTweets: (req, res, next) => {
+    const reqUserId = helpers.getUser(req).id
     return Tweet.findAll({
-      order: [[ 'createdAt', 'DESC' ]]
+      order: [['createdAt', 'DESC']],
+      include: [
+        { model: Reply },
+        { model: Like },
+        { model: User }
+      ],
     })
       .then(tweets => {
-        res.status(200).json(tweets)
+        const tweetsArray = tweets
+          .map(t => ({
+            ...t.toJSON()
+          }))
+
+        tweetsArray
+          .forEach(tweet => {
+            tweet.name = tweet.User.name
+            tweet.account = tweet.User.account
+            tweet.isLiked = tweet.Likes.some(like => like.UserId === reqUserId)
+            tweet.likesCount = tweet.Likes.length
+            tweet.repliesCount = tweet.Replies.length
+            delete tweet.userId
+            delete tweet.Likes
+            delete tweet.Replies
+            delete tweet.User
+          })
+
+        return res.json(tweetsArray)
       })
       .catch(err => next(err))
   },
