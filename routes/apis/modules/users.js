edc661d13fafd1b52ea65dbc435a2c303315685d
DAI <senseye935@gmail.com>
2022-02-27 07:12:46 +0800

    add user-putUser router

--- routes/apis/modules/users.js
+++ routes/apis/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const upload = require('../../../middleware/multer')
 
 const { authenticated } = require('../../../middleware/api-auth')
 const userController = require('../../../controllers/user-controller')
@@ -12,5 +13,7 @@ router.get('/:id/followers', authenticated, tempController.getFollowers)
 router.get('/:id/followings', authenticated, tempController.getFollowings)
 router.get('/:id/likes', tempController.getLikes)
 router.get('/:id', authenticated, userController.getUser)
+router.put('/:id', authenticated, upload.fields([{ name: 'avatar', maxCount: 1 },{ name: 'cover', maxCount: 1 }]), userController.putUser)
+
 
 exports = module.exports = router
