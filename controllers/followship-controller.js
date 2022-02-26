f4639e30951f9f928c02db69b928b78ce394e707
wenrou77 <wendyog@gmail.com>
2022-02-27 02:12:04 +0800

    feat: remove followship

--- controllers/followship-controller.js
+++ controllers/followship-controller.js
@@ -48,6 +48,51 @@ const followshipController = {
         message: error
       })
     }
+  },
+  removeFollowing: async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.id)
+      if (!user) {
+        return res
+          .status(404)
+          .json({
+            status: 'error',
+            message: '您要取消追蹤的使用者不存在'
+          })
+      }
+      if (req.user.id == req.params.id) {
+        return res
+          .status(400)
+          .json({
+            status: 'error',
+            message: '不可取消追蹤自己'
+          })
+      }
+      const followingUser = await Followship.findOne({
+        where: {
+          followingId: req.params.id,
+        }
+      })
+      if (!followingUser) {
+        return res
+          .status(400)
+          .json({
+            status: 'error',
+            message: '您本來就無追蹤過此使用者'
+          })
+      } else {
+        followingUser.destroy()
+      }
+      return res.status(200).json({
+        status: 'success',
+        message: '成功取消追蹤此使用者!'
+      })
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error
+      })
+    }
   }
 }
 module.exports = followshipController
--- routes/modules/followship.js
+++ routes/modules/followship.js
@@ -4,5 +4,6 @@ const followshipController = require('../../controllers/followship-controller')
 const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
 
 router.post('/:id', authenticated, followshipController.addFollowing)
+router.delete('/:id', authenticated, followshipController.removeFollowing)
 
 module.exports = router
