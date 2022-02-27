4f6a2a7919b4f5c961986cc4fc48eeaf3fe2ffc4
martinchiu <martin87713@gmail.com>
2022-02-27 21:28:08 +0800

    refactor: modify error handler(把錯誤代碼用文字的形式傳給前端)

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const helpers = require('../_helpers')
 
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ status: 'error', message: '沒有提供正確的authencation token' })
+    if (err || !user) return res.status(200).json({ status: 'error', statusCode: '401', message: '沒有提供正確的authencation token' })
     req.user = user
     next()
   })(req, res, next)
@@ -11,19 +11,19 @@ const authenticated = (req, res, next) => {
 
 const authenticatedUser = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ status: 'error', message: '沒有提供正確的authencation token' })
+    if (err || !user) return res.status(200).json({ status: 'error', statusCode: '401', message: '沒有提供正確的authencation token' })
     req.user = user
     if (helpers.getUser(req).role !== 'admin') return next()
-    return res.status(403).json({ status: 'error', message: '管理者不能使用前台API功能' })
+    return res.status(200).json({ status: 'error', statusCode: '403', message: '管理者不能使用前台API功能' })
   })(req, res, next)
 }
 
 const authenticatedAdmin = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ status: 'error', message: '沒有提供正確的authencation token' })
+    if (err || !user) return res.status(200).json({ status: 'error', statusCode: '401', message: '沒有提供正確的authencation token' })
     req.user = user
     if (helpers.getUser(req).role === 'admin') return next()
-    return res.status(403).json({ status: 'error', message: '權限不足，沒有提供後台管理員的 authencation token' })
+    return res.status(200).json({ status: 'error', statusCode: '403', message: '權限不足，沒有提供後台管理員的 authencation token' })
   })(req, res, next)
 }
 
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,23 +2,27 @@ module.exports = {
   apiErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
       if (err.message === '帳號不存在') {
-        return res.status(404).json({
+        return res.status(200).json({
           status: 'error',
+          statusCode: '404',
           message: `${err.name}: ${err.message}`
         })
       }
-      res.status(400).json({
+      res.status(200).json({
         status: 'error',
+        statusCode: '400',
         message: `${err.name}: ${err.message}`
       })
     } else if (err instanceof RangeError) {
-      res.status(400).json({
+      res.status(200).json({
         status: 'error',
+        statusCode: '400',
         message: `${err.name}: ${err.message}`
       })
     } else {
-      res.status(400).json({
+      res.status(200).json({
         status: 'error',
+        statusCode: '400',
         message: `${err}`
       })
     }
