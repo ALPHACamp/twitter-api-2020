7e9ad299381a97e7fd6e033c509d82a61219a5d3
phiphi0912 <610733005@gms.ndhu.edu.tw>
2022-02-28 00:52:14 +0800

    refactor: replyController and replyService

--- controllers/reply-controllers.js
+++ controllers/reply-controllers.js
@@ -1,51 +1,32 @@
 const { User, Tweet, Reply } = require('../models')
+const replyServices = require('../services/reply-service')
 const helper = require('../_helpers')
 
 const replyController = {
   // Get all replies include user on one tweet
   getReplies: async (req, res, next) => {
+    const TweetId = req.params.tweet_id
     try {
-      const TweetId = req.params.tweet_id
-
-      // Check if tweet exists
-      const tweet = await Tweet.findByPk(TweetId)
-      if (!tweet) throw new Error('This tweet does not exist.')
-
-      const replies = await Reply.findAll({
-        where: { TweetId },
-        include: [{ model: User, attributes: ['name', 'account', 'avatar'] }],
-        order: [['createdAt', 'DESC']]
-      })
+      const replies = await replyServices.getReplies(TweetId)
 
       return res.status(200).json(replies)
-    } catch (error) {
-      next(error)
+    } catch (err) {
+      next(err)
     }
   },
 
   // Add a new reply
   postReply: async (req, res, next) => {
+    const { comment } = req.body
+    const TweetId = req.params.tweet_id
+    const user = helper.getUser(req)
+
     try {
-      const { comment } = req.body
-      const TweetId = req.params.tweet_id
-      const user = helper.getUser(req)
-
-      // Check if tweet exists
-      const tweet = await Tweet.findByPk(TweetId)
-      if (!tweet) throw new Error('This tweet does not exist.')
-
-      // Check if comment is valid
-      if (!comment.trim()) throw new Error('Comment must not be empty')
-
-      const reply = await Reply.create({
-        UserId: user.id,
-        TweetId,
-        comment
-      })
+      const reply = await replyServices.postReply(comment, TweetId, user)
 
       return res.status(200).json(reply)
-    } catch (error) {
-      next(error)
+    } catch (err) {
+      next(err)
     }
   }
 }
