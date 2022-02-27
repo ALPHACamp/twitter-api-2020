43219b2341978b68f67b27751aa5ffb5071a32e9
songerard <songerard@gmail.com>
2022-02-27 22:07:29 +0800

    Refactor user seeder file

--- seeders/20220222142729-users-seed-file.js
+++ seeders/20220222142729-users-seed-file.js
@@ -5,7 +5,9 @@ const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // create 6 users, only root user is admin
-    await queryInterface.bulkInsert('Users', [{
+
+    const users = []
+    users.push({
       account: 'root',
       password: await bcrypt.hash('12345678', 10),
       name: 'root',
@@ -16,67 +18,25 @@ module.exports = {
       cover: 'https://loremflickr.com/600/200/nature?random=100',
       createdAt: new Date(),
       updatedAt: new Date()
-    },
-    {
-      account: 'user1',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user1',
-      email: 'user1@example.com',
-      avatar: 'https://loremflickr.com/140/140/people?random=100',
-      introduction: faker.lorem.text().substring(0, 100),
-      role: '',
-      cover: 'https://loremflickr.com/600/200/nature?random=100',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      account: 'user2',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user2',
-      email: 'user2@example.com',
-      avatar: 'https://loremflickr.com/140/140/people?random=100',
-      introduction: faker.lorem.text().substring(0, 100),
-      role: '',
-      cover: 'https://loremflickr.com/600/200/nature?random=100',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      account: 'user3',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user3',
-      email: 'user3@example.com',
-      avatar: 'https://loremflickr.com/140/140/people?random=100',
-      introduction: faker.lorem.text().substring(0, 100),
-      role: '',
-      cover: 'https://loremflickr.com/600/200/nature?random=100',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      account: 'user4',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user4',
-      email: 'user4@example.com',
-      avatar: 'https://loremflickr.com/140/140/people?random=100',
-      introduction: faker.lorem.text().substring(0, 100),
-      role: '',
-      cover: 'https://loremflickr.com/600/200/nature?random=100',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      account: 'user5',
-      password: await bcrypt.hash('12345678', 10),
-      name: 'user5',
-      email: 'user5@example.com',
-      avatar: 'https://loremflickr.com/140/140/people?random=100',
-      introduction: faker.lorem.text().substring(0, 100),
-      role: '',
-      cover: 'https://loremflickr.com/600/200/nature?random=100',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    })
+
+    for (let i = 1; i <= 5; i++) {
+      users.push(
+        {
+          account: 'user' + i,
+          password: await bcrypt.hash('12345678', 10),
+          name: 'user' + i,
+          email: 'user' + i + '@example.com',
+          avatar: 'https://loremflickr.com/140/140/people?random=100',
+          introduction: faker.lorem.text().substring(0, 100),
+          role: '',
+          cover: 'https://loremflickr.com/600/200/nature?random=100',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+    }
+
+    await queryInterface.bulkInsert('Users', users, {});
   },
 
   down: async (queryInterface, Sequelize) => {
