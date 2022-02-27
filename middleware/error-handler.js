3f7376d6e958dabd210f1d5338d21168ba3d1286
martinchiu <martin87713@gmail.com>
2022-02-27 19:23:09 +0800

    feat: modify error handler(如果是帳號不存在的錯誤代碼設定為404)

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,12 @@
 module.exports = {
   apiErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
+      if (err.message === '帳號不存在') {
+        return res.status(404).json({
+          status: 'error',
+          message: `${err.name}: ${err.message}`
+        })
+      }
       res.status(400).json({
         status: 'error',
         message: `${err.name}: ${err.message}`
