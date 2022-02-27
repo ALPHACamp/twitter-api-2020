c088923b37be797507589b0d5d8a1fef0a53c1fa
hmrvc <hmrvc@yahoo.com.tw>
2022-02-27 10:32:28 +0800

    Add reply function

--- routes/modules/tweet.js
+++ routes/modules/tweet.js
@@ -3,10 +3,13 @@ const express = require('express')
 const router = express.Router()
 const tweetController = require('../../controllers/tweet-controller')
 const likeController = require('../../controllers/like-controller')
+const replyController = require('../../controllers/reply-controller')
 
 // tweet相關路由
 router.post('/:id/like', likeController.likeTweet)
 router.post('/:id/unlike', likeController.unlikeTweet)
+router.get('/:tweet_id/replies', replyController.getReplies)
+router.post('/:tweet_id/replies', replyController.addReply)
 router.get('/:tweet_id', tweetController.getTweet)
 router.post('/', tweetController.postTweet)
 router.get('/', tweetController.getTweets)
