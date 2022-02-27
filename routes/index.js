68d8903bb8d582419265e8b6a3fbf732de1be915
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-02-27 17:09:18 +0800

    modify:ajust coding style

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const passport = ('../config/passport')
-
 const userController = ('../controllers/user-controller')
 
-router.post('/api/users/signin', passport.authenticate('local', {
-  session: false
-}), userController.signIn)
 router.post('/api/users', userController.signUp)
 module.exports = router
