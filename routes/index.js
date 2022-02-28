daf3680a73e17d762cc3c7bd3e1e5789189c209d
Young <phanton0117@gmail.com>
2022-03-01 03:33:58 +0800

    feat: add error-handler & auth to router

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,19 @@ const admin = require('./modules/admin')
 const tweets = require('./modules/tweets')
 const users = require('./modules/users')
 const followships = require('./modules/followships')
+const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
-router.use('/api/tweets', tweets)
-router.use('/api/admin', admin)
+router.use('/api/admin', authenticated, authenticatedAdmin, admin)
+router.use('/api/tweets', authenticated, tweets)
+router.use('/api/followships', authenticated, followships)
+
+// TODO user router
 router.use('/api/users', users)
-router.use('/api/followships', followships)
+
+// TODO 登入 用這裡
+// router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn) // 注意是 Post
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
