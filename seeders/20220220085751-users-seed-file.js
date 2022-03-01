fe98d1cef0d665fdd971f38300db31151c21f001
chenchiachi <chenchiachi.ccc@gmail.com>
2022-03-01 23:05:22 +0100

    modify seeder

--- seeders/20220220085751-users-seed-file.js
+++ seeders/20220220085751-users-seed-file.js
@@ -14,8 +14,8 @@ module.exports = {
       role: 'admin',
       name: 'root',
       introduction: faker.lorem.text().substring(0, 160),
-      cover: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
-      avatar: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
+      cover: 'https://i.imgur.com/aWCidFh.jpeg',
+      avatar: 'https://i.imgur.com/jb7dyQ4.jpeg',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
@@ -26,8 +26,8 @@ module.exports = {
       role: 'user',
       name: 'user1',
       introduction: faker.lorem.text().substring(0, 160),
-      cover: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
-      avatar: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
+      cover: 'https://i.imgur.com/2MHUybP.jpeg',
+      avatar: 'https://i.imgur.com/twLCVen.jpeg',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
@@ -38,8 +38,8 @@ module.exports = {
       role: 'user',
       name: 'user2',
       introduction: faker.lorem.text().substring(0, 160),
-      cover: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
-      avatar: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
+      cover: 'https://i.imgur.com/9bQRQXh.jpeg',
+      avatar: 'https://i.imgur.com/p5CQC07.jpeg',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
@@ -50,8 +50,8 @@ module.exports = {
       role: 'user',
       name: 'user3',
       introduction: faker.lorem.text().substring(0, 160),
-      cover: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
-      avatar: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
+      cover: 'https://i.imgur.com/gcEvEDq.jpeg',
+      avatar: 'https://i.imgur.com/OdItn5D.jpeg',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
@@ -62,8 +62,8 @@ module.exports = {
       role: 'user',
       name: 'user4',
       introduction: faker.lorem.text().substring(0, 160),
-      cover: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
-      avatar: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
+      cover: 'https://i.imgur.com/NqEYhmH.jpeg',
+      avatar: 'https://i.imgur.com/H6qDZ2P.jpeg',
       createdAt: new Date(),
       updatedAt: new Date()
     },{
@@ -74,8 +74,8 @@ module.exports = {
       role: 'user',
       name: 'user5',
     introduction: faker.lorem.text().substring(0, 160),
-      cover: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
-      avatar: `https://loremflickr.com/320/240/monster/?random=${Math.random() * 100}`,
+      cover: 'https://i.imgur.com/fHWfByO.jpeg',
+      avatar: 'https://i.imgur.com/dTE51VX.jpeg',
       createdAt: new Date(),
       updatedAt: new Date()
     }], {})
