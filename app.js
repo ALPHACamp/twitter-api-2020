a183d158bc42e80335189dbbd1ff5834cb573d1c
Young <phanton0117@gmail.com>
2022-03-02 15:29:14 +0800

    feat: add CORS middleware

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const cors = require('cors')
+const { CORS } = require('./middleware/CORS')
+// const cors = require('cors')
 const helpers = require('./_helpers')
 const passport = require('./config/passport')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(cors({ credentials: true, origin: true }))
+// app.use(cors({ credentials: true, origin: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
@@ -17,8 +18,8 @@ app.use((req, res, next) => {
   res.locals.user = helpers.getUser(req)
   next()
 })
+app.use('/', CORS, routes)
 app.get('/', (req, res) => res.send('..Hello World!'))
-app.use(routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
---
+++ middleware/CORS.js
@@ -0,0 +1,12 @@
+const CORS = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS')
+  }
+  return next()
+}
+
+module.exports = {
+  CORS
+}
