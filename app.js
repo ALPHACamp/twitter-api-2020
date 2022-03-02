cdcb4d943d3555a342d6f8241cab6966a9073b3e
Young <phanton0117@gmail.com>
2022-03-02 17:47:55 +0800

    fix: fix cors

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const { CORS } = require('./middleware/CORS')
-// const cors = require('cors')
+const cors = require('cors')
 const helpers = require('./_helpers')
 const passport = require('./config/passport')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use(cors({ credentials: true, origin: true }))
+app.use(cors({ credentials: true, origin: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
@@ -18,7 +17,7 @@ app.use((req, res, next) => {
   res.locals.user = helpers.getUser(req)
   next()
 })
-app.use('/', CORS, routes)
+app.use(routes)
 app.get('/', (req, res) => res.send('..Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
