20f9403aa35356afe4fd8f22dfc482a754830dec
hsinyi8997 <hsinyi8997@gmail.com>
2022-03-03 10:03:15 +0800

    modify: comment unused code

--- _helpers.js
+++ _helpers.js
@@ -1,31 +1,31 @@
-const multer = require('multer')
-const imgur = require('imgur')
-const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+// const multer = require('multer')
+// const imgur = require('imgur')
+// const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
-//multer
-const upload = multer({ dest: 'temp/' })
+// //multer
+// const upload = multer({ dest: 'temp/' })
 
 //getUser
 function getUser(req) {
   return req.user;
 }
 
-//imgur
+// //imgur
 
-imgur.setClientId(IMGUR_CLIENT_ID)
-const imgurFileHandler = file => {
-  return new Promise((resolve, reject) => {
-    if (!file) return resolve(null)
-    return imgur.uploadFile(file[0].path)
-      .then(img => {
-        resolve(img?.link || null)
-      })
-      .catch(err => reject(err))
-  })
-}
+// imgur.setClientId(IMGUR_CLIENT_ID)
+// const imgurFileHandler = file => {
+//   return new Promise((resolve, reject) => {
+//     if (!file) return resolve(null)
+//     return imgur.uploadFile(file[0].path)
+//       .then(img => {
+//         resolve(img?.link || null)
+//       })
+//       .catch(err => reject(err))
+//   })
+// }
 
 module.exports = {
-  getUser,
-  upload,
-  imgurFileHandler
+  getUser
+  // upload,
+  // imgurFileHandler
 };
