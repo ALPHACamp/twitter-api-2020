69aca27e7784add4c7b6b6e4806380193fba797b
Young <phanton0117@gmail.com>
2022-03-03 16:04:37 +0800

    feat: add top10User & currentUser controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,8 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const { User } = require('../models')
+const { User, Tweet, Reply, Like, Followship, sequelize } = require('../models')
 const helpers = require('../_helpers')
-const sequelize = require('sequelize')
+const Op = require('../models').Sequelize.Op
 
 const userController = {
 
@@ -75,6 +75,165 @@ const userController = {
       })
       .then(updatedUser => res.status(200).json({ user: updatedUser }))
       .catch(err => next(err))
+  },
+  getUserTweets: async (req, res, next) => {
+    try {
+      const error = new Error()
+      const targetUserId = req.params.id
+      const loginUserId = helpers.getUser(req).id
+
+      if (isNaN(targetUserId) || !(await User.findByPk(targetUserId))) {
+        error.code = 404
+        error.message = '對應使用者不存在'
+        return next(error)
+      }
+
+      const tweets = await Tweet.findAll({
+        where: { UserId: targetUserId },
+        attributes: [
+          'id',
+          'UserId',
+          'description',
+          'createdAt',
+          'updatedAt',
+          'likeCount',
+          'replyCount',
+          [
+            sequelize.literal(
+              `EXISTS (SELECT 1 FROM Likes WHERE UserId = ${loginUserId} AND TweetId = Tweet.id)`
+            ),
+            'isLiked'
+          ],
+          [
+            sequelize.literal(
+              `EXISTS (SELECT 1 FROM Replies WHERE UserId = ${loginUserId} AND TweetId = Tweet.id)`
+            ),
+            'isReplied'
+          ]
+        ],
+        include: [
+          { model: User, attributes: ['id', 'name', 'account', 'avatar'] }
+        ],
+        order: [['createdAt', 'DESC']]
+      })
+
+      const results = tweets.map(t => {
+        t = t.toJSON()
+        t.isLiked = Boolean(t.isLiked)
+        t.isReplied = Boolean(t.isReplied)
+        return t
+      })
+
+      return res.json([...results])
+    } catch (err) {
+      next(err)
+    }
+  },
+  getRepliedTweets: (req, res, next) => {
+    const userId = Number(req.params.id)
+    return Promise.all([
+      User.findByPk(userId),
+      Reply.findAll({
+        where: { UserId: userId },
+        attributes: ['id', 'comment', 'createdAt', 'tweetId'],
+        include: [{
+          model: Tweet,
+          attributes: ['id', 'description', 'createdAt', 'replyCount', 'likeCount'],
+          include: [{ model: User, attributes: ['id', 'name', 'account', 'avatar'] }
+          ]
+        }]
+      })
+    ])
+      .then(([user, replies]) => {
+        if (!user) { throw new Error('帳號不存在!') }
+        if (!replies) throw new Error('使用者沒有回覆任何推文!')
+        return res.status(200).json(replies)
+      })
+      .catch(err => next(err))
+  },
+  getLikes: (req, res, next) => {
+    const userId = Number(req.params.id)
+    return Like.findAll({
+      where: { userId: userId },
+      attributes: ['id', 'createdAt', 'TweetId'],
+      order: [['createdAt', 'DESC']],
+      include: [{
+        model: Tweet,
+        attributes: ['id', 'description', 'createdAt', 'replyCount', 'likeCount'],
+        include: [{
+          model: User,
+          attributes: ['id', 'name', 'account', 'avatar']
+        }]
+      }]
+    })
+      .then(userLikes => {
+        if (!userLikes) throw new Error('使用者並未喜歡任何推文!')
+        return res.status(200).json(userLikes)
+      })
+      .catch(err => next(err))
+  },
+  getUserFollowings: (req, res, next) => {
+
+  },
+  getUserFollowers: (req, res, next) => {
+
+  },
+  getTopUsers: async (req, res, next) => {
+    try {
+      const user = helpers.getUser(req)
+      const users = await User.findAll({
+        where: {
+          id: {
+            [Op.ne]: user.id
+          },
+          role: 'user'
+        },
+        include: [{ model: User, as: 'Followers', attributes: { exclude: ['password'] } }],
+        attributes: [
+          'id',
+          'name',
+          'avatar',
+          'account',
+          'followerCount'
+        ],
+        order: [[sequelize.literal('followerCount'), 'DESC']],
+        limit: 10
+      })
+
+      const followedUsers = helpers.getUser(req).Followings
+
+      const results = users.map(u => ({
+        ...u.toJSON(),
+        isFollowed: followedUsers.some(fu => fu.id === u.id)
+      }))
+
+      return res.json({ status: 'success', message: '成功獲取', data: results })
+    } catch (err) {
+      next(err)
+    }
+  },
+  getCurrentUser: async (req, res, next) => {
+    try {
+      const userId = helpers.getUser(req).id
+
+      const currentUser = await User.findByPk(userId, {
+        attributes: [
+          'id',
+          'account',
+          'email',
+          'name',
+          'avatar',
+          'role',
+          'cover',
+          'followerCount',
+          'followingCount',
+          'tweetCount'
+        ]
+      })
+      return res.json({ status: 'success', message: '成功獲取當下登入的使用者', data: currentUser })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,17 +1,21 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
-// const { apiErrorHandler } = require('../../middleware/error-handler')
-const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
+const { authenticated } = require('../../middleware/auth')
 const upload = require('../../middleware/multer')
 
-// router.use('/users', users)
-// router.post('/signIn', userController.signIn)
+router.get('/:id/tweets', authenticated, userController.getUserTweets)
+router.get('/:id/replied_tweets', authenticated, userController.getRepliedTweets)
+router.get('/:id/likes', authenticated, userController.getLikes)
+// router.get('/:id/followings', authenticated, userController.getUserFollowings)
+// router.get('/:id/followers', authenticated, userController.getUserFollowers)
+router.get('/top', authenticated, userController.getTopUsers)
+router.get('/current_user', authenticated, userController.getCurrentUser)
 router.get('/:id', authenticated, userController.getUser)
 router.put('/:id', authenticated,
   upload.fields([{ name: 'avatar' }, { name: 'cover' }]), userController.putUser)
-router.post('/', userController.signUp)
 
-// router.use('/', apiErrorHandler)
+// 前台註冊
+router.post('/', userController.signUp)
 
 module.exports = router
