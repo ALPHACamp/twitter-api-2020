437a9957625795a0b69d2756998be5020d7702a1
Richard Widjaya <ricwidjaya@gmail.com>
2022-03-04 16:25:28 +0800

    add reason and room to disconnect event

--- socket/server.js
+++ socket/server.js
@@ -1,23 +1,20 @@
-
-
 module.exports = server => {
   const io = require('socket.io')(server)
 
-  io.on('connection', (socket) => {
+  io.on('connection', socket => {
     console.log('---user connected---')
 
-
-    socket.on('chat message', (message) => {
+    socket.on('chat message', message => {
       console.log('message: ' + message)
 
       //發送 allMessage事件的訊息給所有連線用戶
       io.emit('chat message', message)
     })
 
-
-
-    socket.on('disconnect', () => {
-      console.log('Bye~');  // 顯示 bye~
-    });
+    socket.on('disconnect', reason => {
+      console.log(reason)
+      console.log(socket.rooms)
+      console.log('Bye~') // 顯示 bye~
+    })
   })
 }
