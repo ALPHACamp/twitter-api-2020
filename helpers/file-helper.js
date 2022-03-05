91f647ae985467f85b3c3ad92901237735b8928f
Young <phanton0117@gmail.com>
2022-03-05 23:52:57 +0800

    fix: fix file helper

--- helpers/file-helper.js
+++ helpers/file-helper.js
@@ -5,7 +5,7 @@ imgur.setClientId(IMGUR_CLIENT_ID)
 const imgurFileHandler = file => {
   return new Promise((resolve, reject) => {
     if (file === undefined) return resolve(null)
-    return imgur.uploadFile(file[0].path)
+    return imgur.uploadFile(file.path)
       .then(img => {
         const imgLink = img ? img.link : null
         resolve(imgLink)
