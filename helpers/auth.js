852c262700c870d9a75d15cceefeccfe46f39c7b
phiphi0912 <610733005@gms.ndhu.edu.tw>
2022-03-06 14:12:23 +0800

    refactor: authenticatedSocket into helpers

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,5 +1,6 @@
 const passport = require('../config/passport')
 const helpers = require('../_helpers')
+const jwt = require('jsonwebtoken')
 
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
@@ -25,7 +26,29 @@ const blockRole = (role) => {
   }
 }
 
+const authenticatedSocket = (socket, next) => {
+  console.log('=== Socket auth ===')
+
+  // Mock socket auth
+  socket.handshake.auth.token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiZW1haWwiOiJ1c2VyMUBleGFtcGxlLmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJEFSYnpVaW1PdE1BSkNPODNHSzdFM3V3RFhBa1BZdG1jSlJvS1hudktxNzcwdldEQzBGaE11IiwibmFtZSI6InVzZXIxIiwiYWNjb3VudCI6InVzZXIxIiwiY292ZXIiOiJodHRwczovL2kuaW1ndXIuY29tL2p1NXdGdDMuanBnIiwiYXZhdGFyIjoiaHR0cHM6Ly9pLmltZ3VyLmNvbS9oQUtjUzNFLmpwZyIsImludHJvZHVjdGlvbiI6bnVsbCwicm9sZSI6InVzZXIiLCJsaWtlZENvdW50IjoyMCwicmVwbGllZENvdW50IjozMCwiZm9sbG93aW5nQ291bnQiOjIsImZvbGxvd2VyQ291bnQiOjMsImNyZWF0ZWRBdCI6IjIwMjItMDMtMDVUMTI6NTE6MDMuMDAwWiIsInVwZGF0ZWRBdCI6IjIwMjItMDMtMDVUMTI6NTE6MDMuMDAwWiIsImlhdCI6MTY0NjQ4NDY3NSwiZXhwIjoxNjQ3MDg5NDc1fQ.r1I8-nQw4pVPKNnKOwj42C3JtcWy2piIVEECWGefzi8'
+
+  if (!socket.handshake.auth.token) throw Error('No socket token!')
+  jwt.verify(
+    socket.handshake.auth.token,
+    process.env.JWT_SECRET,
+    (err, decoded) => {
+      if (err) {
+        next(err)
+      }
+      socket.user = decoded
+    }
+  )
+  next()
+}
+
 module.exports = {
   authenticated,
-  blockRole
+  blockRole,
+  authenticatedSocket
 }
--- socket/server.js
+++ socket/server.js
@@ -1,29 +1,8 @@
-const jwt = require('jsonwebtoken')
 const PUBLIC_ROOM_ID = 1
 const messageServices = require('../services/message-service')
 const { generateMessage } = require('../helpers/message')
+const { authenticatedSocket } = require('../helpers/auth')
 
-// Auth middleware
-const authenticatedSocket = (socket, next) => {
-  console.log('=== Socket auth ===')
-
-  // Mock socket auth
-  socket.handshake.auth.token =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6OCwiZW1haWwiOiJ1c2VyMUBleGFtcGxlLmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJEFSYnpVaW1PdE1BSkNPODNHSzdFM3V3RFhBa1BZdG1jSlJvS1hudktxNzcwdldEQzBGaE11IiwibmFtZSI6InVzZXIxIiwiYWNjb3VudCI6InVzZXIxIiwiY292ZXIiOiJodHRwczovL2kuaW1ndXIuY29tL2p1NXdGdDMuanBnIiwiYXZhdGFyIjoiaHR0cHM6Ly9pLmltZ3VyLmNvbS9oQUtjUzNFLmpwZyIsImludHJvZHVjdGlvbiI6bnVsbCwicm9sZSI6InVzZXIiLCJsaWtlZENvdW50IjoyMCwicmVwbGllZENvdW50IjozMCwiZm9sbG93aW5nQ291bnQiOjIsImZvbGxvd2VyQ291bnQiOjMsImNyZWF0ZWRBdCI6IjIwMjItMDMtMDVUMTI6NTE6MDMuMDAwWiIsInVwZGF0ZWRBdCI6IjIwMjItMDMtMDVUMTI6NTE6MDMuMDAwWiIsImlhdCI6MTY0NjQ4NDY3NSwiZXhwIjoxNjQ3MDg5NDc1fQ.r1I8-nQw4pVPKNnKOwj42C3JtcWy2piIVEECWGefzi8'
-
-  if (!socket.handshake.auth.token) throw Error('No socket token!')
-  jwt.verify(
-    socket.handshake.auth.token,
-    process.env.JWT_SECRET,
-    (err, decoded) => {
-      if (err) {
-        next(err)
-      }
-      socket.user = decoded
-    }
-  )
-  next()
-}
 
 module.exports = server => {
   const io = require('socket.io')(server, {
@@ -32,6 +11,7 @@ module.exports = server => {
       methods: ['GET', 'POST'],
     }
   })
+  
   io.use(authenticatedSocket).on('connection', socket => {
     console.log('---user connected---')
     console.log('目前連線數量: ', socket.server.engine.clientsCount)
@@ -44,7 +24,6 @@ module.exports = server => {
 
     socket.on('join', ({ roomId }) => {
       roomId = Number(roomId)
-      console.log(roomId)
       socket.join(`${roomId}`)
       io.emit('test message', '後端 test 123，收到請回答')
 
@@ -67,7 +46,6 @@ module.exports = server => {
       if (message.replace(/\s+/, '') === '')
         throw new Error("message can't be null")
       console.log('message: ' + message)
-      console.log(socket.user)
 
       //發送 allMessage事件的訊息給所有連線用戶
       io.emit('chat message', message)
