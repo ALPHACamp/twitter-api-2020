94d09ff76b24c95d3ef351821cbecc3109665222
Dandy <lavenderflyyaya@gmail.com>
2022-03-06 20:26:23 +0800

    push

--- socket/index.js
+++ socket/index.js
@@ -13,21 +13,63 @@ const socket = server => {
     },
     allowEI03: true
   })
+
+  let numUsers = 0
+
   io.on('connection', socket => {
+    let joinUser = false
+
     socket.on('chat message', msg => {
       io.emit('chat message', msg)
+      socket.emit('chat message', {
+        userId: socket.userId,
+        username: socket.username,
+        msg
+      })
     })
 
-    socket.on('join', () => {
-      console.log('===== user join =====')
-      socket.join('chatroom')
-      socket.broadcast.to('chatroom').emit('===== User connect =====')
+    // when the client emits 'add user', this listens and executes
+    socket.on('join', (username) => {
+      if (joinUser) return
+
+
+      socket.username = username
+      ++numUsers
+      joinUser = true
+      socket.emit('login', {
+        numUsers: numUsers
+      })
+
+      socket.broadcast.emit('user joined', {
+        username: socket.username
+      })
     })
 
-    socket.on('leave', () => {
-      console.log('===== user leave =====')
-      socket.leave('chatroom')
-      socket.broadcast.to('chatroom').emit('===== User disconnect =====')
+    // when the client emits 'typing', we broadcast it to others
+    socket.on('typing', () => {
+      socket.broadcast.emit('typing', {
+        username: socket.username
+      })
+    })
+
+    // when the client emits 'stop typing', we broadcast it to others
+    socket.on('stop typing', () => {
+      socket.broadcast.emit('stop typing', {
+        username: socket.username
+      })
+    })
+
+    // when the user disconnects.. perform this
+    socket.on('disconnect', () => {
+      if (joinUser) {
+        --numUsers
+
+        // echo globally that this client has left
+        socket.broadcast.emit('user left', {
+          username: socket.username,
+          numUsers: numUsers
+        })
+      }
     })
   })
 }
