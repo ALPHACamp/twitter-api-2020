1b70039c6608ad86bd6a5e3278bc702c8cba216a
Dandy <lavenderflyyaya@gmail.com>
2022-03-10 11:42:01 +0800

    feat/disconnect

--- socket/index.js
+++ socket/index.js
@@ -54,16 +54,16 @@ const socket = server => {
     //   socket.emit('user join', msg)
     // })
     
-    // socket.on('disconnect', () => {
-    //   if (joinUser) {
-    //     const msg = '離開聊天室'
-    //     console.log(msg)
-    //     --numUsers
-    //     connectedUser.splice(connectedUser.indexOf(userName), 1)
-    //     socket.emit('user leave', msg)
-    //     updateUser()
-    //   }
-    // })
+    socket.on('disconnect', () => {
+      if (joinUser) {
+        const msg = '離開聊天室'
+        console.log(msg)
+        --numUsers
+        connectedUser.splice(connectedUser.indexOf(userName), 1)
+        socket.emit('user leave', msg)
+        updateUser()
+      }
+    })
 
     function updateUser() {
       io.emit('connectedUser', connectedUser)
