1d955b9893e697984ef13fff47bacf0716dc875d
GallonShih <gallonshih0526@gmail.com>
2022-05-10 23:35:56 +0800

    feet; add associate in like

--- models/tweet.js
+++ models/tweet.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       Tweet.belongsTo(models.User, { foreignKey: 'userId' })
       Tweet.hasMany(models.Reply, { foreignKey: 'tweetId' })
+      Tweet.hasMany(models.Like, { foreignKey: 'tweetId' })
       Tweet.belongsToMany(models.User, {
         through: models.Like,
         foreignKey: 'tweetId',
