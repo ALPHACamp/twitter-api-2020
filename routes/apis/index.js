8a3f7be3c8ec67e82c425276fb40dfa0fd3da038
KateChu <chu.shihchien@gmail.com>
2022-05-11 21:05:43 +0800

    feat: api/signin, signup; errorHandler/apiError, catchAsync

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/apis/user-controllers')
+const { apiErrorHandler, catchAsync } = require('../../middleware/error-handler')
 
-router.post('users', userController.signin)
+router.post('users', userController.signIn)
+router.post('/signin', catchAsync, userController.signUp)
+
+router.use('/', apiErrorHandler)
 
 module.exports = router
