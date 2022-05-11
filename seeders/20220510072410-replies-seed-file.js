73ec8cfc08f58084f39e3ae30dc62fda1bd16ed1
KateChu <chu.shihchien@gmail.com>
2022-05-11 08:26:11 +0800

    merge master

--- seeders/20220510072410-replies-seed-file.js
+++ seeders/20220510072410-replies-seed-file.js
@@ -1,29 +1,29 @@
-'use strict';
+'use strict'
 const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const tweets = await queryInterface.sequelize.query(
       'SELECT id FROM Users;',
-      { type: queryInterface.sequelize.QueryTypes.SELECT}
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
     const users = await queryInterface.sequelize.query(
       'SELECT id FROM Users;',
-      { type: queryInterface.sequelize.QueryTypes.SELECT}
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
     const replies = []
 
     for (let i = 0; i < tweets.length; i++) {
-      for(let j = 0; j < 3; j++) {
+      for (let j = 0; j < 3; j++) {
         const reply = {
           reply_text: faker.lorem.text(),
           created_at: new Date(),
           updated_at: new Date(),
-          user_id: users[ Math.floor(Math.random() * users.length) ].id,
+          user_id: users[Math.floor(Math.random() * users.length)].id,
           tweet_id: tweets[i].id
         }
         replies.push(reply)
-      }  
+      }
     }
 
     await queryInterface.bulkInsert('Replies', replies, {})
@@ -32,4 +32,4 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Replies', null, {})
   }
-};
+}
