6a6024a50e073cba2df92b712c6555310adcc2ed
maximeri <t.s.eliot1996@gmail.com>
2022-05-11 20:59:56 +0800

    create controller files and routes

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const express = require('express')
-const helpers = require('./_helpers')
-
-const app = express()
-const port = 3000
-
-// use helpers.getUser(req) to replace req.user
-function authenticated (req, res, next) {
-  // passport.authenticate('jwt', { ses...
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
+const express = require('express')
+const app = express()
+const { apis } = require('./routes')
+const port = process.env.PORT || 3000
+const session = require('express-session')
+const passport = require('./config/passport')
+const SESSION_SECRET = 'secret'
+const { getUser } = require('./helpers/auth-helpers')
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) // Express v4.16 以後的版本已內建 body-parser
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(passport.initialize()) // 增加這行，初始化 Passport
+app.use(passport.session())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
+  next()
+})
+app.use('/api', apis)
+app.listen(port, () => console.log(`App is listening on port ${port}!`))
 
 module.exports = app
