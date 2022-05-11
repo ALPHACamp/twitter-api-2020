2b6c9a0c7f0b7fa64c54b2a1bbc81a7e1b48046f
eruc <eruc101010@gmail.com>
2022-05-11 16:53:51 +0800

    feat: add api-auth file

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
 const router = express.Router()
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 
 router.post('/api/login', passport.authenticate('local', { session: false }), userController.login)
 
