9bd894ddce0138a0e37dff88266748247ff8f02d
maximeri <t.s.eliot1996@gmail.com>
2022-05-11 21:14:32 +0800

    feat: set up jwt strategy and signin api

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,40 @@
 const passport = require('passport')
+const passportJWT = require('passport-jwt')
+const { User, Tweet, Reply } = require('../models')
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
 
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
+passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+  User.findByPk(jwtPayload.id, {
+    include: [
+      { model: Tweet, as: 'LikedTweets' },
+      { model: Reply, as: 'LikedReplies' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+    .then(user => cb(null, user))
+    .catch(err => cb(err))
+}))
+// serialize and deserialize user
+// 序列化 (serialization) 是一種轉換資料轉化為可儲存形式的過程 (process)，並且這個形式能夠在需要的時候恢復原先狀態 (反序列化)
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+passport.deserializeUser((id, cb) => {
+  return User.findByPk(id, {
+    include: [
+      { model: Tweet, as: 'LikedTweets' },
+      { model: Reply, as: 'LikedReplies' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+    .then(user => cb(null, user.toJSON()))
+    .catch(err => cb(err))
+})
 module.exports = passport
