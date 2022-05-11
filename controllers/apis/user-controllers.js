bd5990c7510040cc183cb03709737ba8c46ed45f
KateChu <chu.shihchien@gmail.com>
2022-05-11 16:33:16 +0800

    feat: 登入 api

---
+++ controllers/apis/user-controllers.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+const userController = {
+  signIn: (req, res, next) => {
+    try {
+      const userData = req.user.toJSON()
+      delete userData.password
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = userController
