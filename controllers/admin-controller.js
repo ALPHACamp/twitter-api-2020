11c72d75881ed8eada546e8133e580664eba8654
JINGWEN LIN <yaysa903@gmail.com>
2022-05-12 23:30:45 +0800

    feat: add get all users feature for admin

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,31 @@
-const { Tweet } = require('../models')
+const { User, Tweet, sequelize } = require('../models')
 
 const adminController = {
+  getAllUsers: async (req, res, next) => {
+    try {
+      const user = await User.findAll({
+        attributes: [
+          'name', 'account', 'avatar',
+          [sequelize.literal('(SELECT COUNT(DISTINCT id) FROM Tweets WHERE Tweets.User_id = User.id)'),
+            'tweetCounts'],
+          [sequelize.literal('(SELECT COUNT(DISTINCT id) FROM Replies WHERE Replies.User_id = User.id)'),
+            'replyCounts'],
+          [sequelize.literal('(SELECT COUNT(DISTINCT id) FROM Likes WHERE Likes.User_id = User.id)'),
+            'likeCounts'],
+          [sequelize.literal('(SELECT COUNT(DISTINCT id) FROM Followships WHERE Followships.Follower_id = User.id)'),
+            'FollowingCounts'],
+          [sequelize.literal('(SELECT COUNT(DISTINCT id) FROM Followships WHERE Followships.Following_id = User.id)'), 'FollowerCounts']
+        ],
+        order: [[sequelize.col('tweetCounts'), 'DESC'], ['created_at']],
+        nest: true,
+        raw: true
+      })
+      
+      res.json(user)
+    } catch (err) {
+      next(err)
+    }
+  },
   deleteTweets: async (req, res, next) => {
     try {
       const deletedTweetId = req.params.id
