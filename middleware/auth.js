3eb6cf61f2d72d30e80355d880332b13b2d1ff8e
maximeri <t.s.eliot1996@gmail.com>
2022-05-12 21:10:17 +0800

    feat: add getUser api

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,22 @@
 const passport = require('../config/passport')
+const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
-
+    req.user = user
     next()
   })(req, res, next)
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) return next()
-
-  return res.status(403).json({ status: 'error', message: 'permission denied' })
+  if (req.user && req.user.role === 'admin') return next()
+  return res.status(403).json({ status: 'error', message: 'admin permission denied' })
 }
 
 const authenticatedUser = (req, res, next) => {
-  if (req.user && req.user.isAdmin) return next()
-
-  return res.status(403).json({ status: 'error', message: 'permission denied' })
+  if (req.user && req.user.role === 'user') return next()
+  return res.status(403).json({ status: 'error', message: 'user permission denied' })
 }
 
 module.exports = {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../../controllers/user-controller')
+router.get('/:id', userController.getUser) // 瀏覽使用者資料
+router.put('/:id', userController.putUser) // 修改使用者資料
+// router.get('/:id/tweets', userController.getTweets) // 瀏覽使用者發過的推文
+// router.get('/:id/replied_tweets', userController.getRepliedTweets) // 瀏覽使用者回覆的推文
+// router.get('/:id/likes', userController.getLikes) // 瀏覽使用者like的推文
+// router.get('/:id/followings', userController.getFollowings) // 瀏覽使用者跟隨的人
+// router.get('/:id/followers', userController.getFollowers) // 瀏覽使用者的跟隨者
 
 module.exports = router
