710a4d80dd533d7da73297d1e64ac4a06a4cce44
HitomiHuang <black011602@gmail.com>
2022-05-13 02:42:23 +0800

    feat: Fix getUser feature

--- app.js
+++ app.js
@@ -2,25 +2,16 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-
 const helpers = require('./_helpers');
-const routes = require('./routes')
 const passport = require('./config/passport')
-
-
+const routes = require('./routes')
 
 const app = express()
 const port = 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use((req, res, next) => {
-  res.locals.loginUser = helpers.getUser(req)
-  next()
-})
-
 app.use(passport.initialize())
-app.use(passport.session())
 app.use('/api', routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,21 @@
 const passport = require('../config/passport')
-const authenticated = passport.authenticate('jwt', { session: false })
+
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+    if (user) {
+      req.user = user.dataValues
+      // console.log(req.user)
+    } 
+    return next()
+  })(req, res, next)
+}
 
 const authenticatedAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
+
 module.exports = {
   authenticated,
   authenticatedAdmin
