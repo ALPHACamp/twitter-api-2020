0262578ad4f997ab5ba05cdd135f3838bc6401d5
HitomiHuang <black011602@gmail.com>
2022-05-12 14:53:29 +0800

    feat: add signup and getUser feature

--- app.js
+++ app.js
@@ -2,24 +2,25 @@ if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+
 const helpers = require('./_helpers');
 const routes = require('./routes')
+const passport = require('./config/passport')
+
 
 
 const app = express()
 const port = 3000
 
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 app.use((req, res, next) => {
-  res.locals.loginUser = getUser(req)
+  res.locals.loginUser = helpers.getUser(req)
   next()
 })
-// use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
-  // passport.authenticate('jwt', { ses...
-};
 
-
+app.use(passport.initialize())
+app.use(passport.session())
 app.use('/api', routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
