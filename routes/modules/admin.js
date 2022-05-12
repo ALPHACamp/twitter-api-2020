60ea89ce2311ef35205714d89d8da05222869bcf
JINGWEN LIN <yaysa903@gmail.com>
2022-05-12 23:30:45 +0800

    feat: add get all users feature for admin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.delete('/tweets/:id', adminController.deleteTweets)
+router.get('/users', adminController.getAllUsers)
 
 module.exports = router
