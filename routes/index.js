d019e51bd3925c31c1d60c3d06d498ca8f375570
Charles <charles0527@hotmail.com>
2022-05-12 11:20:09 +0800

    create routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+module.exports = router
+
 // 尚未加入 authenticatedAdmin
 const { authenticated, authenticatedUser } = require('../middleware/auth')
+
+// 以下待完成/routes/modules/users.js路由後再補上
