36927bf2927efab7f494c40f82a9f33ea9359bf1
GallonShih <Gallon.Shih@testritegroup.com>
2022-05-13 14:01:27 +0800

    feet: add user-reply like api

---
+++ controllers/reply-controller.js
@@ -0,0 +1,44 @@
+const { LikedReply } = require('../models')
+
+// setting reply-related controller
+const replyController = {
+  likeReply: (req, res, next) => {
+     /*
+    :param id: replyId
+    This api would create a like relation between user and reply, and return a json
+    */
+    const userId = req.user.id
+    const replyId = req.params.id
+    return Promise.all([
+      Reply.findByPk(replyId),
+      LikedReply.findOne({
+        where: {
+          userId,
+          replyId
+        }
+      })
+    ])
+    .then(([reply, likedReply]) => {
+      if (!reply) throw new Error("Reply didn't exist!")
+      if (likedReply) throw new Error('You have liked this reply!')
+
+      return LikedReply.create({
+        userId,
+        replyId
+      })
+    })
+    .then(likedReply => {
+      return res.json({
+        status: 'Success',
+        statusCode: 200,
+        data: {
+          likedReply
+        },
+        message: ''
+      })
+    })
+    .catch(err => next(err))
+  }
+}
+
+module.exports = replyController
--- routes/modules/replies.js
+++ routes/modules/replies.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const replyController = require('../../controllers/reply-controller')
+
+// setting routes
+router.post('/replies/:id/like', replyController.likeReply)
 
 module.exports = router
