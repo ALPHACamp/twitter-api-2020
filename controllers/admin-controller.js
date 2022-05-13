d1f3cbc3c3a69e2da86e28996980ffe6e8b1a545
naivelove0822 <naivelove0822@gmail.com>
2022-05-13 22:23:13 +0800

    feat: add api/admin/users

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -3,6 +3,9 @@ const adminServices = require('../services/admin-services')
 const adminController = {
   deleteTweet: (req, res, next) => {
     adminServices.deleteTweet(req, (err, user) => err ? next(err) : res.json(user))
+  },
+  getUsers: (req, res, next) => {
+    adminServices.getUsers(req, (err, user) => err ? next(err) : res.json(user))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -10,15 +10,11 @@ const { apiErrorHandler } = require('../middleware/error-handler')
 
 router.post('/admin/signin', passport.authenticate('local', { session: false }), authenticatedAdmin, userController.signIn)
 router.post('/signin', passport.authenticate('local', { session: false }), authenticatedUser, userController.signIn)
+
 router.post('/users', userController.signUp)
 
 router.use('/admin', authenticated, authenticatedAdmin, admin)
 router.use('/users', authenticated, authenticatedUser, users)
-
-
-
-
-router.get('/users/:id', authenticated, userController.getUser)
 
 router.use('/', apiErrorHandler)
 module.exports = router
