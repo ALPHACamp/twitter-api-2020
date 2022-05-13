e46cce358b135c32d8ee5d20292a9fea76de9db8
Charles <charles0527@hotmail.com>
2022-05-13 15:52:07 +0800

    refactor: rewrite user controller with promise syntax

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,41 +10,64 @@ const userController = {
     if (!account || !password) throw new Error('帳號和密碼為必填！')
 
     User.findOne({ where: { account } })
-      .then(user => {
+      .then((user) => {
         if (!user) throw new Error('帳號不存在！')
         if (user.role === 'admin') throw new Error('帳號不存在！')
-        if (!bcrypt.compareSync(password, user.password)) throw new Error('密碼錯誤！')
+        if (!bcrypt.compareSync(password, user.password))
+          throw new Error('密碼錯誤！')
         const userData = user.toJSON()
         delete userData.password
-        const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+        const token = jwt.sign(userData, process.env.JWT_SECRET, {
+          expiresIn: '30d',
+        })
         return res.status(200).json({
           token,
-          user: userData
+          user: userData,
         })
       })
-      .catch(err => next(err))
+      .catch((err) => next(err))
   },
 
-  signUp: async (req, res, next) => {
+  signUp: (req, res, next) => {
     const { account, name, email, password, checkPassword } = req.body
     if (password !== checkPassword) throw new Error('密碼與確認密碼不符！')
+    if (!account || !name || !email || !password || !checkPassword)
+      throw new Error('此欄位不可空白！')
 
     // 確認資料裡面沒有相同的 email，若有，就建立一個 Error 物件並拋出
-    const user = await User.findOne({ where: { email } })
-    if (user) throw new Error('此 Email 已被註冊')
-    const newUser = await User.create({
-      account,
-      name,
-      email,
-      password: bcrypt.hash(password, 10)
+    User.findAll ({ 
+      $or: [
+        { where: { email } },
+        { where: { account } }
+      ]
     })
+    .then (users => {
+      if (users.some(u => u.email === email)) throw new Error('此 Email 已被註冊！')
+      if (users.some(u => u.account === account)) throw new Error('此 Account 已被註冊！')
+
+      return bcrypt.hash(password, 10)
+    })
+    .then(hash => {
+      return User.create({
+        account,
+        name,
+        email,
+        password: hash,
+        role: ''
+      })
+    })
+    .then(newUser => {
       const userData = newUser.toJSON()
       delete userData.password
-      const token = jwt.sign(newUser, process.env.JWT_SECRET, { expiresIn: '30d' })
+      const token = jwt.sign(newUser, process.env.JWT_SECRET, {
+        expriesIn: '30d',
+      })
       return res.status(200).json({
         token,
         user: userData
       })
+    })
+    .catch(err => next(err))
   },
 
   getUser: (req, res, next) => {
@@ -54,16 +77,20 @@ const userController = {
       include: [
         { model: User, as: 'LikedTweets' },
         { model: User, as: 'Followers' },
-        { model: User, as: 'Followings' }
-      ]
+        { model: User, as: 'Followings' },
+      ],
     })
-    .then (user => {
-      if (!user || user.role === 'admin') throw new Error ('Account does not exist!')
-      user.dataValues.isFollowed = user.Followers.map(u => u.id).includes(reqUserId)
-      return res.status(200).json(user)
-    })
-    .catch(err => next(err))
+      .then((user) => {
+        if (!user || user.role === 'admin')
+          throw new Error('帳號不存在！')
+        user.dataValues.isFollowed = user.Followers.map((u) => u.id).includes(
+          reqUserId
+        )
+        return res.status(200).json(user)
+      })
+      .catch((err) => next(err))
   }
 }
+
 
 module.exports = userController
