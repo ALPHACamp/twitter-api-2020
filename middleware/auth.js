c871dc50242fe2c8b83518e19c10e217ace95364
Charles Chen <charles0527@hotmail.com>
2022-05-14 23:27:29 +0800

    feat: fix middleware auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,34 +2,24 @@ const passport = require('../config/passport')
 const { getUser } = require('../_helpers')
 
 const authenticated = (req, res, next) => {
-  // if (req.isAuthenticated)
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || user) {
-      return res.status(200).json({
-        status: 'error',
-        message: 'Unauthorized'
-      })
-    }
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'Unauthorized' })
     req.user = user
     return next()
   })(req, res, next)
 }
 
 const authenticatedUser = (req, res, next) => {
-  req.user = getUser(req)
-  passport.authenticate('jwt', { session: false}, (err, user) => {
+  passport.authenticate('jwt', { session: false }, () => {
+    req.user = getUser(req)
     if (req.user && req.user.role !== 'admin') return next()
-  })
-  return res.status(200).json({
-    status: 'error',
-    message: 'Permission denied',
-  })
+    return res.status(403).json({ status: 'error', message: 'Permission denied' })
+  }) (req, res, next)
 }
 
-// Angela: authenticated Admin
-
+// Angela: authenticatedAdmin
 
 module.exports = {
   authenticated,
-  authenticatedUser
+  authenticatedUser,
 }
