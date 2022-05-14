57ee31ee4f42b6b5debac24f95300afbbf5c8da0
eruc <eruc101010@gmail.com>
2022-05-14 08:28:03 +0800

    feat: supplementary information message

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,11 +1,12 @@
 const createToken = require('../function/token')
+const tweetServices = require('../services/tweets')
 const { Tweet, User } = require('../models')
 
 const adminController = {
-  login: async (req, res) => {
+  login: async (req, res, next) => {
     try {
       const userData = req.user.toJSON()
-      if (userData.role !== 'admin') return res.status(403).json({ status: 'error', message: '非管理者' })
+      if (userData.role !== 'admin') res.status(403).json({ status: 'error', message: '非管理者' })
       const token = await createToken(userData)
       res.json({
         status: 'success',
@@ -15,12 +16,16 @@ const adminController = {
         }
       })
     } catch (err) {
-      console.log(err)
+      next(err)
     }
   },
   tweets: async (req, res) => {
     try {
-      const tweets = await Tweet.findAll({ raw: true })
+      const tweets = await tweetServices.getAll()
+      if (!(tweets.length)) return res.status(403).json({ status: 'error', message: '推文不存在' })
+      tweets.forEach(element => {
+        element.description = element.description.substring(0, 51)
+      })
       res.json({
         status: 'success',
         data: {
@@ -34,6 +39,7 @@ const adminController = {
   users: async (req, res) => {
     try {
       const users = await User.findAll({ raw: true })
+      if (!users) return res.status(403).json({ status: 'error', message: '使用者不存在' })
       res.status(200)
         .json(users)
     } catch (err) {
--- services/tweets.js
+++ services/tweets.js
@@ -1,8 +1,51 @@
-const { Tweet } = require('../models')
+const { Tweet, User, Like, Reply } = require('../models')
+
 const tweets = {
   getAll: async () => {
     try {
-      const tweets = await Tweet.findAll({ raw: true })
+      const rawTweets = await Tweet.findAll({
+        attributes: {
+          exclude: ['updatedAt']
+        },
+        include: [
+          {
+            model: User,
+            attributes: ['name']
+          }
+        ],
+        raw: true
+      })
+      const likes = await Like.count({
+        group: ['Tweet_id'],
+        raw: true
+      })
+      const replies = await Reply.count({
+        group: ['Tweet_id'],
+        raw: true
+      })
+
+      // 下面可能可以用 [sequelize.fn("COUNT", sequelize.col('')), "count"]] 合併到資料庫搜索語法
+
+      const tweets = []
+      for (let likeIndex = 0; likeIndex < likes.length; likeIndex++) {
+        for (let tweetIndex = 0; tweetIndex < rawTweets.length; tweetIndex++) {
+          if (rawTweets[tweetIndex].id === likes[likeIndex].Tweet_id) {
+            rawTweets[tweetIndex].likeCounts = likes[likeIndex].count
+          }
+        }
+      }
+      for (let replyIndex = 0; replyIndex < replies.length; replyIndex++) {
+        for (let tweetIndex = 0; tweetIndex < rawTweets.length; tweetIndex++) {
+          if (rawTweets[tweetIndex].id === replies[replyIndex].Tweet_id) {
+            rawTweets[tweetIndex].replyCounts = replies[replyIndex].count
+            tweets.push(rawTweets[tweetIndex])
+          }
+        }
+      }
+      tweets.forEach(element => {
+        if (element.likeCounts === undefined) element.likeCounts = 0
+        if (element.replyCounts === undefined) element.replyCounts = 0
+      })
       return tweets
     } catch (err) {
       console.log(err)
