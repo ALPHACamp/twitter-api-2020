ab283e415396161b8710cb8cde95ce4b072f9720
eruc <eruc101010@gmail.com>
2022-05-14 09:30:37 +0800

    feat: supplement the number of users who are following and the number of followers

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,6 @@
 const createToken = require('../function/token')
 const tweetServices = require('../services/tweets')
-const { Tweet, User } = require('../models')
+const { Tweet, User, Followship } = require('../models')
 
 const adminController = {
   login: async (req, res, next) => {
@@ -38,7 +38,40 @@ const adminController = {
   },
   users: async (req, res) => {
     try {
-      const users = await User.findAll({ raw: true })
+      const users = await User.findAll({
+        attributes: {
+          exclude: ['createdAt', 'password', 'introduction', 'updatedAt']
+        },
+        raw: true
+      })
+      const followers = await Followship.count({
+        group: ['followerId'],
+        raw: true
+      })
+      const followings = await Followship.count({
+        group: ['followingId'],
+        raw: true
+      })
+      // 下面可能可以用 [sequelize.fn("COUNT", sequelize.col('')), "count"]] 合併到資料庫搜索語法
+
+      for (let followerIndex = 0; followerIndex < followers.length; followerIndex++) {
+        for (let userIndex = 0; userIndex < users.length; userIndex++) {
+          if (users[userIndex].id === followers[followerIndex].followerId) {
+            users[userIndex].followers = followers[followerIndex].count
+          } else {
+            if (users[userIndex].followers === undefined) users[userIndex].followers = 0
+          }
+        }
+      }
+      for (let followingIndex = 0; followingIndex < followings.length; followingIndex++) {
+        for (let userIndex = 0; userIndex < users.length; userIndex++) {
+          if (users[userIndex].id === followings[followingIndex].followingId) {
+            users[userIndex].followings = followings[followingIndex].count
+          } else {
+            if (users[userIndex].followings === undefined) users[userIndex].followings = 0
+          }
+        }
+      }
       if (!users) return res.status(403).json({ status: 'error', message: '使用者不存在' })
       res.status(200)
         .json(users)
--- services/tweets.js
+++ services/tweets.js
@@ -3,7 +3,7 @@ const { Tweet, User, Like, Reply } = require('../models')
 const tweets = {
   getAll: async () => {
     try {
-      const rawTweets = await Tweet.findAll({
+      const tweets = await Tweet.findAll({
         attributes: {
           exclude: ['updatedAt']
         },
@@ -25,27 +25,24 @@ const tweets = {
       })
 
       // 下面可能可以用 [sequelize.fn("COUNT", sequelize.col('')), "count"]] 合併到資料庫搜索語法
-
-      const tweets = []
       for (let likeIndex = 0; likeIndex < likes.length; likeIndex++) {
-        for (let tweetIndex = 0; tweetIndex < rawTweets.length; tweetIndex++) {
-          if (rawTweets[tweetIndex].id === likes[likeIndex].Tweet_id) {
-            rawTweets[tweetIndex].likeCounts = likes[likeIndex].count
+        for (let tweetIndex = 0; tweetIndex < tweets.length; tweetIndex++) {
+          if (tweets[tweetIndex].id === likes[likeIndex].Tweet_id) {
+            tweets[tweetIndex].likeCounts = likes[likeIndex].count
+          } else {
+            if (tweets[tweetIndex].likeCounts === undefined) tweets[tweetIndex].likeCounts = 0
           }
         }
       }
       for (let replyIndex = 0; replyIndex < replies.length; replyIndex++) {
-        for (let tweetIndex = 0; tweetIndex < rawTweets.length; tweetIndex++) {
-          if (rawTweets[tweetIndex].id === replies[replyIndex].Tweet_id) {
-            rawTweets[tweetIndex].replyCounts = replies[replyIndex].count
-            tweets.push(rawTweets[tweetIndex])
+        for (let tweetIndex = 0; tweetIndex < tweets.length; tweetIndex++) {
+          if (tweets[tweetIndex].id === replies[replyIndex].Tweet_id) {
+            tweets[tweetIndex].replyCounts = replies[replyIndex].count
+          } else {
+            if (tweets[tweetIndex].replyCounts === undefined) tweets[tweetIndex].replyCounts = 0
           }
         }
       }
-      tweets.forEach(element => {
-        if (element.likeCounts === undefined) element.likeCounts = 0
-        if (element.replyCounts === undefined) element.replyCounts = 0
-      })
       return tweets
     } catch (err) {
       console.log(err)
