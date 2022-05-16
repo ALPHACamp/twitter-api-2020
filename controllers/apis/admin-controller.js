9e2551fdf7c740adbefcc42238d9fd938aa36e49
KateChu <chu.shihchien@gmail.com>
2022-05-16 18:46:52 +0800

    drop test code

--- controllers/apis/admin-controller.js
+++ controllers/apis/admin-controller.js
@@ -6,38 +6,38 @@ const adminController = {
   getUsers: async (req, res, next) => {
     try {
       const users = await User.findAll({
-        attributes: ['id', 'account', 'name', 'coverImg', 'avatarImg']
-        // include: [
-        //   {
-        //     model: Identity,
-        //     where: { identity: 'user' },
-        //     attributes: []
-        //   }
-        // ],
-        // raw: true,
-        // nest: true
+        attributes: ['id', 'account', 'name', 'coverImg', 'avatarImg'],
+        include: [
+          {
+            model: Identity,
+            where: { identity: 'user' },
+            attributes: []
+          }
+        ],
+        raw: true,
+        nest: true
       })
-      console.log('===== test =====', users)
-      // for (const user of users) {
-      //   Promise.all([
-      //     (user.tweetAmount = await Tweet.findAndCountAll({
-      //       where: { UserId: user.id },
-      //       attributes: ['id']
-      //     })),
-      //     (user.likeAmount = await Like.findAndCountAll({
-      //       where: { UserId: user.id },
-      //       attributes: ['id']
-      //     })),
-      //     (user.followingAmount = await Followship.findAndCountAll({
-      //       where: { follower_id: user.id },
-      //       attributes: ['id']
-      //     })),
-      //     (user.followedAmount = await Followship.findAndCountAll({
-      //       where: { following_id: user.id },
-      //       attributes: ['id']
-      //     }))
-      //   ])
-      // }
+
+      for (const user of users) {
+        Promise.all([
+          (user.tweetAmount = await Tweet.findAndCountAll({
+            where: { UserId: user.id },
+            attributes: ['id']
+          })),
+          (user.likeAmount = await Like.findAndCountAll({
+            where: { UserId: user.id },
+            attributes: ['id']
+          })),
+          (user.followingAmount = await Followship.findAndCountAll({
+            where: { follower_id: user.id },
+            attributes: ['id']
+          })),
+          (user.followedAmount = await Followship.findAndCountAll({
+            where: { following_id: user.id },
+            attributes: ['id']
+          }))
+        ])
+      }
       return res.status(200).json(users)
     } catch (err) {
       next(err)
