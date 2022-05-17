00c742e8c664d48f3c5b894036ff216b8227a577
AngelaC123 <angelarare@gmail.com>
2022-05-17 14:24:10 +0800

    modify: post a reply api

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -48,7 +48,7 @@ const tweetController = {
       nest: true
     })
       .then(tweet => {
-        if (!tweet) throw new Error('這篇推文不存在！')
+        if (!tweet) throw new Error('推文不存在！')
 
         const likedTweetId = getUser(req)?.LikedTweets ? getUser(req).LikedTweets.map(t => t.id) : []
 
@@ -89,26 +89,31 @@ const tweetController = {
       })
     ])
       .then(([tweet, replies]) => {
-        if (!tweet) throw new Error('這篇推文不存在！')
+        if (!tweet) throw new Error('這篇回覆的推文不存在！')
         if (replies.length <= 0) throw new Error('這篇推文沒有回覆！')
         return res.status(200).json(replies)
       })
       .catch(err => next(err))
   },
 
-  // 尚未通過測試
   postTweetReply: (req, res, next) => {
-    const UserId = Number(helpers.getUser(req).id)
+    const UserId = Number(getUser(req).id)
     const TweetId = Number(req.params.tweet_id)
     const { comment } = req.body
     if (comment.length > 140) throw new Error('回覆字數不可超過140字！')
     if (!comment) throw new Error('回覆內容不可空白！')
-    return Reply.create({
-      TweetId,
-      comment,
-      UserId
-    })
-      .then(reply => res.status(200).json(reply))
+
+    return Tweet.findByPk(TweetId)
+      .then(tweet => {
+        if (!tweet) throw new Error('要回覆的篇推文不存在！')
+
+        return Reply.create({
+          TweetId,
+          comment,
+          UserId
+        })
+      })
+      .then(reply => res.status(200).json({ message: '回覆已成功送出！', reply }))
       .catch(err => next(err))
   },
 
