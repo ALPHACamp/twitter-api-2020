342a8750d39c77f742a5304b1ed07b6b33d460cf
yanpin0524 <a131234568@gmail.com>
2022-05-17 18:19:09 +0800

    fix: 修復會產生 admin 的推文、留言、按讚 資料的問題

--- seeders/20220511154529-tweets-seed-file.js
+++ seeders/20220511154529-tweets-seed-file.js
@@ -4,7 +4,7 @@ const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const users = await queryInterface.sequelize.query(
-      'SELECT id FROM Users;',
+      'SELECT id FROM Users WHERE role = "user";',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
 
