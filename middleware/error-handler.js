2141be4a6ea0eb3638ec683c70e9084bd130a5ce
GallonShih <Gallon.Shih@testritegroup.com>
2022-05-19 20:55:11 +0800

    adjust: error-handler

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,11 +3,15 @@ module.exports = {
     if (err instanceof Error) {
       res.status(500).json({
         status: 'error',
+        statusCode: res.statusCode,
+        data: null,
         message: `${err.name}: ${err.message}`
       })
     } else {
       res.status(500).json({
         status: 'error',
+        statusCode: res.statusCode,
+        data: null,
         message: `${err}`
       })
     }
