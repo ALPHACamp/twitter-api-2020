cf3f3193ecdeea967ac87ce3090e9973cc91307c
maximeri <t.s.eliot1996@gmail.com>
2022-05-20 13:33:40 +0800

    change to file

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -3,10 +3,10 @@ const imgur = require('imgur')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 imgur.setClientId(IMGUR_CLIENT_ID)
 
-const imgurFileHandler = files => {
+const imgurFileHandler = file => {
   return new Promise((resolve, reject) => {
-    if (!files) return resolve(null)
-    return imgur.uploadFile(files.path)
+    if (!file) return resolve(null)
+    return imgur.uploadFile(file.path)
       .then(img => {
         resolve(img?.link || null) // 檢查 img 是否存在 寫成 object?.key，JavaScript 會先去幫我們檢查符號前面這個 object 值存不存在。存在才往下取值 object.key，不存在就直接回傳 undefined
       })
