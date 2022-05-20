a2e5cc49a158c6a56519a34fcbc6ba02cb331416
eruc <eruc101010@gmail.com>
2022-05-20 14:38:12 +0800

    feat: print error detail

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { getUser } = require('./_helpers')
 const port = process.env.PORT || 3000
 const cors = require('cors')
 const passport = require('./config/passport')
-const nodemailer = require('nodemailer')
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
@@ -20,26 +20,8 @@ app.use((req, res, next) => {
 })
 // 將 request 導入路由器
 app.use(routes)
-
 process.on('unhandledRejection', (reason, promise) => {
-  const herokuUser = 'eruc101010@gmail.com'
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    auth: {
-      user: 'xxxxxxx',
-      pass: 'XXXXXXX'
-    }
-  })
-  transporter.verify().then(console.log).catch(console.error)
-
-  const userEmail = herokuUser
-  transporter.sendMail({
-    from: 'eruc11111@gmail.com', // sender address
-    to: userEmail, // list of receivers
-    subject: 'Heroku error', // Subject line
-    text: `Unhandled Rejection at:${reason.stack || reason}` // plain text body
-  })
+  console.log(`Unhandled Rejection at:${reason.stack || reason}`)
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
