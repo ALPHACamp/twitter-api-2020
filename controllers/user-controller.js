e1dda5df130748f2a45bf050ac2281c06b6f147d
maximeri <t.s.eliot1996@gmail.com>
2022-05-20 13:31:42 +0800

    adjust paswword and formatting

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -103,14 +103,20 @@ const userController = {
       throw new Error("User doen't have permission!")
     }
     const introduction = req.body.introduction || req.user.introduction || ''
-    const password = req.body.password ? bcrypt.hashSync(req.body.password, 10) : req.user.password || '12345678'
+    const password = req.body.password ? bcrypt.hashSync(req.body.password, 10) : req.user.password || bcrypt.hashSync('12345678', 10)
     const name = req.body.name || req.user.name || 'name'
     const account = req.body.account || req.user.account || 'account'
     const email = req.body.email || req.user.email || 'email@email.com'
     const avatar = req.files.avatar ? req.files.avatar[0] : null
     const cover = req.files.cover ? req.files.cover[0] : null
     Promise.all([User.findOne({ where: { email } }), User.findOne({ where: { account } }), User.findByPk(req.params.id), imgurFileHandler(avatar), imgurFileHandler(cover)])
-      .then(([findEmail, findAccount, user, avatarFilePath, coverFilePath]) => {
+      .then(([
+        findEmail,
+        findAccount,
+        user,
+        avatarFilePath,
+        coverFilePath
+      ]) => {
         if (findEmail && findEmail.id !== req.user.id) throw new Error('Email has already been taken.')
         if (findAccount && findAccount.id !== req.user.id) throw new Error('Account has already been taken.')
         return user.update({
