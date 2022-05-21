d8311830c2f280517bc020961277c561b6adcd23
GallonShih <gallonshih0526@gmail.com>
2022-05-21 16:27:36 +0800

    adjust: delete swagger notation

--- controllers/reply-controller.js
+++ controllers/reply-controller.js
@@ -4,10 +4,6 @@ const { Reply, LikedReply } = require('../models')
 const replyController = {
   likeReply: (req, res, next) => {
     /*
-    #swagger.tags = ['Replies']
-    #swagger.security = [{
-            'bearerAuth': []
-    }]
     :param id: ReplyId
     This api would create a like relation between user and reply, and return a json
     */
@@ -38,10 +34,6 @@ const replyController = {
   },
   unlikeReply: (req, res, next) => {
     /*
-    #swagger.tags = ['Replies']
-    #swagger.security = [{
-            'bearerAuth': []
-    }]
     :param id: ReplyId
     This api would destroy a like relation between user and reply, and return a json
     */
@@ -65,10 +57,6 @@ const replyController = {
   },
   putReply: async (req, res, next) => {
     /*
-    #swagger.tags = ['Replies']
-    #swagger.security = [{
-            'bearerAuth': []
-    }]
     :param id: ReplyId
     :body comment: reply's content
     This api would edit a reply record and return a json
@@ -92,10 +80,6 @@ const replyController = {
   },
   deleteReply: async (req, res, next) => {
     /*
-    #swagger.tags = ['Replies']
-    #swagger.security = [{
-            'bearerAuth': []
-    }]
     :param id: ReplyId
     This api would delete a reply record and its related likeReplies record, and return a json
     */
