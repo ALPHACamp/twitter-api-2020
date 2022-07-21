65274f981334dad0e07f55d1a6bd58d90498f61f
Pin Hsu Chen <paul791111@gmail.com>
2022-07-21 15:30:43 +0800

    feat: add user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,20 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-  }, {})
+    account: DataTypes.STRING,
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    introduction: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    cover: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users'
+  })
   User.associate = function (models) {
   }
   return User
