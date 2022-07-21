017c2064b30f9b09f6823b9f405aa0f528d920e8
Pin Hsu Chen <paul791111@gmail.com>
2022-07-21 15:37:46 +0800

    feat: add all basic setup for server

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const cors = require('cors')
+const routes = require('./routes/index')
+const helpers = require('./_helpers')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
-// use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
-  // passport.authenticate('jwt', { ses...
-};
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use((req, res, next) => {
+  res.user = helpers.getUser(req)
+  next()
+})
+
+app.use('/', routes)
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
