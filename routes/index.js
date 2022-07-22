25280d5fe02b6ef0e5f8debe3e093224cc97ad5d
Pin Hsu Chen <paul791111@gmail.com>
2022-07-22 20:49:39 +0800

    feat: add get current user function

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
+const users = require('./modules/user')
 const passport = require('../config/passport')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { errorHandler } = require('../middleware/error-handler')
@@ -8,12 +9,11 @@ const userController = require('../controllers/user-controller')
 const adminController = require('../controllers/admin-controller')
 
 router.post('/signin', passport.authenticate('local', { session: false, failWithError: true }), userController.signin)
-
 router.post('/admin/signin', passport.authenticate('local', { session: false, failWithError: true }), adminController.signin)
-
 router.post('/users', userController.signUp)
 
 router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.use('/users', authenticated, users)
 
 router.use('/', errorHandler)
 
