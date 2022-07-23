0af615ddc5f721e593a143f1b8a4ab891ec789c0
Pin Hsu Chen <paul791111@gmail.com>
2022-07-23 17:22:12 +0800

    feature: add delete tweets route

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,5 +4,6 @@ const adminController = require('../../controllers/admin-controller')
 
 router.get('/users', adminController.getUsers)
 router.get('/tweets', adminController.getTweets)
+router.delete('/tweet/:id', adminController.deleteTweet)
 
 module.exports = router
