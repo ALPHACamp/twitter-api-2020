b13207b51ae65440899752e7c3735afefd518feb
Pin Hsu Chen <paul791111@gmail.com>
2022-07-23 20:43:05 +0800

    feature: add editUser function

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const upload = require('../../middleware/multer')
 const userController = require('../../controllers/user-controller')
 
-router.get('/:id', userController.getUser)
+router.get('/:id', userController.getUserPage)
 router.get('/current_user', userController.getCurrentUser)
+router.put('/:id', upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }]), userController.editUser)
 
 module.exports = router
