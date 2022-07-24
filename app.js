4d29d9409d6013de5c097e6157dedaf35e42133d
Ingrid <ingrid.chi77@gmail.com>
2022-07-24 19:04:13 +0800

    test_Ingrid

--- app.js
+++ app.js
@@ -13,5 +13,3 @@ app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
-
-console.log("testTestHere");
