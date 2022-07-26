669275e5614d41f2793e9d4fd2ed08745c85b0a9
dodouuu <joe30566@gmail.com>
2022-07-26 09:41:46 +0800

    [A02] feat: signUp and signIn response ...data, app.js cors (#18)

--- controllers/apis/user-controller.js
+++ controllers/apis/user-controller.js
@@ -2,10 +2,10 @@ const userServices = require('../../services/user-services')
 
 const userController = {
   signUp: (req, res, next) => {
-    userServices.signUp(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    userServices.signUp(req, (err, data) => err ? next(err) : res.json({ status: 'success', ...data }))
   },
   signIn: (req, res, next) => {
-    userServices.signIn(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+    userServices.signIn(req, (err, data) => err ? next(err) : res.json({ status: 'success', ...data }))
   },
   getUser: (req, res, next) => {
     userServices.getUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', ...data.user }))
