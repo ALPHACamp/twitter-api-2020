2e887fc6ade82849eb460239913f1200d2d3e600
wuwac <wuwachon44@gmail.com>
2022-07-28 14:23:13 +0800

    feat: user sign in

---
+++ controllers/user-controller.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+
+const userController = {
+  signIn: (req, res, next) => {
+    try {
+      const user = req.user.toJSON()
+      delete user.password
+      const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '3d' })
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = userController
