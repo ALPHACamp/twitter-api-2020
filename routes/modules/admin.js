b5fd47df6c8e3c4a9f075543591a9cd6f0c00402
Rita Chien <ritachien929@gmail.com>
2022-07-28 16:23:10 +0800

    feat: add adminController.deleteTweet

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,9 +3,10 @@ const router = express.Router()
 
 const passport = require('../../config/passport')
 
-// const { authenticated, authAdmin } = require('../../middleware/auth')
+const { authenticated, authAdmin } = require('../../middleware/auth')
 const adminController = require('../../controllers/admin-controller')
 
 router.post('/signin', passport.authenticate('local', { session: false }), adminController.signin)
+router.delete('/tweets/:id', authenticated, authAdmin, adminController.deleteTweet)
 
 module.exports = router
