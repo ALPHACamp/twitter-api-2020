92df0c823c9bfbbbabbb1010dac535334821a174
rubylo718 <rubylo718@gmail.com>
2022-07-28 16:58:40 +0800

    feat: add getUser in user-controller

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,10 +3,11 @@ const router = express.Router()
 
 const passport = require('../../config/passport')
 
-// const { authenticated, authUser } = require('../../middleware/auth')
+const { authenticated, authUser } = require('../../middleware/auth')
 const userController = require('../../controllers/user-controller')
 
 router.post('/signin', passport.authenticate('jwt', { session: false }), userController.signin)
+router.get('/:id', authenticated, userController.getUser)
 router.post('/', userController.signup)
 
 module.exports = router
