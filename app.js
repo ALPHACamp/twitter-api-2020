4176c6c3d3f58f1983f472b143b05d15b2573d2d
wuwac <wuwachon44@gmail.com>
2022-07-28 14:08:01 +0800

    feat: add routes files

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const express = require('express')
-const helpers = require('./_helpers')
+// const helpers = require('./_helpers')
 const cors = require('cors')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 // use helpers.getUser(req) to replace req.user
-function authenticated (req, res, next) {
+// function authenticated (req, res, next) {
   // passport.authenticate('jwt', { ses...
-};
+// };
 
 app.use(cors())
 
-app.use(routes)
+app.use('/api', routes)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
---
+++ routes/modules/users.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const userController = require('../../controllers/user-controller')
+
+// router.get('/:id/setting', userController.getSetting)
+// router.patch('/:id/setting', userController.patchSetting)
+// router.get('/:id/tweets', userController.getUserTweets)
+// router.get('/:id/replied_tweets', userController.getUserReplies)
+// router.get('/:id/likes', userController.getUserLikes)
+// router.get('/:id/followings', userController.getUserFollowings)
+// router.get('/:id/followers', userController.getUserFollowers)
+// router.get('/:id', userController.getUser)
+// router.put('/:id', userController.putUser)
+
+module.exports = router
