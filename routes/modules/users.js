b5e035b86c8fe772a37f5446f066cff31ded94a8
wuwac <wuwachon44@gmail.com>
2022-07-28 23:19:37 +0800

    feat: edit specific user's singn up data

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const userController = require('../../controllers/user-controller')
 
 router.get('/:id/setting', userController.getSetting)
-// router.patch('/:id/setting', userController.patchSetting)
+router.patch('/:id/setting', userController.patchSetting)
 // router.get('/:id/tweets', userController.getUserTweets)
 // router.get('/:id/replied_tweets', userController.getUserReplies)
 // router.get('/:id/likes', userController.getUserLikes)
