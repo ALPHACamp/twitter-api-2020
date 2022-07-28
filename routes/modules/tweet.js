bd6b31f2c001519d2f1fd21dca2c9ff7bc45b58b
Pin Hsu Chen <paul791111@gmail.com>
2022-07-28 21:31:02 +0800

    modify: refactor file

--- routes/modules/tweet.js
+++ routes/modules/tweet.js
@@ -7,7 +7,7 @@ router.get('/', tweetController.getTweets)
 router.post('/', tweetController.createTweet)
 router.get('/:id', tweetController.getTweet)
 router.post('/:id/replies', replyController.postReply)
-router.get('/:id/replies', tweetController.getReply)
+router.get('/:id/replies', tweetController.getReplies)
 router.post('/:id/like', tweetController.addLike)
 router.post('/:id/unlike', tweetController.removeLike)
 
