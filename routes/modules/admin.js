4fa21eeaa5fa3d3af644233e2688e3eb7b51b09c
Rita Chien <ritachien929@gmail.com>
2022-07-28 16:41:49 +0800

    fix: modify admin router

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,12 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const passport = require('../../config/passport')
-
 const { authenticated, authAdmin } = require('../../middleware/auth')
 const adminController = require('../../controllers/admin-controller')
 
-router.post('/signin', passport.authenticate('local', { session: false }), adminController.signin)
-router.delete('/tweets/:id', authenticated, authAdmin, adminController.deleteTweet)
+router.post('/signin', adminController.signin)
+router.delete('/tweets/:id', authenticated, adminController.deleteTweet)
 
 module.exports = router
