9cb61358450af2310e2db5a897eb4d2e39f8278c
InzooC <bluefreaks@gmail.com>
2022-07-29 16:16:36 +0800

    modify: admin getUsers funtion show Counts

--- services/admin-services.js
+++ services/admin-services.js
@@ -1,27 +1,33 @@
 const db = require('../models')
 const { User, Tweet, Like } = db
+const sequelize = require('sequelize')
 const { Op } = require("sequelize")
 const bcrypt = require('bcryptjs')
 
 const adminServices = {
   getUsers: async (req, cb) => {
-    return await User.findAndCountAll({
+    return await User.findAll({
       where: {
         [Op.not]: [
           { role: 'admin' },
         ]
       },
-      // order: [['', 'DESC']],  // ! 排序還沒有寫出來
-      include: [Like, Tweet],
-      // raw: true,
+      attributes: [
+        'id', 'account', 'name', 'email', 'avatar',
+        [sequelize.literal('(SELECT COUNT(*) FROM Tweets WHERE user_id = User.id)'), 'TweetsCount'],
+        [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE user_id = User.id)'), 'LikesCount'],
+        [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE following_id = User.id)'), 'FollowingCount'],
+        [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE follower_id = User.id)'), 'FollowerCount']
+      ],
+      order: [[sequelize.literal('TweetsCount'), 'DESC']],
+      raw: true,
       nest: true
     })
-      // ! 還沒成功取出Tweet&Like的數量
       .then(users => cb(null, { users }))
       .catch(err => cb(err))
   },
   getTweets: async (req, cb) => {
-    await Tweet.findAll({
+    return await Tweet.findAll({
       order: [['createdAt', 'DESC']], //? post tweet功能做好後測試 order 功能
       include: {
         model: User,
