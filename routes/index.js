8d1a7e4832fbc1f67400f2b21c3a8666b25abf09
AdrieneTZ <tznali17@gmail.com>
2022-07-29 21:59:51 +0800

    feat: admin sign in & admin can see all users

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router()
 
 const APItestController = require('../controllers/APItest-controller')
 const userController = require('../controllers/user-controller')
-// const adminController = require('../controllers/admin-controller')
+const adminController = require('../controllers/admin-controller')
 
-// const admin = require('./modules/admin')
+const admin = require('./modules/admin')
 const users = require('./modules/users')
 // const tweets = require('./modules/tweets')
 // const followships = require('./modules/followships')
@@ -14,12 +14,13 @@ const passport = require('../config/passport')
 const { authenticated, authenticateUser, authenticateAdmin } = require('../middlewares/api-auth')
 const { apiErrorHandler } = require('../middlewares/error-handler')
 
+
 router.get('/test', APItestController.getTestJSON)
-// router.post('/admin/signin', passport.authenticate('local', { session: false }), authenticateAdmin, adminController.signIn)
+router.post('/admin/signin', passport.authenticate('local', { session: false }), authenticateAdmin, adminController.signIn)
 router.post('/users/signin', passport.authenticate('local', { session: false }), authenticateUser, userController.signIn)
 router.post('/users', userController.signUp)
 
-// router.use('/admin', authenticated, authenticateAdmin, admin)
+router.use('/admin', authenticated, authenticateAdmin, admin)
 router.use('/users', authenticated, authenticateUser, users)
 // router.use('/tweets', authenticated, authenticateUser, tweets)
 // router.use('/followships', authenticated, authenticateUser, followships)
