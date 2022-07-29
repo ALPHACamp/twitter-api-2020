0027950850aab4dd4d99cd7fc9638e61154d9b2f
JasonChou <swchou524@gmail.com>
2022-07-29 22:27:27 +0800

    feat: add port env

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const routes = require('./routes')
 const passport = require('./config/passport')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
