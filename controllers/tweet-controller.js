dd5619034d07667301dd4174593b90f5be07902b
Rita Chien <ritachien929@gmail.com>
2022-07-30 23:45:44 +0800

    feat: tweetController.getTweet

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,7 +1,49 @@
 const helpers = require('../_helpers')
-const { Tweet, User } = require('../models')
+const { Tweet, User, Reply, Like } = require('../models')
 
 const tweetController = {
+  getTweet: async (req, res, next) => {
+    try {
+      const { id } = req.params
+      if (!id) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'id of tweet is required'
+        })
+      }
+
+      const tweetData = await Tweet.findByPk(id, {
+        attributes: ['id', 'description', 'createdAt'],
+        include: [
+          { model: User, attributes: ['id', 'name', 'account', 'avatar'] },
+          { model: Like, attributes: ['UserId'] },
+          { model: Reply, attributes: ['id'] }
+        ],
+        nest: true
+      })
+      if (!tweetData) {
+        return res.status(404).json({
+          status: 'error',
+          message: 'tweet not found'
+        })
+      }
+
+      // set response contents
+      const tweet = {
+        id: tweetData.id,
+        description: tweetData.description,
+        createdAt: tweetData.createdAt,
+        User: tweetData.User,
+        replyCount: tweetData.Replies.length,
+        likeCount: tweetData.Likes.length,
+        isLiked: tweetData.Likes.map(like => like.UserId).includes(helpers.getUser(req).id)
+      }
+
+      return res.status(200).json(tweet)
+    } catch (err) {
+      next(err)
+    }
+  },
   getTweets: async (req, res, next) => {
     try {
       const tweets = await Tweet.findAll({
--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const tweetController = require('../../controllers/tweet-controller')
 const { authUser } = require('../../middleware/auth')
 
+router.get('/:id', tweetController.getTweet)
 router.get('/', tweetController.getTweets)
 router.post('/', authUser, tweetController.addTweet)
 
