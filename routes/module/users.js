743a1f5d9474b50e485ff55344c59fb703d1a415
JasonChou <swchou524@gmail.com>
2022-07-30 20:16:36 +0800

    feat: add /api/users/currentUser

--- routes/module/users.js
+++ routes/module/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const userController = require('../../controllers/user-controller')
 
+router.get('/currentUser', userController.getCurrentUser)
 router.get('/', userController.getUsers)
 
 module.exports = router
