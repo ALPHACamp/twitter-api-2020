2cd446598c21da607e898cd73987d6b0e6bad287
InzooC <bluefreaks@gmail.com>
2022-07-31 18:00:38 +0800

    modify: change router order

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,13 @@ const users = require('./modules/users')
 
 //登入與註冊相關
 router.post('/api/users/signin', passport.authenticate('local', { session: false }), authenticatedUser, userController.signIn)
-router.post('/api/admin/users', passport.authenticate('local', { session: false }), authenticatedAdmin, adminController.signIn)
 router.post('/api/users', userController.signUp) //註冊
-//後台相關
-router.use('/api/users', authenticated, authenticatedUser, users)
+router.post('/api/admin/users', passport.authenticate('local', { session: false }), authenticatedAdmin, adminController.signIn)
 router.use('/api/admin', authenticated, authenticatedAdmin, admin)
-//前台相關
 router.use('/api/tweets', authenticated, authenticatedUser, tweets)
 router.use('/api/followships', authenticated, authenticatedUser, followship)
+router.use('/api/users', authenticated, authenticatedUser, users)
+
 router.use('/', apiErrorHandler)
 
 module.exports = router
