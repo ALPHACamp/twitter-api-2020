5c94db0742727cd9fd2d5343ae3671e48dce0adb
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-07-31 22:56:45 +0800

    feat: add userLike API

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const userController = require('../../controllers/user-controller')
 
+router.get('/:id/likes', userController.getUserLikes)
 router.get('/currentUser', userController.getCurrentUser)
 router.get('/', userController.getUsers)
 
