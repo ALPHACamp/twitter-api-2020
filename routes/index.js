42b76f2e639a656979af5a7232efda87d6bc7732
Rita Chien <ritachien929@gmail.com>
2022-08-01 16:18:36 +0800

    refactor: modify currentUser route

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,9 @@ const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 const followships = require('./modules/followships')
 const { authenticated, authUser } = require('../middleware/auth')
-const { getCurrentUser } = require('../controllers/user-controller')
 
 router.use('/api/admin', admin)
 router.use('/api/users', users)
-router.get('/api/currentUser', authenticated, getCurrentUser)
 router.use('/api/tweets', authenticated, tweets)
 router.use('/api/followships', authenticated, authUser, followships)
 
