6148b55c16fa0fdbef2e7ec8ec23b59b23425ebe
Rita Chien <ritachien929@gmail.com>
2022-08-01 14:41:08 +0800

    feat: add like/unlike tweet feature

--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const tweetController = require('../../controllers/tweet-controller')
 const { authUser } = require('../../middleware/auth')
 
+router.post('/:id/like', authUser, tweetController.likeTweet)
+router.post('/:id/unlike', authUser, tweetController.unlikeTweet)
 router.get('/:id', tweetController.getTweet)
 router.get('/', tweetController.getTweets)
 router.post('/', authUser, tweetController.addTweet)
