66a1237e1311e95a6b4391ad8fc7d98c439cddc6
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-08-04 01:47:25 +0800

    feat: add getfollowersTweets

--- routes/modules/followships.js
+++ routes/modules/followships.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const followController = require('../../controllers/follow-controller')
 
+router.get('/tweets', followController.getFollowersTweets)
 router.post('/', followController.addFollow)
 router.delete('/:followingId', followController.removeFollow)
 
