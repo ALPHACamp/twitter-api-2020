f1d6e864b1559ceed057f2e129dddef4ed6186f1
yc62897441 <yc62897441@gmail.com>
2022-08-09 13:52:10 +0800

    測試中，完成上傳圖片到 imgur，優化流程，整理註解

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,9 @@ const tweetController = require('../controllers/tweetController')
 const passport = require('../config/passport')
 const authenticated = passport.authenticate('jwt', { session: false })
 
-
 const { ImgurClient } = require('imgur');
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const multer = require('multer')
-// 單一檔案
-// let upload = multer()
-// 多個檔案
 let upload = multer()
 
 const db = require('../models')
@@ -49,27 +45,28 @@ module.exports = (app) => {
   app.delete('/api/admin/tweets/:id', adminController.deleteTweet)
 
 
-  // 測試用，多個圖片 OK；只傳單一圖片也 OK
+  // 測試用，多個圖片、單一圖片、不傳圖片都 OK
   app.post('/api/test', authenticated, upload.array('files', 2), async (req, res) => {
     try {
       // user name、introduction 會在 body 內；avatar、banner 在 files 內
-      console.log('req.body:', req.body)
-      console.log('req.body:', req.body)
-      console.log('req.body:', req.body)
+
+      // user.name 不可更新為空白，如傳入資料為空白，則結束函式
       if (!req.body.name) {
-        console.log('req.body.name:', req.body.name)
         return res.json({ status: 'error', message: 'Name 不可為空白' })
       }
-      console.log('xxxxx')
 
-      //return res.json({ status: 'success' })
+      // 傳送 avatar、banner 檔案給 imgur，可以兩個都有、一有一無，或兩個都沒有
       Promise.all(req.files.map(async (file) => {
         let encode_image = file.buffer.toString('base64')
         const client = new ImgurClient({ clientId: IMGUR_CLIENT_ID })
+
+        // 傳 encode_image 給 imgur
         const response = await client.upload({
           image: encode_image,
           type: 'base64',
         })
+
+        // 整理 imgur 回傳的圖片連結，存到 imgurLink
         const result = {
           'originalname': file.originalname,
           'imgurLink': response.data.link
@@ -77,18 +74,23 @@ module.exports = (app) => {
         return result
       }))
         .then((results) => {
-          console.log('results', results)
           const name = req.body.name
           const introduction = req.body.introduction
           let avatar = undefined
           let banner = undefined
-          results.forEach(result => {
-            if (result.originalname === 'avatar') {
-              avatar = result.imgurLink
-            } else if (result.originalname === 'banner') {
-              banner = result.imgurLink
-            }
-          })
+
+          //如果有更新 avatar 或 banner，則 results 至少會有一個物件，將該物件中的 imgurLink 存到變數 avatar 或 banner 中，等等再更新到資料庫內
+          if (results.length > 0) {
+            results.forEach(result => {
+              if (result.originalname === 'avatar') {
+                avatar = result.imgurLink
+              } else if (result.originalname === 'banner') {
+                banner = result.imgurLink
+              }
+            })
+          }
+
+          // 更新到資料庫
           User.findByPk(req.user.id)
             .then(user => {
               user.update({
@@ -98,42 +100,15 @@ module.exports = (app) => {
                 avatar: avatar,
                 banner: banner,
               })
+                .then(() => {
+                  return res.json({ status: 'success', results: results })
+                })
             })
-          return res.json({ status: 'success', results: results })
         })
     } catch (error) {
       console.warn(error)
     }
   })
-  // // 測試用，多個圖片 OK；只傳單一圖片也 OK
-  // app.post('/api/test', authenticated, upload.array('files', 2), async (req, res) => {
-  //   try {
-  //     // user name、introduction 會在 body 內；avatar、banner 在 files 內
-  //     console.log('req.body', req.body)
-  //     console.log('req.files', req.files)
-  //     console.log('req.user', req.user)
-
-  //     //return res.json({ status: 'success' })
-  //     Promise.all(req.files.map(async (file) => {
-  //       let encode_image = file.buffer.toString('base64')
-  //       const client = new ImgurClient({ clientId: IMGUR_CLIENT_ID })
-  //       const response = await client.upload({
-  //         image: encode_image,
-  //         type: 'base64',
-  //       })
-  //       return response
-  //     }))
-  //       .then((results) => {
-  //         const likes = []
-  //         results.forEach(result => {
-  //           likes.push(result.data.link)
-  //         })
-  //         return res.json({ status: 'success', links: likes })
-  //       })
-  //   } catch (error) {
-  //     console.warn(error)
-  //   }
-  // })
   // 測試用，單一圖片 OK
   // app.post('/api/test', authenticated, upload.single('file'), async (req, res) => {
   //   try {
