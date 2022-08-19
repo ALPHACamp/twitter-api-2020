8676271fee39ad4a16759346c738348e6f5bc511
yc62897441 <yc62897441@gmail.com>
2022-08-19 10:15:30 +0800

    set process.env.port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // cors 的預設為全開放
 app.use(cors())
