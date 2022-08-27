7dadd8f450393afba5dc7e6d778feb77e4fa9a3d
yc62897441 <yc62897441@gmail.com>
2022-08-27 15:20:57 +0800

    try socket port

--- app.js
+++ app.js
@@ -279,6 +279,9 @@ app.use(express.json())
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 // module.exports = app
 
+server.listen('websocket', () => {
+  console.log('listening on *:websocket')
+})
 // server.listen(port, () => {
 //   console.log('listening on *:3030')
 // })
