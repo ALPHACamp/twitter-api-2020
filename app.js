0f645f36dd3216f9cb381b5e4105d15a1a94a262
Eric LIAO <d1044171009@gmail.com>
2022-10-02 18:58:27 +0800

    feat:  fix 正確回傳登入失敗錯誤訊息

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ function authenticated (req, res, next) {
 
 app.use(express.json()) // 把json 文件功能打開，這樣程式才能解析JSON格式的請求物件
 app.use(passport.initialize()) // 初始化 Passport
+app.use((req, res, next) => {
+  next()
+})
 app.use('/api', apis)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
