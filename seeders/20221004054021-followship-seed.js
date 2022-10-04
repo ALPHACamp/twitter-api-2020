136058443cac84dcd679664fd871d5df007baee4
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-10-04 15:35:32 +0800

    feat: add followship-seeder

--- seeders/20221004054021-followship-seed.js
+++ seeders/20221004054021-followship-seed.js
@@ -1,18 +1,18 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const users = await queryInterface.sequelize.query('SELECT id FROM Users;', { type: queryInterface.sequelize.QueryTypes.SELECT })
-    
+    queryInterface.bulkInsert('Followships', Array.from({ length: users.length }, (_, i) => ({
+      follower_id: users[i].id,
+      following_id: users?.filter(u => u.id !== users[i].id)[Math.floor(Math.random() * (users.length-2))]?.id,
+      created_at: new Date(),
+      updated_at: new Date()
+    }))
+    )
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Followships', null, {})
   }
-};
+}
