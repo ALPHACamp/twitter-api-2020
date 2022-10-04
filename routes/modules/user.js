8a6bde85978dbe8300bc86434ba48daefd0b113f
Yen <p12427@gmail.com>
2022-10-04 17:09:03 +0800

    feat: PUT /api/users/:id/setting

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
 
-router.put('/:id', userController.putUser)
+router.put('/:id/setting', userController.putUserSetting)
+router.put('/:id', userController.putUserProfile)
 
 module.exports = router
