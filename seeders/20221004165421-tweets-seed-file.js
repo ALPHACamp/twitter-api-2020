d6e6c3383f674bdac996c0106cc3ba360e1c6972
AlleyChang <yifanchang0602@gmail.com>
2022-10-05 09:09:28 +0800

    feat: add tweets seeder

--- seeders/20221004165421-tweets-seed-file.js
+++ seeders/20221004165421-tweets-seed-file.js
@@ -1,26 +1,19 @@
 'use strict'
-
+const faker = require('faker')
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+  up: async (queryInterface, Sequelize) => {
+    // 先查詢目前user的id
+    const users = await queryInterface.sequelize.query('SELECT id FROM Users;', { type: queryInterface.sequelize.QueryTypes.SELECT })
+    await queryInterface.bulkInsert('Tweets', [{
+      user_id: users[Math.floor(Math.random() * users.length)].id,
+      content: faker.lorem.text(),
+      created_at: new Date(),
+      updated_at: new Date()
+    }
+    ], {})
   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Tweets', null, {})
   }
 }
