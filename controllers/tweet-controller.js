cd36f33d4200ec303e87f90aacf839524702e34c
Ckai <ckai.trans@gmail.com>
2022-10-06 02:53:33 +0800

    feat: add getTweets

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -2,6 +2,16 @@ const { Like, Tweet, User } = require('../models')
 
 const tweetController = {
   getTweets: (req, res, next) => {
+    return Tweet.findAll({ raw:true })
+    .then(tweets => {
+      data = tweets.sort((a, b) => b.createdAt - a.createdAt)
+      console.log('暫時移除中介軟體、檢查data是否為陣列', data)
+      res.json({ 
+        status: 'success',
+        data
+      })
+    })
+    .catch(err => next(err))
   },
   getTweet: (req, res, next) => {
   },
