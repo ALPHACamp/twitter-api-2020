d31b1d1fb226ce012c4f0437d9efe0b6ed39c725
darrenjon <darrenlin23@gmail.com>
2022-10-05 09:06:14 +0800

    modify config for heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
