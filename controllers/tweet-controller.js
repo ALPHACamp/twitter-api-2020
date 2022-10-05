4cd6874ab688348dbf0a91fb4f0195ec57f13dbb
WIlson <eaz73727@gmail.com>
2022-10-05 20:20:25 +0800

    feat: GET /api/tweets/:id

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,6 +1,40 @@
+const { Tweet, User, Like, sequelize } = require('../models')
+const helpers = require('../_helpers')
 const tweetController = {
-  getTweet: (req, res) => {
-    res.send('tweet')
+  getTweet: async (req, res, next) => {
+    try {
+      const currentUserId = helpers.getUser(req).id
+      const tweetId = Number(req.params.id)
+      const tweet = await Tweet.findOne({
+        where: { id: tweetId },
+        include: [{ model: User, attributes: ['id', 'name', 'account', 'avatar'] }],
+        attributes: ['id', 'description', 'createdAt',
+          [sequelize.literal('(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id)'), 'replyCount'],
+          [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id)'), 'likeCount']
+        ],
+        raw: true,
+        nest: true
+      })
+      // error code 404
+      if (!tweet) {
+        return res.status(404).json({
+          status: 'error',
+          message: 'The tweet does not exist.'
+        })
+      }
+      const likes = await Like.findAll({
+        where: { tweetId },
+        attributes: ['UserId'],
+        nest: true,
+        raw: true
+      })
+      const currentUserLikedTweetsId = new Set()
+      likes.forEach(like => currentUserLikedTweetsId.add(like.UserId))
+      tweet.isLiked = currentUserLikedTweetsId.has(currentUserId)
+      return res.status(200).json(tweet)
+    } catch (err) {
+      return next(err)
+    }
   }
 }
 
--- routes/modules/tweet.js
+++ routes/modules/tweet.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const tweetController = require('../../controllers/tweet-controller')
 
-router.get('/', tweetController.getTweet)
+router.get('/:id', tweetController.getTweet)
 
 module.exports = router
