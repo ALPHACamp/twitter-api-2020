c0ca092b1d48c6a3dc4b015e4fb02b7521d55638
AlleyChang <yifanchang0602@gmail.com>
2022-10-05 13:01:22 +0800

    feat: modify tweets & users & replies seeder

--- seeders/20221004165421-tweets-seed-file.js
+++ seeders/20221004165421-tweets-seed-file.js
@@ -4,13 +4,13 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     // 先查詢目前user的id
     const users = await queryInterface.sequelize.query('SELECT id FROM Users;', { type: queryInterface.sequelize.QueryTypes.SELECT })
-    await queryInterface.bulkInsert('Tweets', [{
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+    await queryInterface.bulkInsert('Tweets', Array.from({ length: 60 }, (_, i) => ({
+      user_id: users[(i % users.length)].id,
       content: faker.lorem.text(),
       created_at: new Date(),
       updated_at: new Date()
-    }
-    ], {})
+    })
+    ), {})
   },
 
   down: async (queryInterface, Sequelize) => {
