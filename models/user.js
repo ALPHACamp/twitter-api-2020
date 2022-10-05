fc1ef9a0019f2226d4945a02c87e0baff94f88f6
stu96534 <stu96534@gmail.com>
2022-10-05 08:51:24 +0800

    feat: create user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-  }, {});
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING
+  }, {
+    underscored: true,
+  });
   User.associate = function(models) {
+    // associations can be defined here
   };
   return User;
 };
