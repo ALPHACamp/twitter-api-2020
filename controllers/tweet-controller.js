e23f159df6e4e6010dd96d0b4d12f3ff5d88460a
Ckai <ckai.trans@gmail.com>
2022-10-06 01:33:53 +0800

    before made the tweet branch

---
+++ controllers/tweet-controller.js
@@ -0,0 +1,16 @@
+const { Like, Tweet, User } = require('../models')
+
+const tweetController = {
+  getTweets: (req, res, next) => {
+  },
+  getTweet: (req, res, next) => {
+  },
+  postTweet:(req, res, next) => {
+  },
+  likeTweet:(req, res, next) => {
+  },
+  unlikeTweet:(req, res, next) => {
+  }
+}
+
+module.exports = tweetController
---
+++ routes/modules/tweets.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const router = express.Router()
+const { apiErrorHandler } = require('../../middleware/error-handler')
+const { authenticated } = require('../../middleware/auth')
+const tweetController = require('../../controllers/tweet-controller')
+
+router.post('/:id/like', authenticated, tweetController.likeTweet)
+router.post('/:id/unlike', authenticated, tweetController.unlikeTweet)
+router.get('/:id', authenticated, tweetController.getTweet)
+router.post('/', authenticated, tweetController.postTweet)
+router.get('/', authenticated, tweetController.getTweets)
+
+router.use('/', apiErrorHandler)
+
+module.exports = router
