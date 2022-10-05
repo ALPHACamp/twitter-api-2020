9074cb9207f6c61df7614f620161aaca7dad3a42
WIlson <eaz73727@gmail.com>
2022-10-05 13:06:16 +0800

    feat: GET /api/admin/users

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,24 @@
+const { User, sequelize } = require('../models')
 const adminController = {
-  getUsers: (req, res) => {
-    res.send('admin')
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({
+        attributes: {
+          exclude: ['email', 'password', 'introduction', 'role', 'createdAt', 'updatedAt'],
+          include: [
+            [sequelize.literal('(SELECT COUNT(*) FROM Tweets WHERE Tweets.UserId = User.id)'), 'tweetCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id)'), 'followerCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followerId = User.id)'), 'followingCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Tweets JOIN Likes ON Tweets.id = Likes.TweetId WHERE Tweets.UserId = User.id)'), 'likeCount']
+          ]
+        },
+        order: [[sequelize.literal('tweetCount'), 'DESC']],
+        raw: true
+      })
+      res.status(200).json(users)
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
