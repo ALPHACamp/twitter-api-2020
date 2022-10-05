73e2651f45841e0ab886bf1bdbf75f2f205ee3c7
AlleyChang <yifanchang0602@gmail.com>
2022-10-06 05:13:02 +0800

    feat: add user sign up route

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,18 +1,14 @@
-const jwt = require('jsonwebtoken')
+const userServices = require('../services/user-services')
 const userController = {
   signIn: (req, res, next) => {
-    try {
-      const token = jwt.sign(req.user, process.env.JWT_SECRET, { expiresIn: '30d' })
-      res.jason({
-        status: 'success',
-        data: {
-          token,
-          user: req.user
-        }
-      })
-    } catch (err) {
-      next(err)
-    }
+    return userServices.signIn(req, (err, data) => {
+      err ? next(err) : res.json(data)
+    })
+  },
+  signUp: (req, res, next) => {
+    return userServices.signUp(req, (err, data) => {
+      err ? next(err) : res.json(data)
+    })
   }
 }
 module.exports = userController
--- services/user-services.js
+++ services/user-services.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken')
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
 const userServices = {
   signIn: (req, cb) => {
     try {
@@ -13,6 +15,24 @@ const userServices = {
     } catch (err) {
       cb(err)
     }
+  },
+  signUp: (req, cb) => {
+    // 密碼輸入不一致
+    if (req.body.password !== req.user.passwordCheck) throw new Error("Passwords doesn't match!")
+    return User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        // 錯誤處理: user已註冊
+        if (user) throw new Error('User already exists!')
+        // user未註冊過
+        return bcrypt.hash(user.password, 10)
+      })
+      .then(hash => User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      }))
+      .then(newUser => cb(null, { newUser }))
+      .catch(err => cb(err))
   }
 }
 module.exports = userServices
