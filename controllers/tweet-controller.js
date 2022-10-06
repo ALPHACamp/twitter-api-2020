337501ce0154f5d1abf4d0a69a2a78d4656fce24
WIlson <eaz73727@gmail.com>
2022-10-06 21:48:18 +0800

    feat: POST /api/tweet/:id/like

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,4 +1,4 @@
-const { Tweet, User, Reply, sequelize } = require('../models')
+const { Tweet, Like, User, Reply, sequelize } = require('../models')
 const helpers = require('../_helpers')
 
 const tweetController = {
@@ -66,6 +66,40 @@ const tweetController = {
         raw: true
       })
       res.status(200).json(replies)
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  addTweetLike: async (req, res, next) => {
+    try {
+      const TweetId = req.params.id
+      const currentUserId = helpers.getUser(req).id
+      const tweet = await Tweet.findByPk(TweetId, { raw: true })
+
+      // status 404 tweet not exist
+      if (!tweet) {
+        res.status(404).json({
+          status: 'error',
+          message: 'The tweet does not exist'
+        })
+      }
+      const like = await Like.findOne({
+        where: { TweetId, UserId: currentUserId }
+      })
+
+      // status 400 like liked tweets
+      if (like) throw new Error('You already liked the tweet')
+      await Like.create({ TweetId, UserId: currentUserId })
+      return res.status(200).json({ status: 'success' })
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  deleteTweetLike: async (req, res, next) => {
+    try {
+      res.send('delete')
     } catch (err) {
       next(err)
     }
--- routes/modules/tweet.js
+++ routes/modules/tweet.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const tweetController = require('../../controllers/tweet-controller')
 
+router.post('/:id/like', tweetController.addTweetLike)
+router.post('/:id/unlike', tweetController.deleteTweetLike)
 router.get('/:tweet_id/replies', tweetController.getTweetReplies)
 router.get('/:id', tweetController.getTweet)
 
