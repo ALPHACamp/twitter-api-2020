bb5859ad7153eb8f25b52845bf84940fffb71d2b
LiyLi1122 <teppei.252033@gmail.com>
2022-10-06 16:29:04 +0800

    feat: app.js init

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
 
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
+function authenticated (req, res, next) {
   // passport.authenticate('jwt', { ses...
 };
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api/signin', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
