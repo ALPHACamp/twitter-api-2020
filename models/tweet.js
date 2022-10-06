891b9905eb84b81126bbc80db6983c84d4184383
Eric LIAO <d1044171009@gmail.com>
2022-10-07 00:46:28 +0800

    fix: 修正回傳兩次外鍵問題

--- models/tweet.js
+++ models/tweet.js
@@ -5,9 +5,9 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Tweet extends Model {
     static associate (models) {
-      Tweet.hasMany(models.Reply, { foreignKey: 'tweetId' })
-      Tweet.hasMany(models.Like, { foreignKey: 'tweetId' })
-      Tweet.belongsTo(models.User, { foreignKey: 'userId' })
+      Tweet.hasMany(models.Reply, { foreignKey: 'TweetId' })
+      Tweet.hasMany(models.Like, { foreignKey: 'TweetId' })
+      Tweet.belongsTo(models.User, { foreignKey: 'UserId' })
     }
   }
   Tweet.init({
