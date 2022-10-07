eadaa610e539d3f4a99a4836d1e066b71c30594d
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-10-07 20:30:38 +0800

    feat: add getUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt-nodejs')
-const { User } = require('../models')
+const { User, Tweet } = require('../models')
+const helpers = require('../_helpers')
 
 const userController = {
   signIn: (req, res, next) => {
@@ -25,25 +26,10 @@ const userController = {
       })
       .catch(err => next(err))
   },
-  getUser: (req, res, next) => {
-    console.log(req.user.toJSON())
-    const { id } = req.params
-    return User.findByPk(id, { raw: true })
-      .then(user => {
-        if (!user) throw new Error('帳號不存在！')
-        res.status(200).json({
-          status: 'success',
-          data: {
-            user
-          }
-        })
-      })
-      .catch(err => next(err))
-  },
   postUser: (req, res, next) => {
     const { account, name, email, password, checkPassword } = req.body
     if (!account || !name || !email || !password || !checkPassword) throw new Error('all fields are required')
-    
+
     if (password !== checkPassword) throw new Error('Two password need to be same.')
 
     return Promise.all([User.findOne({ where: { account } }), User.findOne({ where: { email } })])
@@ -64,6 +50,45 @@ const userController = {
           user: user.toJSON()
         }
       })).catch(err => next(err))
+  },
+  getUser: (req, res, next) => {
+    const currentUser = helpers.getUser(req).dataValues
+    console.log(currentUser)
+    const { id } = req.params
+    return User.findByPk(id, {
+      include:
+        [{
+          model: User,
+          as: 'Followings',
+          attributes: ['id']
+        },
+        {
+          model: User,
+          as: 'Followers',
+          attributes: ['id']
+        },
+        {
+          model: Tweet,
+          attributes: ['id']
+        }
+        ]
+    })
+      .then(user => {
+        if (!user) throw new Error('帳號不存在！')
+        user = {
+          ...user.toJSON(),
+          tweetCount: user.Tweets.length,
+          followerCount: user.Followers.length,
+          followingCount: user.Followings.length,
+          isFollow: user.Followers.some(u => u.id === currentUser.id)
+        }
+
+        delete user.password
+        delete user.Tweets
+        delete user.Followers
+        res.status(200).json(user)
+      })
+      .catch(err => next(err))
   }
 
 }
