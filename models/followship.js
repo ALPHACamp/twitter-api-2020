f549d0f6f72d94ae1b85a9dc474df1ee28e307dc
darrenjon <darrenlin23@gmail.com>
2022-10-07 23:12:46 +0800

    modify models for test standard

--- models/followship.js
+++ models/followship.js
@@ -1,25 +1,21 @@
 'use strict'
+const {
+  Model
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const Followship = sequelize.define('Followship', {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    followerId: {
-      type: DataTypes.INTEGER
-    },
-    followingId: {
-      type: DataTypes.INTEGER
+  class Followship extends Model {
+    static associate (models) {
+      // define association here
     }
+  }
+  Followship.init({
+    followerId: DataTypes.INTEGER,
+    followingId: DataTypes.INTEGER
   }, {
+    sequelize,
+    modelName: 'Followship',
     tableName: 'Followships',
-    timestamps: true,
-    paranoid: false,
     underscored: true
   })
-  Followship.associate = function (models) {
-  }
   return Followship
 }
