183c709ac156dc474992eedb869dd8fcf2c8eaa9
Ckai <ckai.trans@gmail.com>
2022-10-09 14:08:34 +0800

    feat: add geTweets function which has passed test

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -3,6 +3,27 @@ const { getUser } = require('../_helpers')
 const tweetController = {
   getTweets: (req, res, next) => {
     // GET /api/tweets - 瀏覽所有推文
+    return Tweet.findAll({
+      attributes: { exclude:[ 'updatedAt' ] },
+      include: [{
+        model: User,
+        as: 'tweetAuthor',
+        attributes: [ 'id', 'account', 'name', 'avatar' ]
+      }, 
+      {model: Like},
+      {model: Reply},
+    ]
+    })
+    .then(tweets => {
+      const data = tweets.map(tweet => ({
+        ...tweet.toJSON(),
+        likeCount: tweet.Likes.length,
+        replyCount: tweet.Replies.length
+      }))
+      .sort((a, b) => b.createdAt - a.createdAt)
+      res.status(200).json(data)
+    })
+    .catch(err => next(err))
   },
   getTweet: (req, res, next) => {
     return Tweet.findByPk(req.params.id, {
