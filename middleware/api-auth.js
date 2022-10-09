42cfe16f97d9f1d7561dbaf23032414ebb9b951f
darrenjon <darrenlin23@gmail.com>
2022-10-09 11:29:33 +0800

    modify middleware & authenticate function

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,10 +1,26 @@
 const passport = require('../config/passport')
-const authenticated = passport.authenticate('jwt', { session: false })
-const authenticatedAdmin = (req, res, next) => {
-  if (req.user && req.user.isAdmin) return next()
-  return res.status(403).json({ status: 'error', message: 'permission denied' })
+const helpers = require('../_helpers')
+
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'Unauthorized' })
+    req.user = user
+    next()
+  })(req, res, next)
 }
+
+const authenticatedAdmin = (req, res, next) => {
+  if (helpers.getUser(req).role === 'admin') return next()
+  return res.status(403).json({ status: 'error', message: 'Permission denied' })
+}
+
+const authenticatedUser = (req, res, next) => {
+  if (helpers.getUser(req)?.role === 'user') return next()
+  return res.status(403).json({ status: 'error', message: 'Permission denied' })
+}
+
 module.exports = {
   authenticated,
-  authenticatedAdmin
+  authenticatedAdmin,
+  authenticatedUser
 }
