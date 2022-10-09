b22796e34537fc9284377a0cbc8f40ee377066e3
youjheng <chagam0507@gmail.com>
2022-10-09 16:34:10 +0800

    feat: add admin api about tweets

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -43,6 +43,43 @@ const adminController = {
         return res.status(200).json(userData)
       })
       .catch(error => next(error))
+  },
+  getTweets: (req, res, next) => {
+    return Tweet.findAll({
+      include: [User],
+      order: [['createdAt', 'DESC']]
+    })
+      .then(tweets => {
+        const tweetData = tweets.map(tweet => ({
+          id: tweet.id,
+          description: tweet.description.substring(0, 50),
+          createdAt: tweet.createdAt,
+          updatedAt: tweet.updatedAt,
+          user: {
+            name: tweet.User.name,
+            account: tweet.User.account,
+            image: tweet.User.image
+          }
+        }))
+        return res.status(200).json(tweetData)
+      })
+      .catch(error => next(error))
+  },
+  deleteTweet: (req, res, next) => {
+    const TweetId = req.params.id
+    return Tweet.findByPk(TweetId)
+      .then(tweet => {
+        if (!tweet) throw new Error('推文不存在')
+        return tweet.destroy()
+      })
+      .then(tweet => {
+        return res.status(200).json({
+          status: 'success',
+          message: '刪除推文成功',
+          data: tweet
+        })
+      })
+      .catch(error => next(error))
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,4 +5,8 @@ const adminController = require('../../controllers/admin-controller')
 // user
 router.get('/users', adminController.getUsers)
 
+// tweet
+router.delete('/tweets/:id', adminController.deleteTweet)
+router.get('/tweets', adminController.getTweets)
+
 module.exports = router
