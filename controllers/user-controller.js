d88aecbd98d91cc0b650fa6c3f653d883670bfc9
olly <parker3216@hotmail.com>
2022-10-10 10:58:29 +0800

    fix:editCurrent User bug

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -94,7 +94,7 @@ const userController = {
       const userID = req.user.id
       const id = req.params.id
       const{ account, name, email, password, checkPassword }= req.body
-      const{ account:currentAccount, email:currentEmail }= req.user
+      const{ account: currentAccount, email: currentEmail }= req.user
       
       if(userID !== Number(id)) {
         return res.status(401).json({status:'error',message:'無法編輯其他使用者'})
@@ -114,19 +114,22 @@ const userController = {
       if (account && !validator.isByteLength(account, { min: 0, max: 15 })) {
         res.status(403).json({ status: 'error', message: '帳號長度不可超過15字' })
       }
+
       if (account !== currentAccount) {
-        const userAccount = await User.findOne({ where:account})
+        const userAccount = await User.findOne({ where: { account } })
         if(userAccount){
           res.status(403).json({ status: 'error', message: '此帳號已有人使用！' })
         }
       }
-      if(email !== currentEmail) { 
-        const userEmail = await User.findOne({ where:email })
-        if(userEmail){
+      if (email !== currentEmail) {
+        const userEmail = await User.findOne({ where: { email } })
+        if (userEmail) {
           res.status(403).json({ status: 'error', message: '此信箱已有人使用！' })
         }
       }
-      await User.update({ 
+
+      let user = await User.findByPk(userID)
+      await user.update({ 
         account,
         name,
         email,
