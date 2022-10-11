9b89d6c0f15b22b61064a5ab8490e8005c649146
darrenjon <darrenlin23@gmail.com>
2022-10-11 17:49:16 +0800

    organize user api to solve conflict

--- services/user-services.js
+++ services/user-services.js
@@ -1,9 +1,9 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const { User } = require('../models')
+const { User, Tweet, Reply, Like, Followship } = require('../models')
 const { Op } = require('sequelize')
 const { getUser } = require('../_helpers')
-// const { imgurFileHandler } = require('../helpers/file-helper')
+const { imgurFileHandler } = require('../helpers/file-helper')
 
 const userServices = {
   signUp: (req, cb) => {
@@ -46,6 +46,133 @@ const userServices = {
     } catch (err) {
       cb(err)
     }
+  },
+  getTweets: (req, cb) => {
+    return Tweet.findAll({
+      where: {
+        UserId: req.params.id
+      },
+      include: User,
+      order: [['createdAt', 'DESC']],
+      raw: true,
+      nest: true
+    })
+      .then(tweets => cb(null, tweets))
+      .catch(err => cb(err))
+  },
+  getUser: (req, cb) => {
+    return User.findByPk(req.params.id, {
+      attributes: [
+        'id', 'account', 'name', 'email', 'password'
+      ]
+    })
+      .then(user => {
+        if (!user) {
+          const err = new Error("User didn't exist!")
+          err.status = 404
+          throw err
+        }
+        return cb(null, user)
+      })
+      .catch(err => cb(err))
+  },
+  putUser: (req, cb) => {
+    // if (Number(req.params.id) !== Number(req.user.id)) {
+    //   const err = new Error('User not authorized to edit.')
+    //   err.status = 404
+    //   throw err
+    // }
+    const { file } = req
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist.")
+        return user.update({
+          account: req.body.account,
+          name: req.body.name,
+          email: req.body.email,
+          nickname: req.body.nickname,
+          password: req.body.password,
+          introduction: req.body.introduction,
+          avatar: filePath || user.avatar
+        })
+      })
+      .then(user => {
+        return cb(null, user)
+      })
+      .catch(err => cb(err))
+  },
+  getRepliedTweets: (req, cb) => {
+    // tweetId, userId, repliedId 看見某使用者發過回覆的推文
+    return Promise.all([User.findByPk(req.params.id), Reply.findAll({
+      where: {
+        UserId: req.params.id
+      },
+      include: Tweet,
+      raw: true,
+      nest: true,
+      order: [['createdAt', 'DESC']]
+    })])
+      .then(([user, replies]) => {
+        if (!user) throw new Error("User didn't exist.")
+        return cb(null, replies)
+      })
+      .catch(err => cb(err))
+  },
+  getLikes: (req, cb) => {
+    // 看見某使用者點過 Like的推文(text)
+    return Promise.all([
+      User.findByPk(req.params.id),
+      Like.findAll({
+        where: {
+          UserId: req.params.id
+        }
+      })
+    ])
+      .then(([user, likedTweet]) => {
+        if (!user) {
+          const err = new Error("User didn't exist!")
+          err.status = 404
+          throw err
+        }
+        return cb(null, likedTweet)
+      })
+      .catch((err) => cb(err))
+  },
+  getFollowings: (req, cb) => {
+    // 看見某使用者跟隨中的人
+    return Promise.all([
+      User.findByPk(req.params.id),
+      Followship.findAll({
+        where: {
+          followerId: req.params.id
+        }
+      })
+    ])
+      .then(([user, following]) => {
+        if (!user) throw new Error("User didn't exist.")
+        if (!following) throw new Error("This user isn't following anyone.")
+        return cb(null, following)
+      })
+      .catch(err => cb(err))
+  },
+  getFollowers: (req, cb) => {
+    return Promise.all([
+      User.findByPk(req.params.id),
+      Followship.findAll({
+        where: {
+          followingId: req.params.id
+        }
+      })
+    ])
+      .then(([user, follower]) => {
+        if (!user) throw new Error("User didn't exist.")
+        if (!follower) throw new Error('This user has no followers.')
+        return cb(null, follower)
+      })
+      .catch(err => cb(err))
   }
 }
 
