df56023d0a4e2ded1cd90c87db6aa574193bc57b
olly <parker3216@hotmail.com>
2022-10-11 09:31:44 +0800

    fix:editUser bug

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -14,10 +14,10 @@ const localFileHandler = file => {
   })
 }
 const imgurFileHandler = file =>{
-  return new Promise ((resole,reject) =>{
+  return new Promise ((resolve,reject) =>{
     if(!file) return resolve(null)
     return imgur.uploadFile(file.path)
-    .then(img=>{
+    .then(img => {
       resolve(img?.link || null)
     })
     .catch(err => reject(err))
