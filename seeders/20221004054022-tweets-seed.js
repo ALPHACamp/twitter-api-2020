704e004f02b3e3f5b0641eb8be0b89efb25c96b3
Ckai <ckai.trans@gmail.com>
2022-10-14 21:18:59 +0800

    modify: set the creation time to random

--- seeders/20221004054022-tweets-seed.js
+++ seeders/20221004054022-tweets-seed.js
@@ -11,7 +11,7 @@ module.exports = {
     await queryInterface.bulkInsert('Tweets',
       Array.from({ length: bulkAmount }, (_element, index) => ({
         description: faker.lorem.sentence(Math.floor(Math.random() * 5) + 1),
-        created_at: new Date(),
+        created_at: `2022-09-${Math.floor(Math.random() * 30 + 1)} 05:53:${Math.floor(Math.random() * 59)}`,
         updated_at: new Date(),
         user_id: users[index % usersLength].id
       }))
