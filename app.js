f1485c11602988237860cc8f83aa1f1baa071ed8
Eric LIAO <d1044171009@gmail.com>
2022-10-15 01:21:57 +0800

    modify: 檢查路由位置，刪除不必要的東西與註解

--- app.js
+++ app.js
@@ -10,17 +10,11 @@ const { apis } = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
-// use helpers.getUser(req) to replace req.user
-// function authenticated(req, res, next) {
-//   // passport.authenticate('jwt', { ses...
-// };
-
 app.use(express.urlencoded({ extended: true }))
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use(express.json()) // 把json 文件功能打開，這樣程式才能解析JSON格式的請求物件
 app.use(passport.initialize()) // 初始化 Passport
 app.use((req, res, next) => {
-  // res.locals.user = getUser(req)
   next()
 })
 
--- routes/apis/modules/users.js
+++ routes/apis/modules/users.js
@@ -9,18 +9,18 @@ const upload = require('../../../middleware/multer')
 // 使用者頁面ＡＰＩ
 router.get('/:id/tweets', authenticated, authenticatedUser, userController.getUserTweets) // 取得使用者發過的推文
 router.get('/:id/replied_tweets', authenticated, authenticatedUser, userController.getUserReplies) // 取得使用者回覆過的留言
-router.get('/:id/likes', authenticated, authenticatedUser, userController.getUserLikes)
-router.get('/:id/followings', authenticated, authenticatedUser, userController.getUserFollowings)
-router.get('/:id/followers', authenticated, authenticatedUser, userController.getUserFollowers)
+router.get('/:id/likes', authenticated, authenticatedUser, userController.getUserLikes) // 取得使用者喜歡內容
+router.get('/:id/followings', authenticated, authenticatedUser, userController.getUserFollowings) // 取得使用者跟蹤清單
+router.get('/:id/followers', authenticated, authenticatedUser, userController.getUserFollowers) // 取得使用者追隨清單
 router.put('/:id/setting', authenticated, authenticatedUser, userController.putUserSetting) // 使用者帳號更新
+router.patch('/:id/cover', authenticated, authenticatedUser, userController.removeUserCover) // 刪除封面照片
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn) // 使用者登入
 router.get('/:id', authenticated, authenticatedUser, userController.getUserProfile) // 取得使用者資料
 router.put('/:id', upload.fields([
   { name: 'profilePhoto', maxCount: 1 },
   { name: 'coverPhoto', maxCount: 1 }
 ]), authenticated, authenticatedUser, userController.putUserProfile) // 更新使用者資料
-router.patch('/:id/cover', authenticated, authenticatedUser, userController.removeUserCover)
-router.get('/', authenticated, userController.getCurrentUser)
+router.get('/', authenticated, userController.getCurrentUser) // 取得使用者資料
 router.post('/', userController.signUp) // 使用者註冊
 
 module.exports = router
