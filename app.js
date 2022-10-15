5cde52d41d3097cf77eea0fd4febc713dcc62b2e
LiyLi1122 <teppei.252033@gmail.com>
2022-10-15 20:58:50 +0800

    feat: add port fot front-end

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const cors = require('cors')
 const corsOption = {
   origin: [
     'https://m790101.github.io',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'http://localhost:8080',
+    'http://localhost:8000'
   ],
   methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization']
