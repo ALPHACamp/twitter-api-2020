4fb0e428be15e707383d0764e89016fea7278d9c
olly <parker3216@hotmail.com>
2022-10-16 22:35:35 +0800

    fix:cors

--- routes/apis/modules/tweets.js
+++ routes/apis/modules/tweets.js
@@ -3,9 +3,7 @@ const router = express.Router()
 const tweetController = require('../../../controllers/tweet-controller')
 
 router.get('/:tweet_id/replies', tweetController.getReplies)
-router.post('/:tweet_id/replies', tweetController.addReply,function(req,res){
-  res.set('Access-Control-Allow-Origin', 'http://localhost:8080')
-})
+router.post('/:tweet_id/replies', tweetController.addReply)
 router.post('/:id/unlike', tweetController.unlikeTweet)
 router.post('/:id/like', tweetController.likeTweet)
 router.get('/:id', tweetController.getTweet)
