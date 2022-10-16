2e6530b738cd07613929aa33cc97846ff934a38a
olly <parker3216@hotmail.com>
2022-10-16 17:31:03 +0800

    fix:followshipSeed bug

--- seeders/20221013082147-followshipSeed.js
+++ seeders/20221013082147-followshipSeed.js
@@ -1,20 +1,23 @@
 'use strict'
-
 const faker = require('faker')
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Followships',
-    Array.from({length:18}).map((item,index) => ({
-      id: index + 1,
-      following_id: index + 1,
-      follower_id: index + 2,
-      created_at: faker.date.between('2022-07-05T00:00:00.000Z', '2022-08-05T00:00:00.000Z'),
-      updated_at: new Date()
+    const followship =[]
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;', 
+      { type: queryInterface.sequelize.QueryTypes.SELECT })
 
-    }) 
-    
-    ),{})
+    users.forEach(user => {users.map(following => {
+        if (following.id > user.id) {followship.push({
+            follower_id: user.id,
+            following_id: following.id,
+            created_at: faker.date.between('2022-07-05T00:00:00.000Z', '2022-08-05T00:00:00.000Z'),
+            updated_at: new Date()
+          })
+        }
+      })
+    })
+    queryInterface.bulkInsert('Followships', followship)
   },
 
   down: async (queryInterface, Sequelize) => {
