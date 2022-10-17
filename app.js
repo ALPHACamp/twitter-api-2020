379631a2502c36b5f34be32b7ef2f3eb5bc22a9c
olly <parker3216@hotmail.com>
2022-10-17 08:32:19 +0800

    fix:cors

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use(cors({
 
 app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,HEAD,PATCH,OPTIONS');
+  res.append('Access-Control-Allow-Headers', 'Content-Type', 'authorization');
   next();
 })
 
