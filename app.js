b420bf0ab8ee6472103ef06c4ea258e218711636
seanlin1125 <sean821125@gmail.com>
2022-12-07 17:04:09 +0800

    project init

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const path = require('path')
 const express = require('express')
-const helpers = require('./_helpers');
+const flash = require('connect-flash')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('./config/passport')
+const helpers = require('./_helpers')
+const routes = require('./routes')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = helpers.getUser(req)
+  next()
+})
+
+app.use(routes)
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
-  // passport.authenticate('jwt', { ses...
-};
+// function authenticated (req, res, next) {
+// passport.authenticate('jwt', { ses...
+// };
 
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
--- test/models/Reply.spec.js
+++ test/models/Reply.spec.js
@@ -1,7 +1,7 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const proxyquire = require('proxyquire');
-chai.use(require('sinon-chai'));
+const chai = require('chai')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+chai.use(require('sinon-chai'))
 
 const { expect } = require('chai')
 const {
@@ -37,13 +37,13 @@ describe('# Reply Model', () => {
     it('called Reply.init with the correct parameters', () => {
       expect(Reply.init).to.have.been.calledWithMatch(
         {
-          comment: DataTypes.TEXT,
-        },
+          comment: DataTypes.TEXT
+        }
       )
     })
   })
 
-  // 檢查 reply 的關聯是否正確 
+  // 檢查 reply 的關聯是否正確
   context('associations', () => {
     const User = 'User'
     const Tweet = 'Tweet'
@@ -53,12 +53,12 @@ describe('# Reply Model', () => {
       Reply.associate({ Tweet })
     })
 
-    it('should belong to user', (done) => {
+    it('should belong to user', done => {
       // 檢查是否有呼叫 belongsTo(User)
       expect(Reply.belongsTo).to.have.been.calledWith(User)
       done()
     })
-    it('should belong to tweet', (done) => {
+    it('should belong to tweet', done => {
       // 檢查是否有呼叫 belongsTo(Tweet)
       expect(Reply.belongsTo).to.have.been.calledWith(Tweet)
       done()
@@ -66,40 +66,38 @@ describe('# Reply Model', () => {
   })
   // 檢查 model 的新增、修改、刪除、更新
   context('action', () => {
-
     let data = null
     // 檢查 db.Reply 是否真的可以新增一筆資料
-    it('create', (done) => {
-      db.Reply.create({}).then((reply) => {   
+    it('create', done => {
+      db.Reply.create({}).then(reply => {
         data = reply
         done()
       })
     })
     // 檢查 db.Reply 是否真的可以讀取一筆資料
-    it('read', (done) => {
-      db.Reply.findByPk(data.id).then((reply) => {  
+    it('read', done => {
+      db.Reply.findByPk(data.id).then(reply => {
         expect(data.id).to.be.equal(reply.id)
-          done()
-        })
+        done()
+      })
     })
     // 檢查 db.Reply 是否真的可以更新一筆資料
-    it('update', (done) => {
-      db.Reply.update({}, { where: { id: data.id }}).then(() => {
-        db.Reply.findByPk(data.id).then((reply) => { 
-          expect(data.updatedAt).to.be.not.equal(reply.updatedAt) 
+    it('update', done => {
+      db.Reply.update({}, { where: { id: data.id } }).then(() => {
+        db.Reply.findByPk(data.id).then(reply => {
+          expect(data.updatedAt).to.be.not.equal(reply.updatedAt)
           done()
         })
       })
     })
     // 檢查 db.Reply 是否真的可以刪除一筆資料
-    it('delete', (done) => {
-      db.Reply.destroy({ where: { id: data.id }}).then(() => {
-        db.Reply.findByPk(data.id).then((reply) => { 
-          expect(reply).to.be.equal(null) 
+    it('delete', done => {
+      db.Reply.destroy({ where: { id: data.id } }).then(() => {
+        db.Reply.findByPk(data.id).then(reply => {
+          expect(reply).to.be.equal(null)
           done()
         })
       })
     })
   })
-
 })
