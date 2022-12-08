669544df61b077cfecd4b442f2e95b8ec34cdc24
seanlin1125 <sean821125@gmail.com>
2022-12-08 16:44:10 +0800

    feat: test main branch

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/', (req, res) => {
-  res.json('api test')
+  res.json('api test main')
 })
 
 module.exports = router
