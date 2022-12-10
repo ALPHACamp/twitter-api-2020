1ba907ea6e396d02c6739ae9b2b8960e51b8ccc5
seanlin1125 <sean821125@gmail.com>
2022-12-10 19:53:13 +0800

    feat: set up tweet model

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,25 @@
 'use strict'
+const {
+  Model
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-  }, {})
-  Tweet.associate = function (models) {
-  }
+  class Tweet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+
+    }
+  };
+  Tweet.init({
+    UserId: DataTypes.INTEGER,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Tweet',
+    tableName: 'Tweets'
+  })
   return Tweet
 }
