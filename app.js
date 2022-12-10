f44237a205bd18549551dea4b70361bf9b9be70e
Benson.YBS <yehbo0921@gmail.com>
2022-12-10 23:52:56 +0800

    env: set dotenv

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const helpers = require('./_helpers')
-
 const app = express()
 const port = process.env.PORT || 3000
 
