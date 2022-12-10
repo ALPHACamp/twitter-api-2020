f6ff1e8f77870d537ae4daecc09fdb94d92479ad
Benson.YBS <yehbo0921@gmail.com>
2022-12-10 22:18:28 +0800

    env: add relations in models Since the code in models is different from forum project, use template code first and will rewrite other models in next commit

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,23 @@
-'use strict';
+'use strict'
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {
-  }, {});
-  Reply.associate = function(models) {
-  };
-  return Reply;
-};
+  class XXXX extends Model {
+    static associate (models) {
+      // Relation(Two Example below)
+      // User.hasMany(models.Reply, { foreignKey: 'UserId' })
+      // User.belongsToMany(models.User, {
+      //   through: models.Followship,
+      //   foreignKey: 'followerId',
+      //   as: 'Followings'
+      // })
+    }
+  }
+  XXXX.init({
+    // field: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'XXXX',
+    tableName: 'XXXXs'
+  })
+  return XXXX
+}
