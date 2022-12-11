fed84a943311a564e4e40dfca802c7caba19adb7
weizi0328 <ankewang26@gmail.com>
2022-12-11 20:07:43 +0800

    feat (error): add error handler in middleware

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const { generalErrorHandler } = require('../../middleware/error-handler')
+
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
+router.use('/', generalErrorHandler)
 
 module.exports = router
