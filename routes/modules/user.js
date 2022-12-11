3077962ad83c4f35439d8188f1d9f4242dce7d89
Howhow Chen <debby8698@gmail.com>
2022-12-11 20:42:05 +0800

    feat: add user self

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,4 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const { User, Tweet, Reply, Like } = require('../../models')
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const user = await User.findByPk(id, {
+      raw: true
+    })
+
+    //  刪除密碼
+    delete user.password
+    res.json({
+      user
+    })
+  } catch (err) {
+    next(err)
+  }
+})
 
 module.exports = router
