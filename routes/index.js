91ff43a0bd75f1dcc5476eca7f7fbe5ea7ea7377
Howhow Chen <debby8698@gmail.com>
2022-12-11 13:43:50 +0800

    feat: add users router file

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const user = require('./modules/user')
+
 const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
@@ -9,6 +11,9 @@ const { RegisterValidator } = require('../middleware/validator-handler')
 router.post('/api/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
 router.post('/api/signup', RegisterValidator, userController.signUp)
+
+// modules
+router.use('/api/users', authenticated, authenticatedUser, user)
 
 // not found router
 router.use('/', (_, res, next) => {
