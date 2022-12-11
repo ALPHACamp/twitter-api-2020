2876cb954214081226a2c964a9ebb6a02e25bdf2
OneZerocococo <113199704+OneZerocococo@users.noreply.github.com>
2022-12-11 15:35:10 +0800

    Feature user get tweet (#15)
    * feat: add admin get tweets
    
    * feat: modify config for heroku
    
    * modify start of scripts
    
    * modify port
    
    * merge origin master
    
    * feat: add user get tweet feature
    
    * remove replies of get tweet

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -31,6 +31,31 @@ const tweetController = {
     } catch (err) {
       next(err)
     }
+  },
+  getTweet: async (req, res, next) => {
+    try {
+      const loginUser = getUser(req)?.id
+      const tweetId = req.params.tweet_id
+      const tweet = await Tweet.findByPk(tweetId, {
+        include: [{ model: User, attributes: ['account', 'name', 'avatar'] }],
+        attributes: {
+          include: [
+            [sequelize.literal(`(SELECT COUNT(*) AS replyCounts FROM replies
+          WHERE Tweet_id = tweet.id)`), 'replyCounts'],
+            [sequelize.literal('(SELECT COUNT(*) AS likeCounts FROM likes WHERE Tweet_id = tweet.id)'), 'likeCounts']
+          ]
+        },
+        raw: true,
+        nest: true
+      })
+      if (!tweet) throw new Error('推文不存在')
+      const likes = await Like.findAll({ where: { TweetId: tweetId } })
+      const isLiked = likes.some(l => l.UserId === loginUser)
+      const data = { ...tweet, isLiked }
+      res.status(200).json(data)
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = tweetController
--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const { authenticated, authenticatedUser } = require('../../middleware/auth')
 const tweetController = require('../../controllers/tweet-controller')
 
+// 使用者取得一則貼文
+router.get('/:tweet_id', authenticated, authenticatedUser, tweetController.getTweet)
 // 使用者取得所有貼文
 router.get('/', authenticated, authenticatedUser, tweetController.getTweets)
 
