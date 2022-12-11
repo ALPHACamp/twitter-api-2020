9897a6484d7d472ba3351d2290892b591e797b47
seanlin1125 <sean821125@gmail.com>
2022-12-11 16:37:17 +0800

    feat: add passport, jwt and userController.signIn

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
+const userController = require('../controllers/user-controller')
 
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.use('/', (req, res) => {
   res.json('api test main')
 })
