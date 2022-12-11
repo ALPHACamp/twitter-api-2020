0ffc80e17b7e1022f1264780818a77e93b2f4c1a
weizi0328 <ankewang26@gmail.com>
2022-12-11 20:13:43 +0800

    feat: add authentication via passport

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,37 @@
 const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const { User } = require('../models')
 
-
+// set up Passport strategy
+passport.use(new LocalStrategy(
+  // customize user field
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  // authenticate user
+  (req, email, password, cb) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        bcrypt.compare(password, user.password).then(res => {
+          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          return cb(null, user)
+        })
+      })
+  }
+))
+// serialize and deserialize user
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then(user => {
+    console.log(user)  //暫時添加
+    return cb(null, user)
+  })
+})
 
 module.exports = passport
