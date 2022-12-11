1cf58ce6ac3dd7dd4d11a8a5c8e549eb32f4c732
cosine0720 <cosine0720@gmail.com>
2022-12-11 17:10:10 +0800

    feat: add userController.getCurrentUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,21 @@
-const userController = {
+const { User, Tweet, Reply, Like, Followship } = require('../models')
+const { getUser } = require('../_helpers')
 
+const userController = {
+  // 瀏覽 Profile：tweetCount, followerCount, followingCount
+  getCurrentUser: (req, res, next) => {
+    return User.findByPk(getUser(req).id, {
+      include: [
+        { model: Tweet },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        return res.json({ currentUser: getUser(req) })
+      })
+      .catch(err => next(err))
+  }
 }
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/user-controller')
+
+router.get('/users', userController.getCurrentUser)
+// router.get('/users/:id', userController.getUser)
 
 router.use('/', (req, res) => {
   res.json('api test main')
