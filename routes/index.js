7892d85304b1e1a162b653b783f566fd10d8cb13
seanlin1125 <sean821125@gmail.com>
2022-12-11 19:56:41 +0800

    feat:add admin signIn and getUsers routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
+const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/auth')
 
-router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+// 後台登入
+router.post('/admin/signin', passport.authenticate('local', { session: false }), authenticatedAdmin, userController.signIn)
+// 後台功能路由
+router.use('/admin', authenticated, authenticatedAdmin, admin)
+// 前台登入
+router.post('/signin', passport.authenticate('local', { session: false }), authenticatedUser, userController.signIn)
+
 router.use('/', (req, res) => {
   res.json('api test main')
 })
