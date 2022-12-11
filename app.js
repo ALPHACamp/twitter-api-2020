3407738ec011611296cf2327160ae88e3645003c
weizi0328 <ankewang26@gmail.com>
2022-12-11 18:16:06 +0800

    project init

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const routes = require('./routes')
-const helpers = require('./_helpers');
+const routes = require('./routes/apis')
+const helpers = require('./_helpers')
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -8,11 +9,15 @@ const port = process.env.PORT || 3000
 // use helpers.getUser(req) to replace req.user
 function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
-};
+}
+
+app.use(express.urlencoded({ extended: true }))
+// app.use(methodOverride('_method'))
 
 app.use(routes)
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
