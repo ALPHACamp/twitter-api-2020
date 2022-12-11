bb07e526f0cf0c156c6d4045ae01e5a4565db767
weizi0328 <ankewang26@gmail.com>
2022-12-11 20:17:58 +0800

    feat: update authentication in passport.js

--- config/passport.js
+++ config/passport.js
@@ -7,13 +7,13 @@ const { User } = require('../models')
 passport.use(new LocalStrategy(
   // customize user field
   {
-    usernameField: 'email',
+    usernameField: 'account',
     passwordField: 'password',
     passReqToCallback: true
   },
   // authenticate user
-  (req, email, password, cb) => {
-    User.findOne({ where: { email } })
+  (req, account, password, cb) => {
+    User.findOne({ where: { account } })
       .then(user => {
         if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
         bcrypt.compare(password, user.password).then(res => {
