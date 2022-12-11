7fc8d4303b8d10c8c61ba0349e37845a0cefb745
cosine0720 <cosine0720@gmail.com>
2022-12-11 18:07:04 +0800

    feat: add authenticated

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
-{ authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/auth')
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
-router.get('/users', userController.getCurrentUser)
+router.get('/users', authenticated, userController.getCurrentUser)
 
 router.use('/', (req, res) => {
   res.json('api test main')
