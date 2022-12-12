23607be62fdeb39238223675bd7b15895beee37a
AlanSquid <squid78313@gmail.com>
2022-12-12 17:52:50 +0800

    feat: create user seeder

--- models/user.js
+++ models/user.js
@@ -1,8 +1,21 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
   }, {});
-  User.associate = function(models) {
+  User.associate = function (models) {
   };
+  User.init({
+    account: DataTypes.STRING,
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.BOOLEAN,
+    avator: DataTypes.STRING,
+    introduction: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true,
+  });
   return User;
 };
