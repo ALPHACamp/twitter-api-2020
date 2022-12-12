b4756af88367de1448acb939a6b6022e33242431
YangYgYang <you@example.com>
2022-12-13 07:20:44 +0800

    feat(user): add postUsers route

---
+++ controllers/user-controller.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcryptjs')
+const { User } = require('../models')
+
+const userController = {
+    postUsers:(req, res, next) => {
+        const {account, name, email, password, passwordCheck} = req.body
+        if (!account || !name || !email || !password || !passwordCheck) throw Error('all field is required!',{},Error.prototype.code = 402)
+        if (password !== passwordCheck) throw Error('passwords do not match!',{},Error.prototype.code = 422)
+        if (password !== passwordCheck) throw Error('passwords do not match!',{},Error.prototype.code = 422)
+        if (account.length > 50 || name.length > 50) throw Error('name or account over',{},Error.prototype.code = 403)
+        if (!email.match(/^\w+@\w+\.\w+$/i)) throw Error('invalid email format',{},Error.prototype.code = 401)
+
+        Promise.all([
+            User.findOne({ where: { email } }),
+            User.findOne({ where: { account } }),
+        ])
+        .then(([userEmail,userAccount]) => {
+          if (userEmail) throw new Error('Email already exists!',{},Error.prototype.code = 408)
+          if (userAccount) throw new Error('Account already exists!',{},Error.prototype.code = 423)
+          return bcrypt.hash(password, 10)
+        })
+        .then(hash => User.create({
+          account,
+          name,
+          email,
+          password: hash,
+          cover:'https://i.imgur.com/KNbtyGq.png'
+
+        }))
+        .then((user) => {
+          const data = {
+             'status':200,
+             'data':user.get({plain:true})
+            }
+          res.json(data)
+        })
+        .catch(err => next(err))
+    }
+}
+
+module.exports = userController
