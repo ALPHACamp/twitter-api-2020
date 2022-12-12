bf366df0b56e77b3da4ea90ff818945af6afe858
Howhow Chen <debby8698@gmail.com>
2022-12-13 02:10:34 +0800

    feat: add deleteFollowing

--- controllers/followship-controller.js
+++ controllers/followship-controller.js
@@ -46,6 +46,48 @@ const followshipController = {
     } catch (err) {
       next(err)
     }
+  },
+  deleteFollowing: async (req, res, next) => {
+    try {
+      const currentUserId = helpers.getUser(req).id
+      const followingId = Number(req.params.followingId)
+
+      if (followingId === currentUserId) {
+        return res.status(422).json({
+          status: 'error',
+          message: '使用者不可以取消追蹤自己.'
+        })
+      }
+
+      const [user, followship] = await Promise.all([
+        User.findByPk(followingId),
+        Followship.findOne({
+          where: {
+            followerId: currentUserId,
+            followingId
+          }
+        })
+      ])
+
+      // 確認使用者是否存在
+      if (!user || user.role === 'admin') {
+        return res.status(404).json({
+          status: 'error',
+          message: '使用者不存在.'
+        })
+      }
+
+      // 確認是否尚未按過追蹤
+      if (!followship) throw new Error('你還沒追蹤喔!')
+
+      await followship.destroy()
+
+      res.status(200).json({
+        status: 'success'
+      })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
