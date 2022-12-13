9cabcb0f48b7c2f035062bb893c2e8e34789df47
Elio <eliochen34@gmail.com>
2022-12-13 17:25:16 +0800

    eslint

--- migrations/20190115071418-create-followship.js
+++ migrations/20190115071418-create-followship.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Followships', {
@@ -26,9 +26,9 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Followships');
+    return queryInterface.dropTable('Followships')
   }
-};
+}
---
+++ seeders/20221213075653-replies-seed-file.js
@@ -0,0 +1,28 @@
+'use strict'
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users WHERE is_admin = false;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const tweets = await queryInterface.sequelize.query(
+      'SELECT id FROM Tweets;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    await queryInterface.bulkInsert('Replies',
+      Array.from({ length: 150 }, () => ({
+        description: faker.lorem.sentence(),
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+        tweet_id: tweets[Math.floor(Math.random() * tweets.length)].id,
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', {})
+  }
+}
