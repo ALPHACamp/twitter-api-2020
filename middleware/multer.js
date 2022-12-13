ce4018d44d2af3b3b70890c5796dde1d8bd0f990
SimonHung8 <111163128+SimonHung8@users.noreply.github.com>
2022-12-13 15:47:46 +0800

    update multer middleware with mimetype check (#29)
    * feat: add editUserSetting feature
    
    * feat: add editUserProfile feature
    
    * update multer middleware with mimetype check

--- middleware/multer.js
+++ middleware/multer.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 const upload = multer({
   dest: 'uploads/',
   fileFilter: (req, file, cb) => {
-    if (file && file.mimetype !== 'image/png') cb(new Error('Image file only!'))
+    if (file && !file.mimetype.includes('image')) cb(new Error('Image file only!'))
     cb(null, true)
   }
 })
