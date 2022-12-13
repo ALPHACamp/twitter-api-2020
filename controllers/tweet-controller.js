df1f7b6e013e18f264ff97f67c6ce6277be1f112
OneZerocococo <113199704+OneZerocococo@users.noreply.github.com>
2022-12-13 10:26:29 +0800

    Feature user get tweets (#27)
    * feat: add admin get tweets
    
    * feat: modify config for heroku
    
    * modify start of scripts
    
    * modify port
    
    * merge origin master
    
    * feat: add user get tweets feature
    
    * modify variable name
    
    * modify get tweets literal sql syntax do not limit dev database
    
    * refactor: get tweets function
    
    * add nest to get tweets

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -31,7 +31,8 @@ const tweetController = {
           ]
         },
         order: [['createdAt', 'DESC']],
-        raw: true
+        raw: true,
+        nest: true
       })
       const loginUser = getUser(req)?.id
       const likes = await Like.findAll({
