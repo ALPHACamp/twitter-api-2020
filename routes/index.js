2c33a8a9af0bd042b432ff7b55903475395f4499
weizi0328 <ankewang26@gmail.com>
2022-12-13 23:03:56 +0800

    feat: modify config for Heroku rev

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user-controller')
 
-const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
-
 
 router.put('/users/:id', userController.putUser)
 router.get('/users/:id', userController.getUser)
