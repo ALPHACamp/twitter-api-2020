42ba34573c2fac26d452dd696c1721342b1888bd
weizi0328 <ankewang26@gmail.com>
2022-12-13 13:14:18 +0800

    merge origin/master

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const userController = require('../controllers/user-controller')
 
 const userController = require('../controllers/user-controller')
 
@@ -9,5 +10,8 @@ router.get('/api/users/:id', userController.getUser)
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+router.post('/users', userController.postUsers)
+router.use('/', generalErrorHandler)
 
 module.exports = router
