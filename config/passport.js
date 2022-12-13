f366f8432a3546cc88485a0e0fe9af05b9624f2c
Howhow Chen <debby8698@gmail.com>
2022-12-13 14:59:29 +0800

    feat: add defaultJwtKey

--- config/passport.js
+++ config/passport.js
@@ -40,7 +40,7 @@ passport.use(new LocalStrategy(
 
 const jwtOptions = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET,
+  secretOrKey: process.env.JWT_SECRET || 'alphacamp',
   passReqToCallback: true
 }
 
