57604b580fa1af7b861c5e95487b65c11459cc92
weizi0328 <ankewang26@gmail.com>
2022-12-13 22:59:50 +0800

    feat: modify config for heroku

--- app.js
+++ app.js
@@ -11,11 +11,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 
-
 app.use('/api', routes)
-
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!!Let's go to http://localhost:${port}`))
 
