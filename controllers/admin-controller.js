6f93c8666a929cd017d62b5f1612d62f5fc9a27f
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-12-13 09:50:01 +0800

    merge: master

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -8,11 +8,8 @@ const adminController = {
       const loginUser = helpers.getUser(req).toJSON()
       if (loginUser?.role === 'user') return res.status(403).json({ status: 'error', message: 'Permission denied.' })
       delete loginUser.password
-<<<<<<< HEAD
-=======
       loginUser.createdAt = dayjs(loginUser.createdAt).valueOf()
       loginUser.updatedAt = dayjs(loginUser.updatedAt).valueOf()
->>>>>>> 1609435b036c0f9e17c09af833fdee22d5340325
       const token = jwt.sign(loginUser, process.env.JWT_SECRET, { expiresIn: '5d' })
       res.status(200).json({ status: 'success', data: { token, user: loginUser } })
     } catch (err) { next(err) }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,20 +1,14 @@
 const jwt = require('jsonwebtoken')
 const helpers = require('../_helpers')
-<<<<<<< HEAD
-=======
 const dayjs = require('dayjs')
->>>>>>> 1609435b036c0f9e17c09af833fdee22d5340325
 const userController = {
   signIn: (req, res, next) => {
     try {
       const loginUser = helpers.getUser(req).toJSON()
       if (loginUser?.role === 'admin') return res.status(403).json({ status: 'error', message: 'Permission denied.' })
       delete loginUser.password
-<<<<<<< HEAD
-=======
       loginUser.createdAt = dayjs(loginUser.createdAt).valueOf()
       loginUser.updatedAt = dayjs(loginUser.updatedAt).valueOf()
->>>>>>> 1609435b036c0f9e17c09af833fdee22d5340325
       const token = jwt.sign(loginUser, process.env.JWT_SECRET, { expiresIn: '5d' })
       res.status(200).json({ status: 'success', data: { token, user: loginUser } })
     } catch (err) { next(err) }
