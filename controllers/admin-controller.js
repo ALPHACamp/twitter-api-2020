5aceb26e842a70e9bdc644f35d3a109de6f6d08e
weizi0328 <ankewang26@gmail.com>
2022-12-14 16:42:59 +0800

    feat (admin): add delete tweet feature without finishing wrong msg

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,33 +1,48 @@
 const { User, Tweet } = require('../models')
+// const { getUser } = require('../_helpers')
 
 const adminController = {
   getTweets: (req, res, next) => {
+    // if (!getUser(req)) {
+    //   return res.status(401).json({ status: 'error', message: 'token is invalidated' })
+    // } else if (!req.user) // error 5XX 這邊應該怎麼判斷伺服器錯誤呀？
+    // {
+    //   return res.status(500).json({ status: 'error', message: 'internal server error' })
+    // }
     Tweet.findAll({
       nest: true,
       raw: true,
-      include: [User]
+      include: {
+        model: User,
+        attributes: ['id', 'account', 'name', 'avatar']
+      }
     })
       .then(tweets => {
         const data = tweets.map(t => ({
           ...t,
           description: t.description.substring(0, 50)
         }))
-        // delete data.User.password 我不會刪除user資料的password
         return res.status(200).json({ status: 'success', data })
       })
       .catch(err => next(err))
   },
   deleteTweet: (req, res, next) => {
+    // if (!getUser(req)) {
+    //   return res.status(401).json({ status: 'error', message: 'token is invalidated' })
+    // } else if (!req.user) // error 5XX 這邊應該怎麼判斷伺服器錯誤呀？
+    // {
+    //   return res.status(500).json({ status: 'error', message: 'internal server error' })
+    // }
     return Tweet.findByPk(req.params.id)
       .then(tweet => {
         if (!tweet) {
-          const err = new Error('Tweet did not exist!')
-          err.status = 404
-          throw err
+          return res.status(400).json(
+            { status: 'error', message: 'Tweet did not exist!' }
+          )
         }
+        res.status(200).json({ status: 'success', data: tweet })
         return tweet.destroy()
       })
-      .then(deletedTweet => res.json({ status: 'success', data: deletedTweet }))
       .catch(err => next(err))
   }
 }
