56c67c548b8e2973dbfe4d42fbf638474dcdac88
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-12-14 19:37:44 +0800

    feat: add cors origin and header

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const routes = require('./routes')
 const passport = require('./config/passport')
+const cors = require('cors')
 const { generalErrorHandler } = require('./middleware/error-handler')
 const app = express()
 const port = process.env.PORT || 3000
 
+const corsOptions = {
+  origin: ['https://s339428326.github.io/', 'http://localhost:3000/'],
+  methods: ['GET', 'POST', 'DELETE', 'PATCH', 'PUT', 'HEAD', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
+app.use(cors(corsOptions))
 
 app.use('/api', routes)
 app.use('/', (req, res) => res.send('Hello World!')) // fallback
