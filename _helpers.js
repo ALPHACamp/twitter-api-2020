dec3444c72e62109c802adcd4dbff372dda0f736
ping8601 <wanping86701@gmail.com>
2022-12-14 14:39:27 +0800

    feat: add imgur file handler

--- _helpers.js
+++ _helpers.js
@@ -1,8 +1,24 @@
+const imgur = require('imgur')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
+imgur.setClientId(IMGUR_CLIENT_ID)
 
 const getUser = req => {
   return req.user || null
 }
 
+const imgurFileHandler = file => {
+  return new Promise((resolve, reject) => {
+    if (!file) return resolve(null)
+    return imgur.uploadFile(file.path)
+      .then(img => {
+        return resolve(img?.link || null)
+      })
+      .catch(err => reject(err))
+  })
+}
+
 module.exports = {
-  getUser
+  getUser,
+  imgurFileHandler
 }
