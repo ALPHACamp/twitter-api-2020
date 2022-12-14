7fa6f4baf9755973a69475d437a313af7a247d67
miaout11 <miaout.11@gmail.com>
2022-12-14 19:42:08 +0800

    先把admin/followships routes放上去並補上auth middleware

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,17 @@ const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
 
 const { apiErrorHandler } = require('../middleware/error-handler')
 const users = require('./modules/users')
+const tweets = require('./modules/tweets')
+const admin = require('./modules/admin')
+const followships = require('./modules/followships')
 
 // signup & login
 router.use('/users', authenticated, users)
 
+router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.use('/tweets', authenticated, tweets)
+router.use('/followships', authenticated, followships)
+
 router.use('/', apiErrorHandler)
-
-const tweets = require('./modules/tweets')
-
-router.use('/tweets', tweets)
 
 module.exports = router
