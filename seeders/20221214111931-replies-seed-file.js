1e55184e57ce775ca2a742d89d361484e3e9931c
Verna0214 <yuhsichen0214@gmail.com>
2022-12-14 21:45:39 +0800

    feat: add replies_seed

---
+++ seeders/20221214111931-replies-seed-file.js
@@ -0,0 +1,28 @@
+'use strict'
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const tweets = await queryInterface.sequelize.query(
+      'SELECT id FROM Tweets;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    await queryInterface.bulkInsert('Replies',
+      Array.from({ length: tweets.length * 3 }, (_, index) => ({
+        userId: users[index % users.length].id,
+        tweetId: tweets[index % tweets.length].id,
+        comment: faker.lorem.sentence(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', {})
+  }
+}
