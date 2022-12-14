ab767d414fe01aa2898cc29f1932e5f3925c8eb6
ping8601 <wanping86701@gmail.com>
2022-12-14 17:15:24 +0800

    fix: exclude root & 10 tweets for each user

--- seeders/20221214082811-tweets-seed-file.js
+++ seeders/20221214082811-tweets-seed-file.js
@@ -1,13 +1,16 @@
 'use strict'
 const faker = require('faker')
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const users = await queryInterface.sequelize.query('SELECT id FROM Users;', { type: queryInterface.sequelize.QueryTypes.SELECT }
+    const userIds = await queryInterface.sequelize.query(
+      "SELECT id FROM Users WHERE role = 'user';",
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
     await queryInterface.bulkInsert('Tweets',
-      Array.from({ length: 50 }, () => ({
-        userId: users[Math.floor(Math.random() * users.length)].id,
-        description: faker.lorem.text(),
+      Array.from({ length: 50 }, (_, index) => ({
+        userId: userIds[index % userIds.length].id,
+        description: faker.lorem.sentences(2),
         createdAt: new Date(),
         updatedAt: new Date()
       }))
