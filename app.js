c3d7b3035601e5241201fb75e03b382a9f88011f
wujia777 <cwu005104@gmail.com>
2022-12-14 21:57:25 +0800

    feat: unknown error to be U01 without U00

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
-app.use('/api', routes)
 // use helpers.getUser(req) to replace req.user
 // / function authenticated (req, res, next) {
 // passport.authenticate('jwt', { ses...
@@ -19,7 +18,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
 app.use(methodOverride('_method'))
-
+app.use('/api', routes)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
 
+router.post('/signup', userController.signUp)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 
 module.exports = router
