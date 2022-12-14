624e5c0b837a993b6a6de47cf0a92be24728c801
ping8601 <wanping86701@gmail.com>
2022-12-14 21:21:53 +0800

    fix: add account

--- models/user.js
+++ models/user.js
@@ -18,6 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     })
   }
   User.init({
+    account: DataTypes.STRING,
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
