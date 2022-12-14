d051fcf5bf1d9779f008ad588925f0caad307006
Gincoolwant <soulbox790326@gmail.com>
2022-12-14 16:31:06 +0800

    feat: tweets seed ready

--- seeders/20221214071702-tweet-seed-file.js
+++ seeders/20221214071702-tweet-seed-file.js
@@ -2,13 +2,14 @@
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const TweetsPerUser = 10
     const users = await queryInterface.sequelize.query(
       'SELECT id FROM Users;',
       { type: queryInterface.sequelize.QueryTypes.SELECT })
 
     await queryInterface.bulkInsert('Tweets',
-      Array.from({ length: 50 }, (_, index) => ({
-        User_id: users[Math.floor(index / 10) + 1].id,
+      Array.from({ length: (users.length - 1) * TweetsPerUser }, (_, index) => ({
+        User_id: users[Math.floor(index / TweetsPerUser) + 1].id,
         description: faker.lorem.text(),
         created_at: new Date(),
         updated_at: new Date()
