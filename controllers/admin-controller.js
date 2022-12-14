5ddcf7f912f17a6f287fd45894535b3f6c8e694e
weizi0328 <ankewang26@gmail.com>
2022-12-14 14:19:47 +0800

    feat (admin): add view tweets feature without deleting user's password

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -3,12 +3,17 @@ const { User, Tweet } = require('../models')
 const adminController = {
   getTweets: (req, res, next) => {
     Tweet.findAll({
+      nest: true,
+      raw: true,
       include: [User]
     })
       .then(tweets => {
-        console.log(tweets)
-        res.send('hi')
-        // res.status(200).json({ status: 'success', data: tweets })
+        const data = tweets.map(t => ({
+          ...t,
+          description: t.description.substring(0, 50)
+        }))
+        // delete data.User.password 我不會刪除user資料的password
+        return res.status(200).json({ status: 'success', data })
       })
       .catch(err => next(err))
   }
--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,8 @@ const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
-router.use('/admin', authenticated, authenticatedAdmin, admin)
+// router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.use('/admin', admin)
 
 router.put('/users/:id', authenticated, userController.putUser)
 router.get('/users/:id', authenticated, userController.getUser)
