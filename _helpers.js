7abe0e968994772d3d3fa2b9f39674540bd9319e
ping8601 <wanping86701@gmail.com>
2022-12-14 16:44:58 +0800

    feat: add pagination helpers

--- _helpers.js
+++ _helpers.js
@@ -3,10 +3,16 @@ const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 imgur.setClientId(IMGUR_CLIENT_ID)
 
+// auth helpers
 const getUser = req => {
   return req.user || null
 }
 
+const ensureAuthenticated = req => {
+  return req.isAuthenticated()
+}
+
+// file helpers
 const imgurFileHandler = file => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
@@ -18,7 +24,28 @@ const imgurFileHandler = file => {
   })
 }
 
+// pagination helpers
+const getOffset = (limit = 10, page = 1) => (page - 1) * limit
+
+const getPagination = (limit = 10, page = 1, total = 50) => {
+  const totalPage = Math.ceil(total / limit)
+  const pages = Array.from({ length: totalPage }, (_, index) => index + 1)
+  const currentPage = page < 1 ? 1 : page > totalPage ? totalPage : page
+  const prev = currentPage - 1 < 1 ? 1 : currentPage - 1
+  const next = currentPage + 1 > totalPage ? totalPage : currentPage + 1
+  return {
+    totalPage,
+    pages,
+    currentPage,
+    prev,
+    next
+  }
+}
+
 module.exports = {
   getUser,
-  imgurFileHandler
+  ensureAuthenticated,
+  imgurFileHandler,
+  getOffset,
+  getPagination
 }
