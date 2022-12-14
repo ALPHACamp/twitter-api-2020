b990db64c88358fa696268ef49f239e92cfac130
ping8601 <wanping86701@gmail.com>
2022-12-14 20:31:47 +0800

    feat: add get users

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const { User, Reply, Tweet, Like } = require('../models')
+const { getUser } = require('../_helpers')
 
 const userController = {
   getUser: (req, res, next) => {
@@ -15,6 +16,25 @@ const userController = {
       .then(user => {
         if (!user) assert(user, "User doesn't exist!")
         res.json({ status: 'success', data: user })
+      })
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    const top = Number(req.query.top)
+    const currentUser = getUser(req)
+    return User.findAll({
+      include: [{ model: User, as: 'Followers' }]
+    })
+      .then(users => {
+        const result = users
+          .map(user => ({
+            ...user.toJSON(),
+            followerCount: user.Followers.length,
+            isFollowed: currentUser.Followings.some(f => f.id === user.id)
+          }))
+          .sort((a, b) => b.followerCount - a.followerCount)
+          .slice(0, top || users.length)
+        res.json({ status: 'success', data: result })
       })
       .catch(err => next(err))
   }
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const userController = require('../../controllers/user-controller')
 
 router.get('/users/:id', userController.getUser)
+router.get('/users', userController.getUsers)
 
 module.exports = router
