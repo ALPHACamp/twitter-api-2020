9f2be6f17ea9f8dc9f12d99f4ee1f756316cefdd
Coli-co <awestlifeblue@gmail.com>
2022-12-15 15:46:09 +0800

    authenticate user and admin route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -4,18 +4,21 @@ const router = express.Router()
 const passport = require('../../config/passport')
 const userController = require('../../controllers/user-controller')
 const {
-  authenticated,
-  authenticatedAdmin
+  authenticatedUser,
+  authenticated
 } = require('../../middleware/api-auth')
 
+// 登入不需要驗證登入狀態
 router.post(
   '/signin',
   passport.authenticate('jwt', { session: false }),
+  authenticatedUser,
   userController.signIn
 )
 
-router.get('/:id', authenticated, userController.getUser)
+router.get('/:id', authenticatedUser, authenticated, userController.getUser)
 
+// 註冊不需要驗證登入狀態
 router.post('/', userController.signUp)
 
 module.exports = router
