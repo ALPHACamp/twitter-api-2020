4fe53b53fa344e9b9f38c6cb35e9e265a32f56d9
weizi0328 <ankewang26@gmail.com>
2022-12-15 09:51:30 +0800

    feat: commit before push to heroku

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,27 @@
 const { User, Tweet } = require('../models')
-// const { getUser } = require('../helpers/auth-helper')
+// const { getUser } = require('../_helpers')
 
 const adminController = {
+  getUsers: (req, res, next) => {
+    // if (!getUser(req)) {
+    //   return res.status(401).json({ status: 'error', message: 'token is invalidated' })
+    // }
+    return User.findAll({
+      include: [
+        { model: User, as: 'Followers' }
+        // { model: User, as: 'Followings' }
+      ]
+    })
+      .then(users => {
+        users = users.map(user => ({
+          ...user.toJSON(),
+          followerCount: user.Followers.length
+          // followingCount: user.Followings.length
+        }))
+        res.status(200).json(users)
+      })
+      .catch(err => next(err))
+  },
   getTweets: (req, res, next) => {
     // if (!getUser(req)) {
     //   return res.status(401).json({ status: 'error', message: 'token is invalidated' })
