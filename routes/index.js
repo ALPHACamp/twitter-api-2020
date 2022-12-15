e5dd51f2847698a813a7321da9a5f1e32b05863e
Verna0214 <yuhsichen0214@gmail.com>
2022-12-15 17:48:56 +0800

    feat: add admin login

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,9 @@ const user = require('./modules/user')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedUser, authenticatedAdmin } = require('../middleware/auth')
 const userController = require('../controllers/user-controller')
-
+const adminController = require('../controllers/admin-controller')
 // admin
+router.post('/api/admin/users/login', passport.authenticate('local', { session: false }), adminController.adminLogin)
 router.use('/api/admin', authenticated, authenticatedAdmin, admin)
 
 // user management
