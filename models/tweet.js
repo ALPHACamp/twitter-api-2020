ff0143fe538e2d21c4e12b1d6fbfe6f19c6ed2c9
ping8601 <wanping86701@gmail.com>
2022-12-15 12:16:31 +0800

    feat: add associate between User & Tweet

--- models/tweet.js
+++ models/tweet.js
@@ -6,6 +6,11 @@ module.exports = (sequelize, DataTypes) => {
     Tweet.hasMany(models.Reply, { foreignKey: 'TweetId' })
     Tweet.belongsTo(models.User, { foreignKey: 'UserId' })
     Tweet.hasMany(models.Like, { foreignKey: 'TweetId' })
+    Tweet.belongsToMany(models.User, {
+      through: models.Like,
+      foreignKey: 'TweetId',
+      as: 'LikedUsers'
+    })
   }
   Tweet.init({
     description: DataTypes.TEXT,
