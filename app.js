28ac4a56dcf73d673ad73942bec0df1cf4c49689
Gincoolwant <soulbox790326@gmail.com>
2022-12-16 14:28:58 +0800

    test authenticated on heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const routes = require('./routes/')
 
 const app = express()
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
