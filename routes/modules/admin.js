008453b3f15767b4a98dd84da6ad3573ecc489c6
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-12-16 11:02:07 +0800

    feat: add admin getUsers

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,4 +3,5 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/tweets', adminController.getTweets)
+router.get('/users', adminController.getUsers)
 module.exports = router
