567c38bc5216cc9a2afa9b41c4aa767965ee9d17
Mami <113319389+shanelin0904@users.noreply.github.com>
2022-12-16 14:54:20 +0800

    Update apis.js
     rename route id

--- routes/modules/apis.js
+++ routes/modules/apis.js
@@ -22,14 +22,14 @@ router.get('/tweets/:tweet_id', authenticated, tweetController.getTweet)
 router.get('/tweets', authenticated, tweetController.getTweets)
 router.post('/tweets', authenticated, tweetController.postTweet)
 
-router.post('/tweets/:id/like', authenticated, likeController.addLike)
-router.post('/tweets/:id/unlike', authenticated, likeController.unLike)
+router.post('/tweets/:tweet_id/like', authenticated, likeController.addLike)
+router.post('/tweets/:tweet_id/unlike', authenticated, likeController.unLike)
 
 router.post('/followships', authenticated, followshipController.addFollowing)
-router.delete('/followships/:followingId', authenticated, followshipController.removeFollowing)
+router.delete('/followships/:following_id', authenticated, followshipController.removeFollowing)
 
 
 router.get('/admin/users', authenticatedAdmin, adminController.getUsers)
-router.delete('/admin/tweets/:tweetId', authenticatedAdmin, adminController.deleteTweet)
+router.delete('/admin/tweets/:tweet_id', authenticatedAdmin, adminController.deleteTweet)
 
 module.exports = router
