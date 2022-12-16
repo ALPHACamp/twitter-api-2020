ff23b9b546457623d334ebc44b5ee1d28ba41d14
ping8601 <wanping86701@gmail.com>
2022-12-16 17:59:08 +0800

    feat: add users get likes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -156,6 +156,17 @@ const userController = {
     } catch (err) {
       next(err)
     }
+  },
+  getLikes: async (req, res, next) => {
+    try {
+      const { id } = req.params
+      let user = await User.findOne({ where: { id }, include: Like, nest: true })
+      if (!user) return res.status(404).json({ status: 'error', message: '使用者不存在！' })
+      user = user.toJSON()
+      return res.status(200).json(user.Likes)
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
