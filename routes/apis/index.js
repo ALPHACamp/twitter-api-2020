734d75686239c519284ac012bce49c2b91690180
programmerP22 <chiangcw19@gmail.com>
2022-12-16 19:17:55 +0800

    feat add authenticatedUser

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -6,8 +6,7 @@ const admin = require('./modules/admin')
 const userController = require('../../controllers/user-controller')
 const tweetController = require('../../controllers/tweet-controller')
 
-// const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
-const { authenticated } = require('../../middleware/api-auth')
+const { authenticated, authenticatedUser } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
 router.use('/admin', admin)
@@ -16,9 +15,9 @@ router.post('/users', userController.register)
 
 router.post('/login', passport.authenticate('local', { session: false }), userController.login)
 
-router.get('/tweets/:tweet_id', authenticated, tweetController.getTweet)
-router.get('/tweets', authenticated, tweetController.getTweets)
-router.post('/tweets', authenticated, tweetController.postTweets)
+router.get('/tweets/:tweet_id', authenticated, authenticatedUser, tweetController.getTweet)
+router.get('/tweets', authenticated, authenticatedUser, tweetController.getTweets)
+router.post('/tweets', authenticated, authenticatedUser, tweetController.postTweets)
 
 router.use('/', apiErrorHandler)
 
