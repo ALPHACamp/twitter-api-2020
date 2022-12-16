c517f5f384891e5b81804a327229057834830a56
Elio <eliochen34@gmail.com>
2022-12-16 12:32:42 +0800

    feat: add getUser-function

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -6,14 +6,16 @@ const admin = require('./modules/admin')
 const userController = require('../../controllers/user-controller')
 const tweetController = require('../../controllers/tweet-controller')
 
-const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
+const { authenticated } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
-router.use('/admin', authenticated, authenticatedAdmin, admin)
+// router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.use('/admin', passport.authenticate('local', { session: false }), admin)
 
 router.post('/users', userController.register)
 
 router.post('/login', passport.authenticate('local', { session: false }), userController.login)
+router.get('/users/:user_id', authenticated, userController.getUser)
 
 router.get('/tweets/:tweet_id', authenticated, tweetController.getTweet)
 router.get('/tweets', authenticated, tweetController.getTweets)
