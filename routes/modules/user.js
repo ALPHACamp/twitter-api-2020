c78fc9ec84a66940f495b868e29ed7b120baf8dc
ping8601 <wanping86701@gmail.com>
2022-12-16 16:20:22 +0800

    feat: update put user profile & put user account

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -3,10 +3,12 @@ const router = express.Router()
 
 const userController = require('../../controllers/user-controller')
 const upload = require('../../middleware/multer')
+const batchUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }])
 
-router.put('/users/:id', userController.putUser)
+router.put('/users/:id', batchUpload, userController.putUserProfile)
+router.put('/users/:id/account', batchUpload, userController.putUserAccount)
 router.get('/users/:id', userController.getUser)
-router.post('/users', upload.single('image'), userController.postUser)
+router.post('/users', userController.postUser)
 router.get('/users', userController.getUsers)
 
 module.exports = router
