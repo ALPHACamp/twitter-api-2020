e93402221da61ba10dee2cea298557b9b8d24258
weizi0328 <ankewang26@gmail.com>
2022-12-16 11:40:48 +0800

    merge devV1

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,19 +2,19 @@ const passport = require('../config/passport')
 
 
 const authenticated = (req, res, next) => {
-    passport.authenticate('jwt', { session: false }, (err, user) => {
-      if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
-      req.user = user
-      next()
-    })(req, res, next)
-  }
+	passport.authenticate('jwt', { session: false }, (err, user) => {
+		if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+		req.user = user
+		next()
+	})(req, res, next)
+}
 
 const authenticatedAdmin = (req, res, next) => {
-    passport.authenticate('jwt', { session: false }, (err, user) => {
-        if (err || !user || user.dataValues.role !== 1) return res.status(401).json({ status: 'error', message: 'permission denied' })
-        req.user = user
-        next()
-      })(req, res, next)
+	passport.authenticate('jwt', { session: false }, (err, user) => {
+		if (err || !user || user.dataValues.role !== 1) return res.status(401).json({ status: 'error', message: 'permission denied' })
+		req.user = user
+		next()
+	})(req, res, next)
 
 }
 
