0e7ceff6c3a24190680105837a1f520ecba1c4e1
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-12-16 20:00:31 +0800

    Merge branch 'feature/addAdmin' of https://github.com/shanelin0904/twitter-api-2020 into feature/addAdmin
    # Conflicts:
    #	services/admin-services.js

--- services/user-services.js
+++ services/user-services.js
@@ -6,26 +6,17 @@ const { User } = require('../models')
 const userServices = {
   // 使用者註冊
   signUp: (req, cb) => {
-    const { account, email, password, checkPassword } = req.body
-    let name = req.body.name
-    // 驗證name內容是否超過上限字數，若超過則提示
-    const nameLengthLimit = 50
-    if (name.length > nameLengthLimit) {
-      throw new Error(
-        `Name的內容超過${nameLengthLimit}字, 請縮短!(${name.length}/${nameLengthLimit})`)
-    }
+    const { account, name, email, password, checkPassword } = req.body
     // 驗證兩次密碼輸入是否相符，若不符則提示錯誤訊息
-    if (password !== checkPassword) throw new Error('請再次確認密碼!')
-    // 若name未填，default為account
-    if (!name) name = account
+    if (password !== checkPassword) throw new Error('Password do not match!')
     // 使用者account & email在資料庫皆須為唯一，任一已存在資料庫則提示錯誤訊息
     Promise.all([
       User.findOne({ where: { account } }),
       User.findOne({ where: { email } })
     ])
       .then(([userFindByAccount, userFindByEmail]) => {
-        assert(!userFindByAccount, 'Account 已重複註冊!')
-        assert(!userFindByEmail, 'Email 已重複註冊！')
+        assert(!userFindByAccount, 'The account already exist.')
+        assert(!userFindByEmail, 'The Email already exist.')
         // input驗證OK，bcrypt密碼
         return bcrypt.hash(password, 10)
       })
@@ -38,11 +29,7 @@ const userServices = {
           password: hash
         })
       })
-      .then(createdUser => {
-        createdUser = createdUser.toJSON()
-        delete createdUser.password
-        cb(null, { createdUser })
-      })
+      .then(createdUser => cb(null, { createdUser }))
       .catch(err => cb(err))
   },
   signIn: (req, cb) => {
@@ -56,5 +43,6 @@ const userServices = {
     }
   }
 }
+
 
 module.exports = userServices
