7109aa3b6460b2e8181c2cdc2a3fdad6a6585da5
miaout11 <miaout.11@gmail.com>
2022-12-17 13:03:45 +0800

    調整回傳的錯誤訊息

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 module.exports = {
   generalErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
-      req.flash('error_messages', `${err.name}: ${err.message}`)
+      req.flash('error_messages', `${err.message}`)
     } else {
       req.flash('error_messages', `${err}`)
     }
@@ -11,12 +11,12 @@ module.exports = {
   apiErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
       res.status(err.status || 500).json({
-        success: 'false',
-        message: `${err.name}: ${err.message}`
+        success: false,
+        message: `${err.message}`
       })
     } else {
       res.status(500).json({
-        success: 'false',
+        success: false,
         message: `${err}`
       })
     }
