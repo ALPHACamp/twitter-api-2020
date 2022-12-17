e6d1c20730041b59625ea4d13cfdb8dc2b8ca55a
miaout11 <miaout.11@gmail.com>
2022-12-17 09:59:45 +0800

    delete tweet補上admin身分驗證

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -7,6 +7,6 @@ const adminController = require('../../controllers/admin-controller')
 
 router.post('/signin', adminController.signIn) // 後台登入
 router.get('/users', authenticated, authenticatedAdmin, adminController.getUsers) // 查看所有使用者
-router.delete('/tweets/:id', adminController.deleteTweet) // 刪除特定推文
+router.delete('/tweets/:id', authenticated, authenticatedAdmin, adminController.deleteTweet) // 刪除特定推文
 
 module.exports = router
