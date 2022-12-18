292621f3cdd417d8500bb9027ed42fccb8ea95fe
miaout11 <miaout.11@gmail.com>
2022-12-19 00:22:05 +0800

    modify putUser feature & users route

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -8,50 +8,27 @@ const {
 } = require('../../middleware/api-auth')
 const upload = require('../../middleware/multer')
 
-// 登入不需要驗證登入狀態
-router.post('/signin', userController.signIn)
-
 // 查看特定使用者按過like的推文 ********Evelyn 先註記一下 避免搞混
 router.get('/:id/likes', authenticated, authenticatedUser, user2Controller.getUserLikes)
 // 查看特定使用者發過的所有回覆 ********Evelyn 先註記一下 避免搞混
 router.get('/:id/replied_tweets', authenticated, authenticatedUser, user2Controller.getUserReplies)
 // 查看特定使用者發過的所有推文 ********Evelyn 先註記一下 避免搞混
-router.get(
-  '/:id/tweets',
-  authenticated,
-  authenticatedUser,
-  user2Controller.getUserTweets
-)
-
+router.get('/:id/tweets', authenticated, authenticatedUser, user2Controller.getUserTweets)
+// 查看特定使用者的追蹤者們
+router.get('/:id/followings', authenticated, authenticatedUser, userController.getFollowing)
+// 查看特定使用者的追隨者們
+router.get('/:id/followers', authenticated, authenticatedUser, userController.getFollower)
 // 查看特定使用者頁面
 router.get('/:id', authenticated, authenticatedUser, userController.getUser)
 
 // 編輯個人頁面
-router.put(
-  '/:id',
-  authenticated,
-  authenticatedUser,
-  upload.fields([
-    { name: 'avatar', maxCount: 1 },
-    { name: 'cover', maxCount: 1 }
-  ]),
-  userController.putUser
-)
-// 查看特定使用者的追蹤者們
-router.get(
-  '/:id/followings',
-  authenticated,
-  authenticatedUser,
-  userController.getFollowing
-)
+router.put('/:id', upload.fields([
+  { name: 'avatar', maxCount: 1 },
+  { name: 'cover', maxCount: 1 }
+]), authenticated, authenticatedUser, user2Controller.putUser)
 
-// 查看特定使用者的追隨者們
-router.get(
-  '/:id/followers',
-  authenticated,
-  authenticatedUser,
-  userController.getFollower
-)
+// 登入不需要驗證登入狀態
+router.post('/signin', userController.signIn)
 // 註冊不需要驗證登入狀態
 router.post('/', userController.signUp)
 
