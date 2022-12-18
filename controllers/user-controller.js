deb15af24fcbf682a4ad5668964d357b8996e60a
SimonHung8 <111163128+SimonHung8@users.noreply.github.com>
2022-12-18 19:55:59 +0800

    remove pagination in getUserFollowers and getUserFollowings (#38)
    * feat: add admin get tweets
    
    * feat: modify config for heroku
    
    * modify start of scripts
    
    * modify port
    
    * merge origin master
    
    * merge feature user get tweet
    
    * merge modify: user get tweets function
    
    * modified: package-lock.json
    
    * modified: package-lock.json
    
    * merge branch refactor
    
    * feat: get users tweets replies add pagination
    
    * feat: admin get tweets add pagination
    
    * feat: get user tweets/replies/likes/followings/followers add pagination
    
    * remove pagination in getUserFollowers and getUserFollowings
    
    Co-authored-by: OneZerocococo <ylflyfly@gmail.com>

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -161,9 +161,6 @@ const userController = {
   },
   getUserFollowings: async (req, res, next) => {
     try {
-      const page = Number(req.query.page) || 1
-      const limit = 10
-      const offset = (page - 1) * limit
       const { id } = req.params
       const user = await User.findOne({ where: { id, role: 'user' } })
       if (!user) throw new Error("User didn't exist!")
@@ -178,8 +175,6 @@ const userController = {
           ]
         },
         order: [['createdAt', 'DESC']],
-        limit,
-        offset,
         raw: true
       })
       res.status(200).json(followings)
@@ -189,9 +184,6 @@ const userController = {
   },
   getUserFollowers: async (req, res, next) => {
     try {
-      const page = Number(req.query.page) || 1
-      const limit = 10
-      const offset = (page - 1) * limit
       const { id } = req.params
       const user = await User.findOne({ where: { id, role: 'user' } })
       if (!user) throw new Error("User didn't exist!")
@@ -206,8 +198,6 @@ const userController = {
           ]
         },
         order: [['createdAt', 'DESC']],
-        limit,
-        offset,
         raw: true
       })
       res.status(200).json(followers)
