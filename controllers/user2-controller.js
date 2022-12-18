260bb8115888f04729a4b3d05692a4539bce8168
miaout11 <miaout.11@gmail.com>
2022-12-18 16:47:19 +0800

    add getUserLikes feature

--- controllers/user2-controller.js
+++ controllers/user2-controller.js
@@ -63,6 +63,44 @@ const user2Controller = {
         res.status(200).json(replies)
       })
       .catch(err => next(err))
+  },
+  getUserLikes: (req, res, next) => {
+    const currentUserId = helpers.getUser(req)?.id // 正在使用網站的使用者id
+    const UserId = Number(req.params.id) // 要查看的特定使用者id
+
+    // 要撈特定使用者資料/特定使用者like的資料/現在使用者like的資料(要判斷現在使用者是否like)
+    return Promise.all([
+      User.findByPk(UserId),
+      Like.findAll({
+        where: { UserId },
+        attributes: ['id', 'TweetId', 'createdAt'],
+        include: [{
+          model: Tweet,
+          attributes: ['id', 'description', 'createdAt',
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM Replies AS replyCount WHERE tweet_id = Tweet.id)'), 'replyCount' // 回傳留言數
+            ],
+            [
+              sequelize.literal('(SELECT COUNT(*) FROM Likes AS likeCount WHERE tweet_id = Tweet.id)'), 'likeCount' // 回傳按讚數
+            ]],
+          include: [{ model: User, attributes: ['id', 'name', 'account', 'avatar'] }]
+        }],
+        order: [['createdAt', 'DESC']],
+        raw: true,
+        nest: true
+      }),
+      Like.findAll({ where: { UserId: currentUserId }, raw: true })
+    ])
+      .then(([user, likes, currentUserlikes]) => {
+        if (!user) throw new Error("User didn't exist")
+        const currentUserlikeList = currentUserlikes.map(like => like.TweetId)
+        const data = likes.map(like => ({
+          ...like,
+          isLiked: currentUserlikeList.some(TweetId => TweetId === like.TweetId)
+        }))
+        res.status(200).json(data)
+      })
+      .catch(err => next(err))
   }
 
 }
