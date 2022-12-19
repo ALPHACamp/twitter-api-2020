87777317f82f1d4282eece757b94e623c3a1b06c
wujia777 <cwu005104@gmail.com>
2022-12-19 13:31:04 +0800

    feat: add getUserTweets

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,10 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
-const { User, sequelize } = require('../models')
+const { User, Tweet, sequelize } = require('../models')
 const helpers = require('../_helpers')
 const { imgurFileHandler } = require('../helpers/file-helper')
+const { relativeTime } = require('../helpers/date-helper')
 
 const userController = {
   signIn: (req, res, next) => {
@@ -152,6 +153,38 @@ const userController = {
         delete updatedUser.password
         delete updatedUser.role
         res.status(200).json({ status: 'success', message: '使用者資料更新成功!', data: updatedUser })
+      })
+      .catch(err => next(err))
+  },
+  getUserTweets: (req, res, next) => {
+    const { id } = req.params
+    const currentUser = helpers.getUser(req)
+    return Promise.all([
+      User.findByPk(id),
+      Tweet.findAll({
+        where: { UserId: id },
+        order: [['createdAt', 'DESC']],
+        raw: true,
+        nest: true,
+        attributes: [
+          'id',
+          'UserId',
+          'description',
+          'createdAt',
+          [sequelize.literal('(SELECT COUNT(id) FROM Likes WHERE Likes.Tweet_id = Tweet.id)'), 'likeCount'],
+          [sequelize.literal('(SELECT COUNT(id) FROM Replies WHERE Replies.Tweet_id = Tweet.id)'), 'replyCount']
+        ]
+      })
+    ])
+      .then(([user, tweets]) => {
+        if (!user) throw new Error('查無使用者!')
+        if (!tweets) throw new Error('貼文不存在!')
+        const data = tweets.map(t => ({
+          ...t,
+          createdAt: relativeTime(t.createdAt),
+          isLiked: currentUser?.Likes?.some(currentUserLike => currentUserLike?.TweetId === t.id)
+        }))
+        res.status(200).json(data)
       })
       .catch(err => next(err))
   }
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -4,9 +4,10 @@ const userController = require('../../controllers/user-controller')
 const upload = require('../../middleware/multer')
 const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }])
 
+// 取得使用者推文
+router.get('/:id/tweets', userController.getUserTweets)
 // 取得使用者資料
 router.get('/:id', userController.getUser)
-
 // 更新使用者資料
 router.put('/:id', cpUpload, userController.putUser)
 module.exports = router
