0f4e20630f5973bbad5cb96bf1916b30ca72701c
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-12-20 12:52:04 +0800

    fix: change cors setting, allowing all

--- app.js
+++ app.js
@@ -10,21 +10,7 @@ const { generalErrorHandler } = require('./middleware/error-handler')
 const app = express()
 const port = process.env.PORT || 3000
 
-const corsOptions = {
-  origin: [
-    'https://s339428326.github.io',
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:3002',
-    'http://localhost:3003',
-    'http://localhost:3004',
-    'http://localhost:3005',
-    'http://localhost:3006'
-  ],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-  allowedHeaders: ['Content-Type', 'Authorization']
-}
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
