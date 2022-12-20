9133c9cf4f7e2ca9a5b8c51ca0b33d7c974d250d
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-12-20 22:44:49 +0800

    refactor: likes seed

--- seeders/20221211182804-likes-seed-file.js
+++ seeders/20221211182804-likes-seed-file.js
@@ -1,13 +1,12 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const LIKES_NUMBER = 36
     const users = await queryInterface.sequelize.query("SELECT id FROM Users WHERE `role` <> 'admin'", { type: queryInterface.sequelize.QueryTypes.SELECT })
     const tweets = await queryInterface.sequelize.query('SELECT id FROM Tweets', { type: queryInterface.sequelize.QueryTypes.SELECT })
     await queryInterface.bulkInsert(
       'Likes',
-      Array.from({ length: LIKES_NUMBER }, () => ({
-        UserId: users[Math.floor(Math.random() * users.length)].id,
+      Array.from({ length: users.length }, (_, i) => ({
+        UserId: users[i].id,
         TweetId: tweets[Math.floor(Math.random() * tweets.length)].id,
         createdAt: new Date(),
         updatedAt: new Date()
