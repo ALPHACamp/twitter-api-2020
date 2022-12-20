01c1bad1042946065483fddb5b8903332aa711c4
Gincoolwant <soulbox790326@gmail.com>
2022-12-20 12:35:25 +0800

    feat: setting CORS via cors

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,7 +1,10 @@
 const fs = require('fs') // 引入 fs 模組
 const imgur = require('imgur')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+imgur.setClientId(IMGUR_CLIENT_ID) // 匿名也可以上傳。有Client ID可讓上傳圖片被歸戶便於管理
+
 // 將temp folder中file複製至本地端upload folder
-const uploadFileLocal = file => { // file 是 multer 處理完的檔案
+const uploadLocal = file => { // file 是 multer 處理完的檔案
   // Promise找不到file就結束，有file就複製一份並回傳file name
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
@@ -25,6 +28,6 @@ const uploadImgur = file => {
   })
 }
 module.exports = {
-  uploadFileLocal,
+  uploadLocal,
   uploadImgur
 }
