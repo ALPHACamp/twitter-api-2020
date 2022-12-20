6d99c67e2c849c2f27147f95ca9a962c7d1c7fdb
miaout11 <miaout.11@gmail.com>
2022-12-20 11:33:05 +0800

    整理一下暫時沒用到的東西

--- helpers/auth-helpers.js
+++ helpers/auth-helpers.js
@@ -1,12 +1,12 @@
-const getUser = req => {
-  return req.user || null
-}
+// const getUser = req => {
+//   return req.user || null
+// }
 
-const ensureAuthenticated = req => {
-  return req.isAuthenticated()
-}
+// const ensureAuthenticated = req => {
+//   return req.isAuthenticated()
+// }
 
-module.exports = {
-  getUser,
-  ensureAuthenticated
-}
+// module.exports = {
+//   getUser,
+//   ensureAuthenticated
+// }
