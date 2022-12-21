aaf8414f8fb26469d268cf701b9ef81e2f5431f7
Verna0214 <yuhsichen0214@gmail.com>
2022-12-21 10:03:50 +0800

    feat: add github cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000
 // 使用cors允許跨域連線
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://yvonne0414.github.io'],
     credentials: true
   })
 )
