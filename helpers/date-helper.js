91c03508515549e295241ff4a3b242dd93a585ea
seanlin1125 <sean821125@gmail.com>
2022-12-22 20:46:04 +0800

    refactor: set time zone to Taiwan time

--- helpers/date-helper.js
+++ helpers/date-helper.js
@@ -1,8 +1,13 @@
 const dayjs = require('dayjs')
 const relativeTime = require('dayjs/plugin/relativeTime')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone') // dependent on utc plugin
 require('dayjs/locale/zh-tw')
 dayjs.locale('zh-tw')
 dayjs.extend(relativeTime)
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Asia/Taipei')
 
 module.exports = {
   dateFormat: date => dayjs(date)
