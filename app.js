e1f6306c2d9cba8bc1fcc3e9433af636d682beb8
weizi0328 <ankewang26@gmail.com>
2022-12-22 14:49:39 +0800

    feat: remove cors options in app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
+  require('dotenv').config()
 }
 
 const express = require('express')
@@ -10,10 +10,10 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
-const corsOptions = {
-  origin: ['https://yhosutun2490.github.io', 'http://localhost:3000'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}
+// const corsOptions = {
+//   origin: ['https://yhosutun2490.github.io', 'http://localhost:3000'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }
 
 const path = require('path')
 app.use('/upload', express.static(path.join(__dirname, 'upload')))
@@ -23,13 +23,13 @@ app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use(express.urlencoded({ extended: true }))
 app.use(passport.initialize())
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use('/api', routes)
 
 // app.use('/', (req, res) => res.send('Hello World!'))
 // app.use(generalErrorHandler)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!!Let's go to http://localhost:${port}`))
+app.listen(port, () => console.log(`E---xample app listening on port ${port}!!Let's go to http://localhost:${port}`))
 
 module.exports = app
