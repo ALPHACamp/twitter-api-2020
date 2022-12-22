d68787c10305dc507f648a349243ca8ed22d6024
weizi0328 <ankewang26@gmail.com>
2022-12-22 16:25:25 +0800

    fix(admin): followerCount in getUsers

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -23,10 +23,14 @@ const adminController = {
 					'id', 'account', 'email', 'name', 'avatar', 'introduction', 'cover', 'role', 'followingCount', 'createdAt', 'updatedAt',
 					[sequelize.literal('(SELECT COUNT(id) FROM Tweets WHERE Tweets.user_id = User.id)'), 'tweetCount'],
 					[sequelize.literal('(SELECT COUNT(id) FROM Likes WHERE Likes.user_id = User.id)'), 'likeCount'],
-					[sequelize.literal('(SELECT COUNT(id) FROM Followships WHERE Followships.follower_id = User.id)'), 'followerCount']
+					[sequelize.literal('(SELECT COUNT(id) FROM Followships WHERE Followships.following_id = User.id)'), 'followerCount']
 				],
 				order: [['createdAt', 'DESC']]
 			})
+			// const dataOrder = await usersData.sort(function (a, b) {
+			// 	return a.tweetCount < b.tweetCount
+			// })
+			// console.log(dataOrder)
 			res.status(200).json(usersData)
 		} catch (err) { next(err) }
 	},
