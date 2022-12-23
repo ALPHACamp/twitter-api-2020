86ed3504bb54a10c63b1a52a04dd78ffe6eb97b0
weizi0328 <ankewang26@gmail.com>
2022-12-23 14:53:31 +0800

    fix(admin): wrong msg in login

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
-const passport = require('../../config/passport')
+const { authenticated, authenticatedAdmin, adminLoginAuth } = require('../../middleware/auth')
 const { generalErrorHandler } = require('../../middleware/error-handler')
-
 const adminController = require('../../controllers/admin-controller')
 
+router.post('/login', adminLoginAuth, adminController.login)
 router.delete('/tweets/:id', authenticated, authenticatedAdmin, adminController.deleteTweet)
 router.get('/tweets', authenticated, authenticatedAdmin, adminController.getTweets)
 router.get('/users', authenticated, authenticatedAdmin, adminController.getUsers)
-router.post('/login', passport.authenticate('localAdmin', { session: false }), adminController.login)
+
 
 router.use('/', generalErrorHandler)
 
