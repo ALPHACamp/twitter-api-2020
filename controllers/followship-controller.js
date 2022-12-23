71865498f04f200357edad8043c5949278d92638
seanlin1125 <sean821125@gmail.com>
2022-12-23 14:41:56 +0800

    refactor: modify followship-controller to try-catch

--- controllers/followship-controller.js
+++ controllers/followship-controller.js
@@ -2,62 +2,68 @@ const { User, Followship, sequelize } = require('../models')
 const helpers = require('../_helpers')
 
 const followshipController = {
-  addFollowing: (req, res, next) => {
-    const followingId = Number(req.body.id)
-    if (helpers.getUser(req).id === followingId) throw new Error('不能追蹤自己!')
-    return Promise.all([
-      User.findByPk(followingId),
-      Followship.findOne({
+  addFollowing: async (req, res, next) => {
+    try {
+      const currentUser = helpers.getUser(req)
+      const followingId = Number(req.body.id)
+      if (currentUser.id === followingId) throw new Error('不能追蹤自己!')
+      const [user, followship] = await Promise.all([
+        User.findByPk(followingId),
+        Followship.findOne({
+          where: {
+            followerId: currentUser.id,
+            followingId
+          }
+        })
+      ])
+      if (!user) throw new Error('使用者不存在!')
+      if (followship) throw new Error('你已經追蹤此使用者了!')
+      const newFollowship = await Followship.create({
+        followerId: currentUser.id,
+        followingId
+      })
+      const newFollowshipData = newFollowship.toJSON()
+      res.status(200).json(newFollowshipData)
+    } catch (error) {
+      next(error)
+    }
+  },
+  removeFollowing: async (req, res, next) => {
+    try {
+      const followingId = Number(req.params.followingId)
+      const followship = await Followship.findOne({
         where: {
           followerId: helpers.getUser(req).id,
           followingId: followingId
         }
       })
-    ])
-      .then(([user, followship]) => {
-        if (!user || user?.role === 'admin') throw new Error('使用者不存在!')
-        if (followship) throw new Error('你已經追蹤此使用者了!')
-        return Followship.create({
-          followerId: helpers.getUser(req).id,
-          followingId: followingId
-        })
-      })
-      .then(newFollowship => res.json(newFollowship))
-      .catch(err => next(err))
+      if (!followship) throw new Error('你還沒追蹤此使用者!')
+      const deleteFollowship = await followship.destroy()
+      const deleteFollowshipData = deleteFollowship.toJSON()
+      res.status(200).json(deleteFollowshipData)
+    } catch (error) {
+      next(error)
+    }
   },
-  removeFollowing: (req, res, next) => {
-    const followingId = Number(req.params.followingId)
-    Followship.findOne({
-      where: {
-        followerId: helpers.getUser(req).id,
-        followingId: followingId
-      }
-    })
-      .then(followship => {
-        if (!followship) throw new Error('你還沒追蹤此使用者!')
-        return followship.destroy()
+  getTopUsers: async (req, res, next) => {
+    try {
+      const currentUser = helpers.getUser(req)
+      const users = await User.findAll({
+        attributes: [
+          'id', 'account', 'name',
+          [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id )'), 'followerCount'],
+          [sequelize.literal(`EXISTS (SELECT id FROM Followships WHERE Followships.followerId = ${currentUser.id} AND Followships.followingId = User.id )`), 'isFollowed']
+        ],
+        order: [
+          [sequelize.literal('followerCount'), 'DESC'], ['createdAt', 'DESC']
+        ],
+        raw: true
       })
-      .then(deleteFollowship => res.json(deleteFollowship))
-      .catch(err => next(err))
-  },
-  getTopUsers: (req, res, next) => {
-    const currentUser = helpers.getUser(req)
-    return User.findAll({
-      attributes: [
-        'id', 'account', 'name',
-        [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id )'), 'followerCount'],
-        [sequelize.literal(`EXISTS (SELECT id FROM Followships WHERE Followships.followerId = ${currentUser.id} AND Followships.followingId = User.id )`), 'isFollowed']
-      ],
-      order: [
-        [sequelize.literal('followerCount'), 'DESC'], ['createdAt', 'DESC']
-      ],
-      raw: true
-    })
-      .then(users => {
-        const usersData = users.slice(0, 10)
-        res.json(usersData)
-      })
-      .catch(err => next(err))
+      const topUsersData = users.slice(0, 10)
+      res.status(200).json(topUsersData)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
