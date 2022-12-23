52d6bec7feba4faa2faf0c23c64b6c8488cd4ba0
weizi0328 <ankewang26@gmail.com>
2022-12-23 16:56:50 +0800

    fix(passport): wrong msg code

--- config/passport.js
+++ config/passport.js
@@ -24,7 +24,7 @@ passport.use('local', new LocalStrategy(
 				if (user.dataValues.role !== 'user') return cb(null, false, { status: 423, message: 'you aren’t registered!' })
 				bcrypt.compare(password, user.password)
 					.then(res => {
-						if (!res) return cb(null, false, { status: '402', message: 'account or password invalid!' })
+						if (!res) return cb(null, false, { status: 402, message: 'account or password invalid!' })
 						return cb(null, user)
 					})
 			})
@@ -46,7 +46,7 @@ passport.use('localAdmin', new LocalStrategy(
 				if (user.dataValues.role !== 'admin') return cb(null, false, { status: 423, message: "you aren’t registered!" })
 				bcrypt.compare(password, user.password)
 					.then(res => {
-						if (!res) return cb(null, false, { status: "402", message: 'account or password invalid!' })
+						if (!res) return cb(null, false, { status: 402, message: 'account or password invalid!' })
 						return cb(null, user)
 					})
 			})
