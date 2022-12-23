8cb54395645ea469d34ddaecf44f0dd1eb7faaa4
seanlin1125 <sean821125@gmail.com>
2022-12-23 15:55:54 +0800

    refactor: modify user-controller to try-catch

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,262 +16,259 @@ const userController = {
         token,
         user: userData
       })
-    } catch (err) {
-      next(err)
+    } catch (error) {
+      next(error)
     }
   },
-  signUp: (req, res, next) => {
-    const { account, name, email, password, checkPassword } = req.body
-    if (!account?.trim() || !name?.trim() || !email?.trim() || !password?.trim() || !checkPassword?.trim()) throw new Error('所有欄位皆為必填!')
-    if (password !== checkPassword) throw new Error('密碼與確認密碼不相符!')
-    if (name?.length > 50) throw new Error('暱稱 name 上限 50 字!')
-    return Promise.all([
-      User.findOne({ where: { account }, raw: true }),
-      User.findOne({ where: { email }, raw: true })
-    ])
-      .then(([userFoundByAccount, userFoundByEmail]) => {
-        if (userFoundByAccount) throw new Error('account 已重複註冊!')
-        if (userFoundByEmail) throw new Error('email 已重複註冊!')
-        return bcrypt.hash(password, 10)
+  signUp: async (req, res, next) => {
+    try {
+      const { account, name, email, password, checkPassword } = req.body
+      if (!account?.trim() || !name?.trim() || !email?.trim() || !password?.trim() || !checkPassword?.trim()) throw new Error('所有欄位皆為必填!')
+      if (password !== checkPassword) throw new Error('密碼與確認密碼不相符!')
+      if (name?.length > 50) throw new Error('暱稱 name 上限 50 字!')
+      const [userFoundByAccount, userFoundByEmail] = await Promise.all([
+        User.findOne({ where: { account }, raw: true }),
+        User.findOne({ where: { email }, raw: true })
+      ])
+      if (userFoundByAccount) throw new Error('account 已重複註冊!')
+      if (userFoundByEmail) throw new Error('email 已重複註冊!')
+      const hashedPassword = await bcrypt.hash(password, 10)
+      const user = await User.create({
+        account,
+        name,
+        email,
+        password: hashedPassword
       })
-      .then(hash => {
-        return User.create({
-          account,
-          name,
-          email,
-          password: hash
-        })
-      })
-      .then(newUser => {
-        const userData = newUser.toJSON()
-        delete userData.password
-        res.json({ status: 'success', message: '帳號已成功註冊!', newUser: userData })
-      })
-      .catch(err => next(err))
+      const newUser = user.toJSON()
+      delete newUser.password
+      res.status(200).json({ status: 'success', message: '帳號已成功註冊!', newUser })
+    } catch (error) {
+      next(error)
+    }
   },
-  getCurrentUser: (req, res, next) => {
-    const currentUser = helpers.getUser(req)
-    return User.findByPk(currentUser.id, {
-      attributes: {
-        exclude: ['password', 'role', 'createdAt', 'updatedAt'],
-        include: [
-          [sequelize.literal('(SELECT COUNT(*) FROM Tweets WHERE Tweets.UserId = User.id )'), 'tweetCount'],
-          [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id )'), 'followerCount'],
-          [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followerId = User.id )'), 'followingCount']
-        ]
-      },
-      raw: true
-    })
-      .then(user => {
-        if (!user) throw new Error('使用者不存在!')
-        return res.json(user)
-      })
-      .catch(err => next(err))
-  },
-  getUser: (req, res, next) => {
-    const currentUser = helpers.getUser(req)
-    const paramsId = Number(req.params.id)
-    return User.findByPk(paramsId, {
-      attributes: {
-        exclude: ['password', 'role', 'createdAt', 'updatedAt'],
-        include: [
-          [sequelize.literal('(SELECT COUNT(*) FROM Tweets WHERE Tweets.UserId = User.id )'), 'tweetCount'],
-          [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id )'), 'followerCount'],
-          [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followerId = User.id )'), 'followingCount'],
-          [sequelize.literal(`EXISTS (SELECT id FROM Followships WHERE Followships.followerId = ${currentUser.id} AND Followships.followingId = User.id )`), 'isFollowed']
-        ]
-      },
-      raw: true
-    })
-      .then(user => {
-        if (!user) throw new Error('使用者不存在!')
-        return res.json(user)
-      })
-      .catch(err => next(err))
-  },
-  getUserTweets: (req, res, next) => {
-    const currentUser = helpers.getUser(req)
-    return Tweet.findAll({
-      where: { UserId: req.params.id },
-      include: [
-        {
-          model: User,
-          attributes: ['id', 'account', 'name']
-        }],
-      attributes: {
-        exclude: ['updatedAt'],
-        include: [
-          'id', 'UserId', 'description', 'createdAt',
-          [sequelize.literal('(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id )'), 'replyCount'],
-          [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id )'), 'likeCount'],
-          [sequelize.literal(`EXISTS (SELECT id FROM Likes WHERE Likes.UserId = ${currentUser.id} AND Likes.TweetId = Tweet.id )`), 'isLiked']
-        ]
-      },
-      order: [['createdAt', 'DESC']],
-      raw: true,
-      nest: true
-    })
-      .then(tweets => {
-        return tweets
-          .map(tweet => ({
-            ...tweet,
-            relativeTime: dateFormat(tweet.createdAt).fromNow()
-          }))
-      })
-      .then(tweets => {
-        if (!tweets) throw new Error('推文不存在!')
-        return res.json(tweets)
-      })
-      .catch(err => next(err))
-  },
-  getUserReplies: (req, res, next) => {
-    return Reply.findAll({
-      where: { UserId: req.params.id },
-      attributes: { exclude: ['updatedAt'] },
-      include: [
-        {
-          model: User,
-          attributes: ['id', 'account', 'name']
+  getCurrentUser: async (req, res, next) => {
+    try {
+      const currentUser = helpers.getUser(req)
+      const user = await User.findByPk(currentUser.id, {
+        attributes: {
+          exclude: ['password', 'role', 'createdAt', 'updatedAt'],
+          include: [
+            [sequelize.literal('(SELECT COUNT(*) FROM Tweets WHERE Tweets.UserId = User.id )'), 'tweetCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id )'), 'followerCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followerId = User.id )'), 'followingCount']
+          ]
         },
-        {
-          model: Tweet,
-          attributes: ['UserId'],
-          include: {
-            model: User,
-            attributes: ['account']
-          }
-        }
-      ],
-      order: [['createdAt', 'DESC']],
-      raw: true,
-      nest: true
-    })
-      .then(replies => {
-        return replies
-          .map(reply => ({
-            ...reply,
-            relativeTime: dateFormat(reply.createdAt).fromNow()
-          }))
+        raw: true
       })
-      .then(replies => {
-        if (!replies) throw new Error('留言回覆不存在!')
-        return res.json(replies)
-      })
-      .catch(err => next(err))
+      if (!user) throw new Error('使用者不存在!')
+      res.status(200).json(user)
+    } catch (error) {
+      next(error)
+    }
   },
-  getUserLikedTweets: (req, res, next) => {
-    const currentUser = helpers.getUser(req)
-    return Like.findAll({
-      where: { UserId: req.params.id },
-      attributes: { exclude: ['updatedAt'] },
-      include: [
-        {
-          model: Tweet,
-          attributes: [
-            'id', 'UserId', 'description',
+  getUser: async (req, res, next) => {
+    try {
+      const currentUser = helpers.getUser(req)
+      const paramsId = Number(req.params.id)
+      const user = await User.findByPk(paramsId, {
+        attributes: {
+          exclude: ['password', 'role', 'createdAt', 'updatedAt'],
+          include: [
+            [sequelize.literal('(SELECT COUNT(*) FROM Tweets WHERE Tweets.UserId = User.id )'), 'tweetCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id )'), 'followerCount'],
+            [sequelize.literal('(SELECT COUNT(*) FROM Followships WHERE Followships.followerId = User.id )'), 'followingCount'],
+            [sequelize.literal(`EXISTS (SELECT id FROM Followships WHERE Followships.followerId = ${currentUser.id} AND Followships.followingId = User.id )`), 'isFollowed']
+          ]
+        },
+        raw: true
+      })
+      if (!user) throw new Error('使用者不存在!')
+      res.status(200).json(user)
+    } catch (error) {
+      next(error)
+    }
+  },
+  getUserTweets: async (req, res, next) => {
+    try {
+      const currentUser = helpers.getUser(req)
+      const tweets = await Tweet.findAll({
+        where: { UserId: req.params.id },
+        include: [
+          {
+            model: User,
+            attributes: ['id', 'account', 'name']
+          }],
+        attributes: {
+          exclude: ['updatedAt'],
+          include: [
+            'id', 'UserId', 'description', 'createdAt',
             [sequelize.literal('(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id )'), 'replyCount'],
             [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id )'), 'likeCount'],
             [sequelize.literal(`EXISTS (SELECT id FROM Likes WHERE Likes.UserId = ${currentUser.id} AND Likes.TweetId = Tweet.id )`), 'isLiked']
-          ],
-          include: {
+          ]
+        },
+        order: [['createdAt', 'DESC']],
+        raw: true,
+        nest: true
+      })
+      if (!tweets) throw new Error('推文不存在!')
+      const newTweets = tweets
+        .map(tweet => ({
+          ...tweet,
+          relativeTime: dateFormat(tweet.createdAt).fromNow()
+        }))
+      res.status(200).json(newTweets)
+    } catch (error) {
+      next(error)
+    }
+  },
+  getUserReplies: async (req, res, next) => {
+    try {
+      const replies = await Reply.findAll({
+        where: { UserId: req.params.id },
+        attributes: { exclude: ['updatedAt'] },
+        include: [
+          {
             model: User,
             attributes: ['id', 'account', 'name']
+          },
+          {
+            model: Tweet,
+            attributes: ['UserId'],
+            include: {
+              model: User,
+              attributes: ['account']
+            }
           }
-        }
-      ],
-      order: [['createdAt', 'DESC']],
-      raw: true,
-      nest: true
-    })
-      .then(likes => {
-        return likes
-          .map(like => ({
-            ...like,
-            relativeTime: dateFormat(like.createdAt).fromNow()
-          }))
+        ],
+        order: [['createdAt', 'DESC']],
+        raw: true,
+        nest: true
       })
-      .then(likes => {
-        if (!likes) throw new Error('按讚不存在!')
-        return res.json(likes)
-      })
-      .catch(err => next(err))
+      if (!replies) throw new Error('留言回覆不存在!')
+      const newReplies = replies
+        .map(reply => ({
+          ...reply,
+          relativeTime: dateFormat(reply.createdAt).fromNow()
+        }))
+      res.status(200).json(newReplies)
+    } catch (error) {
+      next(error)
+    }
   },
-  getUserFollowings: (req, res, next) => {
-    const currentUser = helpers.getUser(req)
-    return Followship.findAll({
-      where: { followerId: req.params.id },
-      attributes: { exclude: ['updatedAt'] },
-      include: {
-        model: User,
-        as: 'Followings',
-        attributes: {
-          exclude: ['password'],
-          include: [
-            [sequelize.literal(`EXISTS (SELECT id FROM Followships WHERE Followships.followerId = ${currentUser.id} AND Followships.followingId = Followings.id )`), 'isFollowed']
-          ]
-        }
-      },
-      order: [['createdAt', 'DESC']],
-      raw: true,
-      nest: true
-    })
-      .then(followships => {
-        if (!followships) throw new Error('追蹤關係不存在!')
-        return res.json(followships)
+  getUserLikedTweets: async (req, res, next) => {
+    try {
+      const currentUser = helpers.getUser(req)
+      const likes = await Like.findAll({
+        where: { UserId: req.params.id },
+        attributes: { exclude: ['updatedAt'] },
+        include: [
+          {
+            model: Tweet,
+            attributes: [
+              'id', 'UserId', 'description',
+              [sequelize.literal('(SELECT COUNT(*) FROM Replies WHERE Replies.TweetId = Tweet.id )'), 'replyCount'],
+              [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId = Tweet.id )'), 'likeCount'],
+              [sequelize.literal(`EXISTS (SELECT id FROM Likes WHERE Likes.UserId = ${currentUser.id} AND Likes.TweetId = Tweet.id )`), 'isLiked']
+            ],
+            include: {
+              model: User,
+              attributes: ['id', 'account', 'name']
+            }
+          }
+        ],
+        order: [['createdAt', 'DESC']],
+        raw: true,
+        nest: true
       })
-      .catch(err => next(err))
+      if (!likes) throw new Error('按讚不存在!')
+      const newLikes = likes
+        .map(like => ({
+          ...like,
+          relativeTime: dateFormat(like.createdAt).fromNow()
+        }))
+      res.status(200).json(newLikes)
+    } catch (error) {
+      next(error)
+    }
   },
-  getUserFollowers: (req, res, next) => {
-    const currentUser = helpers.getUser(req)
-    return Followship.findAll({
-      where: { followingId: req.params.id },
-      attributes: { exclude: ['createdAt', 'updatedAt'] },
-      include: {
-        model: User,
-        as: 'Followers',
-        attributes: {
-          exclude: ['password'],
-          include: [
-            [sequelize.literal(`EXISTS (SELECT id FROM Followships WHERE Followships.followerId = ${currentUser.id} AND Followships.followingId = Followers.id )`), 'isFollowed']
-          ]
-        }
-      },
-      order: [['createdAt', 'DESC']],
-      raw: true,
-      nest: true
-    })
-      .then(followships => {
-        if (!followships) throw new Error('追蹤關係不存在!')
-        return res.json(followships)
+  getUserFollowings: async (req, res, next) => {
+    try {
+      const currentUser = helpers.getUser(req)
+      const followships = await Followship.findAll({
+        where: { followerId: req.params.id },
+        attributes: { exclude: ['updatedAt'] },
+        include: {
+          model: User,
+          as: 'Followings',
+          attributes: {
+            exclude: ['password'],
+            include: [
+              [sequelize.literal(`EXISTS (SELECT id FROM Followships WHERE Followships.followerId = ${currentUser.id} AND Followships.followingId = Followings.id )`), 'isFollowed']
+            ]
+          }
+        },
+        order: [['createdAt', 'DESC']],
+        raw: true,
+        nest: true
       })
-      .catch(err => next(err))
+      if (!followships) throw new Error('追蹤關係不存在!')
+      res.status(200).json(followships)
+    } catch (error) {
+      next(error)
+    }
   },
-  putUserProfile: (req, res, next) => {
-    const { name, introduction } = req.body
-    const id = Number(req.params.id)
-    if (name?.length > 50) throw new Error('暱稱 name 上限 50 字!')
-    if (introduction?.length > 160) throw new Error('自我介紹 introduction 上限 160 字!')
-    const { files } = req
-    return Promise.all([
-      User.findByPk(id),
-      imgurUploadImageHandler(files?.avatar ? files.avatar[0] : null),
-      imgurUploadImageHandler(files?.cover ? files.cover[0] : null)
-    ])
-      .then(([user, avatarFilePath, coverFilePath]) => {
-        if (!user) throw new Error('使用者不存在!')
-        return user.update({
-          name,
-          introduction,
-          avatar: avatarFilePath || user.avatar,
-          cover: coverFilePath || user.cover
-        })
+  getUserFollowers: async (req, res, next) => {
+    try {
+      const currentUser = helpers.getUser(req)
+      const followships = await Followship.findAll({
+        where: { followingId: req.params.id },
+        attributes: { exclude: ['createdAt', 'updatedAt'] },
+        include: {
+          model: User,
+          as: 'Followers',
+          attributes: {
+            exclude: ['password'],
+            include: [
+              [sequelize.literal(`EXISTS (SELECT id FROM Followships WHERE Followships.followerId = ${currentUser.id} AND Followships.followingId = Followers.id )`), 'isFollowed']
+            ]
+          }
+        },
+        order: [['createdAt', 'DESC']],
+        raw: true,
+        nest: true
       })
-      .then(updatedUser => {
-        const userData = updatedUser.toJSON()
-        delete userData.password
-        res.json(userData)
+      if (!followships) throw new Error('追蹤關係不存在!')
+      res.status(200).json(followships)
+    } catch (error) {
+      next(error)
+    }
+  },
+  putUserProfile: async (req, res, next) => {
+    try {
+      const { name, introduction } = req.body
+      const paramsId = Number(req.params.id)
+      if (name?.length > 50) throw new Error('暱稱 name 上限 50 字!')
+      if (introduction?.length > 160) throw new Error('自我介紹 introduction 上限 160 字!')
+      const { files } = req
+      const [user, avatarFilePath, coverFilePath] = await Promise.all([
+        User.findByPk(paramsId),
+        imgurUploadImageHandler(files?.avatar ? files.avatar[0] : null),
+        imgurUploadImageHandler(files?.cover ? files.cover[0] : null)
+      ])
+      if (!user) throw new Error('使用者不存在!')
+      const updatedUser = await user.update({
+        name,
+        introduction,
+        avatar: avatarFilePath || user.avatar,
+        cover: coverFilePath || user.cover
       })
-      .catch(err => next(err))
+      const updatedUserData = updatedUser.toJSON()
+      delete updatedUserData.password
+      res.status(200).json(updatedUserData)
+    } catch (error) {
+      next(error)
+    }
   },
   putUserSetting: async (req, res, next) => {
     try {
@@ -295,28 +292,27 @@ const userController = {
         email,
         password: bcrypt.hashSync(password, 10)
       })
-      const userData = renewUser.toJSON()
-      delete userData.password
-      res.json({ status: 'success', message: '帳號內容已成功修改!', renewUser: userData })
+      const renewUserData = renewUser.toJSON()
+      delete renewUserData.password
+      res.json({ status: 'success', message: '帳號內容已成功修改!', renewUser: renewUserData })
     } catch (error) {
       next(error)
     }
   },
-  patchUserCover: (req, res, next) => {
-    const id = Number(req.params.id)
-    return User.findByPk(id)
-      .then(user => {
-        if (!user) throw new Error('使用者不存在!')
-        return user.update({
-          cover: 'https://i.imgur.com/dIsjVjn.jpeg' || user.cover
-        })
+  patchUserCover: async (req, res, next) => {
+    try {
+      const paramsId = Number(req.params.id)
+      const user = await User.findByPk(paramsId)
+      if (!user) throw new Error('使用者不存在!')
+      const updatedUser = await user.update({
+        cover: 'https://i.imgur.com/dIsjVjn.jpeg' || user.cover
       })
-      .then(updatedUser => {
-        const userData = updatedUser.toJSON()
-        delete userData.password
-        res.json(userData)
-      })
-      .catch(err => next(err))
+      const updatedUserData = updatedUser.toJSON()
+      delete updatedUserData.password
+      res.status(200).json(updatedUserData)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
