e5e78d696ccd58ad337e0b8de88b5e099af15392
weizi0328 <ankewang26@gmail.com>
2022-12-24 18:13:23 +0800

    fix(migration): update association options in replies

--- migrations/20190115071420-create-reply.js
+++ migrations/20190115071420-create-reply.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       user_id: {
         type: Sequelize.INTEGER,
-        reference: {
+        references: {
           model: 'Users',
           key: 'id'
         },
@@ -19,7 +19,7 @@ module.exports = {
       },
       tweet_id: {
         type: Sequelize.INTEGER,
-        reference: {
+        references: {
           model: 'Tweets',
           key: 'id'
         },
