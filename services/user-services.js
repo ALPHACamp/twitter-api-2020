4e82ef52fd48690c95f992725d21528d0983bcae
henrysmile2071 <henrychen.chc@gmail.com>
2022-12-26 01:59:57 +0800

    refactor: image upload trial

--- services/user-services.js
+++ services/user-services.js
@@ -89,12 +89,12 @@ const userServices = {
       .catch(err => cb(err))
   },
   editUser: (req, cb) => {
-    const { account, name, email, introduction, password, checkPassword, avatar, cover } = req.body
+    const { account, name, email, introduction, password, checkPassword } = req.body
     const { files } = req
     const avatarData = files?.avatar ? files.avatar[0] : null
     const coverData = files?.cover ? files.cover[0] : null
-    const avatarString = avatar
-    const coverString = cover
+    // const avatarString = avatar
+    // const coverString = cover
     const UserId = Number(req.params.userId)
     const { id, role } = helpers.getUser(req)
     if (role !== 'admin' && UserId !== id) throw new Error('不可更改其他使用者資料!') // add role !== 'admin' for development purposes
@@ -121,8 +121,8 @@ const userServices = {
           email,
           introduction,
           password: password ? bcrypt.hashSync(password, 10) : user.password,
-          avatar: avatar || avatarString,
-          cover: cover || coverString
+          avatar: avatar || user.avatar,
+          cover: cover || user.cover
         })
       })
       .then(user => {
