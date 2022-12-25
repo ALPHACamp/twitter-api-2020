cc2d67e10e55b908e8c2c68b0d3f550325ecbbac
Gincoolwant <soulbox790326@gmail.com>
2022-12-25 19:23:42 +0800

    fix: allow Null to user modal

--- migrations/20190115071421-create-user.js
+++ migrations/20190115071421-create-user.js
@@ -9,15 +9,12 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       account: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       email: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       password: {
-        allowNull: false,
         type: Sequelize.STRING
       },
       name: {
