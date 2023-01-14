52c387c6508baff1f84eac475b2e6f00d6b3cc21
seanlin1125 <sean821125@gmail.com>
2023-01-14 15:18:13 +0800

    feat: complete jenkins test

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const helpers = require('../_helpers')
 
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ status: 'error', message: '未經授權!!' })
+    if (err || !user) return res.status(401).json({ status: 'error', message: '未經授權!' })
     req.user = user
     next()
   })(req, res, next)
