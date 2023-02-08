68df5ef765ecc4ec39aef4eab4dc688f4bdcbe84
LiyLi1122 <teppei.252033@gmail.com>
2023-02-08 20:16:25 +0800

    feat: modify corsOption

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ const passport = require('./config/passport')
 const cors = require('cors')
 const corsOption = {
   origin: [
-    '*'
-    // 'https://m790101.github.io',
-    // 'http://localhost:3000',
-    // 'http://localhost:10000',
-    // 'http://localhost:8080',
-    // 'http://localhost:8000',
-    // 'http://192.168.68.101:8081'
+    // '*'
+    'https://m790101.github.io',
+    'http://localhost:3000',
+    'http://localhost:10000',
+    'http://localhost:8080',
+    'http://localhost:8000',
+    'http://192.168.68.101:8081'
   ],
   methods: 'GET,HEAD,PUT,POST,DELETE,OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization']
 }
 
-const app = express()
+const app = express(corsOption)
 const port = process.env.PORT || 3000
 require('./models')
 
