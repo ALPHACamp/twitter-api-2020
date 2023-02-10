be1e7d5312f81c3e908674d24d8ba4776d58e816
LiyLi1122 <teppei.252033@gmail.com>
2023-02-10 11:38:30 +0800

    fix: like&reply migration

--- migrations/20190115071333-create-like.js
+++ migrations/20190115071333-create-like.js
@@ -13,7 +13,9 @@ module.exports = {
         references: {
           model: 'Users',
           key: 'id'
-        }
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
       },
       Tweet_id: {
         type: Sequelize.INTEGER,
