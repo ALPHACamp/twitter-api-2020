b471ba5683ce960d7fcb4fe164259b3fed741af9
wujia777 <cwu005104@gmail.com>
2023-02-14 07:35:50 +0800

    feat: add cors header setting

--- app.js
+++ app.js
@@ -10,7 +10,13 @@ const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
-
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
