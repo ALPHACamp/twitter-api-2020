46c9da95f7a2c5cbac4f9deb997abc638b068cce
sd880428 <sd660428@gmail.com>
2023-03-17 00:34:53 +0800

    feat: 關聯like,reply,tweet資料庫

--- models/like.js
+++ models/like.js
@@ -1,8 +1,40 @@
-'use strict';
+'use strict'
+
 module.exports = (sequelize, DataTypes) => {
   const Like = sequelize.define('Like', {
-  }, {});
-  Like.associate = function(models) {
-  };
-  return Like;
-};
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    UserId: {
+      type: DataTypes.INTEGER
+    },
+    TweetId: {
+      type: DataTypes.INTEGER
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE
+    }
+  }, {})
+  Like.associate = function (models) {
+    Like.belongsTo(models.User, { foreignKey: 'userId' })
+    Like.belongsTo(models.Tweet, { foreignKey: 'tweetId' })
+  }
+  Like.init({
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Like',
+    tableName: 'Likes',
+    underscored: true
+  })
+  return Like
+}
--- models/user.js
+++ models/user.js
@@ -1,8 +1,11 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
-  };
-  return User;
-};
+  }, {})
+  User.associate = function (models) {
+    User.hasMany(models.Like, { foreignKey: 'userId' })
+    User.hasMany(models.Tweet, { foreignKey: 'userId' })
+    User.hasMany(models.Reply, { foreignKey: 'userId' })
+  }
+  return User
+}
