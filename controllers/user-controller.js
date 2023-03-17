9f1b80acd98bc82a2b4a92b6bda59c8bd506d24e
FrankHuang <frankcool321@gmail.com>
2023-03-17 23:42:50 +0800

    feat: add error-handler

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,11 +4,25 @@ const userController = {
   signUp: async (req, res, next) => {
     const { name, account, email, password } = req.body;
     try {
-      // if (!name || !account || !email || !password) {
-      //   const error = new Error('欄位不可空白!')
-      //   error.status = 400
-      //   throw error
-      // }
+      if (!name || !account || !email || !password) {
+        const error = new Error("欄位不可空白!");
+        error.status = 400;
+        throw error;
+      }
+      const [isEmailExist, isAccountExist] = await Promise.all([
+        User.findOne({ where: { email } }),
+        User.findOne({ where: { account } }),
+      ]);
+      if (isEmailExist) {
+        const error = new Error("email 已重複註冊！");
+        error.status = 400;
+        throw error;
+      }
+      if (isAccountExist) {
+        const error = new Error("email 已重複註冊！");
+        error.status = 400;
+        throw error;
+      }
       const newUser = await User.create({
         name,
         account,
