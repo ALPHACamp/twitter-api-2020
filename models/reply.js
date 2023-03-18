5d2c2db0ec451200e209e58bbf51cdcf694ad0ff
LAPTOP-QJBRI58J\Anne <koala1995610@gmail.com>
2023-03-18 17:52:05 +0800

    feat: eslint init

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,8 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
   const Reply = sequelize.define('Reply', {
-  }, {});
-  Reply.associate = function(models) {
-  };
-  return Reply;
-};
+  }, {})
+  Reply.associate = function (models) {
+  }
+  return Reply
+}
