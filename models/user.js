e367c2832f11738af5e92e0b76032eac484dad78
wenliangsu <wenliangsu65018@gmail.com>
2023-03-20 02:29:02 +0800

    Feat: finish the User model and both User and Followship models pass the testing

--- models/user.js
+++ models/user.js
@@ -2,7 +2,31 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    static associate(models) {}
+    static associate(models) {
+      User.hasMany(models.Reply, {
+        foreignKey: 'UserId',
+      });
+      User.hasMany(models.Tweet, {
+        foreignKey: 'UserId',
+      });
+      User.hasMany(models.Like, {
+        foreignKey: 'UserId',
+      });
+
+      // Who follows this user
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings',
+      });
+
+      // Ths user is following another user
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers',
+      });
+    }
   }
 
   User.init(
