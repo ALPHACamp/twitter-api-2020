f05cf6d066cf285f30ef255bde3e3dd08e60be57
ray199229 <ray199229@gmail.com>
2023-03-19 18:28:59 +0800

    feat: adjust models/migations to pass the test and add eslint in the project

--- migrations/20190115071421-create-user.js
+++ migrations/20190115071421-create-user.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
@@ -26,17 +26,20 @@ module.exports = {
       role: {
         type: Sequelize.STRING
       },
-      createdAt: {
+      cover: {
+        type: Sequelize.STRING
+      },
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    })
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable('Users')
   }
-};
+}
--- models/user.js
+++ models/user.js
@@ -1,8 +1,44 @@
-'use strict';
+'use strict'
+const {
+  Model
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+      User.hasMany(models.Reply, { foreignKey: 'UserId' })
+      User.hasMany(models.Tweet, { foreignKey: 'UserId' })
+      User.hasMany(models.Like, { foreignKey: 'UserId' })
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
+    }
   };
-  return User;
-};
+  User.init({
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING,
+    cover: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true
+  })
+  return User
+}
