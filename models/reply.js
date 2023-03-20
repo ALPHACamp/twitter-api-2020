404221c35816bc1f740576113789e96aa9328bd0
yy933 <emilyyu025@gmail.com>
2023-03-20 14:25:20 +0800

    feat: update reply model

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,26 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {
-  }, {})
+  const Reply = sequelize.define(
+    "Reply",
+    {
+      UserId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      TweetId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      comment: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+    },
+    { sequelize, modelName: "Tweet", tableName: "Tweets", underscored: true }
+  );
   Reply.associate = function (models) {
+    Reply.belongsTo(models.User)
+    Reply.belongsTo(models.Tweet)
   }
   return Reply
 }
