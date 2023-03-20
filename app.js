660e8eec58f306927b0e8ee74340fc80d7b05bc1
abbie930 <abbiecheng930@gmail.com>
2023-03-20 19:00:59 +0800

    feat: add jwt module

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const helpers = require('./_helpers')
+const passport = require('./config/passport')
 
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated (req, res, next) {
+function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
-};
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+
+    next()
+  })(req, res, next)
+}
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,21 @@
 const passport = require('passport')
+const passportJWT = require('passport-jwt')
+
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+
+const { User } = require('../models')
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET,
+}
+passport.use(
+  new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+    User.findByPk(jwtPayload.id)
+      .then((user) => cb(null, user))
+      .catch((err) => cb(err))
+  })
+)
 
 module.exports = passport
