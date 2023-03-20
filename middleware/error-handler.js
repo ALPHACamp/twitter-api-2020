801b4facf769725415d089c7622dd60f0bc0cd36
Ellen <ellen31725@gmail.com>
2023-03-21 02:26:58 +0800

    chore: delete console.log

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,5 @@
 module.exports = {
   errorHandler (err, req, res, next) {
-    console.table({ err: JSON.stringify(err) })
     if (err instanceof Error) {
       res.status(err.status || 500).json({
         status: 'error',
