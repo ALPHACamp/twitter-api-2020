49b05e1ad53635ddaafd224d40787a72e22be37d
hl94vul3h6 <gary6780gary6780@gmail.com>
2023-03-20 02:34:01 -0700

    feat: fix the bug of running npm run test

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const helpers = require('./_helpers')
 
@@ -7,7 +10,7 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 function authenticated (req, res, next) {
   // passport.authenticate('jwt', { ses...
-};
+}
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
