d9b7fe210f817ab6502a13f7abecd0178e542d83
ray199229 <ray199229@gmail.com>
2023-03-20 15:26:55 +0800

    feat: add error-handler.js

---
+++ routes/apis/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+const { apiErrorHandler } = require('../../middleware/error-handler')
+
+router.use('/', apiErrorHandler)
+
+module.exports = router
