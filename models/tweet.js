70d077146a09474392d2d6ddcb8fdfa51bda0032
Chloe905 <cchloehsieh@gmail.com>
2023-03-21 17:52:11 +0800

    feat:modify tweet model

--- models/tweet.js
+++ models/tweet.js
@@ -1,8 +1,23 @@
 'use strict'
+const {
+  Model
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
-  }, {})
-  Tweet.associate = function (models) {
+  class Tweet extends Model {
+    static associate(models) {
+      Tweet.hasMany(models.Reply)
+      Tweet.hasMany(models.Like)
+      Tweet.belongsTo(models.User, { foreignKey: 'UserId' })
+    }
   }
+  Tweet.init({
+    description: DataTypes.TEXT,
+    UserId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Tweet',
+    tableName: 'Tweets',
+    underscored: true
+  })
   return Tweet
 }
