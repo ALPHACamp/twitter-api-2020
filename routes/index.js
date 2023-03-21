0f22eb5f469e2f508b2001d418b5987e4756af6b
wego11ya <wego11ya@gmail.com>
2023-03-21 23:29:17 +0800

    feat: getTweets to be continued

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const userController = require('../controllers/user-controller')
+const tweetController = require('../controllers/tweet-controller')
 const { errorHandler } = require('../middleware/error-handler')
 
 router.post('/api/users/signup', userController.signUp)
-
+router.get('/api/tweets', tweetController.getTweets)
 router.use('/', errorHandler)
 
 module.exports = router
