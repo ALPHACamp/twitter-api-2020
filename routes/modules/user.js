fc2af083783268ca43e99fc07874cb322d0d9fb0
Ellen <ellen31725@gmail.com>
2023-03-21 23:29:39 +0800

    fix: remove unused file

--- routes/modules/user.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../../controllers/user-controller')
-
-router.get('/:id', userController.getUser)
-
-module.exports = router
