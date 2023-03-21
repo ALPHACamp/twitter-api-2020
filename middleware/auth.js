994743b399cfdc22d1175d5a5f50da51b788cb52
abbie930 <abbiecheng930@gmail.com>
2023-03-21 16:42:03 +0800

    feat: changing variable name

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 const passport = require('../config/passport')
-const helpers = require('../_helpers')
+const { getUser } = require('../_helpers')
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next) {
+const authenticated = (req, res, next) => {
   // passport.authenticate('jwt', { ses...
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
@@ -11,7 +11,7 @@ function authenticated(req, res, next) {
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if (helpers.getUser(req) && helpers.getUser(req).isAdmin) return next()
+  if (getUser(req) && getUser(req).isAdmin) return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
 
