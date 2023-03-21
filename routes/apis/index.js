b70ab74d4d9180f89cb1d9aec944aba64b02f0fa
ray199229 <ray199229@gmail.com>
2023-03-21 18:34:10 +0800

    feat: add signup signin features

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 
+const userController = require('../../controllers/apis/user-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
+// 還沒用到
+// const { authenticated, authenticatedAdmin } = require('../../middleware/auth')
 
+router.post('/users', userController.signUp)
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.use('/', apiErrorHandler)
 
 module.exports = router
