f44c3fe8c340b59bdef7157fe471bf6829d704f8
abbie930 <abbiecheng930@gmail.com>
2023-03-21 16:32:17 +0800

    feat: add passport LocalStrategy and revise JWTStrategy

--- config/passport.js
+++ config/passport.js
@@ -1,20 +1,53 @@
 const passport = require('passport')
+const LocalStrategy = require('passport-local')
 const passportJWT = require('passport-jwt')
+const bcrypt = require('bcryptjs')
 
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
 
 const { User } = require('../models')
 
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true,
+    },
+    async (req, email, password, cb) => {
+      try {
+        const user = await User.findOne({ where: { email } })
+        if (!user) return cb(null, false)
+
+        const isMatched = await bcrypt.compare(password, user.password)
+        if (!isMatched) return cb(null, false)
+
+        return cb(null, user)
+      } catch (err) {
+        return cb(err, null)
+      }
+    }
+  )
+)
+
 const jwtOptions = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET,
 }
 passport.use(
-  new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
-    User.findByPk(jwtPayload.id)
-      .then((user) => cb(null, user))
-      .catch((err) => cb(err))
+  new JWTStrategy(jwtOptions, async (jwtPayload, cb) => {
+    try {
+      const user = await User.findByPk(jwtPayload.id, {
+        include: [
+          { model: User, as: 'Followers' },
+          { model: User, as: 'Followings' },
+        ],
+      })
+      return cb(null, user)
+    } catch (err) {
+      return cb(err, null)
+    }
   })
 )
 
