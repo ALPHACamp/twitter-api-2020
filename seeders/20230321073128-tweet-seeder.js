5a6858b651d30a2a0851d2a6ac14e803147f69c2
Isis Lin <qweb32114@gmail.com>
2023-03-21 16:14:22 +0800

    feat: create seeder

---
+++ seeders/20230321073128-tweet-seeder.js
@@ -0,0 +1,26 @@
+const { faker } = require('@faker-js/faker');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    );
+    const tweetList = [];
+    users.forEach((user) => {
+      tweetList.push(
+        ...Array.from({ length: 10 }, () => ({
+          User_id: user.id,
+          description: faker.lorem.sentence(),
+          created_at: faker.date.recent(),
+          updated_at: new Date(),
+        }))
+      );
+    });
+    await queryInterface.bulkInsert('Tweets', tweetList, {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Tweets', null, {});
+  },
+};
