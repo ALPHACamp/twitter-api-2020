06936e673ee335bd0ab5530aa986368df46c9386
Ian_Lin <ian920511@gmail.com>
2023-03-21 22:24:10 +0800

    feat: repush R08

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const passport = require('../config/passport')
-
-const userController = require('../controllers/user-controller')
-
-const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
-const { apiErrorHandler } = require('../middleware/error-handler')
