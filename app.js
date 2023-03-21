d70c9b9eacc24819719afa4b574edb1ecc60b8f0
yichennn <yichennn1010@gmail.com>
2023-03-22 00:02:42 +0800

    heroku init (#4)

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const passport = require('./config/passport')
 const router = require('./routes')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
