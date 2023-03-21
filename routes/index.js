a0db0bb1c969bd16e039ed8e87105272e1329817
Ellen <ellen31725@gmail.com>
2023-03-21 22:38:03 +0800

    feat: add admin login, get admin users, get single user

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router = express.Router()
-// const admin = require('./modules/admin')
+const admin = require('./modules/admin')
+const user = require('./modules/user')
 const userController = require('../controllers/user-controller')
+const adminController = require('../controllers/admin-controller')
 const { errorHandler } = require('../middleware/error-handler')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
+// no need to authenticate
+router.post('/admin/login', adminController.login)
 router.post('/users/login', userController.login)
-
 router.post('/users', userController.signUp)
+
+router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.use('/users', authenticated, user)
 
 router.use('/', errorHandler)
 
