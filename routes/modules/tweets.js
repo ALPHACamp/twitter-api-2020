4b783f81f1c0872b2142cc5314ccde3460c00a9f
cayang <cayang@simenvi.com.tw>
2023-03-21 17:42:17 +0800

    feat: add get one tweet API

--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const tweetController = require('../../controllers/tweet-controller')
 
+router.get('/:id', tweetController.getTweet)
 router.get('/', tweetController.getTweets)
 
 module.exports = router
