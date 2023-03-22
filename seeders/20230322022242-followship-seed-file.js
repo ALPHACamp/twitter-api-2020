b3ba45be31581dc34cb7ac7398e592af742f0a9d
Chloe905 <cchloehsieh@gmail.com>
2023-03-22 16:13:49 +0800

    feat:modify seeder files

--- seeders/20230322022242-followship-seed-file.js
+++ seeders/20230322022242-followship-seed-file.js
@@ -5,12 +5,12 @@ module.exports = {
     const users = await queryInterface.sequelize.query("SELECT id FROM Users WHERE role = 'user';", { type: queryInterface.sequelize.QueryTypes.SELECT })
 
     await queryInterface.bulkInsert('Followships', Array.from({ length: 20 }, (_, index) => ({
-      createdAt: new Date(),
-      updatedAt: new Date(),
-			followingId: users[4 - index % 5].id,
-      followerId: users[index % 5].id,
+      created_at: new Date(),
+      updated_at: new Date(),
+      followingId: users[4 - index % 5].id,
+      followerId: users[index % 5].id
     })))
-		await queryInterface.sequelize.query("DELETE FROM followships WHERE followingId = followerId;", { type: queryInterface.sequelize.QueryTypes.DELETE })
+    await queryInterface.sequelize.query('DELETE FROM followships WHERE followingId = followerId;', { type: queryInterface.sequelize.QueryTypes.DELETE })
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('followships', null, {})
