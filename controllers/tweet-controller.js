c148770a200f20c387c9bec49b7fb2bce91050d6
cayang <cayang@simenvi.com.tw>
2023-03-22 13:32:31 +0800

    feat: add one tweet API

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,4 +1,5 @@
 const { Tweet, User, sequelize } = require('../models')
+const { getUser } = require('../_helpers')
 const tweetController = {
   getTweets: (req, res, next) => {
     return Tweet.findAll({
@@ -30,8 +31,49 @@ const tweetController = {
       },
       include: [{ model: User, attributes: ['id', 'name', 'account', 'avatar'] }]
     })
-      .then(tweet => res.status(200).json(tweet))
+      .then(tweet => {
+        if (!tweet) {
+          const error = new Error('此推文不存在！')
+          error.status = 404
+          throw error
+        }
+        return res.status(200).json(tweet)
+      })
       .catch(err => next(err))
+  },
+  postTweet: async (req, res, next) => {
+    try {
+      const { description } = req.body
+      if (!description) {
+        const error = new Error('內容不可空白！')
+        error.status = 400
+        throw error
+      }
+      if (description.length > 140) {
+        const error = new Error('內容上限 140 字！')
+        error.status = 400
+        throw error
+      }
+      const user = await User.findByPk(getUser(req).dataValues.id)
+      if (!user) throw new Error('使用者不存在!')
+      const tweet = await Tweet.create({
+        UserId: getUser(req).dataValues.id,
+        description
+      })
+
+      const data = {
+        ...tweet.dataValues,
+        user: user.dataValues
+      }
+      Object.keys(data.user).forEach(e => {
+        if (!['id', 'name', 'account', 'avatar'].includes(e)) return delete data.user[e]
+      })
+      delete data.UserId
+
+      return res.status(200).json(data)
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = tweetController
--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -4,5 +4,6 @@ const tweetController = require('../../controllers/tweet-controller')
 
 router.get('/:id', tweetController.getTweet)
 router.get('/', tweetController.getTweets)
+router.post('/', tweetController.postTweet)
 
 module.exports = router
