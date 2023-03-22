5a6fa15353ea14bfa83d19e7879be46dd78e36a1
peggy8865 <peggy8865@gmail.com>
2023-03-22 16:23:52 +0800

    feat: add post tweets function

---
+++ controllers/tweet-controller.js
@@ -0,0 +1,19 @@
+const helper = require('../_helpers')
+const { Tweet, User, Reply, Like } = require('../models')
+
+const tweetController = {
+  postTweet: async (req, res, next) => {
+    try {
+      const { description } = req.body
+      if (!description) throw new Error('推文不可空白')
+      if (description.length > 140) throw new Error('字數限制 140 字')
+      const UserId = helper.getUser(req).id
+      const newTweet = await Tweet.create({ description, UserId })
+      res.status(200).json(newTweet)
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = tweetController
