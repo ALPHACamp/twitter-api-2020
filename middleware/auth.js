0265eee319fd3fcc6448c4017b3625ec2339d584
Chloe905 <cchloehsieh@gmail.com>
2023-03-23 01:05:41 +0800

    feat:add admin getUser function

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const authenticated = (req, res, next) => {
   // passport.authenticate('jwt', { ses...
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) return res.status(401).json({ status: 'error', message: '尚未登入' })
-    helpers.getUser(req) = user
+    req.user = user
     next()
   })(req, res, next)
 }
@@ -13,7 +13,6 @@ const authenticatedAdmin = (req, res, next) => {
   if (helpers.getUser(req) && helpers.getUser(req).role === 'admin') return next()
   return res.status(403).json({ status: 'error', message: '權限不足' })
 }
-
 const authenticatedUser = (req, res, next) => {
   if (helpers.getUser(req) && helpers.getUser(req).role === 'user') return next()
   return res.status(403).json({ status: 'error', message: '帳號不存在' })
