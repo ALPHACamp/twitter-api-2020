272de26233969190d0f77ee43a31b293a129960d
Noelle-KH <noelletang86@gmail.com>
2023-03-23 07:57:50 +0800

    feat: add like and followship seeder

---
+++ seeders/20230322160116-followship-seed-file.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const DEFAULT_NUMBER = 15
+    const users = await queryInterface.sequelize.query(
+      `SELECT DISTINCT u1.id AS follower_id, u2.id AS following_id
+      FROM Users u1, Users u2
+      WHERE u1.role='user' AND u2.role='user' AND u1.id <> u2.id
+      ORDER BY RAND()
+      limit ${DEFAULT_NUMBER}`, { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+
+    await queryInterface.bulkInsert('Followships',
+      users.map(user => ({
+        follower_id: user.follower_id,
+        following_id: user.following_id,
+        created_at: new Date(),
+        updated_at: new Date()
+      })), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Followships', {})
+  }
+}
