e12369423dcc048f2681c89f8ed9019f59abd5a5
LinTzuAn <a22341254@gmail.com>
2023-03-22 13:05:46 +0800

    feat: add tweet controller

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 const userController = require('../controllers/user-controller')
+const tweetController = require('../controllers/tweet-controller')
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
 const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
 const { apiErrorHandler } = require('../middleware/error-handler')
 
-router.use('/api/admin', admin)
+router.use('/api/admin', authenticated, authenticatedAdmin, admin)
 // router.get('/api/admin/restaurants', authenticated, apiErrorHandler)
 
 router.post('/api/users', userController.signUp) // 註冊帳號路由
 // (下1) session: false 的功能，把 cookie/session 功能關掉，不管理它
 router.post('/api/signin', passport.authenticate('local', { session: false }), userController.signIn) // 注意是 post
+
+router.get('/api/tweets/:id', authenticated, tweetController.getTweet)
+router.get('/api/tweets', authenticated, tweetController.getTweets)
+router.post('/api/tweets', authenticated, tweetController.postTweet)
 
 router.use('/', apiErrorHandler)
 
