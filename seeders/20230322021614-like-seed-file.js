55f9e95544f885c7a452f94282e932adb3cb4ac1
Johnny Chiu <90211190+dinsky21@users.noreply.github.com>
2023-03-22 11:33:45 +0800

    add followship and like seeder

---
+++ seeders/20230322021614-like-seed-file.js
@@ -0,0 +1,26 @@
+'use strict'
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query("SELECT id FROM Users WHERE role = 'user';", { type: queryInterface.sequelize.QueryTypes.SELECT })
+		const tweets = await queryInterface.sequelize.query("SELECT id FROM Tweets;", { type: queryInterface.sequelize.QueryTypes.SELECT })
+
+    await queryInterface.bulkInsert('Likes', Array.from({ length: 250 }, (_, index) => ({
+      createdAt: new Date(),
+      updatedAt: new Date(),
+			TweetId: tweets[index % 50].id,
+      UserId: users[index % 5].id,
+    })))
+		// await queryInterface.sequelize.query(
+		// 	`DELETE likes FROM likes
+		// 	 INNER JOIN tweets ON likes.tweetId = tweets.id
+		// 	 WHERE likes.UserId = tweets.UserId
+		// 	 AND likes.UserId <> tweets.UserId`,
+		// 	{ type: Sequelize.QueryTypes.DELETE }
+		// );
+  },
+// !這邊還沒有解決不能Like自己貼文的問題
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Likes', null, {})
+  }
+}
---
+++ seeders/20230322022242-followship-seed-file.js
@@ -0,0 +1,18 @@
+'use strict'
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query("SELECT id FROM Users WHERE role = 'user';", { type: queryInterface.sequelize.QueryTypes.SELECT })
+
+    await queryInterface.bulkInsert('Followships', Array.from({ length: 20 }, (_, index) => ({
+      createdAt: new Date(),
+      updatedAt: new Date(),
+			followingId: users[4 - index % 5].id,
+      followerId: users[index % 5].id,
+    })))
+		await queryInterface.sequelize.query("DELETE FROM followships WHERE followingId = followerId;", { type: queryInterface.sequelize.QueryTypes.DELETE })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('followships', null, {})
+  }
+}
