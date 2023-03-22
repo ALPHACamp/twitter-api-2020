b73d77503b014574cdf6553c945db325d2347730
Noelle-KH <noelletang86@gmail.com>
2023-03-22 14:19:03 +0800

    fix: seed random user avatar

--- seeders/20230320231223-user-seed-file.js
+++ seeders/20230320231223-user-seed-file.js
@@ -10,7 +10,7 @@ module.exports = {
       account: 'root',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
       role: 'admin',
-      avatar: `https://loremflickr.com/320/240/man,woman/?random=${Math.floor(Math.random() * 50)}`,
+      avatar: `https://loremflickr.com/320/240/man,woman/?lock=${Math.floor(Math.random() * 50)}`,
       introduction: faker.lorem.sentence(5),
       created_at: new Date(),
       updated_at: new Date()
@@ -18,7 +18,7 @@ module.exports = {
     ...Array.from({ length: 5 }).map((_, i) => ({
       email: `user${i + 1}@example.com`,
       name: `user${i + 1}`,
-      avatar: `https://loremflickr.com/320/240/man,woman/?random=${Math.floor(Math.random() * 50)}`,
+      avatar: `https://loremflickr.com/320/240/man,woman/?lock=${Math.floor(Math.random() * 50)}`,
       introduction: faker.lorem.sentence(5),
       account: `user${i + 1}`,
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10)),
