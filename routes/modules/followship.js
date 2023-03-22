d74a990502d2dcd6d4e5209d8320559d05ba6822
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-03-22 18:22:52 +0800

    feat: add getTop10 route

--- routes/modules/followship.js
+++ routes/modules/followship.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const followController = require('../../controllers/follow-controller')
 
-router.post('/', followController.postFollow)
 router.delete('/:id', followController.deleteFollow)
+router.get('/topFollow', followController.topFollow)
+router.post('/', followController.postFollow)
 
 module.exports = router
