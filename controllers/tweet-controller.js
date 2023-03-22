003b95486d41a4a4009b59f58ada7f20d8da9ba2
jjerry1225 <i183801225@gmail.com>
2023-03-22 14:43:23 +0800

    add post tweet and revise middleware auth.js

---
+++ controllers/tweet-controller.js
@@ -0,0 +1,29 @@
+const { Tweet, User } = require('../models')
+const helpers = require('../_helpers')
+
+const tweetController = {
+  postTweet: (req, res, next) => {
+    const { description } = req.body
+    const user = helpers.getUser(req)
+    const UserId = user.id
+
+    return User.findByPk(UserId)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+
+        return Tweet.create({
+          UserId,
+          description
+        })
+      })
+      .then(tweet => {
+        res.json({
+          status: 'success',
+          data: tweet
+        })
+      })
+      .catch(err => next(err))
+  }
+}
+
+module.exports = tweetController
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
+const tweetController = require('../controllers/tweet-controller')
 const { apiErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 
 router.post('/users/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.post('/users', userController.signUp)
+
+router.post('/tweets', authenticated, tweetController.postTweet)
 
 router.use('/', apiErrorHandler)
 
