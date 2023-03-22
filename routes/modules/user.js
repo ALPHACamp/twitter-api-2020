2638156dd11602695d9d8d2b4e31a9ae773dbdda
kai3kai2 <111210838+kai3kai2@users.noreply.github.com>
2023-03-22 17:35:44 +0800

    Update user.js

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -4,6 +4,8 @@ const userController = require('../../controllers/user-controller')
 const { authenticated, authenticatedUser } = require('../../middleware/auth')
 const upload = require('../../middleware/multer')
 
+router.get('/:id/followers', userController.getUsersFollowings)
+router.get('/:id/followings', userController.getUsersFollowings)
 router.get('/:id/replied_tweets', authenticated, authenticatedUser, userController.getRepliedTweets)
 router.get('/:id/likes', authenticated, authenticatedUser, userController.getLikeTweets)
 router.get('/:id/tweets', authenticated, authenticatedUser, userController.getUserTweets)
