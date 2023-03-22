2474d998a94d611b5e11da0f4c02d8227a212f88
Johnny Chiu <90211190+dinsky21@users.noreply.github.com>
2023-03-22 17:28:25 +0800

    update package-lock.json and seed file

--- seeders/20230322021614-like-seed-file.js
+++ seeders/20230322021614-like-seed-file.js
@@ -1,5 +1,4 @@
 'use strict'
-const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const users = await queryInterface.sequelize.query("SELECT id FROM Users WHERE role = 'user';", { type: queryInterface.sequelize.QueryTypes.SELECT })
