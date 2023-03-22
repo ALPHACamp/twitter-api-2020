4844af2c412c9840ff52bdf2bc766acea6f8b596
Chloe905 <cchloehsieh@gmail.com>
2023-03-22 12:15:13 +0800

    feat:modify authenticate file

--- app.js
+++ app.js
@@ -2,14 +2,11 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const helpers = require('./_helpers')
 const passport = require('./config/passport')
 const app = express()
 const port = 3000
 
-
 app.use(passport.initialize()) // 初始化 Passport
-
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
