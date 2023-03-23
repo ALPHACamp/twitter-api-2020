0aa838ea9b2dc3c64612655949fa6b7961ba338c
FrankHuang <frankcool321@gmail.com>
2023-03-23 19:21:20 +0800

    feat: add cors package

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
-
+const cors = require('cors')
 const passport = require('./config/passport')
 const helpers = require('./helpers/auth-helper')
 const routes = require('./routes/index')
@@ -12,17 +12,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-
-// use helpers.getUser(req) to replace req.user
-function authenticated (req, res, next) {
-  // passport.authenticate('jwt', { ses...
-};
-
-
 // use helpers.getUser(req) to replace req.user
 // function authenticated(req, res, next){
 //   // passport.authenticate('jwt', { ses...
 // };
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
