063d1fe90faed439c3db8c5b52b1f6ae21136331
ray199229 <ray199229@gmail.com>
2023-03-23 18:49:48 +0800

    fix: delete comment

--- services/tweet-services.js
+++ services/tweet-services.js
@@ -1,33 +1,32 @@
 // const assert = require('assert')
 const sequelize = require('sequelize')
 const helpers = require('../_helpers')
-const { User, Tweet, Reply, Like } = require('../models')
-const { database } = require('faker/lib/locales/en')
+const { User, Tweet } = require('../models')
 const { assert } = require('chai')
 
 const tweetServices = {
   getTweets: (req, cb) => {
     const userId = helpers.getUser(req).id
-      return Tweet.findAll({
-        attributes: [
+    return Tweet.findAll({
+      attributes: [
         'id', 'description', 'createdAt', 'updatedAt',
         [sequelize.literal('(SELECT COUNT(*) FROM Replies WHERE Tweet_id = Tweet.id)'), 'repliedCount'],
         [sequelize.literal('(SELECT COUNT(*) FROM Likes WHERE Tweet_id = Tweet.id)'), 'likedCount'],
         [sequelize.literal(`EXISTS (SELECT id FROM Likes WHERE Tweet_id = Tweet.id AND User_id = ${userId})`), 'isLiked']
       ],
-        include: [
-          { model: User, attributes: ['id', 'name', 'account', 'avatar'] }
-        ],
-        order: [['updatedAt', 'DESC']],
-        nest: true,
-        raw: true
-      })
+      include: [
+        { model: User, attributes: ['id', 'name', 'account', 'avatar'] }
+      ],
+      order: [['updatedAt', 'DESC']],
+      nest: true,
+      raw: true
+    })
       .then(tweetsData => {
         const tweets = tweetsData.map(t => ({
           ...t,
           isLiked: t.isLiked === 1
         }))
-        cb(null, tweets )
+        cb(null, tweets)
       })
       .catch(err => cb(err))
   },
@@ -52,34 +51,20 @@ const tweetServices = {
         }
         return cb(null, data)
       })
-        .catch(err => cb(err))
+      .catch(err => cb(err))
   },
-  // postTweet: (req, cb) => {
-  //   const { description } = req.body
-  //   const userId = helpers.getUser(req).id
-  //   return User.findByPk(userId)
-  //     .then(user => {
-  //       assert(user, '使用者不存在')
-  //       return Tweet.create({
-  //         UserId: userId,
-  //         description
-  //       })
-  //     })
-  //     .then(tweet => cb(null, tweet))
-  //     .catch(err => cb(err))
-  // }
   postTweet: (req, cb) => {
-    const description = req.body.description
-    const UserId = helpers.getUser(req).id
-    return User.findByPk(UserId)
+    const { description } = req.body
+    const userId = helpers.getUser(req).id
+    return User.findByPk(userId)
       .then(user => {
-        if (!user) throw new Error('使用者不存在!')
+        assert(user, '使用者不存在')
         return Tweet.create({
-          description,
-          UserId
+          UserId: userId,
+          description
         })
       })
-      .then(postedTweet => cb(null, postedTweet))
+      .then(tweet => cb(null, tweet))
       .catch(err => cb(err))
   }
 }
