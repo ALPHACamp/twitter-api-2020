005e324d8a0ddffaadb29790c05bec4a583e329a
Lilynews <lilinews2021@gmail.com>
2023-03-22 21:03:53 -0400

    feat: add GET & POST tweets, GET :tweet & replies

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 const passport = require('../config/passport')
 const errorHandler = require('../middleware/error-handler')
 const userController = require('../controllers/user-controller')
+const tweetController = require('../controllers/tweet-controller')
 const { authenticated, authenticatedUser, checkFieldNotEmpty } = require('../middleware/auth')
 const users = require('./modules/users')
+const tweets = require('./modules/tweets')
 
 // 使用者登入
 router.post('/users/login', checkFieldNotEmpty, passport.authenticate('local', { session: false }), userController.login)
@@ -13,8 +15,14 @@ router.post('/admin/login', checkFieldNotEmpty, passport.authenticate('local', {
 
 // 使用者註冊
 router.post('/users', userController.register)
+// 查看所有貼文
+router.get('/tweets', authenticated, authenticatedUser, tweetController.getTweets)
+// 新增一筆推文
+router.post('/tweets', authenticated, authenticatedUser, tweetController.postTweets)
 
 router.use('/users', authenticated, authenticatedUser, users)
+router.use('/tweets', authenticated, authenticatedUser, tweets)
+
 router.use('/', errorHandler)
 
 module.exports = router
