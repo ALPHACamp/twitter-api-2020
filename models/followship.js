234fdb2aa3631e3a1ec4c3fbaa143a358bbae466
wenliangsu <wenliangsu65018@gmail.com>
2023-03-24 02:45:06 +0800

    Fix: fix the relationship of model Followship

--- models/followship.js
+++ models/followship.js
@@ -2,7 +2,16 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Followship extends Model {
-    static associate(models) {}
+    static associate(models) {
+      Followship.belongsTo(models.User, {
+        foreignKey: 'followerId',
+        as: 'followerUser',
+      });
+      Followship.belongsTo(models.User, {
+        foreignKey: 'followingId',
+        as: 'followingUser',
+      });
+    }
   }
 
   Followship.init(
