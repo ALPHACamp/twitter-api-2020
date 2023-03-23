bae405ee0bafd025721dbd41a60544ed42ad0683
cayang <cayang@simenvi.com.tw>
2023-03-23 15:05:11 +0800

    feat: user delete one following API

--- routes/modules/followships.js
+++ routes/modules/followships.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const followshipController = require('../../controllers/followship-controller')
 
+router.delete('/:followingId', followshipController.deleteFollowing)
 router.post('/', followshipController.postFollowing)
 
 module.exports = router
