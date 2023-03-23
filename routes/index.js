c380c35af3442a39f8147255c2c4323cd440a437
wego11ya <wego11ya@gmail.com>
2023-03-23 12:05:38 +0800

    feat: add tweet-controller

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
-const router = require('express').Router()
-const userController = require('../controllers/user-controller')
-const tweetController = require('../controllers/tweet-controller')
-const { errorHandler } = require('../middleware/error-handler')
+const router = require("express").Router();
+const userController = require("../controllers/user-controller");
+const tweetController = require("../controllers/tweet-controller");
+const { errorHandler } = require("../middleware/error-handler");
+const { authenticatedUser } = require("../middleware/auth");
 
-router.post("/api/users", userController.signUp)
-router.post("/api/users/signin", userController.signIn)
-router.get('/api/tweets', tweetController.getTweets)
-router.use('/', errorHandler)
+router.post("/api/users", userController.signUp);
+router.post("/api/users/signin", userController.signIn);
+router.get("api/tweets/:tweet_id", authenticatedUser, tweetController.getTweet);
+router.get("/api/tweets", authenticatedUser, tweetController.getTweets);
+router.post("/api/tweets", authenticatedUser, tweetController.postTweet);
+router.use("/", errorHandler);
 
 module.exports = router;
