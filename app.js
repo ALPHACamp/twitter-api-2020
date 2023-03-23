e8893fcf9d7bbf96d9d7990d5b1418a7486f5054
jjerry1225 <i183801225@gmail.com>
2023-03-23 11:48:01 +0800

    modify port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express')
 const helpers = require('./_helpers')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const routes = require('./routes')
 
