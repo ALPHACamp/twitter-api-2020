c9921d8eccab7ac4ad73427ddfa9347194a35b19
yy933 <emilyyu025@gmail.com>
2023-03-23 14:26:35 +0800

    feat: add getUserLikes route

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../../config/passport')
-
+const { authenticatedUser } = require('../../middleware/auth')
 const userController = require('../../controllers/user-controller')
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+router.get('/:userId/likes', authenticatedUser, userController.getUserLikes)
 router.post('/', userController.signUp)
 
 module.exports = router
