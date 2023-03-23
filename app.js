588dadc540c6d0ea5be05ab593ca8fa3e7293703
LAPTOP-QJBRI58J\Anne <koala1995610@gmail.com>
2023-03-23 19:29:51 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ if (process.env.NODE_ENV !== 'production') {
 const path = require('path')
 const express = require('express')
 const routes = require('./routes')
+const cors = require('cors')
 const methodOverride = require('method-override')
 const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
-
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
