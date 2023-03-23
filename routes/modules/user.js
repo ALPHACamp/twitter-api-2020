5d01e98a25d1ac907ae4203a1e74c0af4d1c5344
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-03-23 14:20:48 +0800

    fix: getuserfollower route

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -4,7 +4,7 @@ const userController = require('../../controllers/user-controller')
 const upload = require('../../middleware/multer')
 
 router.get('/:id/followers', userController.getUsersFollowings)
-router.get('/:id/followings', userController.getUsersFollowings)
+router.get('/:id/followings', userController.getUsersFollowers)
 router.get('/:id/replied_tweets', userController.getRepliedTweets)
 router.get('/:id/likes', userController.getLikeTweets)
 router.get('/:id/tweets', userController.getUserTweets)
