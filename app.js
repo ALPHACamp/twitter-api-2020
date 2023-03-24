ee552440e3cfe94a8188bf88b471d7ba345d3637
Joshua <j2h5068312@gmail.com>
2023-03-24 21:25:53 +0800

    feat: cors

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
 
 const methodOverride = require('method-override')
 const passport = require('./config/passport')
 
 const router = require('./routes')
 
-const app = express()
 const port = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
