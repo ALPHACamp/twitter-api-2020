ad862acbcf22e7ac1654b1f8bf055b54de84c63b
yy933 <emilyyu025@gmail.com>
2023-03-24 16:22:08 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const express = require('express')
 const helpers = require('./_helpers')
 const passport = require('./config/passport')
 const router = require('./routes')
-
 const app = express()
 const port = process.env.PORT || 3000
-
+const corsOptions = {
+  origin: ["http://localhost:3000/"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+app.use(cors(corsOptions));
 app.use(passport.initialize())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
