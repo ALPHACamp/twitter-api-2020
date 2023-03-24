7765db43c0490df28aee0418d941133b850594ba
abbie930 <abbiecheng930@gmail.com>
2023-03-24 19:03:20 +0800

    feat: add postTweet function

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -4,6 +4,8 @@ const helpers = require('../_helpers')
 const tweetController = {
   getTweets: async (req, res, next) => {
     try {
+      const userId = helpers.getUser(req).id
+
       const tweets = await Tweet.findAll({
         order: [['createdAt', 'DESC']],
         include: [User, Reply, Like, { model: User, as: 'LikedUsers' }]
@@ -30,7 +32,7 @@ const tweetController = {
           avatar: User.avatar,
           repliedCount: Replies.length,
           likedCount: Likes.length,
-          isLike: LikedUsers.some((u) => u.id === helpers.getUser(req).id)
+          isLike: LikedUsers.some((u) => u.id === userId)
         })
         return result
       }, [])
@@ -42,6 +44,8 @@ const tweetController = {
   getTweet: async (req, res, next) => {
     try {
       const { tweetId } = req.params
+      const userId = helpers.getUser(req).id
+
       const tweet = await Tweet.findByPk(tweetId, {
         include: [User, Reply, Like, { model: User, as: 'LikedUsers' }]
       })
@@ -49,7 +53,7 @@ const tweetController = {
       if (!tweet) {
         return res.status(404).json({
           status: 'error',
-          message: 'Tweet not found!',
+          message: 'Tweet not found!'
         })
       }
 
@@ -63,13 +67,35 @@ const tweetController = {
         avatar: tweet.User.avatar,
         repliedCount: tweet.Replies.length,
         likedCount: tweet.Likes.length,
-        isLike: tweet.LikedUsers.some((u) => u.id === helpers.getUser(req).id)
+        isLike: tweet.LikedUsers.some((u) => u.id === userId)
       }
       return res.status(200).json({
         status: 'success',
         message: 'Successfully retrieved this Tweet.',
-        ...tweetData,
+        ...tweetData
       })
+    } catch (err) {
+      next(err)
+    }
+  },
+  postTweet: async (req, res, next) => {
+    try {
+      const { description } = req.body
+      const UserId = helpers.getUser(req).id
+
+      if (!description || !description.trim()) {
+        return res.status(400).json({ status: 'error', message: 'Please enter a message before posting.' })
+      }
+      if (description.length > 140) {
+        return res.status(400).json({ status: 'error', message: 'Tweet cannot be longer than 140 characters.' })
+      }
+
+      const tweet = await Tweet.create({
+        UserId,
+        description
+      })
+
+      return res.status(200).json({ status: 'success', message: 'The tweet was successfully posted.', tweet })
     } catch (err) {
       next(err)
     }
