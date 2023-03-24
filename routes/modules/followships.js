09a504623d685e79ce09e5d23b7053b9e3bcd652
WilliamChen15 <turelight13@gmail.com>
2023-03-24 23:20:40 +0800

    feat: recommend follower

--- routes/modules/followships.js
+++ routes/modules/followships.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 const followshipController = require('../../controllers/followship-controller')
 
-router.post('/', followshipController.addFollowing)
+router.get('/top/:count', followshipController.getMostFollowed)
+
 router.delete('/:followingId', followshipController.removeFollowing)
+
+router.post('/', followshipController.addFollowing)
 
 module.exports = router
