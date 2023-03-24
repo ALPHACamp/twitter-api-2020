b0ffcec12e3bd4e551ea29c113184ea95679da4e
daniel155158 <daniel155158@gmail.com>
2023-03-24 10:52:13 +0800

    feat: add GET admin/users API

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const helpers = require('../_helpers')
+const { User, Tweet, Like } = require('../models')
 
 const adminController = {
   signIn: (req, res, next) => {
@@ -20,6 +21,33 @@ const adminController = {
     } catch (err) {
       next(err)
     }
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      attributes: ['id', 'name', 'account', 'avatar', 'cover'],
+      include: [
+        { model: Tweet, include: Like },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' }
+      ]
+    })
+      .then(users => {
+        const usersData = users.map(user => {
+          const data = {
+            ...user.toJSON(),
+            tweetCounts: user.Tweets.length,
+            // 先取出每篇推文被like的數量, 再透過array.reduce依序加起來(達到加總的效果)
+            beLikedCounts: user.Tweets.map(tweet => tweet.Likes.length).reduce((accumulator, currentValue) => accumulator + currentValue, 0),
+            followerCounts: user.Followers.length,
+            followingCounts: user.Followings.length
+          }
+          delete data.Tweets
+          delete data.Followers
+          delete data.Followings
+          return data
+        })
+        res.json(usersData.sort((a, b) => b.tweetCounts - a.tweetCounts))
+      })
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,6 +4,7 @@ const passport = require('../../config/passport')
 const adminController = require('../../controllers/admin-controller')
 const { authenticatedAdmin } = require('../../middleware/auth')
 
+router.get('/users', authenticatedAdmin, adminController.getUsers)
 router.post('/signin', passport.authenticate('local', { session: false }), adminController.signIn)
 
 module.exports = router
