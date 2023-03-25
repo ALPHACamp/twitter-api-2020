7f70bc2a73b2ace0ffd523a68854a3044f91117f
peggy8865 <peggy8865@gmail.com>
2023-03-25 15:14:40 +0800

    fix: use raw queries to get users

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,34 +1,42 @@
-const { User, Tweet, Like } = require('../models')
+const { sequelize } = require('../models')
+const { QueryTypes } = require('sequelize')
 
 const adminController = {
   getUsers: async (req, res, next) => {
     try {
-      const data = await User.findAll({
-        attributes: ['id', 'name', 'account', 'avatar', 'cover'],
-        include: [
-          {
-            model: Tweet,
-            attributes: ['id'],
-            include: [{ model: Like, attributes: ['id'] }]
-          },
-          { model: User, as: 'Followings', attributes: ['id'] },
-          { model: User, as: 'Followers', attributes: ['id'] }
-        ]
+      const users = await sequelize.query(`      
+      SELECT followshipsPerUser.tweeterId AS id, name, account, avatar, cover, tweetsCount AS tweets, likesTotalCount AS likes, followingsCount AS followings, followersCount AS followers FROM (
+        SELECT tweeterId, COUNT(tweetId) AS tweetsCount, SUM(likesCount) AS likesTotalCount FROM (
+          SELECT tweets.id AS tweetId, tweets.UserId AS tweeterId, COUNT(likes.id) AS likesCount FROM tweets AS tweets
+          LEFT JOIN likes AS likes
+          ON likes.TweetId = tweets.id
+          GROUP BY tweets.id
+        ) AS likesPerTweet
+        GROUP BY tweeterId
+      ) AS likesAndTweetsPerUser
+
+      INNER JOIN (
+        SELECT tweeterId, followingsCount, COUNT(followerId) AS followersCount FROM (
+          SELECT users.id AS tweeterId, COUNT(followingId) AS followingsCount FROM users AS users
+          LEFT JOIN followships AS followships
+          ON followships.followerId = users.id
+          GROUP BY users.id
+        ) AS followingsPerUser
+        LEFT JOIN followships AS followships
+        ON followships.followingId = tweeterId
+        GROUP BY tweeterId
+      ) AS followshipsPerUser
+      ON likesAndTweetsPerUser.tweeterId = followshipsPerUser.tweeterId
+      
+      INNER JOIN users AS users
+      ON users.id = followshipsPerUser.tweeterId
+      WHERE users.role = 'user'
+        
+      ORDER BY tweets DESC`,
+      {
+        type: QueryTypes.SELECT
       })
-      const users = data.map(d => {
-        const user = {
-          ...d.toJSON(),
-          tweets: d.Tweets?.length || 0,
-          likes: (d.Tweets?.map(t => t.Likes.length)).reduce((a, b) => a + b, 0),
-          followings: d.Followings?.length || 0,
-          followers: d.Followers?.length || 0
-        }
-        delete user.Tweets
-        delete user.Followers
-        delete user.Followings
-        return user
-      })
-      users.sort((a, b) => b.tweets - a.tweets)
+
       res.status(200).json(users)
     } catch (err) {
       next(err)
