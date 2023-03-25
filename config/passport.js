3489ee4ab778c09bb84321e3e1cf74a8cbecac4d
Ian_Lin <ian920511@gmail.com>
2023-03-25 19:54:07 +0800

    feat: add local strategy

--- config/passport.js
+++ config/passport.js
@@ -1,18 +1,45 @@
 const passport = require('passport')
 const passportJWT = require('passport-jwt')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
 const { User } = require('../models')
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
+
+passport.use(
+  new LocalStrategy(
+    {
+      // 因為只有usernameField變成account，所以只改這個
+      usernameField: 'account'
+    },
+    (account, password, cb) => {
+      User.findOne({ where: { account } })
+        .then(user => {
+          // 如果使用者不存在
+          if (!user) return cb(null, false, { message: '帳號或密碼輸入錯誤!' })
+          // 如果使用者密碼錯誤
+          return bcrypt.compare(password, user.password).then(res => {
+            if (!res) { return cb(null, false, { message: '帳號或密碼輸入錯誤!' }) }
+            // 認證成功，回傳user資訊
+            return cb(null, user, { message: '登入成功!' })
+          })
+        })
+        .catch(error => cb(error))
+    }
+  )
+)
 
 const jwtOptions = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET
 }
-passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
-  User.findByPk(jwtPayload.id)
-    .then(user => cb(null, user))
-    .catch(err => cb(err))
-}))
+passport.use(
+  new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+    User.findByPk(jwtPayload.id)
+      .then(user => cb(null, user))
+      .catch(err => cb(err))
+  })
+)
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -6,13 +6,16 @@ const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 
 const userController = require('../../controllers/user-controller')
+const adminController = require('../../controllers/admin-controller')
 
 const { authenticate, authenticateAdmin } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
+const passport = require('passport')
 
 router.post('/users/signIn', userController.signIn)
 router.post('/users', userController.signUp)
 
+router.post('/admin/signIn', passport.authenticate('local', { session: false }), adminController.signIn)
 router.use('/admin', authenticate, authenticateAdmin, admin)
 router.use('/users', authenticate, users)
 router.use('/tweets', authenticate, tweets)
