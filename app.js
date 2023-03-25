43b71da1ae864302762ff1e2bda105a29f089cc0
chikunmark <chikunmark@gmail.com>
2023-03-25 14:40:55 +0800

    test8

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-if (process.env.NODE_ENV !== 'production') {
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
+if (process.env.NODE_ENV === 'development' || 'test') {
   require('dotenv').config()
 }
 
