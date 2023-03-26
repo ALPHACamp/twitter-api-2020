8110a9c0a47d99d4cd5f4d0eb8cf6704ed72cde0
yy933 <emilyyu025@gmail.com>
2023-03-26 16:02:28 +0800

    feat: add removeFollowing function

--- routes/modules/followship.js
+++ routes/modules/followship.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
 
+router.delete('/:followingId', userController.removeFollowing)
 router.post('/', userController.addFollowing)
 
 module.exports = router
