620d40105c69d7be5322acefd4ae33ba8ff2489e
abbie930 <abbiecheng930@gmail.com>
2023-03-26 22:28:31 +0800

    feat: change the login route to api/signin

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
+
+const { apiErrorHandler } = require('../middleware/error-handler')
+const { authenticatedUser } = require('../middleware/auth')
 
 const user = require('./modules/user')
 const tweet = require('./modules/tweet')
 const followship = require('./modules/followship')
 
-const { apiErrorHandler } = require('../middleware/error-handler')
-const { authenticatedUser } = require('../middleware/auth')
+const userController = require('../controllers/user-controller')
 
-router.use('/users', user)
+router.use('/users', authenticatedUser, user)
 router.use('/tweets', authenticatedUser, tweet)
 router.use('/followships', authenticatedUser, followship)
+
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+
 router.use('/', apiErrorHandler)
 
 module.exports = router
