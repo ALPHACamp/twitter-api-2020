7498e0ccb3fb7366fef5838f132f5cc5c11282d7
Chloe905 <cchloehsieh@gmail.com>
2023-03-27 01:05:20 +0800

    feat: test for travis

--- app.js
+++ app.js
@@ -13,15 +13,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // 設定跨域
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
-    return res.status(200).json({})
-  }
-  next()
-})
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+//   if (req.method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+//     return res.status(200).json({})
+//   }
+//   next()
+// })
 
 app.use('/api', router) // 使用api路由
 
