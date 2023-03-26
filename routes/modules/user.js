0892e948a754f1aedaed7c81c5084050ccaff3e9
yy933 <emilyyu025@gmail.com>
2023-03-27 00:04:55 +0800

    fix: fix authentication route

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {authenticatedUser} = require('../../middleware/auth')
+
 const userController = require('../../controllers/user-controller')
 
-router.get('/:userId', authenticatedUser, userController.getUser)
-router.get('/:userId/tweets', authenticatedUser, userController.getUserTweets)
+router.get('/:userId', userController.getUser)
+router.get('/:userId/tweets', userController.getUserTweets)
 router.get(
   '/:userId/replied_tweets', userController.getUserReplies)
 
