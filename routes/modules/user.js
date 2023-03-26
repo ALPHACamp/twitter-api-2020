2bf60035a39ab44feedd1f91f0437ffffdb9d88d
yy933 <emilyyu025@gmail.com>
2023-03-27 00:33:58 +0800

    formatting code

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const { authenticatedUser } =require('../../middleware/auth')
+const { authenticatedUser } = require('../../middleware/auth')
 const userController = require('../../controllers/user-controller')
 
-router.get("/:userId", authenticatedUser, userController.getUser);
-router.get("/:userId/tweets", authenticatedUser, userController.getUserTweets);
+router.get('/:userId', authenticatedUser, userController.getUser)
+router.get('/:userId/tweets', authenticatedUser, userController.getUserTweets)
 router.get(
-  "/:userId/replied_tweets",
+  '/:userId/replied_tweets',
   authenticatedUser,
   userController.getUserReplies
-);
+)
 
 router.post('/', userController.signUp)
 
