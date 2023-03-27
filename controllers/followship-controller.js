d74f9ba172627a6a978144ff50323cf696d54df6
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-03-28 04:10:56 +0800

    fix: keywords for FrontEnd

--- controllers/followship-controller.js
+++ controllers/followship-controller.js
@@ -50,7 +50,7 @@ const adminController = {
     const result = users.map(user => {
       return {
         user,
-        isFollowed: currentUser.Followings.some(fu => fu.id === user.id) // fu = following user
+        currentfollowed: currentUser.Followings.some(fu => fu.id === user.id) // fu = following user
       }
     })
     res.status(200).json(result)
