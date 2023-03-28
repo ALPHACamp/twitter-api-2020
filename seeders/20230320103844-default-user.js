c2264549b9acdfba9052d06314807fd09b4514e7
FrankHuang <frankcool321@gmail.com>
2023-03-28 10:16:08 +0800

    refactor: modify default user count

--- seeders/20230320103844-default-user.js
+++ seeders/20230320103844-default-user.js
@@ -1,45 +1,45 @@
-'use strict'
-const bcrypt = require('bcryptjs')
-const { faker } = require('@faker-js/faker')
+"use strict";
+const bcrypt = require("bcryptjs");
+const { faker } = require("@faker-js/faker");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // - 一個 admin 和 9 個一般用戶
     await queryInterface.bulkInsert(
-      'Users',
-      Array.from({ length: 10 }, (_, index) =>
+      "Users",
+      Array.from({ length: 15 }, (_, index) =>
         index === 0
           ? {
-              name: 'root',
-              email: 'root@example.com',
-              account: 'root',
-              password: bcrypt.hashSync('12345678', 10),
+              name: "root",
+              email: "root@example.com",
+              account: "root",
+              password: bcrypt.hashSync("12345678", 10),
               isAdmin: true,
-              role: 'admin',
+              role: "admin",
               avatar: faker.image.avatar(),
               cover: faker.image.nature(640, 480, true),
               introduction: faker.lorem.paragraph(4),
               createdAt: new Date(),
-              updatedAt: new Date()
+              updatedAt: new Date(),
             }
           : {
               name: `user${index}`,
               email: `user${index}@example.com`,
               account: `user${index}`,
-              password: bcrypt.hashSync('12345678', 10),
+              password: bcrypt.hashSync("12345678", 10),
               isAdmin: false,
-              role: 'user',
+              role: "user",
               avatar: faker.image.avatar(),
               cover: faker.image.nature(640, 480, true),
               introduction: faker.lorem.paragraph(4),
               createdAt: new Date(),
-              updatedAt: new Date()
+              updatedAt: new Date(),
             }
       ),
       {}
-    )
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Users', null, {})
-  }
-}
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
