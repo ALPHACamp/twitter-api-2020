61a572433f2c426fbeb5a654e321c2153ca601d9
FrankHuang <frankcool321@gmail.com>
2023-03-28 18:04:55 +0800

    fix/modify tweet seeds logic & queries for users

--- seeders/20230320113134-tweet-seeds.js
+++ seeders/20230320113134-tweet-seeds.js
@@ -1,30 +1,30 @@
-'use strict'
-const { faker } = require('@faker-js/faker')
+"use strict";
+const { faker } = require("@faker-js/faker");
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     // - 查詢一般使用者
     const users = await queryInterface.sequelize.query(
-      "SELECT id FROM Users WHERE account <> 'root'",
+      "SELECT id FROM Users WHERE role <> 'admin'",
       {
-        type: queryInterface.sequelize.QueryTypes.SELECT
+        type: queryInterface.sequelize.QueryTypes.SELECT,
       }
-    )
+    );
+    const tweets = [];
     // - 每個一般使用者有 10 篇 tweets
     for (let i = 0; i < users.length; i += 1) {
-      await queryInterface.bulkInsert(
-        'Tweets',
-        Array.from({ length: 10 }, () => ({
+      tweets.push(
+        ...Array.from({ length: 10 }, () => ({
           description: faker.lorem.lines(),
           UserId: users[i].id,
           createdAt: new Date(),
-          updatedAt: new Date()
-        })),
-        {}
-      )
+          updatedAt: new Date(),
+        }))
+      );
     }
+    await queryInterface.bulkInsert("Tweets", tweets, {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Tweets', null, {})
-  }
-}
+    await queryInterface.bulkDelete("Tweets", null, {});
+  },
+};
