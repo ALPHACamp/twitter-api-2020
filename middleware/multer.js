3462f8b2a3a1daaf533502c9f6eb74eb582f69ef
Noelle-KH <noelletang86@gmail.com>
2023-03-29 16:14:11 +0800

    feat: add image file size limit

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,11 +1,14 @@
 const multer = require('multer')
 const createError = require('http-errors')
 
+const fileSizeLimit = 20 * 1024 * 1024
 const upload = multer({
   dest: 'temp/',
   fileFilter: (req, file, cb) => {
     const fileType = file.mimetype.split('/')
-    if (!(fileType[0] === 'image' && fileType[1] !== 'svg+xml')) cb(createError(415, '請上傳圖片檔'))
+
+    if (!(fileType[0] === 'image' && fileType[1] !== 'svg+xml')) cb(createError(400, '請上傳圖片檔'))
+    if (file.size > fileSizeLimit) cb(createError(400, '檔案大小超過限制'))
 
     return cb(null, true)
   }
