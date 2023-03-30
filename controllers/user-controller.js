78a478bf15dd76f73665b72552b0a9983674b0f7
yy933 <emilyyu025@gmail.com>
2023-03-30 23:23:22 +0800

    fix: rename return data in getUserLikes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -214,6 +214,7 @@ const userController = {
           likeId: like.id,
           likeCreatedAt: like.createdAt,
           id: like.TweetId,
+          TweetId: like.TweetId,
           UserId: like.Tweet.UserId,
           account: like.Tweet.User.account,
           name: like.Tweet.User.name,
