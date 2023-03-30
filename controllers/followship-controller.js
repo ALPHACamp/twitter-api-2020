8fed1669b18a0a78449af24df5618ad3faf51047
peggy8865 <peggy8865@gmail.com>
2023-03-30 18:11:47 +0800

    fix: user cannot follow himself

--- controllers/followship-controller.js
+++ controllers/followship-controller.js
@@ -1,17 +1,18 @@
-const helper = require('../_helpers')
+const helpers = require('../_helpers')
 const { User, Followship } = require('../models')
 
 const followshipController = {
   addFollowships: async (req, res, next) => {
     try {
       const followingId = req.body.id
-      const followerId = helper.getUser(req).id
+      const followerId = helpers.getUser(req).id
+      if (Number(followingId) === followerId) throw new Error('不可追蹤自己')
       const users = await Promise.all([
-        User.findByPk(followingId),
-        User.findByPk(followerId)
+        User.findByPk(followingId, { raw: true }),
+        User.findByPk(followerId, { raw: true })
       ])
       if (!users[0] || !users[1]) throw new Error('使用者不存在')
-      const followship = await Followship.findOne({ where: { followingId, followerId } })
+      const followship = await Followship.findOne({ where: { followingId, followerId }, raw: true })
       if (followship) throw new Error('已追蹤對方')
       await Followship.create({ followingId, followerId })
       res.status(200).end()
@@ -22,13 +23,13 @@ const followshipController = {
   removeFollowships: async (req, res, next) => {
     try {
       const followingId = req.params.followingId
-      const followerId = helper.getUser(req).id
+      const followerId = helpers.getUser(req).id
       const users = await Promise.all([
-        User.findByPk(followingId),
-        User.findByPk(followerId)
+        User.findByPk(followingId, { raw: true }),
+        User.findByPk(followerId, { raw: true })
       ])
       if (!users[0] || !users[1]) throw new Error('使用者不存在')
-      const followship = await Followship.findOne({ where: { followingId, followerId } })
+      const followship = await Followship.findOne({ where: { followingId, followerId }, attribute: [] })
       if (!followship) throw new Error('尚未追蹤對方')
       await followship.destroy()
       res.status(200).end()
