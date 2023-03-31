ca41296dd154f89e695c6ac39d51079ea9d20ca8
freeway26tw <freeway26tw@gmail.com>
2023-03-31 17:27:16 +0800

    create-role typo

--- migrations/20230320013749-create-role.js
+++ migrations/20230320013749-create-role.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('roles', {
+    await queryInterface.createTable('Roles', {
       role: {
         allowNull: false,
         primaryKey: true,
