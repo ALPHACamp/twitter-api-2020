c9354fd2399a9c24a9ace57ae8d790d2df1f8b85
abbie930 <abbiecheng930@gmail.com>
2023-04-01 22:30:13 +0800

    feat: set the file format and size for uploading images

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,11 +1,18 @@
 const imgur = require('imgur')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
-
 imgur.setClientId(IMGUR_CLIENT_ID)
+
+// image setting
+const allowedFormats = ['image/jpg', 'image/jpeg', 'image/png']
+const sizeLimit = 1048576
+
 
 const imgurFileHandler = (file) => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
+    if (!allowedFormats.includes(file.mimetype))
+      return reject(new Error(`File format not allowed. Allowed formats: ${allowedFormats.join(', ')}`))
+    if (file.size > sizeLimit) return reject(new Error(`File size exceeds the limit of ${sizeLimit} bytes`))
     return imgur
       .uploadFile(file.path)
       .then((img) => {
