1f23c5c4723f25d4dc71b8a994fbe78e9e8d6d4b
seanlin1125 <sean821125@gmail.com>
2023-04-06 18:48:05 +0800

    test: interview rollback

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,31 @@
-const passport = require("../config/passport")
-const helpers = require("../_helpers")
+const passport = require('../config/passport')
+const helpers = require('../_helpers')
 
 const authenticated = (req, res, next) => {
-  passport.authenticate("jwt", { session: false }, (err, user) => {
-    if (err || !user)
-      return res.status(401).json({ status: "error", message: "未經授權ar2vr" })
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) { return res.status(401).json({ status: 'error', message: '未經授權' }) }
     req.user = user
     next()
   })(req, res, next)
 }
 const authenticatedAdmin = (req, res, next) => {
-  if (helpers.getUser(req) && helpers.getUser(req).role === "admin")
-    return next()
-  return res.status(403).json({ status: "error", message: "未經允許無法使用!" })
+  if (helpers.getUser(req) && helpers.getUser(req).role === 'admin') { return next() }
+  return res.status(403).json({ status: 'error', message: '未經允許無法使用!' })
 }
 const authenticatedUser = (req, res, next) => {
-  if (helpers.getUser(req) && helpers.getUser(req).role === "user")
-    return next()
-  return res.status(403).json({ status: "error", message: "未經允許無法使用!" })
+  if (helpers.getUser(req) && helpers.getUser(req).role === 'user') { return next() }
+  return res.status(403).json({ status: 'error', message: '未經允許無法使用!' })
 }
 const authCurrentUser = (req, res, next) => {
   if (helpers.getUser(req).id === Number(req.params.id)) return next()
   return res
     .status(403)
-    .json({ status: "error", message: "使用者只能編輯自己的資訊!" })
+    .json({ status: 'error', message: '使用者只能編輯自己的資訊!' })
 }
 
 module.exports = {
   authenticated,
   authenticatedAdmin,
   authenticatedUser,
-  authCurrentUser,
+  authCurrentUser
 }
