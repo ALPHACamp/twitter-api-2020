9f513be982808dbf7b0920f0a5d555ec4e8e0f94
tschiang23 <tien23@gmail.com>
2023-06-01 15:42:28 +0800

    feat:add user login

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
-const routes = require('./routes')
 const cors = require('cors')
+const routes = require('./routes')
+const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(passport.initialize())
 
 app.use('/api', routes)
 app.get('/', (req, res) => res.send('Hello World!'))
