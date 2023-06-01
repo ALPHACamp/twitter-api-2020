c67353bd9d3e98b104ada5e852b29f772e5c3640
Anna0118 <annachu0988@gmail.com>
2023-06-01 20:26:00 +0800

    feat: add user seeder

---
+++ seeders/20230601102119-users-seed-file.js
@@ -0,0 +1,34 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          // 一次新增三筆資料
+          name: 'root',
+          account: 'root',
+          email: 'root@example.com',
+          password: await bcrypt.hash('12345678', 10),
+          avatar: `https://loremflickr.com/320/240/people/?random=${
+            Math.random() * 10
+          }`,
+          cover: `https://loremflickr.com/320/240/mountain/?random=${
+            Math.random() * 100
+          }`,
+          role: 'admin',
+          introduction: faker.lorem.text(),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => { // 清空資料表中所有資料
+    await queryInterface.bulkDelete('Users', {})
+  }
+}
