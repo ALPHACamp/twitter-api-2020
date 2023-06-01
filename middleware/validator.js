d421efafccf88be9176bcae70fbe31a404ff17b3
tschiang23 <tien23@gmail.com>
2023-06-01 21:34:17 +0800

    feat:add signin and signup validator

---
+++ middleware/validator.js
@@ -0,0 +1,57 @@
+const { body, validationResult } = require('express-validator')
+
+const signUpValidator = [
+  body('account')
+    .trim()
+    .notEmpty().withMessage('帳號為必填項目')
+    .bail()
+    .isLength({ max: 20 }).withMessage('帳號最多20個字'),
+  body('email')
+    .trim()
+    .notEmpty().withMessage('信箱為必填項目')
+    .bail()
+    .isEmail().withMessage('必須是合法email')
+    .isLength({ max: 32 }).withMessage('信箱最多32個字'),
+  body('name')
+    .trim()
+    .notEmpty().withMessage('姓名為必填項目')
+    .bail() // 第一個條件不通過 不繼續檢查
+    .isLength({ max: 50 }).withMessage('姓名最多50個字'),
+  body('password').trim().isLength({ min: 6, max: 32 }).withMessage('密碼長度至少六位'),
+  // body('confirmPassword')
+  //   .trim()
+  //   .custom((value, { req }) => {
+  //     // 確認密碼欄位的值需要和密碼欄位的值相符
+  //     if (value !== req.body.password) {
+  //       // 驗證失敗時的錯誤訊息
+  //       throw new Error('兩次輸入的密碼不相同')
+  //     }
+  //     // 成功驗證回傳 true
+  //     return true
+  //   }),
+  (req, res, next) => {
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() })
+    }
+    next()
+  }
+]
+
+const signInValidator = [
+  body('account')
+    .trim()
+    .notEmpty().withMessage('帳號為必填項目'),
+  body('password')
+    .trim()
+    .notEmpty().withMessage('密碼為必填項目'),
+  (req, res, next) => {
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+      return res.status(400).json({ errors: result.array() })
+    }
+    next()
+  }
+]
+
+module.exports = { signUpValidator, signInValidator }
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,7 +2,8 @@ const express = require('express')
 const passport = require('../../config/passport')
 const router = express.Router()
 const userController = require('../../controllers/userController')
+const { signUpValidator, signInValidator } = require('../../middleware/validator')
 
-router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
-router.post('/', userController.signUp)
+router.post('/signin', signInValidator, passport.authenticate('local', { session: false }), userController.signIn)
+router.post('/', signUpValidator, userController.signUp)
 module.exports = router
