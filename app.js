04ef76ec9c459b530d63a22461bc1aa66f04024b
tschiang23 <tien23@gmail.com>
2023-06-04 08:31:08 +0800

    feat:add cors

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
+const cors = require('cors')
 const routes = require('./routes')
 const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
+// middleware
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
