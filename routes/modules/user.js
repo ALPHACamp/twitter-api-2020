0c3ced7ef92dc7b72b2f105568729138db859614
tschiang23 <tien23@gmail.com>
2023-06-04 20:04:48 +0800

    fix:modify sigin route role

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,8 +2,8 @@ const express = require('express')
 const passport = require('../../config/passport')
 const router = express.Router()
 const userController = require('../../controllers/userController')
-const { adminRole } = require('../../middleware/auth')
+const { userRole } = require('../../middleware/auth')
 
-router.post('/signin', passport.authenticate('local', { session: false }), adminRole, userController.signIn)
+router.post('/signin', passport.authenticate('local', { session: false }), userRole, userController.signIn)
 router.post('/', userController.signUp)
 module.exports = router
