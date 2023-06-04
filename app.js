bddc178dd11ff761adcc73a5fc15e9777553c5a2
tschiang23 <tien23@gmail.com>
2023-06-04 08:29:03 +0800

    feat:add signin

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const routes = require('./routes')
+const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(passport.initialize())
 
 app.use('/api', routes)
 app.get('/', (req, res) => res.send('Hello World!'))
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const passport = require('../../config/passport')
 const router = express.Router()
 const userController = require('../../controllers/userController')
 
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.post('/', userController.signUp)
 module.exports = router
