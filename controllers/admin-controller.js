364ce438708714f8f6f7285082e4755b2ac5547b
jasmine <jasmine12122005@yahoo.com.tw>
2023-06-06 00:04:23 +0800

    feat: add deleteTweet function

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const { User, Tweet } = require('../models')
+const tweet = require('../models/tweet')
 const adminController = {
   getUsers: (req, res, next) => {
     User.findAll({
@@ -24,10 +25,27 @@ const adminController = {
       raw: true
     })
       .then((tweets) => {
-        if (!tweets) { return res.status(404).json({ message: 'No tweets found' }) }
+        if (!tweets) { return res.status(404).json({ status: 'error', message: 'No tweets found' }) }
         return res.status(200).json(tweets)
       })
       .catch((err) => next(err))
   },
+
+  deleteTweet: (req, res, next) => {
+    return Tweet.findByPk(req.params.id)
+      .then(tweet => {
+        // Error: tweet doesn't exist
+        if (!tweet) {
+          return res.status(404).json({ status: 'error', message: 'No tweets found' })
+        }
+        // keep the deleted data
+        const deletedTweet = tweet.toJSON()
+        return tweet.destroy()
+          .then(() => {
+            return res.status(200).json({ status: 'success', message: 'Tweet deleted', deletedTweet })
+          })
+      })
+      .catch(err => next(err))
+  }
 }
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
-// router.delete('/tweets/:id', adminController.deleteTweet)
+router.delete('/tweets/:id', adminController.deleteTweet)
 router.get('/users', adminController.getUsers)
 router.get('/tweets', adminController.getTweets)
 module.exports = router
