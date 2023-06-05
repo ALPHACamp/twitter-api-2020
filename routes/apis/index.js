907feda982ee1984ec79d1d95a2c930a51ba6c31
popojk <c.alexwu@gmail.com>
2023-06-05 21:11:28 +0800

    setup-app.js-and-error-handler

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const { apiErrorHandler } = require('../../middleware/error-handler')
+
+router.use('/', apiErrorHandler)
 
 module.exports = router
