065395ddd67575ecc586ce2b7d7e70c635821169
Anna0118 <annachu0988@gmail.com>
2023-06-06 03:02:10 +0800

    tweet and reply

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,5 +1,4 @@
 const { getUser } = require('../_helpers')
-// helpers.getUser(req);
 const { Op } = require('sequelize')
 const { User, Tweet, Reply, Like, Followship, sequelize } = require('../models')
 
@@ -9,6 +8,7 @@ const tweetController = {
     try {
       const user = getUser(req)
       // console.log(req)
+      console.log(req.user)
       if (!(user instanceof User)) {
         return res.status(400).json({ error: 'User not found' })
       }
@@ -17,12 +17,12 @@ const tweetController = {
       // console.log(userData)
 
       // 先找到user追蹤的人
-      const followships = await Followship.findAll({
-        where: { followerId: userData.id },
-        attributes: ['followingId'],
-        raw: true
-      })
-      const followingIds = followships.map(followship => followship.followingId)
+      // const followships = await Followship.findAll({
+      //   where: { followerId: userData.id },
+      //   attributes: ['followingId'],
+      //   raw: true
+      // })
+      // const followingIds = followships.map(followship => followship.followingId)
 
       const tweets = await Tweet.findAll({
         attributes: [
@@ -63,7 +63,7 @@ const tweetController = {
       const data = tweets.map(tweet => {
         return {
           tweetId: tweet.id,
-          tweetText: tweet.description,
+          description: tweet.description,
           user: {
             id: tweet['User.id'],
             name: tweet['User.name'],
@@ -75,10 +75,163 @@ const tweetController = {
           likeCount: tweet.likeCount || 0
         }
       })
-      // ------------------------------
-      return res.status(200).json({
-        tweets: data
+      return res.status(200).json(
+        // {tweets: data}
+        data
+      )
+    } catch (err) {
+      next(err)
+    }
+  },
+  postTweet: async (req, res, next) => {
+    try {
+      const { description } = req.body
+      // if (!description) {
+      //   return res.status(400).json({ error: 'Description is required!' })
+      // }
+      // Check if description is more than 160 characters
+      if (description.trim().length > 140) {
+        return res
+          .status(400)
+          .json({ error: 'Description should be within 160 characters!' })
+      }
+      // Check if description is whitespace
+      if (!description.trim().length) {
+        return res
+          .status(400)
+          .json({ error: 'Description cannot be only whitespace!' })
+      }
+      // 取得發文者id
+      const user = getUser(req)
+      if (!user || !user.id) {
+        return res.status(400).json({ error: 'User not found' })
+      }
+      console.log(user)
+      const UserId = user.id
+
+      // 創建新的Tweet
+      const newTweet = await Tweet.create({
+        description,
+        UserId
       })
+      // 回傳成功訊息與新的tweet
+      return (
+        res
+          .status(201)
+          // .json({ message: 'Tweet successfully posted!', tweet: [newTweet] })
+          .json([newTweet])
+      )
+    } catch (err) {
+      next(err)
+    }
+  },
+  getTweet: async (req, res, next) => {
+    try {
+      const tweet = await Tweet.findByPk(req.params.tweet_id, {
+        attributes: ['id', 'description', 'createdAt'],
+        include: [
+          {
+            model: User,
+            attributes: ['id', 'name', 'account', 'avatar']
+          },
+          {
+            model: Reply,
+            attributes: ['id', 'comment', 'createdAt'],
+            include: [
+              {
+                model: User,
+                attributes: ['id', 'account', 'name', 'avatar']
+              }
+            ]
+          },
+          {
+            model: Like,
+            attributes: []
+          }
+        ]
+      })
+      if (!tweet) {
+        return res.status(404).json({ error: 'Tweet not found!' })
+      }
+      const tweetData = tweet.get({ plain: true })
+      // console.log(tweetData)
+
+      tweetData.replyCount = tweet.Replies ? tweet.Replies.length : 0
+      tweetData.likeCount = tweet.Likes ? tweet.Likes.length : 0
+      // console.log(tweetData.Replies)
+
+      tweetData.Replies = tweet.Replies.map(reply => {
+        const { id, comment, createdAt, User } = reply
+        return {
+          replyId: id,
+          replyComment: comment,
+          replyTime: createdAt,
+          User: User
+        }
+      })
+
+      return res.status(200).json(
+        // { tweet: [tweetData] }
+        [tweetData]
+      )
+    } catch (err) {
+      next(err)
+    }
+  },
+  getReplies: async (req, res, next) => {
+    try {
+      const { tweet_id: tweetId } = req.params
+      const tweet = await Tweet.findByPk(tweetId)
+      if (!tweet) {
+        return res.status(404).json({ error: 'Tweet not found!' })
+      }
+
+      const replies = await Reply.findAll({
+        raw: true,
+        nest: true,
+        where: { TweetId: tweetId },
+        order: [['createdAt', 'DESC']]
+      })
+
+      return res.status(200).json(replies)
+    } catch (err) {
+      next(err)
+    }
+  },
+  postReply: async (req, res, next) => {
+    try {
+      const { tweet_id: tweetId } = req.params
+      const tweet = await Tweet.findByPk(tweetId)
+      if (!tweet) {
+        return res.status(404).json({ error: 'Tweet not found!' })
+      }
+      const { comment } = req.body
+      // Check if comment is whitespace
+      if (!comment.trim().length) {
+        return res
+          .status(400)
+          .json({ error: 'Comment cannot be only whitespace!' })
+      }
+      // Get user id
+      const user = getUser(req)
+      if (!user || !user.id) {
+        return res.status(400).json({ error: 'User not found' })
+      }
+      const UserId = user.id
+
+      // Create new reply
+      const newReply = await Reply.create({
+        comment,
+        UserId,
+        TweetId: tweetId
+      })
+
+      return (
+        res
+          .status(201)
+          // .json({ message: 'Tweet successfully posted!', tweet: [newTweet] })
+          .json(newReply)
+      )
     } catch (err) {
       next(err)
     }
--- routes/modules/tweet.js
+++ routes/modules/tweet.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { authenticated } = require('../../middleware/auth')
+const { authenticated, authenticatedUser } = require('../../middleware/auth')
 const tweetController = require('../../controllers/tweetController')
 
 router.get('/', authenticated, tweetController.getTweets)
+router.post('/', authenticated, authenticatedUser, tweetController.postTweet)
+router.get('/:tweet_id', authenticated, authenticatedUser, tweetController.getTweet)
+router.get('/:tweet_id/replies', authenticated, authenticatedUser, tweetController.getReplies)
+router.post('/:tweet_id/replies', authenticated, authenticatedUser, tweetController.postReply)
+
 module.exports = router
