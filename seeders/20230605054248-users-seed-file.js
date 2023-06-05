5d3caf2758281cc16c9fe38a7e4b3660df4db1d9
pleasesailas <pleasesailas@gmail.com>
2023-06-05 14:30:25 +0800

    feat: add admin & user seeder

---
+++ seeders/20230605054248-users-seed-file.js
@@ -0,0 +1,78 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      name: 'root',
+      email: 'root@example.com',
+      account: 'root',
+      password: await bcrypt.hash('12345678', 10),
+      avatar: 'https://icon-library.com/images/default-user-icon/default-user-icon-8.jpg',
+      backgroundImage: '',
+      introduction: faker.lorem.text(),
+      role: '管理員',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'user1',
+      email: 'user1@example.com',
+      account: 'user1',
+      password: await bcrypt.hash('12345678', 10),
+      avatar: 'https://icon-library.com/images/default-user-icon/default-user-icon-8.jpg',
+      backgroundImage: '',
+      introduction: faker.lorem.text(),
+      role: '普通會員',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'user2',
+      email: 'user2@example.com',
+      account: 'user2',
+      password: await bcrypt.hash('12345678', 10),
+      avatar: 'https://icon-library.com/images/default-user-icon/default-user-icon-8.jpg',
+      backgroundImage: '',
+      introduction: faker.lorem.text(),
+      role: '普通會員',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'user3',
+      email: 'user3@example.com',
+      account: 'user3',
+      password: await bcrypt.hash('12345678', 10),
+      avatar: 'https://icon-library.com/images/default-user-icon/default-user-icon-8.jpg',
+      backgroundImage: '',
+      introduction: faker.lorem.text(),
+      role: '普通會員',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'user4',
+      email: 'user4@example.com',
+      account: 'user4',
+      password: await bcrypt.hash('12345678', 10),
+      avatar: 'https://icon-library.com/images/default-user-icon/default-user-icon-8.jpg',
+      backgroundImage: '',
+      introduction: faker.lorem.text(),
+      role: '普通會員',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      name: 'user5',
+      email: 'user5@example.com',
+      account: 'user5',
+      password: await bcrypt.hash('12345678', 10),
+      avatar: 'https://icon-library.com/images/default-user-icon/default-user-icon-8.jpg',
+      backgroundImage: '',
+      introduction: faker.lorem.text(),
+      role: '普通會員',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', {})
+  }
+}
