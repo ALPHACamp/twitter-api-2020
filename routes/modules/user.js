d2a1ce2818baa32993db570103d3a256ee4497be
tschiang23 <tien23@gmail.com>
2023-06-05 08:42:24 +0800

    feat:add signin & signup validator

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -3,7 +3,8 @@ const passport = require('../../config/passport')
 const router = express.Router()
 const userController = require('../../controllers/userController')
 const { userRole } = require('../../middleware/auth')
+const { signInValidator, signUpValidator } = require('../../middleware/validator')
 
-router.post('/signin', passport.authenticate('local', { session: false }), userRole, userController.signIn)
-router.post('/', userController.signUp)
+router.post('/signin', signInValidator, passport.authenticate('local', { session: false }), userRole, userController.signIn)
+router.post('/', signUpValidator, userController.signUp)
 module.exports = router
