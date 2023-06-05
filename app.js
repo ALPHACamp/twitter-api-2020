21ad13c7bd67df70737357c5fb5a07bf002b590a
s1030905 <s1030905@outlook.com>
2023-06-05 10:34:55 +0800

    add .env, .env.test, .env.example, set scripts for windows

--- app.js
+++ app.js
@@ -4,12 +4,7 @@ const helpers = require('./_helpers')
 const app = express()
 const port = 3000
 
-// use helpers.getUser(req) to replace req.user
-// function authenticated (req, res, next) {
-//   // passport.authenticate('jwt', { ses...
-// }
-
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on http://localhost:${port} !`))
 
 module.exports = app
