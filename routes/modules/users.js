9d56059d1872245b759f4b9454e4312a4ff86c1f
klu0926 <lukuoyu@gmail.com>
2023-06-06 10:06:36 +0800

    feat: stepup folders, error handlers and app.js

---
+++ routes/modules/users.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+
+// test route (delete later)
+router.get('/', (req, res, next) => {
+  res.json('/api/users/  (Test API Delete Later)')
+})
+
+module.exports = router
