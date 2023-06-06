4845bd7d72a48e506f2313ba367ff247b7c758fb
WeiWayne1030 <wayne10032004@gmail.com>
2023-06-07 00:32:05 +0800

    test: test Authorization

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const helpers = require('./_helpers');
 const routes = require('./routes')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 //allison少加了
 const SESSION_SECRET = process.env.SESSION_SECRET
 
 app.use(express.urlencoded({ extended: true }))
--- routes/index.js
+++ routes/index.js
@@ -9,8 +9,8 @@ const tweetController = require('../controllers/tweet-controller')
 const { authenticatedAdmin, authenticatedUser } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
-router.post('/api/users/signin', passport.authenticate('jwt', { session: false }), authenticatedUser, userController.signIn)
-router.post('/api/admin/signin', passport.authenticate('jwt', { session: false }), authenticatedAdmin, userController.signIn)
+router.post('/api/users/signin', passport.authenticate('local', { session: false }), authenticatedUser, userController.signIn)
+router.post('/api/admin/signin', passport.authenticate('local', { session: false }), authenticatedAdmin, userController.signIn)
 
 router.get('/api/tweets', tweetController.getTweets)
 
