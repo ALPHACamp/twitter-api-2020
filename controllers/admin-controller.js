655b17f3e06f4a849c94ed5146ef9207b4ba1a4a
CHUCHUDAN <daniel870217@gmail.com>
2023-06-06 15:08:56 +0800

    feat: add deleteTweet function

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
 const helpers = require('../_helpers')
+const { Tweet } = require('../models')
 const { newErrorGenerate } = require('../helpers/newError-helper')
 
 const adminController = {
@@ -9,13 +10,25 @@ const adminController = {
       if (userData.role !== '管理員') newErrorGenerate('帳號不存在！', 404)
       delete userData.password
       const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' }) // 簽發 JWT，效期為 30 天
-      res.json({
+      return res.json({
         status: 'success',
         data: {
           token,
           user: userData
         }
       })
+    } catch (err) {
+      next(err)
+    }
+  },
+  // 管理員能夠刪除貼文
+  deleteTweet: async (req, res, next) => {
+    try {
+      const tweetId = req.params.id
+      const tweet = await Tweet.findByPk(tweetId)
+      if (!tweet) newErrorGenerate('該篇推文不存在', 404)
+      const deletedTweet = await tweet.destroy()
+      return res.json({ status: 'success', data: { deletedTweet } })
     } catch (err) {
       next(err)
     }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const adminController = require('../../controllers/admin-controller')
 const router = express.Router()
+
+// 管理員能夠刪除貼文
+router.delete('/tweets/:id', adminController.deleteTweet)
 
 module.exports = router
