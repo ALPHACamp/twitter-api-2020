ad83de0baae4f8b2910db7df5613361208121385
Ray7808 <yuwang7808@gmail.com>
2023-06-07 00:03:02 +0800

    fix: trying to get user info using post /api/tweets

--- controllers/apis/tweet-controller.js
+++ controllers/apis/tweet-controller.js
@@ -1,16 +1,16 @@
 const { getUser } = require('../../_helpers')
-const { Tweet } = require('../../models')
+// const { Tweet } = require('../../models')
 
 const tweetController = {
   postTweet: async (req, res, next) => {
     try {
-    //   const { description } = req.body
+      // const { description } = req.body
       const user = getUser(req)
       console.log(user)
-      //   const tweet = await Tweet.create({
-      //     UserId: user.id,
-      //     description
-      //   })
+      // const tweet = await Tweet.create({
+      //   UserId: user.id,
+      //   description
+      // })
       return res.json({ status: 'success', data: user })
     } catch (error) {
       next(error)
