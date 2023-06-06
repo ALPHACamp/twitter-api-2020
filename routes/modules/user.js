57f296e6996d7ea94973d658ea9986dcbc4f1965
CHUCHUDAN <daniel870217@gmail.com>
2023-06-06 22:59:32 +0800

    fix: error

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user-controller')
 
-// 查看使用者及其所有推文
+// 查看使用者資料
 router.get('/:id', userController.getUser)
 module.exports = router
