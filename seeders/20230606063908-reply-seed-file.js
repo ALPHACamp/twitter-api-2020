c7bc7ae9d27d2fd379a3ef755fd37cb229f4e356
Chingsan0722 <rory85722@gmail.com>
2023-06-06 17:21:34 +0800

    add reply seed

--- seeders/20230606063908-reply-seed-file.js
+++ seeders/20230606063908-reply-seed-file.js
@@ -1,26 +1,32 @@
-// 'use strict';
+'use strict';
 
-// module.exports = {
-//   up: (queryInterface, Sequelize) => {
-//     /*
-//       Add altering commands here.
-//       Return a promise to correctly handle asynchronicity.
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const tweets = await queryInterface.sequelize.query(
+      'SELECT id FROM Tweets;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const repliesData = []
+      for (let j = 0; j < tweets.length; j++) {
+        for (let i = 1; i < 4; i++) {
+          const replyData = {
+            User_id: users[Math.floor(Math.random() * users.length)].id,
+            Tweet_id: tweets[j].id,
+            comment: `你好棒${i}`,
+            created_at: new Date(),
+            updated_at: new Date()
+          }
+          repliesData.push(replyData)
+        }
+      }
+    await queryInterface.bulkInsert('Replies', repliesData)
+  },
 
-//       Example:
-//       return queryInterface.bulkInsert('People', [{
-//         name: 'John Doe',
-//         isBetaMember: false
-//       }], {});
-//     */
-//   },
-
-//   down: (queryInterface, Sequelize) => {
-//     /*
-//       Add reverting commands here.
-//       Return a promise to correctly handle asynchronicity.
-
-//       Example:
-//       return queryInterface.bulkDelete('People', null, {});
-//     */
-//   }
-// };
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', {})
+  }
+};
