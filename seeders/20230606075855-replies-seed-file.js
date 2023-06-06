9ee5f0562b81a907ab318eb3dcb478bffb3b18c6
CHUCHUDAN <daniel870217@gmail.com>
2023-06-06 17:47:25 +0800

    fix: seeder replys error

--- seeders/20230606075855-replies-seed-file.js
+++ seeders/20230606075855-replies-seed-file.js
@@ -6,7 +6,7 @@ const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const users = await queryInterface.sequelize.query(
-      "SELECT id FROM Users WHERE role = '普通會員';",
+      "SELECT id FROM Users WHERE role = 'user';",
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
     const tweets = await queryInterface.sequelize.query(
