bfdc06beacffec8f895cb1405ac2ca92dbc5e9db
s1030905 <s1030905@outlook.com>
2023-06-07 11:23:07 +0800

    test

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-const passportJWT = require('passport-jwt')
+// const passportJWT = require('passport-jwt')
 // const JWTStrategy = passportJWT.Strategy
 // const ExtractJWT = passportJWT.ExtractJwt
 const bcrypt = require('bcryptjs')
