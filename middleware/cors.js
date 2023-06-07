3f52c7fc8873e8ae546138604e41afb269f55a9b
WeiWayne1030 <wayne10032004@gmail.com>
2023-06-07 14:30:22 +0800

    fix: modify cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,8 +1,8 @@
 const cors =((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '＊');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '＊')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 module.exports = cors
