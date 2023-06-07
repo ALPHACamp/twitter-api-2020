bdca16262e99e1a8cc01a3d61581ce2bd32e047a
WeiWayne1030 <wayne10032004@gmail.com>
2023-06-07 14:26:33 +0800

    fix: modify cors

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,5 +1,5 @@
 const cors =((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '＊');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
