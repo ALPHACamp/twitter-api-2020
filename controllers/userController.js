d015269dadc2d7c63862e08761a166bbf72cc014
Chingsan0722 <rory85722@gmail.com>
2023-06-07 20:39:53 +0800

    modify typo

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,24 +21,24 @@ const userController = {
   },
   signUp: (req, res, next) => {
     if (req.body.password !== req.body.passwordCheck) throw new Error('Password do not match!')
-    if(req.body.name.length > 50) throw new Error('Max length 50')
-      User.findOne({ where: { email: req.body.email } })
-        .then(user => {
-          if (user) throw new Error('Email already exists!')
-          return bcrypt.hash(req.body.password, 10)
-        })
-        .then(hash => User.create({
-          account: req.body.account,
-          name: req.body.name,
-          email: req.body.email,
-          password: hash
-        }))
-        .then(data => {
+    if (req.body.name.length > 50) throw new Error('Max length 50')
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
+      .then(hash => User.create({
+        account: req.body.account,
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      }))
+      .then(data => {
         userData = data.toJSON()
         delete userData.password
-        res.json({ status: 'success', data:userData })
-        })
-        .catch(err => next(err))
+        res.json({ status: 'success', data: userData })
+      })
+      .catch(err => next(err))
   },
   getUser: (req, res, next) => {
     const userId = req.params.id
