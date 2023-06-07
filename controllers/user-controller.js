52b4417ebfc6b9b57168a2e9574823aefe74d42c
jasmine <jasmine12122005@yahoo.com.tw>
2023-06-07 22:56:20 +0800

    feat: add postTweetUnlike function, adjust routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -92,7 +92,8 @@ const userController = {
                     liked.tweet_id = tweet.id
                 )`), 'likeCount'
             ]
-          ]
+          ],
+          exclude: ['UserId']
         },
         raw: true,
         nest: true
@@ -252,9 +253,33 @@ const userController = {
         })
       })
       .then((newLike) => {
-        return res.status(200).json(newLike)
+        return res.status(200).json({ status: 'success', message: 'Like succeed', newLike })
       })
       .catch((err) => next(err))
+  },
+
+  postTweetUnlike: (req, res, next) => {
+    const TweetId = req.params.id
+    return Promise.all([
+      Tweet.findByPk(TweetId),
+      Like.findOne({
+        where: {
+          TweetId
+        }
+      })
+    ])
+      .then(([tweet, like]) => {
+        if (!tweet) throw new Error("Tweet doesn't exist!")
+        if (!like) throw new Error("You haven't liked this tweet!")
+
+        // keep the deleted data
+        const deletedLike = like.toJSON()
+        return like.destroy()
+          .then(() => {
+            return res.status(200).json({ status: 'success', message: 'Unlike succeed', deletedLike })
+          })
+      })
+      .catch(err => next(err))
   }
 };
 
--- routes/index.js
+++ routes/index.js
@@ -17,17 +17,19 @@ router.post("/api/users/signin", userController.signIn);
 router.post("/api/users", userController.signUp);
 // user tweets
 router.get(
-  "/api/users/:id/tweets", userController.getUserTweets);
+  "/api/users/:id/tweets", authenticated, authenticatedUser, userController.getUserTweets);
 router.get(
-  "/api/users/:id/replied_tweets", userController.getUserRepliedTweets);
+  "/api/users/:id/replied_tweets", authenticated, authenticatedUser, userController.getUserRepliedTweets);
 router.get(
-  "/api/users/:id/likes", userController.getUserLikes);
+  "/api/users/:id/likes", authenticated, authenticatedUser, userController.getUserLikes);
 router.post(
   "/api/tweets/:id/like", authenticated, authenticatedUser, userController.postTweetLike);
+router.post(
+  "/api/tweets/:id/unlike", authenticated, authenticatedUser, userController.postTweetUnlike);
 router.get(
-  "/api/tweets/:tweet_id", userController.getTweet);
+  "/api/tweets/:tweet_id", authenticated, authenticatedUser, userController.getTweet);
 router.get(
-  "/api/tweets", userController.getTweets);
+  "/api/tweets", authenticated, authenticatedUser, userController.getTweets);
 router.post(
   "/api/tweets", authenticated, authenticatedUser, userController.postTweets);
 // router.get(
