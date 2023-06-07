18ce93e7af8cbffc308403dcf1e6390bc5fd9283
Chingsan0722 <rory85722@gmail.com>
2023-06-07 10:49:04 +0800

    update to use clearDB

--- app.js
+++ app.js
@@ -23,5 +23,5 @@ function authenticated(req, res, next) {
 
 app.use('/api',router)
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.info(`Example app listening on port ${port}!`))
 module.exports = app
