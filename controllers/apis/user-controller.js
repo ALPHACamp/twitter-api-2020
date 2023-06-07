f1287ac7ad0184911f4ea6ff168c28455ae6541d
Lanways <mcbluedd@gmail.com>
2023-06-07 21:52:42 +0800

    feat: init project

---
+++ controllers/apis/user-controller.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcryptjs')
+const { User } = require('../../models')
+const jwt = require('jsonwebtoken')
+
+const userController = {
+  signIn: (req, res, next) => {
+    try {
+      const userData = req.user.toJSON()
+      delete userData.password
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch (err) {
+      res.status(500).json({ status: 'error', error: err.message })
+    }
+  },
+  signUp: (req, res, next) => {
+    new Promise((resolve, reject) => {
+      if (req.body.password != req.body.passwordCheck) reject(new Error('Password do not match'))
+      resolve()
+    })
+
+      .then(() => User.findOne({ where: { email: req.body.email } }))
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
+      .then(hash => {
+        return User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: hash
+        })
+      })
+      .then((user) => {
+        user = user.toJSON()
+        delete user.password
+        res.json({ status: 'success', data: { user } })
+      })
+      .catch(err => {
+        res.status(500).json({ status: 'error', error: err.message })
+      })
+  },
+}
+
+module.exports = userController
--- models/user.js
+++ models/user.js
@@ -1,8 +1,35 @@
 'use strict';
+const {
+  Model
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
+  class User extends Model {
+    static associate(models) {
+      User.hasMany(models.Reply, { foreignKey: 'userId' })
+      User.hasMany(models.Tweet, { foreignKey: 'userId' })
+      User.hasMany(models.Like, { foreignKey: 'userId' })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
+    }
   };
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    role: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'User'
+  });
   return User;
 };
