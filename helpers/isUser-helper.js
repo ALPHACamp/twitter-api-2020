e62bcd1ff3e29814020cc368a1792d1997318ae3
CHUCHUDAN <daniel870217@gmail.com>
2023-06-07 10:44:21 +0800

    fix: error

--- helpers/isUser-helper.js
+++ helpers/isUser-helper.js
@@ -1,7 +1,7 @@
 const helpers = require('../_helpers')
 module.exports = {
   isUser: req => {
-    const signInUserId = helpers.getUser(req).id
+    const signInUserId = helpers.getUser(req)?.id
     const userId = Number(req.params.id)
     const isSelfUser = signInUserId === userId
     return isSelfUser
