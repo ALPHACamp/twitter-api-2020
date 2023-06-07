c33e5661226aa70bd7baf8010c070bf3f3754355
YuZheLin1987 <billlinxx@gmail.com>
2023-06-08 00:19:29 +0800

    feat: add user model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,32 @@
-'use strict';
+'use strict'
+const {
+  Model
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-  }, {});
-  User.associate = function(models) {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
   };
-  return User;
-};
+  User.init({
+    account: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    banner: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true
+  })
+  return User
+}
