4c5662de619cd868a5f4c7890d383a616dde8097
Chingsan0722 <rory85722@gmail.com>
2023-06-08 11:29:42 +0800

    modify router

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,8 @@ const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
 const userController = require('../controllers/userController')
 
 router.use('/admin', authenticated, authenticatedAdmin, admin)
-router.post('/signup', userController.signUp)
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn) // 告訴passport不用session了 改用token驗證
+router.post('/users', userController.signUp)
 router.put('/users/:id', authenticated, userController.putUser)
 router.get('/users/top',authenticated, userController.getTopUsers)
 router.get('/users/:id',authenticated, userController.getUser)
