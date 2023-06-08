83b5ac0a26eec25c509cb68fe9d83f8c154da48c
Banwind <a84012807@gmail.com>
2023-06-09 01:42:56 +0800

    feat: featureReply

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,17 @@ const { apiErrorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
 const userController = require('../controllers/userController')
 const tweetController = require('../controllers/tweetController')
+const replyController = require('../controllers/replyController')
 
 router.use('/admin', authenticatedAdmin, admin)
 
 // 告訴passport不用session了 改用token驗證
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+
+router.get('/tweets/:id/replies', authenticated, replyController.getReplies)
+router.post('/tweets/:id/replies', authenticated, replyController.postReply)
+router.put('/tweets/:tweet_id/replies/:reply_id', authenticated, replyController.putReply)
+router.delete('/tweets/:tweet_id/replies/:reply_id', authenticated, replyController.deleteReply)
 
 router.get('/users/:id/tweets', authenticated, tweetController.getTweets)
 router.get('/tweets/:id', authenticated, tweetController.getTweet)
