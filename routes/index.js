9e77797f5e9325748115f0bb28eb3e76ccfee4b1
elviawu <elviawu@gmail.com>
2023-06-08 16:42:00 +0800

    feat: add user and admin routes

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
+// require needed modules
 const express = require('express')
 const router = express.Router()
+// require routes modules
+const admin = require('./modules/admin')
+const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 const { apiErrorHandler } = require('../middleware/error-handler')
-
+// use router
+router.use('/api/admin', admin)
+router.use('/api/users', users)
 router.use('/api/tweets', tweets)
 router.get('/', (req, res) => res.redirect('/tweets'))
 router.use('/', apiErrorHandler)
 
 module.exports = router
-=======
-// require needed modules
-const express = require('express')
-const router = express.Router()
-const { apiErrorHandler } = require('../middleware/error-handler')
-
-// require routes modules
-const admin = require('./modules/admin')
-const users = require('./modules/users')
-
-// use router
-router.use('/api/admin', admin)
-router.use('/api/users', users)
-router.use('/', apiErrorHandler)
-
-module.exports = router
->>>>>>> 525e157e1eb46f1a8596ba611a8cca351ffcc3d4
