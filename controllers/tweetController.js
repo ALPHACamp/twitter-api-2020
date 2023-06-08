7d2f9d9a7ee00825ade067be26cb0a295b662c2e
Chingsan0722 <rory85722@gmail.com>
2023-06-08 21:34:00 +0800

    feat: add put &  delete tweet

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -23,7 +23,9 @@ const tweetController = {
     const id = req.params.id
     Tweet.findByPk(
       id, {
-        include: [{ model: User, attributes: ['account', 'name', 'avatar'] }]
+        include: [{
+          model: User, attributes: ['account', 'name', 'avatar']
+        }]
       })
       .then(data => {
         if (!data) throw new Error('Can not find this tweet!')
@@ -37,6 +39,7 @@ const tweetController = {
   postTweet: (req, res, next) => {
     const { description, likable, commendable } = req.body
     if (!description) throw new Error('Description can not be empty!')
+    if (description.length > 140) throw new Error('Max length 140.')
     const id = req.user.id
     Tweet.create({
       UserId: id,
@@ -45,18 +48,44 @@ const tweetController = {
       commendable: commendable || '1'
     })
       .then(data => {
-        if (!data) throw new Error('Can not create tweet!')
+        if (!data) throw new Error('Tweet not found!')
         res.json({ status: 'success', data })
       })
       .catch(err => next(err))
   },
   putTweet: (req, res, next) => {
-
+    const { description, likable, commendable } = req.body
+    const id = req.params.id
+    if (!description) throw new Error('Description can not be empty!')
+    if (description.length > 140) throw new Error('Max length 140.')
+    Tweet.findByPk(id)
+      .then(tweet => {
+        if (!tweet) throw new Error('Tweet not found!')
+        return tweet.update({
+          description,
+          likable: likable || '1',
+          commendable: commendable || '1'
+        })
+      })
+      .then(data => {
+        console.log(data)
+        if (!data) throw new Error('Update failed!')
+        res.json({ status: 'success', data })
+      })
+      .catch(err => next(err))
   },
   deleteTweet: (req, res, next) => {
-
+    const id = req.params.id
+    Tweet.findByPk(id)
+      .then(tweet => {
+        if (!tweet) throw new Error('Tweet not found')
+        tweet.destroy()
+      })
+      .then(() => {
+        res.json({ status: 'success', message: 'Delete success' })
+      })
+      .catch(err => next(err))
   }
-
 }
 
 module.exports = tweetController
