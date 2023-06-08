59c2339f2eb02d6aa9b2f92d71bbc14e03a0e578
Chingsan0722 <rory85722@gmail.com>
2023-06-08 10:14:11 +0800

    feat: add put, delete, top users API & passport association

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -3,6 +3,7 @@ const helpers = require('../_helpers')
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+    req.user = user
     next()
   })(req, res, next)
 }
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ const userController = require('../controllers/userController')
 router.use('/admin', authenticated, authenticatedAdmin, admin)
 router.post('/signup', userController.signUp)
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn) // 告訴passport不用session了 改用token驗證
+router.put('/users/:id', authenticated, userController.putUser)
+router.get('/users/top',authenticated, userController.getTopUsers)
 router.get('/users/:id',authenticated, userController.getUser)
+router.delete('/users/:id', authenticated, userController.deleteUser)
 router.use('/', apiErrorHandler)
 module.exports = router
