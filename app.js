15f84a335e8f57c3e1280702616c733ee6b4897b
JimmyLin <disillusioned2077@gmail.com>
2023-06-08 16:37:49 +0800

    feat: depoly heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const helpers = require('./_helpers');
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const passport = require('./config/passport')
 const routes = require('./routes')
 
