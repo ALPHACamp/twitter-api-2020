2f404e2968f1bfa6cb411dad46b9f7e421ba98e3
aking4511 <aking4511@gmail.com>
2023-06-08 15:22:48 +0800

    feat: edit passport

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,11 +2,13 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../../config/passport')
+const { authenticatedAdmin } = require('../../middleware/api-auth')
 
 // require controller
 const adminController = require('../../controllers/admin-controller')
 
 // set router
 router.post('/login', passport.authenticate('local', { session: false }), adminController.login)
+router.get('/users', authenticatedAdmin)
 
 module.exports = router
