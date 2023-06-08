1d959cf5c2d256ae9c2158f30f8bd27fec197f2e
jasmine <jasmine12122005@yahoo.com.tw>
2023-06-08 15:13:10 +0800

    feat: add admin auth routes, add admin signIn function

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,33 @@
 const { User, Tweet } = require('../models')
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 const adminController = {
+  signIn: (req, res, next) => {
+    const { account, password } = req.body
+    if (!account || !password)
+      throw new Error("Account and password are required")
+    return User.findOne({
+      where: { account }
+    })
+      .then((user) => {
+        if (!user) throw new Error("Admin 權限不存在")
+        if (user.role === "user") throw new Error("Admin 權限不存在")
+        if (!bcrypt.compareSync(password, user.password))
+          throw new Error("密碼不相符")
+        const userData = user.toJSON()
+        delete userData.password
+        const token = jwt.sign(userData, process.env.JWT_SECRET, {
+          expiresIn: "30d",
+        })
+        return res.status(200).json({
+          token,
+          user: userData,
+        })
+      })
+      .catch((err) => next(err));
+  },
+
   // function still need to be modified
   getUsers: (req, res, next) => {
     User.findAll({
