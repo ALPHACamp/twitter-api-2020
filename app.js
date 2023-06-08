bc2b4234ae6b4148a5c997b3fc00f1e962a5baed
klu0926 <lukuoyu@gmail.com>
2023-06-08 12:57:06 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const express = require('express')
 const routes = require('./routes')
 const methodOverride = require('method-override')
 const passport = require('./config/passport')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,6 +16,15 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(passport.initialize())
 app.use(methodOverride('_method'))
+
+// cors
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+  })
+)
 
 // routes
 app.get('/', (req, res) => { res.send('Welcome to the real world!') })
