b926e04aecc158a40addf0b297d5ed08677d788c
azx79115 <azx79115@gmail.com>
2023-06-08 12:32:02 +0800

    登入驗證功能

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const userController = require('../controller/user-controller')
+const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
+const { apiErrorHandler } = require('../middleware/error-handler')
 
+router.post('/api/users/login', passport.authenticate('local', { session: false }), userController.signIn)
 router.post('/api/users/signup', userController.signUp)
+router.use('/', apiErrorHandler)
 
 module.exports = router
