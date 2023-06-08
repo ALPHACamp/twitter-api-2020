eba64131d2bc8c87434a62d9b942f1288cec1939
playcsgo <sam@plum.wine>
2023-06-08 16:17:13 +0800

    add PORT at app.js-2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 // const helpers = require('./_helpers')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const apis = require('./routes/apis')
 const methodOverride = require('method-override')
 
