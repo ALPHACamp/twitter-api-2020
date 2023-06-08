c6f08b8dcfa0c1ba3486fbb47b9be9ca0b9dc54c
Ray7808 <yuwang7808@gmail.com>
2023-06-08 22:20:41 +0800

    feat add reply GET /api/tweets/:tweet_id/replies and POST /api/tweets/:tweet_id/replies

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -18,6 +18,10 @@ router.post('/login', passport.authenticate('local', { session: false }), userCo
 // 有關tweet的routes
 router.post('/tweets', authenticated, tweetController.postTweet)
 router.get('/tweets', authenticated, tweetController.getTweets)
+router.post('/tweets/:tweet_id/replies', authenticated, tweetController.postReply)
+router.get('/tweets/:tweet_id/replies', authenticated, tweetController.getReply)
+// router.post('/tweets/:tweet_id/like', authenticated, tweetController.postReply)
+// router.get('/tweets/:tweet_id/unlike', authenticated, tweetController.getReply)
 router.get('/tweets/:tweet_id', authenticated, tweetController.getTweet)
 
 // 有關user的routes
