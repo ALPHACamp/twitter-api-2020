62b447a95cc86fbccf7009e3f2fcd144253bc2cb
Chingsan0722 <rory85722@gmail.com>
2023-06-09 17:38:44 +0800

    modify passport

--- config/passport.js
+++ config/passport.js
@@ -5,6 +5,8 @@ const bcrypt = require('bcryptjs')
 const { User } = require('../models')
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWWT = passportJWT.ExtractJwt
+
+// 登入
 passport.use(new LocalStrategy(
   {
     usernameField: 'account',
@@ -23,7 +25,7 @@ passport.use(new LocalStrategy(
       })
   }
 ))
-
+// 註冊
 const jwtOptions = {
   jwtFromRequest: ExtractJWWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET
@@ -31,8 +33,8 @@ const jwtOptions = {
 passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
   User.findByPk(jwtPayload.id, {
     include: [
-      { model: User, as: 'Followers' },
-      { model: User, as: 'Followings' }
+      { model: User, as: 'Followers', attributes: ['id'] },
+      { model: User, as: 'Followings', attributes: ['id'] }
     ]
   })
     .then(user => {
@@ -45,13 +47,9 @@ passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
 passport.deserializeUser((id, cb) => {
-  return User.findByPk(id, {
-    include: [
-      { model: User, as: 'Followers' },
-      { model: User, as: 'Followings' }
-    ]
-  })
+  return User.findByPk(id)
     .then(user => {
+      console.log('deserialize', user)
       cb(null, user.toJSON())
     })
     .catch(err => cb(err))
