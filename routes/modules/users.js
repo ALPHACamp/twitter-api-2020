fd8f6be439643b55665b6325495bf3cfb7175ddb
aking4511 <aking4511@gmail.com>
2023-06-09 23:11:25 +0800

    feat: add 'delete tweet' feature to admin

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,7 +2,6 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../../config/passport')
-// 之後要加入multer,處理使用者上傳之圖片
 
 // require controller
 const userController = require('../../controllers/user-controller')
