a0375618c6a23f689afebdd821678835bba7053a
Edward Kuan <edward880814@gmail.com>
2023-06-09 12:32:03 +0800

    feat: add jwt in passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,18 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-const passportJWT = require('passport-jwt')
+const db = require('../models')
+const User = db.User
+const Tweet = db.Tweet
 const bcrypt = require('bcryptjs')
-const { User } = require('../models')
-const JWTStrategy = passportJWT.Strategy
+// JWT set up
+const passportJWT = require('passport-jwt')
 const ExtractJWT = passportJWT.ExtractJwt
+const JWTStrategy = passportJWT.Strategy
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
 
 passport.use(new LocalStrategy(
   // customize user field
@@ -38,16 +46,11 @@ passport.use(new LocalStrategy(
   }
 ))
 
-const jwtOptions = {
-  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET
-}
-
 passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
   User.findByPk(jwtPayload.id, {
     include: [
-      { model: Restaurant, as: 'FavoritedRestaurants' },
-      { model: Restaurant, as: 'LikedRestaurants' },
+      { model: Tweet, as: 'RepliedTweets' },
+      { model: Tweet, as: 'LikedTweets' },
       { model: User, as: 'Followers' },
       { model: User, as: 'Followings' }
     ]
