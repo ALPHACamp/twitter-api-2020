79473072aa9efc8623b198c3cc9c0c4cd90bce80
Chingsan0722 <rory85722@gmail.com>
2023-06-09 11:25:33 +0800

    feat: tweet api completed

--- _helpers.js
+++ _helpers.js
@@ -22,7 +22,27 @@ function getLastUpdated (data) {
   }
   data.lastUpdated = lastUpdated
 }
+function getLastUpd (data) {
+  const now = new Date()
+  const updatedDiff = now - new Date(data.updatedAt)
+  let lastUpdated
+  const diffInHours = updatedDiff / 1000 / 60 / 60
+  if (diffInHours < 1) {
+    lastUpdated = `${Math.round(diffInHours * 60)} 分鐘前`
+  } else if (diffInHours < 24) {
+    lastUpdated = `${Math.round(diffInHours)} 小時前`
+  } else if (diffInHours < 720) {
+    lastUpdated = `${Math.round(diffInHours / 24)} 天前`
+  } else if (diffInHours < 1440) {
+    lastUpdated = '一個月前'
+  } else if (diffInHours < 8640) {
+    lastUpdated = `${Math.round(diffInHours / 24 / 30)} 個月前`
+  } else {
+    lastUpdated = '一年前'
+  }
+  return lastUpdated
+}
 
 module.exports = {
-  getUser, getLastUpdated
+  getUser, getLastUpdated, getLastUpd
 }
