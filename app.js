3ac9e7ab493357ff644753cbeec734953fc050b9
JimmyLin <disillusioned2077@gmail.com>
2023-06-10 00:02:21 +0800

    feat: add heroku API feature, tweetsApi

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const helpers = require('./_helpers');
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 const passport = require('./config/passport')
 const routes = require('./routes')
 
+
 // use helpers.getUser(req) to replace req.user
 function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 };
+
+app.use(cors())
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,12 +1,22 @@
 const passport = require('../config/passport')
 
-const authenticated = passport.authenticate('jwt', { session: false })
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err) return res.status(401).json({ status: 'error', message: 'err' })
+    if (!user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+
+
+    req.user = user
+    next()
+  })(req, res, next)
+}
 
 const authenticatedAdmin = (req, res, next) => { // Admin認證方式
   const userRole = req.user.role
-  if (req.user && userRole === 'admin' ) return next()
+  if (req.user && userRole === 'admin') return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
+
 module.exports = {
   authenticated,
   authenticatedAdmin
