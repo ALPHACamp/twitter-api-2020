beaa070b7e02c8141f557484e07882dda940f571
Banwind <a84012807@gmail.com>
2023-06-09 14:09:34 +0800

    feat: featureFollow

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,21 @@ const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
 const userController = require('../controllers/userController')
 const tweetController = require('../controllers/tweetController')
 const replyController = require('../controllers/replyController')
+const followController = require('../controllers/followController')
 
 router.use('/admin', authenticatedAdmin, admin)
 
 // 告訴passport不用session了 改用token驗證
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+
+router.post('/tweets/:id/like', authenticated, followController.addLike)
+router.delete('/tweets/:id/unlike', authenticated, followController.removeLike)
+router.post('/followships', authenticated, followController.addFollowing)
+router.delete('/followships/:id', authenticated, followController.removeFollowing)
+router.get('/users/:id/likes', authenticated, followController.getLikes)
+router.get('/users/:id/followers', authenticated, followController.getFollowers)
+router.get('/users/:id/followings', authenticated, followController.getFollowings)
+router.get('/users/:id/follow_counts', authenticated, followController.getFollowCounts)
 
 router.get('/tweets/:id/replies', authenticated, replyController.getReplies)
 router.post('/tweets/:id/replies', authenticated, replyController.postReply)
