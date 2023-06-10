85c9786708c511ce058f1bc7360a1c746059b7de
azx79115 <azx79115@gmail.com>
2023-06-10 12:12:45 +0800

    新增 回復&瀏覽單一tweet所有回復功能

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
 const userController = require('../controller/user-controller')
+const tweetController = require('../controller/tweet-controller')
 const { authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/api-auth')
 const { apiErrorHandler } = require('../middleware/error-handler')
 
-// Tweets
+// Replies
+router.post('/api/tweets/:tweet_id/replies', authenticated, tweetController.postReply)
+router.get('/api/tweets/:tweet_id/replies', authenticated, tweetController.getReplies)
 
 // Users
 router.get('/api/users/:id/tweets', authenticated, userController.getUserTweets)
@@ -14,7 +17,6 @@ router.get('/api/users/:id/likes', authenticated, userController.getLikedTweets)
 router.get('/api/users/:id/followings', authenticated, userController.getFollowings)
 router.get('/api/users/:id/followers', authenticated, userController.getFollowers)
 router.put('/api/users/:id', authenticated, userController.putUser)
-
 router.get('/api/users/:id', authenticated, userController.getUser)
 // 登入& 註冊
 router.post('/api/users/login', passport.authenticate('local', { session: false }), userController.signIn)
