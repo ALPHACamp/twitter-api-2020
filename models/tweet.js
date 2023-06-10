18a97002b4a1a8dfd28fcd201d1f8cb36faa3f82
YuZheLin1987 <billlinxx@gmail.com>
2023-06-11 01:24:26 +0800

    feat: add reply seed data

--- models/tweet.js
+++ models/tweet.js
@@ -16,7 +16,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Tweet.init({
-    userId: DataTypes.INTEGER,
+    UserId: DataTypes.INTEGER,
     description: DataTypes.TEXT
   }, {
     sequelize,
