472456908fa8cd1337832ae63bb97a837d9c8f3f
Kevin_L <54697436+av124773@users.noreply.github.com>
2023-06-11 00:30:01 +0800

    feat: add env.PORT (#9)

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const methodOverride = require('method-override')
 const passport = require('./config/passport')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // middleware
 app.use(bodyParser.json())
