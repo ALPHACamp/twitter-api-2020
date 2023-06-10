76c65f8e4dee6c2bc07753a2fbc31e7e1274ffcb
Chingsan0722 <rory85722@gmail.com>
2023-06-10 14:02:14 +0800

    fix signup(post user)

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,21 +27,25 @@ const userController = {
   signUp: (req, res, next) => {
     if (req.body.password !== req.body.passwordCheck) throw new Error('Password do not match!')
     if (req.body.name.length > 50) throw new Error('Max length 50')
-    User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10)
-      })
-      .then(hash => User.create({
+    return Promise.all([
+      User.findOne({ where: { email: req.body.email } }),
+      User.findOne({ where: { account: req.body.account } })
+    ])
+    .then(([emailCheck, accountCheck]) => {
+      if(emailCheck) throw new Error('Email already exists!')
+      if(accountCheck) throw new Error('Account already exists!')
+      return bcrypt.hash(req.body.password, 10)
+    })
+    .then(hash => User.create({
         account: req.body.account,
         name: req.body.name,
         email: req.body.email,
         password: hash
-      }))
+    }))
       .then(data => {
         const userData = data.toJSON()
         delete userData.password
-        res.json({ status: 'success', data: userData })
+        res.json({ status: 'success', message: 'Create success' })
       })
       .catch(err => next(err))
   },
