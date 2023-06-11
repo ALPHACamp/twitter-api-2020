7b0e5140ae608aabb3c5a09c1169d53959a067eb
KenYuChang <justin0915justin0915@gmail.com>
2023-06-11 22:42:45 +0800

    change for travis

--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,24 @@
-const passport = require("passport");
-const passportJWT = require("passport-jwt");
-const JWTStrategy = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
-const { User } = require("../models");
+const passport = require('passport')
+const passportJWT = require('passport-jwt')
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+const { User } = require('../models')
 
 const jwtOptions = {
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.JWT_SECRET,
-};
+  secretOrKey: process.env.JWT_SECRET || 'password', //for travis
+}
 
 passport.use(
   new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
     User.findByPk(jwtPayload.id, {
       include: [
-        { model: User, as: "Followers" },
-        { model: User, as: "Followings" },
+        { model: User, as: 'Followers' },
+        { model: User, as: 'Followings' },
       ],
     })
       .then((user) => cb(null, user))
-      .catch((err) => cb(err));
+      .catch((err) => cb(err))
   })
-);
-module.exports = passport;
+)
+module.exports = passport
