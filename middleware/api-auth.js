2bde58a0a7f162f6dfe5761006f8bab968983cc1
jasmine <jasmine12122005@yahoo.com.tw>
2023-06-12 00:23:04 +0800

    merge

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,6 +1,5 @@
 const passport = require("../config/passport");
 const helpers = require("../_helpers");
-const { User } = require("../models");
 module.exports = {
   //登入驗證
   authenticated: (req, res, next) => {
@@ -35,28 +34,4 @@ module.exports = {
       message: "Permission denied, it's only for admin.",
     });
   },
-
-  checkPermission: (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-      return res.status(401).json({ message: 'Invalid authorization header' });
-    }
-    const authToken = authHeader.split(' ')[1];
-    try {s
-      // verify authtoken
-      const decodedToken = jwt.verify(authToken, 'password');
-
-      // get user id from decoded token
-      const userId = decodedToken.userId;
-      const user = User.find((user) => user.id === userId);
-      if (!user) {
-        return res.status(403).json({ message: 'Insufficient permission' });
-      }
-      // verified user
-      req.user = user;
-      next();
-    } catch (err) {
-      return res.status(401).json({ message: 'Invalid token' });
-    }
-  }
 };
