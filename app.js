ff0871c2891c290f40d6dc01eb0e35ea98a64632
playcsgo <sam@plum.wine>
2023-06-12 00:11:43 +0800

    cors-13

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
+
 app.use(express.json())
 
 // cors
@@ -22,7 +22,7 @@ app.use(
     optionsSuccessStatus: 204
   })
 )
-
+app.use(methodOverride('_method'))
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
