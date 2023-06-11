ab1542aa93f5bac9d94345597419d3c2bf67d643
playcsgo <sam@plum.wine>
2023-06-11 23:04:35 +0800

    cors-7

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 
-app.use(cors())
+app.use(
+  cors({
+    origin: 'https://leemengyun.github.io'
+  })
+)
 
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
