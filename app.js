bf6efca2f88958e0dd5be90690070c421892c3dc
playcsgo <sam@plum.wine>
2023-06-11 22:53:10 +0800

    cors-7

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use(methodOverride('_method'))
+// app.use(methodOverride('_method'))
 
 app.use(cors())
 
