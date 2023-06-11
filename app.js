62c5ca0ec1510e1c6e4f6ba1ddc1c6e6a2307112
playcsgo <sam@plum.wine>
2023-06-12 02:43:21 +0800

    cors-20

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const routes = require('./routes')
-const cors = require('cors')
+// const cors = require('cors')
 const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,20 +13,20 @@ app.use(methodOverride('_method'))
 app.use(express.json())
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://leemengyun.github.io')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
   res.header('Access-Control-Allow-Headers', '*')
   res.header('Access-Control-Allow-Credentials', true)
   next()
 })
 
-app.use(cors({
-  origin: 'https://leemengyun.github.io',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: '*',
-  preflightContinue: true,
-  credentials: true
-}))
+// app.use(cors({
+//   origin: 'https://leemengyun.github.io',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//   allowedHeaders: '*',
+//   preflightContinue: true,
+//   credentials: true
+// }))
 
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
