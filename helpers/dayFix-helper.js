0a682a855dcddad683461d9ba0ea0a22f93c24ed
CHUCHUDAN <daniel870217@gmail.com>
2023-06-11 18:24:37 +0800

    revise middleware dayFix

--- helpers/dayFix-helper.js
+++ helpers/dayFix-helper.js
@@ -1,6 +1,8 @@
 const dayjs = require('dayjs')
 const relativeTime = require('dayjs/plugin/relativeTime')
+require('dayjs/locale/zh-tw')
 dayjs.extend(relativeTime)
+dayjs.locale('zh-tw')
 
 module.exports = {
   currentYear: () => dayjs().year(),
