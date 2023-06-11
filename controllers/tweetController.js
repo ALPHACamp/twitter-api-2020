3cb933c8389acbbf1700bcdaa4171bc7b7a66297
Anna0118 <annachu0988@gmail.com>
2023-06-11 16:44:57 +0800

    refactor: reply

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -156,21 +156,54 @@ const tweetController = {
   },
   getReplies: async (req, res, next) => {
     try {
-      const { tweet_id: tweetId } = req.params
-      const tweet = await Tweet.findByPk(tweetId)
-      if (!tweet) {
-        return res.status(404).json({ error: 'Tweet not found!' })
-      }
+      const { tweet_id: tweetId } = req.params;
+      // const tweet = await Tweet.findByPk(tweetId);
+      // if (!tweet) {
+      //   return res.status(404).json({ error: "Tweet not found!" });
+      // }
 
-      const replies = await Reply.findAll({
-        where: { TweetId: tweetId },
-        order: [['createdAt', 'DESC']],
-        attributes: [['id','replyID'], ['comment'], ['UserId', 'replyUserId'], ['createdAt', 'replyCreatedAt']],
+      // const replies = await Reply.findAll({
+      //   where: { TweetId: tweetId },
+      //   order: [["createdAt", "DESC"]],
+      //   raw: true,
+      //   nest: true,
+      // });
+
+      // return res.status(200).json(replies);
+
+      const tweet = await Tweet.findByPk(tweetId, {
+        attributes: ["id", "description", "createdAt"],
+        include: [
+          {
+            model: User,
+            attributes: ["id", "name", "account", "avatar"],
+          },
+        ],
         raw: true,
-        // nest: true
-      })
+        nest: true,
+      });
+      if (!tweet) {
+        return res.status(404).json({ error: "Tweet not found!" });
+      }
+      let replies = await Reply.findAll({
+        where: { TweetId: tweetId },
+        order: [["createdAt", "DESC"]],
+        attributes: [
+          ["id", "replyId"],
+          ["comment", "comment"],
+          ["UserId", "replyUserId"],
+          ["createdAt", "replyCreatedAt"],
+        ],
+        raw: true,
+        nest: true,
+      });
+      replies = replies.map((reply) => ({
+        ...reply,
+        tweet: tweet,
+      }));
+      return res.status(200).json(replies);
+      // return res.status(200).json({ tweet, replies });
 
-      return res.status(200).json(replies)
     } catch (err) {
       next(err)
     }
