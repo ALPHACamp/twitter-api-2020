247c8285ee04842d7c58706582c7842b4a5d096c
playcsgo <sam@plum.wine>
2023-06-11 23:57:39 +0800

    cors-11

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const routes = require('./routes')
-// const cors = require('cors')
+const cors = require('cors')
 const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,21 +12,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.json())
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT")
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type")
-  next()
-})
-
 // cors
-// app.use(
-//   cors({
-//     origin: '*',
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
-//   })
-// )
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+  })
+)
 
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
