1ec6b5230f376f6c40120c27c5f690becdbfbbe0
playcsgo <sam@plum.wine>
2023-06-12 02:09:59 +0800

    cors-16

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const routes = require('./routes')
 const cors = require('cors')
-
+const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: '*',
+  methods: '*',
+  allowedHeaders: '*',
+  credentials: true
+}))
+
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
