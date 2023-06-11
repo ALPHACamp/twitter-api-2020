a6e8a62aff979c0a2b4964e2b2d89e7e19f0f26c
playcsgo <sam@plum.wine>
2023-06-12 00:06:01 +0800

    cors-12

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ app.use(
   cors({
     origin: '*',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   })
 )
 
