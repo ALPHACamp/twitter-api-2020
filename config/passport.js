d0e9b57f8f467c19ba7277869ea1e6eac398a018
larrytherealboy <abclarryabclarry@gmail.com>
2023-06-11 14:48:51 +0800

    update: controller turn to async/await function

--- config/passport.js
+++ config/passport.js
@@ -19,8 +19,8 @@ passport.use(new LocalStrategy(
           return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
         }
         bcrypt.compare(password, user.password)
-          .then(res => {
-            if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          .then(result => {
+            if (!result) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
             return cb(null, user)
           })
       })
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,15 +3,15 @@ const jwt = require('jsonwebtoken')
 const userServices = require('../services/user-services')
 
 const userController = {
-  signUp: (req, res, next) => {
-    userServices.signUp(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  signUp: async (req, res, next) => {
+    await userServices.signUp(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  signIn: (req, res, next) => {
+  signIn: async (req, res, next) => {
     const userData = req.user.toJSON()
     delete userData.password
     try {
       const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' }) // 簽發 JWT，效期為 30 天
-      res.json({
+      await res.json({
         status: 'success',
         data: {
           token,
@@ -22,44 +22,44 @@ const userController = {
       next(err)
     }
   },
-  getUser: (req, res, next) => {
-    userServices.getUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  getUser: async (req, res, next) => {
+    await userServices.getUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  getUserTweets: (req, res, next) => {
-    userServices.getUserTweets(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  getUserTweets: async (req, res, next) => {
+    await userServices.getUserTweets(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  getUserRepliedTweets: (req, res, next) => {
-    userServices.getUserRepliedTweets(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  getUserRepliedTweets: async (req, res, next) => {
+    await userServices.getUserRepliedTweets(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  getUserLikes: (req, res, next) => {
-    userServices.getUserLikes(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  getUserLikes: async (req, res, next) => {
+    await userServices.getUserLikes(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  getUserFollowings: (req, res, next) => {
-    userServices.getUserFollowings(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  getUserFollowings: async (req, res, next) => {
+    await userServices.getUserFollowings(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  getUserFollowers: (req, res, next) => {
-    userServices.getUserFollowers(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  getUserFollowers: async (req, res, next) => {
+    await userServices.getUserFollowers(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  editUser: (req, res, next) => {
-    userServices.editUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  editUser: async (req, res, next) => {
+    await userServices.editUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  putUser: (req, res, next) => {
-    userServices.putUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  putUser: async (req, res, next) => {
+    await userServices.putUser(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  getTopUsers: (req, res, next) => {
-    userServices.getTopUsers(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  getTopUsers: async (req, res, next) => {
+    await userServices.getTopUsers(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  addFollowing: (req, res, next) => {
-    userServices.addFollowing(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  addFollowing: async (req, res, next) => {
+    await userServices.addFollowing(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  removeFollowing: (req, res, next) => {
-    userServices.removeFollowing(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  removeFollowing: async (req, res, next) => {
+   await userServices.removeFollowing(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  addLike: (req, res, next) => {
-    userServices.addLike(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  addLike: async (req, res, next) => {
+    await userServices.addLike(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   },
-  removeLike: (req, res, next) => {
-    userServices.removeLike(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  removeLike: async (req, res, next) => {
+    await userServices.removeLike(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 module.exports = userController
