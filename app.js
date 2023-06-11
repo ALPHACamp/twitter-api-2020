7af4f5f2436db9d76ea61706eca62dbc391b5e5e
playcsgo <sam@plum.wine>
2023-06-11 22:06:09 +0800

    cors-4

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-// const cors = require('cors')
-const express = require('express')
-// const helpers = require('./_helpers')
-const app = express()
-const port = process.env.PORT || 3000
-const apis = require('./routes/apis')
-const methodOverride = require('method-override')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const express = require('express')
+const apis = require('./routes/apis')
+// const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(methodOverride('_method'))
+const methodOverride = require('method-override')
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-// // use helpers.getUser(req) to replace req.user
-// function authenticated(req, res, next){
-//   // passport.authenticate('jwt', { ses...
-// }
+app.use(methodOverride('_method'))
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://leemengyun.github.io/ac_twitter_frontend/')
   next()
+  app.use('/api', apis)
 })
 // app.use(cors())
-app.use('/api', apis)
+// app.use('/api', apis)
+app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
