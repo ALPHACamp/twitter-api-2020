9795e4b231873072c976c0a029e0ebf793138372
playcsgo <sam@plum.wine>
2023-06-11 22:22:26 +0800

    cors-6

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const apis = require('./routes/apis')
-// const cors = require('cors')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -12,11 +12,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(methodOverride('_method'))
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://leemengyun.github.io/')
-  next()
-})
-// app.use(cors())
+app.use(cors({
+  origin: '*'
+}))
 app.use('/api', apis)
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
