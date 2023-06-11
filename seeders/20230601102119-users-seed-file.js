e14103d74f15656d1f0b1c94addf619171bffaad
Anna0118 <annachu0988@gmail.com>
2023-06-12 02:01:38 +0800

    feat: add more users

--- seeders/20230601102119-users-seed-file.js
+++ seeders/20230601102119-users-seed-file.js
@@ -28,9 +28,7 @@ module.exports = {
           account: 'user1',
           email: 'user1@example.com',
           password: await bcrypt.hash('12345678', 10),
-          avatar: `https://loremflickr.com/320/240/people/?random=${
-            Math.random() * 10
-          }`,
+          avatar: 'https://loremflickr.com/320/240/icon?lock=3',
           cover: `https://loremflickr.com/320/240/mountain/?random=${
             Math.random() * 100
           }`,
@@ -44,12 +42,8 @@ module.exports = {
           account: 'user2',
           email: 'user2@example.com',
           password: await bcrypt.hash('12345678', 10),
-          avatar: `https://loremflickr.com/320/240/people/?random=${
-            Math.random() * 10
-          }`,
-          cover: `https://loremflickr.com/320/240/mountain/?random=${
-            Math.random() * 100
-          }`,
+          avatar: 'https://loremflickr.com/320/240/icon?lock=20',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=20',
           role: 'user',
           introduction: faker.lorem.text().substring(0, 160),
           createdAt: new Date(),
@@ -60,12 +54,8 @@ module.exports = {
           account: 'user3',
           email: 'user3@example.com',
           password: await bcrypt.hash('12345678', 10),
-          avatar: `https://loremflickr.com/320/240/people/?random=${
-            Math.random() * 10
-          }`,
-          cover: `https://loremflickr.com/320/240/mountain/?random=${
-            Math.random() * 100
-          }`,
+          avatar: 'https://loremflickr.com/320/240/icon?lock=1',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=1',
           role: 'user',
           introduction: faker.lorem.text().substring(0, 160),
           createdAt: new Date(),
@@ -76,12 +66,8 @@ module.exports = {
           account: 'user4',
           email: 'user4@example.com',
           password: await bcrypt.hash('12345678', 10),
-          avatar: `https://loremflickr.com/320/240/people/?random=${
-            Math.random() * 10
-          }`,
-          cover: `https://loremflickr.com/320/240/mountain/?random=${
-            Math.random() * 100
-          }`,
+          avatar: 'https://loremflickr.com/320/240/icon?lock=11',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=11',
           role: 'user',
           introduction: faker.lorem.text().substring(0, 160),
           createdAt: new Date(),
@@ -92,12 +78,68 @@ module.exports = {
           account: 'user5',
           email: 'user5@example.com',
           password: await bcrypt.hash('12345678', 10),
-          avatar: `https://loremflickr.com/320/240/people/?random=${
-            Math.random() * 10
-          }`,
-          cover: `https://loremflickr.com/320/240/mountain/?random=${
-            Math.random() * 100
-          }`,
+          avatar: 'https://loremflickr.com/320/240/avatar?lock=2',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=2',
+          role: 'user',
+          introduction: faker.lorem.text().substring(0, 160),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'user6',
+          account: 'user6',
+          email: 'user6@example.com',
+          password: await bcrypt.hash('12345678', 10),
+          avatar: 'https://loremflickr.com/320/240/avatar?lock=15',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=15',
+          role: 'user',
+          introduction: faker.lorem.text().substring(0, 160),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'user7',
+          account: 'user7',
+          email: 'user7@example.com',
+          password: await bcrypt.hash('12345678', 10),
+          avatar: 'https://loremflickr.com/320/240/avatar?lock=50',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=50',
+          role: 'user',
+          introduction: faker.lorem.text().substring(0, 160),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'user8',
+          account: 'user8',
+          email: 'user8@example.com',
+          password: await bcrypt.hash('12345678', 10),
+          avatar: 'https://loremflickr.com/320/240/man?lock=21',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=21',
+          role: 'user',
+          introduction: faker.lorem.text().substring(0, 160),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'user9',
+          account: 'user9',
+          email: 'user9@example.com',
+          password: await bcrypt.hash('12345678', 10),
+          avatar: 'https://loremflickr.com/320/240/man?lock=53',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=53',
+          role: 'user',
+          introduction: faker.lorem.text().substring(0, 160),
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'user10',
+          account: 'user10',
+          email: 'user10@example.com',
+          password: await bcrypt.hash('12345678', 10),
+          avatar: 'https://loremflickr.com/320/240/man?lock=80',
+          cover: 'https://loremflickr.com/320/240/mountain?lock=80',
           role: 'user',
           introduction: faker.lorem.text().substring(0, 160),
           createdAt: new Date(),
