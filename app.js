bf3a4d983c6338d501dad70b5904e8605299623d
playcsgo <sam@plum.wine>
2023-06-12 01:10:21 +0800

    cors-15

--- app.js
+++ app.js
@@ -2,21 +2,40 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const routes = require('./routes')
+const router = require('./routes')
 const cors = require('cors')
-const methodOverride = require('method-override')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride('_method'))
 app.use(express.json())
-
 app.use(cors())
+app.use(router)
 
-app.use(routes)
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app
+
+// if (process.env.NODE_ENV !== 'production') {
+//   require('dotenv').config()
+// }
+// const express = require('express')
+// const routes = require('./routes')
+// const cors = require('cors')
+// const methodOverride = require('method-override')
+
+// const app = express()
+// const port = process.env.PORT || 3000
+
+// app.use(express.urlencoded({ extended: true }))
+// app.use(methodOverride('_method'))
+// app.use(express.json())
+
+// app.use(cors())
+
+// app.use(routes)
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+// module.exports = app
