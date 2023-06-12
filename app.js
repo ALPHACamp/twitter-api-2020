253c1516e0d81854cf034a000eb9f350f36e288d
playcsgo <sam@plum.wine>
2023-06-12 10:02:02 +0800

    cors-20

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const routes = require('./routes')
-// const cors = require('cors')
+const cors = require('cors')
 const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,21 +12,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.json())
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
-  res.header('Access-Control-Allow-Headers', '*')
-  res.header('Access-Control-Allow-Credentials', true)
-  next()
-})
-
-// app.use(cors({
-//   origin: 'https://leemengyun.github.io',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//   allowedHeaders: '*',
-//   preflightContinue: true,
-//   credentials: true
-// }))
+// 也試過直接打 app.use(cors()) 使用他的default這定
+app.use(cors({
+  origin: 'https://leemengyun.github.io',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: '*',
+  preflightContinue: false,
+  credentials: true
+}))
 
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
