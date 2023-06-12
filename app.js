44b42d2ec1ae967ccb5d18f293fceb15274d87e0
playcsgo <sam@plum.wine>
2023-06-12 17:58:10 +0800

    cors-27 Ivan

--- app.js
+++ app.js
@@ -3,15 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const routes = require('./routes')
-const cors = require('cors')
-const corsOptions = {
-  origin: [
-    process.env.GUTHUB_PAGE,
-    'http://localhost:3000/'
-  ],
-  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
-  allowedHeaders: ['Content-Type', 'Authorization']
-}
+
 const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
@@ -19,7 +11,15 @@ const port = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.json())
-
+const cors = require('cors')
+const corsOptions = {
+  origin: [
+    process.env.GITHUB_PAGE,
+    'http://localhost:3000/'
+  ],
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
 // 也試過直接打 app.use(cors()) 使用他的default這定
 // app.use(cors({
 //   origin: ['https://leemengyun.github.io', 'http://localhost:3000/', '*'],
