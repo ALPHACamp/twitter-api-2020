9fa4a52bbc4591478bd0dce8631bdc3864b4b869
WeiWayne1030 <wayne10032004@gmail.com>
2023-06-12 19:20:24 +0800

    fix:modify tweets seeder to qualify ac

--- seeders/20230605165733-tweets-seed-file.js
+++ seeders/20230605165733-tweets-seed-file.js
@@ -2,17 +2,22 @@
 const faker = require('faker')
 module.exports = {
   up: async(queryInterface, Sequelize) => {
-    const users = await queryInterface.sequelize.query('SELECT id FROM Users;', {
-      type: queryInterface.sequelize.QueryTypes.SELECT
-    })
-    await queryInterface.bulkInsert('Tweets',
-      Array.from({ length: 30 }, () => ({
-        UserId: users[Math.floor(Math.random() * users.length)].id, 
-        description: faker.lorem.text(),
-        createdAt: new Date(),
-        updatedAt: new Date()
-      }))
-    )
+        const users = await queryInterface.sequelize.query('SELECT id FROM Users;', {
+            type: queryInterface.sequelize.QueryTypes.SELECT
+            })
+        const tweets = []
+        for (const user of users) {
+          for (let i = 0; i < 3; i++) {
+            tweets.push ({
+              UserId: user.id,
+              description: faker.lorem.text(),
+              createdAt: new Date(),
+              updatedAt: new Date()
+            })
+          }
+        }
+
+        await queryInterface.bulkInsert('Tweets', tweets)
   },
 
   down: async(queryInterface, Sequelize) => {
