8ccf018a536c24b4bac8c39bfe890b236380cdbb
etandmouse <etandmouse@gmail.copm>
2023-06-12 20:38:47 +0800

    Fix PUT /api/users/:id is blocked

--- app.js
+++ app.js
@@ -12,7 +12,16 @@ const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = process.env.SESSION_SECRET
 
-app.use(cors())
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'https://jujucw.github.io'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use(cors(corsOptions))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
