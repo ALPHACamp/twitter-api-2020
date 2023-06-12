37eb8d7f29a143f02d4b3bf8d6b10fe7d1caa4f7
playcsgo <sam@plum.wine>
2023-06-12 17:39:48 +0800

    cors-26

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.json())
@@ -23,13 +24,14 @@ app.use(express.json())
 //   optionsSuccessStatus: 204
 // }))
 
-// app.use(cors())
+app.use(cors())
 
-app.use(cors({
-  origin: 'https://leemengyun.github.io',
-  allowedHeaders: ['Content-Type'],
-  preflightContinue: true
-}))
+// app.use(cors({
+//   origin: 'https://leemengyun.github.io',
+//   allowedHeaders: ['Content-Type'],
+//   preflightContinue: true
+// }))
+
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
