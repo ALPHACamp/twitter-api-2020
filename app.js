15a9d489f3b2f6852f1b0b7c6221312099fe02f1
playcsgo <sam@plum.wine>
2023-06-12 18:10:13 +0800

    cors-27 cors()

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require('express')
 const routes = require('./routes')
-
-// const methodOverride = require('method-override')
+const methodOverride = require('method-override')
 const app = express()
 const port = process.env.PORT || 3000
+const cors = require('cors')
 
 app.use(express.urlencoded({ extended: true }))
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 app.use(express.json())
-const cors = require('cors')
-const corsOptions = {
-  origin: [
-    process.env.GITHUB_PAGE,
-    'http://localhost:3000/'
-  ],
-  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
-  allowedHeaders: ['Content-Type', 'Authorization']
-}
+
+// const corsOptions = {
+//   origin: [
+//     process.env.GITHUB_PAGE,
+//     'http://localhost:3000/'
+//   ],
+//   methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }
+
 // 也試過直接打 app.use(cors()) 使用他的default這定
 // app.use(cors({
 //   origin: ['https://leemengyun.github.io', 'http://localhost:3000/', '*'],
@@ -31,15 +32,15 @@ const corsOptions = {
 //   optionsSuccessStatus: 204
 // }))
 
-// app.use(cors())
-
 // app.use(cors({
 //   origin: 'https://leemengyun.github.io',
 //   allowedHeaders: ['Content-Type'],
 //   preflightContinue: true
 // }))
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+
+app.use(cors())
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
