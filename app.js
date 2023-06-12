2fda21d793cef6e63ce9948be2024811b20afe50
playcsgo <sam@plum.wine>
2023-06-12 15:54:24 +0800

    cors-24

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.json())
 // }))
 
 app.use(cors())
+
 app.use(routes)
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
