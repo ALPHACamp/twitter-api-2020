7cf42eb387fd7e53fc635f90093280ef9f0e86cb
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-06-12 16:13:10 +0800

    R4 user controller (#12)
    * feat: build models
    
    * feat: signUp/get & editUserData(photo upload yet)
    
    * error fixed
    
    * feat: add signUp, get/editUserData,getUserTweets
    
    * fix: move signUp to router/index.js
    
    * fix: add helper in userController
    
    * feat: signUp/get & editUserData(photo upload yet)
    
    * error fixed
    
    * feat: add signUp, get/editUserData,getUserTweets
    
    * fix: move signUp to router/index.js
    
    * fix: add helper in userController
    
    * fix error and get passed in tests
    
    ---------
    
    Co-authored-by: av124773 <av124773@gmail.com>

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,14 @@
 const jwt = require('jsonwebtoken')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const imgur = require('imgur')
+imgur.setAPIUrl('https://api.imgur.com/3/')
+const bcrypt = require('bcryptjs')
+const { User, Tweet, Reply, Like, Followship } = require('../models')
+const helpers = require('../_helpers')
+const { Op } = require('sequelize')
 
 const userController = {
-  signIn: (req, res, next) => {
+  signIn: async (req, res, next) => {
     try {
       const userData = req.user
       delete userData.password
@@ -16,6 +23,128 @@ const userController = {
     } catch (err) {
       next(err)
     }
+  },
+  signUp: async (req, res, next) => {
+    try {
+      const { account, name, email, password, checkPassword } = req.body
+      if (!account || !name || !email || !password || !checkPassword) throw new Error('Information not complete')
+      if (password !== checkPassword) throw new Error('Passwords do not match')
+
+      const user = await User.findOne({
+        where: {
+          [Op.or]: [ //抓出email重複或account重複
+            { email: email },
+            { account: account }
+          ]
+        }
+      })
+      if (user?.email === email) throw new Error('Email has already been registered')
+      if (user?.account === account) throw new Error('This account has already been registered')
+      //create new user
+      await User.create({
+        email,
+        password: bcrypt.hashSync(password),
+        name,
+        account,
+        role: 'user',
+        createAt: new Date(),
+        updatedAt: new Date()
+      })
+      res.status(200).json({
+        status: 'success',
+        message: 'Account created successfully'
+      })
+    } catch (err) {
+      next(err)
+    }
+  },
+  getUserData: async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (!user) throw new Error('This user does not exist')
+    
+      const userData = {
+        ...user.toJSON()
+      }
+      res.status(200).json(userData)
+      // res.json({ status: 'success', user: user.toJSON(userData) })
+    } catch (err) {
+      next(err);
+    }
+  },
+  editUserData: async (req, res, next) => {
+    try {
+      // check user權限(不能改別人的)
+      const currentUserId = helpers.getUser(req).id
+      if (currentUserId.toString() !== req.params.id) {
+        throw new Error('Cannot edit other users profile')
+      }
+      const { account, name, email, avatar, password, checkPassword, introduction } = req.body
+      const { banner } = req.files || {} // 圖片存在req.files屬性中&確保非空值
+
+      // AC測試規定: 自我介紹字數上限 160 字、暱稱上限 50 字
+      if (name && name.length > 50) throw new Error('name length should <= 50')
+      if (introduction && introduction.length > 160) { throw new Error(' introduction length should <= 160') }
+
+      const user = await User.findByPk(req.params.id)
+      if (!user) {
+        throw new Error('user does not exist')
+      }
+
+      // //用bcrypt 加密函數進行密碼驗證
+      // const passwordMatch = await bcrypt.compare(password, user.password);
+      // if (!passwordMatch) {
+      //   throw new Error('Password check failed');
+      // }
+
+      const avatarUrl = avatar ? await imgurFileHandler(avatar[0]) : null
+      const bannerUrl = banner ? await imgurFileHandler(banner[0]) : null
+
+      await user.update({
+        name: name || user.name,
+        account: account || user.account,
+        email: email || user.email,
+        avatar: avatarUrl || user.avatar,
+        password: password ? await bcrypt.hash(password, 10) : user.password,
+        introduction: introduction || user.introduction,
+        banner: bannerUrl || user.banner
+      })
+      res.status(200).json({
+        status: 'success',
+        message: 'user info edited successfully'
+      })
+    } catch (err) {
+      next(err)
+    }
+  },
+  getUserTweets: async (req, res, next) => {
+    try {
+      const userId = req.params.userId
+      const tweets = await Tweet.findAll({
+        where: { UserId: userId },
+        include: [
+          { model: User },
+          { model: Reply },
+          { model: Like }
+        ],
+        order: [['createdAt', 'DESC']],
+        nest: true
+      });
+      const tweetsData = tweets.map(tweet => ({
+        ...tweet.toJSON(),
+      }));
+      res.status(200).json(tweetsData);
+    } catch (err) {
+      next(err);
+    }
+  },
+  getUserReplies: async (req, res, next) => {
+  },
+  getUserLikes: async (req, res, next) => {
+  },
+  getUserFollowings: async (req, res, next) => {
+  },
+  getUserFollowers: async (req, res, next) => {
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -7,30 +7,16 @@ const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 const followships = require('./modules/followships')
 
+const { apiErrorHandler } = require('../middleware/error-handler')
+
 const adminController = require('../controllers/admin-controller')
 const userController = require('../controllers/user-controller')
-const { apiErrorHandler } = require('../middleware/error-handler')
+
 const { authenticated, isUser, isAdmin, authenticatedUser, authenticatedAdmin } = require('../middleware/auth')
 
-router.post('/api/admin/signin', (req, res, next) => {
-  passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (info) {
-      return res.status(401).json(info);
-    }
-    req.user = user
-    next();
-  })(req, res, next);
-}, isAdmin, adminController.signIn)
-
-router.post('/api/users/signin', (req, res, next) => {
-  passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (info) {
-      return res.status(401).json(info);
-    }
-    req.user = user
-    next();
-  })(req, res, next);
-}, isUser, userController.signIn)
+router.post('/api/users', userController.signUp)
+router.post('/api/admin/signin', passport.authenticate('local', { session: false }), isAdmin, adminController.signIn)
+router.post('/api/users/signin', passport.authenticate('local', { session: false }), isUser, userController.signIn)
 
 router.use('/api/admin', authenticated, authenticatedAdmin, admin)
 router.use('/api/users', authenticated, authenticatedUser, users)
