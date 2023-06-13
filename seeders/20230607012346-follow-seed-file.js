b8a7302c2466e948eb6b15f50ac1cc34f19342f7
Chingsan0722 <rory85722@gmail.com>
2023-06-13 13:42:02 +0800

    modify admin getUsers by adding tweetsCount & likesCount

--- seeders/20230607012346-follow-seed-file.js
+++ seeders/20230607012346-follow-seed-file.js
@@ -7,23 +7,28 @@ module.exports = {
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
 
-    const followships = Array.from({ length: 30 }, (d, i) => {
+    const followships = new Set()
+    const followshipArray = []
+
+    while (followships.size < 11) {
       const followerId = users[Math.floor(Math.random() * users.length)].id
       let followingId = users[Math.floor(Math.random() * users.length)].id
 
-      while (followerId === followingId) {
+      while (followerId === followingId || followships.has(`${followerId}-${followingId}`)) {
         followingId = users[Math.floor(Math.random() * users.length)].id
       }
 
-      return {
+      followships.add(`${followerId}-${followingId}`)
+
+      followshipArray.push({
         follower_Id: followerId,
         following_Id: followingId,
         created_at: new Date(),
         updated_at: new Date()
-      }
-    })
+      })
+    }
 
-    await queryInterface.bulkInsert('Followships', followships)
+    await queryInterface.bulkInsert('Followships', followshipArray)
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Followships', {})
