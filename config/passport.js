0f3a4e3a9131032ffd2c24ee2ce4f9ad72fcc2e7
Lanways <mcbluedd@gmail.com>
2023-06-13 13:01:59 +0800

    feat: add getUsers api

--- config/passport.js
+++ config/passport.js
@@ -34,7 +34,7 @@ passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
   User.findByPk(jwtPayload.id, {
     include: [
       //包含user的所有like及這些like所對應的tweet
-      { model: Like, as: 'likedTweets', include: [Tweet] },
+      // { model: Like, as: 'likedTweets', include: [Tweet] },
       { model: User, as: 'Followers' },
       { model: User, as: 'Followings' }
     ]
@@ -49,7 +49,6 @@ passport.serializeUser((user, cb) => {
 passport.deserializeUser((id, cb) => {
   return User.findByPk(id, {
     include: [
-      { model: Like, as: 'likedTweets', include: [Tweet] },
       { model: User, as: 'Followers' },
       { model: User, as: 'Followings' }
     ]
