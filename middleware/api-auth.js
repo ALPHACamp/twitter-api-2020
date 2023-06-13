5ca2157f454d12df5cfc819f2fc1abf0436130b4
aking4511 <aking4511@gmail.com>
2023-06-13 19:37:06 +0800

    feat: edit api-auth & getUsers  in admin-controller

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -1,14 +1,25 @@
+const helpers = require('../_helpers')
 const passport = require('../config/passport')
-const authenticated = passport.authenticate('jwt', { session: false })
-const authenticatedAdmin = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') return next()
-  return res.status(403).json({ status: 'error', message: 'permission denied' })
+const authenticated = (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user) => {
+    if (err || !user) {
+      return res.status(401).json({ status: 'error', message: 'Unauthorized' })
+    }
+    req.user = user // JWT 沒有使用 session，所以需要手動設置
+    return next()
+  })(req, res, next)
 }
-const authenticatedUser = (req, res, next) => {
-  if (req.user && req.user.role === 'user') return next()
+const authenticatedAdmin = (req, res, next) => {
+  const user = helpers.getUser(req)
+  if (user && user.role === 'admin') return next()
   return res.status(403).json({ status: 'error', message: 'permission denied' })
 }
 
+const authenticatedUser = (req, res, next) => {
+  const user = helpers.getUser(req)
+  if (user && user.role === 'user') return next()
+  return res.status(403).json({ status: 'error', message: 'permission denied' })
+}
 module.exports = {
   authenticated,
   authenticatedUser,
