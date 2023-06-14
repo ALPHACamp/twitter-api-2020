c611cba98ad57dc30c0aa13a47951e6a349249e9
CHUCHUDAN <daniel870217@gmail.com>
2023-06-14 10:16:40 +0800

    Updated like and followships seed file

--- seeders/20230607025209-likes-seed-file.js
+++ seeders/20230607025209-likes-seed-file.js
@@ -12,15 +12,21 @@ module.exports = {
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
     const totalLikes = users.length * DEFAULT_EACH_USER_LIKES
-
+    let tweetArray = tweets
     await queryInterface.bulkInsert('Likes',
-      Array.from({ length: totalLikes }, (_, index) => ({
-        // 預設每個user有3個 likes
-        UserId: users[Math.floor(index / DEFAULT_EACH_USER_LIKES)].id,
-        TweetId: tweets[index].id,
-        createdAt: faker.date.past(),
-        updatedAt: faker.date.recent()
-      })))
+      Array.from({ length: totalLikes }, (_, index) => {
+        // 預設每個user有2個 likes
+        const remind = index % DEFAULT_EACH_USER_LIKES
+        if (remind === 0) tweetArray = tweets
+        const tweet = tweetArray[Math.floor(Math.random() * tweetArray.length)].id
+        tweetArray = tweetArray.filter(t => t.id !== tweet)
+        return {
+          UserId: users[Math.floor(index / DEFAULT_EACH_USER_LIKES)].id,
+          TweetId: tweet,
+          createdAt: faker.date.past(),
+          updatedAt: faker.date.recent()
+        }
+      }))
   },
 
   down: async (queryInterface, Sequelize) => {
--- seeders/20230608073358-followships-seed-file.js
+++ seeders/20230608073358-followships-seed-file.js
@@ -1,24 +1,59 @@
+// 'use strict'
+
+// const faker = require('faker')
+// const fixFunction = (index, array, followerId, defaultCount) => {
+//   const remind = index % defaultCount
+//   const newArray = array.filter(a => a.id !== followerId)
+//   const { id } = newArray[remind]
+//   return id
+// }
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     const users = await queryInterface.sequelize.query(
+//       "SELECT id FROM Users WHERE role = 'user';",
+//       { type: queryInterface.sequelize.QueryTypes.SELECT }
+//     )
+//     const DEFAULT_EACH_USER_FOLLOWS = users.length - 1
+//     const totalFollows = users.length * DEFAULT_EACH_USER_FOLLOWS
+//     await queryInterface.bulkInsert('Followships',
+//       Array.from({ length: totalFollows }, (_, index) => {
+//         const followerId = users[Math.floor(index / DEFAULT_EACH_USER_FOLLOWS)].id
+//         const followingId = fixFunction(index, users, followerId, DEFAULT_EACH_USER_FOLLOWS)
+//         return {
+//           followerId,
+//           followingId,
+//           createdAt: faker.date.past(),
+//           updatedAt: faker.date.recent()
+//         }
+//       })
+//     )
+//   },
+
+//   down: async (queryInterface, Sequelize) => {
+//     await queryInterface.bulkDelete('Followships', {})
+//   }
+// }
+
 'use strict'
 
 const faker = require('faker')
-const fixFunction = (index, array, followerId, defaultCount) => {
-  const remind = index % defaultCount
-  const newArray = array.filter(a => a.id !== followerId)
-  const { id } = newArray[remind]
-  return id
-}
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const users = await queryInterface.sequelize.query(
       "SELECT id FROM Users WHERE role = 'user';",
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
-    const DEFAULT_EACH_USER_FOLLOWS = users.length - 1
+    const DEFAULT_EACH_USER_FOLLOWS = 2 || users.length - 1
     const totalFollows = users.length * DEFAULT_EACH_USER_FOLLOWS
+    let userArray = users
     await queryInterface.bulkInsert('Followships',
       Array.from({ length: totalFollows }, (_, index) => {
+        const remind = index % DEFAULT_EACH_USER_FOLLOWS
         const followerId = users[Math.floor(index / DEFAULT_EACH_USER_FOLLOWS)].id
-        const followingId = fixFunction(index, users, followerId, DEFAULT_EACH_USER_FOLLOWS)
+        if (remind === 0) userArray = users
+        userArray = userArray.filter(a => a.id !== followerId)
+        const followingId = userArray[Math.floor(Math.random() * userArray.length)].id
+        userArray = userArray.filter(a => a.id !== followingId)
         return {
           followerId,
           followingId,
