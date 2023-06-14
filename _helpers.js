3b7ab7dbcf03621e7aceb2e2a5d9e6f9f43457a8
JimmyLin <59187640+deamo771003@users.noreply.github.com>
2023-06-14 13:07:30 +0800

    Update _helpers.js

--- _helpers.js
+++ _helpers.js
@@ -3,16 +3,6 @@ function getUser(req) {
   return req.user;
 }
 
-function setUser(req, user) {
-  req.user = user
-}
-
-function ensureAuthenticated(req) {
-  return req.isAuthenticated();
-}
-
 module.exports = {
-  getUser,
-  setUser,
-  ensureAuthenticated
+  getUser
 };
