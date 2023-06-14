9f2de2659e238388883531ac0089b8758714d43f
Anna0118 <annachu0988@gmail.com>
2023-06-14 11:50:15 +0800

    unify name  and status code

--- middleware/auth.js
+++ middleware/auth.js
@@ -14,7 +14,7 @@ const adminRole = (req, res, next) => {
 // 驗證token
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
-    if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
+    if (err || !user) return res.status(401).json({ status: 'error', message: '未經授權' })
     req.user = user
     next()
   })(req, res, next)
@@ -23,20 +23,20 @@ const authenticated = (req, res, next) => {
 // 驗證user
 const authenticatedUser = (req, res, next) => {
   if (helpers.getUser(req).role === 'user') return next()
-  return res.status(403).json({ status: 'error', message: 'permission denied' })
+  return res.status(403).json({ status: 'error', message: '無權限訪問' })
 }
 
 // 驗證admin
 const authenticatedAdmin = (req, res, next) => {
   if (helpers.getUser(req).role === 'admin') return next()
-  return res.status(403).json({ status: 'error', message: 'permission denied' })
+  return res.status(403).json({ status: 'error', message: '無權限訪問' })
 }
 
 // 確認user是否存在
 function validateUser (req, res, next) {
   const user = helpers.getUser(req)
   if (!user || !user.id) {
-    return res.status(400).json({ status: 'error', message: 'User not found' })
+    return res.status(400).json({ status: 'error', message: '使用者不存在' })
   }
   next()
 }
