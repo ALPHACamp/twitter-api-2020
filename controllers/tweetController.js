87d797342296693fdcefef05b4fd96220f4b7e76
Banwind <a84012807@gmail.com>
2023-06-15 18:26:41 +0800

    feat:fix tweet limit

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -1,79 +1,70 @@
 // const Sequelize = require('sequelize')
 const { getLastUpdated, getLastUpd, getUser } = require('../_helpers')
 const { User, Tweet, Like, Reply, Followship } = require('../models')
+const { Op } = require('sequelize')
+
 const tweetController = {
   getTweets: async (req, res, next) => {
     try {
-      const follows = req.query.follows
-      const liked = req.query.liked
-      const userId = req.params.id
-      let tweets
+      const follows = req.query.follows;
+      const liked = req.query.liked;
+      const userId = req.params.id;
+      const lastId = req.query.lastId; // Last seen tweet ID
+      const limit = 10; // Number of tweets to return
+
+      const whereClause = {};
+      if (lastId) {
+        whereClause.id = { [Op.lt]: lastId };
+      }
+
+      let tweetIdCondition = null;
+
       if (follows) {
         const followingIdData = await Followship.findAll({
-          where: [{ follower_Id: userId }],
-          attributes: ['followingId']
-        })
-        const followingIds = followingIdData.map(row => row.followingId)
-        followingIds.push(userId)
-        tweets = await Tweet.findAll({
-          include: [
-            {
-              model: User,
-              as: 'User',
-              attributes: ['account', 'name', 'avatar']
-            },
-            { model: Like, attributes: ['UserId'] },
-            { model: Reply, attributes: ['UserId'] }
-          ],
-          where: [{ User_Id: followingIds }],
-          order: [
-            ['createdAt', 'DESC'],
-            ['id', 'DESC']
-          ],
-          nest: true
-        })
+          where: { follower_Id: userId },
+          attributes: ["followingId"],
+        });
+        const followingIds = followingIdData.map((row) => row.followingId);
+        followingIds.push(userId);
+        tweetIdCondition = { User_Id: followingIds };
       } else if (liked) {
         const likes = await Like.findAll({
-          where: [{ User_Id: userId }],
-          attributes: ['TweetId'],
+          where: { User_Id: userId },
+          attributes: ["TweetId"],
           raw: true,
-          nest: true
-        })
-        const tweetIds = likes.map(row => row.TweetId)
-        tweetIds.push(userId)
-        tweets = await Tweet.findAll({
-          where: [{ id: tweetIds }],
-          include: [
-            {
-              model: User,
-              as: 'User',
-              attributes: ['account', 'name', 'avatar']
-            },
-            { model: Like, attributes: ['UserId'] },
-            { model: Reply, attributes: ['UserId'] }
-          ],
-          order: [
-            ['createdAt', 'DESC'],
-            ['id', 'DESC']
-          ],
-          nest: true
-        })
-      } else {
-        tweets = await Tweet.findAll({
-          where: { UserId: userId },
-          include: [
-            { model: User, attributes: ['account', 'name', 'avatar'] },
-            { model: Like, attributes: ['UserId'] },
-            { model: Reply, attributes: ['UserId'] }
-          ],
-          order: [
-            ['createdAt', 'DESC'],
-            ['id', 'DESC']
-          ],
-          nest: true
-        })
+          nest: true,
+        });
+        const tweetIds = likes.map((row) => row.TweetId);
+        tweetIds.push(userId);
+        tweetIdCondition = { id: tweetIds };
       }
-      if (tweets.length === 0) return res.status(404).json('Tweets not found')
+
+      if (tweetIdCondition) {
+        whereClause[Op.and] = [tweetIdCondition];
+      }
+
+      const options = {
+        include: [
+          {
+            model: User,
+            as: "User",
+            attributes: ["account", "name", "avatar"],
+          },
+          { model: Like, attributes: ["UserId"] },
+          { model: Reply, attributes: ["UserId"] },
+        ],
+        order: [
+          ["createdAt", "DESC"],
+          ["id", "DESC"],
+        ],
+        limit: limit,
+        where: whereClause,
+      };
+
+      const tweets = await Tweet.findAll(options);
+
+      if (tweets.length === 0) return res.status(404).json("Tweets not found");
+
       const counts = tweets.map((tweet) => ({
         ...tweet.toJSON(),
         likesCount: tweet.Likes.length,
@@ -82,80 +73,80 @@ const tweetController = {
         account: tweet.User.account,
         name: tweet.User.name,
         avatar: tweet.User.avatar,
-        isLiked: tweet.Likes?.some((l) => l.UserId === Number(userId))
-      }))
-      const data = counts.map(({ Likes, Replies, User, ...rest }) => rest)
-      return res.status(200).json(data)
+        isLiked: tweet.Likes?.some((l) => l.UserId === Number(userId)),
+      }));
+
+      const data = counts.map(({ Likes, Replies, User, ...rest }) => rest);
+
+      return res.status(200).json(data);
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   getAllTweets: (req, res, next) => {
     Tweet.findAll({
-      include: [
-        { model: User, attributes: ['account', 'name', 'avatar'] }
-      ]
+      include: [{ model: User, attributes: ["account", "name", "avatar"] }],
     })
       .then((tweets) => {
         return tweets.map((tweet) => ({
           ...tweet.get({ plain: true }),
           account: tweet.User.account,
           name: tweet.User.name,
-          avatar: tweet.User.avatar
-        }))
+          avatar: tweet.User.avatar,
+        }));
       })
       .then((data) => res.status(200).json(data))
-      .catch((err) => next(err))
+      .catch((err) => next(err));
   },
   getTweet: (req, res, next) => {
-    const tweetId = req.params.id
-    const userId = getUser(req).id || getUser(req).dataValues.id
+    const tweetId = req.params.id;
+    const userId = getUser(req).id || getUser(req).dataValues.id;
     Promise.all([
-      User.findByPk(userId, { attributes: ['avatar'], raw: true }),
+      User.findByPk(userId, { attributes: ["avatar"], raw: true }),
       Tweet.findByPk(tweetId, {
         include: [
-          { model: User, attributes: ['account', 'name', 'avatar'] },
-          { model: Like, attributes: ['UserId'] },
-          { model: Reply, attributes: ['UserId'] }
+          { model: User, attributes: ["account", "name", "avatar"] },
+          { model: Like, attributes: ["UserId"] },
+          { model: Reply, attributes: ["UserId"] },
         ],
-        nest: true
-      })
+        nest: true,
+      }),
     ])
       .then(([user, data]) => {
-        if (!data) return res.status(404).json('Tweets not found')
-        const tweet = data.dataValues
-        tweet.likesCount = tweet.Likes.length
-        tweet.repliesCount = tweet.Replies.length
-        tweet.account = tweet.User.account
-        tweet.name = tweet.User.name
-        tweet.avatar = tweet.User.avatar
-        tweet.userAvatar = user.avatar
-        tweet.isLiked = tweet.Likes?.some((f) => f.UserId === Number(userId))
-        delete tweet.User
-        delete tweet.Likes
-        delete tweet.Replies
-        getLastUpdated(tweet)
-        return res.status(200).json(data)
+        if (!data) return res.status(404).json("Tweets not found");
+        const tweet = data.dataValues;
+        tweet.likesCount = tweet.Likes.length;
+        tweet.repliesCount = tweet.Replies.length;
+        tweet.account = tweet.User.account;
+        tweet.name = tweet.User.name;
+        tweet.avatar = tweet.User.avatar;
+        tweet.userAvatar = user.avatar;
+        tweet.isLiked = tweet.Likes?.some((f) => f.UserId === Number(userId));
+        delete tweet.User;
+        delete tweet.Likes;
+        delete tweet.Replies;
+        getLastUpdated(tweet);
+        return res.status(200).json(data);
       })
-      .catch((err) => next(err))
+      .catch((err) => next(err));
   },
   getRepliedTweets: (req, res, next) => {
-    const userId = req.params.id
+    const userId = req.params.id;
     Reply.findAll({
       where: { UserId: userId },
       include: [
-        { model: User, attributes: ['avatar', 'name', 'account'] },
+        { model: User, attributes: ["avatar", "name", "account"] },
         {
           model: Tweet,
-          attributes: ['UserId'],
-          include: [{ model: User, attributes: ['avatar', 'name', 'account'] }]
-        }
+          attributes: ["UserId"],
+          include: [{ model: User, attributes: ["avatar", "name", "account"] }],
+        },
       ],
       order: [
-        ['createdAt', 'DESC'],
-        ['id', 'DESC']
+        ["createdAt", "DESC"],
+        ["id", "DESC"],
       ],
-      nest: true
+      nest: true,
     })
       .then((tweets) => {
         const beforeData = tweets.map((tweet) => ({
@@ -166,64 +157,72 @@ const tweetController = {
           repliedUserName: tweet.User.name,
           repliedUserAccount: tweet.User.account,
           repliedUserAvatar: tweet.User.avatar,
-          lastUpdated: getLastUpd(tweet)
-        }))
-        const data = beforeData.map(({ Tweet, User, ...rest }) => rest)
-        res.status(200).json(data)
+          lastUpdated: getLastUpd(tweet),
+        }));
+        const data = beforeData.map(({ Tweet, User, ...rest }) => rest);
+        res.status(200).json(data);
       })
-      .catch((err) => next(err))
+      .catch((err) => next(err));
   },
   postTweet: (req, res, next) => {
-    const { description, likable, commendable } = req.body
-    if (!description) return res.status(400).json('Description can not be empty!')
-    if (description.length > 140) return res.status(400).json('Max length 140.')
-    const id = req.user.id || getUser(req).dataValues.id
+    const { description, likable, commendable } = req.body;
+    if (!description) {
+      return res.status(400).json("Description can not be empty!");
+    }
+    if (description.length > 140) {
+      return res.status(400).json("Max length 140.");
+    }
+    const id = req.user.id || getUser(req).dataValues.id;
     Tweet.create({
       UserId: id,
       description,
-      likable: likable || '1',
-      commendable: commendable || '1'
+      likable: likable || "1",
+      commendable: commendable || "1",
     })
       .then((data) => {
-        if (!data) return res.status(404).json('Tweet not found')
-        getLastUpdated(data)
-        return res.status(200).json('post success')
+        if (!data) return res.status(404).json("Tweet not found");
+        getLastUpdated(data);
+        return res.status(200).json("post success");
       })
-      .catch((err) => next(err))
+      .catch((err) => next(err));
   },
   putTweet: (req, res, next) => {
-    const { description, likable, commendable } = req.body
-    const id = req.params.id
-    if (!description) return res.status(400).json('Description can not be empty!')
-    if (description.length > 140) return res.status(400).json('Max length 140.')
+    const { description, likable, commendable } = req.body;
+    const id = req.params.id;
+    if (!description) {
+      return res.status(400).json("Description can not be empty!");
+    }
+    if (description.length > 140) {
+      return res.status(400).json("Max length 140.");
+    }
     Tweet.findByPk(id)
       .then((tweet) => {
-        if (!tweet) return res.status(404).json('Tweet not found!')
+        if (!tweet) return res.status(404).json("Tweet not found!");
         return tweet.update({
           description,
-          likable: likable || '1',
-          commendable: commendable || '1'
-        })
+          likable: likable || "1",
+          commendable: commendable || "1",
+        });
       })
       .then((data) => {
-        if (!data) return res.status(400).json('Update failed!')
-        getLastUpdated(data)
-        return res.status(200).json('update success')
+        if (!data) return res.status(400).json("Update failed!");
+        getLastUpdated(data);
+        return res.status(200).json("update success");
       })
-      .catch((err) => next(err))
+      .catch((err) => next(err));
   },
   deleteTweet: (req, res, next) => {
-    const id = req.params.id
+    const id = req.params.id;
     Tweet.findByPk(id)
       .then((tweet) => {
-        if (!tweet) return res.status(404).json('Tweet not found')
-        tweet.destroy()
+        if (!tweet) return res.status(404).json("Tweet not found");
+        tweet.destroy();
       })
       .then(() => {
-        return res.status(200).json('Delete success')
+        return res.status(200).json("Delete success");
       })
-      .catch((err) => next(err))
-  }
-}
+      .catch((err) => next(err));
+  },
+};
 
 module.exports = tweetController
