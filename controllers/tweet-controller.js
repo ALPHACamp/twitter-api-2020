d0dd23e26bf65e68169df2e1406e3af660a6e4c1
Ariel <ariel040132@gmail.com>
2023-06-16 02:48:57 +0800

    modify: tweet-controller getTweets api deleted Reply

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -1,5 +1,6 @@
 const { Tweet, User, Reply } = require('../models')
 const Sequelize = require('sequelize')
+const { literal } = Sequelize
 const moment = require('moment')
 
 const tweetController = {
@@ -10,44 +11,50 @@ const tweetController = {
       include: [{
         model: User,
         attributes: {
-        exclude: ['createdAt', 'updatedAt', 'password', 'role'], // 排除原始的 createdAt 和 updatedAt
+        exclude: ['createdAt', 'updatedAt', 'password', 'role'],
         include: [
-          [Sequelize.literal('DATE_FORMAT(User.created_at, "%Y-%m-%d %H:%i:%s")'), 'formattedCreatedAt'],
-          [Sequelize.literal('DATE_FORMAT(User.updated_at, "%Y-%m-%d %H:%i:%s")'), 'formattedUpdatedAt']
+          [Sequelize.literal('DATE_FORMAT(User.created_at, "%Y-%m-%d %H:%i:%s")'), 'createdAt'],
+          [Sequelize.literal('DATE_FORMAT(User.updated_at, "%Y-%m-%d %H:%i:%s")'), 'updatedAt']
           ]
         }
-      }, {
-        model: Reply,
-        attributes: {
-        exclude: ['UserId', 'TweetId', 'createdAt', 'updatedAt'], // 排除原始的 createdAt 和 updatedAt
-        include: [
-          [Sequelize.literal('DATE_FORMAT(Replies.created_at, "%Y-%m-%d %H:%i:%s")'), 'formattedCreatedAt'],
-          [Sequelize.literal('DATE_FORMAT(Replies.updated_at, "%Y-%m-%d %H:%i:%s")'), 'formattedUpdatedAt']
-          ]
-        }
-      }],
+      }, 
+      // {
+      //   model: Reply,
+      //   attributes: {
+      //   exclude: ['UserId', 'TweetId', 'createdAt', 'updatedAt'], // 排除原始的 createdAt 和 updatedAt
+      //   include: [
+      //     [Sequelize.literal('DATE_FORMAT(Replies.created_at, "%Y-%m-%d %H:%i:%s")'), 'createdAt'],
+      //     [Sequelize.literal('DATE_FORMAT(Replies.updated_at, "%Y-%m-%d %H:%i:%s")'), 'updatedAt']
+      //     ]
+      //   }
+      // }
+    ],
       attributes: { 
-        exclude: ['UserId'],
+        exclude: ['UserId', 'createdAt', 'updatedAt'],
         include: [
-          Sequelize.literal('(SELECT COUNT(DISTINCT id) FROM Replies WHERE Replies.tweet_id = Tweet.id)'), 'replyCount'
+          [literal('(SELECT COUNT(DISTINCT id) FROM Replies WHERE Replies.tweet_id = Tweet.id)'), 'replyCount'],
+          [Sequelize.literal('DATE_FORMAT(Tweet.created_at, "%Y-%m-%d %H:%i:%s")'), 'createdAt'],
+          [Sequelize.literal('DATE_FORMAT(Tweet.updated_at, "%Y-%m-%d %H:%i:%s")'), 'updatedAt']
         ]
      },
     })
-    .then(tweet => {
-      const modifyTweet = tweet.map((tweets) => {
-        const createdAt = moment(tweets.createdAt).format('YYYY-MM-DD HH:mm:ss')
-        const updatedAt = moment(tweets.updatedAt).format('YYYY-MM-DD HH:mm:ss')
-        
+    .then(tweets => {
+      const modifiedTweets = tweets.map(tweet => {
+        // const modifiedReplies = tweets
+          // .filter(t => t.id === tweet.id)
+          // .map(t => t.Replies);
+
         return {
-          ...tweets,
-          createdAt,
-          updatedAt,
-          }
-      })
-      return res.status(200).json(modifyTweet)
+          ...tweet,
+          // Replies: modifiedReplies
+        };
+      });
+
+      return res.status(200).json(modifiedTweets);
     })
     .catch(err => next(err))
   },
+
   getTweet: (req, res, next) => {
     return Tweet.findByPk(req.params.tweet_id)
     .then(tweet => {
