e31a3d658c13125639f9b74f2ee12bffdcd30bdb
larrytherealboy <abclarryabclarry@gmail.com>
2023-06-15 12:06:58 +0800

    revise: api-auth

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -4,23 +4,25 @@ const passport = require('../config/passport') // 引入 passport
 const authenticated = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) return res.status(401).json({ status: 'error', message: 'unauthorized' })
-    req.user = user
-    delete req.user.dataValues.password
+    req.user = user.dataValues
+    delete req.user.password
     return next()
   })(req, res, next)
 }
 
 // 前台登入驗證
 const authenticatedUser = (req, res, next) => {
-  if (req.user && req.user.role !== 'admin') { 
-    return next() 
+  if (req.user && (req.user.role !== 'admin')) {
+    return next()
   }
   return res.status(403).json({ status: 'error', message: 'permission denied, only for user' })
 }
 
 // 後台登入驗證
 const authenticatedAdmin = (req, res, next) => {
-  if (req.user && req.user.role == 'admin') return next()
+  if (req.user && (req.user.role === 'admin')) {
+    return next()
+  }
   return res.status(403).json({ status: 'error', message: 'permission denied, only for admin' })
 }
 
