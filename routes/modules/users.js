0fc4aba1074649062bc05c45deb9ff2897c5cede
av124773 <av124773@gmail.com>
2023-06-15 17:02:27 +0800

    feat: modify API path

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -12,7 +12,7 @@ router.get('/:userId/replied_tweets', userController.getUserReplies)
 router.get('/:userId/likes', userController.getUserLikes)
 router.get('/:userId/followings', userController.getUserFollowings)
 router.get('/:userId/followers', userController.getUserFollowers)
-router.get('/:account', userController.getUserDataByAccount)
+router.get('/:account/users', userController.getUserDataByAccount)
 router.get('/:id', userController.getUserData)
 router.put('/:id', upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'banner', maxCount: 1 }]), userController.editUserData)
 
