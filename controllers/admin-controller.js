25ffdfb4beb10b1e03ec18fe6f70880f98aac74e
KenYuChang <justin0915justin0915@gmail.com>
2023-06-17 20:32:57 +0800

    fix:deleteTweet

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -8,26 +8,30 @@ const moment = require('moment')
 const adminController = {
   signIn: (req, res, next) => {
     const { account, password } = req.body
-    if (!account || !password) { throw new Error('Account and password are required') }
+    if (!account || !password) {
+      throw new Error('Account and password are required')
+    }
     return User.findOne({
-      where: { account }
+      where: { account },
     })
       .then((user) => {
         if (!user) throw new Error('Admin not exists')
         if (user.role === 'user') throw new Error('Admin not exists')
-        if (!bcrypt.compareSync(password, user.password)) { throw new Error('Password incorrect') }
+        if (!bcrypt.compareSync(password, user.password)) {
+          throw new Error('Password incorrect')
+        }
         const adminData = user.toJSON()
         delete adminData.password
         const token = jwt.sign(adminData, process.env.JWT_SECRET, {
-          expiresIn: '30d'
+          expiresIn: '30d',
         })
         return res.status(200).json({
           status: 'success',
           message: 'Login successful',
           data: {
             token,
-            admin: adminData
-          }
+            admin: adminData,
+          },
         })
       })
       .catch((err) => next(err))
@@ -42,37 +46,23 @@ const adminController = {
       attributes: {
         include: [
           // user data
+          [literal('(SELECT COUNT(id) FROM Tweets WHERE Tweets.user_id = User.id)'), 'tweetCount'],
+          [literal('(SELECT COUNT(id) FROM Likes WHERE Likes.user_id = User.id)'), 'likeCount'],
           [
-            literal(
-              '(SELECT COUNT(id) FROM Tweets WHERE Tweets.user_id = User.id)'
-            ),
-            'tweetCount'
+            literal('(SELECT COUNT(DISTINCT id) FROM Followships WHERE Followships.following_id = User.id)'),
+            'followerCount',
           ],
           [
-            literal(
-              '(SELECT COUNT(id) FROM Likes WHERE Likes.user_id = User.id)'
-            ),
-            'likeCount'
+            literal('(SELECT COUNT(DISTINCT id) FROM Followships WHERE Followships.follower_id = User.id)'),
+            'followingCount',
           ],
-          [
-            literal(
-              '(SELECT COUNT(DISTINCT id) FROM Followships WHERE Followships.following_id = User.id)'
-            ),
-            'followerCount'
-          ],
-          [
-            literal(
-              '(SELECT COUNT(DISTINCT id) FROM Followships WHERE Followships.follower_id = User.id)'
-            ),
-            'followingCount'
-          ]
         ],
-        exclude: ['password', 'createdAt', 'updatedAt', 'role']
+        exclude: ['password', 'createdAt', 'updatedAt', 'role'],
       },
       // sort user data by tweetCount
       order: [[literal('tweetCount'), 'DESC']],
       raw: true,
-      nest: true
+      nest: true,
     })
       .then((users) => {
         if (!users) throw new Error('No users found')
@@ -86,33 +76,23 @@ const adminController = {
       include: [
         {
           model: User,
-          attributes: { exclude: ['password', 'createdAt', 'updatedAt', 'role', 'introduction'] }
-        }
+          attributes: { exclude: ['password', 'createdAt', 'updatedAt', 'role', 'introduction'] },
+        },
       ],
       attributes: {
         include: [
+          [Sequelize.literal('TIMESTAMPDIFF(SECOND, Tweet.created_at, NOW())'), 'diffCreatedAt'],
           [
-            Sequelize.literal('TIMESTAMPDIFF(SECOND, Tweet.created_at, NOW())'),
-            'diffCreatedAt'
+            Sequelize.literal('(SELECT COUNT(DISTINCT id) FROM Replies WHERE Replies.tweet_id = Tweet.id)'),
+            'replyCount',
           ],
-          [
-            Sequelize.literal(
-              '(SELECT COUNT(DISTINCT id) FROM Replies WHERE Replies.tweet_id = Tweet.id)'
-            ),
-            'replyCount'
-          ],
-          [
-            Sequelize.literal(
-              '(SELECT COUNT(DISTINCT id) FROM Likes WHERE Likes.tweet_id = Tweet.id)'
-            ),
-            'likeCount'
-          ]
+          [Sequelize.literal('(SELECT COUNT(DISTINCT id) FROM Likes WHERE Likes.tweet_id = Tweet.id)'), 'likeCount'],
         ],
         order: [['createdAt', 'DESC']],
-        exclude: ['UserId']
+        exclude: ['UserId'],
       },
       nest: true,
-      raw: true
+      raw: true,
     })
       .then((tweets) => {
         if (!tweets) throw new Error('No tweets found')
@@ -124,7 +104,7 @@ const adminController = {
             ...tweet,
             createdAt,
             updatedAt,
-            diffCreatedAt
+            diffCreatedAt,
           }
         })
         return res.status(200).json(processedTweets)
@@ -134,22 +114,21 @@ const adminController = {
 
   deleteTweet: (req, res, next) => {
     return Tweet.findByPk(req.params.id)
-      .then(tweet => {
+      .then((tweet) => {
         // Error: tweet doesn't exist
         if (!tweet) throw new Error('No tweet found')
         // delete related data
-        Promise.all(
+        Promise.all([
           Reply.destroy({ where: { TweetId: req.params.id } }),
-          Like.destroy({ where: { TweetId: req.params.id } })
-        )
+          Like.destroy({ where: { TweetId: req.params.id } }),
+        ])
         // keep the deleted data
         const deletedTweet = tweet.toJSON()
-        return tweet.destroy()
-          .then(() => {
-            return res.status(200).json({ status: 'success', message: 'Tweet deleted', deletedTweet })
-          })
+        return tweet.destroy().then(() => {
+          return res.status(200).json({ status: 'success', message: 'Tweet deleted', deletedTweet })
+        })
       })
-      .catch(err => next(err))
-  }
+      .catch((err) => next(err))
+  },
 }
 module.exports = adminController
