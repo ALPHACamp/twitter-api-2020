13995874238ab0601c408085d465cc8b3b43c94d
WeiWayne1030 <wayne10032004@gmail.com>
2023-06-17 11:12:50 +0800

    fix:limit admin from the seeders data

--- seeders/20230605165733-tweets-seed-file.js
+++ seeders/20230605165733-tweets-seed-file.js
@@ -2,12 +2,13 @@
 const faker = require('faker')
 module.exports = {
   up: async(queryInterface, Sequelize) => {
-        const users = await queryInterface.sequelize.query('SELECT id FROM Users;', {
-            type: queryInterface.sequelize.QueryTypes.SELECT
-            })
+      const users = await queryInterface.sequelize.query(
+      "SELECT id FROM Users WHERE role <> 'admin'",
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
         const tweets = []
         for (const user of users) {
-          for (let i = 0; i < 10; i++) {
+          for (let i = 0; i < 20; i++) {
             tweets.push ({
               UserId: user.id,
               description: faker.lorem.text(),
--- seeders/20230606011048-replies-seed-file.js
+++ seeders/20230606011048-replies-seed-file.js
@@ -3,9 +3,10 @@ const faker = require('faker')
 
 module.exports = {
   up: async(queryInterface, Sequelize) => {
-  const users = await queryInterface.sequelize.query('SELECT id FROM Users;', {
-            type: queryInterface.sequelize.QueryTypes.SELECT
-        })
+  const users = await queryInterface.sequelize.query(
+      "SELECT id FROM Users WHERE role <> 'admin'",
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
   const tweets = await queryInterface.sequelize.query('SELECT id FROM Tweets;', {
             type: queryInterface.sequelize.QueryTypes.SELECT
         })
