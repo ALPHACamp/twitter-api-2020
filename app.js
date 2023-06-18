c2e7c45d01085c8e84d070294ac2d8cc9e565b25
azx79115 <azx79115@gmail.com>
2023-06-18 09:33:30 +0800

    新增cors前端網域

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 const corsOptions = {
-  origin: 'http://localhost:3000', // 允許的網域
+  origin: ['http://localhost:3000', 'https://zionyou.github.io/simple-twitter'], // 允許的網域
   methods: 'GET, POST, PUT, DELETE', // 允許的 HTTP 方法
   allowedHeaders: 'Content-Type, Authorization' // 允許的請求標頭
 }
