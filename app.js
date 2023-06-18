0f64bc1db7675e7f6982fc2b2a2ff5ad739d2fdd
JimmyLin <disillusioned2077@gmail.com>
2023-06-18 18:57:35 +0800

    feat: feature/core-set

--- app.js
+++ app.js
@@ -16,7 +16,20 @@ function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 };
 
-app.use(cors())
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      let msg = 'The CORS policy for this site does not ' +
+        'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
