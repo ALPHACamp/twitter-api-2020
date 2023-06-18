2b46fd532b990ccde358bf7ea69c871de0694a40
CHUCHUDAN <daniel870217@gmail.com>
2023-06-18 13:50:48 +0800

    add cors setting

--- app.js
+++ app.js
@@ -12,16 +12,15 @@ const SESSION_SECRET = process.env.SESSION_SECRET
 
 const app = express()
 const port = process.env.PORT || 3000
-// const corsOptions = {
-//   origin: [
-//     process.env.GITHUB_PAGE,
-//     'http://localhost:3000'
-//   ],
-//   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }
+const corsOptions = {
+  origin: [
+    process.env.GITHUB_PAGE
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
