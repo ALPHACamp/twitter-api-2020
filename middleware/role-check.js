d647af42bd2c93ac31314e16520f26fed60b2458
klu0926 <lukuoyu@gmail.com>
2023-06-19 10:18:42 +0800

    fix

--- middleware/role-check.js
+++ middleware/role-check.js
@@ -13,8 +13,8 @@ const roleCheck = (role, errorMessage) => {
 }
 
 module.exports = {
-  isUser: roleCheck('user', '帳號不存在!'),
-  isAdmin: roleCheck('admin', '帳號不存在!'),
+  isUser: roleCheck('user', '使用者不存在!'),
+  isAdmin: roleCheck('admin', '使用者不存在!'),
   isAuthUser: roleCheck('user', '請先登入帳號!'),
   isAuthAdmin: roleCheck('admin', '請先登入帳號!')
 }
