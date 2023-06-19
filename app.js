2d35c7837909e653732298ddaf48f0618a4c8c30
CHUCHUDAN <daniel870217@gmail.com>
2023-06-19 11:06:15 +0800

    cors test

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const SESSION_SECRET = process.env.SESSION_SECRET
 const app = express()
 const port = process.env.PORT || 3000
 const corsOptions = {
-  origin: process.env.GITHUB_PAGE,
+  origin: [
+    process.env.GITHUB_PAGE,
+    'http://localhost:3000'
+  ],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   allowedHeaders: ['Content-Type', 'Authorization']
 }
