e6d9fce023c4c5ef8640ddf4b37c08aa9b342cc5
JimmyLin <disillusioned2077@gmail.com>
2023-06-26 15:54:34 +0800

    faet: fature/setUser-api

--- controllers/apis/user-controller.js
+++ controllers/apis/user-controller.js
@@ -250,7 +250,7 @@ const userController = {
       .catch(err => res.status(500).json({ status: 'error', error: err.message }))
   },
   getSetUser: (req, res) => {
-    return User.findByPk(req.params.id, { attributes: ['id', 'name', 'account', 'email'] })
+    return User.findByPk(helpers.getUser(req).id, { attributes: ['id', 'name', 'account', 'email'] })
       .then((user) => {
         if (!user) throw new Error("User didn't exist!")
         res.status(200).json(user)
@@ -260,30 +260,28 @@ const userController = {
   putSetUser: (req, res) => {
     const { account, name, email, password, checkPassword } = req.body
     return new Promise((resolve, reject) => {
-      User.findByPk(req.params.id, { attributes: ['id', 'name', 'account', 'email'] })
-        .then(user => {
-          if (!user) reject(new Error("User didn't exist!"))
-          if (!account || !name || !email || !password) reject(new Error("The fields for account, name, password and email are required!"))
-          if (account.length > 50) reject(new Error("Account too long"))
-          if (name && name.length > 50) reject(new Error(`Name too long`))
-          if (password !== checkPassword) reject(new Error("Password do not match"))
-          updateUser = user
-          resolve()
-        })
+      if (!account || !name || !email || !password) reject(new Error("The fields for account, name, password and email are required!"))
+      if (account.length > 50) reject(new Error("Account too long"))
+      if (name && name.length > 50) reject(new Error(`Name too long`))
+      if (password !== checkPassword) reject(new Error("Password do not match"))
+      resolve()
     })
       .then(() => {
         return Promise.all([
+          User.findByPk(helpers.getUser(req).id, { attributes: ['id', 'name', 'account', 'email'] }),
           User.findOne({ where: { email } }),
           User.findOne({ where: { account } })
         ])
       })
-      .then(([userEmail, userAccount]) => {
+      .then(([user, userEmail, userAccount]) => {
         if (userEmail) throw new Error('Email already exists!')
         if (userAccount) throw new Error('Account already registered!')
+        if (!user) throw new Error("User didn't exist!")
+        updatedUser = user
         return bcrypt.hash(password, 10)
       })
       .then((hash) => {
-        return updateUser.update({
+        return updatedUser.update({
           name,
           account,
           email,
