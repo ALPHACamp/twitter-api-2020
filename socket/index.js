d36c3f59c6871ef7df7a971304b82a850f7d6099
kim1037 <pan913063@gmail.com>
2023-06-29 00:13:55 +0800

    feat: add event listner to socket/index.js

--- socket/index.js
+++ socket/index.js
@@ -1,3 +1,19 @@
-module.exports = io => {
+const join = require('./modules/join')
+const leave = require('./modules/leave')
+const message = require('./modules/message')
 
+module.exports = io => {
+  // Fired upon a connetion from client
+  io.on('connection', socket => {
+    console.log(socket.io)
+
+    // user加入聊天室
+    socket.on('client-join', account => join(io, socket, account))
+
+    // user離開聊天室
+    socket.on('client-leave', account => leave(io, socket, account))
+
+    // 當有user發送message時
+    socket.on('client-message', (account, mes) => message(io, socket, account, mes))
+  })
 }
