95842525fdf3cb6b2c174934923bbdd751594109
kim1037 <pan913063@gmail.com>
2023-07-03 10:07:21 +0800

    feat:add room migration

---
+++ migrations/20230703015940-create-room.js
@@ -0,0 +1,32 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Room', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userOneId: {
+        type: Sequelize.INTEGER
+      },
+      userTwoId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Room')
+  }
+}
