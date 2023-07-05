16fe1c8b441641b603f6f9bad6a6e7ab750504f7
Chingsan0722 <rory85722@gmail.com>
2023-07-05 13:43:26 +0800

    try postgre 5

--- models/index.js
+++ models/index.js
@@ -10,8 +10,9 @@ const db = {}
 
 let sequelize
 if (config.use_env_variable) {
-  console.log(process.env.POSTGRESQL_DB_URI)
-  sequelize = new Sequelize(process.env.POSTGRESQL_DB_URI, config)
+  sequelize = new Sequelize(process.env.POSTGRESQL_DB_URI, {
+    dialect: 'postgres'
+  })
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
