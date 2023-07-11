7dd35dd6d9c509cb9af75f4f0cd6eefcfff145d6
kim1037 <pan913063@gmail.com>
2023-07-11 18:04:39 +0800

    fix: return latest message of each room

--- socket/modules/newMessage.js
+++ socket/modules/newMessage.js
@@ -4,6 +4,8 @@ const { Op, literal } = require('sequelize')
 
 module.exports = async (io, socket) => {
   try {
+    // 目前bug為 sendMessage如果輸入原本沒有的roomId，這邊沒辦法回傳新聊天室的資料回去
+    
     // 確認使用者是否登入
     const currentUser = findUserInPublic(socket.id, 'socketId')
     if (!currentUser) throw new Error('You need to client-join first')
@@ -21,14 +23,19 @@ module.exports = async (io, socket) => {
         { model: User, attributes: ['id', 'name', 'account', 'avatar'] }
       ],
       attributes: ['id', 'message', 'roomId', 'timestamp'],
-      order: [['roomId'], ['id', 'DESC']], // 最新的訊息在最上方
-      group: ['roomId'] // 用group只會回傳第一筆,不會照時間排序 bug待修
+      order: [['id', 'DESC']] // 最新的訊息在最上方
+    })
+    // 暫時想不到直接在DB處理的方式，先抓出來處理
+    const newMessageData = []
+    newMessage.forEach(m => {
+      const roomIdExist = newMessageData.some(d => d.roomId === m.roomId)
+      if (!roomIdExist) newMessageData.push(m.toJSON())
     })
 
     // 以上做完要在做顯示未讀訊息的數量
     const unreadMessage = 0
     // 回傳最新訊息
-    io.emit('server-new-message', newMessage)
+    io.emit('server-new-message', newMessageData)
   } catch (err) {
     emitError(socket, err)
   }
