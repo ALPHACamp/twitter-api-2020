4c5647d83c63b48cf29abff99d0388777f8c749f
klu0926 <lukuoyu@gmail.com>
2023-07-17 18:19:28 +0800

    fix: leaveRoom use currentRoom as roomId

--- socket/modules/leaveRoom.js
+++ socket/modules/leaveRoom.js
@@ -1,21 +1,20 @@
 const { emitError, findUserInPublic } = require('../helper')
 const readEvent = require('./read')
 
-module.exports = async (socket, roomId) => {
+module.exports = async socket => {
   try {
     // find user
     const user = findUserInPublic(socket.id, 'socketId')
 
     // not in room
     if (!user.currentRoom) throw new Error('You are not currently in a room')
-    if (user.currentRoom !== roomId) throw new Error(`You are not currently in ${roomId} room`)
 
     // read again before leave
-    readEvent(socket, roomId, user.id)
+    readEvent(socket, user.currentRoom, user.id)
 
     // cancel currentRoom
+    socket.emit('server-leave-room', `leave room ${user.currentRoom}, read once`)
     delete user.currentRoom
-    socket.emit('server-leave-room', `leave room ${roomId}, read once`)
   } catch (err) {
     emitError(socket, err)
   }
