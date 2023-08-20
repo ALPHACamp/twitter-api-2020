c02af37845f713a6de51d41138357eeeca7c772a
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-08-21 01:27:21 +0800

    feat: add  acount,baner to  user model/migration
    增加User的欄位，並建立關係

--- models/user.js
+++ models/user.js
@@ -1,14 +1,41 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    account: DataTypes.STRING,
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     introduction: DataTypes.TEXT,
-    role: DataTypes.STRING,
-    avatar: DataTypes.STRING
-  }, {})
+    avatar: DataTypes.STRING,
+    banner: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    tableName: 'Users',
+    underscored: true
+  })
+
   User.associate = function (models) {
+    User.hasMany(models.Tweet, { foreignKey: 'UserId', as: 'WroteTweet' })
+
+    User.belongsToMany(models.Tweet, {
+      through: models.Like,
+      foreignKey: 'UserId',
+      as: 'LikedTweet'
+    })
+
+    User.hasMany(models.Reply, { foreignKey: 'UserId' })
+
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followingId',
+      as: 'Followers'
+    })
+
+    User.belongsToMany(User, {
+      through: models.Followship,
+      foreignKey: 'followerId',
+      as: 'Followings'
+    })
   }
   return User
 }
