94170173339cc80ea3d1262c60d36917c34b24c2
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-08-22 06:44:23 +0800

    fix: solve not returning id on create

--- models/like.js
+++ models/like.js
@@ -10,6 +10,13 @@ module.exports = (sequelize, DataTypes) => {
   }
 
   Like.init({
+    id: {
+      field: 'id',
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
     UserId: DataTypes.INTEGER,
     TweetId: DataTypes.INTEGER
   }, {
@@ -21,3 +28,27 @@ module.exports = (sequelize, DataTypes) => {
 
   return Like
 }
+
+// 'use strict'
+// const { Model } = require('sequelize')
+
+// module.exports = (sequelize, DataTypes) => {
+//   class Like extends Model { }
+
+//   Like.associate = function (models) {
+//     Like.belongsTo(models.User, { foreignKey: 'UserId' })
+//     Like.belongsTo(models.Tweet, { foreignKey: 'TweetId' })
+//   }
+
+//   Like.init({
+//     UserId: DataTypes.INTEGER,
+//     TweetId: DataTypes.INTEGER
+//   }, {
+//     sequelize,
+//     modelName: 'Like',
+//     tableName: 'Likes',
+//     underscored: false
+//   })
+
+//   return Like
+// }
--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,20 @@ const apiErrorHandler = require('../middleware/error-handler')
 
 const userController = require('../controllers/user-controller')
 
+const { User, Like } = require('../models')
+
 router.post('/users', userController.signUp) // 註冊帳號
 router.post('/users/signin', passport.authenticate('local', { session: false }), userController.signIn) // 登入前台帳號
+
+router.get('/test', async (req, res, next) => {
+  const like = await Like.create({})
+  console.log(like.toJSON())
+
+  const user = await User.create({})
+  console.log(user.toJSON())
+
+  return res.json({ data: like.toJSON() })
+})
 
 router.use('/', (req, res) => res.status(500).json({ status: 'error', message: 'no such api' })) // fallback路由
 router.use('/', apiErrorHandler) // 錯誤處理
