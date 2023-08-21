e9c90a9862365263d23c041a7d0075904d634621
DannyHucc <dannyhu10011101@gmail.com>
2023-08-21 21:32:54 +0800

    feat: add error handler middleware

---
+++ middleware/error.handler.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiErrorHandler(err, req, res, next) {
+        const code = err.status || 500
+        res.status(code).json({
+            status: 'error',
+            message: `${err.message}`
+        })
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
-'use strict'
-
 const router = require('express').Router()
+
+const { apiErrorHandler } = require('../middleware/error-handler')
+
+// error handler
+router.use('/', apiErrorHandler)
 
 module.exports = router
