ec81bfe01bee80f29d3bccd37586366c325c3a98
TCY1020 <a25023423@gmail.com>
2023-08-21 18:41:39 +0800

    style:  white-space

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,8 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const adminController = require('../controllers/admin-controller')
-<<<<<<< HEAD
 const { authenticated, authenticatedAdmin, authenticatedUser} = require('../middleware/auth')
-=======
 const tweetController = require('../controllers/tweet-controller')
->>>>>>> 625ef042457af15dd198ebbb6a45ffed0acf6b0f
 const { apiErrorHandler } = require('../middleware/error-handler')
 
 router.post('/api/admin/login', adminController.signIn)
