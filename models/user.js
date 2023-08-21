d20a95e333118f9f5c26d9875f06f0d5ba061a69
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-08-21 14:51:38 +0800

    feat: rewrite user model

--- models/user.js
+++ models/user.js
@@ -1,18 +1,8 @@
 'use strict'
+const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    account: DataTypes.STRING,
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    introduction: DataTypes.TEXT,
-    avatar: DataTypes.STRING,
-    banner: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    tableName: 'Users',
-    underscored: true
-  })
+  class User extends Model {}
 
   User.associate = function (models) {
     User.hasMany(models.Tweet, { foreignKey: 'UserId', as: 'WroteTweet' })
@@ -24,6 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     })
 
     User.hasMany(models.Reply, { foreignKey: 'UserId' })
+    User.hasMany(models.Like, { foreignKey: 'UserId' })
 
     User.belongsToMany(User, {
       through: models.Followship,
@@ -37,5 +28,26 @@ module.exports = (sequelize, DataTypes) => {
       as: 'Followings'
     })
   }
+
+  User.init({
+    account: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'EmptyAccount'
+    },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    introduction: DataTypes.TEXT,
+    avatar: DataTypes.STRING,
+    banner: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: false // 和之前的教案不同，此次不使用underscored，如createdAt
+  })
+
   return User
 }
