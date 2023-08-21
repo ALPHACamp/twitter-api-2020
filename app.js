093c203b5067db7c74d376696f4c35355b1efa54
mmm999xp <mmm999xp@gmail.com>
2023-08-21 20:10:41 +0800

    project init

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const helpers = require('./_helpers');
 const app = express()
 const port = 3000
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 // use helpers.getUser(req) to replace req.user
 function authenticated(req, res, next){
   // passport.authenticate('jwt', { ses...
 };
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`http://localhost:${port}`))
 module.exports = app
