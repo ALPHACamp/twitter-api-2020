d50ef38903f127aabb3e8cb42ebb2157aa344c3c
muco0521 <muco0521@gmail.com>
2023-08-23 02:41:39 +0800

    feat: add reply seeder

---
+++ seeders/20230822175856-replies-seed-file.js
@@ -0,0 +1,31 @@
+'use strict'
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users WHERE role = "user";',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const tweets = await queryInterface.sequelize.query(
+      'SELECT id FROM Tweets;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    const replies = []
+    tweets.forEach(tweet => replies.push(
+      ...Array.from({ length: 3 }, () => ({
+        UserId: users[Math.floor(Math.random() * users.length)].id,
+        TweetId: tweet.id,
+        comment: faker.lorem.sentence(),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }))
+    ))
+
+    await queryInterface.bulkInsert('Replies', replies)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Replies', {})
+  }
+}
