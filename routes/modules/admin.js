be5ac833dded7fd1b68ec4f5037cd7014f35b143
TCY1020 <a25023423@gmail.com>
2023-08-22 13:19:09 +0800

    feat: adminGetUsers

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/users', adminController.getUsers)
+router.get('/tweets', adminController.getAdminTweets)
 
 module.exports = router
