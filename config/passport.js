9f2510dfe6f8479a1011683c3be02b76d6902cce
mmm999xp <mmm999xp@gmail.com>
2023-08-22 22:56:19 +0800

    feat: passport Strategy

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,72 @@
 const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const passportJWT = require('passport-jwt')
+const bcrypt = require('bcryptjs')
+const { User } = require('../models')
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+// 本地策略
+passport.use(new LocalStrategy(
+  // customize user field
+  {
+    usernameField: 'account',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  // authenticate user
+  (req, email, password, cb) => {
+    User.findOne({ where: { account } })
+      // 如果找不到帳號
+      .then(user => {
+        if (!user) {
+          const err = new Error("帳號不存在！")
+          err.status = 401
+          throw err
+        }
+        bcrypt.compare(password, user.password).then(res => {
+          // 如果密碼錯誤也提示帳號不存在
+          if (!user) {
+            const err = new Error("帳號不存在！")
+            err.status = 401
+            throw err
+          }
+          return cb(null, user)
+        })
+      })
+      .catch(err => cb(err))
+  }
+))
+// JWT
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
+// JWT
+passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+  User.findByPk(jwtPayload.id, {
+    include: [
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+    .then(user => cb(null, user))
+    .catch(err => cb(err))
+}))
 
-
-
+// API版本不會走序列化與反序列化
+// 序列化
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+// 反序列化
+passport.deserializeUser((id, cb) => {
+  return User.findByPk(id, {
+    include: [
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+    .then(user => cb(null, user.toJSON()))
+    .catch(err => cb(err))
+})
 module.exports = passport
