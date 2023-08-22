60675320c2edd93bd06f56efd6adbdeb6f69a49f
Keiko <keitakeiko@hotmail.com>
2023-08-22 18:28:39 +0800

    feat: add multer function

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
+const flash = require('connect-flash')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,6 +14,8 @@ const port = process.env.PORT || 3000
 function authenticated(req, res, next){
   // passport.authenticate('jwt', { ses...
 };
+
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 //app.listen(port, () => console.log(`Example app listening on port ${port}!`))
