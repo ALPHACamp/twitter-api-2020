88effef13c90e3e3853721982f479b79b0c09545
DannyHucc <dannyhu10011101@gmail.com>
2023-08-22 13:26:29 +0800

    feat: add reply model git merge --squash feature/reply

--- models/reply.js
+++ models/reply.js
@@ -1,8 +1,28 @@
 'use strict'
+
+const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {
-  }, {})
-  Reply.associate = function (models) {
-  }
+  class Reply extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+      Reply.belongsTo(models.User, { foreignKey: 'UserId' })
+      Reply.belongsTo(models.Tweet, { foreignKey: 'TweetId' })
+    }
+  };
+  Reply.init({
+    comment: DataTypes.TEXT,
+    UserId: DataTypes.INTEGER,
+    TweetId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Reply',
+    tableName: 'Replies'
+  })
   return Reply
 }
