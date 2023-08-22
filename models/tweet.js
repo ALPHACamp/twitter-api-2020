1a860650b0696f13bd90f14a1a014985dfefa066
susu725 <zxcv1010017@gmail.com>
2023-08-22 21:23:09 +0800

    refactor: modify tweet model init method

--- models/tweet.js
+++ models/tweet.js
@@ -1,20 +1,25 @@
 'use strict';
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  const Tweet = sequelize.define('Tweet', {
+  class Tweet extends Model {
+    static associate(models) {
+      Tweet.belongsTo(models.User, { foreignKey: 'UserId' })
+      Tweet.hasMany(models.Reply, { foreignKey: 'TweetId' })
+      Tweet.hasMany(models.Like, { foreignKey: 'TweetId' })
+      Tweet.belongsToMany(models.User, {
+        through: models.Like,
+        foreignKey: 'TweetId',
+        as: 'LikeUsers'
+      })
+    }
+  }
+  Tweet.init({
     userId: DataTypes.INTEGER,
     description: DataTypes.TEXT,
   }, {
+    sequelize,
     modelName: 'Tweet',
     tableName: 'Tweets',
-  });
-  Tweet.associate = function (models) {
-    Tweet.belongsTo(models.User, { foreignKey: 'UserId' })
-    Tweet.hasMany(models.Reply, { foreignKey: 'TweetId' })
-    Tweet.belongsToMany(models.User,{
-      through: models.Like,
-      foreignKey: 'TweetId',
-      as: 'LikeUsers'
-    })
-  };
+  })
   return Tweet;
 };
