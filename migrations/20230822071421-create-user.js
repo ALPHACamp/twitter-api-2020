5ffe74cd7f884f00eb1ccdf811702841732b1a6f
DannyHucc <dannyhu10011101@gmail.com>
2023-08-22 11:24:51 +0800

    feat: add user model git merge --squash feature/user-models

--- migrations/20230822071421-create-user.js
+++ migrations/20230822071421-create-user.js
@@ -1,4 +1,5 @@
 'use strict'
+
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('Users', {
@@ -8,23 +9,30 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
+      name: {
+        type: Sequelize.STRING
+      },
+      account: {
+        type: Sequelize.STRING
+      },
       email: {
         type: Sequelize.STRING
       },
       password: {
         type: Sequelize.STRING
       },
-      name: {
-        type: Sequelize.STRING
-      },
       avatar: {
         type: Sequelize.STRING
       },
-      introduction: {
-        type: Sequelize.TEXT
+      cover: {
+        type: Sequelize.STRING
       },
       role: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        defaultValue: 'user'
+      },
+      introduction: {
+        type: Sequelize.TEXT
       },
       createdAt: {
         allowNull: false,
