b3bf32ca4a004ed0b83c34b3213905d8fb9cb17d
DannyHucc <dannyhu10011101@gmail.com>
2023-08-22 13:43:43 +0800

    feat: add followship model git merge --squash feature/followship-model

--- models/followship.js
+++ models/followship.js
@@ -1,8 +1,25 @@
 'use strict'
+
+const { Model } = require('sequelize')
+
 module.exports = (sequelize, DataTypes) => {
-  const Followship = sequelize.define('Followship', {
-  }, {})
-  Followship.associate = function (models) {
-  }
+  class Followship extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  };
+  Followship.init({
+    followerId: DataTypes.INTEGER,
+    followingId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Followship',
+    tableName: 'Followships'
+  })
   return Followship
 }
