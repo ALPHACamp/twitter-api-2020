8a0f86830d5552bd72fb575d4ba2b6b5969cda01
Sean Chen <seanchen112@gmail.com>
2023-08-22 22:21:35 +0800

    feat: admin login

--- config/passport.js
+++ config/passport.js
@@ -7,27 +7,19 @@ passport.use(
   new LocalStrategy(
     // customize user field
     {
-      usernameField: 'email',
+      usernameField: 'account',
       passwordField: 'password',
-      passReqToCallback: true
+      passReqToCallback: false
     },
     // authenticate user
-    (req, email, password, cb) => {
-      User.findOne({ where: { email } }).then(user => {
+    (account, password, cb) => {
+      User.findOne({ where: { account } }).then(user => {
         if (!user) {
-          return cb(
-            null,
-            false,
-            req.flash('error_messages', '帳號或密碼輸入錯誤！')
-          )
+          return cb(null, false, { message: '帳號或密碼輸入錯誤！' })
         }
         bcrypt.compare(password, user.password).then(res => {
           if (!res) {
-            return cb(
-              null,
-              false,
-              req.flash('error_messages', '帳號或密碼輸入錯誤！')
-            )
+            return cb(null, false, { message: '帳號或密碼輸入錯誤！' })
           }
           return cb(null, user)
         })
@@ -39,10 +31,8 @@ passport.use(
 passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
-passport.deserializeUser((id, cb) => {
-  User.findByPk(id).then(user => {
-    console.log(user) // 暫時添加
-    return cb(null, user.toJSON())
-  })
+passport.deserializeUser(async (id, cb) => {
+  const user = await User.findByPk(id)
+  return cb(null, user.toJSON())
 })
 module.exports = passport
