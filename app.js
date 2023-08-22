47f2d0b223501a7a46609faae8e42c6b63d1e521
DannyHucc <dannyhu10011101@gmail.com>
2023-08-22 15:15:48 +0800

    modify: add api router path in app.js

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
-const routes = require('./routes')
 const methodOverride = require('method-override')
-const passport = require('./config/passport')
-const app = express()
 
+const routes = require('./routes')
+const passport = require('./config/passport')
+
+const app = express()
 const port = process.env.PORT || 3000
 
 // middleware
@@ -17,8 +18,8 @@ app.use(passport.initialize())
 app.use(methodOverride('_method'))
 
 // routes
+app.use('/api', routes)
 app.get('/', (req, res) => { res.send('Hello World!') })
-app.use(routes)
 
 // start
 app.listen(port, () => console.log(`Server running on port:${port}!`))
