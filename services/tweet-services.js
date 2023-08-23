adf0026785e1f950091577bd8e59ef2ae8b08fa8
susu725 <zxcv1010017@gmail.com>
2023-08-23 11:44:18 +0800

    feat: add dayjs and association in getTweets

--- services/tweet-services.js
+++ services/tweet-services.js
@@ -1,9 +1,39 @@
-const { Tweet } = require('../models')
+const { Tweet, User, Reply, Like } = require('../models')
+const { relativeTimeFormat } = require('../helpers/day-helpers')
 
 const tweetServices = {
     getTweets: (req, cb) => {
-        Tweet.findAll({ raw: true })
-            .then(tweets => cb(null, { tweets }))
+        Tweet.findAll({
+            raw: true,
+            include: [
+                { model: User, attributes: ['avatar', 'name', 'account'] },
+                { model: Reply, attributes: ['tweetId'] },
+                { model: Like, attributes: ['tweetId'] }
+            ],
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
+            .then(tweets => {
+                let Count = (value, arr) => arr.reduce((t, c) => c === value ? t + 1 : t, 0)
+                const replyList = []
+                const likeList = []
+                tweets.map(tweet => {
+                    replyList.push(tweet["Replies.tweetId"])
+                    likeList.push(tweet["Likes.tweetId"])
+                })
+                const data = tweets.map(tweet => {
+                    let subDescription = tweet.description.length > 80 ? tweet.description.substring(0, 80) + '...' : tweet.description
+                    return {
+                        ...tweet,
+                        description: subDescription,
+                        createdAt: relativeTimeFormat(tweet.createdAt),
+                        replyCount: Count(tweet.id, replyList),
+                        likeCount: Count(tweet.id, likeList)
+                    }
+                })
+                return cb(null, data)
+            })
             .catch(err => cb(err))
     },
     getTweet: (req, cb) => {
@@ -25,6 +55,7 @@ const tweetServices = {
         const { description } = req.body
         if (!UserId) throw new Error('用戶不存在！')
         if (!description) throw new Error('內容不可空白')
+        if (description.length > 140) throw new Error('內容不得超過140字！')
         return Tweet.create({ description, UserId })
             .then(newTweet => cb(null, { tweet: newTweet }))
             .catch(err => cb(err))
