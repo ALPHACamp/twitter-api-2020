c16f171797ffe114a59cf5e54e5046a68e0427b9
susu725 <zxcv1010017@gmail.com>
2023-08-24 00:37:07 +0800

    feat: add addFollowing and removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const adminController = require('../controllers/admin-controller')
-const { authenticated, authenticatedAdmin, authenticatedUser} = require('../middleware/auth')
+const { authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/auth')
 const tweetController = require('../controllers/tweet-controller')
 const replyController = require('../controllers/reply-controller')
+const followshipController = require('../controllers/followship-controller')
 const { apiErrorHandler } = require('../middleware/error-handler')
 
 router.post('/api/admin/login', adminController.signIn)
@@ -23,6 +24,10 @@ router.get('/api/tweets', tweetController.getTweets)
 // 留言
 router.post('/api/tweets/:TweetId/replies', replyController.postReply)
 router.get('/api/tweets/:TweetId/replies', replyController.getReplies)
+
+// 追蹤
+router.post('/api/followships', authenticated, authenticatedUser, followshipController.addFollowing)
+router.delete('/api/followships/:followingId', authenticated, authenticatedUser, followshipController.removeFollowing)
 
 router.use('/', apiErrorHandler)
 
