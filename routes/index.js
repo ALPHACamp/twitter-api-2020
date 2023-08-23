d6af68c570c1a288c878ef11953965a6259405ff
yiching-hsu <ychsu7149@gmail.com>
2023-08-24 02:59:21 +0800

    feat: add user signin and signup

--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,21 @@ const userController = require('../controllers/apis/user-controller')
 const tweetContorller = require('../controllers/apis/tweet-controller')
 
 const { apiErrorHandler } = require('../middleware/error-handler')
-const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
+const { authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/api-auth')
 
 // api/admin
 router.use('/api/admin', authenticated, authenticatedAdmin, admin)
 
 // api/users
 // router.get('/api/users', userController.getUsers)
-// router.get('/api/signup', userController.signUpPage)
-router.post('/api/signup', userController.signUp)
-router.get('/api/users/:id', userController.getUser)
+router.post('/api/users', userController.signUp)
+router.post('/api/users/signin', userController.signIn)
+router.get('/api/users/:id', authenticated, authenticatedUser, userController.getUser)
 
 // api/tweets
 router.get('/api/tweets/:tweet_id', tweetContorller.getTweet)
 router.get('/api/tweets', tweetContorller.getTweets)
 
-// router.get("/restaurants", restController.getRestaurants);
-// router.use("/", (req, res) => res.redirect("/restaurants"));
 
 router.use('/', apiErrorHandler)
 
