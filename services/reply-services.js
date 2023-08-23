87d79e4914fe22758366cfb19e202b2820a168d3
susu725 <zxcv1010017@gmail.com>
2023-08-23 16:34:16 +0800

    fix: modify UserId in postReply from req

--- services/reply-services.js
+++ services/reply-services.js
@@ -1,8 +1,9 @@
 const { User, Tweet, Reply } = require('../models')
+const { getUser } = require('../_helpers')
 
 const replyServices = {
   getReplies: (req, cb) => {
-    const { TweetId } = req.params
+    const TweetId = req.params.tweet_id
     Reply.findAll({
       where: { TweetId },
       raw: true,
@@ -13,29 +14,16 @@ const replyServices = {
       .catch(err => cb(err))
   },
   postReply: (req, cb) => {
-    const { TweetId } = req.params
-    // 因應登入機制相關問題，暫時使用固定的UserId
-    const UserId = 3
+    const UserId = getUser(req).id
+    const TweetId = req.params.tweet_id
     const { comment } = req.body
     Promise.all([
       Tweet.findByPk(TweetId),
       User.findByPk(UserId)
     ]).then(([tweet, user]) => {
-      if (!tweet) {
-        const err = new Error('推文不存在！')
-        err.status = 404
-        throw err
-      }
-      if (!user) {
-        const err = new Error('用戶不存在！')
-        err.status = 404
-        throw err
-      }
-      if (!comment) {
-        const err = new Error('內容不可空白')
-        err.status = 404
-        throw err
-      }
+      if (!tweet) throw new Error('推文不存在！')
+      if (!user) throw new Error('用戶不存在！')
+      if (!comment) throw new Error('內容不可空白')
       return Reply.create({ comment, UserId, TweetId })
     })
       .then(newReply => cb(null, { reply: newReply }))
