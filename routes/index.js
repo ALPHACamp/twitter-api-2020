93d4a5e42507d078ba05c30a61b4a9a121de2ef3
susu725 <zxcv1010017@gmail.com>
2023-08-23 16:33:30 +0800

    feat: add auth in replies-function

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const adminController = require('../controllers/admin-controller')
-const { authenticated, authenticatedAdmin, authenticatedUser} = require('../middleware/auth')
+const { authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/auth')
 const tweetController = require('../controllers/tweet-controller')
 const replyController = require('../controllers/reply-controller')
 const { apiErrorHandler } = require('../middleware/error-handler')
@@ -21,8 +21,8 @@ router.get('/api/tweets/:id', tweetController.getTweet)
 router.get('/api/tweets', tweetController.getTweets)
 
 // 留言
-router.post('/api/tweets/:TweetId/replies', replyController.postReply)
-router.get('/api/tweets/:TweetId/replies', replyController.getReplies)
+router.post('/api/tweets/:tweet_id/replies', authenticated, authenticatedUser, replyController.postReply)
+router.get('/api/tweets/:tweet_id/replies', authenticated, authenticatedUser, replyController.getReplies)
 
 router.use('/', apiErrorHandler)
 
