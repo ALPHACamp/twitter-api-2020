c6cb3a482a20aba303081ff4a2ebf0fd6120b59c
susu725 <zxcv1010017@gmail.com>
2023-08-23 11:45:24 +0800

    feat: add auth in tweets-function

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const adminController = require('../controllers/admin-controller')
-const { authenticated, authenticatedAdmin, authenticatedUser} = require('../middleware/auth')
+const { authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/auth')
 const tweetController = require('../controllers/tweet-controller')
 const replyController = require('../controllers/reply-controller')
 const { apiErrorHandler } = require('../middleware/error-handler')
@@ -13,9 +13,9 @@ router.use('/api/admin', authenticated, authenticatedAdmin, admin)
 router.post('/api/users/login', userController.signIn)
 
 // 推文
-router.post('/api/tweets', tweetController.postTweet)
-router.get('/api/tweets/:id', tweetController.getTweet)
-router.get('/api/tweets', tweetController.getTweets)
+router.post('/api/tweets', authenticated, authenticatedUser, tweetController.postTweet)
+router.get('/api/tweets/:id', authenticated, authenticatedUser, tweetController.getTweet)
+router.get('/api/tweets', authenticated, authenticatedUser, tweetController.getTweets)
 
 // 留言
 router.post('/api/tweets/:TweetId/replies', replyController.postReply)
