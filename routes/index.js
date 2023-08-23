4a701739fd3c4bd914be9a1c7321dcf51485315b
Sean Chen <seanchen112@gmail.com>
2023-08-23 15:11:35 +0800

    fix: add engines to package.json

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const admin = require("./modules/admin");
+const admin = require('./modules/admin')
 
-const userController = require("../controllers/apis/user-controller");
-const tweetContorller = require("../controllers/apis/tweet-controller");
+const userController = require('../controllers/apis/user-controller')
+const tweetContorller = require('../controllers/apis/tweet-controller')
 
-const { apiErrorHandler } = require("../middleware/error-handler");
+const { apiErrorHandler } = require('../middleware/error-handler')
 
 // api/admin
-router.use("/api/admin", admin);
+router.use('/api/admin', admin)
 
 // api/users
 // router.get('/api/users', userController.getUsers)
 // router.get('/api/signup', userController.signUpPage)
-router.post("/api/signup", userController.signUp);
+router.post('/api/signup', userController.signUp)
 
 // api/tweets
-router.get("/api/tweets/:tweet_id", tweetContorller.getTweet);
-router.get("/api/tweets", tweetContorller.getTweets);
+router.get('/api/tweets/:tweet_id', tweetContorller.getTweet)
+router.get('/api/tweets', tweetContorller.getTweets)
 
 // router.get("/restaurants", restController.getRestaurants);
 // router.use("/", (req, res) => res.redirect("/restaurants"));
 
-// router.use("/", apiErrorHandler);
+router.use('/', apiErrorHandler)
 
-module.exports = router;
+module.exports = router
