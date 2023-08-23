4fc2d20929b0a1b4bb30ec6d5b2236ab21c7c921
WinstonAdams <apollo07291@gmail.com>
2023-08-23 17:27:27 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const routes = require('./routes')
 const passport = require('./config/passport')
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -16,6 +17,7 @@ app.use(express.json())
 // 初始化 passport
 app.use(passport.initialize())
 
+app.use(cors())
 // Set routes
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('../config/passport')
+const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const adminController = require('../controllers/admin-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
 router.post('/api/users/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.post('/api/users', userController.signUp)
 
 router.post('/api/admin/signin', passport.authenticate('local', { session: false }), adminController.signIn)
+router.use('/api/admin', authenticated, authenticatedAdmin, admin)
 
 router.get('/', (req, res) => res.send('hello world'))
 
