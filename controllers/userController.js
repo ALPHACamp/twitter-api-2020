8cf5aaaeb7bbab05eaf55962b6d600b3341b85c2
DannyHucc <dannyhu10011101@gmail.com>
2023-08-23 16:44:44 +0800

    feat: add put user profile feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,7 @@ const sequelize = require('sequelize')
 const { User, Tweet, Reply, Like, Followship } = require('../models')
 const { Op } = require('sequelize')
 const { getUser } = require('../_helpers')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
   signUp: async (req, res, next) => {
@@ -307,6 +308,32 @@ const userController = {
       })).sort((a, b) => b.isFollowed - a.isFollowed)
       res.status(200).json(data)
     } catch (err) { next(err) }
+  },
+
+  putUserProfile: async (req, res, next) => {
+    let { name, introduction } = req.body
+    name = name?.trim()
+    introduction = introduction?.trim()
+
+    if (name?.length > 50) return res.status(400).json({ status: 'error', message: '暱稱上限 50 字' })
+    if (introduction?.length > 160) return res.status(400).json({ status: 'error', message: '自我介紹上限 160 字' })
+
+    const avatar = req.files?.avatar?.[0] || null
+    const cover = req.files?.cover?.[0] || null
+    const [user, avatarFilePath, coverFilePath] = await Promise.all([User.findByPk(req.params.id),
+      imgurFileHandler(avatar),
+      imgurFileHandler(cover)
+    ])
+
+    if (!user) return res.status(404).json({ status: 'error', message: '使用者不存在' })
+    const data = await user.update({
+      name,
+      introduction,
+      avatar: avatarFilePath || user.avatar,
+      cover: coverFilePath || user.cover
+    })
+    delete data.dataValues.password
+    return res.status(200).json(data)
   }
 }
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -6,8 +6,10 @@ const router = express.Router()
 const passport = require('../../config/passport')
 const userController = require('../../controllers/userController')
 const { authenticated } = require('../../middleware/auth')
-const { signInValidator, signUpValidator } = require('../../middleware/validator')
 const { isUser, isAuthUser } = require('../../middleware/role-check')
+const { signInValidator, signUpValidator } = require('../../middleware/validator')
+const upload = require('../../middleware/multer')
+const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'cover', maxCount: 1 }])
 
 // signup & signin
 router.post('/signin', signInValidator, passport.authenticate('local', { session: false }), isUser, userController.signIn)
@@ -20,5 +22,6 @@ router.get('/:id/likes', authenticated, isAuthUser, userController.getUserLike)
 router.get('/:id/followings', authenticated, isAuthUser, userController.getUserfollowing)
 router.get('/:id/followers', authenticated, isAuthUser, userController.getUserFollower)
 router.get('/:id', authenticated, isAuthUser, userController.getUser)
+router.put('/:id', authenticated, isAuthUser, cpUpload, userController.putUserProfile)
 
 module.exports = router
