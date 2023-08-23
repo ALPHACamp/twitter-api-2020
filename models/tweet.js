6600684e26364d22fabcfafc05fa7c89c01f1057
susu725 <zxcv1010017@gmail.com>
2023-08-23 15:56:05 +0800

    refactor: modify like model init method

--- models/tweet.js
+++ models/tweet.js
@@ -14,7 +14,7 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Tweet.init({
-    userId: DataTypes.INTEGER,
+    UserId: DataTypes.INTEGER,
     description: DataTypes.TEXT
   }, {
     sequelize,
