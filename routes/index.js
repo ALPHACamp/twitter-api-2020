e83d1c0a8fbcc3642e805cc6d610f7488c1de0dc
realyutou <ssps9430@gmail.com>
2023-08-23 11:55:09 +0800

    feat: add admin signin function

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ const router = express.Router()
 
 const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
+const adminController = require('../controllers/admin-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.post('/api/users/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.post('/api/users', userController.signUp)
+
+router.post('/api/admin/signin', passport.authenticate('local', { session: false }), adminController.signIn)
 
 router.get('/', (req, res) => res.send('hello world'))
 
