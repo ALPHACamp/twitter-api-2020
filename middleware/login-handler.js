6bef641f2e365d24e4d279caa300a5b6bbf8ada0
Yen Lin <lynn575938@outlook.com>
2023-08-24 23:50:01 +0800

    feat: add admin signin api

---
+++ middleware/login-handler.js
@@ -0,0 +1,24 @@
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
+
+const adminSignIn = async (req, res, next) => {
+  try {
+    const { account, password } = req.body
+    if (!account || !password) throw new Error('帳號與密碼皆為必填！')
+    // 在資料庫尋找admin
+    const admin = await User.findOne({ where: { account } })
+    // 驗證資訊
+    if (!admin) throw new Error('使用者不存在！')
+    if (admin.role !== 'admin') throw new Error('使用者不存在！')
+    if (!bcrypt.compareSync(password, admin.password)) throw new Error('帳號或密碼錯誤！')
+
+    req.user = admin
+    next()
+  } catch (err) {
+    next(err)
+  }
+}
+
+module.exports = {
+  adminSignIn
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+const adminController = require('../controllers/admin-controller')
+
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
-const { apiErrorHandler } = require('../../middleware/error-handler')
+const { apiErrorHandler } = require('../middleware/error-handler')
+const { adminSignIn } = require('../middleware/login-handler')
+
+// admin登入
+router.post('/admin/signin', adminSignIn, adminController.signIn)
 
 router.use('/', apiErrorHandler)
+
 module.exports = router
