d02016ce2f3cc1e9cf9d673fcd74e58d44e8cfec
realyutou <ssps9430@gmail.com>
2023-08-24 12:18:00 +0800

    feat: add unlikeTweet function

--- routes/modules/tweets.js
+++ routes/modules/tweets.js
@@ -4,6 +4,8 @@ const tweetController = require('../../controllers/tweet-controller')
 
 router.post('/:id/like', tweetController.likeTweet)
 
+router.post('/:id/unlike', tweetController.unlikeTweet)
+
 router.get('/:tweet_id/replies', tweetController.getTweetReplies)
 
 router.post('/:tweet_id/replies', tweetController.postTweetReply)
