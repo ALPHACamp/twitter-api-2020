4cab0efa0bd6353757767545ca15cd14dc96019d
WinstonAdams <apollo07291@gmail.com>
2023-08-24 14:02:00 +0800

    feat: add unfollowUser function

--- controllers/followship-controller.js
+++ controllers/followship-controller.js
@@ -23,6 +23,27 @@ const followshipController = {
       })
       .then(newFollowship => res.json({ status: 'success', data: { followship: newFollowship } }))
       .catch(err => next(err))
+  },
+
+  unfollowUser: (req, res, next) => {
+    const followingId = req.params.followingId
+    const followerId = helpers.getUser(req).id
+
+    Promise.all([
+      User.findByPk(followingId),
+      Followship.findOne({ where: { followerId, followingId } })
+    ])
+      .then(([user, followship]) => {
+        if (!user) {
+          const err = new Error('使用者不存在!')
+          err.status = 404
+          throw err
+        }
+        if (!followship) throw new Error('沒有跟隨過此使用者!')
+        return followship.destroy()
+      })
+      .then(deletedFollowship => res.json({ status: 'success', data: { followship: deletedFollowship } }))
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/followships.js
+++ routes/modules/followships.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const followshipController = require('../../controllers/followship-controller')
 
 router.post('/', followshipController.followUser)
+router.delete('/:followingId', followshipController.unfollowUser)
 
 module.exports = router
