9c8d202e7122043b7af50f7b45155a755e62a412
susu725 <zxcv1010017@gmail.com>
2023-08-25 03:45:36 +0800

    feat: add absoluteTimeFormat function

--- helpers/day-helpers.js
+++ helpers/day-helpers.js
@@ -5,5 +5,11 @@ dayjs.locale('zh-tw')
 dayjs.extend(relativeTime)
 
 module.exports = {
+  absoluteTimeFormat: createdAt => {
+    const nowFormat = dayjs(createdAt).format('Ah:mm・YYYY年MM月DD日')
+    const chineseString = nowFormat.substring(0, 2) === 'AM' ? '上午' : '下午'
+    const absoluteTime = nowFormat.replace(nowFormat.substring(0, 2), chineseString)
+    return absoluteTime
+  },
   relativeTimeFormat: createdAt => dayjs(createdAt).fromNow()
 }
