8946aa1723d38eecb5ab4161f2041c0b94794771
AlanChiangg <alaln35alan@gmail.com>
2023-08-24 14:27:19 +0800

    feat: add getTweet controller

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -2,11 +2,53 @@ const { Tweet, User, Like, sequelize } = require('../models')
 const { getUser } = require('../_helpers')
 
 const tweetController = {
+  // 瀏覽所有推文
   getTweets: async (req, res, next) => {
     try {
       const userId = getUser(req).id
       const [tweets, likes] = await Promise.all([
         Tweet.findAll({
+          order: [['createdAt', 'desc']],
+          include: [{
+            model: User,
+            attributes: ['account', 'name', 'avatar']
+          }],
+          attributes: {
+            include: [
+              [
+                sequelize.literal(
+                  '(SELECT COUNT(*) FROM `Likes` WHERE `Likes`.`TweetId` = `Tweet`.`id`)' // 在資料庫篩選此篇推文所有like數量
+                ),
+                'likesNum'
+              ],
+              [
+                sequelize.literal(
+                  '(SELECT COUNT(*) FROM `Replies` WHERE `Replies`.`TweetId` = `Tweet`.`id`)' // 在資料庫篩選此篇推文所有回覆數量
+                ),
+                'repliesNum'
+              ]
+            ]
+          },
+          nest: true,
+          raw: true
+        }),
+        Like.findAll({ where: { UserId: userId } })
+      ])
+      const result = tweets.map(tweet => ({
+        ...tweet,
+        isLiked: likes.some(like => like.TweetId === tweet.id) // 若Like model中，登入者id = 推文id，代表登入者有點讚，回傳ture，反之false
+      }))
+      return res.status(200).json(result)
+    } catch (err) {
+      return next(err)
+    }
+  },
+  // 瀏覽特定推文
+  getTweet: async (req, res, next) => {
+    try {
+      const userId = getUser(req).id
+      const [tweet, likes] = await Promise.all([
+        Tweet.findByPk(req.params.id, {
           order: [['createdAt', 'desc']],
           include: [{
             model: User,
@@ -27,19 +69,13 @@ const tweetController = {
                 'repliesNum'
               ]
             ]
-          },
-          nest: true,
-          raw: true
+          }
         }),
-        Like.findAll({
-          where: { UserId: userId }
-        })
+        Like.findAll({ where: { UserId: userId } })
       ])
-      const result = tweets.map(tweet => ({
-        ...tweet,
-        isLiked: likes.some(like => like.TweetId === tweet.id)
-      }))
-      return res.json(result)
+      const tweetData = tweet.toJSON()
+      tweetData.isLiked = likes.some(like => like.TweetId === tweet.id)
+      return res.status(200).json(tweetData)
     } catch (err) {
       return next(err)
     }
