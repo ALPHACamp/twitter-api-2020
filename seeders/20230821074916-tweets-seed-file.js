0d708dae5157357736ba263684daa8fd8f906383
Sean Chen <seanchen112@gmail.com>
2023-08-24 10:48:50 +0800

    fix: seeder tweets

--- seeders/20230821074916-tweets-seed-file.js
+++ seeders/20230821074916-tweets-seed-file.js
@@ -1,9 +1,16 @@
 'use strict'
 const faker = require('faker')
+const db = require('../models')
+const User = db.User
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const data = []
+    const userData = await User.findAll({
+      raw: true,
+      nest: true,
+      attributes: ['id']
+    })
     // eslint-disable-next-line array-callback-return
     Array.from({ length: 10 }).map((user, i) => {
       for (let j = 0; j < 10; ++j) {
@@ -12,7 +19,7 @@ module.exports = {
           description: faker.lorem.text(140),
           created_at: new Date(),
           updated_at: new Date(),
-          User_id: i + 1
+          User_id: userData[i].id
         })
       }
     })
