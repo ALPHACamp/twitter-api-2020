26c83923afeba725deb4379bf6a09a42b169bd45
Sean Chen <seanchen112@gmail.com>
2023-08-25 17:38:26 +0800

    fix: pass test

--- controllers/apis/tweet-controller.js
+++ controllers/apis/tweet-controller.js
@@ -1,147 +1,142 @@
-const { Tweet, Like, Reply, User } = require('../../models')
-const helpers = require('../../_helpers')
+const { Tweet, Like, Reply, User } = require("../../models");
+const helpers = require("../../_helpers");
 
 const tweetContorller = {
   getTweets: async (req, res, next) => {
     try {
       const tweets = await Tweet.findAll({
-        raw: true
-      })
-      if (!tweets) throw new Error("Tweet didn't exist!")
-      res.json({
-        tweets
-      })
+        raw: true,
+      });
+      if (!tweets) throw new Error("Tweet didn't exist!");
+      res.json(tweets);
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   getTweet: async (req, res, next) => {
     try {
-      const tweetId = req.params.tweet_id
-      const tweet = await Tweet.findByPk(tweetId)
+      const tweetId = req.params.tweet_id;
+      const tweet = await Tweet.findByPk(tweetId);
 
-      if (!tweet) throw new Error("Tweet didn't exist!")
-      res.json({
-        status: 'success',
-        data: tweet
-      })
+      if (!tweet) throw new Error("Tweet didn't exist!");
+      res.json(tweet);
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   likeTweet: async (req, res, next) => {
     try {
-      const tweetId = req.params.tweet_id
-      const getUser = helpers.getUser(req)
-      const userId = getUser.id
+      const tweetId = req.params.tweet_id;
+      const getUser = helpers.getUser(req);
+      const userId = getUser.id;
       const [tweet, like] = await Promise.all([
         Tweet.findByPk(tweetId),
         Like.findOne({
           where: {
             userId,
-            tweetId
-          }
-        })
-      ])
-      if (!tweet) throw new Error("Tweet didn't exist!")
-      if (like) throw new Error('You have liked this tweet!')
+            tweetId,
+          },
+        }),
+      ]);
+      if (!tweet) throw new Error("Tweet didn't exist!");
+      if (like) throw new Error("You have liked this tweet!");
       const createdLike = await Like.create({
         userId,
-        tweetId
-      })
+        tweetId,
+      });
       res.json({
-        status: 'success',
+        status: "success",
         data: createdLike,
-        Boolean: true
-      })
+        Boolean: true,
+      });
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   unlikeTweet: async (req, res, next) => {
     try {
-      const tweetId = req.params.tweet_id
-      const getUser = helpers.getUser(req)
-      const userId = getUser.id
+      const tweetId = req.params.tweet_id;
+      const getUser = helpers.getUser(req);
+      const userId = getUser.id;
       const [tweet, like] = await Promise.all([
         Tweet.findByPk(tweetId),
         Like.findOne({
           where: {
             userId,
-            tweetId
-          }
-        })
-      ])
-      if (!tweet) throw new Error("Tweet didn't exist!")
-      if (!like) throw new Error("You haven't liked this tweet!")
+            tweetId,
+          },
+        }),
+      ]);
+      if (!tweet) throw new Error("Tweet didn't exist!");
+      if (!like) throw new Error("You haven't liked this tweet!");
       const unlike = await Like.destroy({
         where: {
           userId,
-          tweetId
-        }
-      })
+          tweetId,
+        },
+      });
       res.json({
-        status: 'success',
+        status: "success",
         data: unlike,
-        Boolean: false
-      })
+        Boolean: false,
+      });
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   createTweet: async (req, res, next) => {
     try {
-      const { description } = req.body
-      const getUser = helpers.getUser(req)
-      const userId = getUser.id
-      if (!description) throw new Error('文章內容不可為空白')
+      const { description } = req.body;
+      const getUser = helpers.getUser(req);
+      const userId = getUser.id;
+      if (!description) throw new Error("文章內容不可為空白");
       const createdTweet = await Tweet.create({
         userId,
         description,
         createdAt: new Date(),
-        updatedAt: new Date()
-      })
+        updatedAt: new Date(),
+      });
       res.status(200).json({
-        status: 'success',
-        message: 'Successfully create tweet.',
-        data: createdTweet
-      })
+        status: "success",
+        message: "Successfully create tweet.",
+        data: createdTweet,
+      });
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   getReplies: async (req, res, next) => {
     try {
-      const tweetId = req.params.tweet_id
+      const tweetId = req.params.tweet_id;
       const replies = await Reply.findAll({
         where: { tweetId },
         include: [
           {
             model: User,
-            as: 'replier',
-            attributes: { exclude: ['password'] }
+            as: "replier",
+            attributes: { exclude: ["password"] },
           },
           {
             model: Tweet,
-            as: 'tweetreply',
+            as: "tweetreply",
 
             include: [
               {
                 model: User,
-                as: 'author',
-                attributes: ['account', 'name', 'avatar']
-              }
-            ]
-          }
+                as: "author",
+                attributes: ["account", "name", "avatar"],
+              },
+            ],
+          },
         ],
-        order: [['createdAt', 'DESC']],
-        nest: true
+        order: [["createdAt", "DESC"]],
+        nest: true,
         // raw: true // 為何設true就無法取資料？
-      })
+      });
 
-      if (!replies) throw new Error('This tweet has no replies')
-      console.log(replies)
-      const repliesData = replies.map(reply => ({
+      if (!replies) throw new Error("This tweet has no replies");
+      console.log(replies);
+      const repliesData = replies.map((reply) => ({
         replyId: reply.id,
         comment: reply.comment,
         replierId: reply.replier.id,
@@ -151,82 +146,83 @@ const tweetContorller = {
         createdAt: reply.createdAt,
         tweetId: reply.TweetId,
         tweetBelongerName: reply.tweetreply.author.name,
-        tweetBelongerAccount: reply.tweetreply.author.account
-      }))
+        tweetBelongerAccount: reply.tweetreply.author.account,
+      }));
       // console.log(repliesData);
-      res.json({
-        status: 'success',
-        data: repliesData
-      })
+      // res.json({
+      //   status: "success",
+      //   data: repliesData,
+      // });
+      res.status(200).json(repliesData);
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   createReply: async (req, res, next) => {
     try {
-      const tweetId = req.params.tweet_id
-      const { comment } = req.body
-      const getUser = helpers.getUser(req)
-      const userId = getUser.id
+      const tweetId = req.params.tweet_id;
+      const { comment } = req.body;
+      const getUser = helpers.getUser(req);
+      const userId = getUser.id;
 
-      if (!comment) throw new Error('回覆內容不可為空白')
+      if (!comment) throw new Error("回覆內容不可為空白");
       const replyData = await Reply.create({
         tweetId,
         userId,
         comment,
         createdAt: new Date(),
-        updatedAt: new Date()
-      })
+        updatedAt: new Date(),
+      });
 
       res.json({
-        status: 'success',
-        message: 'successfully created reply',
-        replyData
-      })
+        status: "success",
+        message: "successfully created reply",
+        replyData,
+      });
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   getLikes: async (req, res, next) => {
     try {
       // const getUser = helpers.getUser(req);
-      const tweetId = req.params.tweet_id
+      const tweetId = req.params.tweet_id;
       const [tweet, likes] = await Promise.all([
         Tweet.findByPk(tweetId),
         Like.findAll({
           where: {
-            tweetId
+            tweetId,
           },
           nest: true,
           raw: true,
           include: [
             {
               model: User,
-              attributes: { exclude: ['password', 'introduction', 'cover'] }
-            }
-          ]
-        })
-      ])
-      console.log(likes)
-      if (!tweet) throw new Error("Tweet didn't exist!")
-      if (!likes) throw new Error('NO ONE liked this tweet!')
-      const dataLike = likes.map(like => ({
+              attributes: { exclude: ["password", "introduction", "cover"] },
+            },
+          ],
+        }),
+      ]);
+      console.log(likes);
+      if (!tweet) throw new Error("Tweet didn't exist!");
+      if (!likes) throw new Error("NO ONE liked this tweet!");
+      const dataLike = likes.map((like) => ({
         likedId: like.id,
         likedTweetId: like.tweetId,
         likedUserId: like.User.id,
         likedUserName: like.User.name,
         likedUserAccount: like.User.account,
-        likedUserAvatar: like.User.avatar
-      }))
+        likedUserAvatar: like.User.avatar,
+      }));
       res.json({
-        status: 'success',
-        message: 'successfully get all the liked users',
-        dataLike
-      })
+        status: "success",
+        message: "successfully get all the liked users",
+        dataLike,
+      });
     } catch (err) {
-      next(err)
+      next(err);
     }
-  }
-}
+  },
+};
 
-module.exports = tweetContorller
+module.exports = tweetContorller;
