c4cc17fcc1f93753bcc0ec582ed4f7f4cdb206ea
BoxunW <tony1572002@gmail.com>
2023-08-26 13:37:44 +0800

    feat: add CORS

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 const express = require('express')
+const cors = require('cors')
 const methodOverride = require('method-override')
 const helpers = require('./_helpers')
 const { apis } = require('./routes')
@@ -9,6 +10,18 @@ const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+// set CORS
+app.use(cors())
+const corsOptions = {
+  origin: [
+    'https://young-waters-15158-8b230f0b0919.herokuapp.com',
+    'http://localhost:8080'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
