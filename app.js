4bd69c5b65fc16d27a20cf15d517714ee499b4f1
susu725 <zxcv1010017@gmail.com>
2023-08-26 15:31:53 +0800

    feat: add cors

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const helpers = require('./_helpers')
 const routes = require('./routes')
 
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
