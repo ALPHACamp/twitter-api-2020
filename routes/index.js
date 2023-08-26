098c626f24ec7d3a275c774d86c34f8a6c8ac036
WinstonAdams <apollo07291@gmail.com>
2023-08-26 15:43:52 +0800

    feat: add user function

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
+const users = require('./modules/users')
 const tweets = require('./modules/tweets')
 const followships = require('./modules/followships')
-const passport = require('../config/passport')
 const admin = require('./modules/admin')
+const passport = require('../config/passport')
 const userController = require('../controllers/user-controller')
 const adminController = require('../controllers/admin-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
@@ -20,6 +21,8 @@ router.use('/api/admin', authenticatedAdmin, admin)
 router.use('/api/tweets', authenticatedUser, tweets)
 
 router.use('/api/followships', authenticatedUser, followships)
+
+router.use('/api/users', authenticatedUser, users)
 
 router.get('/', (req, res) => res.send('hello world'))
 
---
+++ routes/modules/users.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const userController = require('../../controllers/user-controller')
+
+router.get('/:id', userController.getUser)
+
+router.get('/:id/tweets', userController.getUserTweets)
+
+router.get('/:id/replied_tweets', userController.getUserReplies)
+
+router.get('/:id/likes', userController.getUserLikes)
+
+module.exports = router
