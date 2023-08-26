9b3d79d481ac9a3c24e227b4a4adb124de1c718e
yiching-hsu <ychsu7149@gmail.com>
2023-08-26 18:04:39 +0800

    feat: add followship seeder

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,7 @@ const tweetController = require('../controllers/apis/tweet-controller')
 const followshipController = require('../controllers/apis/followship-controller')
 
 const { apiErrorHandler } = require('../middleware/error-handler')
-const {
-  authenticated,
-  authenticatedAdmin,
-  authenticatedUser
-} = require('../middleware/api-auth')
+const { authenticated, authenticatedAdmin, authenticatedUser } = require('../middleware/api-auth')
 
 // api/admin
 router.use('/api/admin', authenticated, authenticatedAdmin, admin)
@@ -29,74 +25,19 @@ router.get('/api/users/:id/replied_tweets', authenticated, authenticatedUser, us
 router.get('/api/users/:id', authenticated, authenticatedUser, userController.getUser)
 
 // api/tweets
-router.get(
-  '/api/tweets/:tweet_id/replies',
-  authenticated,
-  authenticatedUser,
-  tweetController.getReplies
-)
-router.post(
-  '/api/tweets/:tweet_id/replies',
-  authenticated,
-  authenticatedUser,
-  tweetController.createReply
-)
-router.get(
-  '/api/tweets/:tweet_id/likes',
-  authenticated,
-  authenticatedUser,
-  tweetController.getLikes
-)
-router.post(
-  '/api/tweets/:tweet_id/like',
-  authenticated,
-  authenticatedUser,
-  tweetController.likeTweet
-)
-router.post(
-  '/api/tweets/:tweet_id/unlike',
-  authenticated,
-  authenticatedUser,
-  tweetController.unlikeTweet
-)
-router.get(
-  '/api/tweets/:tweet_id',
-  authenticated,
-  authenticatedUser,
-  tweetController.getTweet
-)
-router.post(
-  '/api/tweets',
-  authenticated,
-  authenticatedUser,
-  tweetController.createTweet
-)
-router.get(
-  '/api/tweets',
-  authenticated,
-  authenticatedUser,
-  tweetController.getTweets
-)
+router.get('/api/tweets/:tweet_id/replies', authenticated, authenticatedUser, tweetController.getReplies)
+router.post('/api/tweets/:tweet_id/replies', authenticated, authenticatedUser, tweetController.createReply)
+router.get('/api/tweets/:tweet_id/likes', authenticated, authenticatedUser, tweetController.getLikes)
+router.post('/api/tweets/:tweet_id/like', authenticated, authenticatedUser, tweetController.likeTweet)
+router.post('/api/tweets/:tweet_id/unlike', authenticated, authenticatedUser, tweetController.unlikeTweet)
+router.get('/api/tweets/:tweet_id', authenticated, authenticatedUser, tweetController.getTweet)
+router.post('/api/tweets', authenticated, authenticatedUser, tweetController.createTweet)
+router.get('/api/tweets', authenticated, authenticatedUser, tweetController.getTweets)
 
 // api/followship
-router.delete(
-  '/api/followships/:following_id',
-  authenticated,
-  authenticatedUser,
-  followshipController.unfollowUser
-)
-router.get(
-  '/api/followships/top10',
-  authenticated,
-  authenticatedUser,
-  followshipController.getTop10
-)
-router.post(
-  '/api/followships',
-  authenticated,
-  authenticatedUser,
-  followshipController.followUser
-)
+router.delete('/api/followships/:following_id', authenticated, authenticatedUser, followshipController.unfollowUser)
+router.get('/api/followships/top10', authenticated, authenticatedUser, followshipController.getTop10)
+router.post('/api/followships', authenticated, authenticatedUser, followshipController.followUser)
 router.use('/', apiErrorHandler)
 
 module.exports = router
