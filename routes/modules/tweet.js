6bdfaa63351641542028753b11911d6a03969f1f
Keiko <keitakeiko@hotmail.com>
2023-08-26 23:59:02 +0800

    feat: add unlikeTweet function

--- routes/modules/tweet.js
+++ routes/modules/tweet.js
@@ -4,8 +4,11 @@ const router = express.Router()
 const tweetController = require('../../controllers/tweet-controller')
 const upload = require('../../middleware/multer')
 
+// like or unlike function 
+router.delete('/:id/like', tweetController.unlikeTweet)
 router.post('/:id/like', tweetController.likeTweet)
 
+// tweets function 
 router.get('/:id', tweetController.getTweet)
 router.post('/', tweetController.postTweet)
 router.get('/', tweetController.getTweets)
