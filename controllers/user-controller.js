a143c81c840e98404184e56404121af24ba131f2
harrison0502 <harrison0502@gmail.com>
2023-08-26 15:46:24 +0800

    refactor: restructure the signin return data

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,12 +7,9 @@ const userController = {
       const userData = getUser(req).toJSON()
       delete userData.password
       const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })// 簽發 JWT Token(期限30天)
-      return res.json({
-        status: 'success',
-        data: {
-          token,
-          user: userData
-        }
+      return res.status(200).json({
+        token,
+        user: userData
       })
     } catch (err) {
       return next(err)
