f706f0274a3152d592af571ce09c672b43308b7d
Yen Lin <lynn575938@outlook.com>
2023-08-27 03:29:42 +0800

    feat: add date time format module

--- helpers/dayjs-helpers.js
+++ helpers/dayjs-helpers.js
@@ -5,5 +5,7 @@ dayjs.locale('zh-tw')
 dayjs.extend(relativeTime)
 
 module.exports = {
-  relativeTimeFromNow: a => dayjs(a).fromNow()
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
+  formatDate: a => dayjs(a).format('YYYY年MM月DD日'),
+  formatTime: a => dayjs(a).format('A h:mm').replace('AM', '上午').replace('PM', '下午')
 }
