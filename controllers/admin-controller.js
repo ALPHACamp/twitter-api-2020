a977d6dfba4788f0441567d73b76d645f9dc09c2
AlanChiangg <alaln35alan@gmail.com>
2023-08-27 15:54:00 +0800

    feat: add getTweets & deleteTweet

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,17 @@
 
 const jwt = require('jsonwebtoken')
-
+const helpers = require('../_helpers')
 const { User, Tweet, Reply, Like, Followship, sequelize } = require('../models')
+const dayjs = require('dayjs')
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const relativeTime = require('dayjs/plugin/relativeTime')
+const zhTw = require('dayjs/locale/zh-tw')
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(relativeTime)
+dayjs.locale(zhTw)
 
 const adminController = {
   // No.20 - 登入後台帳號 POST /api/admin/signin
@@ -49,17 +59,43 @@ const adminController = {
     }
   },
   // No.22 - 取得所有推文清單 GET /api/admin/tweets
-  getTweets: (req, res, next) => {
+  getTweets: async (req, res, next) => {
     try {
-      return res.status(200).json({})
+      const tweets = await Tweet.findAll({
+        order: [['createdAt', 'desc']],
+        include: [{
+          model: User,
+          attributes: ['account', 'name', 'avatar']
+        }],
+        nest: true,
+        raw: true
+      })
+      const result = tweets.map(tweet => ({
+        ...tweet,
+        description: tweet.description.substring(0, 50),
+        updatedAt: dayjs(tweet.updatedAt)
+          .tz('Asia/Taipei')
+          .fromNow()
+      }))
+      return res.status(200).json(result)
     } catch (err) {
       return next(err)
     }
   },
   // No.23 - 刪除特定推文 DELETE /api/admin/tweets/:id
-  deleteTweet: (req, res, next) => {
+  deleteTweet: async (req, res, next) => {
     try {
-      return res.status(200).json({})
+      const tweetId = req.params.id
+      const tweet = await Tweet.findByPk(tweetId)
+      if (!tweet) throw new Error('此推文不存在')
+
+      const deletedTweet = await Promise.all([
+        tweet.destroy(),
+        Reply.destroy({ where: { TweetId: tweetId } }),
+        Like.destroy({ where: { TweetId: tweetId } })
+      ])
+
+      return res.status(200).json(deletedTweet[0])
     } catch (err) {
       return next(err)
     }
