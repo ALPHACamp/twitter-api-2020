edc77be4b79db8ebe2042d890d2882123d911fe3
Sean Chen <seanchen112@gmail.com>
2023-08-27 16:16:36 +0800

    fix: es lint

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
-if (process.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.NODE_ENV !== 'production') {
+  require('dotenv').config()
 }
 
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const routes = require("./routes");
-const passport = require("./config/passport");
-// const cors = require("cors");
-const { CORSHeader } = require("./middleware/CORS-header");
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+const passport = require('./config/passport')
+const { CORSHeader } = require('./middleware/CORS-header')
 
-const app = express();
-
-// const corsOptions = {
-//   origin: "https://mighty-temple-45104-0d6672fb07d0.herokuapp.com",
-// };
-// app.use(cors(corsOptions));
+const app = express()
 
 // bodyparser設定
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 // passport 初始化
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use("/upload", express.static(path.join(__dirname, "upload")));
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next) {
+function authenticated (req, res, next) {
   // passport.authenticate('jwt', { ses...
 }
 
-// app.use(routes);
-app.use("/", CORSHeader, routes);
+app.use('/', CORSHeader, routes)
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-module.exports = app;
+module.exports = app
