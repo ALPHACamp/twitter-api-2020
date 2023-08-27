cd6f82e32bf212ba3b6d7053192454a42a55660d
Sean Chen <seanchen112@gmail.com>
2023-08-27 23:10:32 +0800

    fix: bugs

--- app.js
+++ app.js
@@ -22,12 +22,6 @@ app.use("/upload", express.static(path.join(__dirname, "upload")));
 
 const port = process.env.PORT || 3000;
 
-// use helpers.getUser(req) to replace req.user
-// function authenticated (req, res, next) {
-// passport.authenticate('jwt', { ses...
-
-// }
-
 app.use("/", CORSHeader, routes);
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
