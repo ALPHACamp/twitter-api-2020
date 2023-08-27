d7b441e8b565b9c5aa042a83de8ea332cc80d53a
yanyanyaa <yclin3411@gmail.com>
2023-08-27 13:32:10 +0800

    merge

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -3,6 +3,7 @@ const { getUser } = require('../_helpers')
 
 const tweetController = {
   getTweets: (req, res, next) => {
+    console.log('userId:', getUser(req).id)
     return Tweet.findAll({
       order: [['createdAt', 'DESC']],
       include: [
@@ -55,7 +56,6 @@ const tweetController = {
   },
   getTweetReplies: (req, res, next) => {
     const tweetId = req.params.tweetId
-    console.log('userId:', getUser(req))
     Promise.all([
       Reply.findAll({
         where: { tweetId: tweetId },
@@ -78,6 +78,25 @@ const tweetController = {
         return res.json({
           status: 'success',
           data: data
+        })
+      })
+      .catch(err => next(err))
+  },
+  postTweet: async (req, res, next) => {
+    const { description } = req.body
+    const userId = getUser(req).id
+    console.log('userId:', userId)
+    console.log('description:', description)
+    if (!description) throw new Error('內容不可空白')
+    if (description.length > 140) throw new Error('內容不可超過 140 字')
+    return User.findByPk(userId)
+      .then(userId => {
+        Tweet.create({
+          description,
+          userId
+        })
+        return res.json({
+          status: 'success'
         })
       })
       .catch(err => next(err))
