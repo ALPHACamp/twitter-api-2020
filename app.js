26c5dbfba9fa54f916bc4014049e638583d23f70
Sean Chen <seanchen112@gmail.com>
2023-08-27 16:15:09 +0800

    feat: CORS-header

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
-if (process.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const routes = require('./routes')
-const passport = require('./config/passport')
-const cors = require('cors')
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const passport = require("./config/passport");
+// const cors = require("cors");
+const { CORSHeader } = require("./middleware/CORS-header");
 
-const app = express()
+const app = express();
 
-const corsOptions = {
-  origin: '＊',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization']
-}
-
-app.use(cors(corsOptions))
+// const corsOptions = {
+//   origin: "https://mighty-temple-45104-0d6672fb07d0.herokuapp.com",
+// };
+// app.use(cors(corsOptions));
 
 // bodyparser設定
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 // passport 初始化
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use("/upload", express.static(path.join(__dirname, "upload")));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 // use helpers.getUser(req) to replace req.user
-function authenticated (req, res, next) {
+function authenticated(req, res, next) {
   // passport.authenticate('jwt', { ses...
 }
 
-app.use(routes)
+// app.use(routes);
+app.use("/", CORSHeader, routes);
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-module.exports = app
+module.exports = app;
--- controllers/apis/followship-controller.js
+++ controllers/apis/followship-controller.js
@@ -1,109 +1,109 @@
-const db = require('../../models')
-const { User, Followship } = db
-const helpers = require('../../_helpers')
-const sequelize = require('sequelize')
+const db = require("../../models");
+const { User, Followship } = db;
+const helpers = require("../../_helpers");
+const sequelize = require("sequelize");
 
 const followshipController = {
   followUser: async (req, res, next) => {
     try {
-      const followingId = req.body.id
-      const getUser = helpers.getUser(req)
-      const userId = getUser.id
+      const followingId = req.body.id;
+      const getUser = helpers.getUser(req);
+      const userId = getUser.id;
       const [user, followship] = await Promise.all([
         User.findByPk(userId),
         Followship.findOne({
-          where: { followerId: userId, followingId }
-        })
-      ])
-      if (!user) throw new Error("User didn't exist!")
-      if (followship) throw new Error("You've are already followed this user!")
+          where: { followerId: userId, followingId },
+        }),
+      ]);
+      if (!user) throw new Error("User didn't exist!");
+      if (followship) throw new Error("You've are already followed this user!");
       Followship.create({
         followerId: userId,
-        followingId
-      })
+        followingId,
+      });
       res.json({
-        status: 'success',
-        message: 'successfully follow user!'
-      })
+        status: "success",
+        message: "successfully follow user!",
+      });
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   unfollowUser: async (req, res, next) => {
     try {
-      const followingId = req.params.following_id
-      const getUser = helpers.getUser(req)
-      const userId = getUser.id
+      const followingId = req.params.following_id;
+      const getUser = helpers.getUser(req);
+      const userId = getUser.id;
       const [user, followship] = await Promise.all([
         User.findByPk(userId),
         Followship.findOne({
-          where: { followerId: userId, followingId }
-        })
-      ])
-      if (!user) throw new Error("User didn't exist!")
-      if (!followship) throw new Error("You haven't followed this user!")
+          where: { followerId: userId, followingId },
+        }),
+      ]);
+      if (!user) throw new Error("User didn't exist!");
+      if (!followship) throw new Error("You haven't followed this user!");
       Followship.destroy({
-        where: { followerId: userId, followingId }
-      })
+        where: { followerId: userId, followingId },
+      });
       res.json({
-        status: 'success',
-        message: 'successfully unfollow user!'
-      })
+        status: "success",
+        message: "successfully unfollow user!",
+      });
     } catch (err) {
-      next(err)
+      next(err);
     }
   },
   getTop10: async (req, res, next) => {
     try {
-      const getUser = helpers.getUser(req)
-      const currentUserId = getUser.id
-      if (!currentUserId) throw new Error("User didn't exist!")
+      const getUser = helpers.getUser(req);
+      const currentUserId = getUser.id;
+      if (!currentUserId) throw new Error("User didn't exist!");
 
       const top10UsersWithFollowStatus = await User.findAll({
         where: {
-          role: 'user',
-          id: { [sequelize.Op.not]: currentUserId }
+          role: "user",
+          id: { [sequelize.Op.not]: currentUserId },
         },
         attributes: [
-          'id',
-          'account',
-          'name',
-          'avatar',
+          "id",
+          "account",
+          "name",
+          "avatar",
           [
             sequelize.literal(
-              '(SELECT COUNT(DISTINCT id) FROM Followships WHERE following_id = User.id)'
+              "(SELECT COUNT(DISTINCT id) FROM Followships WHERE following_id = User.id)"
             ),
-            'totalFollowers'
+            "totalFollowers",
           ],
           [
             sequelize.literal(
               `EXISTS (SELECT 1 FROM Followships WHERE following_id = User.id AND follower_Id = ${currentUserId})`
             ),
-            'isFollowed'
-          ]
+            "isFollowed",
+          ],
         ],
         include: [
           {
             model: User,
-            as: 'Followers',
+            as: "Followers",
             attributes: [],
-            through: { attributes: [] }
-          }
+            through: { attributes: [] },
+          },
         ],
-        order: [[sequelize.literal('totalFollowers'), 'DESC']],
+        order: [[sequelize.literal("totalFollowers"), "DESC"]],
         limit: 10,
         subQuery: false, // 避免因查詢多張表造成limit失常
         raw: true,
-        nest: true
-      })
+        nest: true,
+      });
 
       res.json({
-        top10UsersWithFollowStatus
-      })
+        top10UsersWithFollowStatus,
+      });
     } catch (err) {
-      next(err)
+      next(err);
     }
-  }
-}
+  },
+};
 
-module.exports = followshipController
+module.exports = followshipController;
