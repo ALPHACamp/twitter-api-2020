6e7c20dfb87daca069b97192696bb9146c30bdb0
harrison0502 <harrison0502@gmail.com>
2023-08-27 15:04:29 +0800

    refactor: use async/await for asynchronous operations

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -23,15 +23,13 @@ const userController = {
       if (!name || !account || !email || !password || !checkPassword) throw new Error('所有欄位皆為必填！')
       if (name.length > 50) throw new Error('名稱字數超出上限！')
       if (password !== checkPassword) throw new Error('密碼與確認密碼不符合！')
-      await Promise.all([
+      const [userEmail, userName] = await Promise.all([
         User.findOne({ where: { email } }),
         User.findOne({ where: { account } })
       ])
-        .then(([userEmail, userName]) => {
-          if (userEmail) throw new Error('email已重複註冊！')
-          if (userName) throw new Error('account已重複註冊！')
-          return bcrypt.hash(password, 10)
-        })
+      if (userEmail) throw new Error('email已重複註冊！')
+      if (userName) throw new Error('account已重複註冊！')
+
       const hash = await bcrypt.hash(password, 10)
       const newUser = await User.create({
         name,
