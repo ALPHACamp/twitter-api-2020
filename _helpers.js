997c5177a05b3a01fd45daed207a7fed1a1779ec
BoxunW <tony1572002@gmail.com>
2023-08-27 09:49:51 +0800

    feat: add ensureAuthenticated to _helper

--- _helpers.js
+++ _helpers.js
@@ -1,8 +1,13 @@
 
 function getUser(req) {
-  return req.user
+  return req.user || null
+}
+
+function ensureAuthenticated(req) {
+  return req.isAuthenticated()
 }
 
 module.exports = {
-  getUser
+  getUser,
+  ensureAuthenticated
 }
