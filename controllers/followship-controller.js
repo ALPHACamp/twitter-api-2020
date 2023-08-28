2bed8bab754c2db953a67db971fcf4351147da29
Keiko <keitakeiko@hotmail.com>
2023-08-28 16:59:22 +0800

    feat: add removeFollowing

--- controllers/followship-controller.js
+++ controllers/followship-controller.js
@@ -58,6 +58,47 @@ const followshipController = {
         status: 'success'
       }))
       .catch(err => next(err))
+  },
+  removeFollowing: (req, res, next) => {
+    const followingId = req.params.id // 當下按的使用者
+    const UserId = helpers.getUser(req).id // 登入使用者 ID
+
+    // 不能追蹤自己
+    if (followingId === UserId) {
+        return res.status(422).json({
+          status: 'error',
+          message: '使用者不可以取消追蹤自己.'
+        })
+      }
+
+    return Promise.all([
+        User.findByPk(followingId),
+        Followship.findOne({
+          where: {
+            followerId: UserId,
+            followingId
+          }
+        })
+      ])
+    .then(([user, followship]) => {
+    
+      // 確認使用者是否存在
+    if (!user || user.role === 'admin') {
+      return res.status(404).json({
+        status: 'error',
+        message: '使用者不存在.'
+      })
+    }
+    
+    // 確認是否尚未按過追蹤
+    if (!followship) throw new Error('你還沒追蹤喔!')
+    return followship.destroy()
+    })
+    
+    .then(() => res.status(200).json({
+      status: 'success'
+    }))
+    .catch(err => next(err))
   }
 }
 
