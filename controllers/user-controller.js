a2871f4b26631735df44751f2fcaa478a0052eba
BoxunW <tony1572002@gmail.com>
2023-08-28 15:54:40 +0800

    feat: add putUserAccount

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -18,24 +18,28 @@ const userController = {
     }
   },
   signUp: (req, res, next) => {
-    const { account, email, password, checkPassword } = req.body
-    if (password !== checkPassword) throw new Error('密碼不相符!')
-    Promise.all([
+    const { account, name, email, password, checkPassword } = req.body
+    if (!account || !email || !name || !password) throw new Error('帳戶、暱稱、信箱和密碼不得為空！')
+    if (password !== checkPassword) throw new Error('密碼不相符！')
+    if (name.length > 50) throw new Error('超過暱稱字數上限 50 字！')
+    return Promise.all([
       User.findOne({ where: { account } }),
       User.findOne({ where: { email } })
     ])
       .then(([user1, user2]) => {
         if (user1) throw new Error('account 已重複註冊！')
         if (user2) throw new Error('email 已重複註冊！')
-        return bcrypt.hash(req.body.password, 10)
+        return bcrypt.hash(password, 10)
       })
       .then(hash => User.create({
-        account: req.body.account,
-        name: req.body.name,
-        email: req.body.email,
+        account,
+        name,
+        email,
         password: hash
       }))
       .then(newUser => {
+        newUser = newUser.toJSON()
+        delete newUser.password
         res.json({
           status: 'success',
           message: '成功註冊帳號！',
@@ -73,6 +77,71 @@ const userController = {
         })
       })
       .catch(err => next(err))
-  }
+  },
+  putUserAccount: (req, res, next) => {
+    const id = req.params.id
+    if (Number(id) !== getUser(req).id) throw new Error('只能編輯本人帳戶資料！')
+    const { account, name, email, password, checkPassword } = req.body
+    if (!account || !email || !name) throw new Error('帳戶、暱稱和信箱不得為空！')
+    if (password !== checkPassword) throw new Error('密碼不相符！')
+    if (name.length > 50) throw new Error('超過暱稱字數上限 50 字！')
+    Promise.all([
+      User.findOne({ where: { account } }),
+      User.findOne({ where: { email } })
+    ])
+      .then(([user1, user2]) => {
+        if (user1) throw new Error('account 已存在！')
+        if (user2) throw new Error('email 已存在！')
+        return User.findByPk(req.params.id)
+      })
+      .then(user => {
+        if (!user) throw new Error('使用者不存在！')
+        return user.update({
+          account,
+          name,
+          email,
+          password: password ? bcrypt.hashSync(password, 10) : user.password
+        })
+      })
+      .then(user => {
+        user = user.toJSON()
+        delete user.password
+        delete user.role
+        res.json({
+          status: 'success',
+          message: '成功編輯帳號！',
+          ...user
+        })
+      })
+      .catch(err => next(err))
+  },
+  // putUserProfile: (req, res, next) => {
+  //   const id = req.params.id
+  //   if (Number(id) !== getUser(req).id) throw new Error('只能編輯本人主頁資料！')
+  //   const { name, introduction } = req.body
+  //   const { avatar, cover } = req.files
+  //   if (!name) throw new Error('帳戶不得為空！')
+  //   if (name.length > 50) throw new Error('超過暱稱字數上限 50 字！')
+  //   User.findByPk(req.params.id)
+  //     .then(user => {
+  //       if (!user) throw new Error('使用者不存在！')
+  //       avatar = avatar ? avatar : user.avatar
+  //       cover = cover ? cover : user.cover
+  //       return user.update({
+  //         name,
+  //         introduction,
+  //         avatar,
+  //         cover
+  //       })
+  //     })
+  //     .then(user => {
+  //       res.json({
+  //         status: 'success',
+  //         message: '成功編輯個人主頁！',
+  //         ...user
+  //       })
+  //     })
+  //     .catch(err => next(err))
+  // }
 }
 module.exports = userController
