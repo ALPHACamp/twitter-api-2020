772bfaa4a2f822750b66276c571cd262cbd045fe
Sean Chen <seanchen112@gmail.com>
2023-08-29 23:27:46 +0800

    fix : bugs

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,29 +1,32 @@
-const express = require('express')
-const router = express.Router()
-const adminController = require('../../controllers/apis/admin-controller')
-const passport = require('passport')
-const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
+const express = require("express");
+const router = express.Router();
+const adminController = require("../../controllers/apis/admin-controller");
+const passport = require("passport");
+const {
+  authenticated,
+  authenticatedAdmin,
+} = require("../../middleware/api-auth");
 // router.post('/login', passport.authenticate('jwt', { session: false }), adminController.signIn)
 
 router.get(
-  '/users',
+  "/users",
   authenticated,
   authenticatedAdmin,
   adminController.getUsers
-)
+);
 router.delete(
-  '/tweets/:id',
+  "/tweets/:id",
   authenticated,
   authenticatedAdmin,
   adminController.deleteTweet
-)
+);
 router.get(
-  '/tweets',
+  "/tweets",
   authenticated,
   authenticatedAdmin,
   adminController.getTweets
-)
+);
 
 // router.use('/', (req, res) => res.redirect('api/admin/users'))
 
-module.exports = router
+module.exports = router;
