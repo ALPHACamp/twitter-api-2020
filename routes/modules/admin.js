a7fd569f218fbf0dbb3f1a97358ce6120166bee7
DannyHucc <dannyhu10011101@gmail.com>
2023-08-29 16:23:13 +0800

    feat: modify admin.js

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -8,16 +8,10 @@ const { authenticated } = require('../../middleware/auth')
 const { isAdmin, isAuthAdmin } = require('../../middleware/role-check')
 const { signInValidator } = require('../../middleware/validator')
 
-router.delete('/tweets/:id', authenticated, isAuthAdmin, adminController.deleteTweet)
 router.post('/login', signInValidator, passport.authenticate('local', { session: false }), isAdmin, adminController.login)
-<<<<<<< HEAD
-router.get('/users', authenticated, isAuthAdmin, adminController.getUsers)
-router.get('/tweets', authenticated, adminController.getTweets)
-=======
 
 router.delete('/tweets/:id', authenticated, isAuthAdmin, adminController.deleteTweet)
 router.get('/users', authenticated, isAuthAdmin, adminController.getUsers)
 router.get('/tweets', authenticated, isAuthAdmin, adminController.getTweets)
->>>>>>> feature/admin-get-tweets
 
 module.exports = router
