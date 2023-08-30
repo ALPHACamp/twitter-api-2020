2ff3502ac830c7c5afe3ce73694a9e921c4c63a0
harrison0502 <harrison0502@gmail.com>
2023-08-30 23:38:49 +0800

    feat: add prevent following admin and avoid returning user's password

--- controllers/followship-controller.js
+++ controllers/followship-controller.js
@@ -12,13 +12,14 @@ const followshipController = {
         User.findByPk(followingId),
         Followship.findOne({ where: { followerId, followingId } })
       ])
-      if (!user) {
+      if (!user || user.admin === 'admin') {
         const err = new Error('特定使用者不存在!')
         err.status = 404
         throw err
       }
       if (followship) throw new Error('已跟隨該使用者!')
 
+      delete user.password
       const data = await Followship.create({ followerId, followingId })
       return res.status(200).json({
         message: '跟隨成功!',
@@ -38,13 +39,14 @@ const followshipController = {
         User.findByPk(followingId),
         Followship.findOne({ where: { followerId, followingId } })
       ])
-      if (!user) {
+      if (!user || user.admin === 'admin') {
         const err = new Error('特定使用者不存在!')
         err.status = 404
         throw err
       }
       if (!followship) throw new Error('未跟隨該使用者!')
 
+      delete user.password
       const data = followship
       await followship.destroy()
       return res.status(200).json({
