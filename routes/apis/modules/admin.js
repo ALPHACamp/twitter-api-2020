3a0df805b5bac7f31b9e87f2e65549e7c17ac3ad
BoxunW <tony1572002@gmail.com>
2023-08-30 23:45:21 +0800

    feat: add delete tweet

--- routes/apis/modules/admin.js
+++ routes/apis/modules/admin.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const adminController = require('../../../controllers/admin-controller')
 
 router.get('/users', adminController.getUsers)
-// router.delete('/tweets/:id', adminController)
+router.delete('/tweets/:id', adminController.deleteTweet)
 router.get('/tweets', adminController.getTweets)
 
 module.exports = router
