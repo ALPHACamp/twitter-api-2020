62eeb7b2e247a574a7bdf742608f4294f1574425
Sean Chen <seanchen112@gmail.com>
2023-08-30 23:14:20 +0800

    fix: get user liked tweets

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,24 +1,24 @@
 module.exports = {
-  apiErrorHandler(err, req, res, next) {
+  apiErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
-      if (err.message === "帳號不存在") {
+      if (err.message === '帳號不存在') {
         return res.status(200).json({
-          status: "error",
-          statusCode: "404",
-          message: `${err.message}`,
-        });
+          status: 'error',
+          statusCode: '404',
+          message: `${err.message}`
+        })
       }
       res.status(200).json({
-        status: "error",
-        statusCode: "400",
-        message: `${err.message}`,
-      });
+        status: 'error',
+        statusCode: '400',
+        message: `${err.message}`
+      })
     } else {
       res.status(500).json({
-        status: "error",
-        message: `${err}`,
-      });
+        status: 'error',
+        message: `${err}`
+      })
     }
-    next(err);
-  },
-};
+    next(err)
+  }
+}
