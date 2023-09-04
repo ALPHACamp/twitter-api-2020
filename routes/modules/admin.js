7c8f781ec36ddde66035dafff7ba2a9a6694fdce
yiching-hsu <ychsu7149@gmail.com>
2023-09-04 21:21:47 +0800

    fix: conflict

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,19 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/apis/admin-controller')
-<<<<<<< HEAD
-const passport = require('passport')
+
 const { authenticated, authenticatedAdmin } = require('../middleware/api-auth')
 // router.post('/login', passport.authenticate('jwt', { session: false }), adminController.signIn)
-=======
-
-const {
-  authenticated,
-  authenticatedAdmin
-} = require('../../middleware/api-auth')
-
-router.post('/signin', adminController.signIn)
->>>>>>> 4397115c6ac10aa993d100d5a86d715bf599b3af
 
 router.get(
   '/users',
