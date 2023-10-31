52e5a2c96c1fe5974e78614fba57bbd8de0c22e7
paul.wu <tease0623@gmail.com>
2023-10-31 16:16:03 +0800

    fix eslint error

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
 
 const app = express()
 const port = 3000
 
 // use helpers.getUser(req) to replace req.user
-function authenticated(req, res, next){
+function authenticated (req, res, next) {
   // passport.authenticate('jwt', { ses...
 };
 
