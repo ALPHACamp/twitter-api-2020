008873b724fb47982756a7115362dafedc7d9a1a
Sunnylin0320 <sunny8503201@gmail.com>
2023-11-02 11:11:23 +0800

    feat: auth middleware

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const { errorHandler } = require('../middleware/error-handler')
 const userController = require('../controllers/user-controller')
 
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+
 router.post('/api/users', userController.signUp)
 router.use('/', errorHandler)
 module.exports = router
