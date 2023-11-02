986de1a611d5e3aeb4e3099ca529911dc4c781f3
Sunnylin0320 <sunny8503201@gmail.com>
2023-11-03 00:09:15 +0800

    feat: admin get users

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
-// const { User } = require('../models')
+const { User } = require('../models')
+const sequelize = require('sequelize')
 
 const adminController = {
   signIn: (req, res, next) => {
@@ -18,6 +19,55 @@ const adminController = {
       })
     } catch (err) {
       next(err)
+    }
+  },
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({
+        where: { role: 'user' },
+        attributes: [
+          'id',
+          'name',
+          'account',
+          'avatar',
+          [
+            sequelize.literal(
+              '(SELECT COUNT(*) FROM Tweets WHERE Tweets.UserId = User.id)'
+            ),
+            'tweetsAmount'
+          ],
+          [
+            sequelize.literal(
+              '(SELECT COUNT(*) FROM Likes WHERE Likes.TweetId IN (SELECT id FROM Tweets WHERE Tweets.UserId = User.id))'
+            ),
+            'likesAmount'
+          ],
+          [
+            sequelize.literal(
+              '(SELECT COUNT(*) FROM Followships WHERE Followships.followerId = User.id)'
+            ),
+            'followingsAmount'
+          ],
+          [
+            sequelize.literal(
+              '(SELECT COUNT(*) FROM Followships WHERE Followships.followingId = User.id)'
+            ),
+            'followersAmount'
+          ]
+        ],
+        order: [
+          [sequelize.literal('tweetsAmount'), 'DESC'],
+          [sequelize.literal('likesAmount'), 'DESC'],
+          [sequelize.literal('followingsAmount'), 'DESC'],
+          [sequelize.literal('followersAmount'), 'DESC']
+        ],
+        nest: true,
+        raw: true
+      })
+
+      return res.status(200).json(users)
+    } catch (err) {
+      return next(err)
     }
   }
 }
