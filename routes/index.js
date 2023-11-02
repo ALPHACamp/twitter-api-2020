7575e60183a7bb5b8b8d62bf5bfef81c2759e2a2
Sunnylin0320 <sunny8503201@gmail.com>
2023-11-02 14:39:32 +0800

    feat: admin signin

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const admin = require('./modules/admin')
 
 const { errorHandler } = require('../middleware/error-handler')
+const { adminSignin } = require('../middleware/signin-handler')
 const userController = require('../controllers/user-controller')
+const adminController = require('../controllers/admin-controller')
 
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
@@ -10,6 +13,8 @@ const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 router.post('/api/users/signin', userController.signIn)
 router.post('/api/users', userController.signUp)
 
+router.post('/api/admin/signin', adminSignin, adminController.signIn) // admin登入
+router.use('/api/admin', authenticated, authenticatedAdmin, admin) // admin其他3支路由
 router.use('/', errorHandler)
 
 module.exports = router
