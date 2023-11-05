1269745512d01070885d8e7ecdb0365bab8f30b4
blade <blade8128ch@gmail.com>
2023-11-06 00:12:15 +0800

    feature : signup api

--- controllers/apis/user-controller.js
+++ controllers/apis/user-controller.js
@@ -1,5 +1,42 @@
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const { User, Tweet, Reply, Like, Followship, sequelize } = require('../../models')
+const { Op } = require("sequelize");
+
 const userController = {
+  signUp: (req, res, next) => {
+    console.log("7777",req.body,"7777")
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    User.findOne( { where: { [Op.or]: [{email: req.body.email} , {account: req.body.account}] } } )
+    .then(user => {
+        console.log("6666",user,"6666")
+        if(user===null) user=[]
+        console.log("5555",user,"5555")
+          
+          if (user.account===req.body.account) throw new Error('account 已重複註冊！')
+          else if (user.email===req.body.email) throw new Error('email 已重複註冊！')
+        
+
+        return bcrypt.hash(req.body.password, 10)
+      })
+      .then(hash => User.create({
+        account:req.body.account,
+        name: req.body.name,
+        email: req.body.email,
+        role: 'user',
+        password: hash
+      }))
+      .then((createdUser) => {
+        createdUser.toJSON()
+        delete createdUser.password
+        return res.json({
+          status: 'success',
+          message: '註冊成功！',
+          ...createdUser
+        })
+      })
+      .catch(err => next(err))
+  },
   signIn: (req, res, next) => {
     try {
       const userData = req.user.toJSON()
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -7,8 +7,8 @@ const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth
 const { apiErrorHandler } = require('../../middleware/error-handler')
 router.use('/admin', authenticated, authenticatedAdmin, admin)
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
-//const restController = require('../../controllers/apis/restaurant-controller')
-//router.get('/restaurants', restController.getRestaurants)
+router.post('/users', userController.signUp)
+
 router.use('/', apiErrorHandler)
 
 module.exports = router
