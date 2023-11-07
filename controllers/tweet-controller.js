3b4ee9a59f5c631559449f508305b0708cde0f0b
Sunnylin0320 <sunny8503201@gmail.com>
2023-11-07 15:33:12 +0800

    fix user addLike

--- controllers/tweet-controller.js
+++ controllers/tweet-controller.js
@@ -19,6 +19,7 @@ const tweetController = {
       const data = tweets.map(tweet => ({
         ...tweet.toJSON(),
         createdAt: relativeTimeFromNow(tweet.createdAt),
+        isLiked: tweet.LikedUsers?.some(like => like.TweetId === tweet.id) || false,
         repliesAmount: tweet.Replies.length || 0,
         likesAmount: tweet.LikedUsers.length || 0
       }))
@@ -33,7 +34,7 @@ const tweetController = {
       const { description } = req.body
 
       if (!description) {
-        throw new Error('內容不可空白！')
+        throw new Error('內容不可空白')
       } else if (description.length > 140) {
         throw new Error('推文字數不可超過140字！')
       }
@@ -71,11 +72,18 @@ const tweetController = {
         ],
         order: [[{ model: Reply }, 'createdAt', 'DESC']]
       })
+      const repliesData = tweet.Replies.map(reply => ({
+        ...reply.toJSON(),
+        createdAt: relativeTimeFromNow(reply.createdAt)
+      }))
       const data = {
         ...tweet.toJSON(),
         createdAt: relativeTimeFromNow(tweet.createdAt),
         createdAtDate: formatDate(tweet.createdAt),
         createdAtTime: formatTime(tweet.createdAt),
+        Replies: repliesData,
+        isLiked:
+          tweet.LikedUsers?.some(like => like.TweetId === tweet.id) || false,
         likesAmount: tweet.LikedUsers.length || 0
       }
 
@@ -96,18 +104,28 @@ const tweetController = {
         })
       ])
 
-      if (!tweet) throw new Error('推文不存在！')
-      if (like) throw new Error('你已經對這則推文按過喜歡！')
+      if (!tweet) {
+        throw new Error('推文不存在！')
+      } else if (like) {
+        throw new Error('你已經對這則推文按過喜歡！')
+      }
 
       await Like.create({
         UserId,
         TweetId
       })
 
+      const isLiked = true
+      const likeCount = await Like.count({ where: { TweetId } })
+
       return res.status(200).json({
         status: 'success',
         message: '成功對這則推文按下喜歡！',
-        likedTweet: tweet
+        likedTweet: {
+          ...tweet.toJSON(),
+          isLiked,
+          likeCount
+        }
       })
     } catch (err) {
       next(err)
@@ -177,7 +195,7 @@ const tweetController = {
 
       if (!comment || comment.length > 140) {
         throw new Error(
-          !comment ? '回覆內容不可空白！' : '回覆字數不可超過140字！'
+          !comment ? '內容不可空白' : '回覆字數不可超過140字！'
         )
       }
 
